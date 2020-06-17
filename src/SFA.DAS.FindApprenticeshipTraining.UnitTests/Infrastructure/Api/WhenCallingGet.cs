d1d3b70672013c7990ee3b75db3a6784855716b1
Scott Wakefield <scott@expertcc.co.uk>
2020-06-17 15:32:07 +0100

    FAT2-80 added test for api client.

---
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/Infrastructure/Api/WhenCallingGet.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Infrastructure.Api
+{
+    public class WhenCallingGet
+    {
+        [Test]
+        public async Task Then_Request_Url_Is_Used()
+        {
+
+        }
+        
+        [Test]
+        public async Task Then_Deserialises_Response()
+        {
+
+        }
+
+        [Test]
+        public async Task And_Not_Success_Then_Throws_Exception()
+        {
+
+        }
+    }
+}
