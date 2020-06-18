0c99108b149f647117d21d97cf96153d9def8bcc
Scott Wakefield <scott@expertcc.co.uk>
2020-06-18 09:42:34 +0100

    FAT2-80 added test for get list url.

---
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/Domain/InnerApi/Requests/WhenBuildingTheGetStandardsListRequest.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.Application.Domain.InnerApi.Requests;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Domain.InnerApi.Requests
+{
+    public class WhenBuildingTheGetStandardsListRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Constructed(
+            string baseUrl)
+        {
+            var actual = new GetStandardsListRequest{BaseUrl = baseUrl};
+
+            actual.BaseUrl.Should().Be(baseUrl);
+            actual.GetUrl.Should().Be($"{baseUrl}api/courses/standards");
+        }
+    }
+}
