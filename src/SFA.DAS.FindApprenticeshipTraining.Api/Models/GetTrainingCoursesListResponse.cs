543ab29e5787b187f6798d1090239b8c93662713
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-22 16:43:41 +0100

    Update controller to take keyword from query
    Keyword is passed to inner api from request

--- src/SFA.DAS.FindApprenticeshipTraining.Api/Models/GetTrainingCoursesListResponse.cs
+++ src/SFA.DAS.FindApprenticeshipTraining.Api/Models/GetTrainingCoursesListResponse.cs
@@ -5,5 +5,7 @@ namespace SFA.DAS.FindApprenticeshipTraining.Api.Models
     public class GetTrainingCoursesListResponse
     {
         public IEnumerable<GetTrainingCourseListItem> TrainingCourses { get; set; }
+        public int Total { get ; set ; }
+        public int TotalFiltered { get ; set ; }
     }
 }
