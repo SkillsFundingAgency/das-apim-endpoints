0c4b3db5875b8d13e8eac67fc51686c8e148e07a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-06-24 12:36:54 +0100

    Add mapping to api return type from mediator result sectors

---
+++ src/SFA.DAS.FindApprenticeshipTraining.Api.UnitTests/Models/WhenCastingGetTrainingCourseResponseToSector.cs
@@ -0,0 +1,21 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.Api.Models;
+using SFA.DAS.FindApprenticeshipTraining.Application.InnerApi.Responses;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Api.UnitTests.Models
+{
+    public class WhenCastingGetTrainingCourseResponseToSector
+    {
+        [Test, AutoData]
+        public void Then_Maps_Fields_Appropriately(
+            GetSectorsListItem source
+        )
+        {
+            var response = (GetTrainingSectorsListItem) source;
+            
+            response.Should().BeEquivalentTo(source);
+        }
+    }
+}
