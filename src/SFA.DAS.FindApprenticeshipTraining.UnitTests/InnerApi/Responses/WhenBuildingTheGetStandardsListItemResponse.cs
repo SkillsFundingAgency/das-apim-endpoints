5e28420320a8f3bfd58e865a5a6e86be4ef54214
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-02 15:45:41 +0100

    Re-organise tests to make the intent clearer

--- src/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Responses/WhenBuildingTheGetStandardsListItemResponse.cs
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Responses/WhenBuildingTheGetStandardsListItemResponse.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using AutoFixture.NUnit3;
+using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.FindApprenticeshipTraining.Application.InnerApi.Responses;
 
@@ -13,8 +14,8 @@ namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Responses
         [Test]
         public void Then_If_There_Is_No_Available_Funding_Zero_Is_Returned()
         {
-            var standards = new List<GetStandardsListItem>
-                {
+            //Arrange/Act
+            var standard = 
                     new GetStandardsListItem
                     {
                         Title = "Available",
@@ -27,101 +28,112 @@ namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Responses
                                 LastDateStarts = null
                             }
                         }
-                    }
-                
-            };
+                    };
             
             //Assert
-            Assert.IsTrue(standards.ToList().TrueForAll(c=>c.MaxFunding.Equals(0)));
+            standard.MaxFunding.Should().Be(0);
         }
-        
+
         [Test, AutoData]
-        public void Then_The_Funding_Amount_Is_Taken_From_The_Valid_ApprenticeshipFunding_List(long fundingPrice, long notFundingPrice)
+        public void Then_The_ApprenticeshipFunding_Price_With_No_EffectiveTo_Date_And_Has_A_From_Date_In_The_Past_Is_Used(long fundingPrice)
         {
-            var standards = new List<GetStandardsListItem>
+            //Arrange / Act
+            var standard = new GetStandardsListItem
+            {
+                Title = "Available",
+                ApprenticeshipFunding = new List<ApprenticeshipFunding>
                 {
-                    new GetStandardsListItem
+                    new ApprenticeshipFunding
                     {
-                        Title = "Available",
-                        ApprenticeshipFunding = new List<ApprenticeshipFunding>
-                        {
-                            new ApprenticeshipFunding
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddDays(-1),
-                                EffectiveTo = null,
-                                MaxEmployerLevyCap = fundingPrice
-                            }
-                        },
-                        StandardDates = new List<StandardDate>
-                        {
-                            new StandardDate
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddMonths(-1),
-                                LastDateStarts = null
-                            }
-                        }
-                    },
-                    new GetStandardsListItem
-                    {
-                        Title = "Available",
-                        ApprenticeshipFunding = new List<ApprenticeshipFunding>
-                        {
-                            new ApprenticeshipFunding
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddDays(-10),
-                                EffectiveTo = DateTime.UtcNow.AddDays(-9),
-                                MaxEmployerLevyCap = notFundingPrice
-                            },
-                            new ApprenticeshipFunding
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddDays(-1),
-                                EffectiveTo = null,
-                                MaxEmployerLevyCap = fundingPrice
-                            }
-                        },
-                        StandardDates = new List<StandardDate>
-                        {
-                            new StandardDate
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddMonths(-1),
-                                LastDateStarts = null
-                            }
-                        }
-                        
-                    },
-                    new GetStandardsListItem
-                    {
-                        Title = "Available",
-                        ApprenticeshipFunding = new List<ApprenticeshipFunding>
-                        {
-                            new ApprenticeshipFunding
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddDays(-10),
-                                EffectiveTo = DateTime.UtcNow.AddDays(9),
-                                MaxEmployerLevyCap = fundingPrice
-                            },
-                            new ApprenticeshipFunding
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddDays(4),
-                                EffectiveTo = null,
-                                MaxEmployerLevyCap = notFundingPrice
-                            }
-                        },
-                        StandardDates = new List<StandardDate>
-                        {
-                            new StandardDate
-                            {
-                                EffectiveFrom = DateTime.UtcNow.AddMonths(-1),
-                                LastDateStarts = null
-                            }
-                        }
-                        
+                        EffectiveFrom = DateTime.UtcNow.AddDays(-1),
+                        EffectiveTo = null,
+                        MaxEmployerLevyCap = fundingPrice
                     }
-                
+                },
+                StandardDates = new List<StandardDate>
+                {
+                    new StandardDate
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddMonths(-1),
+                        LastDateStarts = null
+                    }
+                }
             };
             
             //Assert
-            Assert.IsTrue(standards.ToList().TrueForAll(c=>c.MaxFunding.Equals(fundingPrice)));
+            standard.MaxFunding.Should().Be(fundingPrice);
+        }
+
+        [Test, AutoData]
+        public void Then_The_Current_Funding_Price_Is_Used(long notFundingPrice, long fundingPrice)
+        {
+            //Arrange / Act
+            var standard = new GetStandardsListItem
+            {
+                Title = "Available",
+                ApprenticeshipFunding = new List<ApprenticeshipFunding>
+                {
+                    new ApprenticeshipFunding
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddDays(-10),
+                        EffectiveTo = DateTime.UtcNow.AddDays(-9),
+                        MaxEmployerLevyCap = notFundingPrice
+                    },
+                    new ApprenticeshipFunding
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddDays(-1),
+                        EffectiveTo = null,
+                        MaxEmployerLevyCap = fundingPrice
+                    }
+                },
+                StandardDates = new List<StandardDate>
+                {
+                    new StandardDate
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddMonths(-1),
+                        LastDateStarts = null
+                    }
+                }
+            };
+            
+            //Assert
+            standard.MaxFunding.Should().Be(fundingPrice);
+        }
+
+        [Test, AutoData]
+        public void Then_The_Future_Price_Is_Not_Used(long fundingPrice, long notFundingPrice)
+        {
+            //Arrange / Act
+            var standard = new GetStandardsListItem
+            {
+                Title = "Available",
+                ApprenticeshipFunding = new List<ApprenticeshipFunding>
+                {
+                    new ApprenticeshipFunding
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddDays(-10),
+                        EffectiveTo = DateTime.UtcNow.AddDays(9),
+                        MaxEmployerLevyCap = fundingPrice
+                    },
+                    new ApprenticeshipFunding
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddDays(4),
+                        EffectiveTo = null,
+                        MaxEmployerLevyCap = notFundingPrice
+                    }
+                },
+                StandardDates = new List<StandardDate>
+                {
+                    new StandardDate
+                    {
+                        EffectiveFrom = DateTime.UtcNow.AddMonths(-1),
+                        LastDateStarts = null
+                    }
+                }
+            };
+            
+            //Assert
+            standard.MaxFunding.Should().Be(fundingPrice);
         }
     }
 }
