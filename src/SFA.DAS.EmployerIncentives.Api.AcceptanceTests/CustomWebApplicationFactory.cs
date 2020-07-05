2ff6253d8af5f82c3dc494abe1d9be9559848509
Paul Graham <paul.graham@coprime.co.uk>
2020-07-05 08:03:59 +0100

    Added basic infrastructure tests

---
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests
+{
+    public class CustomWebApplicationFactory<TEntryPoint> : WebApplicationFactory<TEntryPoint> where TEntryPoint : class
+    {
+        //protected override void ConfigureWebHost(IWebHostBuilder builder)
+        //{
+        //    builder.UseEnvironment(SFA.DAS.CommitmentsV2.Domain.Constants.IntegrationTestEnvironment);
+        //}
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Pipeline/HealthCheckTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Pipeline
+{
+    [TestFixture]
+    [Parallelizable]
+    public class WhenCheckingPipeLine
+    {
+
+        [Test]
+        public async Task WhenCall() { }
+
+
+        [Test]
+        public async Task CallPingEndpoint_ThenShouldReturnOkResponse()
+        {
+            var f = new HealthCheckFixture();
+            var r =  await f.Client.GetAsync("/ping");
+            r.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Test]
+        public async Task CallHealthEndpoint_ThenShouldReturnOkResponse()
+        {
+            var f = new HealthCheckFixture();
+            var r = await f.Client.GetAsync("/health");
+            r.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+    }
+
+    public class HealthCheckFixture
+    {
+        public readonly WebApplicationFactory<Startup> Factory;
+        public readonly HttpClient Client;
+
+
+        public HealthCheckFixture()
+        {
+            Factory = new CustomWebApplicationFactory<Startup>();
+
+            Client = Factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+    }
+}
