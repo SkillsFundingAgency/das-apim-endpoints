71a4c5202efdaf8d27da80bb1f578c8f017410ac
adam230594 <47275045+adam230594@users.noreply.github.com>
2020-07-21 13:52:19 +0100

    Updated Title to ReservationsOuterApi

--- src/SFA.DAS.Reservations.Api/Startup.cs
+++ src/SFA.DAS.Reservations.Api/Startup.cs
@@ -60,7 +60,7 @@ namespace SFA.DAS.Reservations.Api
 
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReservationsApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ReservationsOuterApi", Version = "v1" });
             });
         }
 
