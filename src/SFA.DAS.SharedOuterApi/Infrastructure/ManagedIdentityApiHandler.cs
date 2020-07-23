58a8c201f90f9c713bbc62a66ea08ff51455f688
Paul Graham <paul.graham@coprime.co.uk>
2020-07-23 16:00:01 +0100

    moved restApiClient and PassThrough Api Client into shared

---
+++ src/SFA.DAS.SharedOuterApi/Infrastructure/ManagedIdentityApiHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Services.AppAuthentication;
+
+namespace SFA.DAS.SharedOuterApi.Infrastructure
+{
+    public class ManagedIdentityApiHandler : DelegatingHandler
+    {
+        private readonly string _managedIdentifier;
+
+        public ManagedIdentityApiHandler(string managedIdentifier) 
+        {
+            _managedIdentifier = managedIdentifier;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (!string.IsNullOrWhiteSpace(_managedIdentifier) && !request.Headers.Contains("authentication"))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await GetAccessTokenAsync());
+            }
+                            
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var accessToken = await azureServiceTokenProvider.GetAccessTokenAsync(_managedIdentifier);
+
+            return accessToken;
+        }
+    }
+}
---
+++ src/SFA.DAS.SharedOuterApi/Interfaces/IPassThroughApiClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+
+namespace SFA.DAS.SharedOuterApi.Interfaces
+{
+    public interface IPassThroughApiClient
+    {
+        Task<InnerApiResponse> Get(Uri uri, object queryData = null, CancellationToken cancellationToken = default);
+        Task<InnerApiResponse> Get(string uri, object queryData = null, CancellationToken cancellationToken = default);
+        Task<InnerApiResponse> Post(string uri, CancellationToken cancellationToken = default);
+        Task<InnerApiResponse> Post<TRequest>(string uri, TRequest request, CancellationToken cancellationToken = default) where TRequest : class;
+        Task<InnerApiResponse> Delete(string uri, CancellationToken cancellationToken = default);
+    }
+}
