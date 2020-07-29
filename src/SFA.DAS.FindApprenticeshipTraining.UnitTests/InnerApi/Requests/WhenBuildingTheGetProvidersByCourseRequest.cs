8d64ac31b6e6c7459b5f592205f42d62420d67ed
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-29 09:56:24 +0100

    Add GetProvidersByCourseRequest

---
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Requests/WhenBuildingTheGetProvidersByCourseRequest.cs
@@ -0,0 +1,21 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.Application.InnerApi.Requests;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetProvidersByCourseRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(string baseUrl, int courseId)
+        {
+            var actual = new GetProvidersByCourseRequest(courseId)
+            {
+                BaseUrl = baseUrl
+            };
+
+            actual.GetUrl.Should().Be($"{baseUrl}api/courses/{courseId}/providers");
+        }
+    }
+}
