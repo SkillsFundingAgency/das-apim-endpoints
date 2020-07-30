79f2a13fc660d7ab94b9105caee326368b806116
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-30 20:17:43 +0100

    Add GetEligibleApprenticeshipsRequest

---
+++ src/SFA.DAS.EmployerIncentives.UnitTests/InnerApi/Requests/WhenBuildingTheGetEligibleApprenticeshipsRequest.cs
@@ -0,0 +1,23 @@
+using System;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests;
+
+namespace SFA.DAS.EmployerIncentives.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetEligibleApprenticeshipsRequest
+    {
+        [Test, AutoData]
+        public void Then_The_GetUrl_Is_Correctly_Built(string baseUrl,long uln, DateTime startDate)
+        {
+            var actual = new GetEligibleApprenticeshipsRequest(uln, startDate)
+            {
+                BaseUrl = baseUrl
+            };
+
+            actual.GetUrl.Should()
+                .Be($"{baseUrl}eligible-apprenticeships/{uln}?startDate={startDate:yyyy-MM-dd}&isApproved=true");
+        }
+    }
+}
