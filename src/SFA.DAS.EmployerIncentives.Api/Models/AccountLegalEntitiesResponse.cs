60adf5ff119604f2f5dd50862329a3b230951b68
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-31 10:20:39 +0100

    Add DTO mapper for AccountLegalEntity

---
+++ src/SFA.DAS.EmployerIncentives.Api/Models/AccountLegalEntitiesResponse.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.EmployerIncentives.Api.Models
+{
+    public class AccountLegalEntitiesResponse
+    {
+        public AccountLegalEntityDto[] AccountLegalEntities { get; set; }
+    }
+}
