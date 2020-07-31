eff7d052b1cfc025815fe860e4293933e1396de3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-07-31 13:12:27 +0100

    Add test for patch request for Signed Agreement request

---
+++ src/SFA.DAS.EmployerIncentives.UnitTests/InnerApi/Requests/WhenBuildingPatchSignAgreementRequest.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests;
+
+namespace SFA.DAS.EmployerIncentives.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingPatchSignAgreementRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Patch_Url_Is_Correctly_Built(long accountId, long accountLegalEntityId, string baseUrl, List<string> data)
+        {
+            var actual = new PatchSignAgreementRequest(accountId,accountLegalEntityId){BaseUrl = baseUrl, Data = data};
+
+            actual.PatchUrl.Should().Be($"{baseUrl}accounts/{accountId}/legalentities/{accountLegalEntityId}");
+            actual.Data.Should().BeEquivalentTo(data);
+        }
+    }
+}
