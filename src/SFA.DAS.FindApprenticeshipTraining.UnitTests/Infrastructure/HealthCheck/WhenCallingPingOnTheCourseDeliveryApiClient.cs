dab1af5fcd7b4d2a8e0c5b44983cad9380ad4be7
Peter Williams <peter.david.williams.1991@gmail.com>
2020-08-03 16:54:25 +0100

    Added health checks for courses api and course delivery api

---
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/Infrastructure/HealthCheck/WhenCallingPingOnTheCourseDeliveryApiClient.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests;
+using SFA.DAS.FindApprenticeshipTraining.Infrastructure;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Infrastructure.HealthCheck
+{
+    public class WhenCallingPingOnTheCourseDeliveryApiClient
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Ping_Endpoint_Is_Called_For_CourseDeliveryApi(
+            [Frozen] Mock<ICourseDeliveryApiClient<CourseDeliveryApiConfiguration>> client,
+            HealthCheckContext healthCheckContext,
+            CourseDeliveryApiHealthCheck healthCheck)
+        {
+            //Act
+            await healthCheck.CheckHealthAsync(healthCheckContext, CancellationToken.None);
+            //Assert
+            client.Verify(x => x.GetResponseCode(It.IsAny<GetPingRequest>()), Times.Once);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_If_It_Is_Successful_200_Is_Returned(
+            [Frozen] Mock<ICourseDeliveryApiClient<CourseDeliveryApiConfiguration>> client,
+            HealthCheckContext healthCheckContext,
+            CourseDeliveryApiHealthCheck healthCheck)
+        {
+            //Arrange
+            client.Setup(x => x.GetResponseCode(It.IsAny<GetPingRequest>()))
+                .ReturnsAsync(HttpStatusCode.OK);
+            //Act
+            var actual = await healthCheck.CheckHealthAsync(healthCheckContext, CancellationToken.None);
+            //Assert
+            Assert.AreEqual(HealthStatus.Healthy, actual.Status);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_If_It_Is_Not_Successful_An_Exception_Is_Thrown(
+            [Frozen] Mock<ICourseDeliveryApiClient<CourseDeliveryApiConfiguration>> client,
+            HealthCheckContext healthCheckContext,
+            CourseDeliveryApiHealthCheck healthCheck)
+        {
+            //Arrange
+            client.Setup(x => x.GetResponseCode(new GetPingRequest()))
+                .ReturnsAsync(HttpStatusCode.NotFound);
+            //Act
+            var actual = await healthCheck.CheckHealthAsync(healthCheckContext, CancellationToken.None);
+            //Assert
+            Assert.AreEqual(HealthStatus.Unhealthy, actual.Status);
+        }
+    }
+}
---
+++ src/SFA.DAS.FindApprenticeshipTraining/Infrastructure/HealthCheck/CourseDeliveryApiHealthCheck.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests;
+using SFA.DAS.FindApprenticeshipTraining.Infrastructure.Extensions.SFA.DAS.FAT.Infrastructure.Extensions;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Infrastructure
+{
+    public class CourseDeliveryApiHealthCheck :IHealthCheck
+    {
+        private const string HealthCheckResultDescription = "Courses Api check";
+
+        private readonly ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> _apiClient;
+        private readonly ILogger<CourseDeliveryApiHealthCheck> _logger;
+
+        public CourseDeliveryApiHealthCheck(ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> apiClient, ILogger<CourseDeliveryApiHealthCheck> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Pinging Courses API");
+
+            var timer = Stopwatch.StartNew();
+            var response = await _apiClient.GetResponseCode(new GetPingRequest());
+            timer.Stop();
+
+            if ((int)response == 200)
+            {
+                var durationString = timer.Elapsed.ToHumanReadableString();
+
+                _logger.LogInformation($"Courses API ping successful and took {durationString}");
+
+                return HealthCheckResult.Healthy(HealthCheckResultDescription,
+                    new Dictionary<string, object> { { "Duration", durationString } });
+            }
+
+            _logger.LogWarning($"Courses API ping failed : [Code: {response}]");
+            return HealthCheckResult.Unhealthy(HealthCheckResultDescription);
+        }
+    }
+}
