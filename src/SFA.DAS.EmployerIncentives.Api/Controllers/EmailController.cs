7a668ead20abc1f81635852bb93169e5ae5967e4
Dave Read <david.read@digital.education.gov.uk>
2020-08-11 20:15:12 +0100

    EI-145 updated controller route prefix

--- src/SFA.DAS.EmployerIncentives.Api/Controllers/EmailController.cs
+++ src/SFA.DAS.EmployerIncentives.Api/Controllers/EmailController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace SFA.DAS.EmployerIncentives.Api.Controllers
 {
     [ApiController]
+    [Route("email")]
     public class EmailController : ControllerBase
     {
         private readonly IMediator _mediator;
