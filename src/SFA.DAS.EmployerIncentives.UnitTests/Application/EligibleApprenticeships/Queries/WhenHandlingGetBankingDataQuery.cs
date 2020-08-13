d8eb2807983e03375e82027e41c3beb4b74cf7af
Michael Young <mick.young1981@googlemail.com>
2020-08-13 08:58:29 +0100

    Initial query handler

---
+++ src/SFA.DAS.EmployerIncentives.UnitTests/Application/EligibleApprenticeships/Queries/WhenHandlingGetBankingDataQuery.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.InnerApi.Responses;
+using SFA.DAS.EmployerIncentives.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerIncentives.Application.Queries.GetBankingData;
+
+namespace SFA.DAS.EmployerIncentives.UnitTests.Application.EligibleApprenticeships.Queries
+{
+    public class WhenHandlingGetBankingDataQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Application_Data_Is_Returned(
+            GetBankingDataQuery query,
+            IncentiveApplicationDto applicationResponse,
+            [Frozen] Mock<IEmployerIncentivesService> employerIncentivesService,
+            GetBankingDataHandler handler
+            )
+        {
+            employerIncentivesService.Setup(x => x.GetApplication(query.AccountId, query.ApplicationId)).ReturnsAsync(applicationResponse);
+
+            var actual = await handler.Handle(query, CancellationToken.None);
+
+            actual.Data.ApplicantEmail.Should().Be("TODO");
+            actual.Data.ApplicantName.Should().Be("TODO");
+            actual.Data.LegalEntityId.Should().Be(0);
+            actual.Data.ApplicationValue.Should().Be(applicationResponse.Apprenticeships.Sum(x => x.TotalIncentiveAmount));
+            actual.Data.VendorCode.Should().Be("00000000");
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives/Application/Queries/GetBankingData/GetBankingDataHandler.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerIncentives.Interfaces;
+using SFA.DAS.EmployerIncentives.Models;
+
+namespace SFA.DAS.EmployerIncentives.Application.Queries.GetBankingData
+{
+    public class GetBankingDataHandler : IRequestHandler<GetBankingDataQuery, GetBankingDataResult>
+    {
+        private readonly IEmployerIncentivesService _employerIncentivesService;
+
+        public GetBankingDataHandler(IEmployerIncentivesService employerIncentivesService)
+        {
+            _employerIncentivesService = employerIncentivesService;
+        }
+        public async Task<GetBankingDataResult> Handle(GetBankingDataQuery request, CancellationToken cancellationToken)
+        {
+            var application = await _employerIncentivesService.GetApplication(request.AccountId, request.ApplicationId);
+
+            var bankingData = new BankingData
+            {
+                VendorCode = "00000000",
+                LegalEntityId = 0, //TODO
+                ApplicantEmail = "TODO", //TODO
+                ApplicantName = "TODO", //TODO
+                ApplicationValue = application.Apprenticeships.Sum(x => x.TotalIncentiveAmount)
+            };
+
+            return new GetBankingDataResult
+            {
+                Data = bankingData
+            };
+        }
+    }
+}
