6889c7bc41f6a02e8bb6fe75f1921afae34a6b1f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-21 16:43:18 +0100

    Add query handler for location search

---
+++ src/SFA.DAS.FindApprenticeshipTraining/Application/Locations/GetLocations/GetLocationsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Application.Locations.GetLocations
+{
+    public class GetLocationsResponse
+    {
+        public IEnumerable<GetLocationsListItem> Locations { get ; set ; }
+    }
+}
