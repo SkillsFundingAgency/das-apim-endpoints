ec035fbd9b966d22039ff9bbc0f5eff87b7a6c47
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-24 08:56:10 +0100

    Add locations controller to outer API

---
+++ src/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/LocationsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.FindApprenticeshipTraining.Api.Models;
+using SFA.DAS.FindApprenticeshipTraining.Application.Locations.GetLocations;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class LocationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<LocationsController> _logger;
+
+        public LocationsController (IMediator mediator, ILogger<LocationsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetByQuery([FromQuery]string searchTerm)
+        {
+            try
+            {
+                var queryResult = await _mediator.Send(new GetLocationsQuery {SearchTerm = searchTerm});
+                
+                var response = new GetLocationSearchResponse
+                {
+                    Locations = queryResult.Locations.Select(c=>(GetLocationSearchResponse.GetLocationSearchResponseItem)c)
+                };
+
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error attempting to get list of locations, search term:{searchTerm}");
+                return BadRequest();
+            }
+        }
+    }
+}
