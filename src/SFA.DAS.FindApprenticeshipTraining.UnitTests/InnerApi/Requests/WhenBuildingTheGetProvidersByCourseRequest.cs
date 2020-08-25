78872da3aee3900cd2b28a9aa7146ca6267e4f52
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-25 08:37:47 +0100

    Add location information to get providers for course request

--- src/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Requests/WhenBuildingTheGetProvidersByCourseRequest.cs
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Requests/WhenBuildingTheGetProvidersByCourseRequest.cs
@@ -8,14 +8,25 @@ namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Requests
     public class WhenBuildingTheGetProvidersByCourseRequest
     {
         [Test, AutoData]
-        public void Then_The_Url_Is_Correctly_Built(string baseUrl, int courseId)
+        public void Then_The_Url_Is_Correctly_Built(string baseUrl, int courseId, double latitude, double longitude)
+        {
+            var actual = new GetProvidersByCourseRequest(courseId, latitude, longitude)
+            {
+                BaseUrl = baseUrl
+            };
+
+            actual.GetUrl.Should().Be($"{baseUrl}api/courses/{courseId}/providers?latitude={latitude}&longitude={longitude}");
+        }
+
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built_With_No_Location(string baseUrl, int courseId)
         {
             var actual = new GetProvidersByCourseRequest(courseId)
             {
                 BaseUrl = baseUrl
             };
 
-            actual.GetUrl.Should().Be($"{baseUrl}api/courses/{courseId}/providers");
+            actual.GetUrl.Should().Be($"{baseUrl}api/courses/{courseId}/providers?latitude=&longitude=");
         }
     }
 }
--- src/SFA.DAS.FindApprenticeshipTraining/InnerApi/Requests/GetProvidersByCourseRequest.cs
+++ src/SFA.DAS.FindApprenticeshipTraining/InnerApi/Requests/GetProvidersByCourseRequest.cs
@@ -4,14 +4,18 @@ namespace SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests
 {
     public class GetProvidersByCourseRequest : IGetApiRequest
     {
-        private int _courseId { get ; set ; }
+        private readonly double? _latitude;
+        private readonly double? _longitude;
+        private int CourseId { get ;}
         
-        public GetProvidersByCourseRequest (int id)
+        public GetProvidersByCourseRequest (int id, double? latitude = null, double? longitude = null)
         {
-            _courseId = id;
+            _latitude = latitude;
+            _longitude = longitude;
+            CourseId = id;
         }
         public string BaseUrl { get; set; }
         
-        public string GetUrl => $"{BaseUrl}api/courses/{_courseId}/providers";
+        public string GetUrl => $"{BaseUrl}api/courses/{CourseId}/providers?latitude={_latitude}&longitude={_longitude}";
     }
 }
