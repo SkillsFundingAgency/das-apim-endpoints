b555ee5de065f50f34d72903fe8d4b77a67cc23a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-08-25 08:30:54 +0100

    Add Request for getting location by name and authority

---
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Requests/WhenBuildingTheGetLocationByLocationAndAuthorityRequest.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetLocationByLocationAndAuthorityRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Request_Is_Correctly_Built(string baseUrl, string locationName, string authorityName)
+        {
+            var actual = new GetLocationByLocationAndAuthorityName(locationName, authorityName)
+            {
+                BaseUrl = baseUrl
+            };
+
+            actual.GetUrl.Should()
+                .Be($"{baseUrl}api/locations?locationName={locationName}&authorityName={authorityName}");
+        }
+    }
+}
---
+++ src/SFA.DAS.FindApprenticeshipTraining/InnerApi/Requests/GetLocationByLocationAndAuthorityName.cs
@@ -0,0 +1,19 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests
+{
+    public class GetLocationByLocationAndAuthorityName : IGetApiRequest
+    {
+        private readonly string _locationName;
+        private readonly string _authorityName;
+
+        public GetLocationByLocationAndAuthorityName(string locationName, string authorityName)
+        {
+            _locationName = locationName;
+            _authorityName = authorityName;
+        }
+
+        public string BaseUrl { get; set; }
+        public string GetUrl  => $"{BaseUrl}api/locations?locationName={_locationName}&authorityName={_authorityName}";
+    }
+}
