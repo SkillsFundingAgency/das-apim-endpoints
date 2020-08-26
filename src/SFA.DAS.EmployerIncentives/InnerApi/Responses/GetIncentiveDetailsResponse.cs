69c3a99c158f1b5416efac6f248aa7b51d0cc6ef
Michael Young <mick.young1981@googlemail.com>
2020-08-26 13:36:55 +0100

    Add service method.

---
+++ src/SFA.DAS.EmployerIncentives/InnerApi/Responses/GetIncentiveDetailsResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.EmployerIncentives.InnerApi.Responses
+{
+    public class GetIncentiveDetailsResponse
+    {
+        public DateTime EligibilityStartDate { get; set; }
+        public DateTime EligibilityEndDate { get; set; }
+    }
+}
