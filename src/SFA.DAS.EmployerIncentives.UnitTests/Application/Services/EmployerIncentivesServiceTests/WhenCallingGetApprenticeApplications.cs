43f7f3326b71c79acb8adc91611e8b2e8dc079ea
Dave Read <david.read@digital.education.gov.uk>
2020-08-28 15:52:54 +0100

    EI-301 add endpoint to list employer incentives applications for account

---
+++ src/SFA.DAS.EmployerIncentives.UnitTests/Application/Services/EmployerIncentivesServiceTests/WhenCallingGetApprenticeApplications.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Application.Services;
+using SFA.DAS.EmployerIncentives.Configuration;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests;
+using SFA.DAS.EmployerIncentives.Interfaces;
+using SFA.DAS.EmployerIncentives.Models;
+using SFA.DAS.Testing.AutoFixture;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.UnitTests.Application.Services.EmployerIncentivesServiceTests
+{
+    [TestFixture]
+    public class WhenCallingGetApprenticeApplications
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Api_Is_Called_Returning_Applications_For_The_Account(
+            long accountId,
+            IEnumerable<ApprenticeApplication> apiResponse,
+            [Frozen] Mock<IEmployerIncentivesApiClient<EmployerIncentivesConfiguration>> client,
+            EmployerIncentivesService service
+        )
+        {
+            client.Setup(x =>
+                    x.Get<IEnumerable<ApprenticeApplication>>(
+                        It.Is<GetApplicationsRequest>(c => c.GetUrl.Contains(accountId.ToString()))))
+                .ReturnsAsync(apiResponse);
+
+            var actual = await service.GetApprenticeApplications(accountId);
+
+            actual.Should().BeEquivalentTo(apiResponse);
+        }
+    }
+}
