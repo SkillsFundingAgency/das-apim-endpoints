435ccf76cbbc92e990cb7b01ada1ad89eaad4efe
Michael Young <mick.young1981@googlemail.com>
2020-09-02 13:49:15 +0100

    Added controller.

---
+++ src/SFA.DAS.EmployerIncentives.Api.UnitTests/Controllers/LegalEntity/WhenUpdatingLegalEntityVrfDetails.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Api.Controllers;
+using SFA.DAS.EmployerIncentives.Application.Commands.UpdateVendorRegistrationFormCaseDetails;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerIncentives.Api.UnitTests.Controllers.LegalEntity
+{
+    public class WhenUpdatingLegalEntityVrfDetails
+    {
+        [Test, MoqAutoData]
+        public async Task Then_UpdateApplicationCommand_Is_Sent(
+            long legalEntityId,
+            string hashedLegalEntityId,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] LegalEntityController controller)
+        {
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.Is<UpdateVendorRegistrationFormCaseDetailsCommand>(c =>
+                        c.LegalEntityId == legalEntityId
+                        && c.HashedLegalEntityId == hashedLegalEntityId
+                    ), It.IsAny<CancellationToken>())).ReturnsAsync(Unit.Value);
+
+            var controllerResult = await controller.UpdateVendorRegistrationForm(legalEntityId, hashedLegalEntityId) as NoContentResult;
+
+            Assert.IsNotNull(controllerResult);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Api/Controllers/LegalEntityController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerIncentives.Application.Commands.UpdateVendorRegistrationFormCaseDetails;
+
+namespace SFA.DAS.EmployerIncentives.Api.Controllers
+{
+    [ApiController]
+    public class LegalEntityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LegalEntityController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPatch("legalentities/{legalEntityId}/vendorregistrationform")]
+        public async Task<IActionResult> UpdateVendorRegistrationForm(long legalEntityId, string hashedLegalEntityId)
+        {
+            await _mediator.Send(new UpdateVendorRegistrationFormCaseDetailsCommand(legalEntityId, hashedLegalEntityId));
+
+            return NoContent();
+        }
+    }
+}
