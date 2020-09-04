e5c460c24b99e5d71e4e0768ee6b6fd7c20bc4b5
Michael Young <mick.young1981@googlemail.com>
2020-09-04 09:35:33 +0100

    Added new request

---
+++ src/SFA.DAS.EmployerIncentives.UnitTests/InnerApi/Requests/WhenBuildingTheGetVendorRegistrationStatusByCaseIdRequest.cs
@@ -0,0 +1,21 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests.VendorRegistrationForm;
+
+namespace SFA.DAS.EmployerIncentives.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetVendorRegistrationStatusByCaseIdRequest
+    {
+        [Test, AutoData]
+        public void Then_The_GetUrl_Is_Correctly_Built(string baseUrl, string caseId)
+        {
+            var actual = new GetVendorRegistrationStatusByCaseIdRequest(caseId)
+            {
+                BaseUrl = baseUrl
+            };
+
+            actual.GetUrl.Should().Be($"{baseUrl}Finance/Registrations/{caseId}");
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives/InnerApi/Requests/VendorRegistrationForm/GetVendorRegistrationStatusByCaseIdRequest.cs
@@ -0,0 +1,17 @@
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerIncentives.InnerApi.Requests.VendorRegistrationForm
+{
+    public class GetVendorRegistrationStatusByCaseIdRequest : IGetApiRequest
+    {
+        private readonly string _caseId;
+        
+        public GetVendorRegistrationStatusByCaseIdRequest(string caseId)
+        {
+            _caseId = caseId;
+        }
+
+        public string BaseUrl { get; set; }
+        public string GetUrl => $"{BaseUrl}Finance/Registrations/{_caseId}";
+    }
+}
