50fc6546fdb85c0d90936e70a2f0e8f0bfacbed8
Scott Wakefield <scott@expertcc.co.uk>
2020-09-07 16:42:16 +0100

    FAT2-171 added handler.

---
+++ src/SFA.DAS.Reservations/Application/Providers/Queries/GetProvider/GetProviderQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Reservations.Application.Providers.Queries.GetProvider
+{
+    public class GetProviderQuery : IRequest<GetProviderResult>
+    {
+        public int Ukprn { get; set; }
+    }
+}
---
+++ src/SFA.DAS.Reservations/Application/Providers/Queries/GetProvider/GetProviderResult.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.Reservations.InnerApi.Responses;
+
+namespace SFA.DAS.Reservations.Application.Providers.Queries.GetProvider
+{
+    public class GetProviderResult
+    {
+        public GetProviderResponse Provider { get; set; }
+    }
+}
