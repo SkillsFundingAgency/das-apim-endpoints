ea3ba6ad25b0e3ffd6a032f1217c7176792d27ab
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-09-21 22:28:05 +0100

    Change API response for feedback items

--- src/SFA.DAS.FindApprenticeshipTraining/Application/TrainingCourses/Queries/GetTrainingCourseProviders/GetTrainingCourseProvidersQueryHandler.cs
+++ src/SFA.DAS.FindApprenticeshipTraining/Application/TrainingCourses/Queries/GetTrainingCourseProviders/GetTrainingCourseProvidersQueryHandler.cs
@@ -16,7 +16,6 @@ namespace SFA.DAS.FindApprenticeshipTraining.Application.TrainingCourses.Queries
     {
         private readonly ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> _courseDeliveryApiClient;
         private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
-        private readonly ILocationApiClient<LocationApiConfiguration> _locationApiClient;
         private readonly LocationHelper _locationHelper;
 
         public GetTrainingCourseProvidersQueryHandler (
@@ -26,8 +25,7 @@ namespace SFA.DAS.FindApprenticeshipTraining.Application.TrainingCourses.Queries
         {
             _courseDeliveryApiClient = courseDeliveryApiClient;
             _coursesApiClient = coursesApiClient;
-            _locationApiClient = locationApiClient;
-            _locationHelper = new LocationHelper(_locationApiClient);
+            _locationHelper = new LocationHelper(locationApiClient);
         }
         public async Task<GetTrainingCourseProvidersResult> Handle(GetTrainingCourseProvidersQuery request, CancellationToken cancellationToken)
         {
