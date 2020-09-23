175e7afc9550950b6533294a43870ba8364f0879
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-09-23 19:59:33 +0100

    Add request and response for get ukprns by standard and location

---
+++ src/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Requests/WhenBuildingTheGetUkprnsForStandardAndLocation.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetUkprnsForStandardAndLocation
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(int standardId, double lat, double lon)
+        {
+            //Arrange Act
+            var actual = new GetUkprnsForStandardAndLocation(standardId, lat, lon);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"/api/courses/{standardId}/ukprns?lat={lat}&lon={lon}");
+        }
+    }
+}
---
+++ src/SFA.DAS.FindApprenticeshipTraining/InnerApi/Responses/GetUkprnsForStandardAndLocationResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses
+{
+    public class GetUkprnsForStandardAndLocationResponse
+    {
+        public IEnumerable<int> Ukprns { get; set; }
+    }
+}
