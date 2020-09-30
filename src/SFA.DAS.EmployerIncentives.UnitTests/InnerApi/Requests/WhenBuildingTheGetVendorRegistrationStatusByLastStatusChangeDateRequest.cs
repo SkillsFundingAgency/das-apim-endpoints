f0e42703fb3436ac0de83189644b9050e27563f5
Dave Read <david.read@digital.education.gov.uk>
2020-09-30 17:09:32 +0100

    EI-422 add unit test for get vendor registration status request

---
+++ src/SFA.DAS.EmployerIncentives.UnitTests/InnerApi/Requests/WhenBuildingTheGetVendorRegistrationStatusByLastStatusChangeDateRequest.cs
@@ -0,0 +1,23 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Extensions;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests.VendorRegistrationForm;
+using System;
+
+namespace SFA.DAS.EmployerIncentives.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetVendorRegistrationStatusByLastStatusChangeDateRequest
+    {
+        [Test, AutoData]
+        public void Then_The_GetUrl_Is_Correctly_Built(DateTime fromDate, DateTime toDate, string baseUrl)
+        {
+            var actual = new GetVendorRegistrationStatusByLastStatusChangeDateRequest(fromDate, toDate)
+            {
+                BaseUrl = baseUrl
+            };
+
+            actual.GetUrl.Should().Be($"{baseUrl}Finance/Registrations?DateTimeFrom={fromDate.ToIsoDateTime()}&DateTimeTo={toDate.ToIsoDateTime()}&api-version=2019-06-01");
+        }
+    }
+}
