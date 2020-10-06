a173c74c8ba29768c173aa18ef02366963fd099c
Scott Wakefield <scott@expertcc.co.uk>
2020-10-06 09:42:14 +0100

    FAT2-198 added handler and assoc types.

--- src/SFA.DAS.FindEpao/Application/Courses/Queries/GetCourseList/GetCourseListResult.cs
+++ src/SFA.DAS.FindEpao/Application/Courses/Queries/GetCourseList/GetCourseListResult.cs
@@ -1,6 +1,10 @@
-﻿namespace SFA.DAS.FindEpao.Application.Courses.Queries.GetCourseList
+﻿using System.Collections.Generic;
+using SFA.DAS.FindEpao.InnerApi.Responses;
+
+namespace SFA.DAS.FindEpao.Application.Courses.Queries.GetCourseList
 {
     public class GetCourseListResult
     {
+        public IEnumerable<GetStandardsListItem> Courses { get ; set ; }
     }
 }
