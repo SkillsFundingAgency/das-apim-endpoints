744bc484c699d326da477bce79b87b556b67806e
Scott Wakefield <scott@expertcc.co.uk>
2020-10-19 18:25:31 +0100

    FAT2-207 added controller for epao delivery areas.

---
+++ src/SFA.DAS.FindEpao.Api/Controllers/EpaosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.FindEpao.Api.Models;
+using SFA.DAS.FindEpao.Application.Epaos.Queries.GetDeliveryAreaList;
+
+namespace SFA.DAS.FindEpao.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class EpaosController : ControllerBase
+    {
+        private readonly ILogger<EpaosController> _logger;
+        private readonly IMediator _mediator;
+
+        public EpaosController(ILogger<EpaosController> logger,
+            IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/delivery-areas")]
+        public async Task<IActionResult> GetDeliveryAreas()
+        {
+            try
+            {
+                var queryResult = await _mediator.Send(new GetDeliveryAreaListQuery());
+                
+                var model = new GetDeliveryAreaListResponse
+                {
+                    DeliveryAreas = queryResult.DeliveryAreas.Select(c=>(GetDeliveryAreaListItem)c).ToList()
+                };
+
+                return Ok(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error attempting to get list of delivery areas");
+                return BadRequest();
+            }
+        }
+    }
+}
