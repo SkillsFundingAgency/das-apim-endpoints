0a82650a822b3c04af143561cd2be16a1ca9fd92
Scott Wakefield <scott@expertcc.co.uk>
2020-10-20 08:44:21 +0100

    FAT2-207 adding controller action for getting epaos.

--- src/SFA.DAS.FindEpao/InnerApi/Responses/GetCourseEpaoListItem.cs
+++ src/SFA.DAS.FindEpao/InnerApi/Responses/GetCourseEpaoListItem.cs
@@ -1,7 +1,22 @@
-﻿namespace SFA.DAS.FindEpao.InnerApi.Responses
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.FindEpao.InnerApi.Responses
 {
     public class GetCourseEpaoListItem
     {
-        
+        [JsonProperty("endPointAssessorOrganisationId")]
+        public string EpaoId { get; set; }
+        [JsonProperty("endPointAssessorName")]
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Postcode { get; set; }
+        [JsonProperty("deliveryAreasDetails")]
+        public IEnumerable<EpaoDeliveryArea> DeliveryAreas { get; set; }
+    }
+
+    public class EpaoDeliveryArea
+    {
+        public int DeliveryAreaId { get; set; }
     }
 }
