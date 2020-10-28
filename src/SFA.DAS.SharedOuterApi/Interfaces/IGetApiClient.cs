08acebdd9b3cd83a4b606a00bcca8780fbd141bd
Ruslan <ruslanss@gmail.com>
2020-10-28 15:25:10 +0000

    Move GetResponseCode method to child interface

--- src/SFA.DAS.SharedOuterApi/Interfaces/IGetApiClient.cs
+++ src/SFA.DAS.SharedOuterApi/Interfaces/IGetApiClient.cs
@@ -1,11 +1,9 @@
-﻿using System.Net;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace SFA.DAS.SharedOuterApi.Interfaces
 {
     public interface IGetApiClient<T>
     {
         Task<TResponse> Get<TResponse>(IGetApiRequest request);
-        Task<HttpStatusCode> GetResponseCode(IGetApiRequest request);
     }
 }
