f3d8f37284c845acf3fb923b643404b21907773a
Michael Young <mick.young1981@googlemail.com>
2020-11-03 14:26:44 +0000

    Performance improvements.

---
+++ src/SFA.DAS.EmployerIncentives.Api.AcceptanceTests/Bindings/TestCleanUp.cs
@@ -0,0 +1,28 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SFA.DAS.EmployerIncentives.Api.AcceptanceTests.Bindings
+{
+    [Binding]
+    public class TestCleanUp
+    {
+        [AfterTestRun()]
+        public static void CleanUp()
+        {
+            MockServers.InnerApi?.Dispose();
+            MockServers.AccountsApi?.Dispose();
+            MockServers.CommitmentsV2InnerApi?.Dispose();
+            MockServers.FinanceApi?.Dispose();
+            
+            OuterApi.Factory?.Dispose();
+            OuterApi.Client?.Dispose();
+
+            OuterApi.Factory = null;
+            OuterApi.Client = null;
+
+            MockServers.InnerApi = null;
+            MockServers.AccountsApi = null;
+            MockServers.CommitmentsV2InnerApi = null;
+            MockServers.FinanceApi = null;
+        }
+    }
+}
