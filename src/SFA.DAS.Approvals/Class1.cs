ea29ece03837601c9ea1314d6db677def40c6177
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-11-26 17:19:55 +0000

    Add logic for MaxFunding and Duration on standard to shared

--- src/SFA.DAS.Approvals/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace SFA.DAS.Approvals
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/SFA.DAS.SharedOuterApi/InnerApi/Responses/StandardApiResponseBase.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Responses
+{
+    public abstract class StandardApiResponseBase
+    {
+        public List<ApprenticeshipFunding> ApprenticeshipFunding { get; set; }
+        
+        [JsonIgnore]
+        public int MaxFunding => GetFundingDetails(nameof(MaxFunding));
+        [JsonIgnore]
+        public int TypicalDuration => GetFundingDetails(nameof(TypicalDuration));
+        private int GetFundingDetails(string prop)
+        {
+            var funding = ApprenticeshipFunding
+                .FirstOrDefault(c =>
+                    c.EffectiveFrom <= DateTime.UtcNow && (c.EffectiveTo == null
+                                                           || c.EffectiveTo >= DateTime.UtcNow));
+
+            if (funding == null)
+            {
+                funding = ApprenticeshipFunding.FirstOrDefault(c => c.EffectiveTo == null);
+            }
+
+            if (prop == nameof(MaxFunding))
+            {
+                return funding?.MaxEmployerLevyCap
+                       ?? ApprenticeshipFunding.FirstOrDefault()?.MaxEmployerLevyCap
+                       ?? 0;
+            }
+                
+            return funding?.Duration
+                   ?? ApprenticeshipFunding.FirstOrDefault()?.Duration
+                   ?? 0;
+        }
+    }
+    
+    public class ApprenticeshipFunding
+    {
+        public int MaxEmployerLevyCap { get; set; }
+
+        public DateTime? EffectiveTo { get; set; }
+
+        public DateTime EffectiveFrom { get; set; }
+        public int Duration { get; set; }
+    }
+}
