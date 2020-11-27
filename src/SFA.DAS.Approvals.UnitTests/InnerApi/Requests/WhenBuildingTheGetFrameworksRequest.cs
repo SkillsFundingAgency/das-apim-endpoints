6bdcb0853bf788a5963c3f7ab37dadea3b8e4c58
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-11-27 15:39:16 +0000

    Add GetFrameworks request and response types

---
+++ src/SFA.DAS.Approvals.UnitTests/InnerApi/Requests/WhenBuildingTheGetFrameworksRequest.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture.NUnit3;
+using NUnit.Framework;
+using SFA.DAS.Approvals.InnerApi.Requests;
+
+namespace SFA.DAS.Approvals.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetFrameworksRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(string baseUrl)
+        {
+            //Arrange Act
+            var actual = new GetFrameworksRequest();
+            
+            //Assert
+            Assert.AreEqual("api/courses/frameworks", actual.GetUrl);
+        }
+    }
+}
