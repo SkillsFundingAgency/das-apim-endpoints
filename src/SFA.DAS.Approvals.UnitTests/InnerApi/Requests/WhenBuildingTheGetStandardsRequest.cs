9deabcbb3aff12d3e2689edffc424fa785e1413f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-11-27 14:23:37 +0000

    Add get all standards request for approvals

---
+++ src/SFA.DAS.Approvals.UnitTests/InnerApi/Requests/WhenBuildingTheGetStandardsRequest.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture.NUnit3;
+using NUnit.Framework;
+using SFA.DAS.Approvals.InnerApi.Requests;
+
+namespace SFA.DAS.Approvals.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetStandardsRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(string baseUrl)
+        {
+            //Arrange Act
+            var actual = new GetStandardsRequest();
+            
+            //Assert
+            Assert.AreEqual("api/courses/standards", actual.GetUrl);
+        }
+    }
+}
---
+++ src/SFA.DAS.Approvals/InnerApi/Requests/GetStandardsRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Approvals.InnerApi.Requests
+{
+    public class GetStandardsRequest : IGetApiRequest
+    {
+        public string GetUrl => $"api/courses/standards";
+    }
+}
