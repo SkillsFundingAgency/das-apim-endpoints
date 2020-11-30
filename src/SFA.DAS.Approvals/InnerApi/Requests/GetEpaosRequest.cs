05a2b2c9b1e24c70825df32af90b319804be81a8
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-11-30 16:21:11 +0000

    Add request for getting EPAOs

---
+++ src/SFA.DAS.Approvals/InnerApi/Requests/GetEpaosRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Approvals.InnerApi.Requests
+{
+    public class GetEpaosRequest : IGetApiRequest
+    {
+        public string GetUrl => "api/v1/organisations";
+    }
+}
