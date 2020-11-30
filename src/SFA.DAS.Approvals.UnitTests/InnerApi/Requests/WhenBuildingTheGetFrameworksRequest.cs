866f70cb359855030818eda4bdb78f9f5a1e08f5
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-11-30 16:23:09 +0000

    Test tidy up

--- src/SFA.DAS.Approvals.UnitTests/InnerApi/Requests/WhenBuildingTheGetFrameworksRequest.cs
+++ src/SFA.DAS.Approvals.UnitTests/InnerApi/Requests/WhenBuildingTheGetFrameworksRequest.cs
@@ -1,13 +1,12 @@
-﻿using AutoFixture.NUnit3;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using SFA.DAS.Approvals.InnerApi.Requests;
 
 namespace SFA.DAS.Approvals.UnitTests.InnerApi.Requests
 {
     public class WhenBuildingTheGetFrameworksRequest
     {
-        [Test, AutoData]
-        public void Then_The_Url_Is_Correctly_Built(string baseUrl)
+        [Test]
+        public void Then_The_Url_Is_Correctly_Built()
         {
             //Arrange Act
             var actual = new GetFrameworksRequest();
