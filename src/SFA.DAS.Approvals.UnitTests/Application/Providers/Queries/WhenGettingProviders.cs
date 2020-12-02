80ee2a4b7f82d8f231bc01813725f029e229db9e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-02 16:07:30 +0000

    Add handler for getting all registered providers

---
+++ src/SFA.DAS.Approvals.UnitTests/Application/Providers/Queries/WhenGettingProviders.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Approvals.Application.Providers.Queries;
+using SFA.DAS.Approvals.InnerApi.Requests;
+using SFA.DAS.Approvals.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Approvals.UnitTests.Application.Providers.Queries
+{
+    public class WhenGettingProviders
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Api_Is_Called_With_The_Request_And_Epaos_Returned(
+            GetProvidersQuery query,
+            GetProvidersListResponse apiResponse,
+            [Frozen] Mock<ICourseDeliveryApiClient<CourseDeliveryApiConfiguration>> apiClient,
+            GetProvidersQueryHandler handler
+        )
+        {
+            apiClient.Setup(x => x.Get<GetProvidersListResponse>(It.IsAny<GetProvidersRequest>())).ReturnsAsync(apiResponse);
+
+            var actual = await handler.Handle(query, CancellationToken.None);
+
+            actual.Providers.Should().BeEquivalentTo(apiResponse.Providers);
+        }
+    }
+}
---
+++ src/SFA.DAS.Approvals/InnerApi/Responses/GetProvidersListItem.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Approvals.InnerApi.Responses
+{
+    public class GetProvidersListItem
+    {
+        public int Ukprn { get; set; }
+        public string Name { get; set; }
+    }
+}
