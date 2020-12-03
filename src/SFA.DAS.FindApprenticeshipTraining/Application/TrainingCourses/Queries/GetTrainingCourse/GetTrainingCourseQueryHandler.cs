e2bc4844f74bc7520389943c8e8a3362abf9b217
Scott Wakefield <scott@expertcc.co.uk>
2020-12-03 13:58:50 +0000

    FAT2-287 missed files from prev commit.

--- src/SFA.DAS.FindApprenticeshipTraining/Application/TrainingCourses/Queries/GetTrainingCourse/GetTrainingCourseQueryHandler.cs
+++ src/SFA.DAS.FindApprenticeshipTraining/Application/TrainingCourses/Queries/GetTrainingCourse/GetTrainingCourseQueryHandler.cs
@@ -7,6 +7,7 @@ using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
 using SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses;
 using SFA.DAS.FindApprenticeshipTraining.Interfaces;
 using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.FindApprenticeshipTraining.Application.TrainingCourses.Queries.GetTrainingCourse
