cff5431eb22555af4a6a1074adbfdb1d85d2efd6
Scott Wakefield <scott@expertcc.co.uk>
2020-12-03 14:25:04 +0000

    FAT2-287 moved test file for request.

--- src/SFA.DAS.SharedOuterApi.UnitTests/InnerApi/Requests/WhenBuildingTheGetStandardRequest.cs
+++ src/SFA.DAS.SharedOuterApi.UnitTests/InnerApi/Requests/WhenBuildingTheGetStandardRequest.cs
@@ -1,8 +1,8 @@
-using AutoFixture.NUnit3;
+﻿using AutoFixture.NUnit3;
 using NUnit.Framework;
-using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 
-namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Requests
+namespace SFA.DAS.SharedOuterApi.UnitTests.InnerApi.Requests
 {
     public class WhenBuildingTheGetStandardRequest
     {
