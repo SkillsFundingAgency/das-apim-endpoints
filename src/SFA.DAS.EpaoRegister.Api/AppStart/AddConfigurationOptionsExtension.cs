4f4a5aa5abd066d0050750636af232d979460966
Scott Wakefield <scott@expertcc.co.uk>
2020-12-04 12:30:18 +0000

    FAT2-287 setup redis connection config.

--- src/SFA.DAS.EpaoRegister.Api/AppStart/AddConfigurationOptionsExtension.cs
+++ src/SFA.DAS.EpaoRegister.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Api.Common.Configuration;
+using SFA.DAS.EpaoRegister.Configuration;
 using SFA.DAS.SharedOuterApi.Configuration;
 
 namespace SFA.DAS.EpaoRegister.Api.AppStart
@@ -11,6 +12,8 @@ namespace SFA.DAS.EpaoRegister.Api.AppStart
         public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddOptions();
+            services.Configure<EpaoRegisterConfiguration>(configuration.GetSection(nameof(EpaoRegisterConfiguration)));
+            services.AddSingleton(cfg => cfg.GetService<IOptions<EpaoRegisterConfiguration>>().Value);
             services.Configure<CoursesApiConfiguration>(configuration.GetSection(nameof(CoursesApiConfiguration)));
             services.AddSingleton(cfg => cfg.GetService<IOptions<CoursesApiConfiguration>>().Value);
             services.Configure<AssessorsApiConfiguration>(configuration.GetSection(nameof(AssessorsApiConfiguration)));
