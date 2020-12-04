f4e8c1c848432a7a589fb5e66610e97f74961454
Scott Wakefield <scott@expertcc.co.uk>
2020-12-04 12:01:40 +0000

    FAT2-287 caching courses api call in epao register.

---
+++ src/SFA.DAS.EpaoRegister/InnerApi/Responses/GetStandardsListResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.EpaoRegister.InnerApi.Responses
+{
+    public class GetStandardsListResponse
+    {
+        public IEnumerable<GetStandardResponse> Standards { get; set; }
+    }
+}
