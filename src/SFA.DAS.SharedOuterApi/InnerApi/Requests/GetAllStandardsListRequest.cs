4da9968435f5b88f0864b2a77286aeac2f22aec1
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-15 16:26:47 +0000

    Add new request for getting all standards
    Get all standards including ones not available to start

---
+++ src/SFA.DAS.SharedOuterApi/InnerApi/Requests/GetAllStandardsListRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests
+{
+    public class GetAllStandardsListRequest : IGetApiRequest
+    {
+        public List<Guid> RouteIds { get ; set ; }
+        public List<int> Levels { get ; set ; }
+        public string GetUrl => BuildUrl();
+
+        private string BuildUrl()
+        {
+            var url = $"api/courses/standards?filterAvailableToStart=false";
+
+            if (RouteIds != null && RouteIds.Any())
+            {
+                url += "&routeIds=" + string.Join("&routeIds=", RouteIds);
+            }
+
+            if (Levels != null && Levels.Any())
+            {
+                url += "&levels=" + string.Join("&levels=", Levels);
+            }
+
+            return url;
+        }
+    }
+}
