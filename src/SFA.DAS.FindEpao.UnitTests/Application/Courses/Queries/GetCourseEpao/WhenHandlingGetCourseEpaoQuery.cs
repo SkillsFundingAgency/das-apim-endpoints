ba8e4dfe5c20e7d863ae014cd18ba437e046b8fa
Scott Wakefield <scott@expertcc.co.uk>
2020-12-17 15:03:58 +0000

    FAT2-212 added cache for delivery areas.

--- src/SFA.DAS.FindEpao.UnitTests/Application/Courses/Queries/GetCourseEpao/WhenHandlingGetCourseEpaoQuery.cs
+++ src/SFA.DAS.FindEpao.UnitTests/Application/Courses/Queries/GetCourseEpao/WhenHandlingGetCourseEpaoQuery.cs
@@ -59,13 +59,15 @@ namespace SFA.DAS.FindEpao.UnitTests.Application.Courses.Queries.GetCourseEpao
         }
 
         [Test, MoqAutoData]
-        public async Task Then_Gets_Epao_From_Assessors_Api_And_Course_From_Courses_Api(
+        public async Task Then_Gets_Epao_From_Assessors_Api_And_Course_From_Courses_Api_And_DeliveryAreas_From_Cache(
             GetCourseEpaoQuery query,
             GetEpaoResponse epaoApiResponse,
             List<GetCourseEpaoListItem> courseEpaosApiResponse,
             GetStandardsListItem coursesApiResponse,
+            List<GetDeliveryAreaListItem> areasFromCache,
             [Frozen] Mock<IAssessorsApiClient<AssessorsApiConfiguration>> mockAssessorsApiClient,
             [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> mockCoursesApiClient,
+            [Frozen] Mock<ICacheStorageService> mockCacheService,
             GetCourseEpaoQueryHandler handler)
         {
             courseEpaosApiResponse[0].EpaoId = query.EpaoId;
@@ -77,6 +79,9 @@ namespace SFA.DAS.FindEpao.UnitTests.Application.Courses.Queries.GetCourseEpao
                 .Setup(client => client.GetAll<GetCourseEpaoListItem>(
                     It.Is<GetCourseEpaosRequest>(request => request.CourseId == query.CourseId)))
                 .ReturnsAsync(courseEpaosApiResponse);
+            mockCacheService
+                .Setup(service => service.RetrieveFromCache<IEnumerable<GetDeliveryAreaListItem>>(nameof(GetDeliveryAreasRequest)))
+                .ReturnsAsync(areasFromCache);
 
             mockCoursesApiClient
                 .Setup(client => client.Get<GetStandardsListItem>(
@@ -89,6 +94,51 @@ namespace SFA.DAS.FindEpao.UnitTests.Application.Courses.Queries.GetCourseEpao
             result.EpaoDeliveryAreas.Should().BeEquivalentTo(courseEpaosApiResponse.Single(item => item.EpaoId == query.EpaoId).DeliveryAreas);
             result.CourseEpaosCount.Should().Be(courseEpaosApiResponse.Count);
             result.Course.Should().BeEquivalentTo(coursesApiResponse);
+            result.DeliveryAreas.Should().BeEquivalentTo(areasFromCache);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_DeliveryAreas_Not_Cached_Then_Gets_From_Api_And_Stores_In_Cache(
+            GetCourseEpaoQuery query,
+            GetEpaoResponse epaoApiResponse,
+            List<GetCourseEpaoListItem> courseEpaosApiResponse,
+            GetStandardsListItem coursesApiResponse,
+            List<GetDeliveryAreaListItem> areasApiResponse,
+            [Frozen] Mock<IAssessorsApiClient<AssessorsApiConfiguration>> mockAssessorsApiClient,
+            [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> mockCoursesApiClient,
+            [Frozen] Mock<ICacheStorageService> mockCacheService,
+            GetCourseEpaoQueryHandler handler)
+        {
+            var expectedExpirationInHours = 1;
+            courseEpaosApiResponse[0].EpaoId = query.EpaoId;
+            mockAssessorsApiClient
+                .Setup(client => client.Get<GetEpaoResponse>(
+                    It.Is<GetEpaoRequest>(request => request.EpaoId == query.EpaoId)))
+                .ReturnsAsync(epaoApiResponse);
+            mockAssessorsApiClient
+                .Setup(client => client.GetAll<GetCourseEpaoListItem>(
+                    It.Is<GetCourseEpaosRequest>(request => request.CourseId == query.CourseId)))
+                .ReturnsAsync(courseEpaosApiResponse);
+            mockAssessorsApiClient
+                .Setup(client => client.GetAll<GetDeliveryAreaListItem>(
+                    It.IsAny<GetDeliveryAreasRequest>()))
+                .ReturnsAsync(areasApiResponse);
+            mockCoursesApiClient
+                .Setup(client => client.Get<GetStandardsListItem>(
+                    It.Is<GetStandardRequest>(request => request.StandardId == query.CourseId)))
+                .ReturnsAsync(coursesApiResponse);
+            mockCacheService
+                .Setup(service => service.RetrieveFromCache<IEnumerable<GetDeliveryAreaListItem>>(nameof(GetDeliveryAreasRequest)))
+                .ReturnsAsync((IEnumerable<GetDeliveryAreaListItem>)null);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+            
+            result.DeliveryAreas.Should().BeEquivalentTo(areasApiResponse);
+            mockCacheService.Verify(service =>
+                service.SaveToCache(
+                    nameof(GetDeliveryAreasRequest),
+                    areasApiResponse.AsEnumerable(),
+                    expectedExpirationInHours));
         }
     }
 }
--- src/SFA.DAS.FindEpao/Application/Courses/Queries/GetCourseEpao/GetCourseEpaoQueryHandler.cs
+++ src/SFA.DAS.FindEpao/Application/Courses/Queries/GetCourseEpao/GetCourseEpaoQueryHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -15,17 +16,22 @@ namespace SFA.DAS.FindEpao.Application.Courses.Queries.GetCourseEpao
 {
     public class GetCourseEpaoQueryHandler : IRequestHandler<GetCourseEpaoQuery, GetCourseEpaoResult>
     {
+        private const int DeliveryAreaCacheDurationInHours = 1;
+
         private readonly IValidator<GetCourseEpaoQuery> _validator;
         private readonly IAssessorsApiClient<AssessorsApiConfiguration> _assessorsApiClient;
         private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
+        private readonly ICacheStorageService _cacheStorageService;
 
         public GetCourseEpaoQueryHandler(IValidator<GetCourseEpaoQuery> validator,
             IAssessorsApiClient<AssessorsApiConfiguration> assessorsApiClient,
-            ICoursesApiClient<CoursesApiConfiguration> CoursesApiClient)
+            ICoursesApiClient<CoursesApiConfiguration> coursesApiClient,
+            ICacheStorageService cacheStorageService)
         {
             _validator = validator;
             _assessorsApiClient = assessorsApiClient;
-            _coursesApiClient = CoursesApiClient;
+            _coursesApiClient = coursesApiClient;
+            _cacheStorageService = cacheStorageService;
         }
 
         public async Task<GetCourseEpaoResult> Handle(GetCourseEpaoQuery request, CancellationToken cancellationToken)
@@ -36,23 +42,41 @@ namespace SFA.DAS.FindEpao.Application.Courses.Queries.GetCourseEpao
                 throw new ValidationException(validationResult.DataAnnotationResult, null, null);
             }
 
-            var epao = await _assessorsApiClient.Get<GetEpaoResponse>(new GetEpaoRequest(request.EpaoId));
-            var courseEpaos = await _assessorsApiClient.GetAll<GetCourseEpaoListItem>(
+            var epaoTask = _assessorsApiClient.Get<GetEpaoResponse>(new GetEpaoRequest(request.EpaoId));
+            var courseEpaosTask = _assessorsApiClient.GetAll<GetCourseEpaoListItem>(
                 new GetCourseEpaosRequest {CourseId = request.CourseId});
-            var course = await _coursesApiClient.Get<GetStandardsListItem>(new GetStandardRequest(request.CourseId));
+            var areasTask = GetDeliveryAreas();
+            var courseTask = _coursesApiClient.Get<GetStandardsListItem>(new GetStandardRequest(request.CourseId));
+            
+            await Task.WhenAll(epaoTask, courseEpaosTask, areasTask, courseTask);
 
-            if (epao == default)
+            if (epaoTask.Result == default)
             {
                 throw new NotFoundException<GetCourseEpaoResult>();
             }
 
             return new GetCourseEpaoResult
             {
-                Epao = epao,
-                Course = course,
-                EpaoDeliveryAreas = courseEpaos.Single(item => item.EpaoId == request.EpaoId).DeliveryAreas,
-                CourseEpaosCount = courseEpaos.Count()
+                Epao = epaoTask.Result,
+                Course = courseTask.Result,
+                EpaoDeliveryAreas = courseEpaosTask.Result.Single(item => item.EpaoId == request.EpaoId).DeliveryAreas,
+                CourseEpaosCount = courseEpaosTask.Result.Count(),
+                DeliveryAreas = areasTask.Result
             };
+        }
+
+        private async Task<IEnumerable<GetDeliveryAreaListItem>> GetDeliveryAreas()
+        {
+            var deliveryAreas = await _cacheStorageService
+                .RetrieveFromCache<IEnumerable<GetDeliveryAreaListItem>>(nameof(GetDeliveryAreasRequest));
+            
+            if (deliveryAreas != null) 
+                return deliveryAreas;
+            
+            deliveryAreas = await _assessorsApiClient.GetAll<GetDeliveryAreaListItem>(new GetDeliveryAreasRequest());
+            await _cacheStorageService.SaveToCache(nameof(GetDeliveryAreasRequest), deliveryAreas, DeliveryAreaCacheDurationInHours);
+
+            return deliveryAreas;
         }
     }
 }
