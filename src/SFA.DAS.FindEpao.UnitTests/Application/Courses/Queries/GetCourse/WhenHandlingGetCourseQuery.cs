93d081b0e3b30799ee11d0200f5bd9992d27b0af
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-18 20:42:45 +0000

    Add new handler for getting standard by id

---
+++ src/SFA.DAS.FindEpao.UnitTests/Application/Courses/Queries/GetCourse/WhenHandlingGetCourseQuery.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindEpao.Application.Courses.Queries.GetCourse;
+using SFA.DAS.FindEpao.InnerApi.Requests;
+using SFA.DAS.FindEpao.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindEpao.UnitTests.Application.Courses.Queries.GetCourse
+{
+    public class WhenHandlingGetCourseQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_Standard_From_Courses_Api(
+            GetCourseQuery query,
+            GetStandardsListItem apiResponse,
+            [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> mockApiClient,
+            GetCourseQueryHandler handler)
+        {
+            mockApiClient
+                .Setup(client => client.Get<GetStandardsListItem>(It.IsAny<GetStandardRequest>()))
+                .ReturnsAsync(apiResponse);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Course.Should().BeEquivalentTo(apiResponse);
+        }
+    }
+}
---
+++ src/SFA.DAS.FindEpao/Application/Courses/Queries/GetCourse/GetCourseQueryHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.FindEpao.InnerApi.Requests;
+using SFA.DAS.FindEpao.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindEpao.Application.Courses.Queries.GetCourse
+{
+    public class GetCourseQueryHandler : IRequestHandler<GetCourseQuery, GetCourseResult>
+    {
+        private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
+
+        public GetCourseQueryHandler (ICoursesApiClient<CoursesApiConfiguration> coursesApiClient)
+        {
+            _coursesApiClient = coursesApiClient;
+        }
+        public async Task<GetCourseResult> Handle(GetCourseQuery request, CancellationToken cancellationToken)
+        {
+            var course = await _coursesApiClient.Get<GetStandardsListItem>(
+                new GetStandardRequest
+                {
+                    StandardId = request.CourseId
+                });
+            
+            return new GetCourseResult
+            {
+                Course = course
+            };
+        }
+    }
+}
