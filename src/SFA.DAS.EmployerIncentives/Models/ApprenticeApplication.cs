77530cf87af261883e4cc52d3e8b80389c2cd10a
Paul Graham <paul.graham@coprime.co.uk>
2020-12-22 14:21:27 +0000

    extended ApprenticeshpApplication model to include the ULN

--- src/SFA.DAS.EmployerIncentives/Models/ApprenticeApplication.cs
+++ src/SFA.DAS.EmployerIncentives/Models/ApprenticeApplication.cs
@@ -9,6 +9,7 @@ namespace SFA.DAS.EmployerIncentives.Models
         public string LegalEntityName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public long ULN { get; set; }
         public DateTime ApplicationDate { get; set; }
         public decimal TotalIncentiveAmount { get; set; }
         public string Status { get; set; }
