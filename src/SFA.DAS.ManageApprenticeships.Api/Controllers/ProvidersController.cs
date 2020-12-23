2a792a32fad4da473d7f451caf3cc42ce466b5dc
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-23 16:22:31 +0000

    Implement action for getting all providers

--- src/SFA.DAS.ManageApprenticeships.Api/Controllers/ProvidersController.cs
+++ src/SFA.DAS.ManageApprenticeships.Api/Controllers/ProvidersController.cs
@@ -1,5 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ManageApprenticeships.Api.Models;
+using SFA.DAS.ManageApprenticeships.Application.Queries.GetProviders;
 
 namespace SFA.DAS.ManageApprenticeships.Api.Controllers
 {
@@ -7,11 +13,33 @@ namespace SFA.DAS.ManageApprenticeships.Api.Controllers
     [Route("[controller]/")]
     public class ProvidersController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProvidersController> _logger;
+
+        public ProvidersController (IMediator mediator, ILogger<ProvidersController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
         [HttpGet]
         [Route("")]
         public async Task<IActionResult> GetAllProviders()
         {
-            return Ok();
+            try
+            {
+                var response = await _mediator.Send(new GetProvidersQuery());
+                var model = new GetProvidersListResponse
+                {
+                    Providers = response.Providers.Select(c=>(GetProviderResponse)c)
+                };
+                
+                return Ok(model);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error getting all providers");
+                return BadRequest();
+            }
         }
     }
 }
