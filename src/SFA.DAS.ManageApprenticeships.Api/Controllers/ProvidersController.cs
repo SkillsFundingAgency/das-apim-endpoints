fe014cfe889960549b89706ffacb440235e1ff6a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-23 08:44:17 +0000

    Initial commit for MA outer api

---
+++ src/SFA.DAS.ManageApprenticeships.Api/Controllers/ProvidersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.ManageApprenticeships.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class ProvidersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllProviders()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/SFA.DAS.ManageApprenticeships.Api/Controllers/TrainingCoursesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.ManageApprenticeships.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class TrainingCoursesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("standards")]
+        public async Task<IActionResult> GetStandards()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        [Route("frameworks")]
+        public async Task<IActionResult> GetFrameworks()
+        {
+            return Ok();
+        }
+    }
+}
