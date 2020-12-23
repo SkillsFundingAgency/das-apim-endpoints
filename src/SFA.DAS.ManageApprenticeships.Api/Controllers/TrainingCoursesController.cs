bdf2025a05918cada0e8b92a285fe8092354529d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2020-12-23 16:33:46 +0000

    Implement get all standards controller action

--- src/SFA.DAS.ManageApprenticeships.Api/Controllers/TrainingCoursesController.cs
+++ src/SFA.DAS.ManageApprenticeships.Api/Controllers/TrainingCoursesController.cs
@@ -1,5 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ManageApprenticeships.Api.Models;
+using SFA.DAS.ManageApprenticeships.Application.Queries.GetStandards;
 
 namespace SFA.DAS.ManageApprenticeships.Api.Controllers
 {
@@ -7,11 +13,34 @@ namespace SFA.DAS.ManageApprenticeships.Api.Controllers
     [Route("[controller]/")]
     public class TrainingCoursesController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        private readonly ILogger<TrainingCoursesController> _logger;
+
+        public TrainingCoursesController (IMediator mediator, ILogger<TrainingCoursesController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
         [HttpGet]
         [Route("standards")]
         public async Task<IActionResult> GetStandards()
         {
-            return Ok();
+            try
+            {
+                var result = await _mediator.Send(new GetStandardsQuery());
+                
+                var response = new GetStandardsListResponse
+                {
+                    Standards = result.Standards.Select(c=>(GetStandardResponse)c)
+                };
+                
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error getting list of standards");
+                return BadRequest();
+            }
         }
         [HttpGet]
         [Route("frameworks")]
