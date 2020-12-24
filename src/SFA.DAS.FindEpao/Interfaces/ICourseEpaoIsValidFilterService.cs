a3910dc30dd5299c68b3b195d83b52e2cc84b8a4
Scott Wakefield <scott@expertcc.co.uk>
2020-12-24 14:06:09 +0000

    FAT2-311 adding new filter service for course epaos.

---
+++ src/SFA.DAS.FindEpao/Interfaces/ICourseEpaoIsValidFilterService.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.FindEpao.InnerApi.Responses;
+
+namespace SFA.DAS.FindEpao.Interfaces
+{
+    public interface ICourseEpaoIsValidFilterService
+    {
+        bool IsValidCourseEpao(GetCourseEpaoListItem courseEpao);
+    }
+}
