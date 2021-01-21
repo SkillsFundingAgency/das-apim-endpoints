80d6eaea1d0f72e2d3d829f808069db363df7161
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-01-21 15:54:50 +0000

    Add basic project structure for campaign outer api

---
+++ src/SFA.DAS.Campaign.Api/Controllers/SectorsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Campaign.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class SectorsController : ControllerBase
+    {
+        public SectorsController ()
+        {
+            
+        }
+        
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetSectors()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ src/SFA.DAS.Campaign.Api/Startup.cs
@@ -0,0 +1,100 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using SFA.DAS.Api.Common.AppStart;
+using SFA.DAS.Api.Common.Configuration;
+using SFA.DAS.Campaign.Api.AppStart;
+using SFA.DAS.SharedOuterApi.AppStart;
+using SFA.DAS.SharedOuterApi.Infrastructure.HealthCheck;
+
+namespace SFA.DAS.Campaign.Api
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _env = env;
+            _configuration = configuration.BuildSharedConfiguration();
+        }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_env);
+
+            services.AddConfigurationOptions(_configuration);
+
+            if (!_configuration.IsLocalOrDev())
+            {
+                var azureAdConfiguration = _configuration
+                    .GetSection("AzureAd")
+                    .Get<AzureActiveDirectoryConfiguration>();
+                var policies = new Dictionary<string, string>
+                {
+                    {"default", "APIM"}
+                };
+
+                services.AddAuthentication(azureAdConfiguration, policies);
+            }
+
+            //services.AddMediatR(typeof(GetStandardsQuery).Assembly);
+            services.AddServiceRegistration();
+            
+            services
+                .AddMvc(o =>
+                {
+                    if (!_configuration.IsLocalOrDev())
+                    {
+                        o.Filters.Add(new AuthorizeFilter("default"));
+                    }
+                }).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            if (_configuration["Environment"] != "DEV")
+            {
+                services.AddHealthChecks()
+                    .AddCheck<CoursesApiHealthCheck>("Courses API health check");
+            }
+            
+            services.AddApplicationInsightsTelemetry(_configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CampaignOuterApi", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "api/{controller=Sectors}/{action=GetSectors}/{id?}");
+            });
+        
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CampaignOuterApi");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
