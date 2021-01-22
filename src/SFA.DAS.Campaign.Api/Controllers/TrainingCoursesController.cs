37f4bd634ad08aca653e601f045c543ec627e40a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-01-22 08:50:57 +0000

    Implement TrainingCourses controller

--- src/SFA.DAS.Campaign.Api/Controllers/TrainingCoursesController.cs
+++ src/SFA.DAS.Campaign.Api/Controllers/TrainingCoursesController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Campaign.Api.Models;
+using SFA.DAS.Campaign.Application.Queries.Standards;
 
 namespace SFA.DAS.Campaign.Api.Controllers
 {
@@ -6,16 +13,33 @@ namespace SFA.DAS.Campaign.Api.Controllers
     [Route("[controller]/")]
     public class TrainingCoursesController : ControllerBase
     {
-        public TrainingCoursesController ()
+        private readonly ILogger<TrainingCoursesController> _logger;
+        private readonly IMediator _mediator;
+
+        public TrainingCoursesController (ILogger<TrainingCoursesController> logger, IMediator mediator)
         {
-            
+            _logger = logger;
+            _mediator = mediator;
         }
         
         [HttpGet]
         [Route("")]
-        public IActionResult GetStandardsBySector([FromQuery]string sector)
+        public async Task<IActionResult> GetStandardsBySector([FromQuery]string sector)
         {
-            return Ok();
+            try
+            {
+                var result = await _mediator.Send(new GetStandardsQuery {Sector = sector});
+
+                return Ok(new GetStandardsResponse
+                {
+                    Standards = result.Standards.Select(c => (GetStandardsResponseItem)c).ToList()
+                });
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error getting standards by sector {sector}");
+                return BadRequest();
+            }
         }
     }
 }
