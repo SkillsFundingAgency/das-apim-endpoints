73d456e238abcc1b611edae61994ed2b17d74b60
Ben Arnold <ben@arnoldfamily.me.uk>
2021-01-28 20:19:44 +0000

    Add apprentice and course name to integration test

--- src/SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests/Steps/AddApprenticeshipSteps.cs
+++ src/SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests/Steps/AddApprenticeshipSteps.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Newtonsoft.Json;
 using SFA.DAS.ApprenticeCommitments.Apis.ApprenticeLoginApi;
 using SFA.DAS.ApprenticeCommitments.Apis.CommitmentsV2InnerApi;
 using SFA.DAS.ApprenticeCommitments.Apis.InnerApi;
 using SFA.DAS.ApprenticeCommitments.Application.Commands.CreateApprenticeship;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using WireMock.Matchers;
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
 
@@ -21,54 +23,39 @@ namespace SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests.Steps
     {
         private readonly TestContext _context;
         private CreateApprenticeshipCommand _request;
-        private Dictionary<string, string> _errors;
-        private SendInvitationRequestData loginApiRequest;
+        private IEnumerable<ApprenticeshipResponse> _approvedApprenticeships;
 
         public AddApprenticeshipSteps(TestContext context)
         {
             _context = context;
-            _errors = new Dictionary<string, string>();
-            _errors.Add("apprenticeshipId", "Not valid");
-            _errors.Add("email", "Not valid");
-        }
 
-        [Given(@"apprenticeship details are valid")]
-        public void GivenApprenticeshipDetailsAreValid()
-        {
-            _request = new CreateApprenticeshipCommand
-            {
-                ApprenticeshipId = 1020,
-                Email = "Test@Test.com",
-                Organisation = "OrganisationName",
-            };
-        }
-
-        [Given(@"apprenticeship details are not valid")]
-        public void GivenApprenticeshipDetailsAreNotValid()
-        {
-            _request = new CreateApprenticeshipCommand
-            {
-                ApprenticeshipId = 0,
-                Email = "noemail",
-            };
-        }
-
-        [Given(@"the inner api is ready")]
-        public void GivenTheInnerApiIsReady()
-        {
             _context.InnerApi.MockServer
                 .Given(
-                    Request.Create().WithPath($"/apprenticeships")
-                        .UsingPost())
+                    Request.Create()
+                        .WithPath("/apprenticeships")
+                        .UsingPost()
+                        .WithBody(new JmesPathMatcher(
+                            "ApprenticeshipId != `0` && contains(Email, '@')"))
+                      )
                 .RespondWith(
                     Response.Create()
                         .WithStatusCode((int)HttpStatusCode.Accepted)
-                );
-        }
+                            );
 
-        [Given(@"the apprentice login api is ready")]
-        public void GivenTheApprenticeLoginApiIsReady()
-        {
+            _context.InnerApi.MockServer
+                .Given(
+                    Request.Create().WithPath("/apprenticeships")
+                        .UsingPost()
+                        .WithBody(new JmesPathMatcher(
+                            "!contains(Email, '@')"))
+                      )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.BadRequest)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody("{'email':'Not valid'}")
+                            );
+
             _context.LoginApi.MockServer
                 .Given(
                     Request.Create().WithPath($"/invitations/{_context.LoginConfig.IdentityServerClientId}")
@@ -76,104 +63,95 @@ namespace SFA.DAS.ApprenticeCommitments.Api.AcceptanceTests.Steps
                 .RespondWith(
                     Response.Create()
                         .WithStatusCode((int)HttpStatusCode.OK)
-                );
+                            );
         }
 
-        [Given(@"the Approvals API is ready")]
-        public void GivenTheApprovalsAPIIsReady()
+        [Given("the following apprenticeships have been approved")]
+        public void GivenTheFollowingApprenticeshipsHaveBeenApproved(Table table)
         {
-            _context.CommitmentsV2InnerApi.MockServer
-                .Given(
-                    Request.Create().WithPath($"/api/apprenticeships/1020")
-                        .UsingGet())
-                .RespondWith(
-                    Response.Create()
-                        .WithStatusCode((int)HttpStatusCode.OK)
-                        .WithHeader("Content-Type", "application/json")
-                        .WithBody(JsonConvert.SerializeObject(new ApprenticeshipResponse
-                        {
-                            FirstName = "GivenName",
-                            LastName = "FamilyName",
-                            CourseName = "ApprenticeshipName",
-                        }))
-                ); ;
+            _approvedApprenticeships = table.CreateSet<ApprenticeshipResponse>();
+
+            foreach (var approval in _approvedApprenticeships)
+            {
+                _context.CommitmentsV2InnerApi.MockServer
+                    .Given(
+                        Request.Create()
+                            .WithPath($"/api/apprenticeships/{approval.Id}")
+                            .UsingGet()
+                          )
+                    .RespondWith(
+                        Response.Create()
+                            .WithStatusCode((int)HttpStatusCode.OK)
+                            .WithHeader("Content-Type", "application/json")
+                            .WithBody(JsonConvert.SerializeObject(approval))
+                                );
+            }
         }
 
-
-        [Given(@"the inner api will return a bad request")]
-        public void GivenTheInnerApiWillReturnABadRequest()
+        [When("the following apprenticeship is posted")]
+        public async Task WhenTheFollowingApprenticeshipIsPosted(Table table)
         {
-            _context.InnerApi.MockServer
-                .Given(
-                    Request.Create().WithPath($"/apprenticeships")
-                        .UsingPost())
-                .RespondWith(
-                    Response.Create()
-                        .WithStatusCode((int)HttpStatusCode.BadRequest)
-                        .WithHeader("Content-Type", "application/json")
-                        .WithBody(JsonConvert.SerializeObject(_errors))
-                );
-        }
-
-        [When(@"the apprenticeship is posted")]
-        public async Task WhenTheApprenticeshipIsPosted()
-        {
+            _request = table.CreateInstance<CreateApprenticeshipCommand>();
             await _context.OuterApiClient.Post("apprenticeships", _request);
         }
 
-        [Then(@"the result should be Accepted")]
-        public void ThenTheResultShouldBeAccepted()
-        {
-            _context.OuterApiClient.Response.StatusCode.Should().Be(HttpStatusCode.Accepted);
-        }
-
-        [Then(@"the result should be Bad Request")]
-        public void ThenTheResultShouldBeBadRequest()
-        {
-            _context.OuterApiClient.Response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
-        }
-
-        [Then(@"the request to the inner api was mapped correctly")]
+        [Then("the inner API was called successfully")]
         public void ThenTheRequestToTheInnerApiWasMappedCorrectly()
         {
+            _context.OuterApiClient.Response.StatusCode
+                .Should().Be(HttpStatusCode.Accepted);
+
             var logs = _context.InnerApi.MockServer.LogEntries;
-            logs.Count().Should().Be(1);
-            var log = logs.First();
-            
-            var innerApiRequest = JsonConvert.DeserializeObject<CreateApprenticeshipRequestData>(log.RequestMessage.Body);
+            logs.Should().HaveCount(1);
+
+            var innerApiRequest = JsonConvert.DeserializeObject<CreateApprenticeshipRequestData>(
+                logs.First().RequestMessage.Body);
+
             innerApiRequest.Should().NotBeNull();
             innerApiRequest.RegistrationId.Should().NotBe(Guid.Empty);
             innerApiRequest.Email.Should().Be(_request.Email);
             innerApiRequest.ApprenticeshipId.Should().Be(_request.ApprenticeshipId);
         }
 
-        [Then(@"the result should contain errors")]
-        public async Task ThenTheResultShouldContainErrors()
+        [Then("the inner API should return these errors")]
+        public async Task ThenTheInnerAPIShouldReturnTheseErrors(Table table)
         {
+            _context.OuterApiClient.Response.StatusCode
+                .Should().Be(HttpStatusCode.BadRequest);
+
+            var expectedErrors = table.Rows.ToDictionary(r => r[0], r => r[1]);
+
             var content = await _context.OuterApiClient.Response.Content.ReadAsStringAsync();
-
             var errors = JsonConvert.DeserializeObject<Dictionary<string, string>>(content);
-
-            errors.Should().BeEquivalentTo(_errors);
+            errors.Should().BeEquivalentTo(expectedErrors);
         }
 
-        [Then(@"the invitation was sent successfully")]
+        [Then("the invitation was sent successfully")]
         public void ThenTheInvitationWasSentSuccessfully()
         {
-            var logs = _context.LoginApi.MockServer.LogEntries;
-            logs.Count().Should().Be(1);
-            var log = logs.First();
+            var expectedCommitment = _approvedApprenticeships.First(
+                x => x.Id == _request.ApprenticeshipId);
 
-            loginApiRequest = JsonConvert.DeserializeObject<SendInvitationRequestData>(log.RequestMessage.Body);
+            var logs = _context.LoginApi.MockServer.LogEntries;
+            logs.Should().HaveCount(1);
+
+            var loginApiRequest = JsonConvert.DeserializeObject<SendInvitationRequestData>(
+                logs.First().RequestMessage.Body);
             loginApiRequest.Should().NotBeNull();
             loginApiRequest.SourceId.Should().NotBe(Guid.Empty);
             loginApiRequest.Email.Should().Be(_request.Email);
-            loginApiRequest.GivenName.Should().Be("GivenName");
-            loginApiRequest.FamilyName.Should().Be("FamilyName");
-            loginApiRequest.OrganisationName.Should().Be("OrganisationName");
-            loginApiRequest.ApprenticeshipName.Should().Be("ApprenticeshipName");
+            loginApiRequest.GivenName.Should().Be(expectedCommitment.FirstName);
+            loginApiRequest.FamilyName.Should().Be(expectedCommitment.LastName);
+            loginApiRequest.OrganisationName.Should().Be(_request.Organisation);
+            loginApiRequest.ApprenticeshipName.Should().Be(expectedCommitment.CourseName);
             loginApiRequest.Callback.Should().Be(_context.LoginConfig.CallbackUrl);
             loginApiRequest.UserRedirect.Should().Be(_context.LoginConfig.RedirectUrl);
+        }
+
+        [Then("the invitation was not sent")]
+        public void ThenTheInvitationWasNotSent()
+        {
+            _context.LoginApi.MockServer.LogEntries.Should().BeEmpty();
         }
     }
 }
