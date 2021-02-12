51593df9c52f288213269a2f113889288f6cd074
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-12 14:16:55 +0000

    Add delete shortlist item request

---
+++ src/SFA.DAS.FindApprenticeshipTraining/InnerApi/Requests/DeleteShortlistItemForUserRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests
+{
+    public class DeleteShortlistItemForUserRequest : IDeleteApiRequest
+    {
+        private readonly Guid _id;
+        private readonly Guid _userId;
+
+        public DeleteShortlistItemForUserRequest(Guid id, Guid userId)
+        {
+            _id = id;
+            _userId = userId;
+        }
+
+        public string DeleteUrl => $"users/{_userId}/items/{_id}";
+    }
+}
