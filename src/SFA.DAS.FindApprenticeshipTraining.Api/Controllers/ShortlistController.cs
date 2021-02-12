6cf8f0ad1ab20da018d1777b69d568605b744114
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-12 11:56:53 +0000

    Add post for shortlist

--- src/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/ShortlistController.cs
+++ src/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/ShortlistController.cs
@@ -4,8 +4,11 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.FindApprenticeshipTraining.Api.ApiRequests;
 using SFA.DAS.FindApprenticeshipTraining.Api.Models;
+using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.CreateShortlistForUser;
 using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Queries.GetShortlistForUser;
+using SFA.DAS.SharedOuterApi.Infrastructure;
 
 namespace SFA.DAS.FindApprenticeshipTraining.Api.Controllers
 {
@@ -39,6 +42,36 @@ namespace SFA.DAS.FindApprenticeshipTraining.Api.Controllers
             catch (Exception e)
             {
                 _logger.LogError(e, $"Error attempting to get shortlist for user:{userId}");
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> CreateShortlistForUser(CreateShortListRequest shortlistRequest)
+        {
+            try
+            {
+                await _mediator.Send(new CreateShortlistForUserCommand
+                {
+                    Lat = shortlistRequest.Lat,
+                    Lon = shortlistRequest.Lon,
+                    Ukprn = shortlistRequest.Ukprn,
+                    LocationDescription = shortlistRequest.LocationDescription,
+                    StandardId = shortlistRequest.StandardId,
+                    SectorSubjectArea = shortlistRequest.SectorSubjectArea,
+                    ShortlistUserId = shortlistRequest.ShortlistUserId
+                });
+
+                return Created("", null);
+            }
+            catch (HttpRequestContentException e)
+            {
+                return StatusCode((int) e.StatusCode, e.ErrorContent);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error creating shortlist item");
                 return BadRequest();
             }
         }
