e0acdf93a64c2773bedb72e74a892676c26988c3
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-02-18 20:41:06 +0000

    FAT2-410 - Return 404 if no result found for get provider

--- src/SFA.DAS.Reservations.Api.UnitTests/Controllers/Providers/WhenCallingGetProvider.cs
+++ src/SFA.DAS.Reservations.Api.UnitTests/Controllers/Providers/WhenCallingGetProvider.cs
@@ -40,6 +40,25 @@ namespace SFA.DAS.Reservations.Api.UnitTests.Controllers.Providers
             Assert.IsNotNull(model);
             model.Should().BeEquivalentTo(mediatorResult.Provider);
         }
+        [Test, MoqAutoData]
+        public async Task Then_Returns_Not_Found_If_No_Provider(
+            int ukPrn,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] ProvidersController controller)
+        {
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.Is<GetProviderQuery>(query => 
+                        query.Ukprn == ukPrn),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GetProviderResult{ Provider = null});
+
+            var controllerResult = await controller.GetProvider(ukPrn) as NotFoundResult;
+
+            Assert.IsNotNull(controllerResult);
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.NotFound);
+            
+        }
 
         [Test, MoqAutoData]
         public async Task And_Exception_Then_Returns_Bad_Request(
