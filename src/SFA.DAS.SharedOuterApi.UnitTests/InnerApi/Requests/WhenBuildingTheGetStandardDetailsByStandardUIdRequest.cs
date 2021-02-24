da5a35a8dac08f3e05be97ccb5c9ee67c759c492
dev4valtech <dev4valtech@gmail.com>
2021-02-24 11:01:33 +0000

    :sparkles: Request object for get details by standarduid

---
+++ src/SFA.DAS.SharedOuterApi.UnitTests/InnerApi/Requests/WhenBuildingTheGetStandardDetailsByStandardUIdRequest.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+
+namespace SFA.DAS.SharedOuterApi.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetStandardDetailsByStandardUIdRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(GetStandardDetailsByStandardUIdRequest actual)
+        {
+            actual.GetUrl.Should().Be($"api/courses/standards/{actual.StandardUId}");
+        }
+    }
+}
