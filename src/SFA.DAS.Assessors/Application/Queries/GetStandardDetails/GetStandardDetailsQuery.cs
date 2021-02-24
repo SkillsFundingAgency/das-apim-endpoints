a7c4cfe8a209b434d7cfaa7f7c74e56a58e39963
dev4valtech <dev4valtech@gmail.com>
2021-02-24 14:36:03 +0000

    :sparkles: Added standard details query handler

---
+++ src/SFA.DAS.Assessors/Application/Queries/GetStandardDetails/GetStandardDetailsQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Assessors.Application.Queries.GetStandardDetails
+{
+    public class GetStandardDetailsQuery : IRequest<GetStandardDetailsResult>
+    {
+        public string StandardUId { get; }
+        public GetStandardDetailsQuery(string standardUId)
+        {
+            StandardUId = standardUId;
+        }
+    }
+}
