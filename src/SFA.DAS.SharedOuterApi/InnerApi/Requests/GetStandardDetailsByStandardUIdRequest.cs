8da569fad642a49e8fdd741c6f9da13a979336c0
dev4valtech <dev4valtech@gmail.com>
2021-02-24 11:01:33 +0000

    :sparkles: Request object for get details by standarduid

---
+++ src/SFA.DAS.SharedOuterApi/InnerApi/Requests/GetStandardDetailsByStandardUIdRequest.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests
+{
+    public class GetStandardDetailsByStandardUIdRequest : IGetApiRequest
+    {
+        public string StandardUId { get; }
+        public string GetUrl => $"api/courses/standards/{StandardUId}";
+
+        public GetStandardDetailsByStandardUIdRequest(string standardUId)
+        {
+            StandardUId = standardUId;
+        }
+    }
+}
