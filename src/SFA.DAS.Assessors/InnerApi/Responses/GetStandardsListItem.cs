9e4f4d0261b11006ee5309df77be7389dbda16cb
dev4valtech <dev4valtech@gmail.com>
2021-02-25 15:09:12 +0000

    :zap: Included key fields in assessor service response

--- src/SFA.DAS.Assessors/InnerApi/Responses/GetStandardsListItem.cs
+++ src/SFA.DAS.Assessors/InnerApi/Responses/GetStandardsListItem.cs
@@ -5,8 +5,11 @@ namespace SFA.DAS.Assessors.InnerApi.Responses
     public class GetStandardsListItem : StandardApiResponseBase
     {
         public string StandardUId { get; set; }
+        public string IfateReferenceNumber { get; set; }
         public int LarsCode { get; set; }
         public string Title { get; set; }
+        public decimal? Version { get; set; }
         public int Level { get; set; }
+        public string Status { get; set; }
     }
 }
