d12069a4acd58e2cfda4b7fa52c4297871d788c5
Ben Stone <ben1.stone@education.gov.uk>
2021-02-26 16:09:06 +0000

    SV-485 Add GetStandardOptionsList to assessor outer api

---
+++ src/SFA.DAS.Assessors.Api/Models/GetStandardOptionsItem.cs
@@ -0,0 +1,24 @@
+﻿using SFA.DAS.Assessors.InnerApi.Responses;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Assessors.Api.Models
+{
+    public class GetStandardOptionsItem
+    {
+        public string StandardUid { get; set; }
+        public int LarsCode { get; set; }
+        public string IfateReferenceNumber { get; set; }
+        public IEnumerable<string> Options { get; set; }
+
+        public static implicit operator GetStandardOptionsItem(GetStandardOptionsListItem source)
+        {
+            return new GetStandardOptionsItem
+            {
+                StandardUid = source.StandardUid,
+                IfateReferenceNumber = source.IfateReferenceNumber,
+                LarsCode = source.LarsCode,
+                Options = source.Options
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.Assessors/Application/Queries/GetStandardOptions/GetStandardOptionsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Assessors.Application.Queries.GetStandardOptions
+{
+    public class GetStandardOptionsQuery : IRequest<GetStandardOptionsResult>
+    {
+    }
+}
