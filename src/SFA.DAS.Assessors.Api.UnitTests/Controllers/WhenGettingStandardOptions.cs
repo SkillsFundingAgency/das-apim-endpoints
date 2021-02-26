3aa855283ea8376effd3c16d4921b6427c21dd60
Ben Stone <ben1.stone@education.gov.uk>
2021-02-26 16:09:06 +0000

    SV-485 Add GetStandardOptionsList to assessor outer api

---
+++ src/SFA.DAS.Assessors.Api.UnitTests/Controllers/WhenGettingStandardOptions.cs
@@ -0,0 +1,46 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Assessors.Api.Controllers;
+using SFA.DAS.Assessors.Api.Models;
+using SFA.DAS.Assessors.Application.Queries.GetStandardOptions;
+using SFA.DAS.Testing.AutoFixture;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Assessors.Api.UnitTests.Controllers
+{
+    public class WhenGettingStandardOptions
+    {
+
+        [Test, MoqAutoData]
+        public async Task Then_gets_Standard_and_options_from_mediator(GetStandardOptionsResult mediatorResult,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] TrainingCoursesController controller)
+        {
+            mockMediator.Setup(mediator => mediator.Send(It.IsAny<GetStandardOptionsQuery>(),It.IsAny<CancellationToken>()))
+                .ReturnsAsync(mediatorResult);
+
+            var controllerResult = await controller.GetStandardOptionsList() as ObjectResult;
+
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
+
+            var model = controllerResult.Value as GetStandardOptionsResponse;
+
+            var response = mediatorResult.Standards.Select(s => new GetStandardOptionsItem
+            {
+                StandardUid = s.StandardUid,
+                LarsCode = s.LarsCode,
+                IfateReferenceNumber = s.IfateReferenceNumber,
+                Options = s.Options
+            });
+
+            model.Standards.Should().BeEquivalentTo(response);
+        }
+    }
+}
