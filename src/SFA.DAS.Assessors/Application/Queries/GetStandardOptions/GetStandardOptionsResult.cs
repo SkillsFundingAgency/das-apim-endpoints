cd0bd0492e660f0fcd076426daf42d3ed5a793f2
Ben Stone <ben1.stone@education.gov.uk>
2021-02-26 16:09:06 +0000

    SV-485 Add GetStandardOptionsList to assessor outer api

---
+++ src/SFA.DAS.Assessors/Application/Queries/GetStandardOptions/GetStandardOptionsResult.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.Assessors.InnerApi.Responses;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Assessors.Application.Queries.GetStandardOptions
+{
+    public class GetStandardOptionsResult
+    {
+        public IEnumerable<GetStandardOptionsListItem> Standards { get; set; }
+    }
+}
