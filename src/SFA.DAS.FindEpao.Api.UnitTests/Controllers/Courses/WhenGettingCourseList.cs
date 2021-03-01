efd8d3b9745e858ae456046056e27c0fdea5fc20
dev4valtech <dev4valtech@gmail.com>
2021-03-01 12:33:08 +0000

    :zap: Fix PR comments

--- src/SFA.DAS.FindEpao.Api.UnitTests/Controllers/Courses/WhenGettingCourseList.cs
+++ src/SFA.DAS.FindEpao.Api.UnitTests/Controllers/Courses/WhenGettingCourseList.cs
@@ -36,10 +36,7 @@ namespace SFA.DAS.FindEpao.Api.UnitTests.Controllers.Courses
             controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
             var model = controllerResult.Value as GetCourseListResponse;
             Assert.IsNotNull(model);
-            model.Courses.Should().BeEquivalentTo(mediatorResult.Courses, options => options
-            .Excluding(c => c.StandardUId)
-            .Excluding(c => c.LarsCode));
-            model.Courses.All(c => c.Id > 0).Should().BeTrue();
+            model.Courses.Should().BeEquivalentTo(mediatorResult.Courses.Select(course => (GetCourseListItem)course));
         }
 
         [Test, MoqAutoData]
