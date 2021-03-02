a0d23dbb79d6bfaefef5ba8d0b4118564e66791a
Ben Stone <ben1.stone@education.gov.uk>
2021-03-02 16:25:12 +0000

    SV-485 Add unit test for mapping to api response

---
+++ src/SFA.DAS.Assessors.Api.UnitTests/Models/WhenCastingGetStandardOptionsResponseFromMediatorType.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Assessors.Api.Models;
+using SFA.DAS.Assessors.InnerApi.Responses;
+
+namespace SFA.DAS.Assessors.Api.UnitTests.Models
+{
+    public class WhenCastingGetStandardOptionsResponseFromMediatorType
+    {
+        [Test, AutoData]
+        public void Then_Maps_Fields_Appropriately(
+            GetStandardOptionsListItem source)
+        {
+            var response = (GetStandardOptionsItem)source;
+
+            response.Should().BeEquivalentTo(source, options => options.ExcludingMissingMembers());
+        }
+    }
+}
