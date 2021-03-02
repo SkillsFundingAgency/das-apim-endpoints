68aa2b49806a097fba78bd70bc5aeb69f11dc1f5
Ben Stone <ben1.stone@education.gov.uk>
2021-03-02 09:54:56 +0000

    SV-485 casing on StandardUId

--- src/SFA.DAS.Assessors/InnerApi/Responses/GetStandardOptionsListItem.cs
+++ src/SFA.DAS.Assessors/InnerApi/Responses/GetStandardOptionsListItem.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.Assessors.InnerApi.Responses
 {
     public class GetStandardOptionsListItem
     {
-        public string StandardUid { get; set; }
+        public string StandardUId { get; set; }
         public int LarsCode { get; set; }
         public string IfateReferenceNumber { get; set; }
         public List<string> Options { get; set; }
