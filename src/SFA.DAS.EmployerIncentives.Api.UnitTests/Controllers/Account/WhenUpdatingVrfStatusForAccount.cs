509c4cf9558f1736accce272e403241f88c1addc
Dave Read <david.read@digital.education.gov.uk>
2021-03-10 14:50:51 +0000

    EI-963 add endpoint to update vrf case status for single legal entity

---
+++ src/SFA.DAS.EmployerIncentives.Api.UnitTests/Controllers/Account/WhenUpdatingVrfStatusForAccount.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Api.Controllers;
+using SFA.DAS.EmployerIncentives.Application.Commands.SignAgreement;
+using SFA.DAS.EmployerIncentives.Application.Commands.UpdateVendorRegistrationFormCaseStatus;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerIncentives.Api.UnitTests.Controllers.Account
+{
+    [TestFixture]
+    public class WhenUpdatingVrfStatusForAccount
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Updates_Vrf_Case_Status_Calling_Mediator(
+            long accountId,
+            long accountLegalEntityId,
+            string vrfCaseStatus,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy]AccountController controller)
+        {
+            var controllerResult = await controller.UpdateVrfCaseStatus(accountId, accountLegalEntityId, vrfCaseStatus) as NoContentResult;
+
+            Assert.IsNotNull(controllerResult);
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.NoContent);
+            mockMediator
+                .Verify(mediator => mediator.Send(
+                    It.Is<UpdateVendorRegistrationCaseStatusCommand>(c =>
+                        c.AccountId.Equals(accountId)
+                        && c.AccountLegalEntityId.Equals(accountLegalEntityId)
+                        && c.VrfCaseStatus.Equals(vrfCaseStatus)),
+                    It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives/Application/Commands/UpdateVendorRegistrationFormCaseStatus/UpdateVendorRegistrationCaseStatusCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerIncentives.InnerApi.Requests.VendorRegistrationForm;
+using SFA.DAS.EmployerIncentives.Interfaces;
+
+namespace SFA.DAS.EmployerIncentives.Application.Commands.UpdateVendorRegistrationFormCaseStatus
+{
+    public class UpdateVendorRegistrationCaseStatusCommandHandler : IRequestHandler<UpdateVendorRegistrationCaseStatusCommand>
+    {
+        private readonly IEmployerIncentivesService _incentivesService;
+        public UpdateVendorRegistrationCaseStatusCommandHandler(IEmployerIncentivesService incentivesService)
+        {
+            _incentivesService = incentivesService;
+        }
+
+        public async Task<Unit> Handle(UpdateVendorRegistrationCaseStatusCommand request, CancellationToken cancellationToken)
+        {
+            var legalEntity = await _incentivesService.GetLegalEntity(request.AccountId, request.AccountLegalEntityId);
+
+            var updateRequest = new UpdateVendorRegistrationCaseStatusRequest
+            {
+                HashedLegalEntityId = legalEntity.HashedLegalEntityId,
+                Status = request.VrfCaseStatus,
+                CaseStatusLastUpdatedDate = DateTime.Now
+            };
+            await _incentivesService.UpdateVendorRegistrationCaseStatus(updateRequest);
+            return await Task.FromResult(Unit.Value);
+        }
+    }
+}
