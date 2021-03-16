acbe36922681b946c6671f51f49f9f81b44768ed
Ben Arnold <ben@arnoldfamily.me.uk>
2021-03-16 14:17:08 +0000

    Training Provider confirmation

---
+++ src/SFA.DAS.ApprenticeCommitments/Apis/InnerApi/TrainingProviderConfirmationRequest.cs
@@ -0,0 +1,35 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+
+namespace SFA.DAS.ApprenticeCommitments.Apis.InnerApi
+{
+    public class TrainingProviderConfirmationRequest : IPostApiRequest
+    {
+        private readonly Guid _apprenticeId;
+        private readonly long _apprenticeshipId;
+
+        public TrainingProviderConfirmationRequest(
+            Guid apprentice, long apprenticeship, bool trainingProviderCorrect)
+        {
+            _apprenticeId = apprentice;
+            _apprenticeshipId = apprenticeship;
+            Data = new TrainingProviderConfirmationRequestData
+            {
+                TrainingProviderCorrect = trainingProviderCorrect
+            };
+        }
+
+        public string PostUrl => $"/apprentices/{_apprenticeId}/apprenticeships/{_apprenticeshipId}/trainingproviderconfirmation";
+
+        public object Data { get; set; }
+    }
+
+    public class TrainingProviderConfirmationRequestData
+    {
+        public bool TrainingProviderCorrect { get; set; }
+    }
+
+    public class TrainingProviderConfirmationResponse
+    {
+    }
+}
