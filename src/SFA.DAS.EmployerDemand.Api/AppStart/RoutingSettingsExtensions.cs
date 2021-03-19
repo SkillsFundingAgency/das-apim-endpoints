a8229fdb95105695284002d8a68bd3628263ebc9
Scott Wakefield <scott@expertcc.co.uk>
2021-03-19 15:58:40 +0000

    AEDB-19 setup nservicebus in app.

---
+++ src/SFA.DAS.EmployerDemand.Api/AppStart/RoutingSettingsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using SFA.DAS.SharedOuterApi.Models;
+
+namespace SFA.DAS.EmployerDemand.Api.AppStart
+{
+    public static class RoutingSettingsExtensions
+    {
+        private const string NotificationsMessageHandler = "SFA.DAS.Notifications.MessageHandlers";
+
+        public static void AddRouting(this RoutingSettings routingSettings)
+        {
+            routingSettings.RouteToEndpoint(typeof(SendEmailCommand), NotificationsMessageHandler);
+        }
+    }
+}
