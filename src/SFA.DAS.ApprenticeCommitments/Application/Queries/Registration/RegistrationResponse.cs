20cf0f7e2b8f5024c9d05c042a2cd2c9a4c59443
Paul Graham <paul.graham@coprime.co.uk>
2021-03-29 12:32:32 +0100

    added hasViewed and HasCompleted Verification flags to response

--- src/SFA.DAS.ApprenticeCommitments/Application/Queries/Registration/RegistrationResponse.cs
+++ src/SFA.DAS.ApprenticeCommitments/Application/Queries/Registration/RegistrationResponse.cs
@@ -6,5 +6,7 @@ namespace SFA.DAS.ApprenticeCommitments.Application.Queries.Registration
     {
         public Guid ApprenticeId { get; set; }
         public string Email { get; set; }
+        public bool HasViewedVerification { get; set; }
+        public bool HasCompletedVerification { get; set; }
     }
 }
