7fc93372f2b998be7cf04005a6cae899b143273e
Ben Arnold <ben@arnoldfamily.me.uk>
2021-03-30 03:08:51 +0100

    Add confirmation states to be passed through in Apprenticeship query response

--- src/SFA.DAS.ApprenticeCommitments/Apis/InnerApi/GetApprenticeshipResponse.cs
+++ src/SFA.DAS.ApprenticeCommitments/Apis/InnerApi/GetApprenticeshipResponse.cs
@@ -7,5 +7,8 @@
         public string TrainingProviderName { get; set; }
         public bool? TrainingProviderCorrect { get; set; }
         public bool? EmployerCorrect { get; set; }
+        public bool? ApprenticeshipDetailsConfirmation { get; set; }
+        public bool? ApprenticeshipDeliveryConfirmation { get; set; }
+        public bool? RolesAndResponsibilitiesConfirmation { get; set; }
     }
 }
