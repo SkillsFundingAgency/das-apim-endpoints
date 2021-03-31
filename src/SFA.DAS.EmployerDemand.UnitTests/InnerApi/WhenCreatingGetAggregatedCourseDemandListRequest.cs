28b85f5d8599dc531e4156f4b8c603ca61874dad
Scott Wakefield <scott@expertcc.co.uk>
2021-03-31 11:25:51 +0100

    AEDB-25 adding api request.

---
+++ src/SFA.DAS.EmployerDemand.UnitTests/InnerApi/WhenCreatingGetAggregatedCourseDemandListRequest.cs
@@ -0,0 +1,16 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.InnerApi.Requests;
+
+namespace SFA.DAS.EmployerDemand.UnitTests.InnerApi
+{
+    public class WhenCreatingGetAggregatedCourseDemandListRequest
+    {
+        [Test, AutoData]
+        public void Then_Sets_Url_Correctly(GetAggregatedCourseDemandListRequest request)
+        {
+            request.GetUrl.Should().Be($"api/demand/aggregated/providers/{request.Ukprn}?courseId={request.CourseId}");
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand/InnerApi/Requests/GetAggregatedCourseDemandListRequest.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.InnerApi.Requests
+{
+    public class GetAggregatedCourseDemandListRequest : IGetApiRequest
+    {
+        public string GetUrl => $"api/demand/aggregated/providers/{Ukprn}?courseId={CourseId}";
+        public int Ukprn { get; }
+        public int CourseId { get; }
+
+        public GetAggregatedCourseDemandListRequest(int ukprn, int courseId)
+        {
+            Ukprn = ukprn;
+            CourseId = courseId;
+        }
+    }
+}
