4cd4055b0aea017f99ebcf1f7b9da5fcefc4d984
Scott Wakefield <scott@expertcc.co.uk>
2021-04-01 12:57:16 +0100

    AEDB-25 adding mapping in api model.

---
+++ src/SFA.DAS.EmployerDemand.Api/Models/GetAggregatedCourseDemandSummary.cs
@@ -0,0 +1,26 @@
+﻿namespace SFA.DAS.EmployerDemand.Api.Models
+{
+    public class GetAggregatedCourseDemandSummary
+    {
+        public GetCourseListItem Course { get; set; }
+        public int EmployersCount { get; set; }
+        public int ApprenticesCount { get; set; }
+
+        public static implicit operator GetAggregatedCourseDemandSummary(
+            InnerApi.Responses.GetAggreatedCourseDemandSummaryResponse source)
+        {
+            return new GetAggregatedCourseDemandSummary
+            {
+                EmployersCount = source.EmployersCount,
+                ApprenticesCount = source.ApprenticesCount,
+                Course = new GetCourseListItem
+                {
+                    Id = source.CourseId,
+                    Level = source.CourseLevel,
+                    Sector = source.CourseRoute,
+                    Title = source.CourseTitle
+                }
+            };
+        }
+    }
+}
