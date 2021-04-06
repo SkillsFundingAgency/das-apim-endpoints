c70123bed2c6e7ebfa8519a67cdc6545ee64c864
Ben Stone <ben1.stone@education.gov.uk>
2021-04-06 12:03:51 +0100

    Added building request tests

---
+++ src/SFA.DAS.SharedOuterApi.UnitTests/InnerApi/Requests/WhenBuildingTheGetNotYetApprovedStandardsRequest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+
+namespace SFA.DAS.SharedOuterApi.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingTheGetNotYetApprovedStandardsRequest
+    {
+        [Test]
+        public void Then_the_URL_is_correctly_built()
+        {
+            var actual = new GetNotYetApprovedStandardsRequest();
+
+            actual.GetUrl.Should().Be("api/courses/standards?filter=NotYetApproved");
+        }
+    }
+}
