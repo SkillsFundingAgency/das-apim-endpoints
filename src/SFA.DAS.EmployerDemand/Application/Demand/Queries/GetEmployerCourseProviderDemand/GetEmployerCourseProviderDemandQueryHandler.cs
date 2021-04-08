8342116061fe237ab47e740a0819d65bfdafc84b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-04-08 09:50:33 +0100

    AEDb-31 - Add Handler for getting course demands

---
+++ src/SFA.DAS.EmployerDemand/Application/Demand/Queries/GetEmployerCourseProviderDemand/GetEmployerCourseProviderDemandQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerDemand.InnerApi.Requests;
+using SFA.DAS.EmployerDemand.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Application.Demand.Queries.GetEmployerCourseProviderDemand
+{
+    public class GetEmployerCourseProviderDemandQueryHandler : IRequestHandler<GetEmployerCourseProviderDemandQuery, GetEmployerCourseProviderDemandQueryResult>
+    {
+        private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
+        private readonly IEmployerDemandApiClient<EmployerDemandApiConfiguration> _employerDemandApiClient;
+        private readonly ILocationLookupService _locationLookupService;
+
+        public GetEmployerCourseProviderDemandQueryHandler (
+            ICoursesApiClient<CoursesApiConfiguration> coursesApiClient, 
+            IEmployerDemandApiClient<EmployerDemandApiConfiguration> employerDemandApiClient, 
+            ILocationLookupService locationLookupService)
+        {
+            _coursesApiClient = coursesApiClient;
+            _employerDemandApiClient = employerDemandApiClient;
+            _locationLookupService = locationLookupService;
+        }
+        public async Task<GetEmployerCourseProviderDemandQueryResult> Handle(GetEmployerCourseProviderDemandQuery request, CancellationToken cancellationToken)
+        {
+            var courseTask = _coursesApiClient.Get<GetStandardsListItem>(new GetStandardRequest(request.CourseId));
+            var locationTask = _locationLookupService.GetLocationInformation(request.LocationName,0, 0);
+
+            await Task.WhenAll(courseTask, locationTask);
+
+            var demand = await _employerDemandApiClient.Get<GetEmployerCourseProviderListResponse>(
+                new GetCourseProviderDemandsRequest(request.Ukprn, request.CourseId,
+                    locationTask.Result.GeoPoint.FirstOrDefault(), locationTask.Result.GeoPoint.LastOrDefault(),
+                    request.Radius));
+
+            return new GetEmployerCourseProviderDemandQueryResult
+            {
+                Course = courseTask.Result,
+                Location = locationTask.Result,
+                EmployerCourseDemands = demand
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand/Application/Demand/Queries/GetEmployerCourseProviderDemand/GetEmployerCourseProviderDemandQueryResult.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.EmployerDemand.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Models;
+
+namespace SFA.DAS.EmployerDemand.Application.Demand.Queries.GetEmployerCourseProviderDemand
+{
+    public class GetEmployerCourseProviderDemandQueryResult
+    {
+        public GetStandardsListItem Course { get ; set ; }
+        public LocationItem Location { get ; set ; }
+        public object EmployerCourseDemands { get ; set ; }
+    }
+}
