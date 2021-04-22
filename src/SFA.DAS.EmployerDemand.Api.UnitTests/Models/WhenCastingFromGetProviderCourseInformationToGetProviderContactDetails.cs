82183a548954458146d5f1ef0a0ca07eb6157942
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-04-22 17:08:31 +0100

    AEDB-40 - Add mapping from meditor response

---
+++ src/SFA.DAS.EmployerDemand.Api.UnitTests/Models/WhenCastingFromGetProviderCourseInformationToGetProviderContactDetails.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Api.Models;
+using SFA.DAS.EmployerDemand.InnerApi.Responses;
+
+namespace SFA.DAS.EmployerDemand.Api.UnitTests.Models
+{
+    public class WhenCastingFromGetProviderCourseInformationToGetProviderContactDetails
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Correctly_Mapped(GetProviderCourseInformation source)
+        {
+            //Act
+            var actual = (GetProviderContactDetails)source;
+            
+            //Assert
+            actual.Ukprn.Should().Be(source.Ukprn);
+            actual.Website.Should().Be(source.ContactUrl);
+            actual.PhoneNumber.Should().Be(source.Phone);
+            actual.EmailAddress.Should().Be(source.Email);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand.Api/Models/GetProviderContactDetails.cs
@@ -0,0 +1,23 @@
+﻿using SFA.DAS.EmployerDemand.InnerApi.Responses;
+
+namespace SFA.DAS.EmployerDemand.Api.Models
+{
+    public class GetProviderContactDetails
+    {
+        public int Ukprn { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Website { get; set; }
+
+        public static implicit operator GetProviderContactDetails(GetProviderCourseInformation source)
+        {
+            return new GetProviderContactDetails
+            {
+                Ukprn = source.Ukprn,
+                Website = source.ContactUrl,
+                EmailAddress = source.Email,
+                PhoneNumber = source.Phone
+            };
+        }
+    }
+}
