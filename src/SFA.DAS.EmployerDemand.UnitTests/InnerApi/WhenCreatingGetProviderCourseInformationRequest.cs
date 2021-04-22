c6759d9dfd051f0360a10d63cd2c07c5ac84260f
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-04-22 16:39:29 +0100

    AEDB-40 - Add request for getting course provider information

---
+++ src/SFA.DAS.EmployerDemand.UnitTests/InnerApi/WhenCreatingGetProviderCourseInformationRequest.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.InnerApi.Requests;
+
+namespace SFA.DAS.EmployerDemand.UnitTests.InnerApi
+{
+    public class WhenCreatingGetProviderCourseInformationRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Constructed(int ukprn, int courseId)
+        {
+            //Act
+            var actual = new GetProviderCourseInformationRequest(ukprn, courseId);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"api/Courses/{courseId}/providers/{ukprn}");
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand/InnerApi/Requests/GetProviderCourseInformationRequest.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.InnerApi.Requests
+{
+    public class GetProviderCourseInformationRequest : IGetApiRequest
+    {
+        private readonly int _ukprn;
+        private readonly int _courseId;
+
+        public GetProviderCourseInformationRequest(int ukprn, int courseId)
+        {
+            _ukprn = ukprn;
+            _courseId = courseId;
+        }
+
+        public string GetUrl => $"api/Courses/{_courseId}/providers/{_ukprn}";
+    }
+}
