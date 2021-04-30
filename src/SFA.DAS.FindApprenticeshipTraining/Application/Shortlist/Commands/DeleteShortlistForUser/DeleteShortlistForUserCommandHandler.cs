f228af8702e4916290c085c96073ca0392c194cf
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-04-30 11:24:14 +0100

    Add command handler for deleting shortlist by user id

---
+++ src/SFA.DAS.FindApprenticeshipTraining/Application/Shortlist/Commands/DeleteShortlistForUser/DeleteShortlistForUserCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.DeleteShortlistForUser
+{
+    public class DeleteShortlistForUserCommandHandler : IRequestHandler<DeleteShortlistForUserCommand, Unit>
+    {
+        private readonly ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> _courseDeliveryApiClient;
+
+        public DeleteShortlistForUserCommandHandler (ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> courseDeliveryApiClient)
+        {
+            _courseDeliveryApiClient = courseDeliveryApiClient;
+        }
+        public async Task<Unit> Handle(DeleteShortlistForUserCommand request, CancellationToken cancellationToken)
+        {
+            await _courseDeliveryApiClient.Delete(new DeleteShortlistForUserRequest(request.UserId));
+
+            return new Unit();
+        }
+    }
+}
