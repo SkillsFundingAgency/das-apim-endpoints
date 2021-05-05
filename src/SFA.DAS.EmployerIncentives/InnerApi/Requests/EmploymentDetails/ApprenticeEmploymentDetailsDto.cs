1751e1d2d3fa343ac888dff97c37f96cfbc4ffd8
Dave Read <david.read@digital.education.gov.uk>
2021-05-05 17:07:27 +0100

    EI-973 add endpoint for submitting apprentice employment start dates

---
+++ src/SFA.DAS.EmployerIncentives/InnerApi/Requests/EmploymentDetails/ApprenticeEmploymentDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SFA.DAS.EmployerIncentives.InnerApi.Requests.EmploymentDetails
+{
+    public class ApprenticeEmploymentDetailsDto
+    {
+        public long ApprenticeId { get; set; }
+        public DateTime EmploymentStartDate { get; set; }
+    }
+}
