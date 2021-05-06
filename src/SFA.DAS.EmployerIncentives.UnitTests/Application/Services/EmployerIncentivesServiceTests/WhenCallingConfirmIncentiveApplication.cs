24db5a6d4c973a8ba12d27ff4b06b35534770cdb
Michael Young <mick.young1981@googlemail.com>
2021-05-06 13:45:55 +0100

    Check response code in service layer.

--- src/SFA.DAS.EmployerIncentives.UnitTests/Application/Services/EmployerIncentivesServiceTests/WhenCallingConfirmIncentiveApplication.cs
+++ src/SFA.DAS.EmployerIncentives.UnitTests/Application/Services/EmployerIncentivesServiceTests/WhenCallingConfirmIncentiveApplication.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net;
 using AutoFixture.NUnit3;
 using Moq;
 using NUnit.Framework;
@@ -7,6 +9,9 @@ using SFA.DAS.EmployerIncentives.InnerApi.Requests.IncentiveApplication;
 using SFA.DAS.EmployerIncentives.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
 using System.Threading.Tasks;
+using FluentAssertions;
+using SFA.DAS.EmployerIncentives.Exceptions;
+using SFA.DAS.SharedOuterApi.Models;
 
 namespace SFA.DAS.EmployerIncentives.UnitTests.Application.Services.EmployerIncentivesServiceTests
 {
@@ -18,14 +23,36 @@ namespace SFA.DAS.EmployerIncentives.UnitTests.Application.Services.EmployerInce
             [Frozen] Mock<IEmployerIncentivesApiClient<EmployerIncentivesConfiguration>> client,
             EmployerIncentivesService service)
         {
+            client.Setup(x =>
+                x.PatchWithResponseCode(It.Is<ConfirmIncentiveApplicationRequest>(
+                    c =>
+                        c.PatchUrl.Contains(request.Data.IncentiveApplicationId.ToString())
+                ))).ReturnsAsync(new ApiResponse<string>("The Body", HttpStatusCode.OK));
 
             await service.ConfirmIncentiveApplication(request);
 
             client.Verify(x =>
-                x.Patch(It.Is<ConfirmIncentiveApplicationRequest>(
+                x.PatchWithResponseCode(It.Is<ConfirmIncentiveApplicationRequest>(
                     c =>
                         c.PatchUrl.Contains(request.Data.IncentiveApplicationId.ToString())
                 )), Times.Once);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_An_Exception_Is_Thrown_When_The_InnerApi_Returns_A_Conflict(
+            ConfirmIncentiveApplicationRequest request,
+            [Frozen] Mock<IEmployerIncentivesApiClient<EmployerIncentivesConfiguration>> client,
+            EmployerIncentivesService service)
+        {
+            client.Setup(x =>
+                x.PatchWithResponseCode(It.Is<ConfirmIncentiveApplicationRequest>(
+                    c =>
+                        c.PatchUrl.Contains(request.Data.IncentiveApplicationId.ToString())
+                ))).ReturnsAsync(new ApiResponse<string>("The Body", HttpStatusCode.Conflict));
+
+            Func<Task> action = async () => await service.ConfirmIncentiveApplication(request);
+
+            action.Should().Throw<UlnAlreadySubmittedException>();
         }
     }
 }
