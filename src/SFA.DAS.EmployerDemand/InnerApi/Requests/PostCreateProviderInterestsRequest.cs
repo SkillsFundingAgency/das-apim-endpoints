28ae8d8f3df5d717e08f29f7b566ecc42609d366
Scott Wakefield <scott@expertcc.co.uk>
2021-05-11 12:21:12 +0100

    AEDB-34 adding post api request.

---
+++ src/SFA.DAS.EmployerDemand/InnerApi/Requests/PostCreateProviderInterestsRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.InnerApi.Requests
+{
+    public class PostCreateProviderInterestsRequest : IPostApiRequest
+    {
+        public PostCreateProviderInterestsRequest(CreateProviderInterestsData data)
+        {
+            Data = data;
+        }
+
+        public string PostUrl => "api/providerinterest/create";
+        public object Data { get; set; }
+    }
+
+    public class CreateProviderInterestsData
+    {
+        public IEnumerable<Guid> EmployerDemandIds { get; set; }
+        public int Ukprn { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+    }
+}
