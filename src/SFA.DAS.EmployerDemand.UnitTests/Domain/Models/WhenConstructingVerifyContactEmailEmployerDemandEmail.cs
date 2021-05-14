1a8dfd2b74e2a75f6927afff4b567e4c87d6de58
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-14 14:29:38 +0100

    Add email template for verify email

---
+++ src/SFA.DAS.EmployerDemand.UnitTests/Domain/Models/WhenConstructingVerifyContactEmailEmployerDemandEmail.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Domain.Models;
+
+namespace SFA.DAS.EmployerDemand.UnitTests.Domain.Models
+{
+    public class WhenConstructingVerifyContactEmailEmployerDemandEmail
+    {
+        [Test, AutoData]
+        public void Then_The_Values_Are_Set_Correctly(string employerName, string trainingCourse, string confirmationLink)
+        {
+            var expectedTokens = new Dictionary<string, string>
+            {
+                {"AEDEmployerName", employerName},
+                {"AEDApprenticeshipTrainingCourse", trainingCourse},
+                {"AEDConfirmEmailURL", confirmationLink}
+            };
+
+            var actual = new CreateVerifyEmployerDemandEmail(employerName, trainingCourse, confirmationLink);
+
+            actual.TemplateId.Should().Be(EmailConstants.VerifyContactEmailEmployerDemandTemplateId);
+            actual.Subject.Should().Be("Confirm your contact email address");
+            actual.Tokens.Should().BeEquivalentTo(expectedTokens);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand/Domain/Models/CreateVerifyEmployerDemandEmail.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.SharedOuterApi.Models.Messages;
+
+namespace SFA.DAS.EmployerDemand.Domain.Models
+{
+    public class CreateVerifyEmployerDemandEmail : EmailTemplateArguments
+    {
+        public CreateVerifyEmployerDemandEmail(string employerName, string trainingCourse, string confirmationLink)
+        {
+            TemplateId = EmailConstants.VerifyContactEmailEmployerDemandTemplateId;
+            Subject  = "Confirm your contact email address";
+            Tokens = new Dictionary<string, string>
+            {
+                {"AEDEmployerName", employerName },
+                {"AEDApprenticeshipTrainingCourse", trainingCourse },
+                {"AEDConfirmEmailURL", confirmationLink }
+            };
+        }
+    }
+}
