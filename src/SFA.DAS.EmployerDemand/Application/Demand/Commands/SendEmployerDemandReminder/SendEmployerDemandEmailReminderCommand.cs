7268866a7447fa2c50a5ebf3cfedb5c1b1791f85
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-27 22:20:41 +0100

    AEDB-36 - Create reminder email template

---
+++ src/SFA.DAS.EmployerDemand/Application/Demand/Commands/SendEmployerDemandReminder/SendEmployerDemandEmailReminderCommand.cs
@@ -0,0 +1,9 @@
+using MediatR;
+
+namespace SFA.DAS.EmployerDemand.Application.Demand.Commands.SendEmployerDemandReminder
+{
+    public class SendEmployerDemandRemindCommand : IRequest<Unit>
+    {
+        
+    }
+}
