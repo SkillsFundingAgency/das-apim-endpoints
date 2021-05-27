91356cd2eb711cc3a628e958194fb839bce4ce1c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-27 21:37:53 +0100

    AEDB-36 - Add request for get url to get unmet demands

---
+++ src/SFA.DAS.EmployerDemand.UnitTests/InnerApi/WhenCreatingGetUnmetEmployerDemandsRequest.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.InnerApi.Requests;
+
+namespace SFA.DAS.EmployerDemand.UnitTests.InnerApi
+{
+    public class WhenCreatingGetUnmetEmployerDemandsRequest
+    {
+        [Test, AutoData]
+        public void Then_Creates_Url_Correctly(uint ageOfDemandInDays)
+        {
+            //Arrange
+            var actual = new GetUnmetEmployerDemandsRequest(ageOfDemandInDays);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"api/Demand/unmet?ageOfDemandInDays={ageOfDemandInDays}");
+        }
+    }
+}
