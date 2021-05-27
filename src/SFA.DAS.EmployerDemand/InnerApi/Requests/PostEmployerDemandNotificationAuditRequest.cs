ee0598b52713da1a33a8ee6d66260aa8c391174c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-05-27 21:42:36 +0100

    AEDB-36 - Add create notification audit post request

---
+++ src/SFA.DAS.EmployerDemand/InnerApi/Requests/PostEmployerDemandNotificationAuditRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.InnerApi.Requests
+{
+    public class PostEmployerDemandNotificationAuditRequest : IPostApiRequest<object>
+    {
+        private readonly Guid _id;
+        private readonly Guid _courseDemandId;
+
+        public PostEmployerDemandNotificationAuditRequest(Guid id, Guid courseDemandId)
+        {
+            _id = id;
+            _courseDemandId = courseDemandId;
+        }
+
+        public string PostUrl => $"api/Demand/{_courseDemandId}/notification-audit/{_id}";
+        public object Data { get; set; }
+    }
+}
