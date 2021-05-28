96e973ea460175eb0265d92c6b7fbe57fb6e6d44
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-05-28 12:13:54 +0100

    Unit tests for new controller.

---
+++ src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/Account/WhenGettingAccount.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture.NUnit3;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Api.Controllers;
+using SFA.DAS.LevyTransferMatching.Api.Models;
+using SFA.DAS.LevyTransferMatching.Application.Queries.GetAccount;
+using SFA.DAS.Testing.AutoFixture;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.Account
+{
+    public class WhenGettingAccount
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_Account_From_Mediator(
+            string encodedAccountId,
+            GetAccountResult getAccountResult,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] AccountController accountController)
+        {
+            mockMediator
+                .Setup(x => x.Send(
+                    It.Is<GetAccountQuery>(y => y.EncodedAccountId.Equals(encodedAccountId)),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(getAccountResult);
+
+            var controllerResult = await accountController.GetAccount(encodedAccountId) as ObjectResult;
+
+            Assert.IsNotNull(controllerResult);
+            Assert.AreEqual(controllerResult.StatusCode, (int)HttpStatusCode.OK);
+
+            var model = controllerResult.Value as AccountDto;
+            Assert.IsNotNull(model);
+            Assert.AreEqual(getAccountResult.Account.RemainingTransferAllowance, model.RemainingTransferAllowance);
+        }
+    }
+}
