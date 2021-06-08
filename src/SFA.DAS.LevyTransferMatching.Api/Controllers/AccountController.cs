3d40d61d52ea35e5732456b1a8745b4e2d189f32
chrisfoster76 <chrisfoster186@googlemail.com>
2021-06-08 08:04:19 +0100

    Adds logging to getaccount call

--- src/SFA.DAS.LevyTransferMatching.Api/Controllers/AccountController.cs
+++ src/SFA.DAS.LevyTransferMatching.Api/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
-﻿using MediatR;
+﻿using System;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.LevyTransferMatching.Api.Models;
 using SFA.DAS.LevyTransferMatching.Application.Queries.GetAccount;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace SFA.DAS.LevyTransferMatching.Api.Controllers
 {
@@ -11,24 +13,34 @@ namespace SFA.DAS.LevyTransferMatching.Api.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(IMediator mediator)
+        public AccountController(IMediator mediator, ILogger<AccountController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet]
         [Route("{encodedAccountId}")]
         public async Task<IActionResult> GetAccount(string encodedAccountId)
         {
-            var queryResult = await _mediator.Send(new GetAccountQuery()
+            try
             {
-                EncodedAccountId = encodedAccountId,
-            });
+                var queryResult = await _mediator.Send(new GetAccountQuery()
+                {
+                    EncodedAccountId = encodedAccountId,
+                });
 
-            var response = (AccountDto)queryResult.Account;
+                var response = (AccountDto)queryResult.Account;
 
-            return Ok(response);
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error getting account");
+                return BadRequest();
+            }
         }
     }
 }
