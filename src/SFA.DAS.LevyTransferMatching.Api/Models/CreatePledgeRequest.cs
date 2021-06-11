42c79219da23994ad8bd93a8be1b415cac0dd08c
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-06-11 09:49:38 +0100

    Additional validation - Requiring all properties on the CreatePledgeRequest

--- src/SFA.DAS.LevyTransferMatching.Api/Models/CreatePledgeRequest.cs
+++ src/SFA.DAS.LevyTransferMatching.Api/Models/CreatePledgeRequest.cs
@@ -1,13 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace SFA.DAS.LevyTransferMatching.Api.Models
 {
     public class CreatePledgeRequest
     {
+        [JsonProperty(Required = Required.Always)]
         public int Amount { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
         public bool IsNamePublic { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
         public IEnumerable<string> Sectors { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
         public IEnumerable<string> JobRoles { get; set; }
+
+        [JsonProperty(Required = Required.Always)]
         public IEnumerable<string> Levels { get; set; }
     }
 }
