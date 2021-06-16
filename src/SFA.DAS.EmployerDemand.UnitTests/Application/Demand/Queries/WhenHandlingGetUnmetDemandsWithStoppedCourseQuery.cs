a8fe84b485ce3455bd31236183851288525f1f23
Scott Wakefield <scott@expertcc.co.uk>
2021-06-16 16:27:47 +0100

    AEDB-52 add handler to get unmet demands for closed courses.

---
+++ src/SFA.DAS.EmployerDemand.UnitTests/Application/Demand/Queries/WhenHandlingGetUnmetDemandsWithStoppedCourseQuery.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerDemand.Application.Demand.Queries.GetUnmetDemandsWithStoppedCourse;
+using SFA.DAS.EmployerDemand.InnerApi.Requests;
+using SFA.DAS.EmployerDemand.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerDemand.UnitTests.Application.Demand.Queries
+{
+    public class WhenHandlingGetUnmetDemandsWithStoppedCourseQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_The_CourseDemand_Ids_From_The_Api(
+            GetStandardsListResponse closedStandardsApiResponse,
+            List<GetUnmetCourseDemandsResponse> unmetDemandResponses,
+            GetUnmetDemandsWithStoppedCourseQuery query,
+            [Frozen]Mock<ICoursesApiClient<CoursesApiConfiguration>> mockCoursesApiClient,
+            [Frozen]Mock<IEmployerDemandApiClient<EmployerDemandApiConfiguration>> mockEmployerDemandApiClient,
+            GetUnmetDemandsWithStoppedCourseQueryHandler handler)
+        {
+            //Arrange
+            mockCoursesApiClient
+                .Setup(client => client.Get<GetStandardsListResponse>(
+                    It.IsAny<GetStandardsClosedToNewStartsRequest>()))
+                .ReturnsAsync(closedStandardsApiResponse);
+            var standards = closedStandardsApiResponse.Standards.ToList();
+            for (var x = 0; x < standards.Count; x++)
+            {
+                var i = x;//needed to extend lifetime of value of x
+                var expectedApiRequest = new GetUnmetEmployerDemandsRequest(0, standards[i].LarsCode);
+                mockEmployerDemandApiClient
+                    .Setup(client => client.Get<GetUnmetCourseDemandsResponse>(
+                        It.Is<GetUnmetEmployerDemandsRequest>(c => c.GetUrl == expectedApiRequest.GetUrl)))
+                    .ReturnsAsync(unmetDemandResponses[i]);
+            }
+            var expectedDemandIds = new List<Guid>();
+            foreach (var demandsResponse in unmetDemandResponses)
+            {
+                expectedDemandIds.AddRange(demandsResponse.EmployerDemandIds);
+            }
+
+            //Act
+            var actual = await handler.Handle(query, CancellationToken.None);
+            
+            //Assert
+            actual.EmployerDemandIds.Should().BeEquivalentTo(expectedDemandIds);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerDemand/Application/Demand/Queries/GetUnmetDemandsWithStoppedCourse/GetUnmetDemandsWithStoppedCourseQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerDemand.InnerApi.Requests;
+using SFA.DAS.EmployerDemand.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Application.Demand.Queries.GetUnmetDemandsWithStoppedCourse
+{
+    public class GetUnmetDemandsWithStoppedCourseQueryHandler : IRequestHandler<GetUnmetDemandsWithStoppedCourseQuery, GetUnmetDemandsWithStoppedCourseResult> 
+    {
+        private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
+        private readonly IEmployerDemandApiClient<EmployerDemandApiConfiguration> _employerDemandApiClient;
+
+        public GetUnmetDemandsWithStoppedCourseQueryHandler(
+            ICoursesApiClient<CoursesApiConfiguration> coursesApiClient,
+            IEmployerDemandApiClient<EmployerDemandApiConfiguration> employerDemandApiClient)
+        {
+            _coursesApiClient = coursesApiClient;
+            _employerDemandApiClient = employerDemandApiClient;
+        }
+
+        public async Task<GetUnmetDemandsWithStoppedCourseResult> Handle(GetUnmetDemandsWithStoppedCourseQuery request, CancellationToken cancellationToken)
+        {
+            var stoppedCourses = await _coursesApiClient.Get<GetStandardsListResponse>(new GetStandardsClosedToNewStartsRequest());
+
+            var unmetDemandIdsForAllCourses = new List<Guid>();
+            foreach (var standard in stoppedCourses.Standards)
+            {
+                var unmetDemandsResponse = await _employerDemandApiClient.Get<GetUnmetCourseDemandsResponse>(
+                        new GetUnmetEmployerDemandsRequest(0, standard.LarsCode));
+                unmetDemandIdsForAllCourses.AddRange(unmetDemandsResponse.EmployerDemandIds);
+            }
+
+            return new GetUnmetDemandsWithStoppedCourseResult
+            {
+                EmployerDemandIds = unmetDemandIdsForAllCourses
+            };
+        }
+    }
+}
