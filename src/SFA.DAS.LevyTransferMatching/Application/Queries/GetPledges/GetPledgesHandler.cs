43942249ce49f8a4d3740b76ec3c79aef110d0ef
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-06-17 11:49:13 +0100

    Renamed GetAllPledgesResult -> GetPledgesResult to be in line with other naming. Unit test coverage

--- src/SFA.DAS.LevyTransferMatching/Application/Queries/GetPledges/GetPledgesHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Application/Queries/GetPledges/GetPledgesHandler.cs
@@ -7,18 +7,18 @@ using System.Threading.Tasks;
 
 namespace SFA.DAS.LevyTransferMatching.Application.Queries.GetAllPledges
 {
-    public class GetAllPledgesHandler : IRequestHandler<GetAllPledgesQuery, GetAllPledgesResult>
+    public class GetPledgesHandler : IRequestHandler<GetPledgesQuery, GetPledgesResult>
     {
         private readonly ILevyTransferMatchingService _levyTransferMatchingService;
         private readonly IEncodingService _encodingService;
 
-        public GetAllPledgesHandler(IEncodingService encodingService, ILevyTransferMatchingService levyTransferMatchingService)
+        public GetPledgesHandler(IEncodingService encodingService, ILevyTransferMatchingService levyTransferMatchingService)
         {
             _levyTransferMatchingService = levyTransferMatchingService;
             _encodingService = encodingService;
         }
 
-        public async Task<GetAllPledgesResult> Handle(GetAllPledgesQuery request, CancellationToken cancellationToken)
+        public async Task<GetPledgesResult> Handle(GetPledgesQuery request, CancellationToken cancellationToken)
         {
             var result = await _levyTransferMatchingService.GetAllPledges();
 
@@ -28,7 +28,7 @@ namespace SFA.DAS.LevyTransferMatching.Application.Queries.GetAllPledges
                 pledge.EncodedAccountId = _encodingService.Encode(pledge.AccountId, EncodingType.AccountId);
             }
 
-            return new GetAllPledgesResult(result);
+            return new GetPledgesResult(result);
         }
     }
 }
