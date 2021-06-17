2d10b946c71841693d253e1dfea24feadd98aba0
Scott Wakefield <scott@expertcc.co.uk>
2021-06-17 12:08:24 +0100

    AEDB-52 added handler.

---
+++ src/SFA.DAS.EmployerDemand/Application/Demand/Commands/CourseStopped/CourseStoppedCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.EmployerDemand.Domain.Models;
+using SFA.DAS.EmployerDemand.InnerApi.Requests;
+using SFA.DAS.EmployerDemand.InnerApi.Responses;
+using SFA.DAS.Notifications.Messages.Commands;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerDemand.Application.Demand.Commands.CourseStopped
+{
+    public class CourseStoppedCommandHandler : IRequestHandler<CourseStoppedCommand, Unit> 
+    {
+        private readonly IEmployerDemandApiClient<EmployerDemandApiConfiguration> _employerDemandApiClient;
+        private readonly INotificationService _notificationService;
+
+        public CourseStoppedCommandHandler(
+            IEmployerDemandApiClient<EmployerDemandApiConfiguration> employerDemandApiClient, 
+            INotificationService notificationService)
+        {
+            _employerDemandApiClient = employerDemandApiClient;
+            _notificationService = notificationService;
+        }
+
+        public async Task<Unit> Handle(CourseStoppedCommand request, CancellationToken cancellationToken)
+        {
+            var courseDemand =
+                await _employerDemandApiClient.Get<GetEmployerDemandResponse>(
+                    new GetEmployerDemandRequest(request.EmployerDemandId));
+
+            var emailModel = new StopSharingEmployerDemandCourseClosedEmail(
+                courseDemand.ContactEmailAddress,
+                courseDemand.OrganisationName, 
+                courseDemand.Course.Title, 
+                courseDemand.Course.Level,
+                courseDemand.Location.Name, 
+                courseDemand.NumberOfApprentices);
+            
+            await _notificationService.Send(new SendEmailCommand(emailModel.TemplateId,emailModel.RecipientAddress, emailModel.Tokens));
+            
+            var auditTask = _employerDemandApiClient.PostWithResponseCode<object>(
+                new PostEmployerDemandNotificationAuditRequest(request.Id, request.EmployerDemandId));
+            var patchTask = _employerDemandApiClient.PatchWithResponseCode(new PatchCourseDemandRequest(
+                request.EmployerDemandId, new PatchCourseDemandData
+                {
+                    Stopped = true
+                }));
+
+            await Task.WhenAll(auditTask, patchTask);
+            
+            return Unit.Value;
+        }
+    }
+}
