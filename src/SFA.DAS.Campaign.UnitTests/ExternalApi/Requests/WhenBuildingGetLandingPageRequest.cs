7af80f5c136cf475d5d9358083734d4eee7248c9
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-06-21 10:05:50 +0100

    AM-447 - Add get landing page request

---
+++ src/SFA.DAS.Campaign.UnitTests/ExternalApi/Requests/WhenBuildingGetLandingPageRequest.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Campaign.ExternalApi.Requests;
+
+namespace SFA.DAS.Campaign.UnitTests.ExternalApi.Requests
+{
+    public class WhenBuildingGetLandingPageRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(string hubType, string slug)
+        {
+            //Act
+            var actual = new GetLandingPageRequest(hubType, slug);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"entries?content_type=landingpage&fields.hubType={hubType}&fields.slug={slug}");
+        }
+
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built_For_An_Entry(string entryId)
+        {
+            //Act
+            var actual = new GetLandingPageRequest(entryId);
+            
+            //Assert
+            actual.GetUrl.Should().Be($"entries?content_type=landingpage&sys.id={entryId}");
+        }
+    }
+}
