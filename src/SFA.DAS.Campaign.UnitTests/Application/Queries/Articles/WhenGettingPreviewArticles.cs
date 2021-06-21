529aad4aa46efde7e9e6d8563aeef751e334f427
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-06-21 20:20:44 +0100

    AM-348 - Add handler for getting preview content

--- src/SFA.DAS.Campaign.UnitTests/Application/Queries/Articles/WhenGettingPreviewArticles.cs
+++ src/SFA.DAS.Campaign.UnitTests/Application/Queries/Articles/WhenGettingPreviewArticles.cs
@@ -1,68 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.Campaign.Api.Models;
-using SFA.DAS.Campaign.Application.Queries.Articles;
+using SFA.DAS.Campaign.Application.Queries.PreviewArticles;
+using SFA.DAS.Campaign.Configuration;
 using SFA.DAS.Campaign.Extensions;
 using SFA.DAS.Campaign.ExternalApi.Requests;
 using SFA.DAS.Campaign.ExternalApi.Responses;
+using SFA.DAS.Campaign.Interfaces;
 using SFA.DAS.Campaign.Models;
-using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.Campaign.UnitTests.Application.Queries.Articles
 {
-    public class WhenGettingArticles
+    public class WhenGettingPreviewArticles
     {
         [Test, RecursiveMoqAutoData]
         public async Task Then_The_Api_Is_Called_With_The_Valid_Request_Parameters_And_The_Article_Is_Returned(
-            GetArticleByHubAndSlugQuery query,
+            GetPreviewArticleByHubAndSlugQuery query,
             CmsContent apiResponse,
             CmsPageModel response,
-            [Frozen] Mock<IReliableCacheStorageService> service,
-            GetArticleByHubAndSlugQueryHandler handler)
+            [Frozen] Mock<IContentfulPreviewApiClient<ContentfulPreviewApiConfiguration>> apiClient,
+            GetPreviewArticleByHubAndSlugQueryHandler handler)
         {
-            service.Setup(o =>
-                    o.GetData<CmsContent>(
+            apiClient.Setup(o =>
+                    o.Get<CmsContent>(
                         It.Is<GetArticleEntriesRequest>(c =>
-                            c.GetUrl.Contains($"fields.hubType={query.Hub.ToTitleCase()}&fields.slug={query.Slug}")),
-                        $"{query.Hub.ToTitleCase()}_{query.Slug}_article"))
+                            c.GetUrl.Contains($"fields.hubType={query.Hub.ToTitleCase()}&fields.slug={query.Slug}"))))
                 .ReturnsAsync(apiResponse);
 
             var actual = await handler.Handle(query, CancellationToken.None);
 
             actual.PageModel.Should().BeEquivalentTo(response.Build(apiResponse));
-            service.Verify(x=>x.GetData<CmsContent>(It.IsAny<GetLandingPageRequest>(), It.IsAny<string>()), Times.Never);
         }
 
         [Test, RecursiveMoqAutoData]
         public async Task Then_If_The_Article_Is_Not_Found_The_Landing_Page_Is_Requested(
-            GetArticleByHubAndSlugQuery query,
+            GetPreviewArticleByHubAndSlugQuery query,
             CmsContent apiResponse,
             CmsPageModel response,
-            [Frozen] Mock<IReliableCacheStorageService> service,
-            GetArticleByHubAndSlugQueryHandler handler)
+            [Frozen] Mock<IContentfulPreviewApiClient<ContentfulPreviewApiConfiguration>> apiClient,
+            GetPreviewArticleByHubAndSlugQueryHandler handler)
         {
-            service.Setup(o =>
-                    o.GetData<CmsContent>(
+            apiClient.Setup(o =>
+                    o.Get<CmsContent>(
                         It.Is<GetArticleEntriesRequest>(c =>
-                            c.GetUrl.Contains($"fields.hubType={query.Hub.ToTitleCase()}&fields.slug={query.Slug}")),
-                        $"{query.Hub.ToTitleCase()}_{query.Slug}_article"))
+                            c.GetUrl.Contains($"fields.hubType={query.Hub.ToTitleCase()}&fields.slug={query.Slug}"))))
                 .ReturnsAsync(new CmsContent
                 {
                     Total = 0
                 });
-            service.Setup(o =>
-                    o.GetData<CmsContent>(
+            apiClient.Setup(o =>
+                    o.Get<CmsContent>(
                         It.Is<GetLandingPageRequest>(c =>
-                            c.GetUrl.Contains($"fields.hubType={query.Hub.ToTitleCase()}&fields.slug={query.Slug}")),
-                        $"{query.Hub.ToTitleCase()}_{query.Slug}_landing"))
+                            c.GetUrl.Contains($"fields.hubType={query.Hub.ToTitleCase()}&fields.slug={query.Slug}"))))
                 .ReturnsAsync(apiResponse);
 
             var actual = await handler.Handle(query, CancellationToken.None);
