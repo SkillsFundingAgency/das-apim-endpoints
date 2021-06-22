36abf5d600a1f96f62c7e6058d21c0644998f8eb
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-06-22 09:31:03 +0100

    Controller tests and handler tests updated

---
+++ src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/PledgeTests/WhenCallingGetPledge.cs
@@ -0,0 +1,64 @@
+﻿using AutoFixture.NUnit3;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Api.Controllers;
+using SFA.DAS.LevyTransferMatching.Api.Models;
+using SFA.DAS.LevyTransferMatching.Application.Queries.GetAllPledges;
+using SFA.DAS.LevyTransferMatching.Models;
+using SFA.DAS.Testing.AutoFixture;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.PledgeTests
+{
+    public class WhenCallingGetPledge
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_Pledge_From_Mediator(
+            Pledge pledge,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] PledgeController pledgeController)
+        {
+            var encodedPledgeId = pledge.EncodedPledgeId;
+
+            mockMediator
+                .Setup(x => x.Send(
+                    It.Is<GetPledgesQuery>(x => x.EncodedId == encodedPledgeId),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GetPledgesResult(new Pledge[] { pledge }));
+
+            var controllerResult = await pledgeController.GetPledge(encodedPledgeId);
+            var okObjectResult = controllerResult as OkObjectResult;
+            var pledgeDto = okObjectResult.Value as PledgeDto;
+
+            Assert.IsNotNull(controllerResult);
+            Assert.IsNotNull(okObjectResult);
+            Assert.IsNotNull(pledgeDto);
+            Assert.AreEqual(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+            Assert.AreEqual(encodedPledgeId, pledgeDto.EncodedPledgeId);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_Doesnt_Get_Pledge_From_Mediator(
+            string encodedPledgeId,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] PledgeController pledgeController)
+        {
+            mockMediator
+                .Setup(x => x.Send(
+                    It.Is<GetPledgesQuery>(x => x.EncodedId == encodedPledgeId),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new GetPledgesResult(new Pledge[] { }));
+
+            var controllerResult = await pledgeController.GetPledge(encodedPledgeId);
+            var notFoundResult = controllerResult as NotFoundResult;
+
+            Assert.IsNotNull(controllerResult);
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual(notFoundResult.StatusCode, (int)HttpStatusCode.NotFound);
+        }
+    }
+}
--- src/SFA.DAS.LevyTransferMatching.UnitTests/Application/Queries/GetPledges/WhenCallingHandle.cs
+++ src/SFA.DAS.LevyTransferMatching.UnitTests/Application/Queries/GetPledges/WhenCallingHandle.cs
@@ -4,8 +4,10 @@ using NUnit.Framework;
 using SFA.DAS.Encoding;
 using SFA.DAS.LevyTransferMatching.Application.Queries.GetAllPledges;
 using SFA.DAS.LevyTransferMatching.Interfaces;
+using SFA.DAS.LevyTransferMatching.Models;
 using SFA.DAS.Testing.AutoFixture;
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,12 +16,15 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.Application.Queries.GetPledges
     public class WhenCallingHandle
     {
         [Test, MoqAutoData]
-        public async Task Then_Pledge_AccountId_And_PledgeId_Are_Encoded_And_Pledges_Returned(
-            GetPledgesQuery getPledgesQuery,
+        public async Task Then_Pledge_AccountId_And_PledgeId_Are_Encoded_And_All_Pledges_Returned(
             [Frozen] Mock<IEncodingService> mockEncodingService,
-            [Frozen] Mock<ILevyTransferMatchingService> mockLevyTransferMatchingService,
             GetPledgesHandler getPledgesHandler)
         {
+            GetPledgesQuery getPledgesQuery = new GetPledgesQuery()
+            {
+                // Get all pledges
+            };
+
             Func<long, EncodingType, string> fakeEncoder =
                 (value, encodingType) =>
                 {
@@ -37,6 +42,68 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.Application.Queries.GetPledges
                 Assert.AreEqual(pledge.EncodedAccountId, $"{EncodingType.AccountId}_{pledge.AccountId}");
                 Assert.AreEqual(pledge.EncodedPledgeId, $"{EncodingType.PledgeId}_{pledge.Id}");
             }
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_EncodedId_Requested_Incorrect_Format_Then_No_Pledges_Returned(
+            string encodedId,
+            [Frozen] Mock<IEncodingService> mockEncodingService,
+            GetPledgesHandler getPledgesHandler)
+        {
+            GetPledgesQuery getPledgesQuery = new GetPledgesQuery()
+            {
+                EncodedId = encodedId,
+            };
+
+            mockEncodingService
+                .Setup(x => x.Decode(It.Is<string>(y => y == encodedId), It.Is<EncodingType>(y => y == EncodingType.PledgeId)))
+                .Throws<IndexOutOfRangeException>();
+
+            var results = await getPledgesHandler.Handle(getPledgesQuery, CancellationToken.None);
+
+            Assert.AreEqual(results.Count(), 0);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_EncodedId_Requested_Then_Pledge_AccountId_And_PledgeId_Are_Encoded_And_One_Pledge_Returned(
+            string encodedId,
+            long id,
+            Pledge expectedPledge,
+            [Frozen] Mock<IEncodingService> mockEncodingService,
+            [Frozen] Mock<ILevyTransferMatchingService> mockLevyTransferMatchingService,
+            GetPledgesHandler getPledgesHandler)
+        {
+            GetPledgesQuery getPledgesQuery = new GetPledgesQuery()
+            {
+                EncodedId = encodedId,
+            };
+
+            Func<long, EncodingType, string> fakeEncoder =
+                (value, encodingType) =>
+                {
+                    return $"{encodingType}_{value}";
+                };
+
+            mockEncodingService
+                .Setup(x => x.Encode(It.IsAny<long>(), It.Is<EncodingType>(y => y == EncodingType.AccountId || y == EncodingType.PledgeId)))
+                .Returns(fakeEncoder);
+
+            mockEncodingService
+                .Setup(x => x.Decode(It.Is<string>(y => y == encodedId), It.Is<EncodingType>(y => y == EncodingType.PledgeId)))
+                .Returns(id);
+
+            mockLevyTransferMatchingService
+                .Setup(x => x.GetPledge(It.Is<int>(y => y == id)))
+                .ReturnsAsync(expectedPledge);
+
+            var results = await getPledgesHandler.Handle(getPledgesQuery, CancellationToken.None);
+
+            var actualPledge = results.SingleOrDefault();
+
+            Assert.IsNotNull(actualPledge);
+            Assert.AreEqual(expectedPledge, actualPledge);
+            Assert.AreEqual(actualPledge.EncodedAccountId, $"{EncodingType.AccountId}_{actualPledge.AccountId}");
+            Assert.AreEqual(actualPledge.EncodedPledgeId, $"{EncodingType.PledgeId}_{actualPledge.Id}");
         }
     }
 }
