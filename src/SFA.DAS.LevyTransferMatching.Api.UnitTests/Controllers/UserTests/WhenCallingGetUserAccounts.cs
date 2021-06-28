2d6bb892d9b16410416c6ad2cce41a01603030d7
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-06-28 12:26:01 +0100

    Unit tests for new UserAccounts endpoint.

---
+++ src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/UserTests/WhenCallingGetUserAccounts.cs
@@ -0,0 +1,42 @@
+﻿using AutoFixture.NUnit3;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Api.Controllers;
+using SFA.DAS.LevyTransferMatching.Api.Models;
+using SFA.DAS.LevyTransferMatching.Application.Queries.GetUserAccounts;
+using SFA.DAS.Testing.AutoFixture;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.UserTests
+{
+    [TestFixture]
+    public class WhenCallingGetUserAccounts
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Returns_Accounts(
+            string userId,
+            GetUserAccountsResult getUserAccountsResult,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] UserController userController)
+        {
+            mockMediator
+                .Setup(x => x.Send(It.IsAny<GetUserAccountsQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(getUserAccountsResult);
+
+            var controllerResult = await userController.GetUserAccounts(userId) as OkObjectResult;
+
+            Assert.IsNotNull(controllerResult);
+            Assert.AreEqual(controllerResult.StatusCode, (int)HttpStatusCode.OK);
+
+            var model = controllerResult.Value as IEnumerable<AccountDto>;
+
+            Assert.AreEqual(getUserAccountsResult.Accounts.Count(), model.Count());
+        }
+    }
+}
---
+++ src/SFA.DAS.LevyTransferMatching.UnitTests/Application/Queries/GetUserAccounts/GetUserAccountsHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Application.Queries.GetUserAccounts;
+using SFA.DAS.LevyTransferMatching.Application.Services;
+using SFA.DAS.LevyTransferMatching.Interfaces;
+using SFA.DAS.LevyTransferMatching.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.UnitTests.Application.Queries.GetUserAccounts
+{
+    [TestFixture]
+    public class GetUserAccountsHandlerTests
+    {
+        private Fixture _fixture;
+        private Mock<IUserService> _userService;
+        private GetUserAccountsHandler _getUserAccountsHandler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _fixture = new Fixture();
+            _userService = new Mock<IUserService>();
+            _getUserAccountsHandler = new GetUserAccountsHandler(_userService.Object);
+        }
+
+        [Test]
+        public async Task Handle_Returns_Accounts()
+        {
+            string userId = _fixture.Create<string>();
+            IEnumerable<Account> accounts = _fixture.CreateMany<Account>();
+
+            _userService
+                .Setup(x => x.GetUserAccounts(It.Is<string>(y => y == userId)))
+                .ReturnsAsync(accounts);
+
+            GetUserAccountsQuery getUserAccountsQuery = new GetUserAccountsQuery()
+            {
+                UserId = userId,
+            };
+
+            var result = await _getUserAccountsHandler.Handle(getUserAccountsQuery, CancellationToken.None);
+
+            CollectionAssert.AreEqual(accounts, result.Accounts);
+        }
+    }
+}
