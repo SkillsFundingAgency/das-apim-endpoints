0000492d86b7b9fa154091ea292f6805b6cf0fca
Conor Till <conortill@gmail.com>
2021-07-13 14:35:29 +0100

    Adding GetSectorQueryHandler

---
+++ src/SFA.DAS.LevyTransferMatching/Application/Queries/GetSector/GetSectorQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using SFA.DAS.LevyTransferMatching.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Application.Queries.GetSector
+{
+    public class GetSectorQueryHandler : IRequestHandler<GetSectorQuery, GetSectorQueryResult>
+    {
+        private readonly IReferenceDataService _referenceDataService;
+
+        public GetSectorQueryHandler(IReferenceDataService referenceDataService)
+        {
+            _referenceDataService = referenceDataService;
+        }
+
+        public async Task<GetSectorQueryResult> Handle(GetSectorQuery request, CancellationToken cancellationToken)
+        {
+            var sectors = await _referenceDataService.GetSectors();
+
+            return new GetSectorQueryResult
+            {
+                Sectors = sectors
+            };
+        }
+    }
+}
---
+++ src/SFA.DAS.LevyTransferMatching/Application/Queries/GetSector/GetSectorQueryResult.cs
@@ -0,0 +1,12 @@
+﻿using SFA.DAS.LevyTransferMatching.Models.ReferenceData;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.LevyTransferMatching.Application.Queries.GetSector
+{
+    public class GetSectorQueryResult
+    {
+        public IEnumerable<ReferenceDataItem> Sectors { get; set; }
+    }
+}
