e75c62eabca9178b3234c24cd01680e8eb453b63
chazzysb <cballagan@yahoo.com>
2021-07-17 12:40:03 +0100

    CS-902-ContentSweep2

--- src/SFA.DAS.ApprenticeCommitments/Apis/InnerApi/GetApprenticeshipResponse.cs
+++ src/SFA.DAS.ApprenticeCommitments/Apis/InnerApi/GetApprenticeshipResponse.cs
@@ -11,9 +11,9 @@ namespace SFA.DAS.ApprenticeCommitments.Apis.InnerApi
         public string CourseName { get; set; }
         public int CourseLevel { get; set; }
         public string CourseOption { get; set; }
+        public int CourseDuration { get; set; }
         public DateTime PlannedStartDate { get; set; }
         public DateTime PlannedEndDate { get; set; }
-        public int DurationInMonths { get; set; }
         public bool? TrainingProviderCorrect { get; set; }
         public bool? EmployerCorrect { get; set; }
         public bool? RolesAndResponsibilitiesCorrect { get; set; }
