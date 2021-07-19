a57d46e6a1145800b40f173b73e397d22fa31ef9
Conor Till <conortill@gmail.com>
2021-07-19 08:39:33 +0100

    Tests for opportunity get sector

---
+++ src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/OpportunityTests/WhenCallingGetSector.cs
@@ -0,0 +1,56 @@
+﻿using AutoFixture.NUnit3;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Api.Controllers;
+using SFA.DAS.LevyTransferMatching.Api.Models;
+using SFA.DAS.LevyTransferMatching.Application.Queries.GetOpportunity;
+using SFA.DAS.LevyTransferMatching.Application.Queries.Opportunity.GetSector;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.OpportunityTests
+{
+    public class WhenCallingGetSector
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Returns_Ok_And_Response(
+            int pledgeId,
+            string postcode,
+            GetSectorQueryResult getSectorQueryResult,
+            GetOpportunityResult getOpportunityResult,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] OpportunityController opportunityController)
+        {
+            mockMediator
+                .Setup(x => x.Send(
+                    It.Is<GetSectorQuery>(y => y.Postcode == postcode),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(getSectorQueryResult);
+
+            mockMediator
+                .Setup(x => x.Send(
+                    It.Is<GetOpportunityQuery>(y => y.OpportunityId == pledgeId),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(getOpportunityResult);
+
+            var controllerResult = await opportunityController.Sector(pledgeId, postcode);
+            var okObjectResult = controllerResult as OkObjectResult;
+            var response = okObjectResult.Value as GetSectorResponse;
+
+            Assert.IsNotNull(controllerResult);
+            Assert.IsNotNull(okObjectResult);
+            Assert.IsNotNull(response);
+            Assert.AreEqual(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
+            Assert.AreEqual(getSectorQueryResult.Sectors, response.Sectors);
+            Assert.AreEqual(getSectorQueryResult.Location, response.Location);
+            Assert.AreEqual(getOpportunityResult.Id, response.Opportunity.Id);
+        }
+    }
+}
---
+++ src/SFA.DAS.LevyTransferMatching.UnitTests/Application/Queries/Opportunity/GetSector/GetSectorQueryHandlerTests.cs
@@ -0,0 +1,55 @@
+﻿using AutoFixture;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Application.Queries.Opportunity.GetSector;
+using SFA.DAS.LevyTransferMatching.Interfaces;
+using SFA.DAS.LevyTransferMatching.Models.ReferenceData;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.LevyTransferMatching.UnitTests.Application.Queries.Opportunity.GetSector
+{
+    [TestFixture]
+    public class GetSectorQueryHandlerTests
+    {
+        private GetSectorQueryHandler _handler;
+        private Mock<IReferenceDataService> _referenceDataService;
+        private Mock<ILocationLookupService> _locationLookupService;
+        private List<ReferenceDataItem> _sectors;
+        private GetSectorQuery _query;
+        private readonly Fixture _autoFixture = new Fixture();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sectors = _autoFixture.Create<List<ReferenceDataItem>>();
+            _query = _autoFixture.Create<GetSectorQuery>();
+
+            _referenceDataService = new Mock<IReferenceDataService>();
+            _referenceDataService.Setup(x => x.GetSectors()).ReturnsAsync(_sectors);
+
+            _locationLookupService = new Mock<ILocationLookupService>();
+            _locationLookupService.Setup(x => x.GetLocationInformation(_query.Postcode, 0, 0, true)).Returns(Task.FromResult(new LocationItem(_query.Postcode, new double[2])));
+
+            _handler = new GetSectorQueryHandler(_referenceDataService.Object, _locationLookupService.Object);
+        }
+
+        [Test]
+        public async Task Handle_Returns_Sectors()
+        {
+            var result = await _handler.Handle(_query, new System.Threading.CancellationToken());
+            Assert.AreEqual(_sectors, result.Sectors);
+        }
+
+        [Test]
+        public async Task Handle_Returns_Location()
+        {
+            var result = await _handler.Handle(_query, new System.Threading.CancellationToken());
+            Assert.AreEqual(_query.Postcode, result.Location);
+        }
+    }
+}
