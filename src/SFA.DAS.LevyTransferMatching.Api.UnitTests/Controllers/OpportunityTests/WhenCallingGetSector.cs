35066e8797ad7ba746f20fc56240012cd94aac6b
Conor Till <conortill@gmail.com>
2021-07-21 16:17:14 +0100

    Fixing WhenCallingGetSector test

--- src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/OpportunityTests/WhenCallingGetSector.cs
+++ src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/OpportunityTests/WhenCallingGetSector.cs
@@ -24,7 +24,6 @@ namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.OpportunityTest
             int pledgeId,
             string postcode,
             GetSectorQueryResult getSectorQueryResult,
-            GetOpportunityResult getOpportunityResult,
             [Frozen] Mock<IMediator> mockMediator,
             [Greedy] OpportunityController opportunityController)
         {
@@ -33,12 +32,6 @@ namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.OpportunityTest
                     It.Is<GetSectorQuery>(y => y.Postcode == postcode),
                     It.IsAny<CancellationToken>()))
                 .ReturnsAsync(getSectorQueryResult);
-
-            mockMediator
-                .Setup(x => x.Send(
-                    It.Is<GetOpportunityQuery>(y => y.OpportunityId == pledgeId),
-                    It.IsAny<CancellationToken>()))
-                .ReturnsAsync(getOpportunityResult);
 
             var controllerResult = await opportunityController.Sector(pledgeId, postcode);
             var okObjectResult = controllerResult as OkObjectResult;
@@ -50,7 +43,7 @@ namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.OpportunityTest
             Assert.AreEqual(okObjectResult.StatusCode, (int)HttpStatusCode.OK);
             Assert.AreEqual(getSectorQueryResult.Sectors, response.Sectors);
             Assert.AreEqual(getSectorQueryResult.Location, response.Location);
-            Assert.AreEqual(getOpportunityResult.Id, response.Opportunity.Id);
+            Assert.AreEqual(getSectorQueryResult.Opportunity.Id, response.Opportunity.Id);
         }
     }
 }
