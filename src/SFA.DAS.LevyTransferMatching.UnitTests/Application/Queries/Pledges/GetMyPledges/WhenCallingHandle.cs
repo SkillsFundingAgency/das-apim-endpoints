8dc1b58bb6a44587518e538a55f1f927e42fe645
Conor Till <conortill@gmail.com>
2021-07-28 15:30:45 +0100

    Fixing test

--- src/SFA.DAS.LevyTransferMatching.UnitTests/Application/Queries/Pledges/GetMyPledges/WhenCallingHandle.cs
+++ src/SFA.DAS.LevyTransferMatching.UnitTests/Application/Queries/Pledges/GetMyPledges/WhenCallingHandle.cs
@@ -2,6 +2,8 @@
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.LevyTransferMatching.Application.Queries.Pledges.GetMyPledges;
+using SFA.DAS.LevyTransferMatching.InnerApi.LevyTransferMatching.Responses;
+using SFA.DAS.LevyTransferMatching.InnerApi.Requests.Pledges;
 using SFA.DAS.LevyTransferMatching.Interfaces;
 using SFA.DAS.LevyTransferMatching.Models;
 using System;
@@ -19,7 +21,7 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.Application.Queries.Pledges.Get
         private GetMyPledgesQueryHandler _handler;
         private Mock<ILevyTransferMatchingService> _levyTransferMatchingService;
         private GetMyPledgesQuery _query;
-        private IEnumerable<Pledge> _pledges;
+        private GetPledgesResponse _pledgeResponse;
         private Fixture _fixture;
 
         [SetUp]
@@ -27,13 +29,13 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.Application.Queries.Pledges.Get
         {
             _fixture = new Fixture();
 
-            _pledges = _fixture.Create<IEnumerable<Pledge>>();
+            _pledgeResponse = _fixture.Create<GetPledgesResponse>();
 
             var accountId = _fixture.Create<int>();
             _query = new GetMyPledgesQuery(accountId);
 
             _levyTransferMatchingService = new Mock<ILevyTransferMatchingService>();
-            _levyTransferMatchingService.Setup(x => x.GetPledges()).ReturnsAsync(_pledges);
+            _levyTransferMatchingService.Setup(x => x.GetPledges(It.IsAny<GetPledgesRequest>())).ReturnsAsync(_pledgeResponse);
 
             _handler = new GetMyPledgesQueryHandler(_levyTransferMatchingService.Object);
         }
@@ -48,6 +50,8 @@ namespace SFA.DAS.LevyTransferMatching.UnitTests.Application.Queries.Pledges.Get
             CollectionAssert.IsNotEmpty(result.Pledges);
             Assert.That(!result.Pledges.Any(x => x.Id == 0));
             Assert.That(!result.Pledges.Any(x => x.Amount == 0));
+            Assert.That(!result.Pledges.Any(x => x.RemainingAmount == 0));
+            Assert.That(!result.Pledges.Any(x => x.ApplicationCount == 0));
         }
     }
 }
