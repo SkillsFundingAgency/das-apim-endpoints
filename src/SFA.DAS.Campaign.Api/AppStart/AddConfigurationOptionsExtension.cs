0b30a8331d2e8c239dcbff68fec0191814094e74
Steven Scaife <steven_scaife@hotmail.com>
2021-07-30 15:12:33 +0100

    removed unused using

--- src/SFA.DAS.Campaign.Api/AppStart/AddConfigurationOptionsExtension.cs
+++ src/SFA.DAS.Campaign.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -1,5 +1,4 @@
-﻿using Contentful.Core.Configuration;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Api.Common.Configuration;
