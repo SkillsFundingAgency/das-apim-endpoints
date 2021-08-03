c0ba1e57aa8f1768eb747f8115a906efb458b79a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-08-03 12:09:23 +0100

    AM-537 - Update ReliableCacheUsage to pass content check

--- src/SFA.DAS.Campaign.UnitTests/Application/Queries/Hub/WhenGettingAHub.cs
+++ src/SFA.DAS.Campaign.UnitTests/Application/Queries/Hub/WhenGettingAHub.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using MediatR;
 using Moq;
 using NUnit.Framework;
-using SFA.DAS.Campaign.Application.Queries.Articles;
 using SFA.DAS.Campaign.Application.Queries.Banner;
 using SFA.DAS.Campaign.Application.Queries.Hub;
 using SFA.DAS.Campaign.Application.Queries.Menu;
 using SFA.DAS.Campaign.Extensions;
 using SFA.DAS.Campaign.ExternalApi.Requests;
 using SFA.DAS.Campaign.ExternalApi.Responses;
+using SFA.DAS.Campaign.Interfaces;
 using SFA.DAS.Campaign.Models;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.Campaign.UnitTests.Application.Queries.Hub
@@ -32,15 +30,17 @@ namespace SFA.DAS.Campaign.UnitTests.Application.Queries.Hub
             HubPageModel response,
             GetBannerQueryResult bannerResult,
             BannerPageModel bannerContent,
+            [Frozen] Mock<IContentService> contentService,
             [Frozen] Mock<IReliableCacheStorageService> service,
             [Frozen] Mock<IMediator> mediator,
             GetHubQueryHandler handler)
         {
+            contentService.Setup(x=>x.HasContent(It.IsAny<ApiResponse<CmsContent>>())).Returns(true);
             service.Setup(o =>
                     o.GetData<CmsContent>(
                         It.Is<GetHubEntriesRequest>(c =>
                             c.GetUrl.Contains($"fields.hubType={query.Hub.ToTitleCase()}")),
-                        $"{query.Hub.ToTitleCase()}_hub"))
+                        $"{query.Hub.ToTitleCase()}_hub", contentService.Object.HasContent))
                 .ReturnsAsync(apiResponse);
 
             mediator.SetupMenu(menuResult, menuContent);
@@ -49,7 +49,7 @@ namespace SFA.DAS.Campaign.UnitTests.Application.Queries.Hub
             var actual = await handler.Handle(query, CancellationToken.None);
 
             actual.PageModel.Should().BeEquivalentTo(response.Build(apiResponse, menuContent, bannerContent));
-            service.Verify(x => x.GetData<CmsContent>(It.IsAny<GetHubEntriesRequest>(), It.IsAny<string>()), Times.Once);
+            service.Verify(x => x.GetData<CmsContent>(It.IsAny<GetHubEntriesRequest>(), It.IsAny<string>(), contentService.Object.HasContent), Times.Once);
         }
     }
 }
--- src/SFA.DAS.Campaign/Application/Queries/Articles/GetArticleByHubAndSlugQueryHandler.cs
+++ src/SFA.DAS.Campaign/Application/Queries/Articles/GetArticleByHubAndSlugQueryHandler.cs
@@ -4,8 +4,10 @@ using MediatR;
 using SFA.DAS.Campaign.Extensions;
 using SFA.DAS.Campaign.ExternalApi.Requests;
 using SFA.DAS.Campaign.ExternalApi.Responses;
+using SFA.DAS.Campaign.Interfaces;
 using SFA.DAS.Campaign.Models;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 
 namespace SFA.DAS.Campaign.Application.Queries.Articles
 {
@@ -13,17 +15,19 @@ namespace SFA.DAS.Campaign.Application.Queries.Articles
     {
         private readonly IReliableCacheStorageService _reliableCacheStorageService;
         private readonly IMediator _mediator;
+        private readonly IContentService _contentService;
 
         public GetArticleByHubAndSlugQueryHandler(
-            IReliableCacheStorageService reliableCacheStorageService, IMediator mediator)
+            IReliableCacheStorageService reliableCacheStorageService, IMediator mediator, IContentService contentService)
         {
             _reliableCacheStorageService = reliableCacheStorageService;
             _mediator = mediator;
+            _contentService = contentService;
         }
 
         public async Task<GetArticleByHubAndSlugQueryResult> Handle(GetArticleByHubAndSlugQuery request, CancellationToken cancellationToken)
         {
-            var article = _reliableCacheStorageService.GetData<CmsContent>(new GetArticleEntriesRequest(request.Hub.ToTitleCase(), request.Slug), $"{request.Hub.ToTitleCase()}_{request.Slug}_article");
+            var article = _reliableCacheStorageService.GetData<CmsContent>(new GetArticleEntriesRequest(request.Hub.ToTitleCase(), request.Slug), $"{request.Hub.ToTitleCase()}_{request.Slug}_article", _contentService.HasContent);
             var menu = _mediator.RetrieveMenu(cancellationToken);
             var banners = _mediator.RetrieveBanners(cancellationToken);
 
@@ -36,6 +40,8 @@ namespace SFA.DAS.Campaign.Application.Queries.Articles
                 PageModel = pageModel
             };
         }
+
+        
     }
 }
  
