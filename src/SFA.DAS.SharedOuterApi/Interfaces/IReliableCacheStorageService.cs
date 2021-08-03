04adc95138fe51469a5c95a36f2cad5b3fcb120c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-08-03 11:23:52 +0100

    AM-537 - Add request check func parameter to GetData

--- src/SFA.DAS.SharedOuterApi/Interfaces/IReliableCacheStorageService.cs
+++ src/SFA.DAS.SharedOuterApi/Interfaces/IReliableCacheStorageService.cs
@@ -1,9 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.SharedOuterApi.Models;
 
 namespace SFA.DAS.SharedOuterApi.Interfaces
 {
     public interface IReliableCacheStorageService
     {
-        Task<T> GetData<T>(IGetApiRequest request, string cacheKey);
+        Task<T> GetData<T>(IGetApiRequest request, string cacheKey, Func<ApiResponse<T>, bool> requestCheck);
     }
 }
