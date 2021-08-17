1d4e5271f88e22ea730826a0004440918a1e59b4
Matt Middleton <matt.middleton@mtcs.org.uk>
2021-08-17 08:43:02 +0100

    Unit test coverage

---
+++ src/SFA.DAS.LevyTransferMatching.Api.UnitTests/Controllers/PledgeTests/WhenCallingPledgeApplication.cs
@@ -0,0 +1,61 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.Api.Controllers;
+using SFA.DAS.LevyTransferMatching.Api.Models.Pledges;
+using SFA.DAS.LevyTransferMatching.Application.Queries.GetApplication;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.LevyTransferMatching.Api.UnitTests.Controllers.PledgeTests
+{
+    public class WhenCallingPledgeApplication
+    {
+        [Test, MoqAutoData]
+        public async Task And_Mediator_Returns_Result_Then_Return_Response_And_Ok(
+            int applicationId,
+            GetApplicationResult getApplicationResult,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] PledgeController pledgeController)
+        {
+            mockMediator
+                .Setup(x => x.Send(
+                    It.Is<GetApplicationQuery>(y => y.ApplicationId == applicationId),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(getApplicationResult);
+
+            var controllerResult = await pledgeController.PledgeApplication(applicationId);
+            var createdResult = controllerResult as OkObjectResult;
+            var getApplicationResponse = createdResult.Value as GetApplicationResponse;
+
+            Assert.IsNotNull(controllerResult);
+            Assert.IsNotNull(createdResult);
+            Assert.IsNotNull(getApplicationResponse);
+            Assert.AreEqual(createdResult.StatusCode, (int)HttpStatusCode.OK);
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_Mediator_Doesnt_Return_Result_Then_Return_NotFound(
+            int applicationId,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] PledgeController pledgeController)
+        {
+            mockMediator
+                .Setup(x => x.Send(
+                    It.Is<GetApplicationQuery>(y => y.ApplicationId == applicationId),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync((GetApplicationResult)null);
+
+            var controllerResult = await pledgeController.PledgeApplication(applicationId);
+            var notFoundResult = controllerResult as NotFoundResult;
+
+            Assert.IsNotNull(controllerResult);
+            Assert.IsNotNull(notFoundResult);
+            Assert.AreEqual(notFoundResult.StatusCode, (int)HttpStatusCode.NotFound);
+        }
+    }
+}
---
+++ src/SFA.DAS.LevyTransferMatching.UnitTests/InnerApi/Requests/ApplicationTests/WhenBuildingTheGetApplicationRequest.cs
@@ -0,0 +1,21 @@
+﻿using AutoFixture.NUnit3;
+using NUnit.Framework;
+using SFA.DAS.LevyTransferMatching.InnerApi.Requests.Applications;
+
+namespace SFA.DAS.LevyTransferMatching.UnitTests.InnerApi.Requests.ApplicationTests
+{
+    class WhenBuildingTheGetApplicationRequest
+    {
+        [Test, AutoData]
+        public void Then_The_GetUrl_Is_Correctly_Built(
+            int pledgeId,
+            int applicationId)
+        {
+            var actual = new GetApplicationRequest(pledgeId, applicationId);
+
+            Assert.AreEqual(
+                 $"pledges/{pledgeId}/applications/{applicationId}",
+                actual.GetUrl);
+        }
+    }
+}
