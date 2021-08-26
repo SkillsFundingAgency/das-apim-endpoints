1669cb49d47200ed332313f8d605f35f804cfdde
chrisfoster76 <chrisfoster186@googlemail.com>
2021-08-26 08:43:04 +0100

    Adds controller and request

---
+++ src/SFA.DAS.LevyTransferMatching.Api/Controllers/FunctionsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.LevyTransferMatching.Api.Models.Functions;
+
+namespace SFA.DAS.LevyTransferMatching.Api.Controllers
+{
+    [Route("functions")]
+    [ApiController]
+    public class FunctionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<FunctionsController> _logger;
+
+        public FunctionsController(IMediator mediator, ILogger<FunctionsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [Route("application-approved")]
+        [HttpPost]
+        public Task<IActionResult> ApplicationApproved(ApplicationApprovedRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
