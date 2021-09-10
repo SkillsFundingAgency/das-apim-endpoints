8e4787ca24c0f792d90c5effe77a33337ad5939d
Matt <matt.daniels@digital.education.gov.uk>
2021-09-10 08:45:03 +0100

    Outer API for Approvals in to Certification.

---
+++ src/SFA.DAS.Assessors.Api/Models/GetAllLearnersResponse.cs
@@ -0,0 +1,24 @@
+﻿using SFA.DAS.Assessors.InnerApi.Responses;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Assessors.Api.Models
+{
+    public class GetAllLearnersResponse
+    {
+        public List<Learner> Learners { get; }
+
+        public int BatchNumber { get; }
+
+        public int BatchSize { get; }
+
+        public int TotalNumberOfBatches { get; }
+
+        public GetAllLearnersResponse(List<Learner> learners, int batchNumber, int batchSize, int totalNumberOfBatches)
+        {
+            Learners = learners;
+            BatchNumber = batchNumber;
+            BatchSize = batchSize;
+            TotalNumberOfBatches = totalNumberOfBatches;
+        }
+    }
+}
---
+++ src/SFA.DAS.Assessors/Application/Queries/GetAllLearners/GetAllLearnersQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+
+namespace SFA.DAS.Assessors.Application.Queries.GetAllLearners
+{
+    public class GetAllLearnersQuery : IRequest<GetAllLearnersResult>
+    {
+        public DateTime? SinceTime { get; set; }
+        public int BatchNumber { get; set; }
+        public int BatchSize { get; set; }
+
+        public GetAllLearnersQuery(DateTime? sinceTime, int batchNumber, int batchSize)
+        {
+            SinceTime = sinceTime;
+            BatchNumber = batchNumber;
+            BatchSize = batchSize;
+        }
+    }
+}
