98516d7e559adfda640e99c9950eebe31cb4bdde
Conor Till <conortill@gmail.com>
2021-09-16 17:33:48 +0100

    Fixing duplicate references

--- src/SFA.DAS.LevyTransferMatching/Application/Queries/Pledges/GetApplications/GetApplicationsQueryHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Application/Queries/Pledges/GetApplications/GetApplicationsQueryHandler.cs
@@ -7,6 +7,7 @@ using SFA.DAS.LevyTransferMatching.InnerApi.Responses;
 using SFA.DAS.LevyTransferMatching.Interfaces;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.SharedOuterApi.Models;
 
--- src/SFA.DAS.SharedOuterApi/Models/Standard.cs
+++ src/SFA.DAS.SharedOuterApi/Models/Standard.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+using System.Collections.Generic;
 
 namespace SFA.DAS.SharedOuterApi.Models
 {
