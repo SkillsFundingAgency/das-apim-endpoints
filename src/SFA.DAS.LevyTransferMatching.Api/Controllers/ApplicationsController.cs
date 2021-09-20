542b58eb164334a81bcbc98147bb5d847c88ac31
Steven Scaife <steven_scaife@hotmail.com>
2021-09-20 11:44:29 +0100

    added controller and skeleton method

---
+++ src/SFA.DAS.LevyTransferMatching.Api/Controllers/ApplicationsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.LevyTransferMatching.Api.Controllers
+{
+    [ApiController]
+    public class ApplicationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ApplicationsController> _logger;
+
+        public ApplicationsController(IMediator mediator, ILogger<ApplicationsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("accounts/{accountId}/applications")]
+        public async Task<IActionResult> GetApplications(long accountId)
+        {
+            return Ok();
+        }
+    }
+}
