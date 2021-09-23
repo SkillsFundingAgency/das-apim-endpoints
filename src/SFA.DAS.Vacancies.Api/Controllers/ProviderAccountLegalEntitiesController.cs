58cfe5b28d4ecd883530af4a3960c9015dc28c8b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-09-23 07:42:33 +0100

    FAI-66 - Add controller for returning list of account legal entities

---
+++ src/SFA.DAS.Vacancies.Api/Controllers/ProviderAccountLegalEntitiesController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Vacancies.Api.Models;
+using SFA.DAS.Vacancies.Application.Providers.Queries.GetProviderAccountLegalEntities;
+
+namespace SFA.DAS.Vacancies.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class ProviderAccountLegalEntitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProviderAccountLegalEntitiesController> _logger;
+
+        public ProviderAccountLegalEntitiesController (IMediator mediator, ILogger<ProviderAccountLegalEntitiesController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetList(int ukprn)
+        {
+            try
+            {
+                var queryResponse = await _mediator.Send(new GetProviderAccountLegalEntitiesQuery
+                {
+                    Ukprn = ukprn
+                });
+
+                return Ok((GetProviderAccountLegalEntitiesListResponse) queryResponse);
+
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error attempting to get account legal entities for provider [{ukprn}]");
+                return new StatusCodeResult((int) HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
