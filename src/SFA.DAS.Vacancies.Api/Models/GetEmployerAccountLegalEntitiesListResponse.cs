f5e5d9e17b84d505dc1315926261220aa321fdb3
Scott Wakefield <scott@expertcc.co.uk>
2021-09-24 16:54:22 +0100

    FAI-69 removed extra fields not needed.

--- src/SFA.DAS.Vacancies.Api/Models/GetEmployerAccountLegalEntitiesListResponse.cs
+++ src/SFA.DAS.Vacancies.Api/Models/GetEmployerAccountLegalEntitiesListResponse.cs
@@ -22,18 +22,8 @@ namespace SFA.DAS.Vacancies.Api.Models
 
     public class GetEmployerAccountLegalEntitiesItem
     {
-        public string Code { get; set; }
-        public string DasAccountId { get; set; }
-        public DateTime? DateOfInception { get; set; }
         public string Address { get; set; }
-        public long LegalEntityId { get; set; }
         public string Name { get; set; }
-        public string PublicSectorDataSource { get; set; }
-        public string Sector { get; set; }
-        public string Source { get; set; }
-        public short SourceNumeric { get; set; }
-        public string Status { get; set; }
-        public long AccountLegalEntityId { get; set; }
         public string AccountLegalEntityPublicHashedId { get; set; }
 
         public static implicit operator GetEmployerAccountLegalEntitiesItem(
@@ -41,18 +31,8 @@ namespace SFA.DAS.Vacancies.Api.Models
         {
             return new GetEmployerAccountLegalEntitiesItem
             {
-                Code = source.Code,
-                DasAccountId = source.DasAccountId,
-                DateOfInception = source.DateOfInception,
                 Address = source.Address,
-                LegalEntityId = source.LegalEntityId,
                 Name = source.Name,
-                PublicSectorDataSource = source.PublicSectorDataSource,
-                Sector = source.Sector,
-                Source = source.Source,
-                SourceNumeric = source.SourceNumeric,
-                Status = source.Status,
-                AccountLegalEntityId = source.AccountLegalEntityId,
                 AccountLegalEntityPublicHashedId = source.AccountLegalEntityPublicHashedId
             };
         }
