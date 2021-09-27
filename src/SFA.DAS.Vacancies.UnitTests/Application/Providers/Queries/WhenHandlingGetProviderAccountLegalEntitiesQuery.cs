189348a63889a88196983b6621f45953e09ddc18
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-09-27 16:29:40 +0100

    FAI-66 - Correct response mapping

--- src/SFA.DAS.Vacancies.UnitTests/Application/Providers/Queries/WhenHandlingGetProviderAccountLegalEntitiesQuery.cs
+++ src/SFA.DAS.Vacancies.UnitTests/Application/Providers/Queries/WhenHandlingGetProviderAccountLegalEntitiesQuery.cs
@@ -8,6 +8,7 @@ using SFA.DAS.Testing.AutoFixture;
 using SFA.DAS.Vacancies.Application.Providers.Queries.GetProviderAccountLegalEntities;
 using SFA.DAS.Vacancies.Configuration;
 using SFA.DAS.Vacancies.InnerApi.Requests;
+using SFA.DAS.Vacancies.InnerApi.Responses;
 using SFA.DAS.Vacancies.Interfaces;
 
 namespace SFA.DAS.Vacancies.UnitTests.Application.Providers.Queries
@@ -16,19 +17,19 @@ namespace SFA.DAS.Vacancies.UnitTests.Application.Providers.Queries
     {
         [Test, MoqAutoData]
         public async Task Then_The_Query_Is_Handled_And_Api_Called_For_Ukprn(
-            GetProviderAccountLegalEntitiesQueryResponse apiQueryResponse,
+            GetProviderAccountLegalEntitiesResponse apiQueryResponse,
             GetProviderAccountLegalEntitiesQuery query,
             [Frozen] Mock<IProviderRelationshipsApiClient<ProviderRelationshipsApiConfiguration>> apiClient,
             GetProviderAccountLegalEntitiesQueryHandler handler)
         {
             apiClient.Setup(x =>
-                    x.Get<GetProviderAccountLegalEntitiesQueryResponse>(
+                    x.Get<GetProviderAccountLegalEntitiesResponse>(
                         It.Is<GetProviderAccountLegalEntitiesRequest>(c => c.GetUrl.Contains($"ukprn={query.Ukprn}"))))
                 .ReturnsAsync(apiQueryResponse);
             
             var actual = await handler.Handle(query, CancellationToken.None);
 
-            actual.ProviderAccountLegalEntities.Should().BeEquivalentTo(apiQueryResponse.ProviderAccountLegalEntities);
+            actual.ProviderAccountLegalEntities.Should().BeEquivalentTo(apiQueryResponse.AccountProviderLegalEntities);
         }
     }
 }
--- src/SFA.DAS.Vacancies/Application/Providers/Queries/GetProviderAccountLegalEntities/GetProviderAccountLegalEntitiesQueryHandler.cs
+++ src/SFA.DAS.Vacancies/Application/Providers/Queries/GetProviderAccountLegalEntities/GetProviderAccountLegalEntitiesQueryHandler.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.Vacancies.Configuration;
 using SFA.DAS.Vacancies.InnerApi.Requests;
+using SFA.DAS.Vacancies.InnerApi.Responses;
 using SFA.DAS.Vacancies.Interfaces;
 
 namespace SFA.DAS.Vacancies.Application.Providers.Queries.GetProviderAccountLegalEntities
@@ -18,12 +19,12 @@ namespace SFA.DAS.Vacancies.Application.Providers.Queries.GetProviderAccountLega
         public async Task<GetProviderAccountLegalEntitiesQueryResponse> Handle(GetProviderAccountLegalEntitiesQuery request, CancellationToken cancellationToken)
         {
             var response =
-                await _apiClient.Get<GetProviderAccountLegalEntitiesQueryResponse>(
+                await _apiClient.Get<GetProviderAccountLegalEntitiesResponse>(
                     new GetProviderAccountLegalEntitiesRequest(request.Ukprn));
 
             return new GetProviderAccountLegalEntitiesQueryResponse
             {
-                ProviderAccountLegalEntities = response.ProviderAccountLegalEntities
+                ProviderAccountLegalEntities = response.AccountProviderLegalEntities
             };
         }
     }
