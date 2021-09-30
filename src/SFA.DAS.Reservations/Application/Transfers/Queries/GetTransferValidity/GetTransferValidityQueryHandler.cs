906483af1c2d483dafb020a724552060887626cd
chrisfoster76 <chrisfoster186@googlemail.com>
2021-09-30 13:12:18 +0100

    Adds TransferValidity endpoint (WIP)

---
+++ src/SFA.DAS.Reservations/Application/Transfers/Queries/GetTransferValidity/GetTransferValidityQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.Reservations.Application.Transfers.Queries.GetTransferValidity
+{
+    public class GetTransferValidityQueryHandler : IRequestHandler<GetTransferValidityQuery, GetTransferValidityQueryResult>
+    {
+        public GetTransferValidityQueryHandler()
+        {
+            
+        }
+        public Task<GetTransferValidityQueryResult> Handle(GetTransferValidityQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
