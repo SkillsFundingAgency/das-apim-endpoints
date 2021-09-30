cf51d2986cee035523de2f6dfb824310e29a0647
A-J-Wright <alissa.wright@education.gov.uk>
2021-09-30 10:38:40 +0100

    Add new projects and packages

---
+++ src/SFA.DAS.Manage.Vacancies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFA.DAS.Manage.Vacancies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/SFA.DAS.Vacancies.Manage.Api.UnitTests/Models/WhenMappingFromMediatorResponseToGetProviderAccountLegalEntitiesListResponse.cs
@@ -0,0 +1,30 @@
+using AutoFixture.NUnit3;
+using NUnit.Framework;
+
+namespace SFA.DAS.Vacancies.Manage.Api.UnitTests.Models
+{
+    public class WhenMappingFromMediatorResponseToGetProviderAccountLegalEntitiesListResponse
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Correctly_Mapped(GetProviderAccountLegalEntitiesQueryResponse source)
+        {
+            var actual = (GetProviderAccountLegalEntitiesListResponse) source;
+
+            actual.ProviderAccountLegalEntities.Should().BeEquivalentTo(source.ProviderAccountLegalEntities, options => options
+                .Excluding(c=>c.AccountId)
+                .Excluding(c=>c.AccountLegalEntityId)
+                .Excluding(c=>c.AccountProviderId)
+            );
+        }
+
+        [Test, AutoData]
+        public void Then_If_Null_Empty_List_Returned(GetProviderAccountLegalEntitiesQueryResponse source)
+        {
+            source.ProviderAccountLegalEntities = null;
+            
+            var actual = (GetProviderAccountLegalEntitiesListResponse) source;
+
+            actual.ProviderAccountLegalEntities.Should().BeEmpty();
+        }
+    }
+}
