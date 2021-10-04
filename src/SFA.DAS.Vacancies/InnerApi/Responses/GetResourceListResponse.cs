2c10a389410fa82ce8aac9a247fccdaf4ec71f71
Scott Wakefield <scott@expertcc.co.uk>
2021-10-04 17:56:13 +0100

    FAI-69 remove unused type.

--- src/SFA.DAS.Vacancies/InnerApi/Responses/GetResourceListResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.Vacancies.InnerApi.Responses
-{
-    public class GetResourceListResponse
-    {
-        public IEnumerable<Resource> Resources { get; set; }
-    }
-}
