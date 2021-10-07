31a906a98a02a70becc013af0809840a1394e733
Paul Graham <paul.graham@coprime.co.uk>
2021-10-07 12:42:07 +0100

    added Patch endpoint for Revision

---
+++ src/SFA.DAS.ApprenticeCommitments.Api/Controllers/RevisionController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeCommitments.Apis.CommitmentsV2InnerApi;
+using SFA.DAS.ApprenticeCommitments.Application.Services;
+using System;
+using System.Threading.Tasks;
+using SFA.DAS.ApprenticeCommitments.Apis.InnerApi;
+
+namespace SFA.DAS.ApprenticeCommitments.Api.Controllers
+{
+    [ApiController]
+    public class RevisionController : ControllerBase
+    {
+        private readonly ResponseReturningApiClient _client;
+
+        public RevisionController(ResponseReturningApiClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPatch("apprentices/{apprenticeId}/apprenticeships/{apprenticeshipId}/revisions/{revisionId}")]
+        public Task PatchApprenticeship(Guid apprenticeId, long apprenticeshipId, long revisionId, [FromBody] JsonPatchDocument<RevisionPatch> changes)
+            => _client.Patch($"apprentices/{apprenticeId}/apprenticeships/{apprenticeshipId}/revisions/{revisionId}", changes);
+
+        [HttpPatch("apprentices/{apprenticeId}/apprenticeships/{apprenticeshipId}/revisions/{revisionId}/confirmations")]
+        public Task ConfirmApprenticeship(Guid apprenticeId, long apprenticeshipId, long revisionId, [FromBody] Confirmations confirmations)
+            => _client.Patch($"apprentices/{apprenticeId}/apprenticeships/{apprenticeshipId}/revisions/{revisionId}/confirmations", confirmations);
+    }
+}
