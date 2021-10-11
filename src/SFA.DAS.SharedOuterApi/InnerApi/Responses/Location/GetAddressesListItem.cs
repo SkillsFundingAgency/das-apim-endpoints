0fdec5eb68f7279a2c371a0e183b420204d1cd2f
Chris Woodcock <chris@humesoftware.com>
2021-10-11 14:09:32 +0100

    Add faa outer api

---
+++ src/SFA.DAS.SharedOuterApi/InnerApi/Responses/Location/GetAddressesListItem.cs
@@ -0,0 +1,14 @@
+﻿namespace SFA.DAS.SharedOuterApi.InnerApi.Responses
+{
+    public class GetAddressesListItem
+    {
+        public string Uprn { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Match { get; set; }
+    }
+}
