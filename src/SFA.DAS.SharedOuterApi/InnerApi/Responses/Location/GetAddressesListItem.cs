88591b7bedbf94b00a2594554d9ec62db1af9b29
Chris Woodcock <chris@humesoftware.com>
2021-10-13 14:27:56 +0100

    Updated location api response

--- src/SFA.DAS.SharedOuterApi/InnerApi/Responses/Location/GetAddressesListItem.cs
+++ src/SFA.DAS.SharedOuterApi/InnerApi/Responses/Location/GetAddressesListItem.cs
@@ -3,12 +3,14 @@
     public class GetAddressesListItem
     {
         public string Uprn { get; set; }
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
-        public string Town { get; set; }
+        public string House { get; set; }
+        public string Street { get; set; }
+        public string Locality { get; set; }
+        public string PostTown { get; set; }
+        public string County { get; set; }
         public string Postcode { get; set; }
-        public double Longitude { get; set; }
-        public double Latitude { get; set; }
-        public double Match { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public double? Match { get; set; }
     }
 }
