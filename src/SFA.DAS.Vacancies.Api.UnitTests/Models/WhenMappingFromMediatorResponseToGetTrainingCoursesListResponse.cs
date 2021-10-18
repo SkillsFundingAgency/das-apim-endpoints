7501b67bd96ae73cfad8dfd6f23447cfbab4ad2a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-10-18 15:13:32 +0100

    FAI-114 - Add mapping from mediator to api response

---
+++ src/SFA.DAS.Vacancies.Api.UnitTests/Models/WhenMappingFromMediatorResponseToGetTrainingCoursesListResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Vacancies.Api.Models;
+using SFA.DAS.Vacancies.Application.TrainingCourses.Queries;
+
+namespace SFA.DAS.Vacancies.Api.UnitTests.Models
+{
+    public class WhenMappingFromMediatorResponseToGetTrainingCoursesListResponse
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(GetTrainingCoursesQueryResult source)
+        {
+            var actual = (GetTrainingCoursesListResponse) source;
+
+            actual.TrainingCourses.Select(c => c.Id).ToList().Should()
+                .BeEquivalentTo(source.TrainingCourses.Select(c => c.LarsCode).ToList());
+            actual.TrainingCourses.Select(c => c.Title).ToList().Should()
+                .BeEquivalentTo(source.TrainingCourses.Select(c => $"{c.Title} (level {c.Level})").ToList());
+        }
+    }
+}
