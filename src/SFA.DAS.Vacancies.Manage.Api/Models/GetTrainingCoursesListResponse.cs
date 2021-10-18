419cfffdc574c99f268d91e915b6f6cf91aaa932
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-10-18 11:44:29 +0100

    FAI-96 - Add query response to api response mapper

---
+++ src/SFA.DAS.Vacancies.Manage.Api/Models/GetTrainingCoursesListResponse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using SFA.DAS.Vacancies.Manage.Application.TrainingCourses.Queries;
+using SFA.DAS.Vacancies.Manage.InnerApi.Responses;
+
+namespace SFA.DAS.Vacancies.Manage.Api.Models
+{
+    public class GetTrainingCoursesListResponse
+    {
+        public List<GetTrainingCoursesListResponseItem> TrainingCourses { get; set; }
+
+        public static implicit operator GetTrainingCoursesListResponse(GetTrainingCoursesQueryResult source)
+        {
+            return new GetTrainingCoursesListResponse
+            {
+                TrainingCourses = source.TrainingCourses.Select(c=>(GetTrainingCoursesListResponseItem)c).ToList()
+            };
+        }
+    }
+
+    public class GetTrainingCoursesListResponseItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public static implicit operator GetTrainingCoursesListResponseItem(GetStandardsListItem source)
+        {
+            return new GetTrainingCoursesListResponseItem
+            {
+                Id = source.LarsCode,
+                Title = $"{source.Title} (level {source.Level})"
+            };
+        }
+    }
+}
