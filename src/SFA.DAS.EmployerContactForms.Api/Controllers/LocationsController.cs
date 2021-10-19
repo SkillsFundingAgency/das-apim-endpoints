65ecb9b126e12b5e3838cabab6a9f4bd0adc3b73
Kola samuel <kola.samuel@digital.education.gov.uk>
2021-10-19 16:55:04 +0100

    Outer Api

---
+++ src/SFA.DAS.EmployerContactForms.Api/Controllers/LocationsController.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EmployerContactForms.Application.Queries.GetAddresses;
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerContactForms.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ILogger<LocationsController> _logger;
+        private readonly IMediator _mediator;
+
+        public LocationsController(ILogger<LocationsController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Index([FromQuery] string query)
+        {
+            try
+            {
+                var queryResponse = await _mediator.Send(new GetAddressesQuery(query));
+
+                if (queryResponse.AddressesResponse == null || !queryResponse.AddressesResponse.Addresses.Any())
+                    return NotFound();
+
+                return Ok(queryResponse.AddressesResponse);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error attempting to get list of addresses");
+                return StatusCode((int)HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerContactForms/Configuration/EmployerContactFormsConfiguration.cs
@@ -0,0 +1,7 @@
+namespace SFA.DAS.EmployerContactForms.Configuration
+{
+    public class EmployerContactFormsConfiguration
+    {
+        public double LocationsApiMinMatch { get; set; }
+    }
+}
