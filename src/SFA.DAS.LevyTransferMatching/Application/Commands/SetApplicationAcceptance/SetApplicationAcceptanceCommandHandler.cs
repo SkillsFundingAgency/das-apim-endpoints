1eeee1b6f635831a2ccf68774d72d7099c7bcdc5
Steven Scaife <steven_scaife@hotmail.com>
2021-10-20 13:44:57 +0100

    removed logging that was added to diagnose issue that's not required anymore

--- src/SFA.DAS.LevyTransferMatching/Application/Commands/SetApplicationAcceptance/SetApplicationAcceptanceCommandHandler.cs
+++ src/SFA.DAS.LevyTransferMatching/Application/Commands/SetApplicationAcceptance/SetApplicationAcceptanceCommandHandler.cs
@@ -22,8 +22,6 @@ namespace SFA.DAS.LevyTransferMatching.Application.Commands.SetApplicationAccept
 
         public async Task<bool> Handle(SetApplicationAcceptanceCommand request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation($"");
-
             if (request.Acceptance != Types.ApplicationAcceptance.Accept)
             {
                 throw new NotImplementedException();
@@ -41,11 +39,7 @@ namespace SFA.DAS.LevyTransferMatching.Application.Commands.SetApplicationAccept
 
             var apiRequest = new AcceptFundingRequest(request.ApplicationId, request.AccountId, apiRequestData);
 
-            _logger.LogInformation($"Passing Accept funding request to the LTM service : {apiRequest}");
-
             var result = await _levyTransferMatchingService.AcceptFunding(apiRequest, cancellationToken);
-
-            _logger.LogInformation($"Response from the LTM Service : {result}");
 
             return result.StatusCode == HttpStatusCode.NoContent;
         }
