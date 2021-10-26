1d32000a3f914b0868a49c1fe223498151c37b8e
Shahzad <hkf-tech@users.noreply.github.com>
2021-10-26 10:11:07 +0100

    apim changes

---
+++ src/SFA.DAS.Recruit/Application/Queries/GetAddresses/GetAddressesQueryHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using SFA.DAS.Recruit.Configuration;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Recruit.Application.Queries.GetAddresses
+{
+    public class GetAddressesQueryHandler : IRequestHandler<GetAddressesQuery, GetAddressesQueryResult>
+    {
+        private readonly ILocationApiClient<LocationApiConfiguration> _locationApiClient;
+        private readonly RecruitConfiguration _config;
+
+        public GetAddressesQueryHandler(ILocationApiClient<LocationApiConfiguration> locationApiClient, RecruitConfiguration config)
+        {
+            _locationApiClient = locationApiClient;
+            _config = config;
+        }
+
+        public async Task<GetAddressesQueryResult> Handle(GetAddressesQuery request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(request.Query)) throw new ArgumentException($"Query is required", nameof(GetAddressesQuery.Query));
+
+           // var addressesResponse = await _locationApiClient.Get<GetAddressesListResponse>(new GetAddressesQueryRequest(request.Query, _config.LocationsApiMinMatch));
+
+            return new GetAddressesQueryResult(new GetAddressesListResponse 
+            { 
+                Addresses = new List<GetAddressesListItem> {
+                    new GetAddressesListItem { County = "GB", House = "bb", Latitude = 123, Locality = "11", Longitude = 321, Match = 1, Postcode = "mk42 0uu", PostTown = "Bedford", Street = "Halifax Road", Uprn = "222" } ,
+                        new GetAddressesListItem { County = "GB", House = "12", Latitude = 123, Locality = "11", Longitude = 321, Match = 1, Postcode = "mk42 0uu", PostTown = "Bedford", Street = "Halifax Road", Uprn = "222" },
+                            new GetAddressesListItem { County = "GB", House = "13", Latitude = 123, Locality = "11", Longitude = 321, Match = 1, Postcode = "mk42 0uu", PostTown = "Bedford", Street = "Halifax Road", Uprn = "222" }
+
+                }
+            });
+        }
+    }
+}
---
+++ src/SFA.DAS.Recruit/Application/Queries/GetGeoPoint/GetGeoPointQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using SFA.DAS.Recruit.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Recruit.Application.Queries.GetGeocode
+{
+    public class GetGeoPointQueryHandler : IRequestHandler<GetGeoPointQuery, GetGeoPointQueryResult>
+    {
+        private readonly ILocationLookupService _locationApiClient;
+
+        public GetGeoPointQueryHandler(ILocationLookupService locationApiClient)
+        {
+            _locationApiClient = locationApiClient;
+        }
+
+        public async Task<GetGeoPointQueryResult> Handle(GetGeoPointQuery request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(request.Postcode)) throw new ArgumentException($"Postcode is required", nameof(GetGeoPointQuery.Postcode));
+
+            var location = await _locationApiClient.GetLocationInformation(request.Postcode, default, default);
+
+            return new GetGeoPointQueryResult(new GetGeoPointResponse
+            {
+                GeoPoint = new GeoPoint
+                {
+                    Latitude = location.GeoPoint[0],
+                    Longitude = location.GeoPoint[1]
+                }
+            });
+        }
+    }
+}
