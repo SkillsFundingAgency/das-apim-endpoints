c22d4ff73b645bba490ac9da339c4319dbeaf93d
A-J-Wright <alissa.wright@education.gov.uk>
2021-10-29 11:12:37 +0100

    Created Api Client, interface, config and new testcase in WhenAddingServicesToTheContainer

---
+++ src/SFA.DAS.Vacancies.Manage/Configuration/FindApprenticeshipApiConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Vacancies.Manage.Configuration
+{
+    public class FindApprenticeshipApiConfiguration : IInternalApiConfiguration
+    {
+    public string Url { get; set; }
+    public string Identifier { get; set; }
+    }
+}
