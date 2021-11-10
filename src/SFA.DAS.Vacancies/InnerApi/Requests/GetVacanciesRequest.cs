dbb2a740e71b636479411ed55700f9f51c09c1f2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-11-10 07:42:46 +0000

    FAI-107 - Add extra params to get Vacancies Request

--- src/SFA.DAS.Vacancies/InnerApi/Requests/GetVacanciesRequest.cs
+++ src/SFA.DAS.Vacancies/InnerApi/Requests/GetVacanciesRequest.cs
@@ -6,12 +6,18 @@ namespace SFA.DAS.Vacancies.InnerApi.Requests
     {
         private readonly int _pageNumber;
         private readonly int _pageSize;
+        private readonly string _accountLegalEntityPublicHashedId;
+        private readonly int? _ukprn;
+        private readonly string _accountPublicHashedId;
 
-        public GetVacanciesRequest(int pageNumber, int pageSize, string accountLegalEntityPublicHashedId, int? ukprn, string accountPublicHashedId)
+        public GetVacanciesRequest(int pageNumber, int pageSize, string accountLegalEntityPublicHashedId = "", int? ukprn = null, string accountPublicHashedId = "")
         {
             _pageNumber = pageNumber;
             _pageSize = pageSize;
+            _accountLegalEntityPublicHashedId = accountLegalEntityPublicHashedId;
+            _ukprn = ukprn;
+            _accountPublicHashedId = accountPublicHashedId;
         }
-        public string GetUrl => $"api/Vacancies?pageNumber={_pageNumber}&pageSize={_pageSize}";
+        public string GetUrl => $"api/Vacancies?pageNumber={_pageNumber}&pageSize={_pageSize}&ukprn={_ukprn}&accountLegalEntityPublicHashedId={_accountLegalEntityPublicHashedId}&accountPublicHashedId={_accountPublicHashedId}";
     }
 }
