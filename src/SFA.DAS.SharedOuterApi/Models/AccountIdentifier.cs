8ce272f5a6fa0e748d4032fbc530d94c6dc5b502
Scott Wakefield <scott@expertcc.co.uk>
2021-11-24 15:33:26 +0000

    FAI-188 fix from merges

--- src/SFA.DAS.SharedOuterApi/Models/AccountIdentifier.cs
+++ src/SFA.DAS.SharedOuterApi/Models/AccountIdentifier.cs
@@ -9,13 +9,16 @@ namespace SFA.DAS.SharedOuterApi.Models
             AccountPublicHashedId = null;
             Ukprn = null;
             
+            var identifierParts = accountIdentifier?.Split('-');
+            
+            if (string.IsNullOrEmpty(accountIdentifier) || identifierParts.Length != 3)
             {
                 AccountType = AccountType.Unknown;
                 return;
             }
-            var id = accountIdentifier.Split("-")[1];
+            var id = identifierParts[1];
             
-            Enum.TryParse(typeof(AccountType), accountIdentifier.Split("-")[0], true,
+            Enum.TryParse(typeof(AccountType), identifierParts[0], true,
                 out var accountType);
 
             AccountType = (AccountType?) accountType ?? AccountType.Unknown;
