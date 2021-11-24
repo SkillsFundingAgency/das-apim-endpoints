ff936359957c354d6ba3e49fb375e54ea566297d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-11-24 15:15:33 +0000

    FAI-170 - Add post request for creating key

---
+++ src/SFA.DAS.ApimDeveloper.UnitTests/InnerApi/Requests/WhenBuildingPostCreateSubscriptionKeyRequest.cs
@@ -0,0 +1,22 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.ApimDeveloper.InnerApi.Requests;
+
+namespace SFA.DAS.ApimDeveloper.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingPostCreateSubscriptionKeyRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_And_Body_Is_Set(string accountIdentifier, string productId)
+        {
+            var actual = new PostCreateSubscriptionKeyRequest(accountIdentifier, productId);
+
+            actual.PostUrl.Should().Be("api/subscriptions");
+            var actualData = actual.Data as CreateSubscriptionApiRequest;
+            Assert.IsNotNull(actualData);
+            actualData.AccountIdentifier.Should().Be(accountIdentifier);
+            actualData.ProductId.Should().Be(productId);
+        }
+    }
+}
