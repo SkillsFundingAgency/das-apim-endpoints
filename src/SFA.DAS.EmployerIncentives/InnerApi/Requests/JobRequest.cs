fe7da7cc9ad34b67952adfec86a3209694406875
Kevin <kevin@stephek.co.uk>
2021-12-07 14:57:44 +0000

    EI-1593-update-enum

--- src/SFA.DAS.EmployerIncentives/InnerApi/Requests/JobRequest.cs
+++ src/SFA.DAS.EmployerIncentives/InnerApi/Requests/JobRequest.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace SFA.DAS.EmployerIncentives.InnerApi.Requests
 {
     public enum JobType
     {
         RefreshLegalEntities = 1,
-        RefreshEmploymentChecks = 2
+        RefreshEmploymentChecks = 2,
+        RefreshEmploymentCheck = 3
     }
 
     public class JobRequest
