cbd3d74b11b1273be5efa77cd4823d8565a7f1f1
Scott Wakefield <scott@expertcc.co.uk>
2021-12-08 13:00:58 +0000

    FAI-168 adding send email to handler.

--- src/SFA.DAS.ApimDeveloper.UnitTests/Application/Users/Commands/WhenHandlingCreateUserCommand.cs
+++ src/SFA.DAS.ApimDeveloper.UnitTests/Application/Users/Commands/WhenHandlingCreateUserCommand.cs
@@ -2,13 +2,18 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.ApimDeveloper.Application.Users.Commands.CreateUser;
 using SFA.DAS.ApimDeveloper.Configuration;
 using SFA.DAS.ApimDeveloper.InnerApi.Requests;
 using SFA.DAS.ApimDeveloper.Interfaces;
+using SFA.DAS.ApimDeveloper.Models;
+using SFA.DAS.Notifications.Messages.Commands;
 using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
 
@@ -17,11 +22,14 @@ namespace SFA.DAS.ApimDeveloper.UnitTests.Application.Users.Commands
     public class WhenHandlingCreateUserCommand
     {
         [Test, MoqAutoData]
-        public async Task Then_The_Request_Is_Sent_To_Create_User(
+        public async Task Then_The_Request_Is_Sent_To_Create_User_And_Email_Sent(
             CreateUserCommand command,
             [Frozen] Mock<IApimDeveloperApiClient<ApimDeveloperApiConfiguration>> client,
+            [Frozen] Mock<INotificationService> mockNotificationService,
+            [Frozen] Mock<IOptions<ApimDeveloperMessagingConfiguration>> mockOptions,
             CreateUserCommandHandler handler)
         {
+            //arrange
             client.Setup(
                 x => x.PostWithResponseCode<object>(It.Is<PostCreateUserRequest>(c =>
                     c.PostUrl.Contains(command.Id.ToString())
@@ -32,14 +40,38 @@ namespace SFA.DAS.ApimDeveloper.UnitTests.Application.Users.Commands
                     && ((UserRequestData)c.Data).ConfirmationEmailLink.Equals(command.ConfirmationEmailLink)
                     && ((UserRequestData)c.Data).State.Equals(0)
                 ))).ReturnsAsync(new ApiResponse<object>(null, HttpStatusCode.NoContent, ""));
+            SendEmailCommand actualEmail = null;
+            mockNotificationService
+                .Setup(service => service.Send(It.IsAny<SendEmailCommand>()))
+                .Callback((SendEmailCommand args) => actualEmail = args)
+                .Returns(Task.CompletedTask);
+            var expectedEmail = new VerifyThirdyPartyAccountEmail(command, mockOptions.Object.Value);
             
+            //act
             await handler.Handle(command, CancellationToken.None);
+            
+            //assert
+            client.Verify(
+                x => x.PostWithResponseCode<object>(It.Is<PostCreateUserRequest>(c =>
+                    c.PostUrl.Contains(command.Id.ToString())
+                    && ((UserRequestData)c.Data).Email.Equals(command.Email) 
+                    && ((UserRequestData)c.Data).Password.Equals(command.Password)
+                    && ((UserRequestData)c.Data).FirstName.Equals(command.FirstName)
+                    && ((UserRequestData)c.Data).LastName.Equals(command.LastName)
+                    && ((UserRequestData)c.Data).ConfirmationEmailLink.Equals(command.ConfirmationEmailLink)
+                    && ((UserRequestData)c.Data).State.Equals(0)
+                )), Times.Once);
+            
+            actualEmail.Tokens.Should().BeEquivalentTo(expectedEmail.Tokens);
+            actualEmail.RecipientsAddress.Should().BeEquivalentTo(expectedEmail.RecipientAddress);
+            actualEmail.TemplateId.Should().BeEquivalentTo(expectedEmail.TemplateId);
         }
 
         [Test, MoqAutoData]
         public void Then_If_Error_HttpRequestContentException_Returned(
             CreateUserCommand command,
             [Frozen] Mock<IApimDeveloperApiClient<ApimDeveloperApiConfiguration>> client,
+            [Frozen]Mock<INotificationService> mockNotificationService,
             CreateUserCommandHandler handler)
         {
             client.Setup(
@@ -54,6 +86,8 @@ namespace SFA.DAS.ApimDeveloper.UnitTests.Application.Users.Commands
                 ))).ReturnsAsync(new ApiResponse<object>(null, HttpStatusCode.BadRequest, "Error"));
 
             Assert.ThrowsAsync<HttpRequestContentException>(() => handler.Handle(command, CancellationToken.None));
+            
+            mockNotificationService.Verify(x=>x.Send(It.IsAny<SendEmailCommand>()), Times.Never);
         }
     }
 }
--- src/SFA.DAS.ApimDeveloper/Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
+++ src/SFA.DAS.ApimDeveloper/Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -2,20 +2,31 @@ using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.Extensions.Options;
 using SFA.DAS.ApimDeveloper.Configuration;
 using SFA.DAS.ApimDeveloper.InnerApi.Requests;
 using SFA.DAS.ApimDeveloper.Interfaces;
+using SFA.DAS.ApimDeveloper.Models;
+using SFA.DAS.Notifications.Messages.Commands;
 using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.ApimDeveloper.Application.Users.Commands.CreateUser
 {
     public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Unit>
     {
         private readonly IApimDeveloperApiClient<ApimDeveloperApiConfiguration> _apimDeveloperApiClient;
+        private readonly INotificationService _notificationService;
+        private readonly ApimDeveloperMessagingConfiguration _config;
 
-        public CreateUserCommandHandler (IApimDeveloperApiClient<ApimDeveloperApiConfiguration> apimDeveloperApiClient)
+        public CreateUserCommandHandler (
+            IApimDeveloperApiClient<ApimDeveloperApiConfiguration> apimDeveloperApiClient, 
+            IOptions<ApimDeveloperMessagingConfiguration> options,
+            INotificationService notificationService)
         {
             _apimDeveloperApiClient = apimDeveloperApiClient;
+            _notificationService = notificationService;
+            _config = options.Value;
         }
         public async Task<Unit> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
@@ -33,6 +44,10 @@ namespace SFA.DAS.ApimDeveloper.Application.Users.Commands.CreateUser
             {
                 throw new HttpRequestContentException("Error creating user", actual.StatusCode, actual.ErrorContent);
             }
+
+            var email = new VerifyThirdyPartyAccountEmail(request, _config);
+            var command = new SendEmailCommand(email.TemplateId, email.RecipientAddress, email.Tokens);
+            await _notificationService.Send(command);
             
             return Unit.Value;
         }
