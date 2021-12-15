33534be906f0992a3901e0dfa280fbd3b58cb9b3
Deven Shah <devenshah1974@gmail.com>
2021-12-15 13:41:28 +0000

    APR-2800 Implements Roatp outer api (#580)
    * APR-2800 Added RoATP api project
    
    * RoatP outer API pipeline
    
    * APR-2800 Added RemovalDate to the outer api response
    
    * APR-2800 Added test project for Roatp API
    
    * publish zip of roatp
    
    * APR-2800 Added tests for roatp models
    
    * APR-2800 Fixed sonar cloud reported errors
    
    * APR-2800 Removed local exception handler and used the shared one
    
    * APR-2800 Fixed typo in nlog.config
    
    * APR-2800 added shared library to the roatp solution
    
    Co-authored-by: adam230594 <adam230594@users.noreply.github.com>

---
+++ src/SFA.DAS.Roatp.Api/Startup.cs
@@ -0,0 +1,115 @@
+using System.Collections.Generic;
+using MediatR;
+using MediatR.Extensions.FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using SFA.DAS.Api.Common.AppStart;
+using SFA.DAS.Api.Common.Configuration;
+using SFA.DAS.Roatp.Api.AppStart;
+using SFA.DAS.Roatp.Application.Charities.Queries;
+using SFA.DAS.SharedOuterApi.AppStart;
+using SFA.DAS.SharedOuterApi.Infrastructure.HealthCheck;
+
+namespace SFA.DAS.Roatp.Api
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration.BuildSharedConfiguration();
+            _env = env;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_env);
+            services.AddConfigurationOptions(_configuration);
+
+
+            if (!_configuration.IsLocalOrDev())
+            {
+                var azureAdConfiguration = _configuration
+                    .GetSection("AzureAd")
+                    .Get<AzureActiveDirectoryConfiguration>();
+                var policies = new Dictionary<string, string>
+                {
+                    {"default", "APIM"}
+                };
+
+                services.AddAuthentication(azureAdConfiguration, policies);
+            }
+
+            services.AddHealthChecks()
+                .AddCheck<CharitiesApiHealthCheck>(nameof(CharitiesApiHealthCheck));
+
+            services.AddMediatR(GetType().Assembly, typeof(GetCharityQueryHandler).Assembly);
+
+            services.AddFluentValidation(new[] { typeof(GetCharityQueryHandler).Assembly });
+
+            services.AddServiceRegistration();
+
+            services
+                .AddMvc(o =>
+                {
+                    if (!_configuration.IsLocalOrDev())
+                    {
+                        o.Filters.Add(new AuthorizeFilter("default"));
+                    }
+                })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                });
+
+            services.AddApplicationInsightsTelemetry(_configuration["APPINSIGHTS_INSTRUMENTATIONKEY"]);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RoatpOuterApi", Version = "v1" });
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.ConfigureExceptionHandler(logger);
+
+            app.UseHttpsRedirection()
+                .UseHealthChecks()
+                .UseAuthentication();
+
+            app.UseAuthentication();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "api/{controller=charities}/{action=index}/{id}");
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "RoatpOuterApi");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
