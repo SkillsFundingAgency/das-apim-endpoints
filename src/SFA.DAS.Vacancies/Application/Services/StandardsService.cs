69d4e46673e5e58802b189c82dba61134442ae6c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-12-29 13:07:13 +0000

    FAI-216 - Add Standards service for getting list of standards

---
+++ src/SFA.DAS.Vacancies/Application/Services/StandardsService.cs
@@ -0,0 +1,38 @@
+using System.Threading.Tasks;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Vacancies.InnerApi.Responses;
+using SFA.DAS.Vacancies.Interfaces;
+
+namespace SFA.DAS.Vacancies.Application.Services
+{
+    public class StandardsService  : IStandardsService
+    {
+        private readonly ICacheStorageService _cacheStorageService;
+        private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
+
+        public StandardsService (ICacheStorageService cacheStorageService, ICoursesApiClient<CoursesApiConfiguration> coursesApiClient)
+        {
+            _cacheStorageService = cacheStorageService;
+            _coursesApiClient = coursesApiClient;
+        }
+        public async Task<GetStandardsListResponse> GetStandards()
+        {
+            var cachedCourses =
+                await _cacheStorageService.RetrieveFromCache<GetStandardsListResponse>(
+                    nameof(GetStandardsListResponse));
+
+            if (cachedCourses != null)
+            {
+                return cachedCourses;
+            }
+
+            var apiCourses = await _coursesApiClient.Get<GetStandardsListResponse>(new GetActiveStandardsListRequest());
+
+            await _cacheStorageService.SaveToCache(nameof(GetStandardsListResponse), apiCourses, 4);
+
+            return apiCourses;
+        }
+    }
+}
