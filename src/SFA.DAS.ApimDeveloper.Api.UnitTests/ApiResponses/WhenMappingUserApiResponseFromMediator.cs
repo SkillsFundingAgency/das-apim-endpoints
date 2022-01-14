b01461047d910c0bf8ca65920af57c5d69bf4644
Scott Wakefield <scott@expertcc.co.uk>
2022-01-14 11:49:11 +0000

    FAI-193 api response for get user.

---
+++ src/SFA.DAS.ApimDeveloper.Api.UnitTests/ApiResponses/WhenMappingUserApiResponseFromMediator.cs
@@ -0,0 +1,33 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.ApimDeveloper.Api.ApiResponses;
+using SFA.DAS.ApimDeveloper.Application.Users.Commands.AuthenticateUser;
+using SFA.DAS.ApimDeveloper.Application.Users.Queries.GetUser;
+
+namespace SFA.DAS.ApimDeveloper.Api.UnitTests.ApiResponses
+{
+    public class WhenMappingUserApiResponseFromMediator
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(GetUserQueryResult source)
+        {
+            var actual = (UserApiResponse)source;
+
+            actual.User.Should().BeEquivalentTo(source.User);
+        }
+
+        [Test]
+        public void Then_If_Null_Then_Null_Returned()
+        {
+            var source = new GetUserQueryResult
+            {
+                User = null
+            };
+
+            var actual = (UserApiResponse)source;
+
+            actual.Should().BeNull();
+        }
+    }
+}
---
+++ src/SFA.DAS.ApimDeveloper.Api/ApiResponses/UserApiResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SFA.DAS.ApimDeveloper.Application.Users.Queries.GetUser;
+
+namespace SFA.DAS.ApimDeveloper.Api.ApiResponses
+{
+    public class UserApiResponse
+    {
+        public UserApiResponseItem User { get; set; }
+        
+        public static implicit operator UserApiResponse(GetUserQueryResult source)
+        {
+            if (source.User == null)
+            {
+                return null;
+            }
+                
+            return new UserApiResponse
+            {
+                User = new UserApiResponseItem
+                {
+                    Id = source.User.Id,
+                    Email = source.User.Email,
+                    FirstName = source.User.FirstName,
+                    LastName = source.User.LastName,
+                    State = source.User.State
+                }
+            };
+        }
+    }
+    
+    public class UserApiResponseItem
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public Guid Id { get ; set ; }
+    }
+}
