ad744f408686d7279d620f91d5bf0d61ce9afc85
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-01-28 11:12:52 +0000

    FAI-227 - Add configuration

--- src/SFA.DAS.Campaign.Api/AppStart/AddConfigurationOptionsExtension.cs
+++ src/SFA.DAS.Campaign.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -14,6 +14,10 @@ namespace SFA.DAS.Campaign.Api.AppStart
             services.AddOptions();
             services.Configure<CoursesApiConfiguration>(configuration.GetSection(nameof(CoursesApiConfiguration)));
             services.AddSingleton(cfg => cfg.GetService<IOptions<CoursesApiConfiguration>>().Value);
+            services.Configure<LocationApiConfiguration>(configuration.GetSection(nameof(LocationApiConfiguration)));
+            services.AddSingleton(cfg => cfg.GetService<IOptions<LocationApiConfiguration>>().Value);
+            services.Configure<FindApprenticeshipApiConfiguration>(configuration.GetSection(nameof(FindApprenticeshipApiConfiguration)));
+            services.AddSingleton(cfg => cfg.GetService<IOptions<FindApprenticeshipApiConfiguration>>().Value);
             services.Configure<AzureActiveDirectoryConfiguration>(configuration.GetSection("AzureAd"));
             services.AddSingleton(cfg => cfg.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
             
