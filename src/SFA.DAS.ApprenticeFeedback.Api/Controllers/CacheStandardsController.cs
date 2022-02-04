11401f4949728249b6d8c7682b39ef94aeabd954
Ben Stone <ben1.stone@education.gov.uk>
2022-02-04 17:17:12 +0000

    QF-208 Start Cache Standards

---
+++ src/SFA.DAS.ApprenticeFeedback.Api/Controllers/CacheStandardsController.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeFeedback.Api.Models;
+using SFA.DAS.ApprenticeFeedback.Application.Queries.GetStandards;
+using SFA.DAS.ApprenticeFeedback.InnerApi.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeFeedback.Api.Controllers
+{
+    public class CacheStandardsController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CacheStandardsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CacheStandards()
+        {
+            var standards = await _mediator.Send(new GetStandardsQuery());
+
+            var command = new CacheStandardsCommand
+            {
+                Standards = standards.Standards.Select(s => (Standard)s)
+            };
+
+            var cacheResponse = 
+
+            return Ok(response);
+        }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeFeedback/Application/Queries/GetStandards/GetStandardsResult.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.ApprenticeFeedback.InnerApi.Responses;
+using System.Collections.Generic;
+
+namespace SFA.DAS.ApprenticeFeedback.Application.Queries.GetStandards
+{
+    public class GetStandardsResult
+    {
+        public IEnumerable<GetStandardsListItem> Standards { get; set; }
+    }
+}
