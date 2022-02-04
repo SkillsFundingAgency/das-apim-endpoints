a543570067edfd8807a76d93069526be3d92dd3b
Ben Stone <ben1.stone@education.gov.uk>
2022-02-04 11:57:08 +0000

    QF-208 Add Assessor service setup

--- src/SFA.DAS.ApprenticeFeedback.Api/AppStart/AddConfigurationOptionsExtension.cs
+++ src/SFA.DAS.ApprenticeFeedback.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using SFA.DAS.Api.Common.Configuration;
 using SFA.DAS.SharedOuterApi.Configuration;
 
 namespace SFA.DAS.ApprenticeFeedback.Api.AppStart
@@ -13,9 +12,12 @@ namespace SFA.DAS.ApprenticeFeedback.Api.AppStart
             services.AddOptions();
 
             services.Configure<ApprenticeAccountsApiConfiguration>(configuration.GetSection("ApprenticeAccountsInnerApi"));
-            services.Configure<ApprenticeCommitmentsApiConfiguration>(configuration.GetSection("ApprenticeCommitmentsInnerApi"));
+            services.Configure<AssessorsApiConfiguration>(configuration.GetSection("AssessorServiceInnerApi"));
+            services.Configure<ApprenticeFeedbackApiConfiguration>(configuration.GetSection("ApprenticeFeedbackInnerApi"));
+
             services.AddSingleton(cfg => cfg.GetService<IOptions<ApprenticeAccountsApiConfiguration>>().Value);
-            services.AddSingleton(cfg => cfg.GetService<IOptions<ApprenticeCommitmentsApiConfiguration>>().Value);
+            services.AddSingleton(cfg => cfg.GetService<IOptions<AssessorsApiConfiguration>>().Value);
+            services.AddSingleton(cfg => cfg.GetService<IOptions<ApprenticeFeedbackApiConfiguration>>().Value);
         }
     }
 }
