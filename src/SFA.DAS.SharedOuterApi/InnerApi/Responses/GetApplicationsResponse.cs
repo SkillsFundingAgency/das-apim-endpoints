3fa59afad604786d400e81f750c29c954b68c940
chrisfoster76 <chrisfoster186@googlemail.com>
2022-02-10 09:03:23 +0000

    Implementation

--- src/SFA.DAS.SharedOuterApi/InnerApi/Responses/GetApplicationsResponse.cs
+++ src/SFA.DAS.SharedOuterApi/InnerApi/Responses/GetApplicationsResponse.cs
@@ -49,10 +49,18 @@ namespace SFA.DAS.SharedOuterApi.InnerApi.Responses
             public string AdditionalLocations { get; set; }
             public long SenderEmployerAccountId { get; set; }
             public string SenderEmployerAccountName { get; set; }
+            public IEnumerable<CostProjection> CostProjections { get; set; }
+
             public class ApplicationLocation
             {
                 public int Id { get; set; }
                 public int PledgeLocationId { get; set; }
+            }
+
+            public class CostProjection
+            {
+                public string FinancialYear { get; set; }
+                public int Amount { get; set; }
             }
         }
     }
