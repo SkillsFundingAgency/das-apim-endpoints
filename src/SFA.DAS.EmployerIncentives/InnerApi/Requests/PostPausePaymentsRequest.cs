e9702761abbf0b0648a622301f98d8931c5d9efb
Kevin <kevin@stephek.co.uk>
2022-02-21 18:41:41 +0000

    EI-1677-bulk-process-pause-unpause

--- src/SFA.DAS.EmployerIncentives/InnerApi/Requests/PostPausePaymentsRequest.cs
+++ src/SFA.DAS.EmployerIncentives/InnerApi/Requests/PostPausePaymentsRequest.cs
@@ -22,8 +22,13 @@ namespace SFA.DAS.EmployerIncentives.InnerApi.Requests
     public class PausePaymentsRequest
     {
         public PausePaymentsAction Action { get; set; }
+        public Application[] Applications { get; set; }
+        public ServiceRequest ServiceRequest { get; set; }       
+    }
+
+    public class Application
+    {
         public long AccountLegalEntityId { get; set; }
         public long ULN { get; set; }
-        public ServiceRequest ServiceRequest { get; set; }       
     }
 }
