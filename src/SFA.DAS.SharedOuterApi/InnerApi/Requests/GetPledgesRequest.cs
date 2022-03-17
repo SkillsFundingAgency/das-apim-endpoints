f738aac2bf9b7e6d9acda46284154c3943c99e1d
chrisfoster76 <chrisfoster186@googlemail.com>
2022-03-17 11:22:37 +0000

    Test fixes (temp)

--- src/SFA.DAS.SharedOuterApi/InnerApi/Requests/GetPledgesRequest.cs
+++ src/SFA.DAS.SharedOuterApi/InnerApi/Requests/GetPledgesRequest.cs
@@ -15,15 +15,12 @@ namespace SFA.DAS.SharedOuterApi.InnerApi.Requests
                 filters.Add("accountId", accountId.ToString());
             }
 
-            filters.Add("page", "0");
-            filters.Add("pageSize", int.MaxValue.ToString());
-
             this.GetUrl = $"pledges{filters.ToQueryString()}";
         }
 
         public GetPledgesRequest(int page, int pageSize)
         {
-            GetUrl = $"pledges?page={page}&pageSize={pageSize}";
+            GetUrl = $"pledges";
         }
 
         public string GetUrl { get; set; }
