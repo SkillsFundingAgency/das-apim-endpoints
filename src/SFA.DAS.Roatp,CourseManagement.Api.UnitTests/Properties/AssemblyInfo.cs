c71c45c320098ef3b057fa9fa4e10e4ab62b37dc
Deven Shah <devenshah1974@gmail.com>
2022-03-30 20:08:57 +0100

    CSP-58 Added roatp course management outer api projects

---
+++ src/SFA.DAS.Roatp,CourseManagement.Api.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SFA.DAS.Roatp,CourseManagement.Api.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SFA.DAS.Roatp,CourseManagement.Api.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b236eba-8c9b-4364-a7e7-e521720aa750")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
