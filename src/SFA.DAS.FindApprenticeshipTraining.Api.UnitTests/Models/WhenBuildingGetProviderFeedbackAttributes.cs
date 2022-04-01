025a2d3de98aee87f1a2e27c82ccf07a0467dd5a
anilkdhiman6 <77051469+anilkdhiman6@users.noreply.github.com>
2022-04-01 12:44:14 +0100

    Qf 287 employer feedback ratings api changes (#656)
    * Updates for Strengths/Weaknesses Counts
    
    * Correct spelling
    
    * Simplify model
    
    * Unit test updates
    
    * Update unit tests / add sorting  by total
    
    * Changes following PR - remove extra object
    
    * Unit test updates
    
    * Fix sum
    
    Co-authored-by: CodeWolf86 <54770494+CodeWolf86@users.noreply.github.com>

--- src/SFA.DAS.FindApprenticeshipTraining.Api.UnitTests/Models/WhenBuildingGetProviderFeedbackAttributes.cs
+++ src/SFA.DAS.FindApprenticeshipTraining.Api.UnitTests/Models/WhenBuildingGetProviderFeedbackAttributes.cs
@@ -19,63 +19,62 @@ namespace SFA.DAS.FindApprenticeshipTraining.Api.UnitTests.Models
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "First Attribute",
-                    Strength = 10,
-                    Weakness = 10
+                    Strength = 0,
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Second Attribute",
-                    Strength = 12,
-                    Weakness = 12
+                    Strength = 0,
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Third Attribute",
-                    Strength = 13,
-                    Weakness = 13
+                    Strength = 0,
+                    Weakness = 0
                 }
             };
             
             var response = new GetTrainingCourseProviderListItem().Map(source, sectorSubjectArea,1,new List<DeliveryModeType>(), new List<FeedbackRatingType>(), true);
 
-            response.Feedback.FeedbackAttributes.Strengths.Should().BeEmpty();
-            response.Feedback.FeedbackAttributes.Weaknesses.Should().BeEmpty();
+            response.Feedback.FeedbackAttributes.Should().BeEmpty();
         }
 
         [Test, AutoData]
-        public void Then_No_Strengths_Returns_Empty_List(GetProvidersListItem source, string sectorSubjectArea)
+        public void Then_No_Strengths_Returns_Zero_Count(GetProvidersListItem source, string sectorSubjectArea)
         {
             source.FeedbackAttributes = new List<GetFeedbackAttributeItem>
             {
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "First Attribute",
-                    Strength = 10,
+                    Strength = 0,
                     Weakness = 12
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Second Attribute",
-                    Strength = 10,
+                    Strength = 0,
                     Weakness = 13
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Third Attribute",
-                    Strength = 10,
+                    Strength = 0,
                     Weakness = 14
                 }
             };
-            
-            var response = new GetTrainingCourseProviderListItem().Map(source, sectorSubjectArea,1,new List<DeliveryModeType>(),new List<FeedbackRatingType>(), true);
 
-            response.Feedback.FeedbackAttributes.Strengths.Should().BeEmpty();
-            response.Feedback.FeedbackAttributes.Weaknesses.Should().Contain(source.FeedbackAttributes.Select(c => c.AttributeName).ToList());
-            response.Feedback.FeedbackAttributes.Weaknesses.Should().NotBeEmpty();
+            var response = new GetTrainingCourseProviderListItem().Map(source, sectorSubjectArea, 1, new List<DeliveryModeType>(), new List<FeedbackRatingType>(), true);
+
+            response.Feedback.FeedbackAttributes.Sum(x => x.Strength).Should().Be(0);
+            response.Feedback.FeedbackAttributes.Select(x => x.AttributeName).Should().Contain(source.FeedbackAttributes.Select(c => c.AttributeName).ToList());
+            response.Feedback.FeedbackAttributes.Sum(x => x.Weakness).Should().Be(39);
         }
 
         [Test, AutoData]
-        public void Then_No_Weaknesses_Returns_Empty_List(GetProvidersListItem source, string sectorSubjectArea)
+        public void Then_No_Weaknesses_Returns_Zero_Count(GetProvidersListItem source, string sectorSubjectArea)
         {
             source.FeedbackAttributes = new List<GetFeedbackAttributeItem>
             {
@@ -83,155 +82,107 @@ namespace SFA.DAS.FindApprenticeshipTraining.Api.UnitTests.Models
                 {
                     AttributeName = "First Attribute",
                     Strength = 12,
-                    Weakness = 10
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Second Attribute",
                     Strength = 13,
-                    Weakness = 12
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Third Attribute",
                     Strength = 14,
-                    Weakness = 13
+                    Weakness = 0
                 }
             };
             
             var response = new GetTrainingCourseProviderListItem().Map(source, sectorSubjectArea,1,new List<DeliveryModeType>(),new List<FeedbackRatingType>(), true);
 
-            response.Feedback.FeedbackAttributes.Strengths.Should().NotBeEmpty();
-            response.Feedback.FeedbackAttributes.Strengths.Should().Contain(source.FeedbackAttributes.Select(c => c.AttributeName).ToList());
-            response.Feedback.FeedbackAttributes.Weaknesses.Should().BeEmpty();
+            response.Feedback.FeedbackAttributes.Sum(x => x.Weakness).Should().Be(0);
+            response.Feedback.FeedbackAttributes.Select(x => x.AttributeName).Should().Contain(source.FeedbackAttributes.Select(c => c.AttributeName).ToList());
+            response.Feedback.FeedbackAttributes.Sum(x => x.Strength).Should().Be(39);
         }
-        
-        [Test, AutoData]
-        public void Then_If_Same_Feedback_Attribute_Score_Then_The_Attribute_With_Most_Response_Is_First(GetProvidersListItem source, string sectorSubjectArea)
-        {
-            source.FeedbackAttributes = new List<GetFeedbackAttributeItem>
-            {
-                new GetFeedbackAttributeItem
-                {
-                    AttributeName = "Zattribute",
-                    Strength = 140,
-                    Weakness = 144
-                },
-                new GetFeedbackAttributeItem
-                {
-                    AttributeName = "Yattribute",
-                    Strength = 1040,
-                    Weakness = 1044
-                },
-                new GetFeedbackAttributeItem
-                {
-                    AttributeName = "Attribute",
-                    Strength = 10,
-                    Weakness = 14
-                }
-            };
-            
-            var response = new GetTrainingCourseProviderListItem().Map(source, sectorSubjectArea,1,new List<DeliveryModeType>(), new List<FeedbackRatingType>(), true);
-            
-            response.Feedback.FeedbackAttributes.Weaknesses.Should().ContainInOrder(new List<string>{"Yattribute", "Zattribute", "Attribute"});
-        }
-        
-        [Test, AutoData]
-        public void Then_If_Same_Feedback_Attribute_Score_And_Same_Responses_Then_The_Attribute_Is_Returned_In_Alphabetical_Order(GetProvidersListItem source, string sectorSubjectArea)
-        {
-            source.FeedbackAttributes = new List<GetFeedbackAttributeItem>
-            {
-                new GetFeedbackAttributeItem
-                {
-                    AttributeName = "Sixth Attribute",
-                    Strength = 14,
-                    Weakness = 13
-                },
-                new GetFeedbackAttributeItem
-                {
-                    AttributeName = "Seventh Attribute",
-                    Strength = 15,
-                    Weakness = 14
-                }
-            };
-            
-            var response = new GetTrainingCourseProviderListItem().Map(source, sectorSubjectArea,1,new List<DeliveryModeType>(),new List<FeedbackRatingType>(), true);
-            
-            response.Feedback.FeedbackAttributes.Strengths.Should().ContainInOrder(new List<string>{"Seventh Attribute"});
-        }
-        
 
+       
         [Test, AutoData]
-        public void Then_Returns_The_Top_Three_Feedback_Attribute_Strengths_And_Weaknesses(GetProvidersListItem source, string sectorSubjectArea)
+        public void Then_Returns_All_Available_Feedback_Attribute_Strengths_And_Weaknesses_Where_Strengths_Weaknesses_Greater_Than_Zero(GetProvidersListItem source, string sectorSubjectArea)
         {
             source.FeedbackAttributes = new List<GetFeedbackAttributeItem>
             {
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "First Attribute",
-                    Strength = 11,
-                    Weakness = 10
+                    Strength = 1,
+                    Weakness = 1
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Second Attribute",
-                    Strength = 11,
-                    Weakness = 10
+                    Strength = 0,
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Third Attribute",
-                    Strength = 13,
-                    Weakness = 10
+                    Strength = 1,
+                    Weakness = 1
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Fourth Attribute",
-                    Strength = 14,
-                    Weakness = 10
+                    Strength = 0,
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Fifth Attribute",
-                    Strength = 11,
-                    Weakness = 11
+                    Strength = 1,
+                    Weakness = 1
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Sixth Attribute",
-                    Strength = 13,
-                    Weakness = 14
+                    Strength = 0,
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Seventh Attribute",
-                    Strength = 14,
-                    Weakness = 15
+                    Strength = 1,
+                    Weakness = 1
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Eighth Attribute",
-                    Strength = 14,
-                    Weakness = 20
+                    Strength = 0,
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Ninth Attribute",
-                    Strength = 140,
-                    Weakness = 144
+                    Strength = 0,
+                    Weakness = 0
                 },
                 new GetFeedbackAttributeItem
                 {
                     AttributeName = "Tenth Attribute",
-                    Strength = 10,
-                    Weakness = 14
+                    Strength = 1,
+                    Weakness = 1
                 }
             };
             
             var response = new GetTrainingCourseProviderListItem().Map(source, sectorSubjectArea,1,new List<DeliveryModeType>(),new List<FeedbackRatingType>(), true);
             
-            response.Feedback.FeedbackAttributes.Strengths.Should().ContainInOrder(new List<string>{"Fourth Attribute", "Third Attribute", "First Attribute"});
-            response.Feedback.FeedbackAttributes.Weaknesses.Should().ContainInOrder(new List<string>{"Eighth Attribute", "Ninth Attribute", "Tenth Attribute"});
+            response.Feedback.FeedbackAttributes.Select(x => x.AttributeName)
+                .Should().ContainInOrder(
+                new List<string>{
+                    "First Attribute",
+                    "Third Attribute",
+                    "Fifth Attribute",
+                    "Seventh Attribute",
+                    "Tenth Attribute"});
         }
 
     }
