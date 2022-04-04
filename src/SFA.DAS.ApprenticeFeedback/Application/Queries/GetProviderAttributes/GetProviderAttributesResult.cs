0082af8e1b10aaf87804d990afb83ea92c51cc4f
Sarah Coetzee <82900731+SarahDuncan@users.noreply.github.com>
2022-04-04 14:59:12 +0100

    QF-294 get feedback attributes outer api (#652)
    * QF-294 Build outer api
    
    * QF-294 Add unit tests for get provider attributes

---
+++ src/SFA.DAS.ApprenticeFeedback/Application/Queries/GetProviderAttributes/GetProviderAttributesResult.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.ApprenticeFeedback.InnerApi.Responses;
+using System.Collections.Generic;
+
+namespace SFA.DAS.ApprenticeFeedback.Application.Queries.GetProviderAttributes
+{
+    public class GetProviderAttributesResult
+    {
+        public List<ProviderAttribute> ProviderAttributes { get; set; }
+    }
+}
---
+++ src/SFA.DAS.ApprenticeFeedback/InnerApi/Responses/GetProviderAttributesResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.ApprenticeFeedback.InnerApi.Responses
+{
+    public class GetProviderAttributesResponse
+    {
+        public List<ProviderAttribute> ProviderAttributes { get; set; }
+    }
+}
