168453adc003a1615f5a7e0d60001b73ddca15c6
codewolf86 <chris@codewolfconsulting.com>
2022-04-05 15:37:59 +0100

    Update for Provider Attributes type change.

--- src/SFA.DAS.ApprenticeFeedback/InnerApi/Responses/GetProviderAttributesResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.ApprenticeFeedback.InnerApi.Responses
-{
-    public class GetProviderAttributesResponse
-    {
-        public List<ProviderAttribute> ProviderAttributes { get; set; }
-    }
-}
