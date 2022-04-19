38d26a84e254217a47fd4f034c9aa9004a715f22
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-04-19 12:01:37 +0100

    [CON-4723] New MA outer endpoint to get Forecasting Account summary. (Aggregation of ProjectionCalc and Expiring funds

---
+++ src/SFA.DAS.ManageApprenticeships.Api/Controllers/ProjectionsController.cs
@@ -0,0 +1,46 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ManageApprenticeships.Api.Models;
+using SFA.DAS.ManageApprenticeships.Api.Models.Transfers;
+using SFA.DAS.ManageApprenticeships.Application.Queries.GetAccountProjectionSummary;
+using SFA.DAS.ManageApprenticeships.Application.Queries.Transfers.GetIndex;
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ManageApprenticeships.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class ProjectionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProjectionsController> _logger;
+
+        public ProjectionsController(IMediator mediator, ILogger<ProjectionsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("{accountId}")]
+        public async Task<IActionResult> GetAccountProjectionSummary(long accountId)
+        {
+            try
+            {
+                var response = await _mediator.Send(new GetAccountProjectionSummaryQuery
+                {
+                    AccountId = accountId,
+                });                
+
+                return Ok((GetAccountProjectionSummaryResponse)response);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error getting transfers");
+                return BadRequest();
+            }
+        }
+    }
+}
