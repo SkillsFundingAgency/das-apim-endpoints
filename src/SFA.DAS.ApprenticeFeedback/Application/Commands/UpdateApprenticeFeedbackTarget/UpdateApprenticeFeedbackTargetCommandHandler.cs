6ed3584d0d3f7a2b24fd727f44452badedb01f84
codewolf86 <chris@codewolfconsulting.com>
2022-04-28 13:23:47 +0100

    Sonar Cloud Fixes

--- src/SFA.DAS.ApprenticeFeedback/Application/Commands/UpdateApprenticeFeedbackTarget/UpdateApprenticeFeedbackTargetCommandHandler.cs
+++ src/SFA.DAS.ApprenticeFeedback/Application/Commands/UpdateApprenticeFeedbackTarget/UpdateApprenticeFeedbackTargetCommandHandler.cs
@@ -39,7 +39,7 @@ namespace SFA.DAS.ApprenticeFeedback.Application.Commands.UpdateApprenticeFeedba
 
 
             // 1.a If none, we do nothing, but potential for in future to make it smarter.
-            if (apprenticeFeedbackTargets?.Any() == false)
+            if (apprenticeFeedbackTargets == null || apprenticeFeedbackTargets.Any() == false)
             {
                 var responseMessage = $"No ApprenticeFeedbackTargets found for ApprenticeId: { command.ApprenticeId}";
                 _logger.LogWarning(responseMessage);
