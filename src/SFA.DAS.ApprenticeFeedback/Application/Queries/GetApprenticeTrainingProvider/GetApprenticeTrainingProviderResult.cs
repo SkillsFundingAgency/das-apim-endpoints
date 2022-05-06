6386180bd9dcd3b505bc175f3563dc864064c721
Matt <matt.daniels@digital.education.gov.uk>
2022-05-06 16:32:55 +0100

    GetTrainingProvider needs to return TimeWindow and SignificantDate incase the user games the URL in the UI, so that the UI can display the correct content on the error page. So we may as well align GetTrainingProvider with GetTrainingProvider s

--- src/SFA.DAS.ApprenticeFeedback/Application/Queries/GetApprenticeTrainingProvider/GetApprenticeTrainingProviderResult.cs
+++ src/SFA.DAS.ApprenticeFeedback/Application/Queries/GetApprenticeTrainingProvider/GetApprenticeTrainingProviderResult.cs
@@ -1,18 +1,9 @@
 ﻿using SFA.DAS.ApprenticeFeedback.Models;
-using System;
-using static SFA.DAS.ApprenticeFeedback.Models.Enums;
 
 namespace SFA.DAS.ApprenticeFeedback.Application.Queries.GetApprenticeTrainingProvider
 {
     public class GetApprenticeTrainingProviderResult
     {
-        public Guid ApprenticeFeedbackTargetId { get; set; }
-        public string ProviderName { get; set; }
-        public long Ukprn { get; set; }
-        public DateTime? LastFeedbackSubmittedDate { get; set; }
-        public TimeSpan? TimeWindow { get; set; }
-        public DateTime? SignificantDate { get; set; }
-        public FeedbackEligibility FeedbackEligibility { get; set; }
-
+        public TrainingProvider TrainingProvider { get; set; }
     }
 }
