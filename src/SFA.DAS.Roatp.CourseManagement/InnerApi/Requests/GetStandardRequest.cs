939c76804f360b119c7b542bec947dff6b4fbcfe
MarkFCain <mark.cain@digital.education.gov.uk>
2022-05-06 14:21:18 +0100

    CSP-17 code and coverage to get standard details

---
+++ src/SFA.DAS.Roatp.CourseManagement/InnerApi/Requests/GetStandardRequest.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Roatp.CourseManagement.InnerApi.Requests
+{
+    public class GetStandardRequest : IGetApiRequest
+    {
+        public string GetUrl => $"/api/courses/Standards/{StandardCode}";
+        public int StandardCode { get; }
+
+        public GetStandardRequest(int standardCode)
+        {
+            StandardCode = standardCode;
+        }
+    }
+}
