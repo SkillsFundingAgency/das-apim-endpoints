86c31fd31421c0b2c8824987b337eae3860fc1e0
MarkFCain <mark.cain@digital.education.gov.uk>
2022-05-11 14:07:54 +0100

    CSP-17 minor snagging

--- src/SFA.DAS.Roatp.CourseManagement/InnerApi/Responses/GetStandardResponse.cs
+++ src/SFA.DAS.Roatp.CourseManagement/InnerApi/Responses/GetStandardResponse.cs
@@ -7,9 +7,7 @@
         public int LarsCode { get; set; }
         public string Title { get; set; }
         public int Level { get; set; }
-       
         public string Version { get; set; }
-
         public string ApprovalBody { get; set; }
         public string Route { get; set; }
     }
