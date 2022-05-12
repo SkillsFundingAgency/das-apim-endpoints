1599f2a3cb335f07c53143516c12714f4d338be1
MarkFCain <mark.cain@digital.education.gov.uk>
2022-05-12 08:33:54 +0100

    CSP-17 api config update

--- src/SFA.DAS.Roatp.CourseManagement/InnerApi/Requests/GetStandardRequest.cs
+++ src/SFA.DAS.Roatp.CourseManagement/InnerApi/Requests/GetStandardRequest.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.Roatp.CourseManagement.InnerApi.Requests
 {
     public class GetStandardRequest : IGetApiRequest
     {
-        public string GetUrl => $"/api/courses/Standards/{StandardCode}";
+        public string GetUrl => $"api/courses/Standards/{StandardCode}";
         public int StandardCode { get; }
 
         public GetStandardRequest(int standardCode)
