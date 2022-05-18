ce1f9950991e07b95d556085c586251dd7cab27c
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2022-05-18 11:31:41 +0100

    CSP-17 Get standards from courses api (#707)
    * CSP-17 code and coverage to get standard details
    
    * CSP-17 view standard coverage and code complete
    
    * CSP-17 added route to returned result
    
    * CSP-17 view standard code and coverage
    
    * CSP-17 minor snagging
    
    * CSP-17 api config update
    
    * CSP-17 refined logging/exception handling to be more specific
    
    * CSP-17 removed unneeded query and handler
    
    * CSP-17 snagging from TR

---
+++ src/SFA.DAS.Roatp.CourseManagement/InnerApi/Responses/GetProviderCourseResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.Roatp.CourseManagement.InnerApi.Responses
+{
+    public class GetProviderCourseResponse
+    {
+        public string StandardUId { get; set; }
+        public string IfateReferenceNumber { get; set; }
+        public int LarsCode { get; set; }
+        public string Title { get; set; }
+        public int Level { get; set; }
+        public string Version { get; set; }
+        public string ApprovalBody { get; set; }
+        public string Route { get; set; }
+    }
+}
