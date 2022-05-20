40be4235020f793ea1d9d80659c57af8f7e78f06
MarkFCain <mark.cain@digital.education.gov.uk>
2022-05-20 08:49:42 +0100

    CSP-117 get providers from roatpservice code and coverage

---
+++ src/SFA.DAS.Roatp.CourseManagement/Application/RegisteredProviders/Queries/GetRegisteredProvidersQuery.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using SFA.DAS.Roatp.CourseManagement.InnerApi.Models.RegisteredProvider;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.RegisteredProviders.Queries
+{
+    public class GetRegisteredProvidersQuery : IGetApiRequest, IRequest<ApiResponse<List<RegisteredProviderModel>>>
+    {
+        public string GetUrl => $"v1/fat-data-export";
+    }
+}
