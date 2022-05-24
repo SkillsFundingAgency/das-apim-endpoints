a6aa5ddf0f4dc3888c0e88cc29e99d3e36050210
Deven Shah <devenshah1974@gmail.com>
2022-05-24 02:52:46 +0100

    CSP-19 Added pass through endpoint for updating course contact details

--- src/SFA.DAS.Roatp.CourseManagement.UnitTests/Application/Locations/Queries/GetAllProviderLocationsQueryHandlerTests.cs
+++ src/SFA.DAS.Roatp.CourseManagement.UnitTests/Application/Locations/Queries/GetAllProviderLocationsQueryHandlerTests.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace SFA.DAS.Roatp.CourseManagement.UnitTests.InnerApi.Locations.Queries
+namespace SFA.DAS.Roatp.CourseManagement.UnitTests.Application.Locations.Queries
 {
     [TestFixture]
     public class GetAllProviderLocationsQueryHandlerTests
