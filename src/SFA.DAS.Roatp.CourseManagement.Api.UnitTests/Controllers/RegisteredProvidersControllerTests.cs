4109b5b2b67fd91a23ef0b32e0d7c62d2067d104
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2022-05-27 13:03:21 +0100

    CSP-117 get providers from roatpService code and coverage (#727)
    * CSP-117 get providers from roatpService code and coverage
    
    * CSP-117 moving api/ out of baseUrl
    
    * CSP-117 Upgrading dasplatform automation
    
    * CSP-117 change to route to include lookup
    
    Co-authored-by: Deven Shah <devenshah1974@gmail.com>

---
+++ src/SFA.DAS.Roatp.CourseManagement.Api.UnitTests/Controllers/RegisteredProvidersControllerTests.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Api.Controllers;
+using SFA.DAS.Roatp.CourseManagement.Application.RegisteredProviders.Queries;
+using SFA.DAS.Roatp.CourseManagement.InnerApi.Models.RegisteredProvider;
+using SFA.DAS.SharedOuterApi.Models;
+
+namespace SFA.DAS.Roatp.CourseManagement.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class RegisteredProvidersControllerTests
+    {
+
+        [Test]
+        public async Task GetAllRegisteredProviders_ReturnsAppropriateResponse()
+        {
+            var mediatorMock = new Mock<IMediator>();
+            var registeredProviders = new List<RegisteredProviderModel>();
+
+            var apiResponse = new ApiResponse<List<RegisteredProviderModel>>(registeredProviders, HttpStatusCode.OK, "");
+
+            mediatorMock.Setup(m => m.Send(It.IsAny<GetRegisteredProvidersQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(apiResponse);
+
+            var subject = new RegisteredProvidersController(Mock.Of<ILogger<RegisteredProvidersController>>(), mediatorMock.Object);
+
+            var response = await subject.GetRegisteredProviders();
+
+            var statusCodeResult = response as IStatusCodeActionResult;
+
+            var okResult = response as OkObjectResult;
+            var actualResponse = okResult.Value;
+            Assert.AreSame(apiResponse.Body, actualResponse);
+            Assert.AreEqual((int)HttpStatusCode.OK, statusCodeResult.StatusCode.GetValueOrDefault());
+        }
+
+        [TestCase(HttpStatusCode.BadRequest)]
+        [TestCase(HttpStatusCode.NotFound)]
+        [TestCase(HttpStatusCode.InternalServerError)]
+        [TestCase(HttpStatusCode.ServiceUnavailable)]
+        [TestCase(HttpStatusCode.Gone)]
+        [TestCase(HttpStatusCode.PermanentRedirect)]
+        [TestCase(HttpStatusCode.BadGateway)]
+        public async Task GetAllStandards_NonOkResponse_ReturnsErrorResponse(HttpStatusCode statusCode)
+        {
+            var errorMessage = "Error in retrieval";
+            var mediatorMock = new Mock<IMediator>();
+            var registeredProviders = new List<RegisteredProviderModel>();
+
+            var apiResponse =
+                new ApiResponse<List<RegisteredProviderModel>>(registeredProviders, statusCode,
+                    errorMessage);
+
+            mediatorMock.Setup(m => m.Send(It.IsAny<GetRegisteredProvidersQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(apiResponse);
+
+            var subject = new RegisteredProvidersController(Mock.Of<ILogger<RegisteredProvidersController>>(), mediatorMock.Object);
+
+            var response = await subject.GetRegisteredProviders();
+
+            var statusCodeResult = response as IStatusCodeActionResult;
+
+            Assert.AreEqual((int)statusCode, statusCodeResult.StatusCode.GetValueOrDefault());
+            Assert.AreEqual(errorMessage, ((ObjectResult)statusCodeResult).Value);
+        }
+    }
+}
