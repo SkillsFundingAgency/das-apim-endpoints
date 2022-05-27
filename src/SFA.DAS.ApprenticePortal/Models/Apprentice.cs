cbcb1291c08d855bca9e49e6ec3984e6e6d96a76
Chris Barnard <54770494+CodeWolf86@users.noreply.github.com>
2022-05-27 14:07:25 +0100

    QF-235 Remove private beta (#726)
    * Remove Portal Private Beta
    
    * Remove Beta from Apprentice Account
    
    * Remove private beta from feedback api
    
    * Update response object for feedback target
    
    * Update build tag
    
    * Update build ref tag

--- src/SFA.DAS.ApprenticePortal/Models/Apprentice.cs
+++ src/SFA.DAS.ApprenticePortal/Models/Apprentice.cs
@@ -8,6 +8,5 @@ namespace SFA.DAS.ApprenticePortal.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public bool TermsOfUseAccepted { get; set; }
-        public bool IsPrivateBetaUser { get; set; }
     }
 }
