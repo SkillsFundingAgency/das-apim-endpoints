f088ee988e392b2e11adacebbd3f84086c8beb55
Deven Shah <devenshah1974@gmail.com>
2022-06-01 09:45:41 +0100

    CSP-19 Added pass through endpoint for updating course contact details (#728)
    * CSP-19 Added pass through endpoint for updating course contact details
    
    * CSP-19 Fixed route parameter case for swagger

---
+++ src/SFA.DAS.Roatp.CourseManagement.Api/Controllers/ProviderCourseEditController.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Application.Standards.Commands.UpdateContactDetails;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Api.Controllers
+{
+    [ApiController]
+    public class ProviderCourseEditController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProviderCourseEditController> _logger;
+
+        public ProviderCourseEditController(IMediator mediator, ILogger<ProviderCourseEditController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("providers/{ukprn}/courses/{larsCode}/update-contact-details")]
+        public async Task<IActionResult> UpdateProviderCourseContactDetails(int ukprn, int larsCode, UpdateContactDetailsCommand command)
+        {
+            _logger.LogInformation("Outer API: Request to update course contact details for ukprn: {ukprn} larscode: {larscode}", ukprn, larsCode);
+            command.Ukprn = ukprn;
+            command.LarsCode = larsCode;
+            var httpStatusCode = await _mediator.Send(command);
+            if (httpStatusCode != HttpStatusCode.NoContent)
+            {
+                _logger.LogError("Outer API: Failed request to update course contact details for ukprn: {ukprn} larscode: {larscode} with HttpStatusCode: {httpstatuscode}", ukprn, larsCode, httpStatusCode);
+                return new StatusCodeResult((int)httpStatusCode);
+            }
+            return NoContent();
+        }
+    }
+}
--- src/SFA.DAS.SharedOuterApi/Services/RoatpCourseManagementApiClient.cs
+++ src/SFA.DAS.SharedOuterApi/Services/RoatpCourseManagementApiClient.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
 using System.Threading.Tasks;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Interfaces;
@@ -6,6 +8,7 @@ using SFA.DAS.SharedOuterApi.Models;
 
 namespace SFA.DAS.SharedOuterApi.Services
 {
+    [ExcludeFromCodeCoverage]
     public class RoatpCourseManagementApiClient : IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>
     {
         private readonly IInternalApiClient<RoatpV2ApiConfiguration> _apiClient;
@@ -14,6 +17,7 @@ namespace SFA.DAS.SharedOuterApi.Services
         {
             _apiClient = apiClient;
         }
+
         public Task<TResponse> Get<TResponse>(IGetApiRequest request)
         {
             return _apiClient.Get<TResponse>(request);
@@ -27,6 +31,58 @@ namespace SFA.DAS.SharedOuterApi.Services
         public Task<ApiResponse<TResponse>> GetWithResponseCode<TResponse>(IGetApiRequest request)
         {
             return _apiClient.GetWithResponseCode<TResponse>(request);
+        }
+
+        public async Task<ApiResponse<TResponse>> PostWithResponseCode<TResponse>(IPostApiRequest request)
+        {
+            return await _apiClient.PostWithResponseCode<TResponse>(request);
+        }
+
+
+
+        public Task Delete(IDeleteApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<TResponse>> GetAll<TResponse>(IGetAllApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PagedResponse<TResponse>> GetPaged<TResponse>(IGetPagedApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Patch<TData>(IPatchApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> PatchWithResponseCode<TData>(IPatchApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TResponse> Post<TResponse>(IPostApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Post<TData>(IPostApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Put(IPutApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Put<TData>(IPutApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
