9c666f023553e0ad74e227b8f7807f40d8fa2154
Deven Shah <devenshah1974@gmail.com>
2022-06-07 19:31:51 +0100

    CSP-92 Implements endpoint to get course directory data

--- src/SFA.DAS.Roatp.CourseManagement.Api/AppStart/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.Roatp.CourseManagement.Api/AppStart/ServiceCollectionExtensions.cs
@@ -3,16 +3,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Api.Common.Infrastructure;
 using SFA.DAS.Api.Common.Interfaces;
+using SFA.DAS.Roatp.CourseManagement.Api.Configuration;
+using SFA.DAS.Roatp.CourseManagement.Services;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Infrastructure;
 using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.SharedOuterApi.Services;
+using System;
 
 namespace SFA.DAS.Roatp.CourseManagement.Api.AppStart
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddServiceRegistration(this IServiceCollection services)
+        public static void AddServiceRegistration(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddHttpClient();
             services.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
@@ -20,6 +23,7 @@ namespace SFA.DAS.Roatp.CourseManagement.Api.AppStart
             services.AddTransient<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>, RoatpCourseManagementApiClient>();
             services.AddTransient<IRoatpServiceApiClient<RoatpConfiguration>, RoatpServiceApiClient>();
             services.AddTransient<ICoursesApiClient<CoursesApiConfiguration>, CourseApiClient>();
+            ConfigureCourseDirectoryHttpClient(services, configuration);
         }
 
         public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
@@ -32,6 +36,22 @@ namespace SFA.DAS.Roatp.CourseManagement.Api.AppStart
             services.Configure<RoatpConfiguration>(configuration.GetSection(nameof(RoatpConfiguration)));
             services.AddSingleton(cfg => cfg.GetService<IOptions<RoatpConfiguration>>().Value);
 
+        }
+
+        private static void ConfigureCourseDirectoryHttpClient(IServiceCollection services, IConfiguration configuration)
+        {
+            var handlerLifeTime = TimeSpan.FromMinutes(5);
+            services.AddHttpClient<ICourseDirectoryService, CourseDirectoryService>(config =>
+            {
+                var apiconfiguration = configuration
+                    .GetSection(nameof(CourseDirectoryConfiguration))
+                    .Get<CourseDirectoryConfiguration>();
+
+                config.BaseAddress = new Uri(apiconfiguration.Url, UriKind.Absolute);
+                config.DefaultRequestHeaders.Add("Accept", "application/json");
+                config.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", apiconfiguration.Key);
+            })
+           .SetHandlerLifetime(handlerLifeTime);
         }
     }
 }
