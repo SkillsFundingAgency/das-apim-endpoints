74eea598249083ce3086212902ddb23691c72716
David-B-Read <david.read@digital.education.gov.uk>
2022-06-16 15:04:38 +0100

    EI-1851 create revert payments api endpoint

---
+++ src/SFA.DAS.EmployerIncentives/InnerApi/Requests/PostRevertPaymentsRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerIncentives.InnerApi.Requests
+{
+    public class PostRevertPaymentsRequest : IPostApiRequest
+    {
+        public PostRevertPaymentsRequest(RevertPaymentsRequest request)
+        {
+            Data = request;
+        }
+
+        public string PostUrl => $"revert-payments";
+        public object Data { get; set; }
+    }
+
+    public class RevertPaymentsRequest
+    {
+        public List<Guid> Payments { get; set; }
+        public ServiceRequest ServiceRequest { get; set; }
+    }
+}
