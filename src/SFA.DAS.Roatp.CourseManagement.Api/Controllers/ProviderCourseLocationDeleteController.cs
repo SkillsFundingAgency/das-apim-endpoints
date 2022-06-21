cbae279cae767c7fc5bc06a8d0adb7e3f1c2b5de
Deven Shah <devenshah1974@gmail.com>
2022-06-21 11:32:29 +0100

    CSP-81 Implements bulk delete endpoint for provider course locations (#746)
    * CSP-81 Implemented Delete method on the inner api client
    
    * CSP-81 Added delete provider course location option enum
    
    * CSP-81 Added bulk delete provider course location command
    
    * CSP-81 Added endpoint to bulk delete provider course location

---
+++ src/SFA.DAS.Roatp.CourseManagement.Api/Controllers/ProviderCourseLocationDeleteController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Application.Standards.Commands.BulkDeleteProviderCourse;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.Roatp.CourseManagement.Api.Controllers
+{
+    [ApiController]
+    public class ProviderCourseLocationDeleteController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProviderCourseLocationDeleteController> _logger;
+
+        public ProviderCourseLocationDeleteController(ILogger<ProviderCourseLocationDeleteController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("providers/{ukprn}/courses/{larsCode}/bulk-delete-course-locations")]
+        public async Task<IActionResult> BulkDeleteProviderCourseLocations(int ukprn, int larsCode, BulkDeleteProviderCourseLocationsCommand command)
+        {
+            _logger.LogInformation("Outer API: Request received to bulk delete provider course locations for ukprn: {ukprn} larscode: {larscode} option: {deleteOption}", ukprn, larsCode, command.DeleteProviderCourseLocationOption);
+            command.Ukprn = ukprn;
+            command.LarsCode = larsCode;
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
---
+++ src/SFA.DAS.Roatp.CourseManagement/InnerApi/Models/DeleteProviderCourseLocations/DeleteProviderCourseLocationOption.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.Roatp.CourseManagement.InnerApi.Models.DeleteProviderCourseLocations
+{
+    public enum DeleteProviderCourseLocationOption
+    {
+        DeleteProviderLocations,
+        DeleteEmployerLocations
+    }
+}
