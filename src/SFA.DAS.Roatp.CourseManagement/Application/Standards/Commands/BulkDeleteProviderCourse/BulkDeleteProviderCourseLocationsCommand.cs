00e7a371908584fe2c169b84afbb13ba664442f8
Deven Shah <devenshah1974@gmail.com>
2022-06-21 01:33:57 +0100

    CSP-81 Added bulk delete provider course location command

---
+++ src/SFA.DAS.Roatp.CourseManagement/Application/Standards/Commands/BulkDeleteProviderCourse/BulkDeleteProviderCourseLocationsCommand.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using SFA.DAS.Roatp.CourseManagement.InnerApi.Models.DeleteProviderCourseLocations;
+using SFA.DAS.Roatp.CourseManagement.InnerApi.Requests;
+
+namespace SFA.DAS.Roatp.CourseManagement.Application.Standards.Commands.BulkDeleteProviderCourse
+{
+    public class BulkDeleteProviderCourseLocationsCommand : IRequest<Unit>
+    {
+        public int Ukprn { get; set; }
+        public int LarsCode { get; set; }
+
+        public string UserId { get; set; }
+        public DeleteProviderCourseLocationOption DeleteProviderCourseLocationOption { get; set; }
+
+        public static implicit operator BulkDeleteProviderCourseLocationsRequest(BulkDeleteProviderCourseLocationsCommand command) 
+            => new BulkDeleteProviderCourseLocationsRequest
+            {
+                DeleteProviderCourseLocationOption = command.DeleteProviderCourseLocationOption,
+                UserId = command.UserId,
+                LarsCode = command.LarsCode,
+                Ukprn = command.Ukprn
+            };
+    }
+}
