2ae52225622703d26b21c0c08e49ddaf4788fa40
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-07-07 20:05:07 +0100

    CSP-135 added get provider course locations end point

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderCourseLocationController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.RoatpCourseManagement.Application.Standards.Queries.GetProviderCourseLocation;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers
+{
+    [ApiController]
+    public class ProviderCourseLocationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProviderCourseLocationController> _logger;
+
+        public ProviderCourseLocationController(ILogger<ProviderCourseLocationController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("providers/{ukprn}/courses/{larsCode}/provider-course-locations")]
+        public async Task<IActionResult> GetProviderCourseLocations(int ukprn, int larsCode)
+        {
+            _logger.LogInformation("Outer API: Request received to get provider course locations for ukprn: {ukprn} larscode: {larscode}", ukprn, larsCode);
+            var providerCourselocationsResult = await _mediator.Send(new GetProviderCourseLocationQuery(ukprn, larsCode));
+            if (providerCourselocationsResult == null)
+            {
+                _logger.LogError($"Provider Course Locations not found for ukprn {ukprn} and lars code {larsCode}");
+                return NotFound();
+            }
+
+            return Ok(providerCourselocationsResult);
+        }
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Standards/Queries/GetProviderCourseLocation/GetProviderCourseLocationResult.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.RoatpCourseManagement.InnerApi.Models;
+using System.Collections.Generic;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.Standards.Queries.GetProviderCourseLocation
+{
+    public class GetProviderCourseLocationResult
+    {
+        public List<ProviderCourseLocationModel> ProviderCourseLocations { get; set; } = new List<ProviderCourseLocationModel>();
+    }
+}
