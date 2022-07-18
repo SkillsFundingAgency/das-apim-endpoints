6c4b0625fa9b33d40578c34e7adcbece6c33063d
Deven Shah <devenshah1974@gmail.com>
2022-07-18 08:24:56 +0100

    CSP-48 Implements handler for addresses lookup

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/AddressLookup/AddressLookupQueryHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.AddressLookup;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Threading;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.AddressLookup
+{
+    [TestFixture]
+    public class AddressLookupQueryHandlerTests
+    {
+        [Test]
+        public void Handle_CallsLocationService()
+        {
+            var postcode = "CV1 1ET";
+            var mockService = new Mock<ILocationLookupService>();
+            var sut = new AddressLookupQueryHandler(mockService.Object);
+
+            sut.Handle(new AddresssLookupQuery(postcode), new CancellationToken());
+
+            mockService.Verify(m => m.GetExactMatchAddresses(postcode));
+        }
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/AddressLookupQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.AddressLookup
+{
+    public class AddresssLookupQuery : IRequest<GetAddressesListResponse>
+    {
+        public string Postcode { get; }
+        public AddresssLookupQuery(string postcode)
+        {
+            Postcode = postcode;
+        }
+    }
+}
