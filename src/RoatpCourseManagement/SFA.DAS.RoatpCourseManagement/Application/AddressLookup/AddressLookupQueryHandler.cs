ae1b4b44cb81cf503fb705e4fe9d9709fdbed8a8
Deven Shah <devenshah1974@gmail.com>
2022-07-18 08:24:56 +0100

    CSP-48 Implements handler for addresses lookup

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/AddressLookupQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.AddressLookup
+{
+    public class AddressLookupQueryHandler : IRequestHandler<AddresssLookupQuery, GetAddressesListResponse>
+    {
+        private readonly ILocationLookupService _locationLookupService;
+
+        public AddressLookupQueryHandler(ILocationLookupService locationLookupService)
+        {
+            _locationLookupService = locationLookupService;
+        }
+
+        public Task<GetAddressesListResponse> Handle(AddresssLookupQuery request, CancellationToken cancellationToken)
+        {
+            return _locationLookupService.GetExactMatchAddresses(request.Postcode);
+        }
+    }
+}
