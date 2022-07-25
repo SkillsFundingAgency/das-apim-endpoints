bf0a3c280591696f9e8abde8c07ec4174b1afef9
Ian Rose <ian.rose@digital.education.gov.uk>
2022-07-25 14:52:14 +0100

    QF429 Feedback email transactions

---
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback.Api/Controllers/ApprenticeFeedbackEmailTransactionController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ApprenticeFeedback.Application.Commands.GenerateEmailTransaction;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeFeedback.Api.Controllers
+{
+    [ApiController]
+    [Route("FeedbackTransaction")]
+    public class ApprenticeFeedbackEmailTransactionController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ApprenticeFeedbackEmailTransactionController> _logger;
+
+        public ApprenticeFeedbackEmailTransactionController(IMediator mediator, ILogger<ApprenticeFeedbackEmailTransactionController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GenerateEmailTransactionResponse>> GenerateEmailTransaction(GenerateEmailTransactionCommand request)
+            => await _mediator.Send(request);
+
+    }
+}
