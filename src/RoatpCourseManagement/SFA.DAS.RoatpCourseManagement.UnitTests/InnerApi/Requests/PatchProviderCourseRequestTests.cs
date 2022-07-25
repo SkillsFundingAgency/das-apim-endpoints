5a34986f300f68bac18098b946d9c6041fab243e
MarkFCain <mark.cain@digital.education.gov.uk>
2022-07-25 16:06:45 +0100

    CSP-150 added full coverage

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/InnerApi/Requests/PatchProviderCourseRequestTests.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/InnerApi/Requests/PatchProviderCourseRequestTests.cs
@@ -1,7 +1,9 @@
 ﻿using System.Linq;
 using AutoFixture.NUnit3;
 using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
 
 namespace SFA.DAS.RoatpCourseManagement.UnitTests.InnerApi.Requests
@@ -20,6 +22,42 @@ namespace SFA.DAS.RoatpCourseManagement.UnitTests.InnerApi.Requests
             Assert.IsTrue(request.Data.Any(x => x.Path == "StandardInfoUrl"));
             Assert.IsTrue(request.Data.Any(x => x.Path == "IsApprovedByRegulator"));
             request.PatchUrl.Should().Be($"providers/{data.Ukprn}/courses/{data.LarsCode}");
+        }
+
+        [TestCase("test@test.com","1234567890","http://www.google.com/contact.us","http://www.google.com",null,4)]
+        [TestCase("test@test.com", "1234567890", "http://www.google.com/contact.us", null, null, 3)]
+        [TestCase("test@test.com", "1234567890", null, "http://www.google.com", null, 3)]
+        [TestCase("test@test.com", null, "http://www.google.com/contact.us", "http://www.google.com", null, 3)]
+        [TestCase(null, "1234567890", "http://www.google.com/contact.us", "http://www.google.com", null, 3)]
+        [TestCase(null, null, "http://www.google.com/contact.us", "http://www.google.com", null, 2)]
+        [TestCase(null, "1234567890", null, "http://www.google.com", null, 2)]
+        [TestCase(null, "1234567890", "http://www.google.com/contact.us", null, null, 2)]
+        [TestCase("test@test.com", null, null, "http://www.google.com", null, 2)]
+        [TestCase("test@test.com", null, "http://www.google.com/contact.us", null, null, 2)]
+        [TestCase("test@test.com", "1234567890", null, null, null, 2)]
+        [TestCase("test@test.com", null, null, null, null, 1)] 
+        [TestCase(null, "1234567890", null, null, null, 1)] 
+        [TestCase(null, null, "http://www.google.com/contact.us", null, null, 1)]
+        [TestCase(null, null, null, "http://www.google.com", null, 1)]
+        [TestCase(null, null, null, null, true, 1)]
+        public void Request_BuildDataPatchFromModel( string contactUsEmail, string contactUsPhoneNumber, string contactUsPageUrl, string standardInfoUrl, bool? isApprovedByRegulator, int numberOfPatches)
+        {
+            var model = new ProviderCourseUpdateModel
+            {
+                ContactUsEmail = contactUsEmail,
+                ContactUsPageUrl = contactUsPageUrl,
+                ContactUsPhoneNumber = contactUsPhoneNumber,
+                StandardInfoUrl = standardInfoUrl,
+                IsApprovedByRegulator = isApprovedByRegulator
+            };
+            var request = new PatchProviderCourseRequest(model);
+
+             Assert.AreEqual(numberOfPatches, request.Data.Count);
+             Assert.AreEqual(request.Data.Any(x => x.Path == "ContactUsEmail"), contactUsEmail!=null);
+             Assert.AreEqual(request.Data.Any(x => x.Path == "ContactUsPageUrl"), contactUsPageUrl != null);
+             Assert.AreEqual(request.Data.Any(x => x.Path == "StandardInfoUrl"), standardInfoUrl != null);
+             Assert.AreEqual(request.Data.Any(x => x.Path == "ContactUsPhoneNumber"), contactUsPhoneNumber != null);
+             Assert.AreEqual(request.Data.Any(x => x.Path == "IsApprovedByRegulator"), isApprovedByRegulator != null);
         }
     }
 }
--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Standards/Commands/UpdateApprovedByRegulator/UpdateApprovedByRegulatorCommandHandler.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Standards/Commands/UpdateApprovedByRegulator/UpdateApprovedByRegulatorCommandHandler.cs
@@ -18,21 +18,6 @@ namespace SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.UpdateApp
 
         public async Task<Unit> Handle(UpdateApprovedByRegulatorCommand command, CancellationToken cancellationToken)
         {
-            // var providerCourse = await _innerApiClient.Get<GetProviderCourseResponse>(new GetProviderCourseRequest(command.Ukprn, command.LarsCode));
-            // var updateProviderCourse = new ProviderCourseUpdateModel
-            // {
-            //     Ukprn = command.Ukprn,
-            //     LarsCode = command.LarsCode,
-            //     UserId = command.UserId,
-            //     ContactUsEmail = providerCourse.ContactUsEmail,
-            //     ContactUsPhoneNumber = providerCourse.ContactUsPhoneNumber,
-            //     ContactUsPageUrl = providerCourse.ContactUsPageUrl,
-            //     StandardInfoUrl = providerCourse.StandardInfoUrl,
-            //     IsApprovedByRegulator = command.IsApprovedByRegulator
-            // };
-            // var request = new ProviderCourseUpdateRequest(updateProviderCourse);
-            // await _innerApiClient.Put(request);
-
             var patchUpdateProviderCourse = new ProviderCourseUpdateModel
             {
                 Ukprn = command.Ukprn,
