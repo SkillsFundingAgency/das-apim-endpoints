4efbfa091c05db56ceb64905acc74d465d26dc75
Deven Shah <devenshah1974@gmail.com>
2022-07-25 14:29:24 +0100

    CSP-14 Implements post endpoint to create provider location

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/Queries/AddressLookupQuery.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/Queries/AddressLookupQuery.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 
 namespace SFA.DAS.RoatpCourseManagement.Application.AddressLookup.Queries
 {
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Locations/Commands/CreateProviderLocation/CreateProviderLocationCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.CreateProviderLocation
+{
+    public class CreateProviderLocationCommand : IRequest<Unit>
+    {
+        public int Ukprn { get; set; }
+        public string UserId { get; set; }
+        public string LocationName { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
+        public string County { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Phone { get; set; }
+    }
+}
