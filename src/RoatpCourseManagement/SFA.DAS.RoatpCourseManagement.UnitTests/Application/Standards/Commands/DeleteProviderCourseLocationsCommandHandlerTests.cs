09bd03875d199ba10280e39f7b41b2c50756bc5c
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2022-07-28 12:59:48 +0100

    CSP-135-outer-RemoveTraininglocation (#784)
    CSP-135-outer-RemoveTraininglocation

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Standards/Commands/DeleteProviderCourseLocationsCommandHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.DeleteProviderCourseLocation;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.Standards.Commands
+{
+    [TestFixture]
+    public class DeleteProviderCourseLocationsCommandHandlerTests
+    {
+        [Test, MoqAutoData]
+        public async Task Handle_InvokedDeleteOnApiClient(
+            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+            DeleteProviderCourseLocationsCommandHandler sut,
+            DeleteProviderCourseLocationCommand command)
+        {
+            DeleteProviderCourseLocationRequest expectedRequest = command;
+            await sut.Handle(command, new CancellationToken());
+
+            apiClientMock.Verify(c => c.Delete(It.Is<DeleteProviderCourseLocationRequest>(r => r.Id == command.Id && r.Ukprn == command.Ukprn && r.LarsCode == command.LarsCode)));
+        }
+
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/DeleteProviderCourseLocationRequest.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Web;
+
+namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests
+{
+    public class DeleteProviderCourseLocationRequest : IDeleteApiRequest
+    {
+        public int Ukprn { get; set; }
+        public int LarsCode { get; set; }
+        public int Id { get; set; }
+        public string UserId { get; set; }
+
+        public string DeleteUrl => $"/providers/{Ukprn}/courses/{LarsCode}/location/{Id}?userId={HttpUtility.UrlEncode(UserId)}";
+    }
+}
