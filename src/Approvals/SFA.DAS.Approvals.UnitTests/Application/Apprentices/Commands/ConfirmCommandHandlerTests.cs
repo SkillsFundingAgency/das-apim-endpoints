e97f53b3d63fcdd45fc97359930d6ab16bb92c68
codewolf86 <chris@codewolfconsulting.com>
2022-07-28 16:52:19 +0100

    Fix unit tests and Enum Converter

--- src/Approvals/SFA.DAS.Approvals.UnitTests/Application/Apprentices/Commands/ConfirmCommandHandlerTests.cs
+++ src/Approvals/SFA.DAS.Approvals.UnitTests/Application/Apprentices/Commands/ConfirmCommandHandlerTests.cs
@@ -31,7 +31,7 @@ namespace SFA.DAS.Approvals.UnitTests.Application.Apprentices.Commands
 
             _commitmentsApiClient = new Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>>();
             _commitmentsApiClient.Setup(x => x.PostWithResponseCode<CreateChangeOfPartyRequestResponse>(It.IsAny<CreateChangeOfPartyRequestRequest>(), true))
-                .Callback<IPostApiRequest>(request => _apiRequest = request as CreateChangeOfPartyRequestRequest)
+                .Callback<IPostApiRequest, bool>((request, withResponse) => _apiRequest = request as CreateChangeOfPartyRequestRequest)
                 .ReturnsAsync(new ApiResponse<CreateChangeOfPartyRequestResponse>(null, HttpStatusCode.OK, string.Empty));
 
             _handler = new ConfirmCommandHandler(_commitmentsApiClient.Object);
