03dac58aa8c241ed3354b6441f84eac0d52fd69e
Deven Shah <devenshah1974@gmail.com>
2022-08-01 22:29:28 +0100

    CSP-48 Handle bad response from inner api

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Locations/Commands/CreateProviderLocation/CreateProviderLocationCommandHandlerTests.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Locations/Commands/CreateProviderLocation/CreateProviderLocationCommandHandlerTests.cs
@@ -1,11 +1,15 @@
 ﻿using AutoFixture.NUnit3;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.CreateProviderLocation;
 using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,6 +26,19 @@ namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.Locations.Commands
         {
             await sut.Handle(command, new CancellationToken());
             apiClientMock.Verify(c => c.PostWithResponseCode<int>(It.Is<ProviderLocationCreateRequest>(r => r.Ukprn == command.Ukprn && r.Data == command), true));
+        }
+
+        [Test, MoqAutoData]
+        public async Task Handle_ResponseNotSuccessful_ThrowsException(
+            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+            CreateProviderLocationCommandHandler sut,
+            CreateProviderLocationCommand command)
+        {
+            var response = new ApiResponse<int>(0, HttpStatusCode.InternalServerError, string.Empty);
+            apiClientMock.Setup(c => c.PostWithResponseCode<int>(It.Is<ProviderLocationCreateRequest>(r => r.Ukprn == command.Ukprn && r.Data == command), true)).ReturnsAsync(response);
+
+            Func<Task> action = () => sut.Handle(command, new CancellationToken());
+            await action.Should().ThrowAsync<InvalidOperationException>();
         }
     }
 }
--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Locations/Commands/CreateProviderLocation/CreateProviderLocationCommandHandler.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Locations/Commands/CreateProviderLocation/CreateProviderLocationCommandHandler.cs
@@ -1,7 +1,10 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,16 +13,23 @@ namespace SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.CreatePro
     public class CreateProviderLocationCommandHandler : IRequestHandler<CreateProviderLocationCommand, Unit>
     {
         private readonly IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> _courseManagementApiClient;
+        private readonly ILogger<CreateProviderLocationCommandHandler> _logger;
 
-        public CreateProviderLocationCommandHandler(IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> courseManagementApiClient)
+        public CreateProviderLocationCommandHandler(IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> courseManagementApiClient, ILogger<CreateProviderLocationCommandHandler> logger)
         {
             _courseManagementApiClient = courseManagementApiClient;
+            _logger = logger;
         }
 
         public async Task<Unit> Handle(CreateProviderLocationCommand request, CancellationToken cancellationToken)
         {
             var apiRequest = new ProviderLocationCreateRequest(request);
-            await _courseManagementApiClient.PostWithResponseCode<int>(apiRequest);
+            var response = await _courseManagementApiClient.PostWithResponseCode<int>(apiRequest);
+            if (response.StatusCode != HttpStatusCode.Created)
+            {
+                _logger.LogError("Create provider location name :{locationname} for ukprn: {ukprn} did not come back with successful response", request.LocationName, request.Ukprn);
+                throw new InvalidOperationException($"Create provider location did not come back with successful response for ukprn{request.Ukprn}");
+            }
             return Unit.Value;
         }
     }
