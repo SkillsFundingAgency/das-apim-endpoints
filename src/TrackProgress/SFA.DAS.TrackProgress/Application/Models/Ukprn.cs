b955a77de6235f3a9776f6af1a28aca375fd91f4
Ben Arnold <ben@arnoldfamily.me.uk>
2022-08-01 14:57:46 +0100

    Add nascent track progress endpoint

---
+++ src/TrackProgress/SFA.DAS.TrackProgress/Application/Models/Ukprn.cs
@@ -0,0 +1,15 @@
+﻿namespace SFA.DAS.TrackProgress.Application.Models;
+
+public record Ukprn
+{
+	public long Value { get; set; }
+
+	public override string ToString() => $"UKPRN {Value}";
+
+	public static Ukprn? Parse(string input)
+	{
+		if (!long.TryParse(input, out var value)) return null;
+		if (value < 1) return null;
+		return new() { Value = value };
+	}
+}
