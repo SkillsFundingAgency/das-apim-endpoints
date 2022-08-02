523e9fd60fa46bc93d9c1cffcb790dc7190f7ef8
Deven Shah <devenshah1974@gmail.com>
2022-08-02 13:14:17 +0100

    CSP-48 Implements address lookup endpoint (#781)
    * CSP-48 Extended location lookup service to get addresses
    
    * CSP-48 Implements handler for addresses lookup
    
    * CSP-48 Implements address lookup endpoint
    
    * CSP-48 Refactored Address lookup to return specific response.
    
    * CSP-48 Fixed error in tests
    
    * CSP-48 Renamed result class to keep it consistent
    
    * CSP-48 Moved address query under queries folder
    
    * CSP-14 Implements post endpoint to create provider location
    
    * CSP-48 Fixed service to throw exception on unsuccessful response
    
    * CSP-48 Added tests on null check in address lookup handler
    
    * CSP-48 Minor fix post newstonsoft json overall fix
    
    * CSP-48 fix for update sub region flow due to changes in PostWithResponseCode method
    
    * CSP-48 unit tests fixed
    
    * CSP-48 Handle bad response from inner api
    
    * CSP-48 Fixed failing tests
    
    Co-authored-by: Ravi Udari <ravi.udari@digital.education.gov.uk>

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Locations/Commands/CreateProviderLocation/CreateProviderLocationCommandHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.CreateProviderLocation;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.Locations.Commands.CreateProviderLocation
+{
+    [TestFixture]
+    public class CreateProviderLocationCommandHandlerTests
+    {
+        [Test, MoqAutoData]
+        public async Task Handle_InvokesApiCall(
+            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+            CreateProviderLocationCommandHandler sut,
+            CreateProviderLocationCommand command)
+        {
+            var response = new ApiResponse<int>(1, HttpStatusCode.Created, string.Empty);
+            apiClientMock.Setup(c => c.PostWithResponseCode<int>(It.Is<ProviderLocationCreateRequest>(r => r.Ukprn == command.Ukprn && r.Data == command), true)).ReturnsAsync(response);
+
+            await sut.Handle(command, new CancellationToken());
+            apiClientMock.Verify(c => c.PostWithResponseCode<int>(It.Is<ProviderLocationCreateRequest>(r => r.Ukprn == command.Ukprn && r.Data == command), true));
+        }
+
+        [Test, MoqAutoData]
+        public async Task Handle_ResponseNotSuccessful_ThrowsException(
+            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+            CreateProviderLocationCommandHandler sut,
+            CreateProviderLocationCommand command)
+        {
+            var response = new ApiResponse<int>(0, HttpStatusCode.InternalServerError, string.Empty);
+            apiClientMock.Setup(c => c.PostWithResponseCode<int>(It.Is<ProviderLocationCreateRequest>(r => r.Ukprn == command.Ukprn && r.Data == command), true)).ReturnsAsync(response);
+
+            Func<Task> action = () => sut.Handle(command, new CancellationToken());
+
+            await action.Should().ThrowAsync<InvalidOperationException>();
+            apiClientMock.Verify(c => c.PostWithResponseCode<int>(It.Is<ProviderLocationCreateRequest>(r => r.Ukprn == command.Ukprn && r.Data == command), true));
+        }
+    }
+}
