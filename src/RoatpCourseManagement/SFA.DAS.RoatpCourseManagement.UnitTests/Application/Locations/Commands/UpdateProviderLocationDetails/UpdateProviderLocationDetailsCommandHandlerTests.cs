aed2ed051920d6e89e69a18fb5e50e66e96da9af
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-03 12:18:53 +0100

    CSP-165 added unit tests

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Locations/Commands/UpdateProviderLocationDetails/UpdateProviderLocationDetailsCommandHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.UpdateProviderLocationDetails;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.Locations.Commands.UpdateProviderLocationDetails
+{
+    [TestFixture]
+    public class UpdateProviderLocationDetailsCommandHandlerTests
+    {
+        [Test, MoqAutoData]
+        public async Task Handle_CallsApiClient(
+            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+            UpdateProviderLocationDetailsCommandHandler sut,
+            UpdateProviderLocationDetailsCommand command,
+            CancellationToken cancellationToken)
+        {
+            await sut.Handle(command, cancellationToken);
+
+            apiClientMock.Verify(a => a.Put(It.IsAny<ProviderLocationUpdateRequest>()), Times.Once);
+        }
+
+        [Test, MoqAutoData]
+        public void Handle_CallsApiClient_ReturnException(
+            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+            UpdateProviderLocationDetailsCommandHandler sut,
+            UpdateProviderLocationDetailsCommand command,
+            CancellationToken cancellationToken,
+            HttpRequestContentException expectedException)
+        {
+            apiClientMock.Setup(c => c.Put(It.IsAny<ProviderLocationUpdateRequest>())).Throws(expectedException);
+
+            var actualException = Assert.ThrowsAsync<HttpRequestContentException>(() => sut.Handle(command, cancellationToken));
+
+            actualException.Should().Be(expectedException);
+        }
+    }
+}
