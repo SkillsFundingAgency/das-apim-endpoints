dcf4a74646294be574f5f5803d5c70b6ed445d33
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-04 16:01:53 +0100

    CSP-165 review comment for extra coverage

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/InnerApi/Requests/ProviderLocationUpdateRequestTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.InnerApi.Requests
+{
+    [TestFixture]
+    public class ProviderLocationUpdateRequestTests
+    {
+        [Test, AutoData]
+        public void Constructor_ConstructsRequest(ProviderLocationUpdateModel data)
+        {
+            var request = new ProviderLocationUpdateRequest(data);
+
+            request.Data.Should().Be(data);
+            request.Ukprn.Should().Be(data.Ukprn);
+            request.Id.Should().Be(data.Id);
+            request.PutUrl.Should().Be($"providers/{data.Ukprn}/locations/{data.Id}/");
+        }
+    }
+}
