66c7b05423e9bd0c76f12cc9a6b28a423f69cccd
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-09 15:47:51 +0100

    CSP-184- Add create providercourselocation endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Standards/Commands/AddProviderCourseLocation/AddProviderCourseLocationCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.CreateProviderLocation
+{
+    public class AddProviderCourseLocationCommandHandler : IRequestHandler<AddProviderCourseLocationCommand, Unit>
+    {
+        private readonly IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> _courseManagementApiClient;
+        private readonly ILogger<AddProviderCourseLocationCommandHandler> _logger;
+
+        public AddProviderCourseLocationCommandHandler(IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> courseManagementApiClient, ILogger<AddProviderCourseLocationCommandHandler> logger)
+        {
+            _courseManagementApiClient = courseManagementApiClient;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(AddProviderCourseLocationCommand request, CancellationToken cancellationToken)
+        {
+            var apiRequest = new ProviderCourseLocationCreateRequest(request);
+            var response = await _courseManagementApiClient.PostWithResponseCode<int>(apiRequest);
+            if (response.StatusCode != HttpStatusCode.Created)
+            {
+                _logger.LogError("Create provider course location name :{locationname} for ukprn: {ukprn} larsCode: {larsCode} did not come back with successful response", request.LocationName, request.Ukprn, request.LarsCode);
+                throw new InvalidOperationException($"Create provider course location did not come back with successful response for ukprn{request.Ukprn} larsCode{request.LarsCode}");
+            }
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/ProviderCourseLocationCreateRequest.cs
@@ -0,0 +1,19 @@
+﻿using SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.CreateProviderLocation;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests
+{
+    public class ProviderCourseLocationCreateRequest : IPostApiRequest
+    {
+        public readonly int Ukprn;
+        public readonly int LarsCode;
+        public string PostUrl => $"providers/{Ukprn}/courses/{LarsCode}/create-providercourselocation";
+        public object Data { get; set; }
+        public ProviderCourseLocationCreateRequest(AddProviderCourseLocationCommand data)
+        {
+            Ukprn = data.Ukprn;
+            LarsCode = data.LarsCode;
+            Data = data;
+        }
+    }
+}
