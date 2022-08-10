27fd9c8309e87dda6290aa2f3a64547f2b7927d4
Ian Rose <94618382+ianatesfa@users.noreply.github.com>
2022-08-10 13:40:46 +0100

    QF429 Feedback email transactions (#787)
    * QF429 Feedback email transactions
    
    * QF429 Changes for PR comments

---
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback.Api.UnitTests/Controllers/WhenPostingFeedbackTransaction.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using FluentAssertions;
+using MediatR;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeFeedback.Api.Controllers;
+using SFA.DAS.ApprenticeFeedback.Application.Commands.GenerateEmailTransaction;
+using SFA.DAS.Testing.AutoFixture;
+
+
+namespace SFA.DAS.ApprenticeFeedback.Api.UnitTests.Controllers
+{
+    public class WhenPostingFeedbackTransaction
+    {
+        private Mock<IMediator> _mockMediator;
+
+        private FeedbackTransactionController _controller;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _mockMediator = new Mock<IMediator>();
+            _controller = new FeedbackTransactionController(_mockMediator.Object, 
+                                                            Mock.Of<ILogger<FeedbackTransactionController>>());
+        }
+
+        [Test, MoqAutoData]
+        public async Task And_CommandIsProcessedSuccessfully_Then_ReturnResults()
+        {
+            var result = await _controller.GenerateEmailTransaction();
+            result.Should().NotBeNull();
+        }
+    }
+}
---
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback/Application/Commands/GenerateEmailTransaction/GenerateEmailTransactionCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using SFA.DAS.ApprenticeFeedback.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeFeedback.Application.Commands.GenerateEmailTransaction
+{
+    public class GenerateEmailTransactionCommandHandler : IRequestHandler<GenerateEmailTransactionCommand, GenerateEmailTransactionResponse>
+    {
+        private readonly IApprenticeFeedbackApiClient<ApprenticeFeedbackApiConfiguration> _feedbackApiClient;
+
+        public GenerateEmailTransactionCommandHandler(IApprenticeFeedbackApiClient<ApprenticeFeedbackApiConfiguration> feedbackApiClient)
+        {
+            _feedbackApiClient = feedbackApiClient;
+        }
+
+        public async Task<GenerateEmailTransactionResponse> Handle(GenerateEmailTransactionCommand command, CancellationToken cancellationToken)
+        {
+            var request = new GenerateEmailTransactionRequest(new GenerateEmailTransactionData());
+
+            var response = await _feedbackApiClient.PostWithResponseCode<GenerateEmailTransactionResponse>(request);
+
+            response.EnsureSuccessStatusCode();
+            return response.Body;
+        }
+    }
+}
