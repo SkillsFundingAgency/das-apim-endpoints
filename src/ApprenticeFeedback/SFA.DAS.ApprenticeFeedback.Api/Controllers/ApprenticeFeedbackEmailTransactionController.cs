71152354e2daa3330072bc8473ef0d008239306f
Ian Rose <94618382+ianatesfa@users.noreply.github.com>
2022-08-10 13:40:46 +0100

    QF429 Feedback email transactions (#787)
    * QF429 Feedback email transactions
    
    * QF429 Changes for PR comments

---
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback.Api/Controllers/ApprenticeFeedbackEmailTransactionController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ApprenticeFeedback.Application.Commands.GenerateEmailTransaction;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeFeedback.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class FeedbackTransactionController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<FeedbackTransactionController> _logger;
+
+        public FeedbackTransactionController(IMediator mediator, ILogger<FeedbackTransactionController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<GenerateEmailTransactionResponse>> GenerateEmailTransaction()
+            => await _mediator.Send(new GenerateEmailTransactionCommand());
+
+    }
+}
