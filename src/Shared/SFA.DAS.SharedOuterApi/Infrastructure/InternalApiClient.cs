8dc8ce4c1349e20248a13c053b6218923def08c1
Chris Woodcock <chris@humesoftware.com>
2022-08-15 15:43:43 +0100

    Added temp logging

--- src/Shared/SFA.DAS.SharedOuterApi/Infrastructure/InternalApiClient.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/Infrastructure/InternalApiClient.cs
@@ -4,19 +4,22 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using SFA.DAS.Api.Common.Interfaces;
 using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Logging;
 
 namespace SFA.DAS.SharedOuterApi.Infrastructure
 {
     public class InternalApiClient<T> : ApiClient<T>, IInternalApiClient<T> where T : IInternalApiConfiguration
     {
         private readonly IAzureClientCredentialHelper _azureClientCredentialHelper;
+        private readonly ILogger<InternalApiClient<T>> _logger;
 
         public InternalApiClient(
             IHttpClientFactory httpClientFactory,
             T apiConfiguration,
-            IAzureClientCredentialHelper azureClientCredentialHelper) : base(httpClientFactory, apiConfiguration)
+            IAzureClientCredentialHelper azureClientCredentialHelper, ILogger<InternalApiClient<T>> logger) : base(httpClientFactory, apiConfiguration)
         {
             _azureClientCredentialHelper = azureClientCredentialHelper;
+            _logger = logger;
         }
 
         protected override async Task AddAuthenticationHeader(HttpRequestMessage httpRequestMessage)
@@ -28,6 +31,8 @@ namespace SFA.DAS.SharedOuterApi.Infrastructure
                 // TEMP THIS WILL NOT BE MERGED
                 var azureServiceTokenProvider = new AzureServiceTokenProvider();
                 var accessToken = await azureServiceTokenProvider.GetAccessTokenAsync(Configuration.Identifier, true);
+
+                _logger.LogInformation($"Bearer={accessToken}");
 
                 httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);               
             }
