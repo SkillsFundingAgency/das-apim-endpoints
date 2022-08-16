c61e5a0b2aa2f3cf23a50a9970f1034512d5a185
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-16 15:02:55 +0100

    CSP-184 code refactored for review comment

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Locations/Queries/GetAvailableProviderLocations/GetAvailableProviderLocationsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.Locations.Queries.GetAvailableProviderLocations
+{
+    public class GetAvailableProviderLocationsQuery : IRequest<GetAvailableProviderLocationsQueryResult>
+    {
+        public int Ukprn { get; }
+        public int LarsCode { get; }
+        public GetAvailableProviderLocationsQuery(int ukprn, int larsCode)
+        {
+            Ukprn = ukprn;
+            LarsCode = larsCode;
+        }
+    }
+}
