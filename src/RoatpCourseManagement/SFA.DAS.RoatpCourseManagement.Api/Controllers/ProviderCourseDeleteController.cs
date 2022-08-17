e06f1f2fb0907f908010e594c647eb2e9796745f
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-17 21:09:52 +0100

    CSP-145 added unit tests

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderCourseDeleteController.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderCourseDeleteController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.DeleteProviderCourse;
-using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.DeleteProviderCourseLocation;
 using System.Threading.Tasks;
 
 namespace SFA.DAS.RoatpCourseManagement.Api.Controllers
@@ -21,7 +20,7 @@ namespace SFA.DAS.RoatpCourseManagement.Api.Controllers
 
         [HttpPost]
         [Route("providers/{ukprn}/courses/{larsCode}/delete")]
-        public async Task<IActionResult> DeleteProviderCourseLocation(int ukprn, int larsCode, DeleteProviderCourseCommand command)
+        public async Task<IActionResult> DeleteProviderCourse(int ukprn, int larsCode, DeleteProviderCourseCommand command)
         {
             _logger.LogInformation("Outer API: Request received to delete provider course for ukprn: {ukprn} larscode: {larscode} by UserId: {userId}", ukprn, larsCode, command.UserId);
             command.Ukprn = ukprn;
--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/DeleteProviderCourseRequest.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/DeleteProviderCourseRequest.cs
@@ -1,4 +1,5 @@
-﻿using SFA.DAS.SharedOuterApi.Interfaces;
+﻿using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.DeleteProviderCourse;
+using SFA.DAS.SharedOuterApi.Interfaces;
 using System.Web;
 
 namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests
@@ -9,6 +10,7 @@ namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests
         public int LarsCode { get; set; }
         public string UserId { get; set; }
 
+       
         public string DeleteUrl => $"/providers/{Ukprn}/courses/{LarsCode}/?userId={HttpUtility.UrlEncode(UserId)}";
     }
 }
