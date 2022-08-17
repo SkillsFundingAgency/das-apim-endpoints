7c918c309e6a6f63d6b861e5982939f472c18eb8
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-08-17 15:09:17 +0100

    CSP-145- Delete standard endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderCourseDeleteController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.DeleteProviderCourse;
+using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.DeleteProviderCourseLocation;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers
+{
+    [ApiController]
+    public class ProviderCourseDeleteController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<ProviderCourseDeleteController> _logger;
+
+        public ProviderCourseDeleteController(ILogger<ProviderCourseDeleteController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("providers/{ukprn}/courses/{larsCode}/delete")]
+        public async Task<IActionResult> DeleteProviderCourseLocation(int ukprn, int larsCode, DeleteProviderCourseCommand command)
+        {
+            _logger.LogInformation("Outer API: Request received to delete provider course for ukprn: {ukprn} larscode: {larscode} by UserId: {userId}", ukprn, larsCode, command.UserId);
+            command.Ukprn = ukprn;
+            command.LarsCode = larsCode;
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/DeleteProviderCourseRequest.cs
@@ -0,0 +1,14 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Web;
+
+namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests
+{
+    public class DeleteProviderCourseRequest : IDeleteApiRequest
+    {
+        public int Ukprn { get; set; }
+        public int LarsCode { get; set; }
+        public string UserId { get; set; }
+
+        public string DeleteUrl => $"/providers/{Ukprn}/courses/{LarsCode}/?userId={HttpUtility.UrlEncode(UserId)}";
+    }
+}
