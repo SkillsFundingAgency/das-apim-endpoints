8a6aef16e2d3c404a999f8681f482a99e2b33e68
chazzysb <cballagan@yahoo.com>
2022-08-17 13:24:16 +0100

    Push merged file

--- src/TrackProgress/SFA.DAS.TrackProgress/Application/Commands/TrackProgressCommand.cs
+++ src/TrackProgress/SFA.DAS.TrackProgress/Application/Commands/TrackProgressCommand.cs
@@ -1,6 +1,5 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SFA.DAS.TrackProgress.Apis.CommitmentsV2InnerApi;
 using SFA.DAS.TrackProgress.Application.DTOs;
 using SFA.DAS.TrackProgress.Application.Models;
 using SFA.DAS.TrackProgress.Application.Services;
@@ -22,7 +21,6 @@ public record ErrorDetail(
 public class TrackProgressResponse
 {
     public HttpStatusCode StatusCode { get; set; }
-    public string Message { get; set; } = string.Empty;
     public string ErrorTitle { get; set; } = string.Empty;
     public List<ErrorDetail> ErrorDetails { get; set; } = new List<ErrorDetail>();
 
@@ -30,9 +28,10 @@ public class TrackProgressResponse
         => StatusCode switch
         {
             HttpStatusCode.Created => new CreatedResult(string.Empty, null),
-            HttpStatusCode.NotFound => new NotFoundObjectResult(Message),
+            HttpStatusCode.NotFound => new NotFoundResult(),
             HttpStatusCode.BadRequest => new BadRequestObjectResult(GetProblemDetails()),
-            _ => new ObjectResult(new { StatusCode, Message }),
+            HttpStatusCode.InternalServerError => new StatusCodeResult((int)HttpStatusCode.InternalServerError),
+            _ => new StatusCodeResult((int)StatusCode),
         };
 
     private ProblemDetails GetProblemDetails()
@@ -50,9 +49,6 @@ public class TrackProgressResponse
     public TrackProgressResponse(HttpStatusCode statusCode)
         => StatusCode = statusCode;
 
-    public TrackProgressResponse(HttpStatusCode statusCode, string message)
-        => (StatusCode, Message) = (statusCode, message);
-
     public TrackProgressResponse(HttpStatusCode statusCode, string errorTitle, List<ErrorDetail> errorDetails)
         => (StatusCode, ErrorTitle, ErrorDetails) = (statusCode, errorTitle, errorDetails);
 }
@@ -68,21 +64,19 @@ public class TrackProgressCommandHandler : IRequestHandler<TrackProgressCommand,
     {
         var apprenticeshipResult = await _commitmentsService.GetApprenticeship(request.ProviderContext.ProviderId, request.Uln, request.PlannedStartDate);
         if (apprenticeshipResult.StatusCode != HttpStatusCode.OK)
-            return new TrackProgressResponse(apprenticeshipResult.StatusCode, apprenticeshipResult.ErrorContent);
+            return new TrackProgressResponse(HttpStatusCode.InternalServerError);
 
         if (apprenticeshipResult.Body.TotalApprenticeshipsFound == 0)
-            return new TrackProgressResponse(HttpStatusCode.NotFound, "Apprenticeship not found");
+            return new TrackProgressResponse(HttpStatusCode.NotFound);
 
         if (apprenticeshipResult.Body.TotalApprenticeshipsFound > 1)
             apprenticeshipResult.Body.Apprenticeships?.RemoveAll(x => x.StartDate == x.StopDate);
 
         if (apprenticeshipResult.Body.Apprenticeships?.Count == 0)
-            return new TrackProgressResponse(HttpStatusCode.NotFound, "Apprenticeship not found");
+            return new TrackProgressResponse(HttpStatusCode.NotFound);
 
         if (apprenticeshipResult.Body.Apprenticeships?.Count > 1)
             return new TrackProgressResponse(HttpStatusCode.BadRequest, "Multiple apprenticeship records exist", new List<ErrorDetail>());
-
-        var apprenticeship = apprenticeshipResult.Body.Apprenticeships?.FirstOrDefault();
 
         if (request.ProviderContext.InSandboxMode)
         {
