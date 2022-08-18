e8d496ff5af4389d633942d71df79ced678a1967
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-08-18 09:41:40 +0100

    MAC-89 - sorting sonar cloud

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/Program.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/Program.cs
@@ -4,7 +4,7 @@ using NLog.Web;
 
 namespace SFA.DAS.EmployerAccounts.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
