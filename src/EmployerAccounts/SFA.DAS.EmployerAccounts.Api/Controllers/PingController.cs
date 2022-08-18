67aae9b33764313c54da8a97e206ec5803b67393
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-08-18 11:42:23 +0100

    MAC-89 - Added comments why the  method can not be called  ping

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/Controllers/PingController.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/Controllers/PingController.cs
@@ -5,6 +5,7 @@ namespace SFA.DAS.EmployerAccounts.Api.Controllers
     [ApiController]
     public class PingController : ControllerBase
     {
+        // This method can not be called ping because you can not hit method called ping for some unknown reasons
         [HttpGet]
         [Route("pingouterapi")]        
         public IActionResult Get()
