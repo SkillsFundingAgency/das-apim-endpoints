4f02e3fb1581a9814d432646fa45f1c69d9617e7
Deven Shah <devenshah1974@gmail.com>
2022-08-22 08:27:34 +0100

    CSP-186 Implements endpoint to get all regions

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/RegionsController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Roatp.CourseManagement.Application.Regions.Queries;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<RegionsController> _logger;
+
+        public RegionsController(ILogger<RegionsController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("lookup/regions")]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            _logger.LogInformation($"Outer API: Getting all the regions and sub-regions");
+
+            var result = await _mediator.Send(new GetAllRegionsQuery());
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Regions/Queries/GetAllRegionsQueryHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Roatp.CourseManagement.Application.Regions.Queries;
+using SFA.DAS.Roatp.CourseManagement.InnerApi.Models;
+using SFA.DAS.RoatpCourseManagement.Application.Regions.Queries;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.Regions.Queries
+{
+    [TestFixture]
+    public class GetAllRegionsQueryHandlerTests
+    {
+        [Test, MoqAutoData]
+        public async Task Handle_UnsuccessfulStatus_ThrowsException(
+            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+            GetAllRegionsQueryHandler sut)
+        {
+            var response = new ApiResponse<List<RegionModel>>(null, HttpStatusCode.InternalServerError, "");
+            apiClientMock.Setup(a => a.GetWithResponseCode<List<RegionModel>>(It.IsAny<GetAllRegionsRequest>())).ReturnsAsync(response);
+
+            Func<Task> action = () => sut.Handle(It.IsAny<GetAllRegionsQuery>(), It.IsAny<CancellationToken>());
+
+            await action.Should().ThrowAsync<InvalidOperationException>();
+        }
+
+        [Test, MoqAutoData]
+        public async Task Handle_SuccessfulStatus_ThrowsException(
+           [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+           GetAllRegionsQueryHandler sut,
+           List<RegionModel> data)
+        {
+            var response = new ApiResponse<List<RegionModel>>(data, HttpStatusCode.OK, "");
+            apiClientMock.Setup(a => a.GetWithResponseCode<List<RegionModel>>(It.IsAny<GetAllRegionsRequest>())).ReturnsAsync(response);
+
+            var result = await sut.Handle(It.IsAny<GetAllRegionsQuery>(), It.IsAny<CancellationToken>());
+
+            result.Regions.Should().BeEquivalentTo(data);
+        }
+    }
+}
