7ce2f771d6f282db82604a73fd6255e5d61201e2
Paul Graham <paul.graham@coprime.co.uk>
2022-08-26 10:04:13 +0100

    trying wiring up Azure config

--- src/TrackProgress/SFA.DAS.TrackProgress.Api/AppStart/AddConfigurationOptionsExtensions.cs
+++ src/TrackProgress/SFA.DAS.TrackProgress.Api/AppStart/AddConfigurationOptionsExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Options;
+using SFA.DAS.Api.Common.Configuration;
 using SFA.DAS.SharedOuterApi.Configuration;
 
 namespace SFA.DAS.TrackProgress.Api.AppStart;
@@ -7,16 +8,12 @@ public static class AddConfigurationOptionsExtensions
 {
     public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
     {
-        //services.AddOptions<TrackProgressConfiguration>().BindConfiguration("");
-        //services.AddSingleton(cfg => cfg.GetRequiredService<IOptions<TrackProgressConfiguration>>().Value);
-        //services.AddSingleton(cfg => cfg.GetRequiredService<TrackProgressConfiguration>().AzureAd);
-        //services.AddSingleton(cfg => cfg.GetRequiredService<TrackProgressConfiguration>().CommitmentsV2InnerApi);
-        //services.AddSingleton(cfg => cfg.GetRequiredService<TrackProgressConfiguration>().CoursesApi);
-        //services.AddSingleton(cfg => cfg.GetRequiredService<TrackProgressConfiguration>().TrackProgressApi);
         services.AddOptions();
+        services.Configure<AzureActiveDirectoryConfiguration>(configuration.GetSection("AzureAd"));
         services.Configure<CommitmentsV2ApiConfiguration>(configuration.GetSection("CommitmentsV2InnerApi"));
         services.Configure<CoursesApiConfiguration>(configuration.GetSection("CoursesApi"));
         services.Configure<TrackProgressApiConfiguration>(configuration.GetSection("TrackProgressApi"));
+        services.AddSingleton(cfg => cfg.GetRequiredService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
         services.AddSingleton(cfg => cfg.GetRequiredService<IOptions<CommitmentsV2ApiConfiguration>>().Value);
         services.AddSingleton(cfg => cfg.GetRequiredService<IOptions<CoursesApiConfiguration>>().Value);
         services.AddSingleton(cfg => cfg.GetRequiredService<IOptions<TrackProgressApiConfiguration>>().Value);
