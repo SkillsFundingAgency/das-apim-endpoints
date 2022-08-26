beaa0b8b6e7bada25d0eb51c9b7ea6b0183dbf28
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2022-08-26 14:06:09 +0100

    MAC-89- Removed Health and Ping controller

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/AppStart/AddServiceRegistrationExtension.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/AppStart/AddServiceRegistrationExtension.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using SFA.DAS.Api.Common.Infrastructure;
 using SFA.DAS.Api.Common.Interfaces;
-using SFA.DAS.EmployerAccounts.Application.Interfaces;
-using SFA.DAS.EmployerAccounts.Application.Services;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Infrastructure;
 using SFA.DAS.SharedOuterApi.Interfaces;
@@ -18,8 +16,7 @@ namespace SFA.DAS.EmployerAccounts.Api.AppStart
             services.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
 
             services.AddTransient(typeof(IInternalApiClient<>), typeof(InternalApiClient<>));
-            services.AddTransient<IAccountsApiClient<AccountsConfiguration>, AccountsApiClient>();
-            services.AddTransient<IEmployerAccountsService, EmployerAccountsService>();
+            services.AddTransient<IAccountsApiClient<AccountsConfiguration>, AccountsApiClient>();            
         }
     }
 }
