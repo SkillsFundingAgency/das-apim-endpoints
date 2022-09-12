e143a142bd1d91781d817a2b9c21049434f50f9f
Alasdair Reid <ali.reid@live.co.uk>
2022-09-12 10:20:33 +0100

    Test updates

--- src/Approvals/SFA.DAS.Approvals.UnitTests/Application/Cohorts/GetCohortDetailsQueryHandlerTests.cs
+++ src/Approvals/SFA.DAS.Approvals.UnitTests/Application/Cohorts/GetCohortDetailsQueryHandlerTests.cs
@@ -7,6 +7,7 @@ using Moq;
 using NUnit.Framework;
 using SFA.DAS.Approvals.Application;
 using SFA.DAS.Approvals.Application.Cohorts.Queries.GetCohortDetails;
+using SFA.DAS.Approvals.Application.DraftApprenticeships.Queries.GetEditDraftApprenticeshipDeliveryModel;
 using SFA.DAS.Approvals.InnerApi;
 using SFA.DAS.Approvals.InnerApi.Requests;
 using SFA.DAS.Approvals.InnerApi.Responses;
@@ -27,6 +28,13 @@ namespace SFA.DAS.Approvals.UnitTests.Application.Cohorts
         private GetCohortResponse _cohort;
         private GetCohortDetailsQuery _query;
 
+        private GetDraftApprenticeshipResponse _draftApprenticeship;
+
+        private GetEditDraftApprenticeshipDeliveryModelQuery _queryEditDraft;
+
+        private GetEditDraftApprenticeshipDeliveryModelQueryResult _queryEditDraftResult;
+
+
         private List<string> _deliveryModels;
         private Mock<IDeliveryModelService> _deliveryModelService;
 
@@ -39,7 +47,14 @@ namespace SFA.DAS.Approvals.UnitTests.Application.Cohorts
                 .With(x => x.WithParty, Party.Employer)
                 .Create();
 
+
             _query = fixture.Create<GetCohortDetailsQuery>();
+
+            _queryEditDraft = fixture.Create<GetEditDraftApprenticeshipDeliveryModelQuery>();
+            _queryEditDraftResult = fixture.Create<GetEditDraftApprenticeshipDeliveryModelQueryResult>();
+
+
+            _draftApprenticeship = fixture.Create<GetDraftApprenticeshipResponse>();
 
             _deliveryModels = fixture.Create<List<string>>();
 
@@ -49,12 +64,17 @@ namespace SFA.DAS.Approvals.UnitTests.Application.Cohorts
                 x.GetWithResponseCode<GetCohortResponse>(It.Is<GetCohortRequest>(r => r.CohortId == _query.CohortId)))
                 .ReturnsAsync(new ApiResponse<GetCohortResponse>(_cohort, HttpStatusCode.OK, string.Empty));
 
+            _apiClient.Setup(x =>
+                    x.GetWithResponseCode<GetDraftApprenticeshipResponse>(It.Is<GetDraftApprenticeshipRequest>(r => r.CohortId == _queryEditDraft.CohortId && r.DraftApprenticeshipId == _queryEditDraft.DraftApprenticeshipId)))
+                .ReturnsAsync(new ApiResponse<GetDraftApprenticeshipResponse>(_draftApprenticeship, HttpStatusCode.OK, string.Empty));
+
+
             _deliveryModelService = new Mock<IDeliveryModelService>();
             _deliveryModelService.Setup(x => x.GetDeliveryModels(
                 It.Is<long>(p => p == _cohort.ProviderId),
-                It.Is<string>(s => s == "22"),
+                It.Is<string>(s => s == _draftApprenticeship.CourseCode),
                 It.Is<long>(ale => ale == _cohort.AccountLegalEntityId),
-                It.Is<long?>(a => a == null)))
+                It.Is<long?>(a => a == _draftApprenticeship.ContinuationOfId)))
             .ReturnsAsync(_deliveryModels);
 
 
