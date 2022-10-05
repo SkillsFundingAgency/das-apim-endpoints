49e2bef17ac850407636edb2414e0733efab4ce8
Matt <matt.daniels@digital.education.gov.uk>
2022-10-05 12:30:21 +0100

    QF-666 work in progress

---
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback/Application/Queries/GetExitSurvey/GetExitSurveyForApprenticeFeedbackTargetQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using SFA.DAS.ApprenticeFeedback.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Threading.Tasks;
+using System.Threading;
+using SFA.DAS.ApprenticeFeedback.Models;
+
+namespace SFA.DAS.ApprenticeFeedback.Application.Queries.GetExitSurvey
+{
+    public class GetExitSurveyForApprenticeFeedbackTargetQueryHandler : IRequestHandler<GetExitSurveyForApprenticeFeedbackTargetQuery, GetExitSurveyForApprenticeFeedbackTargetResult>
+    {
+        private readonly IApprenticeFeedbackApiClient<ApprenticeFeedbackApiConfiguration> _apprenticeFeedbackApiClient;
+
+        public GetExitSurveyForApprenticeFeedbackTargetQueryHandler(IApprenticeFeedbackApiClient<ApprenticeFeedbackApiConfiguration> apprenticeFeedbackApiClient)
+        {
+            _apprenticeFeedbackApiClient = apprenticeFeedbackApiClient;
+        }
+
+        public async Task<GetExitSurveyForApprenticeFeedbackTargetResult> Handle(GetExitSurveyForApprenticeFeedbackTargetQuery request, CancellationToken cancellationToken)
+        {
+            var apiResponse = await _apprenticeFeedbackApiClient.Get<ApprenticeExitSurvey>(new GetExitSurveyRequest(request.ApprenticeFeedbackTargetId));
+
+            return new GetExitSurveyForApprenticeFeedbackTargetResult
+            {
+                ExitSurvey = apiResponse
+            };
+
+        }
+    }
+}
