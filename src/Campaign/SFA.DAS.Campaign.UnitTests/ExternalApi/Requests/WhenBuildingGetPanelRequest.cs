f0787d506e6b3e511886937fe162f854081c5740
Sarah Coetzee <82900731+SarahDuncan@users.noreply.github.com>
2022-10-07 16:25:21 +0100

    QF-493-Calculation (#893)
    * Add API endpoint to get a standard by it's larscode
    
    * Add API endpoint to get a standard by it's larscode
    
    * Change larscode to standardUId, and rename some properties
    
    * Add API endpoint to get a standard by it's larscode
    
    * Change larscode to standardUId, and rename some properties
    
    * Reset to restore campaigns unit test proj
    
    Co-authored-by: Sarah <sarah.coetzee@education.gov.uk>
    Co-authored-by: codewolf86 <chris@codewolfconsulting.com>

---
+++ src/Campaign/SFA.DAS.Campaign.UnitTests/ExternalApi/Requests/WhenBuildingGetPanelRequest.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Campaign.ExternalApi.Requests;
+
+namespace SFA.DAS.Campaign.UnitTests.ExternalApi.Requests
+{
+    public class WhenBuildingGetPanelRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Url_Is_Correctly_Built(string slug)
+        {
+            var actual = new GetPanelRequest(slug);
+
+            actual.GetUrl.Should().Be($"entries?content_type=panel&fields.slug={slug}");
+        }
+    }
+}
