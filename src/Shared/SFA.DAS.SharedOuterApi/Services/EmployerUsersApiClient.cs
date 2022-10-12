ebb34d7649d14e36d43fa5fb062b62ad37dc13f2
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-10-12 21:43:14 +0100

    FAI-630 - Add EmployerUsersApiClient.
     Will be used for other gov sign in outer apis

--- src/Shared/SFA.DAS.SharedOuterApi/Services/EmployerUsersApiClient.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/Services/EmployerUsersApiClient.cs
@@ -1,18 +1,16 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
-using SFA.DAS.ApimDeveloper.Configuration;
-using SFA.DAS.ApimDeveloper.Interfaces;
+using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.SharedOuterApi.Models;
 
-namespace SFA.DAS.ApimDeveloper.Services
+namespace SFA.DAS.SharedOuterApi.Services
 {
-    public class ApimDeveloperApiClient : IApimDeveloperApiClient<ApimDeveloperApiConfiguration>
+    public class EmployerUsersApiClient : IEmployerUsersApiClient<EmployerUsersConfiguration>
     {
-        private readonly IInternalApiClient<ApimDeveloperApiConfiguration> _apiClient;
-
-        public ApimDeveloperApiClient (IInternalApiClient<ApimDeveloperApiConfiguration> apiClient)
+        private readonly IInternalApiClient<EmployerUsersConfiguration> _apiClient;
+        public EmployerUsersApiClient(IInternalApiClient<EmployerUsersConfiguration> apiClient)
         {
             _apiClient = apiClient;
         }
@@ -24,7 +22,7 @@ namespace SFA.DAS.ApimDeveloper.Services
 
         public Task<HttpStatusCode> GetResponseCode(IGetApiRequest request)
         {
-            throw new System.NotImplementedException();
+            return _apiClient.GetResponseCode(request);
         }
 
         public Task<ApiResponse<TResponse>> GetWithResponseCode<TResponse>(IGetApiRequest request)
@@ -54,7 +52,7 @@ namespace SFA.DAS.ApimDeveloper.Services
 
         public Task Delete(IDeleteApiRequest request)
         {
-            throw new System.NotImplementedException();
+            return _apiClient.Delete(request);
         }
 
         public Task Patch<TData>(IPatchApiRequest<TData> request)
@@ -64,27 +62,27 @@ namespace SFA.DAS.ApimDeveloper.Services
 
         public Task Put(IPutApiRequest request)
         {
-            return _apiClient.Put(request);
+            throw new System.NotImplementedException();
         }
 
         public Task Put<TData>(IPutApiRequest<TData> request)
         {
-            return _apiClient.Put(request);
+            throw new System.NotImplementedException();
         }
 
         public Task<ApiResponse<TResponse>> PostWithResponseCode<TResponse>(IPostApiRequest request, bool includeResponse = true)
         {
-            return _apiClient.PostWithResponseCode<TResponse>(request, includeResponse);
+            return _apiClient.PostWithResponseCode<TResponse>(request);
         }
 
         public Task<ApiResponse<string>> PatchWithResponseCode<TData>(IPatchApiRequest<TData> request)
         {
-            throw new System.NotImplementedException();
+            return _apiClient.PatchWithResponseCode(request);
         }
+
         public Task<ApiResponse<TResponse>> PutWithResponseCode<TResponse>(IPutApiRequest request)
         {
-            throw new System.NotImplementedException();
+            return _apiClient.PutWithResponseCode<TResponse>(request);
         }
     }
 }
-            
