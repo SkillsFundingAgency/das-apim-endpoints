6de55e97de06587158d9a14dba2a3f23f32d03f3
Ben Arnold <ben@arnoldfamily.me.uk>
2022-10-18 10:49:29 +0100

    Remove unnecessary code

--- src/TrackProgressInternal/SFA.DAS.TrackProgressInternal.Api.AcceptanceTests/TestModels/Apprenticeship.cs
+++ src/TrackProgressInternal/SFA.DAS.TrackProgressInternal.Api.AcceptanceTests/TestModels/Apprenticeship.cs
@@ -2,8 +2,6 @@
 
 public sealed record Apprenticeship(long ProviderId, int Uln, DateOnly StartDate)
 {
-    //public DeliveryModel DeliveryModel { get; init; } = DeliveryModel.PortableFlexiJob;
-    //public ApprenticeshipStatus Status { get; init; } = ApprenticeshipStatus.Live;
     public string Option { get; init; } = "OPTION1";
     public string Standard { get; init; } = "STD";
     public int NumberOfApprenticeships { get; init; } = 1;
@@ -18,15 +16,6 @@ public sealed record Apprenticeship(long ProviderId, int Uln, DateOnly StartDate
 
     internal Apprenticeship WithStartDate(string date)
         => new Apprenticeship(this) with { StartDate = DateOnly.Parse(date) };
-
-    //internal Apprenticeship WithDeliveryModel(DeliveryModel deliveryModel)
-    //    => new Apprenticeship(this) with { DeliveryModel = deliveryModel };
-
-    //internal Apprenticeship WithNotStarted()
-    //    => new Apprenticeship(this) with { Status = ApprenticeshipStatus.WaitingToStart };
-
-    //internal Apprenticeship WithStartAndStopOnSameDay()
-    //    => new Apprenticeship(this) with { Status = ApprenticeshipStatus.Stopped, StopDate = StartDate };
 
     internal Apprenticeship WithStandard(string standard)
         => new Apprenticeship(this) with { Standard = standard };
