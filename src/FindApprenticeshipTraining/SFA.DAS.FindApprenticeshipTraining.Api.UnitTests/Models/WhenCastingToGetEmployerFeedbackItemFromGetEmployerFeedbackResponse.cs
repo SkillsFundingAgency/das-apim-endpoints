159f5622f5fd1f19758cc2f4bc0effceb9fe5d4e
Ian Rose <ian.rose@digital.education.gov.uk>
2022-10-18 15:51:21 +0100

    QF665 Add unit tests

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api.UnitTests/Models/WhenCastingToGetEmployerFeedbackItemFromGetEmployerFeedbackResponse.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using AutoFixture.NUnit3;
+using Castle.Core.Internal;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses;
+using GetEmployerFeedbackAttributeItem = SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses.GetEmployerFeedbackAttributeItem;
+using GetEmployerFeedbackItem = SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses.GetEmployerFeedbackItem;
+using GetEmployerFeedbackResponse = SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses.GetEmployerFeedbackResponse;
+
+
+namespace SFA.DAS.FindApprenticeshipTraining.Api.UnitTests.Models
+{
+    public class WhenCastingToGetEmployerFeedbackItemFromGetEmployerFeedbackResponse
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(GetEmployerFeedbackResponse source)
+        {
+            GetEmployerFeedbackItem actual = (GetEmployerFeedbackResponse)source;
+
+            actual.Should().NotBeNull();
+            actual.FeedbackAttributes.Should().NotBeNull();
+            actual.FeedbackAttributes.Any().Should().BeTrue();
+            actual.FeedbackAttributes.Where(x => x.AttributeName.IsNullOrEmpty()).Any().Should().BeFalse();
+
+            IEnumerable<GetEmployerFeedbackAttributeItem> extra =
+                actual.FeedbackAttributes.Where(e =>
+                    !source.ProviderAttribute.Any(x =>
+                        e.AttributeName == x.Name &&
+                        e.Strength == x.Strength &&
+                        e.Weakness == x.Weakness
+                    )
+                );
+
+            extra.Should().BeEmpty();
+
+            IEnumerable<GetEmployerFeedbackResponseDetailItem> missing =
+                source.ProviderAttribute.Where(m =>
+                    !actual.FeedbackAttributes.Any(x =>
+                        x.AttributeName == m.Name &&
+                        x.Strength == m.Strength &&
+                        x.Weakness == m.Weakness
+                    )
+                );
+
+            missing.Should().BeEmpty();
+
+            actual.FeedbackRatings.Should().NotBeNull();
+            actual.FeedbackRatings.Any().Should().BeTrue();
+            actual.FeedbackRatings.Where(x => x.FeedbackName == "ReviewCount").Count().Should().Equals(1);
+            actual.FeedbackRatings.Where(x => x.FeedbackName == "Stars").Count().Should().Equals(1);
+            actual.FeedbackRatings.FirstOrDefault(x => x.FeedbackName == "ReviewCount").FeedbackCount.Should().Equals(source.ReviewCount);
+            actual.FeedbackRatings.FirstOrDefault(x => x.FeedbackName == "Stars").FeedbackCount.Should().Equals(source.Stars);
+        }
+    }
+}
---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api.UnitTests/Models/WhenCastingToGetEmployerFeedbackItemFromGetEmployerFeedbackSummaryItem.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Linq;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using GetEmployerFeedbackItem = SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses.GetEmployerFeedbackItem;
+using GetEmployerFeedbackSummaryItem = SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses.GetEmployerFeedbackSummaryItem;
+
+
+namespace SFA.DAS.FindApprenticeshipTraining.Api.UnitTests.Models
+{
+    public class WhenCastingToGetEmployerFeedbackItemFromGetEmployerFeedbackSummaryItem
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Mapped(GetEmployerFeedbackSummaryItem source)
+        {
+            GetEmployerFeedbackItem actual = (GetEmployerFeedbackSummaryItem)source;
+
+            actual.Should().NotBeNull();
+            actual.FeedbackAttributes.Should().NotBeNull();
+            actual.FeedbackAttributes.Should().BeEmpty();
+
+            actual.FeedbackRatings.Should().NotBeNull();
+            actual.FeedbackRatings.Any().Should().BeTrue();
+            actual.FeedbackRatings.Where(x => x.FeedbackName == "ReviewCount").Count().Should().Equals(1);
+            actual.FeedbackRatings.Where(x => x.FeedbackName == "Stars").Count().Should().Equals(1);
+            actual.FeedbackRatings.FirstOrDefault(x => x.FeedbackName == "ReviewCount").FeedbackCount.Should().Equals(source.ReviewCount);
+            actual.FeedbackRatings.FirstOrDefault(x => x.FeedbackName == "Stars").FeedbackCount.Should().Equals(source.Stars);
+        }
+    }
+}
