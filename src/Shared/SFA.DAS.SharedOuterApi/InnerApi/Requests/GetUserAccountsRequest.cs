9d228933c370eec3082b04ff8a2145ac4773f35d
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-10-24 20:32:06 +0100

    FAI-626 - Move account and employer request and responses to shared

--- src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/GetUserAccountsRequest.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/GetUserAccountsRequest.cs
@@ -1,6 +1,6 @@
 using SFA.DAS.SharedOuterApi.Interfaces;
 
-namespace SFA.DAS.ApimDeveloper.InnerApi.Requests
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests
 {
     public class GetUserAccountsRequest : IGetAllApiRequest
     {
