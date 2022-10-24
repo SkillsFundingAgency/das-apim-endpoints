d77537bb25bd8ad9c0c40982636d4bc01eb7bcea
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-10-24 21:09:44 +0100

    FAI-626 - Add api response type for user account info

---
+++ src/Reservations/SFA.DAS.Reservations.Api.UnitTests/Models/WhenCastingGetUserAccountsApiResponseFromMediatorType.cs
@@ -0,0 +1,28 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Api.Models;
+using SFA.DAS.Reservations.Application.AccountUsers.Queries;
+
+namespace SFA.DAS.Reservations.Api.UnitTests.Models
+{
+    public class WhenCastingGetUserAccountsApiResponseFromMediatorType
+    {
+        [Test, AutoData]
+        public void Then_The_Fields_Are_Correctly_Mapped(GetAccountsQueryResult source)
+        {
+            var actual = (GetUserAccountsApiResponse) source;
+            
+            actual.UserAccounts.Should().BeEquivalentTo(source.UserAccountResponse);
+        }
+        
+        
+        [Test]
+        public void Then_If_Null_Then_Empty_Returned()
+        {
+            var actual = (GetUserAccountsApiResponse) (GetAccountsQueryResult)null;
+            
+            actual.UserAccounts.Should().BeEmpty();
+        }
+    }
+}
