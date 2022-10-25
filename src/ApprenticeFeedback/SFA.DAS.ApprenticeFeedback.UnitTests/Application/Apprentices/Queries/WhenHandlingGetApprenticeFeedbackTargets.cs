994325bb7c633d57fab58d7a9c44e9de8cdea84a
Matt Daniels <matt.daniels@digital.education.gov.uk>
2022-10-25 13:40:55 +0100

    QF-465 Exit Interview (#880)
    * New exit survey endpoint calling inner api
    
    * Rename "other" to "none"
    
    * QF-666 work in progress
    
    * Tweak json response
    
    * Add Withdrawn property to ApprenticeFeedbackTarget to support QF-691 prevent re-doing the survey journey

---
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback.UnitTests/Application/Apprentices/Queries/WhenHandlingGetApprenticeFeedbackTargets.cs
@@ -0,0 +1,35 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeFeedback.Application.Queries.GetApprenticeFeedbackTargets;
+using SFA.DAS.ApprenticeFeedback.InnerApi.Requests;
+using SFA.DAS.ApprenticeFeedback.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeFeedback.UnitTests.Application.Apprentices.Queries
+{
+    public class WhenHandlingGetApprenticeFeedbackTargets
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_ApprenticeFeedbackTargets_From_The_Api(
+           List<ApprenticeFeedbackTarget> feedbackTargets,
+           GetApprenticeFeedbackTargetsQuery query,
+           [Frozen] Mock<IApprenticeFeedbackApiClient<ApprenticeFeedbackApiConfiguration>> mockApprenticeFeedbackApiClient,
+           GetApprenticeFeedbackTargetsQueryHandler handler)
+        {
+            mockApprenticeFeedbackApiClient
+                .Setup(client => client.GetAll<ApprenticeFeedbackTarget>(
+                It.IsAny<GetAllApprenticeFeedbackTargetsRequest>()))
+                .ReturnsAsync(feedbackTargets);
+
+            var actual = await handler.Handle(query, CancellationToken.None);
+            actual.FeedbackTargets.Should().BeEquivalentTo(feedbackTargets, options => options.ExcludingMissingMembers());
+        }
+    }
+}
