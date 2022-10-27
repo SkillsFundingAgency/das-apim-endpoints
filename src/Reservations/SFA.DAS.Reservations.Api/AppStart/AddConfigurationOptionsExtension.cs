1bc07926417a961e881b63f8739fb304b8f73f44
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-10-27 07:48:41 +0100

    FAI-626 - Add missing account configuration and test for container tests

--- src/Reservations/SFA.DAS.Reservations.Api/AppStart/AddConfigurationOptionsExtension.cs
+++ src/Reservations/SFA.DAS.Reservations.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -15,6 +15,8 @@ namespace SFA.DAS.Reservations.Api.AppStart
             services.AddSingleton(cfg => cfg.GetService<IOptions<CoursesApiConfiguration>>().Value);
             services.Configure<CourseDeliveryApiConfiguration>(configuration.GetSection(nameof(CourseDeliveryApiConfiguration)));
             services.AddSingleton(cfg => cfg.GetService<IOptions<CourseDeliveryApiConfiguration>>().Value);
+            services.Configure<AccountsConfiguration>(configuration.GetSection("AccountsInnerApi"));
+            services.AddSingleton(cfg => cfg.GetService<IOptions<AccountsConfiguration>>().Value);
             services.Configure<AzureActiveDirectoryConfiguration>(configuration.GetSection("AzureAd"));
             services.AddSingleton(cfg => cfg.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
             services.Configure<EmployerUsersApiConfiguration>(configuration.GetSection(nameof(EmployerUsersApiConfiguration)));
