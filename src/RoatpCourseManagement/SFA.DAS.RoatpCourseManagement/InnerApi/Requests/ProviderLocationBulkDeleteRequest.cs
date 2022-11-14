d97657a4ef8fe171970c1461989cf51b79727b01
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-11-14 12:53:04 +0000

    CSP-389 locations/cleanup endpoint is passed with username

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/ProviderLocationBulkDeleteRequest.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/ProviderLocationBulkDeleteRequest.cs
@@ -7,11 +7,13 @@ namespace SFA.DAS.Roatp.CourseManagement.InnerApi.Requests
     {
         public int Ukprn { get; }
         public string UserId { get; set; }
-        public string DeleteUrl => $"/providers/{Ukprn}/locations/cleanup?userId={HttpUtility.UrlEncode(UserId)}";
+        public string UserDisplayName { get; set; }
+        public string DeleteUrl => $"/providers/{Ukprn}/locations/cleanup?userId={HttpUtility.UrlEncode(UserId)}&userDisplayName={HttpUtility.UrlEncode(UserDisplayName)}";
         public ProviderLocationBulkDeleteRequest(ProviderLocationBulkDeleteModel data)
         {
             Ukprn = data.Ukprn;
             UserId = data.UserId;
+            UserDisplayName = data.UserDisplayName;
         }
     }
 }
