125b293b33c94bcd2188e4ce92d9951b626cbbd1
Michael Young <mick.young1981@googlemail.com>
2022-11-17 13:15:51 +0000

    Added controller action.

---
+++ src/Funding/SFA.DAS.Funding.Api/Models/ProviderEarningsSummaryDto.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.Funding.Models;
+
+namespace SFA.DAS.Funding.Api.Models
+{
+    public class ProviderEarningsSummaryDto
+    {
+        public decimal TotalEarningsForCurrentAcademicYear { get; set; }
+
+        public static implicit operator ProviderEarningsSummaryDto(ProviderEarningsSummary source)
+        {
+            return new ProviderEarningsSummaryDto
+            {
+                TotalEarningsForCurrentAcademicYear = source.TotalEarningsForCurrentAcademicYear
+            };
+        }
+    }
+}
