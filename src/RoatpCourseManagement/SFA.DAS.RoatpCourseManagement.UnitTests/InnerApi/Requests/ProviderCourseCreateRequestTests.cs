c2d71432799bafbef96a48e9d7d6627800484c24
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-11-22 17:17:37 +0000

    CSP-391-Audit entries during adding a new standard

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/InnerApi/Requests/ProviderCourseCreateRequestTests.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/InnerApi/Requests/ProviderCourseCreateRequestTests.cs
@@ -1,8 +1,10 @@
 ﻿using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
+using SFA.DAS.NServiceBus;
 using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.CreateProviderCourse;
 using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using System.Web;
 
 namespace SFA.DAS.RoatpCourseManagement.UnitTests.InnerApi.Requests
 {
@@ -16,7 +18,7 @@ namespace SFA.DAS.RoatpCourseManagement.UnitTests.InnerApi.Requests
 
             request.Ukprn.Should().Be(data.Ukprn);
             request.LarsCode.Should().Be(data.LarsCode);
-            request.PostUrl.Should().Be($"providers/{data.Ukprn}/courses/{data.LarsCode}/");
+            request.PostUrl.Should().Be($"providers/{data.Ukprn}/courses/{data.LarsCode}/?userId={HttpUtility.UrlEncode(data.UserId)}&userDisplayName={HttpUtility.UrlEncode(data.UserDisplayName)}");
             request.Data.Should().Be(data);
         }
     }
