d8bc411d69c0fab84f5a526d682a59fe21625973
ratheeshri1984 <ratheeshri1984@gmail.com>
2022-11-28 10:45:09 +0000

    MAP-170-Managing Standards

--- src/Approvals/SFA.DAS.Approvals/Application/Providers/Queries/GetProvidersQueryHandler.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/Providers/Queries/GetProvidersQueryHandler.cs
@@ -11,19 +11,38 @@ namespace SFA.DAS.Approvals.Application.Providers.Queries
     public class GetProvidersQueryHandler : IRequestHandler<GetProvidersQuery, GetProvidersResult>
     {
         private readonly ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> _apiClient;
+        private readonly IRoatpServiceApiClient<RoatpConfiguration> _roatpApiClient;
+        private readonly FeatureToggles _featureToggles;
 
-        public GetProvidersQueryHandler (ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> apiClient)
+        public GetProvidersQueryHandler(ICourseDeliveryApiClient<CourseDeliveryApiConfiguration> apiClient,
+            IRoatpServiceApiClient<RoatpConfiguration> roatpApiClient,
+            FeatureToggles featureToggles)
         {
             _apiClient = apiClient;
+            _roatpApiClient = roatpApiClient;
+            _featureToggles = featureToggles;
         }
+
         public async Task<GetProvidersResult> Handle(GetProvidersQuery request, CancellationToken cancellationToken)
         {
-            var result = await _apiClient.Get<GetProvidersListResponse>(new GetProvidersRequest());
-            
-            return new GetProvidersResult
+            if (_featureToggles.FeatureToggleRoatpStandardsEnabled)
             {
-                Providers = result.Providers
-            };
+                var result = await _roatpApiClient.Get<GetRoatpProvidersListResponse>(new GetProvidersRequest());
+
+                return new GetProvidersResult
+                {
+                    Providers = result.RegisteredProviders
+                };
+            }
+            else
+            {
+                var result = await _apiClient.Get<GetProvidersListResponse>(new GetProvidersRequest());
+
+                return new GetProvidersResult
+                {
+                    Providers = result.Providers
+                };
+            }
         }
     }
 }
---
+++ src/Shared/SFA.DAS.SharedOuterApi/Configuration/FeatureToggles.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.SharedOuterApi.Configuration
+{
+    public class FeatureToggles
+    {
+        public bool FeatureToggleRoatpStandardsEnabled { get; set; }
+    }
+}
