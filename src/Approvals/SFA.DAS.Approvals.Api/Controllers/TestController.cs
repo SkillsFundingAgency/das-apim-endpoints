6d3071a402e37f2f625967a6a77bdd742d33f985
Shahzad <shahzad.khan@hkf-technologies.co.uk>
2022-11-29 16:45:27 +0000

    adding a test controller

---
+++ src/Approvals/SFA.DAS.Approvals.Api/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.Approvals.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class TestController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(this.User.Identity.IsAuthenticated);
+        }
+    }
+}
