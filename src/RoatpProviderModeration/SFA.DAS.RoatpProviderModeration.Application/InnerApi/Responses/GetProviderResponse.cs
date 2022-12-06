e2b7272afc7779791e183b4287795480d47b9a4a
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-12-06 15:30:27 +0000

    CSP-380 Get Provider type

--- src/RoatpProviderModeration/SFA.DAS.RoatpProviderModeration.Application/InnerApi/Responses/GetProviderResponse.cs
+++ src/RoatpProviderModeration/SFA.DAS.RoatpProviderModeration.Application/InnerApi/Responses/GetProviderResponse.cs
@@ -1,8 +1,11 @@
-﻿namespace SFA.DAS.RoatpProviderModeration.Application.InnerApi.Responses
+﻿using SFA.DAS.RoatpProviderModeration.Application.InnerApi.Models;
+
+namespace SFA.DAS.RoatpProviderModeration.Application.InnerApi.Responses
 {
     public class GetProviderResponse
     {
         public int Ukprn { get; set; }
         public string MarketingInfo { get; set; }
+        public ProviderType ProviderType { get; set; }
     }
 }
--- src/RoatpProviderModeration/SFA.DAS.RoatpProviderModeration.Application/Queries/GetProvider/GetProviderResult.cs
+++ src/RoatpProviderModeration/SFA.DAS.RoatpProviderModeration.Application/Queries/GetProvider/GetProviderResult.cs
@@ -1,4 +1,5 @@
 ﻿
+using SFA.DAS.RoatpProviderModeration.Application.InnerApi.Models;
 using SFA.DAS.RoatpProviderModeration.Application.InnerApi.Responses;
 
 namespace SFA.DAS.RoatpProviderModeration.Application.Queries.GetProvider
@@ -6,11 +7,13 @@ namespace SFA.DAS.RoatpProviderModeration.Application.Queries.GetProvider
     public class GetProviderResult
     {
         public string MarketingInfo { get; set; }
+        public ProviderType ProviderType { get; set; }
 
         public static implicit operator GetProviderResult(GetProviderResponse source) =>
             new GetProviderResult
             {
-                MarketingInfo = source.MarketingInfo
+                MarketingInfo = source.MarketingInfo,
+                ProviderType = source.ProviderType
             };
     }
 }
