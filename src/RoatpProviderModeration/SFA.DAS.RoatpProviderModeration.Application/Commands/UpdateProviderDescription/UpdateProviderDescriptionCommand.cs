0c26588232102b5149576f881aebdb58e7a5c229
Ravi Udari <ravi.udari@digital.education.gov.uk>
2022-12-21 12:54:18 +0000

    CSP-433- added patch call for ProviderDescription

---
+++ src/RoatpProviderModeration/SFA.DAS.RoatpProviderModeration.Application/Commands/UpdateProviderDescription/UpdateProviderDescriptionCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace SFA.DAS.RoatpProviderModeration.Application.Commands.UpdateProviderDescription
+{
+    public class UpdateProviderDescriptionCommand : IRequest<Unit>
+    {
+        public int Ukprn { get; set; }
+        public string UserId { get; set; }
+        public string UserDisplayName { get; set; }
+        public string ProviderDescription { get; set; }
+    }
+}
