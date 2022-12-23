bf4575d5e6c366eaab95914de3f3658767295558
MarkFCain <mark.cain@digital.education.gov.uk>
2022-12-23 11:11:29 +0000

    CSP-310 shortlist delete by userid now pointing to shortlist.api, coverage updated

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Shortlist/Commands/WhenDeletingShortlistForUser.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Shortlist/Commands/WhenDeletingShortlistForUser.cs
@@ -4,9 +4,9 @@ using AutoFixture.NUnit3;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.DeleteShortlistForUser;
+using SFA.DAS.FindApprenticeshipTraining.Configuration;
 using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
-using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.FindApprenticeshipTraining.Services;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Application.Shortlist.Commands
@@ -16,14 +16,14 @@ namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Application.Shortlist.Com
         [Test, MoqAutoData]
         public async Task Then_Deletes_The_Shortlist_Item_From_The_Request_Calling_CourseDelivery_Api(
             DeleteShortlistForUserCommand command,
-            [Frozen] Mock<ICourseDeliveryApiClient<CourseDeliveryApiConfiguration>> courseDeliveryApiClient,
+            [Frozen] Mock<IShortlistApiClient<ShortlistApiConfiguration>> _apiClient,
             DeleteShortlistForUserCommandHandler handler)
         {
             //Act
             await handler.Handle(command, CancellationToken.None);
         
             //Assert
-            courseDeliveryApiClient.Verify(x=>
+            _apiClient.Verify(x=>
                 x.Delete(It.Is<DeleteShortlistForUserRequest>(c=>
                     c.DeleteUrl.Equals($"api/shortlist/users/{command.UserId}"))), Times.Once);
         }
