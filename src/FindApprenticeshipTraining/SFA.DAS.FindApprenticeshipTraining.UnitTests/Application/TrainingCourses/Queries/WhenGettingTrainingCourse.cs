893372f70ce3714b060f8569a87147f214ce33fa
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2023-01-12 10:39:57 +0000

    CSP-286 training courses shortlist count now pointing to shortlist api (#980)

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/TrainingCourses/Queries/WhenGettingTrainingCourse.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/TrainingCourses/Queries/WhenGettingTrainingCourse.cs
@@ -15,7 +15,6 @@ using SFA.DAS.Testing.AutoFixture;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using SFA.DAS.FindApprenticeshipTraining.Services;
 
 namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Application.TrainingCourses.Queries
 {
