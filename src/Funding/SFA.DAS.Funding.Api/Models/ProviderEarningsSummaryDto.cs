c9a0378e9353ae203720f60e5e787d0b0cce1265
Tom Gough <tom.gough@talentconsulting.uk>
2023-01-30 13:55:01 +0000

    FLP-158 Display the total non-levy employer & government contribution on the earnings report dashboard

--- src/Funding/SFA.DAS.Funding.Api/Models/ProviderEarningsSummaryDto.cs
+++ src/Funding/SFA.DAS.Funding.Api/Models/ProviderEarningsSummaryDto.cs
@@ -7,6 +7,8 @@ namespace SFA.DAS.Funding.Api.Models
         public decimal TotalEarningsForCurrentAcademicYear { get; set; }
         public decimal TotalLevyEarningsForCurrentAcademicYear { get; set; }
         public decimal TotalNonLevyEarningsForCurrentAcademicYear { get; set; }
+        public decimal TotalNonLevyEarningsForCurrentAcademicYearGovernment { get; set; }
+        public decimal TotalNonLevyEarningsForCurrentAcademicYearEmployer { get; set; }
 
         public static implicit operator ProviderEarningsSummaryDto(ProviderEarningsSummary source)
         {
@@ -14,7 +16,9 @@ namespace SFA.DAS.Funding.Api.Models
             {
                 TotalEarningsForCurrentAcademicYear = source.TotalEarningsForCurrentAcademicYear,
                 TotalLevyEarningsForCurrentAcademicYear = source.TotalLevyEarningsForCurrentAcademicYear,
-                TotalNonLevyEarningsForCurrentAcademicYear = source.TotalNonLevyEarningsForCurrentAcademicYear
+                TotalNonLevyEarningsForCurrentAcademicYear = source.TotalNonLevyEarningsForCurrentAcademicYear,
+                TotalNonLevyEarningsForCurrentAcademicYearGovernment = source.TotalNonLevyEarningsForCurrentAcademicYearGovernment,
+                TotalNonLevyEarningsForCurrentAcademicYearEmployer = source.TotalNonLevyEarningsForCurrentAcademicYearEmployer
             };
         }
     }
--- src/Funding/SFA.DAS.Funding/Models/ProviderEarningsSummary.cs
+++ src/Funding/SFA.DAS.Funding/Models/ProviderEarningsSummary.cs
@@ -5,5 +5,7 @@
         public decimal TotalEarningsForCurrentAcademicYear { get; set; }
         public decimal TotalLevyEarningsForCurrentAcademicYear { get; set; }
         public decimal TotalNonLevyEarningsForCurrentAcademicYear { get; set; }
+        public decimal TotalNonLevyEarningsForCurrentAcademicYearGovernment { get; set; }
+        public decimal TotalNonLevyEarningsForCurrentAcademicYearEmployer { get; set; }
     }
 }
