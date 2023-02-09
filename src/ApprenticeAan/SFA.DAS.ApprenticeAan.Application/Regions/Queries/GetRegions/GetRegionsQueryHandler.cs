45041c27a3c92f26a3b2171c9eb5f048582af701
Liam Wright <mail@liam-wright.co.uk>
2023-02-09 12:34:30 +0000

    CSP-500 - Added logger to GetRegionsQueryHandler

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Regions/Queries/GetRegions/GetRegionsQueryHandler.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Regions/Queries/GetRegions/GetRegionsQueryHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using MediatR;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.ApprenticeAan.Api.Configuration;
 using SFA.DAS.ApprenticeAan.Application.InnerApi.Regions.Requests;
 using SFA.DAS.ApprenticeAan.Application.Services;
@@ -9,19 +10,26 @@ namespace SFA.DAS.ApprenticeAan.Application.Regions.Queries.GetRegions
     public class GetRegionsQueryHandler : IRequestHandler<GetRegionsQuery, GetRegionsQueryResult?>
     {
         private readonly IAanHubApiClient<AanHubApiConfiguration> _apiClient;
+        private readonly ILogger<GetRegionsQueryHandler> _logger;
 
-        public GetRegionsQueryHandler(IAanHubApiClient<AanHubApiConfiguration> apiClient) => _apiClient = apiClient;
+        public GetRegionsQueryHandler(IAanHubApiClient<AanHubApiConfiguration> apiClient, ILogger<GetRegionsQueryHandler> logger)
+        {
+            _apiClient = apiClient;
+            _logger = logger;
+        }
 
         public async Task<GetRegionsQueryResult?> Handle(GetRegionsQuery request, CancellationToken cancellationToken)
         {
             var regionsResult = await _apiClient.GetWithResponseCode<GetRegionsQueryResult>(new GetRegionsQueryRequest());
 
-            if (regionsResult.StatusCode != HttpStatusCode.OK || regionsResult.Body == null) return null;
+            if (regionsResult.StatusCode == HttpStatusCode.OK && regionsResult.Body != null)
+                return new GetRegionsQueryResult
+                {
+                    Regions = regionsResult.Body.Regions
+                };
 
-            return new GetRegionsQueryResult
-            {
-                Regions = regionsResult.Body.Regions
-            };
+            _logger.LogError("ApprenticeAan Outer API: Unable to query AanHub API /Regions endpoint");
+            return null;
         }
     }
 }
