b0c2a9b4434db0795bcf90d6ac5afbb55374331a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-02-15 21:54:44 +0000

    FAI-792 - Add Employers Api Client and Configuration

---
+++ src/Shared/SFA.DAS.SharedOuterApi/Services/EmployerProfilesApiClient.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+
+namespace SFA.DAS.SharedOuterApi.Services
+{
+    public class EmployerProfilesApiClient : IEmployerProfilesApiClient<EmployerProfilesApiConfiguration>
+    {
+        private readonly IInternalApiClient<EmployerProfilesApiConfiguration> _apiClient;
+
+        public EmployerProfilesApiClient(IInternalApiClient<EmployerProfilesApiConfiguration> apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        public Task<TResponse> Get<TResponse>(IGetApiRequest request)
+        {
+            return _apiClient.Get<TResponse>(request);
+        }
+
+        public Task<HttpStatusCode> GetResponseCode(IGetApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ApiResponse<TResponse>> GetWithResponseCode<TResponse>(IGetApiRequest request)
+        {
+            return _apiClient.GetWithResponseCode<TResponse>(request);
+        }
+
+        public Task<IEnumerable<TResponse>> GetAll<TResponse>(IGetAllApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<PagedResponse<TResponse>> GetPaged<TResponse>(IGetPagedApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<TResponse> Post<TResponse>(IPostApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Post<TData>(IPostApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Delete(IDeleteApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Patch<TData>(IPatchApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Put(IPutApiRequest request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Put<TData>(IPutApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ApiResponse<TResponse>> PostWithResponseCode<TResponse>(IPostApiRequest request, bool includeResponse = true)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ApiResponse<string>> PatchWithResponseCode<TData>(IPatchApiRequest<TData> request)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<ApiResponse<TResponse>> PutWithResponseCode<TResponse>(IPutApiRequest request)
+        {
+            return _apiClient.PutWithResponseCode<TResponse>(request);
+        }
+    }
+}
