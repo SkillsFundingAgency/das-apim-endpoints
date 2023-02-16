b39efb1a5192881854a1cdafe849a63eb2d979fe
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-02-16 06:46:57 +0000

    FAI-792 - Update upsert to match employer profiles

--- src/Shared/SFA.DAS.SharedOuterApi.UnitTests/InnerApi/Requests/WhenBuildingPutUpsertEmployerUserAccountRequest.cs
+++ src/Shared/SFA.DAS.SharedOuterApi.UnitTests/InnerApi/Requests/WhenBuildingPutUpsertEmployerUserAccountRequest.cs
@@ -1,4 +1,4 @@
-using System.Web;
+using System;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
@@ -9,11 +9,11 @@ namespace SFA.DAS.SharedOuterApi.UnitTests.InnerApi.Requests
     public class WhenBuildingPutUpsertEmployerUserAccountRequest
     {
         [Test, AutoData]
-        public void Then_The_Url_And_Data_Is_Constructed_Correctly(string id, string email, string firstName, string lastName)
+        public void Then_The_Url_And_Data_Is_Constructed_Correctly(Guid userId, string id, string email, string firstName, string lastName)
         {
-            var actual = new PutUpsertEmployerUserAccountRequest(id, email, firstName, lastName);
+            var actual = new PutUpsertEmployerUserAccountRequest(userId,id, email, firstName, lastName);
 
-            actual.PutUrl.Should().Be($"api/users");
+            actual.PutUrl.Should().Be($"api/users/{userId}");
             actual.Data.Should().BeEquivalentTo(new
             {
                 GovUkIdentifier = id,
