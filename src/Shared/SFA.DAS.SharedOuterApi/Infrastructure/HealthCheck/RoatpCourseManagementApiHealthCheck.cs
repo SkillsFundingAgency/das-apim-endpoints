a7b10dcf1bfded2f1e00028016f2266855675bc5
MarkFCain <mark.cain@digital.education.gov.uk>
2023-02-17 15:05:15 +0000

    FATintegrationChangesFinalMerge fix to HttpStatus config

--- src/Shared/SFA.DAS.SharedOuterApi/Infrastructure/HealthCheck/RoatpCourseManagementApiHealthCheck.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/Infrastructure/HealthCheck/RoatpCourseManagementApiHealthCheck.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
