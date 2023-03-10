df0ca846b143c9b95f58eadb2f4016378608598c
Deven Shah <devenshah1974@gmail.com>
2023-03-10 16:45:49 +0000

    CSP-499 Implements Get Addresses endpoint

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/LocationsControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.ApprenticeAan.Api.Controllers;
+using SFA.DAS.ApprenticeAan.Application.Locations.Queries.GetAddresses;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers;
+
+public class LocationsControllerTests
+{
+    [Test]
+    [MoqAutoData]
+    public async Task GetAddresses_ReturnsAddresses(
+        GetAddressesQueryResult response,
+        [Frozen] Mock<IMediator> mockMediator,
+        [Greedy] LocationsController sut)
+    {
+        mockMediator
+            .Setup(m => m.Send(It.IsAny<GetAddressesQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+
+        var result = await sut.GetAddresses("thisIsAQuery");
+
+        result.As<OkObjectResult>().Should().NotBeNull();
+
+        result.As<OkObjectResult>().Value.Should().BeEquivalentTo(response);
+    }
+
+    [Test]
+    [MoqAutoData]
+    public async Task GetAddresses_NoMatch_ReturnNotFoundResponse(
+        [Frozen] Mock<IMediator> mockMediator,
+        [Greedy] LocationsController sut)
+    {
+        var response = new GetAddressesQueryResult();
+        mockMediator
+            .Setup(m => m.Send(It.IsAny<GetAddressesQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+
+        var result = await sut.GetAddresses("thisIsAQuery");
+
+        result.As<NotFoundResult>().Should().NotBeNull();
+    }
+}
