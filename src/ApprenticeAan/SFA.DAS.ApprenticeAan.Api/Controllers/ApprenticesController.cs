c945dbc696125eea82aefb7fb67ebe9fd9c9619d
Deven Shah <devenshah1974@gmail.com>
2023-03-13 23:30:06 +0000

    CSP-558 Implements Get Apprentice Account endpoint

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/ApprenticesController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Application.ApprenticeAccount.Queries.GetApprenticeAccount;
+
+namespace SFA.DAS.ApprenticeAan.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ApprenticesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ApprenticesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("account/{apprenticeId}")]
+    [ProducesResponseType(typeof(GetApprenticeAccountQueryResult), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAccount(Guid apprenticeId, CancellationToken cancellationToken)
+    {
+        var apprenticeAccountDetails = await _mediator.Send(new GetApprenticeAccountQuery { ApprenticeId = apprenticeId }, cancellationToken);
+        if (apprenticeAccountDetails == null) return NotFound();
+        return Ok(apprenticeAccountDetails);
+    }
+}
