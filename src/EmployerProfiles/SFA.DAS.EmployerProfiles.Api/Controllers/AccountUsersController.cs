12303bf8ae0c998a4d64da1e50f24883b3606737
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-03-15 11:29:21 +0000

    Removed obsolute code from the controller and handlers. as a result of PR

--- src/EmployerProfiles/SFA.DAS.EmployerProfiles.Api/Controllers/AccountUsersController.cs
+++ src/EmployerProfiles/SFA.DAS.EmployerProfiles.Api/Controllers/AccountUsersController.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Net;
-using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.EmployerProfiles.Api.Models;
-using SFA.DAS.EmployerProfiles.Application.AccountUsers.Commands.UpsertEmployer;
 using SFA.DAS.EmployerProfiles.Application.AccountUsers.Queries;
-using SFA.DAS.SharedOuterApi.Infrastructure;
+using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.EmployerProfiles.Api.Controllers
 {
@@ -16,14 +14,14 @@ namespace SFA.DAS.EmployerProfiles.Api.Controllers
     {
         private readonly IMediator _mediator;
 
-        public AccountUsersController (IMediator mediator)
+        public AccountUsersController(IMediator mediator)
         {
             _mediator = mediator;
         }
-        
+
         [HttpGet]
         [Route("{userId}/accounts")]
-        public async Task<IActionResult> GetUserAccounts(string userId, [FromQuery]string email)
+        public async Task<IActionResult> GetUserAccounts(string userId, [FromQuery] string email)
         {
             try
             {
@@ -33,35 +31,7 @@ namespace SFA.DAS.EmployerProfiles.Api.Controllers
                     Email = email
                 });
 
-                return Ok((UserAccountsApiResponse) result);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return new StatusCodeResult((int) HttpStatusCode.InternalServerError);
-            }
-        }
-
-        [HttpPut]
-        [Route("{id}/upsert-user")]
-        public async Task<IActionResult> UpsertUserAccount([FromQuery] Guid id, [FromBody] UpsertAccountRequest request)
-        {
-            try
-            {
-                var result = await _mediator.Send(new UpsertAccountCommand
-                {
-                    Id = id,
-                    Email = request.Email,
-                    FirstName = request.FirstName,
-                    LastName = request.LastName,
-                    GovIdentifier = request.GovIdentifier
-                });
-
-                return Ok(result);
-            }
-            catch (HttpRequestContentException e)
-            {
-                return StatusCode((int)e.StatusCode, e.ErrorContent);
+                return Ok((UserAccountsApiResponse)result);
             }
             catch (Exception e)
             {
