e0c657c59072dcdfba45cd7bf61a4d76a7b01216
Deven Shah <devenshah1974@gmail.com>
2023-03-15 17:49:10 +0000

    CSP-645 Created solution structure for RoatpOversight

---
+++ src/RoatpOversight/SFA.DAS.RoatpOversight.Api/Controllers/ProvidersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.RoatpOversight.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProvidersController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult CreateProvider() => Created(String.Empty, null);
+}
---
+++ src/RoatpOversight/SFA.DAS.RoatpOversight/Application/Providers/Command/CreateProvider/CreateProviderCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace SFA.DAS.RoatpOversight.Application.Providers.Command.CreateProvider;
+
+public class CreateProviderCommand : IRequest<Unit>
+{
+    public string RequestingUserId { get; set; } = null!;
+    public int Ukprn { get; set; }
+    public string Name { get; set; } = null!;
+}
