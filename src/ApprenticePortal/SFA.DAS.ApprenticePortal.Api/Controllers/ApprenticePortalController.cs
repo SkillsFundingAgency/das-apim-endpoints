f95e21c596b64b0049e9f11559b8d1db25bf8799
Alasdair Reid <ali.reid@live.co.uk>
2023-03-22 22:43:17 +0000

    Add GetApprentice

--- src/ApprenticePortal/SFA.DAS.ApprenticePortal.Api/Controllers/ApprenticePortalController.cs
+++ src/ApprenticePortal/SFA.DAS.ApprenticePortal.Api/Controllers/ApprenticePortalController.cs
@@ -1,9 +1,11 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticePortal.Application.ApprenticeAccounts.Queries;
 using SFA.DAS.ApprenticePortal.Application.Commands.ApprenticeUpdate;
 using SFA.DAS.ApprenticePortal.InnerApi.ApprenticeAccounts.Requests;
 using System;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace SFA.DAS.ApprenticePortal.Api.Controllers
 {
@@ -15,6 +17,20 @@ namespace SFA.DAS.ApprenticePortal.Api.Controllers
         public ApprenticePortalController(IMediator mediator)
         {
             _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Route("/apprentices/{id}")]
+        public async Task<IActionResult> GetApprentice(Guid id)
+        {
+            var queryResult = await _mediator.Send(new GetApprenticeQuery
+            {
+                ApprenticeId = id
+            });
+
+            var response = queryResult.Apprentice;
+
+            return Ok(response);
         }
 
         [HttpPatch("/apprentices/{apprenticeId}")]
