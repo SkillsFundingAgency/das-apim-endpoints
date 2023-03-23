e2ceb7061053a5b557259603a5bfd8520b671aee
Deven Shah <devenshah1974@gmail.com>
2023-03-23 09:34:25 +0000

    CSP-645 Created solution structure for RoatpOversight (#1026)
    * CSP-645 Created solution structure for RoatpOversight
    
    * CSP-645 Implements post provider endpoint
    
    CSP-645 Implements create provider command handler
    
    * Changes to YAML
    
    * change to building blocks version
    
    ---------
    
    Co-authored-by: Furqan Agwan <>

---
+++ src/RoatpOversight/SFA.DAS.RoatpOversight.Api/Extensions/AddConfigurationOptionsExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Options;
+using SFA.DAS.SharedOuterApi.Configuration;
+
+namespace SFA.DAS.RoatpOversight.Api.Extensions;
+
+[ExcludeFromCodeCoverage]
+public static class AddConfigurationOptionsExtension
+{
+    public static IServiceCollection AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions();
+        services.Configure<RoatpV2ApiConfiguration>(configuration.GetSection(nameof(RoatpV2ApiConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<RoatpV2ApiConfiguration>>()!.Value);
+        return services;
+    }
+}
---
+++ src/RoatpOversight/SFA.DAS.RoatpOversight.Api/Extensions/AddControllersExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using SFA.DAS.SharedOuterApi.AppStart;
+
+namespace SFA.DAS.RoatpOversight.Api.Extensions;
+
+[ExcludeFromCodeCoverage]
+public static class AddControllersExtension
+{
+    public static IServiceCollection AddControllers(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddControllers(o =>
+        {
+            if (!configuration.IsLocalOrDev()) o.Filters.Add(new AuthorizeFilter("default"));
+        });
+        return services;
+    }
+}
