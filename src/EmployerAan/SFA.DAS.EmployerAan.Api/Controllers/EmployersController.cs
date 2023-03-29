9ed9e84b98453bdfbde077b88ba98abfb296d4e0
Deven Shah <devenshah1974@gmail.com>
2023-03-29 17:53:43 +0100

    CSP-417 Created Employer AAN Outer API Solution

---
+++ src/EmployerAan/SFA.DAS.EmployerAan.Api/Controllers/EmployersController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerAan.Application.Employer.Queries.GetEmployerMember;
+
+namespace SFA.DAS.EmployerAan.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployersController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public EmployersController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("{userRef}")]
+    [ProducesResponseType(typeof(GetEmployerMemberQueryResult), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Index(Guid userRef)
+    {
+        var employer = await _mediator.Send(new GetEmployerMemberQuery(userRef));
+        return Ok(employer);
+    }
+}
---
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Queries/GetEmployerMember/GetEmployerMemberQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SFA.DAS.EmployerAan.Configuration;
+using SFA.DAS.EmployerAan.InnerApi.Requests;
+using SFA.DAS.EmployerAan.Services;
+
+namespace SFA.DAS.EmployerAan.Application.Employer.Queries.GetEmployerMember;
+
+public class GetEmployerMemberQueryHandler : IRequestHandler<GetEmployerMemberQuery, GetEmployerMemberQueryResult>
+{
+    private readonly IAanHubApiClient<AanHubApiConfiguration> _aanHubApiClient;
+
+    public GetEmployerMemberQueryHandler(IAanHubApiClient<AanHubApiConfiguration> aanHubApiClient)
+    {
+        _aanHubApiClient = aanHubApiClient;
+    }
+
+    public Task<GetEmployerMemberQueryResult> Handle(GetEmployerMemberQuery request, CancellationToken cancellationToken)
+    {
+        return _aanHubApiClient.Get<GetEmployerMemberQueryResult>(new GetEmployerMemberRequest() { UserRef = request.UserRef });
+    }
+}
