2798f178f5234f5f372270ec7d6908df50154b2a
MarkFCain <mark.cain@digital.education.gov.uk>
2023-03-30 14:47:06 +0100

    CSP-686 removed feedback classes from RegisteredProviderModel details

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Models/RegisteredProvider/ProviderAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SFA.DAS.RoatpCourseManagement.InnerApi.Models.RegisteredProvider
-{
-    public class ProviderAttribute
-    {
-        public string Name { get; set; }
-        public int Strengths { get; set; }
-        public int Weaknesses { get; set; }
-    }
-}
