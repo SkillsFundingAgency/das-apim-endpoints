c74bfb983342e2d4255095694341e9a90fc04fd3
chrisfoster76 <chrisfoster186@googlemail.com>
2023-04-14 09:24:26 +0100

    Adds error handling in handler

--- src/Approvals/SFA.DAS.Approvals.UnitTests/Application/DraftApprenticeships/GetEditDraftApprenticeshipCourseQueryHandlerTests.cs
+++ src/Approvals/SFA.DAS.Approvals.UnitTests/Application/DraftApprenticeships/GetEditDraftApprenticeshipCourseQueryHandlerTests.cs
@@ -1,10 +1,12 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions.Common;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.Approvals.Application;
 using SFA.DAS.Approvals.Application.DraftApprenticeships.Queries.GetEditDraftApprenticeshipCourse;
 using SFA.DAS.Approvals.InnerApi.Requests;
 using SFA.DAS.Approvals.InnerApi.Responses;
@@ -12,7 +14,9 @@ using SFA.DAS.Approvals.Services;
 using SFA.DAS.Approvals.Types;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
+using Party = SFA.DAS.Approvals.Application.Shared.Enums.Party;
 
 namespace SFA.DAS.Approvals.UnitTests.Application.DraftApprenticeships
 {
@@ -25,11 +29,15 @@ namespace SFA.DAS.Approvals.UnitTests.Application.DraftApprenticeships
             GetCohortResponse cohort,
             ProviderStandardsData providerStandardsData,
             [Frozen] Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> apiClient,
-            [Frozen] Mock<IProviderStandardsService> providerStandardsService,
-            GetEditDraftApprenticeshipCourseQueryHandler handler)
+            [Frozen] Mock<IProviderStandardsService> providerStandardsService)
         {
-            apiClient.Setup(x => x.Get<GetCohortResponse>(It.Is<GetCohortRequest>(r => r.CohortId == query.CohortId)))
-                .ReturnsAsync(cohort);
+            var serviceParameters = new ServiceParameters(Party.Provider, cohort.ProviderId);
+
+            var handler = new GetEditDraftApprenticeshipCourseQueryHandler(apiClient.Object,
+                providerStandardsService.Object, serviceParameters);
+
+            apiClient.Setup(x => x.GetWithResponseCode<GetCohortResponse>(It.Is<GetCohortRequest>(r => r.CohortId == query.CohortId)))
+                .ReturnsAsync(new ApiResponse<GetCohortResponse>(cohort, HttpStatusCode.OK, string.Empty));
             
             providerStandardsService.Setup(x => x.GetStandardsData(cohort.ProviderId))
                 .ReturnsAsync(providerStandardsData);
@@ -45,11 +53,15 @@ namespace SFA.DAS.Approvals.UnitTests.Application.DraftApprenticeships
             GetCohortResponse cohort,
             ProviderStandardsData providerStandardsData,
             [Frozen] Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> apiClient,
-            [Frozen] Mock<IProviderStandardsService> providerStandardsService,
-            GetEditDraftApprenticeshipCourseQueryHandler handler)
+            [Frozen] Mock<IProviderStandardsService> providerStandardsService)
         {
-            apiClient.Setup(x => x.Get<GetCohortResponse>(It.Is<GetCohortRequest>(r => r.CohortId == query.CohortId)))
-                .ReturnsAsync(cohort);
+            var serviceParameters = new ServiceParameters(Party.Provider, cohort.ProviderId);
+
+            var handler = new GetEditDraftApprenticeshipCourseQueryHandler(apiClient.Object,
+                providerStandardsService.Object, serviceParameters);
+
+            apiClient.Setup(x => x.GetWithResponseCode<GetCohortResponse>(It.Is<GetCohortRequest>(r => r.CohortId == query.CohortId)))
+                .ReturnsAsync(new ApiResponse<GetCohortResponse>(cohort, HttpStatusCode.OK, string.Empty));
 
             providerStandardsService.Setup(x => x.GetStandardsData(cohort.ProviderId))
                 .ReturnsAsync(providerStandardsData);
@@ -57,6 +69,54 @@ namespace SFA.DAS.Approvals.UnitTests.Application.DraftApprenticeships
             var result = await handler.Handle(query, CancellationToken.None);
 
             result.IsMainProvider.IsSameOrEqualTo(providerStandardsData.IsMainProvider);
+        }
+
+        [Test, MoqAutoData]
+        public async Task When_CallingParty_Does_Not_Own_Cohort_Then_Result_Is_Null(
+            GetEditDraftApprenticeshipCourseQuery query,
+            GetCohortResponse cohort,
+            ProviderStandardsData providerStandardsData,
+            [Frozen] Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> apiClient,
+            [Frozen] Mock<IProviderStandardsService> providerStandardsService)
+        {
+            var serviceParameters = new ServiceParameters(Party.Provider, cohort.ProviderId + 1);
+
+            var handler = new GetEditDraftApprenticeshipCourseQueryHandler(apiClient.Object,
+                providerStandardsService.Object, serviceParameters);
+
+            apiClient.Setup(x => x.GetWithResponseCode<GetCohortResponse>(It.Is<GetCohortRequest>(r => r.CohortId == query.CohortId)))
+                .ReturnsAsync(new ApiResponse<GetCohortResponse>(cohort, HttpStatusCode.OK, string.Empty));
+
+            providerStandardsService.Setup(x => x.GetStandardsData(cohort.ProviderId))
+                .ReturnsAsync(providerStandardsData);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.IsNull(result);
+        }
+
+        [Test, MoqAutoData]
+        public async Task When_Cohort_Is_Not_Found_Then_Result_Is_Null(
+            GetEditDraftApprenticeshipCourseQuery query,
+            ProviderStandardsData providerStandardsData,
+            long providerId,
+            [Frozen] Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> apiClient,
+            [Frozen] Mock<IProviderStandardsService> providerStandardsService)
+        {
+            var serviceParameters = new ServiceParameters(Party.Provider, providerId);
+
+            var handler = new GetEditDraftApprenticeshipCourseQueryHandler(apiClient.Object,
+                providerStandardsService.Object, serviceParameters);
+
+            apiClient.Setup(x => x.GetWithResponseCode<GetCohortResponse>(It.Is<GetCohortRequest>(r => r.CohortId == query.CohortId)))
+                .ReturnsAsync(new ApiResponse<GetCohortResponse>(null, HttpStatusCode.NotFound, "Test - not found"));
+
+            providerStandardsService.Setup(x => x.GetStandardsData(providerId))
+                .ReturnsAsync(providerStandardsData);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            Assert.IsNull(result);
         }
     }
 }
--- src/Approvals/SFA.DAS.Approvals/Application/DraftApprenticeships/Queries/GetEditDraftApprenticeshipCourse/GetEditDraftApprenticeshipCourseQueryHandler.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/DraftApprenticeships/Queries/GetEditDraftApprenticeshipCourse/GetEditDraftApprenticeshipCourseQueryHandler.cs
@@ -1,11 +1,14 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using SFA.DAS.Approvals.Extensions;
 using SFA.DAS.Approvals.InnerApi.Requests;
 using SFA.DAS.Approvals.InnerApi.Responses;
 using SFA.DAS.Approvals.Services;
 using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.Approvals.Application.DraftApprenticeships.Queries.GetEditDraftApprenticeshipCourse
@@ -14,16 +17,35 @@ namespace SFA.DAS.Approvals.Application.DraftApprenticeships.Queries.GetEditDraf
     {
         private readonly ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> _apiClient;
         private readonly IProviderStandardsService _providerStandardsService;
+        private readonly ServiceParameters _serviceParameters;
 
-        public GetEditDraftApprenticeshipCourseQueryHandler(ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> apiClient, IProviderStandardsService providerStandardsService)
+        public GetEditDraftApprenticeshipCourseQueryHandler(
+            ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> apiClient,
+            IProviderStandardsService providerStandardsService,
+            ServiceParameters serviceParameters)
         {
             _apiClient = apiClient;
             _providerStandardsService = providerStandardsService;
+            _serviceParameters = serviceParameters;
         }
 
         public async Task<GetEditDraftApprenticeshipCourseQueryResult> Handle(GetEditDraftApprenticeshipCourseQuery request, CancellationToken cancellationToken)
         {
-            var cohort = await _apiClient.Get<GetCohortResponse>(new GetCohortRequest(request.CohortId));
+            var cohortResponse = await _apiClient.GetWithResponseCode<GetCohortResponse>(new GetCohortRequest(request.CohortId));
+
+            if (cohortResponse.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+            cohortResponse.EnsureSuccessStatusCode();
+
+            var cohort = cohortResponse.Body;
+
+            if (!cohort.CheckParty(_serviceParameters))
+            {
+                return null;
+            }
 
             var providerStandardsData = await _providerStandardsService.GetStandardsData(cohort.ProviderId);
 
