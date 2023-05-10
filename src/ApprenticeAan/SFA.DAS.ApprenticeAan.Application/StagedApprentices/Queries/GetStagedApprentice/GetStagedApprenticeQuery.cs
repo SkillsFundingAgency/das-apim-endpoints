7478eae6ba208c2859663d3e1cdb3a31fa2286eb
Deven Shah <devenshah1974@gmail.com>
2023-05-10 23:29:07 +0100

    CSP-790 Implements GetStagedApprentice endpoint

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/StagedApprentices/Queries/GetStagedApprentice/GetStagedApprenticeQuery.cs
@@ -0,0 +1,4 @@
+﻿using MediatR;
+
+namespace SFA.DAS.ApprenticeAan.Application.StagedApprentices.Queries.GetStagedApprentice;
+public record GetStagedApprenticeQuery(string LastName, DateTime DateOfBirth, string Email) : IRequest<GetStagedApprenticeQueryResult?>;
