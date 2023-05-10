2f2ea6f4b77850604b3b321db3f40b2c4e6bbe6c
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-05-10 08:19:02 +0100

    CSP-775 Added test coverage

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application.UnitTests/Locations/Queries/GetPostcodes/GetPostcodeQueryHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.ApprenticeAan.Application.Locations.Queries.GetPostcodes;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.ApprenticeAan.Application.UnitTests.Locations.Queries.GetPostcodes;
+
+public class GetPostcodeQueryHandlerTests
+{
+    [Test]
+    [MoqAutoData]
+    public async Task Handle_ReturnPostcodeBasedOnQuery(
+        GetPostcodeQuery query,
+        [Frozen] Mock<ILocationApiClient<LocationApiConfiguration>> apiClient,
+        [Frozen(Matching.ImplementedInterfaces)] GetPostcodeQueryHandler handler,
+        GetAddressesListResponse apiResponse)
+    {
+        apiClient
+            .Setup(x => x.Get<GetAddressesListResponse>(It.IsAny<GetAddressesQueryRequest>()))
+            .ReturnsAsync(apiResponse);
+
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        result.Coordinates.Should().NotBeNull();
+    }
+
+    [Test]
+    [MoqAutoData]
+    public async Task Handle_PostcodesNotFound_ReturnsEmptyResult(
+        GetPostcodeQuery query,
+        [Frozen] Mock<ILocationApiClient<LocationApiConfiguration>> apiClient,
+        [Frozen(Matching.ImplementedInterfaces)] GetPostcodeQueryHandler handler)
+    {
+        var apiResponse = new GetAddressesListResponse { Addresses = Enumerable.Empty<GetAddressesListItem>() };
+        apiClient
+            .Setup(x => x.Get<GetAddressesListResponse>(It.IsAny<GetAddressesQueryRequest>()))
+            .ReturnsAsync(apiResponse);
+
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        result.Should().BeNull();
+    }
+}
