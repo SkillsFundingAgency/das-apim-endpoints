64f9d059b55cc8731454d64f253329097d450664
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-05-18 13:36:38 +0100

    CSP-807 Added Test coverage

---
+++ src/EmployerAan/SFA.DAS.EmployerAan.Api.UnitTests/Controllers/ProfilesControllerTests/ProfilesControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.EmployerAan.Api.Controllers;
+using SFA.DAS.EmployerAan.Application.Profiles.Queries.GetProfilesByUserType;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerAan.Api.UnitTests.Controllers.ProfilesControllerTests
+{
+    public class ProfilesControllerTests
+    {
+        [Test]
+        [MoqAutoData]
+        public async Task And_MediatorCommandSuccessful_Then_ReturnOk(GetProfilesByUserTypeQueryResult response,
+            [Frozen] Mock<IMediator> mockMediator)
+        {
+            mockMediator.Setup(m => m.Send(It.IsAny<GetProfilesByUserTypeQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(response);
+            var controller = new ProfilesController(mockMediator.Object);
+
+            var userType = "employer";
+            var result = await controller.GetProfilesByUserType(userType) as OkObjectResult;
+
+            result.Should().NotBeNull();
+
+            var model = result?.Value;
+
+            model.Should().BeEquivalentTo(response);
+        }
+    }
+}
---
+++ src/EmployerAan/SFA.DAS.EmployerAan.UnitTests/Application/Profiles/Queries/GetProfilesByUserType/GetProfilesByUserTypeQueryHandlerTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.EmployerAan.Application.Profiles.Queries.GetProfilesByUserType;
+using SFA.DAS.EmployerAan.Configuration;
+using SFA.DAS.EmployerAan.InnerApi.Profiles;
+using SFA.DAS.EmployerAan.Services;
+using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerAan.UnitTests.Application.Profiles.Queries.GetProfilesByUserType
+{
+    public class GetProfilesByUserTypeQueryHandlerTests
+    {
+        [Test]
+        [MoqAutoData]
+        public async Task Handle_ReturnAllProfiles(
+            GetProfilesByUserTypeQuery query,
+            [Frozen] Mock<IAanHubApiClient<AanHubApiConfiguration>> apiClient,
+            GetProfilesByUserTypeQueryHandler handler,
+            GetProfilesByUserTypeQueryResult response)
+        {
+            apiClient.Setup(x =>
+                            x.GetWithResponseCode<GetProfilesByUserTypeQueryResult>(It.IsAny<GetProfilesByUserTypeRequest>()))
+                            .ReturnsAsync(new ApiResponse<GetProfilesByUserTypeQueryResult>(response, HttpStatusCode.OK, string.Empty));
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result?.Profiles?.Count.Should().NotBe(0);
+            result?.Profiles?.Count.Should().Be(response.Profiles.Count);
+        }
+
+        [Test]
+        [MoqAutoData]
+        public async Task Handle_ProfilesNotFoundOrError(
+            GetProfilesByUserTypeQuery query,
+            [Frozen] Mock<IAanHubApiClient<AanHubApiConfiguration>> apiClient,
+            GetProfilesByUserTypeQueryHandler handler)
+        {
+            apiClient.Setup(x =>
+                            x.GetWithResponseCode<GetProfilesByUserTypeQueryResult>(It.IsAny<GetProfilesByUserTypeRequest>()))
+                            .ReturnsAsync(new ApiResponse<GetProfilesByUserTypeQueryResult>(null!, HttpStatusCode.BadRequest, string.Empty));
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Should().Be(null);
+            result?.Profiles.Should().BeNull();
+        }
+    }
+}
