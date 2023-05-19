47f539f72f84c20220d036df3e1552c2207b2191
Deven Shah <devenshah1974@gmail.com>
2023-05-19 10:43:34 +0100

    CSP-785 Created new RestEase client for Aan Hub (#1064)
    * CSP-785 Added RestEase client for aan hub inner api
    
    * CSP-785 Refactored profile & regions to use new api client
    
    * CSP-785 Fixed broken tests
    
    * CSP-785 Excluded headers handler from coverage

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/ProfilesControllerTests.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/ProfilesControllerTests.cs
@@ -7,26 +7,23 @@ using SFA.DAS.ApprenticeAan.Api.Controllers;
 using SFA.DAS.ApprenticeAan.Application.Profiles.Queries.GetProfilesByUserType;
 using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers
+namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers;
+
+public class ProfilesControllerTests
 {
-    public class ProfilesControllerTests
+    [Test]
+    [MoqAutoData]
+    public async Task And_MediatorCommandSuccessful_Then_ReturnOk(
+        GetProfilesByUserTypeQueryResult response,
+        [Frozen] Mock<IMediator> mockMediator,
+        CancellationToken cancellationToken)
     {
-        [Test]
-        [MoqAutoData]
-        public async Task And_MediatorCommandSuccessful_Then_ReturnOk(GetProfilesByUserTypeQueryResult response,
-            [Frozen] Mock<IMediator> mockMediator)
-        {
-            mockMediator.Setup(m => m.Send(It.IsAny<GetProfilesByUserTypeQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(response);
-            var controller = new ProfilesController(mockMediator.Object);
+        mockMediator.Setup(m => m.Send(It.IsAny<GetProfilesByUserTypeQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(response);
+        var controller = new ProfilesController(mockMediator.Object);
+        var userType = "Apprentice";
 
-            var userType = "Apprentice";
-            var result = await controller.GetProfilesByUserType(userType) as OkObjectResult;
+        var result = await controller.GetProfilesByUserType(userType, cancellationToken);
 
-            result.Should().NotBeNull();
-
-            var model = result?.Value;
-
-            model.Should().BeEquivalentTo(response);
-        }
+        result.As<OkObjectResult>().Value.Should().BeEquivalentTo(response);
     }
 }
