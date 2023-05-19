06ced0cf4dd5c32f197e78b89f63708b30f3a8cb
Deven Shah <devenshah1974@gmail.com>
2023-05-19 08:24:52 +0100

    CSP-785 Fixed broken tests

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/RegionsControllerTests.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/RegionsControllerTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Net;
-using AutoFixture.NUnit3;
+﻿using AutoFixture.NUnit3;
 using FluentAssertions;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -8,39 +7,22 @@ using SFA.DAS.ApprenticeAan.Api.Controllers;
 using SFA.DAS.ApprenticeAan.Application.Regions.Queries.GetRegions;
 using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers
+namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers;
+
+public class RegionsControllerTests
 {
-    public class RegionsControllerTests
+    [Test]
+    [MoqAutoData]
+    public async Task And_MediatorCommandSuccessful_Then_ReturnOk(
+        GetRegionsQueryResult response,
+        [Frozen] Mock<IMediator> mockMediator,
+        CancellationToken cancellationToken)
     {
-        [Test]
-        [MoqAutoData]
-        public async Task And_MediatorCommandSuccessful_Then_ReturnOk(GetRegionsQueryResult response,
-            [Frozen] Mock<IMediator> mockMediator)
-        {
-            mockMediator.Setup(m => m.Send(It.IsAny<GetRegionsQuery>(), It.IsAny<CancellationToken>())).ReturnsAsync(response);
-            var controller = new RegionsController(mockMediator.Object);
+        mockMediator.Setup(m => m.Send(It.IsAny<GetRegionsQuery>(), cancellationToken)).ReturnsAsync(response);
+        var controller = new RegionsController(mockMediator.Object);
 
-            var result = await controller.GetRegions() as OkObjectResult;
+        var result = await controller.GetRegions(cancellationToken);
 
-            result.Should().NotBeNull();
-
-            var model = result?.Value;
-
-            model.Should().BeEquivalentTo(response);
-        }
-
-        [Test]
-        [MoqAutoData]
-        public async Task And_NoRegionsReturnedFromMediator_Then_Return500(
-            [Frozen] Mock<IMediator> mockMediator,
-            [Greedy] RegionsController controller)
-        {
-            mockMediator.Setup(m => m.Send(It.IsAny<GetRegionsQuery>(), CancellationToken.None))
-                .ReturnsAsync((GetRegionsQueryResult)null!);
-
-            var controllerResult = await controller.GetRegions() as NotFoundResult;
-
-            controllerResult?.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
-        }
+        result.As<OkObjectResult>().Value.Should().BeEquivalentTo(response);
     }
 }
