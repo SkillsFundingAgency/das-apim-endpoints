630b13d6b0ebdc3d4bb9354250f0c755f8f4bdc8
ratheeshri1984 <ratheeshri1984@gmail.com>
2023-05-22 12:25:18 +0100

    MAP-231-Estimated transfer balance

--- src/EmployerFinance/SFA.DAS.EmployerFinance/Application/Queries/Transfers/GetCounts/GetCountsQueryHandler.cs
+++ src/EmployerFinance/SFA.DAS.EmployerFinance/Application/Queries/Transfers/GetCounts/GetCountsQueryHandler.cs
@@ -4,6 +4,7 @@ using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.Interfaces;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -13,10 +14,12 @@ namespace SFA.DAS.EmployerFinance.Application.Queries.Transfers.GetCounts
     public class GetCountsQueryHandler : IRequestHandler<GetCountsQuery, GetCountsQueryResult>
     {
         private readonly ILevyTransferMatchingApiClient<LevyTransferMatchingApiConfiguration> _levyTransferMatchingApiClient;
+        private readonly IForecastingApiClient<ForecastingApiConfiguration> _forecastingApiClient;
 
-        public GetCountsQueryHandler(ILevyTransferMatchingApiClient<LevyTransferMatchingApiConfiguration> levyTransferMatchingApiClient)
+        public GetCountsQueryHandler(ILevyTransferMatchingApiClient<LevyTransferMatchingApiConfiguration> levyTransferMatchingApiClient, IForecastingApiClient<ForecastingApiConfiguration> forecastingApiClient)
         {
             _levyTransferMatchingApiClient = levyTransferMatchingApiClient;
+            _forecastingApiClient = forecastingApiClient;
         }
 
         public async Task<GetCountsQueryResult> Handle(GetCountsQuery request, CancellationToken cancellationToken)
@@ -28,13 +31,26 @@ namespace SFA.DAS.EmployerFinance.Application.Queries.Transfers.GetCounts
                 AccountId = request.AccountId
             });
 
-            await Task.WhenAll(pledgesTask, applicationsTask);
+
+            var breakdownTask = _forecastingApiClient.Get<GetTransferFinancialBreakdownResponse>
+                (new GetTransferFinancialBreakdownRequest(request.AccountId, DateTime.UtcNow));
+
+            await Task.WhenAll(breakdownTask, pledgesTask, applicationsTask);
 
             return new GetCountsQueryResult
             {
                 PledgesCount = pledgesTask.Result.TotalPledges,
-                ApplicationsCount = applicationsTask.Result.Applications.Count()
+                ApplicationsCount = applicationsTask.Result.Applications.Count(),
+                CurrentYearEstimatedCommittedSpend = CalculateEstimatedCommittedSpend(breakdownTask.Result.Breakdown),
             };
+        }
+
+        private decimal CalculateEstimatedCommittedSpend(List<GetTransferFinancialBreakdownResponse.BreakdownDetails> breakdown)
+        {
+            return breakdown.Sum(x => x.FundsOut.ApprovedPledgeApplications) +
+                   breakdown.Sum(x => x.FundsOut.AcceptedPledgeApplications) +
+                   breakdown.Sum(x => x.FundsOut.PledgeOriginatedCommitments) +
+                   breakdown.Sum(x => x.FundsOut.TransferConnections);
         }
     }
 }
