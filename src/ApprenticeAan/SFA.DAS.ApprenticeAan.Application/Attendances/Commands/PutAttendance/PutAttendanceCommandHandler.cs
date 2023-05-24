9a2e532998ad230b6a6b49238c87c9412935ecd4
SawNawDfE <119664159+SawNawDfE@users.noreply.github.com>
2023-05-24 11:58:21 +0100

    CSP-772 Added PUT Attendance to Apprentice AAN (#1068)

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Attendances/Commands/PutAttendance/PutAttendanceCommandHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SFA.DAS.ApprenticeAan.Application.Infrastructure;
+
+namespace SFA.DAS.ApprenticeAan.Application.Attendances.Commands.PutAttendance;
+
+public class PutAttendanceCommandHandler : IRequestHandler<PutAttendanceCommand, Unit>
+{
+    private readonly IAanHubRestApiClient _apiClient;
+    public PutAttendanceCommandHandler(IAanHubRestApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    public async Task<Unit> Handle(PutAttendanceCommand command, CancellationToken cancellationToken)
+    {
+        await _apiClient.PutAttendance(command.CalendarEventId, command.RequestedByMemberId, command.AttendanceStatus, cancellationToken);
+        return Unit.Value;
+    }
+}
