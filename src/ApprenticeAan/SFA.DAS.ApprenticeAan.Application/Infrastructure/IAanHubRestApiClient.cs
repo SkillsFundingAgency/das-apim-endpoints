a57df2e4d4751ceefbff806945d52fbe5c7598c2
Saw Naw <waterchan@gmail.com>
2023-05-30 14:31:40 +0100

    CSP-811 Remove unused using

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Infrastructure/IAanHubRestApiClient.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Infrastructure/IAanHubRestApiClient.cs
@@ -1,7 +1,5 @@
 ﻿using RestEase;
-using SFA.DAS.ApprenticeAan.Application.CalendarEvents.Queries.GetCalendarEventById;
 using SFA.DAS.ApprenticeAan.Application.Apprentices.Commands.CreateApprenticeMember;
-using SFA.DAS.ApprenticeAan.Application.CalendarEvents.Queries.GetCalendarEventById;
 using SFA.DAS.ApprenticeAan.Application.CalendarEvents.Queries.GetCalendarEvents;
 using SFA.DAS.ApprenticeAan.Application.Infrastructure.Configuration;
 using SFA.DAS.ApprenticeAan.Application.InnerApi.Attendances;
