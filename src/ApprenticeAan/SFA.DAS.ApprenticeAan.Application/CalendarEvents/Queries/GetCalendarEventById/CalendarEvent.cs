5d6e6cd34a11520231aaac48d6d1d674c20c0a94
Saw Naw <waterchan@gmail.com>
2023-05-31 14:29:44 +0100

    CSP-749 Modify CalendarEvents to match inner API

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/CalendarEvents/Queries/GetCalendarEventById/CalendarEvent.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/CalendarEvents/Queries/GetCalendarEventById/CalendarEvent.cs
@@ -6,18 +6,20 @@ public class CalendarEvent
 {
     public Guid CalendarEventId { get; set; }
     public string CalendarName { get; set; } = null!;
-    public string EventFormat { get; set; } = null!;
-    public DateTime Start { get; set; }
-    public DateTime End { get; set; }
-
-    public string Description { get; set; } = null!;
-    public string Summary { get; set; } = null!;
-    public string Location { get; set; } = null!;
-    public string Postcode { get; set; } = null!;
-    public double? Longitude { get; set; }
+    public string EventFormat { get; set; } = string.Empty;
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string? Summary { get; set; }
+    public int? RegionId { get; set; }
+    public string? Location { get; set; }
+    public string? Postcode { get; set; }
     public double? Latitude { get; set; }
-    public double? Distance { get; set; }
-    public bool IsAttending { get; set; }
+    public double? Longitude { get; set; }
+    public string? EventLink { get; set; }
+    public string ContactName { get; set; } = string.Empty;
+    public string ContactEmail { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
     public string? CancelReason { get; set; } = null!;
     public IEnumerable<Attendee> Attendees { get; set; } = null!;
     public IEnumerable<EventGuest> EventGuests { get; set; } = null!;
