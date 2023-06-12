f21665d0d8c9579aa066024b1cdcd9b7ebb383ff
MarkFCain <mark.cain@digital.education.gov.uk>
2023-06-12 09:35:53 +0100

    CSP-731 removing duplicate directive

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/CalendarEventsController.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/CalendarEventsController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.ApprenticeAan.Application.Attendances.Commands.PutAttendance;
 using SFA.DAS.ApprenticeAan.Application.CalendarEvents.Queries.GetCalendarEventById;
-using SFA.DAS.ApprenticeAan.Application.InnerApi.Attendances;
 using SFA.DAS.ApprenticeAan.Application.CalendarEvents.Queries.GetCalendarEvents;
 using SFA.DAS.ApprenticeAan.Application.Infrastructure.Configuration;
 using SFA.DAS.ApprenticeAan.Application.InnerApi.Attendances;
