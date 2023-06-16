5e68196d657cb784e3b12a7abf87dd697d0c392c
MarkFCain <mark.cain@digital.education.gov.uk>
2023-06-16 11:10:34 +0100

    CSP-731 updated mapping for effective from and to fields

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Calendars/Queries/GetCalendars/CalendarModel.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Calendars/Queries/GetCalendars/CalendarModel.cs
@@ -5,8 +5,8 @@ public class CalendarModel
 {
     public int Id { get; set; }
     public string CalendarName { get; set; } = string.Empty;
-    public DateTime EffectiveFromDate { get; set; }
-    public DateTime? EffectiveToDate { get; set; }
+    public DateTime EffectiveFrom { get; set; }
+    public DateTime? EffectiveTo { get; set; }
     public int Ordering { get; set; }
 
     public static implicit operator CalendarModel(Calendar source) =>
@@ -14,8 +14,8 @@ public class CalendarModel
         {
             Id = source.Id,
             CalendarName = source.CalendarName,
-            EffectiveFromDate = source.EffectiveFromDate,
-            EffectiveToDate = source.EffectiveToDate,
+            EffectiveFrom = source.EffectiveFrom,
+            EffectiveTo = source.EffectiveTo,
             Ordering = source.Ordering
         };
 }
--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Entities/Calendar.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Entities/Calendar.cs
@@ -4,7 +4,7 @@ public class Calendar
 {
     public int Id { get; set; }
     public string CalendarName { get; set; } = string.Empty;
-    public DateTime EffectiveFromDate { get; set; }
-    public DateTime? EffectiveToDate { get; set; }
+    public DateTime EffectiveFrom { get; set; }
+    public DateTime? EffectiveTo { get; set; }
     public int Ordering { get; set; }
 }
