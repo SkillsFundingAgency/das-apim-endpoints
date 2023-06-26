9407628a05f39c44d134736710ba860bfcad1d72
Deven Shah <devenshah1974@gmail.com>
2023-06-26 18:24:39 +0100

    CSP-717 Added endpoint to query accounts

---
+++ src/EmployerAan/SFA.DAS.EmployerAan.Api/Controllers/EmployerAccountsController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerAan.Application.User.GetUserAccounts;
+
+namespace SFA.DAS.EmployerAan.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployerAccountsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public EmployerAccountsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("{userId}/accounts")]
+    public async Task<IActionResult> GetUserAccounts(string userId, [FromQuery] string email)
+    {
+        var result = await _mediator.Send(new GetUserAccountsQuery(userId, email));
+
+        return Ok(result);
+    }
+}
