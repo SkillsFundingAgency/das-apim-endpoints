5c7a1f345e51aa8a6835c18005c7e62be87db25b
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-07-13 09:57:31 +0100

    CSP-648 Implemented Post CreateEmployer

---
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Commands/CreateEmployerMember/CreateEmployerMemberCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace SFA.DAS.EmployerAan.Application.Employer.Commands.CreateEmployerMember;
+
+public class CreateEmployerMemberCommand : IRequest<CreateEmployerMemberCommandResult>
+{
+    public Guid EmployerId { get; set; }
+    public string Email { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime JoinedDate { get; set; }
+    public int RegionId { get; set; }
+    public string OrganisationName { get; set; } = null!;
+    public int NumberOfActiveApprentices { get; set; }
+    public List<ProfileValue> ProfileValues { get; set; } = new();
+}
+public record class ProfileValue(int Id, string Value);
