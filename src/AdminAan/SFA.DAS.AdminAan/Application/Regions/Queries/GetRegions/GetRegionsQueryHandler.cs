f6d8a56ffb3b20c1249b2a45ab52c1db25a4c8b0
Deven Shah <devenshah1974@gmail.com>
2023-07-13 12:16:26 +0100

    CSP-900 Added Admin AAN Outer API Solution

---
+++ src/AdminAan/SFA.DAS.AdminAan/Application/Regions/Queries/GetRegions/GetRegionsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SFA.DAS.AdminAan.Infrastructure;
+
+namespace SFA.DAS.AdminAan.Application.Regions.Queries.GetRegions;
+
+public class GetRegionsQueryHandler : IRequestHandler<GetRegionsQuery, GetRegionsQueryResult>
+{
+    private readonly IAanHubRestApiClient _apiClient;
+
+    public GetRegionsQueryHandler(IAanHubRestApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    public async Task<GetRegionsQueryResult> Handle(GetRegionsQuery request, CancellationToken cancellationToken)
+    {
+        return await _apiClient.GetRegions(cancellationToken);
+    }
+}
