4d8754c55c2612419a0160d21eb940348ebe16eb
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-07-17 13:20:22 +0100

    CSP-648 Resolved PR comments

--- src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Commands/CreateEmployerMember/CreateEmployerMemberCommand.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Commands/CreateEmployerMember/CreateEmployerMemberCommand.cs
@@ -5,13 +5,12 @@ namespace SFA.DAS.EmployerAan.Application.Employer.Commands.CreateEmployerMember
 public class CreateEmployerMemberCommand : IRequest<CreateEmployerMemberCommandResult>
 {
     public Guid EmployerId { get; set; }
-    public string Email { get; set; } = null!;
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public DateTime JoinedDate { get; set; }
-    public int RegionId { get; set; }
-    public string OrganisationName { get; set; } = null!;
-    public int NumberOfActiveApprentices { get; set; }
+    public string? Email { get; set; } = null!;
+    public string? FirstName { get; set; } = null!;
+    public string? LastName { get; set; } = null!;
+    public DateTime? JoinedDate { get; set; }
+    public int? RegionId { get; set; }
+    public string? OrganisationName { get; set; } = null!;
     public List<ProfileValue> ProfileValues { get; set; } = new();
 }
 public record class ProfileValue(int Id, string Value);
