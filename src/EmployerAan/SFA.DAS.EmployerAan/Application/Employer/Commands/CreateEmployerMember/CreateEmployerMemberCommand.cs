2863ccba710d26227582adbe484fcb962f887c17
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-07-17 13:23:57 +0100

    CSP-648 Resolved PR comments, updated few nullable fields

--- src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Commands/CreateEmployerMember/CreateEmployerMemberCommand.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Commands/CreateEmployerMember/CreateEmployerMemberCommand.cs
@@ -5,10 +5,10 @@ namespace SFA.DAS.EmployerAan.Application.Employer.Commands.CreateEmployerMember
 public class CreateEmployerMemberCommand : IRequest<CreateEmployerMemberCommandResult>
 {
     public Guid EmployerId { get; set; }
-    public string? Email { get; set; } = null!;
-    public string? FirstName { get; set; } = null!;
-    public string? LastName { get; set; } = null!;
-    public DateTime? JoinedDate { get; set; }
+    public string Email { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public DateTime JoinedDate { get; set; }
     public int? RegionId { get; set; }
     public string? OrganisationName { get; set; } = null!;
     public List<ProfileValue> ProfileValues { get; set; } = new();
