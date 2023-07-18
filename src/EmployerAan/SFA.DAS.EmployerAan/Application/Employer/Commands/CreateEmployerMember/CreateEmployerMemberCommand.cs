7c9398ac507c59441d1c50f58f91aabbc8395c5a
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-07-18 09:45:11 +0100

    CSP-648 Added missing fields AccountId,UserRef

--- src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Commands/CreateEmployerMember/CreateEmployerMemberCommand.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/Employer/Commands/CreateEmployerMember/CreateEmployerMemberCommand.cs
@@ -12,5 +12,7 @@ public class CreateEmployerMemberCommand : IRequest<CreateEmployerMemberCommandR
     public int? RegionId { get; set; }
     public string? OrganisationName { get; set; } = null!;
     public List<ProfileValue> ProfileValues { get; set; } = new();
+    public long AccountId { get; init; }
+    public Guid UserRef { get; init; }
 }
 public record class ProfileValue(int Id, string Value);
