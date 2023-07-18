b92c58d46adcc2485fa5d622bd232d81bc8034d2
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2023-07-18 10:16:24 +0100

    CSP-783 GetCalendarEvents page and pagesize added (#1093)
    * CSP-783 GetCalendarEvents page and pagesize added
    
    * CSP-783 adding test for mapping model to query
    
    * CSP-783 corrected casing in parameter list
    
    * CSP-783 added coverage for null values in model
    
    * CSP-783 removed long set of parameters and converted to dictionary
    
    * CSP-783 parameter building service added and coverage added
    
    * CSP-783 changes and coverage extended based on TR comments
    
    * CSP-783 fixed issues in naming and namespace in the test suite, as raised in TR
    
    * CSP-783 moved test in wrong namespace and added extra test around implicit operator test
    
    * CSP-783 fix to model operator tests
    
    * CSP-783 removed unneeded element from test

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Models/GetCalendarEventsQueryTests.cs
@@ -0,0 +1,39 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using SFA.DAS.ApprenticeAan.Api.Models;
+using SFA.DAS.ApprenticeAan.Application.CalendarEvents.Queries.GetCalendarEvents;
+
+namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Models;
+public class GetCalendarEventsRequestModelTests
+{
+
+    [Test, AutoData]
+    public void Operator_PopulatesQueryFromModel(GetCalendarEventsRequestModel model)
+    {
+        var query = (GetCalendarEventsQuery)model;
+
+        query.RequestedByMemberId.Should().Be(model.RequestedByMemberId);
+        query.FromDate.Should().Be(model.FromDate?.ToString("yyyy-MM-dd"));
+        query.ToDate.Should().Be(model.ToDate?.ToString("yyyy-MM-dd"));
+        query.EventFormat.Should().BeEquivalentTo(model.EventFormat);
+        query.Page.Should().Be(model.Page);
+        query.PageSize.Should().Be(model.PageSize);
+    }
+
+    [Test, AutoData]
+    public void Operator_PopulatesQueryFromModelWithNulls(Guid memberId)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = memberId,
+        };
+
+        var query = (GetCalendarEventsQuery)model;
+
+        query.RequestedByMemberId.Should().Be(memberId);
+        query.FromDate.Should().BeNull();
+        query.ToDate.Should().BeNull();
+        query.Page.Should().BeNull();
+        query.PageSize.Should().BeNull();
+    }
+}
---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Services/QueryStringParameterBuilderTests.cs
@@ -0,0 +1,227 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using SFA.DAS.ApprenticeAan.Api.Models;
+using SFA.DAS.ApprenticeAan.Application.CalendarEvents.Queries.GetCalendarEvents;
+using SFA.DAS.ApprenticeAan.Application.Common;
+using SFA.DAS.ApprenticeAan.Application.Services;
+
+namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Services;
+
+public class QueryStringParameterBuilderTests
+{
+
+    [Test, AutoData]
+    public void Operator_PopulatesModelFromParameters(Guid memberId, DateTime? fromDate, DateTime? toDate,
+        List<EventFormat> eventFormats, List<int> calendarIds, List<int> regionIds, int? page, int? pageSize)
+    {
+        var sut = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = memberId,
+            FromDate = fromDate,
+            ToDate = toDate,
+            EventFormat = eventFormats,
+            CalendarId = calendarIds,
+            RegionId = regionIds,
+            Page = page,
+            PageSize = pageSize
+        };
+
+        var query = (GetCalendarEventsQuery)sut;
+
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(query);
+
+        parameters.TryGetValue("fromDate", out string[]? fromDateResult);
+        fromDateResult![0].Should().Be(fromDate?.ToString("yyyy-MM-dd"));
+
+        parameters.TryGetValue("toDate", out var toDateResult);
+        toDateResult![0].Should().Be(toDate?.ToString("yyyy-MM-dd"));
+
+        parameters.TryGetValue("eventFormat", out var eventFormatResult);
+        eventFormatResult!.Length.Should().Be(eventFormats.Count);
+        eventFormats.Select(x => x.ToString()).Should().BeEquivalentTo(eventFormatResult.ToList());
+
+        parameters.TryGetValue("calendarId", out var calendarIdsResult);
+        calendarIdsResult!.Length.Should().Be(calendarIds.Count);
+        calendarIds.Select(x => x.ToString()).Should().BeEquivalentTo(calendarIdsResult.ToList());
+
+        parameters.TryGetValue("regionId", out var regionIdsResult);
+        regionIdsResult!.Length.Should().Be(regionIds.Count);
+        regionIds.Select(x => x.ToString()).Should().BeEquivalentTo(regionIdsResult.ToList());
+
+        parameters.TryGetValue("page", out string[]? pageResult);
+        pageResult![0].Should().Be(page?.ToString());
+
+        parameters.TryGetValue("pageSize", out string[]? pageSizeResult);
+        pageSizeResult![0].Should().Be(pageSize?.ToString());
+    }
+
+    [TestCase(null)]
+    [TestCase("2030-06-01")]
+    public void Builder_ConstructParameters_FromDate(DateTime? fromDate)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = Guid.NewGuid(),
+            FromDate = fromDate
+        };
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(model);
+
+        parameters.TryGetValue("fromDate", out var fromDateResult);
+        if (fromDate != null)
+        {
+            fromDateResult![0].Should().Be(fromDate?.ToString("yyyy-MM-dd"));
+        }
+        else
+        {
+            fromDateResult.Should().BeNull();
+        }
+    }
+
+    [TestCase(null)]
+    [TestCase("2030-06-01")]
+    public void Builder_ConstructParameters_ToDate(DateTime? toDate)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = Guid.NewGuid(),
+            ToDate = toDate
+        };
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(model);
+
+
+        parameters.TryGetValue("toDate", out var toDateResult);
+        if (toDate != null)
+        {
+            toDateResult![0].Should().Be(toDate?.ToString("yyyy-MM-dd"));
+        }
+        else
+        {
+            toDateResult.Should().BeNull();
+        }
+    }
+
+    [TestCase(null)]
+    [TestCase(EventFormat.Hybrid)]
+    public void Builder_ConstructParameters_EventFormats(EventFormat? eventFormat)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = Guid.NewGuid(),
+        };
+
+        if (eventFormat != null)
+        {
+            model.EventFormat = new List<EventFormat> { eventFormat.Value };
+        }
+
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(model);
+
+        parameters.TryGetValue("eventFormat", out var eventFormatResult);
+        if (eventFormatResult != null)
+        {
+            eventFormatResult![0].Should().BeEquivalentTo(model.EventFormat[0].ToString());
+        }
+        else
+        {
+            eventFormatResult.Should().BeNull();
+        }
+    }
+
+    [TestCase(null)]
+    [TestCase(1)]
+    public void Builder_ConstructParameters_CalendarIds(int? calendarId)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = Guid.NewGuid(),
+        };
+
+        if (calendarId != null)
+        {
+            model.CalendarId = new List<int> { calendarId.Value };
+        }
+
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(model);
+
+        parameters.TryGetValue("calendarId", out var calendarIdResult);
+        if (calendarIdResult != null)
+        {
+            calendarIdResult![0].Should().BeEquivalentTo(model.CalendarId[0].ToString());
+        }
+        else
+        {
+            calendarIdResult.Should().BeNull();
+        }
+    }
+
+    [TestCase(null)]
+    [TestCase(1)]
+    public void Builder_ConstructParameters_RegionIds(int? regionId)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = Guid.NewGuid(),
+        };
+
+        if (regionId != null)
+        {
+            model.RegionId = new List<int> { regionId.Value };
+        }
+
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(model);
+
+        parameters.TryGetValue("regionId", out var regionIdResult);
+        if (regionIdResult != null)
+        {
+            regionIdResult![0].Should().BeEquivalentTo(model.RegionId[0].ToString());
+        }
+        else
+        {
+            regionIdResult.Should().BeNull();
+        }
+    }
+
+    [TestCase(null)]
+    [TestCase(3)]
+    public void Builder_ConstructParameters_ToPage(int? page)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = Guid.NewGuid(),
+            Page = page
+        };
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(model);
+
+        parameters.TryGetValue("page", out var pageResult);
+        if (pageResult != null)
+        {
+            pageResult![0].Should().Be(page?.ToString());
+        }
+        else
+        {
+            pageResult.Should().BeNull();
+        }
+    }
+
+    [TestCase(null)]
+    [TestCase(6)]
+    public void Builder_ConstructParameters_ToPageSize(int? pageSize)
+    {
+        var model = new GetCalendarEventsRequestModel
+        {
+            RequestedByMemberId = Guid.NewGuid(),
+            PageSize = pageSize
+        };
+        var parameters = QueryStringParameterBuilder.BuildQueryStringParameters(model);
+
+        parameters.TryGetValue("pageSize", out var pageResult);
+        if (pageResult != null)
+        {
+            pageResult![0].Should().Be(pageSize?.ToString());
+        }
+        else
+        {
+            pageResult.Should().BeNull();
+        }
+    }
+}
