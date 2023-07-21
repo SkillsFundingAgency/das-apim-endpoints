d4ac4c907d0378bb06a185dadc96f39101be2f59
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-07-21 15:43:12 +0100

    CSP-882 Removed application project

--- src/EmployerAan/SFA.DAS.EmployerAan.Application/InnerApi/Attendances/GetAttendancesResponse.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace SFA.DAS.EmployerAan.Application.InnerApi.Attendances;
-public record GetAttendancesResponse(List<AttendanceModel> Attendances);
-
-public record AttendanceModel(Guid CalendarEventId, string EventFormat, DateTime EventStartDate, string EventTitle);
