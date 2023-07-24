1ca2cac2a67f50483d2e3fb2a7c6076de414bb1d
Deven Shah <devenshah1974@gmail.com>
2023-07-24 12:14:10 +0100

    CSP-717 Added endpoint to query accounts (#1088)
    * CSP-717 Added endpoint to query accounts
    
    * CSP-717 Added coverage

--- src/EmployerAan/SFA.DAS.EmployerAan.Api/AppStart/ServiceCollectionExtensions.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan.Api/AppStart/ServiceCollectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using MediatR;
+using Microsoft.Extensions.Options;
 using RestEase.HttpClientFactory;
 using SFA.DAS.Api.Common.AppStart;
 using SFA.DAS.Api.Common.Configuration;
@@ -9,17 +10,38 @@ using SFA.DAS.EmployerAan.Application.Employer.Queries.GetEmployerMember;
 using SFA.DAS.EmployerAan.Configuration;
 using SFA.DAS.EmployerAan.Infrastructure;
 using SFA.DAS.SharedOuterApi.AppStart;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Services;
 
 namespace SFA.DAS.EmployerAan.Api.AppStart;
 
 [ExcludeFromCodeCoverage]
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions();
+        services.Configure<AzureActiveDirectoryConfiguration>(configuration.GetSection("AzureAd"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<AzureActiveDirectoryConfiguration>>()!.Value);
+        services.Configure<AccountsConfiguration>(configuration.GetSection("AccountsInnerApi"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<AccountsConfiguration>>()!.Value);
+        services.Configure<EmployerProfilesApiConfiguration>(configuration.GetSection(nameof(EmployerProfilesApiConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<EmployerProfilesApiConfiguration>>()!.Value);
+        return services;
+    }
+
     public static IServiceCollection AddServiceRegistration(this IServiceCollection services, IConfigurationRoot configuration)
     {
         services.AddHttpClient();
         services.AddMediatR(typeof(GetEmployerMemberQuery).Assembly);
         services.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
+        services.AddTransient(typeof(IInternalApiClient<>), typeof(InternalApiClient<>));
+        services.AddTransient<IAccountsApiClient<AccountsConfiguration>, AccountsApiClient>();
+        services.AddTransient<IEmployerProfilesApiClient<EmployerProfilesApiConfiguration>, EmployerProfilesApiClient>();
+
+        services.AddTransient<IEmployerAccountsService, EmployerAccountsService>();
 
         AddAanHubApiClient(services, configuration);
         return services;
