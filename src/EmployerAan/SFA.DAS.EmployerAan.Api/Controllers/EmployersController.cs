882705ec977ba10f38ecde8c5680314c094a33da
Yuvraj Patil <123636581+YuvrajPatil-DFE@users.noreply.github.com>
2023-07-26 15:29:45 +0100

    CSP-648 Employer AAN Outer API Create Employer Endpoint (#1103)
    * CSP-648 Implemented Post CreateEmployer
    
    * CSP-648 Added coverage for CreateEmployer
    
    * CSP-648 Resolved PR comments
    
    * CSP-648 Resolved PR comments, updated few nullable fields
    
    * CSP-648 Added missing fields AccountId,UserRef
    
    * CSP-648 Removed EmployeeId field

--- src/EmployerAan/SFA.DAS.EmployerAan.Api/Controllers/EmployersController.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan.Api/Controllers/EmployersController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerAan.Application.Employer.Commands.CreateEmployerMember;
 using SFA.DAS.EmployerAan.Application.Employer.Queries.GetEmployerMember;
 
 namespace SFA.DAS.EmployerAan.Api.Controllers;
@@ -22,5 +23,13 @@ public class EmployersController : ControllerBase
     {
         var employer = await _mediator.Send(new GetEmployerMemberQuery(userRef), cancellationToken);
         return employer is null ? NotFound() : Ok(employer);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(CreateEmployerMemberCommandResult), StatusCodes.Status200OK)]
+    public async Task<IActionResult> CreateEmployerMember([FromBody] CreateEmployerMemberCommand command, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(command, cancellationToken);
+        return Ok(result);
     }
 }
