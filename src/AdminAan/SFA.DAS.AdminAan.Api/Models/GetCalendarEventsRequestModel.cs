2ba95f7e9ef3846e12ab0dfc82a640cda182da7e
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2023-08-09 17:04:49 +0100

    CSP-919 GetCalendarEvents endpoint added (#1112)
    * CSP-919 GetCalendarEvents endpoint added
    
    * CSP-919 coverage added
    
    * CSP-919 minor fixes based on TR comments
    
    * CSP-919 added EventFormat to the results

---
+++ src/AdminAan/SFA.DAS.AdminAan.Api/Models/GetCalendarEventsRequestModel.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AdminAan.Application.CalendarEvents.Queries.GetCalendarEvents;
+using SFA.DAS.AdminAan.Infrastructure.Configuration;
+
+namespace SFA.DAS.AdminAan.Api.Models;
+
+public class GetCalendarEventsRequestModel
+{
+    [FromHeader(Name = Constants.ApiHeaders.RequestedByMemberIdHeader)]
+    public Guid RequestedByMemberId { get; set; }
+
+
+    [FromQuery]
+    public bool? IsActive { get; set; }
+
+
+    [FromQuery]
+    public DateTime? FromDate { get; set; }
+
+    [FromQuery]
+    public DateTime? ToDate { get; set; }
+
+    [FromQuery]
+    public List<int> CalendarId { get; set; } = new List<int>();
+
+    [FromQuery]
+    public List<int> RegionId { get; set; } = new List<int>();
+
+    [FromQuery]
+    public int? Page { get; set; }
+
+    [FromQuery]
+    public int? PageSize { get; set; }
+
+    public static implicit operator GetCalendarEventsQuery(GetCalendarEventsRequestModel requestModel) => new()
+    {
+        RequestedByMemberId = requestModel.RequestedByMemberId,
+        IsActive = requestModel.IsActive,
+        FromDate = requestModel.FromDate?.ToString("yyyy-MM-dd"),
+        ToDate = requestModel.ToDate?.ToString("yyyy-MM-dd"),
+        RegionIds = requestModel.RegionId,
+        CalendarIds = requestModel.CalendarId,
+        Page = requestModel.Page,
+        PageSize = requestModel.PageSize
+    };
+}
