4d11c7293eee6e10ccbebdf0e47e6aee2ad95131
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-08-10 14:49:34 +0100

    CSP-882 Get Attendance api implemented (#1110)
    * CSP-882 Get Attendance api implemented
    
    * CSP-882_ Fixed Review Comments
    
    * CSP-882 Removed application project
    
    * CSP-882 Fixed minor code formatting
    
    ---------
    
    Co-authored-by: Deven Shah <devenshah1974@gmail.com>

--- src/EmployerAan/SFA.DAS.EmployerAan/Infrastructure/IAanHubRestApiClient.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan/Infrastructure/IAanHubRestApiClient.cs
@@ -3,7 +3,7 @@ using SFA.DAS.EmployerAan.Application.Employer.Commands.CreateEmployerMember;
 using SFA.DAS.EmployerAan.Application.Employer.Queries.GetEmployerMember;
 using SFA.DAS.EmployerAan.Application.Profiles.Queries.GetProfilesByUserType;
 using SFA.DAS.EmployerAan.Application.Regions.Queries.GetRegions;
-
+using SFA.DAS.EmployerAan.InnerApi.Attendances;
 namespace SFA.DAS.EmployerAan.Infrastructure;
 
 public interface IAanHubRestApiClient
@@ -19,7 +19,14 @@ public interface IAanHubRestApiClient
     [Get("/employers/{userRef}")]
     [AllowAnyStatusCode]
     Task<Response<GetEmployerMemberQueryResult?>> GetEmployer([Path] Guid userRef, CancellationToken cancellationToken);
+    
     [Post("/employers")]
     Task<CreateEmployerMemberCommandResult> PostEmployerMember([Body] CreateEmployerMemberCommand command, CancellationToken cancellationToken);
-    
+
+    [Get("attendances")]
+    Task<GetAttendancesResponse> GetAttendances(
+        [Header(Constants.ApiHeaders.RequestedByMemberIdHeader)] Guid requestedByMemberId,
+        string FromDate,
+        string ToDate,
+        CancellationToken cancellationToken);
 }
