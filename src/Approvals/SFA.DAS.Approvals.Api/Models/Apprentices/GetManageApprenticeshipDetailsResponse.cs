b9b626738c33b296011e67254440fa7ec91435e6
Paul Graham <paul.graham@coprime.co.uk>
2023-08-15 14:17:16 +0100

    tidy up

--- src/Approvals/SFA.DAS.Approvals.Api/Models/Apprentices/GetManageApprenticeshipDetailsResponse.cs
+++ src/Approvals/SFA.DAS.Approvals.Api/Models/Apprentices/GetManageApprenticeshipDetailsResponse.cs
@@ -113,27 +113,16 @@ namespace SFA.DAS.Approvals.Api.Models.Apprentices
         public class ChangeOfPartyRequest
         {
             public long Id { get; set; }
-
             public ChangeOfPartyRequestType ChangeOfPartyType { get; set; }
-
             public short OriginatingParty { get; set; }
-
             public byte Status { get; set; }
-
             public string EmployerName { get; set; }
-
             public DateTime? StartDate { get; set; }
-
             public DateTime? EndDate { get; set; }
-
             public int? Price { get; set; }
-
             public long? CohortId { get; set; }
-
             public short? WithParty { get; set; }
-
             public long? NewApprenticeshipId { get; set; }
-
             public long? ProviderId { get; set; }
         }
 
