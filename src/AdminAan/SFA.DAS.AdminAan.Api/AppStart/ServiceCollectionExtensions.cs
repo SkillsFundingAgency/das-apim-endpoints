3ce8d579c2c70082cf66a04987f3a9300891ebdf
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2023-08-16 14:26:13 +0100

    CSP-960 get addresses from locationsApi added, with coverage (#1117)
    * CSP-960 get addresses from locationsApi added, with coverage
    
    * CSP-960 added coverage for AddressItem mapping and added timeout on regex match

--- src/AdminAan/SFA.DAS.AdminAan.Api/AppStart/ServiceCollectionExtensions.cs
+++ src/AdminAan/SFA.DAS.AdminAan.Api/AppStart/ServiceCollectionExtensions.cs
@@ -1,5 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using MediatR;
+﻿using MediatR;
+using Microsoft.Extensions.Options;
 using RestEase.HttpClientFactory;
 using SFA.DAS.AdminAan.Api.Configuration;
 using SFA.DAS.AdminAan.Application.Regions.Queries.GetRegions;
@@ -9,6 +9,11 @@ using SFA.DAS.Api.Common.Configuration;
 using SFA.DAS.Api.Common.Infrastructure;
 using SFA.DAS.Api.Common.Interfaces;
 using SFA.DAS.SharedOuterApi.AppStart;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Services;
+using System.Diagnostics.CodeAnalysis;
 
 namespace SFA.DAS.AdminAan.Api.AppStart;
 
@@ -34,10 +39,17 @@ public static class ServiceCollectionExtensions
 
     public static IServiceCollection AddServiceRegistration(this IServiceCollection services, IConfigurationRoot configuration)
     {
+        AddConfigurationOptions(services, configuration);
         services.AddHttpClient();
-        services.AddMediatR(typeof(GetRegionsQuery).Assembly);
+
         services.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
+        services.AddTransient(typeof(IInternalApiClient<>), typeof(InternalApiClient<>));
+        services.AddTransient<ILocationApiClient<LocationApiConfiguration>, LocationApiClient>();
+
+        services.AddMediatR(typeof(GetRegionsQuery).Assembly);
+
         AddAanHubApiClient(services, configuration);
+
         return services;
     }
 
@@ -53,5 +65,12 @@ public static class ServiceCollectionExtensions
 
         services.AddRestEaseClient<IAanHubRestApiClient>(apiConfig.Url)
             .AddHttpMessageHandler<InnerApiAuthenticationHeaderHandler>();
+    }
+
+    private static void AddConfigurationOptions(IServiceCollection services, IConfigurationRoot configuration)
+    {
+        services.AddOptions();
+        services.Configure<LocationApiConfiguration>(configuration.GetSection(nameof(LocationApiConfiguration)));
+        services.AddSingleton(c => c.GetService<IOptions<LocationApiConfiguration>>()!.Value);
     }
 }
---
+++ src/AdminAan/SFA.DAS.AdminAan/Application/Locations/Queries/GetAddresses/AddressItem.cs
@@ -0,0 +1,30 @@
+﻿using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+
+namespace SFA.DAS.AdminAan.Application.Locations.Queries.GetAddresses;
+
+public class AddressItem
+{
+    public string Uprn { get; set; } = null!;
+    public string? OrganisationName { get; set; }
+    public string? Town { get; set; }
+    public string? County { get; set; }
+    public string? Postcode { get; set; }
+    public string? AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public double? Longitude { get; set; }
+    public double? Latitude { get; set; }
+
+    public static implicit operator AddressItem(GetAddressesListItem source) =>
+        new()
+        {
+            Uprn = source.Uprn,
+            OrganisationName = source.Organisation,
+            Town = source.PostTown,
+            County = source.County,
+            Postcode = source.Postcode,
+            AddressLine1 = source.AddressLine1,
+            AddressLine2 = source.AddressLine2,
+            Longitude = source.Longitude,
+            Latitude = source.Latitude
+        };
+}
