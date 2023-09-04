a76dff9c7f484f5c4b5b7dde555e03b7d7769356
MarkFCain <mark.cain@digital.education.gov.uk>
2023-09-04 15:35:33 +0100

    CSP-961 added some logging for when call to referenceData fails to return an OK

--- src/AdminAan/SFA.DAS.AdminAan/Application/Schools/Queries/GetSchoolsQueryHandler.cs
+++ src/AdminAan/SFA.DAS.AdminAan/Application/Schools/Queries/GetSchoolsQueryHandler.cs
@@ -1,24 +1,29 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.AdminAan.Infrastructure;
 
 namespace SFA.DAS.AdminAan.Application.Schools.Queries;
 public class GetSchoolsQueryHandler : IRequestHandler<GetSchoolsQuery, GetSchoolsQueryApiResult>
 {
     private readonly IReferenceDataApiClient _apiClient;
+    private readonly ILogger<GetSchoolsQueryHandler> _logger;
 
     public const int PageSize = 100;
     public const int PageNumber = 1;
-    public GetSchoolsQueryHandler(IReferenceDataApiClient apiClient)
+    public GetSchoolsQueryHandler(IReferenceDataApiClient apiClient, ILogger<GetSchoolsQueryHandler> logger)
     {
         _apiClient = apiClient;
+        _logger = logger;
     }
 
     public async Task<GetSchoolsQueryApiResult> Handle(GetSchoolsQuery request, CancellationToken cancellationToken)
     {
         var result = await _apiClient.GetSchools(request.SearchTerm, PageSize, PageNumber, cancellationToken);
 
-        return result.ResponseMessage.StatusCode == System.Net.HttpStatusCode.OK
-            ? result.GetContent()
-            : new GetSchoolsQueryApiResult(new List<School>());
+        if (result.ResponseMessage.StatusCode == System.Net.HttpStatusCode.OK) return result.GetContent();
+
+        _logger.LogInformation("Call to ReferenceData Api was no successful. There has been status returned of StatusCode {StatusCode}", result.ResponseMessage.StatusCode);
+
+        return new GetSchoolsQueryApiResult(new List<School>());
     }
 }
