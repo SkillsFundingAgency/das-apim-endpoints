b82e122b7d8d1c1e6fb9df88bea333599336ea6a
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-09-04 12:41:30 +0100

    CSP-996 Implemented controller, Model, TC

---
+++ src/EmployerAan/SFA.DAS.EmployerAan.Api.UnitTests/Controllers/MemberProfilesControllerTests/MemberProfilesControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.EmployerAan.Api.Controllers;
+using SFA.DAS.EmployerAan.Application.MemberProfiles.Commands.PutMemberProfile;
+using SFA.DAS.EmployerAan.InnerApi.MemberProfiles;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerAan.Api.UnitTests.Controllers.MemberProfilesControllerTests;
+public class MemberProfilesControllerTests
+{
+
+    [Test, MoqAutoData]
+    public async Task PutMemberProfile_InvokesSendWithCommandProperties(
+        [Frozen] Mock<IMediator> mediatorMock,
+        Guid memberId,
+        Guid requestedByMemberId,
+        UpdateMemberProfileModel request,
+        CancellationToken cancellationToken)
+    {
+        var sut = new MemberProfilesController(mediatorMock.Object);
+
+        await sut.PutMemberProfile(memberId, requestedByMemberId, request, cancellationToken);
+
+        mediatorMock.Verify(m => m.Send(
+            It.Is<UpdateMemberProfilesCommand>(
+                c => c.MemberId == memberId
+                && c.RequestedByMemberId == requestedByMemberId
+                && c.MemberProfile == request), cancellationToken),
+                     Times.Once());
+    }
+
+    [Test, MoqAutoData]
+    public async Task PutMemberProfile_ReturnsNoContent(
+    [Frozen] Mock<IMediator> mediatorMock,
+    Guid memberId,
+    Guid requestedByMemberId,
+    UpdateMemberProfileModel request,
+    CancellationToken cancellationToken)
+    {
+        var sut = new MemberProfilesController(mediatorMock.Object);
+
+        var result = await sut.PutMemberProfile(memberId, requestedByMemberId, request, cancellationToken);
+
+        result.Should().BeOfType<NoContentResult>();
+    }
+}
---
+++ src/EmployerAan/SFA.DAS.EmployerAan.UnitTests/Application/MemberProfiles/Commands/UpdateMemberProfilesCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using SFA.DAS.EmployerAan.Application.MemberProfiles.Commands.PutMemberProfile;
+using SFA.DAS.EmployerAan.InnerApi.MemberProfiles;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerAan.UnitTests.Application.MemberProfiles.Commands;
+public class UpdateMemberProfilesCommandTests
+{
+    [Test, MoqAutoData]
+    public void Constructor_InitialisesAllProperties(Guid memberId, Guid requestedByMemberId, UpdateMemberProfileModel memberProfile)
+    {
+        var sut = new UpdateMemberProfilesCommand(memberId, requestedByMemberId, memberProfile);
+
+        using (new AssertionScope())
+        {
+            sut.MemberId.Should().Be(memberId);
+            sut.RequestedByMemberId.Should().Be(requestedByMemberId);
+            sut.MemberProfile.Should().Be(memberProfile);
+        }
+    }
+}
