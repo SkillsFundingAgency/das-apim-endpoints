2123d14dc57dad6762d37a52d7b82706d830f846
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-09-14 17:37:03 +0100

    CSP-897 Set PreferenceId null as per inner API output

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Entities/MemberProfile.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Entities/MemberProfile.cs
@@ -4,5 +4,5 @@ public class MemberProfile
 {
     public int ProfileId { get; set; }
     public string Value { get; set; } = string.Empty;
-    public int PreferenceId { get; set; }
+    public int? PreferenceId { get; set; }
 }
