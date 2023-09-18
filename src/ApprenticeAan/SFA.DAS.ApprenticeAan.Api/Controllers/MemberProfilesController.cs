b84017649c6d319a79e01958022e9bd289d32be6
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-09-18 16:57:56 +0100

    CSP-897 Added RequestedByMemberId and updated MemberPreferenceModel

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/MemberProfilesController.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/MemberProfilesController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Application.Infrastructure.Configuration;
 using SFA.DAS.ApprenticeAan.Application.MemberProfiles.Queries.GetMemberProfileWithPreferences;
 
 namespace SFA.DAS.ApprenticeAan.Api.Controllers;
@@ -16,10 +17,12 @@ public class MemberProfilesController : ControllerBase
 
     [HttpGet("{memberId}/profile")]
     [ProducesResponseType(typeof(GetMemberProfileWithPreferencesQueryResult), StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetMemberProfileWithPreferences([FromRoute] Guid memberId,
-            CancellationToken cancellationToken,
-            bool @public = true)
+    public async Task<IActionResult> GetMemberProfileWithPreferences(
+        [FromRoute] Guid memberId,
+        [FromHeader(Name = Constants.ApiHeaders.RequestedByMemberIdHeader)] Guid requestedByMemberId,
+        CancellationToken cancellationToken,
+        bool @public = true)
     {
-        return Ok(await _mediator.Send(new GetMemberProfileWithPreferencesQuery(memberId, @public), cancellationToken));
+        return Ok(await _mediator.Send(new GetMemberProfileWithPreferencesQuery(memberId, requestedByMemberId, @public), cancellationToken));
     }
 }
