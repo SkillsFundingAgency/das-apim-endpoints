23c0f6bf6d654fc0564c4c0b29b8ee6f55d006ba
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-09-26 09:27:38 +0100

    CSP-897 Implemented handler

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Members/Queries/GetMember/GetMemberQueryResult.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Members/Queries/GetMember/GetMemberQueryResult.cs
@@ -3,6 +3,7 @@
 public class GetMemberQueryResult
 {
     public Guid MemberId { get; set; }
+    public Guid? ApprenticeId { get; set; }
     public string Email { get; set; } = null!;
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
