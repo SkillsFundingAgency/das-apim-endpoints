cb58ab91f099472ab3772fca326cf28b528c7469
Yuvraj Patil <yuvraj.patil@digital.education.gov.uk>
2023-09-26 09:27:38 +0100

    CSP-897 Implemented handler

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application.UnitTests/MemberProfiles/Queries/GetMemberProfileWithPreferences/GetMemberProfileWithPreferencesQueryHandlerTests.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application.UnitTests/MemberProfiles/Queries/GetMemberProfileWithPreferences/GetMemberProfileWithPreferencesQueryHandlerTests.cs
@@ -1,8 +1,17 @@
-﻿using AutoFixture.NUnit3;
+﻿using System.Net;
+using AutoFixture.NUnit3;
 using FluentAssertions;
 using Moq;
 using SFA.DAS.ApprenticeAan.Application.Infrastructure;
+using SFA.DAS.ApprenticeAan.Application.InnerApi.MyApprenticeships;
+using SFA.DAS.ApprenticeAan.Application.InnerApi.Standards.Requests;
 using SFA.DAS.ApprenticeAan.Application.MemberProfiles.Queries.GetMemberProfileWithPreferences;
+using SFA.DAS.ApprenticeAan.Application.Members.Queries.GetMember;
+using SFA.DAS.ApprenticeAan.Application.MyApprenticeships.Queries.GetMyApprenticeship;
+using SFA.DAS.ApprenticeAan.Application.Regions.Queries.GetRegions;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.ApprenticeAan.Application.UnitTests.MemberProfiles.Queries.GetMemberProfileWithPreferences;
@@ -12,12 +21,51 @@ public class GetMemberProfileWithPreferencesQueryHandlerTests
     [Test, MoqAutoData]
     public async Task Handle_ReturnAllProfiles(
         [Frozen] Mock<IAanHubRestApiClient> apiClient,
+        [Frozen] Mock<IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration>> apprenticeAccountsApiClientMock,
+        [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> coursesApiClientMock,
         GetMemberProfileWithPreferencesQueryHandler handler,
         GetMemberProfileWithPreferencesQuery query,
         GetMemberProfileWithPreferencesQueryResult expected,
+        GetMemberQueryResult memberResult,
+        GetRegionsQueryResult regionsResult,
+        MyApprenticeshipResponse response,
+        GetMyApprenticeshipQuery request,
         CancellationToken cancellationToken)
     {
-        apiClient.Setup(x => x.GetMemberProfileWithPreferences(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<bool>(), cancellationToken)).ReturnsAsync(expected);
+
+        memberResult.UserType = Common.MemberUserType.Apprentice.ToString();
+        memberResult.ApprenticeId = request.ApprenticeId;
+        regionsResult.Regions.Add(new Entities.Region { Id = (int)memberResult.RegionId!, Area = Guid.NewGuid().ToString(), Ordering = int.MinValue });
+
+        apiClient.Setup(x => x.GetMember(query.MemberId, cancellationToken)).ReturnsAsync(memberResult);
+        apiClient.Setup(x => x.GetRegions(cancellationToken)).ReturnsAsync(regionsResult);
+
+        apiClient.Setup(x => x.GetMemberProfileWithPreferences(query.MemberId, query.MemberId, It.IsAny<bool>(), cancellationToken)).ReturnsAsync(expected);
+
+        apprenticeAccountsApiClientMock.Setup(c => c.GetWithResponseCode<MyApprenticeshipResponse>(It.Is<GetMyApprenticeshipRequest>(r => r.Id == request.ApprenticeId)))!.ReturnsAsync(new ApiResponse<MyApprenticeshipResponse>(response, HttpStatusCode.OK, null));
+
+        const string StandardUid = "ST0418_1.0";
+        const string TrainingCode = null!;
+        const string StandardRoute = "route of standard";
+        const int Duration = 36;
+        const string Title = "Title";
+        const int Level = 3;
+        response.StandardUId = StandardUid;
+        response.TrainingCode = TrainingCode;
+
+        var standardResponse = new GetStandardResponse
+        {
+            Title = Title,
+            Level = Level,
+            Route = StandardRoute,
+            VersionDetail = new StandardVersionDetail { ProposedTypicalDuration = Duration }
+        };
+
+        coursesApiClientMock.Setup(c => c.Get<GetStandardResponse>(It.IsAny<GetStandardQueryRequest>()))
+            .ReturnsAsync(standardResponse);
+
+        coursesApiClientMock.Setup(c => c.Get<GetFrameworkResponse>(It.IsAny<GetFrameworkQueryRequest>()))
+            .ReturnsAsync(new GetFrameworkResponse());
 
         var actual = await handler.Handle(query, cancellationToken);
 
--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/MemberProfiles/Queries/GetMemberProfileWithPreferences/GetMemberProfileWithPreferencesQueryHandler.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/MemberProfiles/Queries/GetMemberProfileWithPreferences/GetMemberProfileWithPreferencesQueryHandler.cs
@@ -1,23 +1,35 @@
 ﻿using MediatR;
 using SFA.DAS.ApprenticeAan.Application.Common;
 using SFA.DAS.ApprenticeAan.Application.Infrastructure;
+using SFA.DAS.ApprenticeAan.Application.MyApprenticeships.Queries.GetMyApprenticeship;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.ApprenticeAan.Application.MemberProfiles.Queries.GetMemberProfileWithPreferences;
 public class GetMemberProfileWithPreferencesQueryHandler : IRequestHandler<GetMemberProfileWithPreferencesQuery, GetMemberProfileWithPreferencesQueryResult?>
 {
     private readonly IAanHubRestApiClient _apiClient;
+    private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
+    private readonly IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration> _apprenticeAccountsApiClient;
 
-    public GetMemberProfileWithPreferencesQueryHandler(IAanHubRestApiClient apiClient)
+    public GetMemberProfileWithPreferencesQueryHandler(
+        IAanHubRestApiClient apiClient,
+        ICoursesApiClient<CoursesApiConfiguration> coursesApiClient,
+        IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration> apprenticeAccountsApiClient
+        )
     {
         _apiClient = apiClient;
+        _coursesApiClient = coursesApiClient;
+        _apprenticeAccountsApiClient = apprenticeAccountsApiClient;
     }
 
     public async Task<GetMemberProfileWithPreferencesQueryResult?> Handle(GetMemberProfileWithPreferencesQuery request, CancellationToken cancellationToken)
     {
         var responseMemberProfileWithPreferenceTask = _apiClient.GetMemberProfileWithPreferences(request.MemberId, request.RequestedByMemberId, request.IsPublicView, cancellationToken);
-        var responseMember = _apiClient.GetMember(request.MemberId, cancellationToken);
+        var responseMemberTask = _apiClient.GetMember(request.MemberId, cancellationToken);
+        var responseRegionsTask = _apiClient.GetRegions(cancellationToken);
 
-        await Task.WhenAll(responseMemberProfileWithPreferenceTask, responseMember);
+        await Task.WhenAll(responseMemberProfileWithPreferenceTask, responseMemberTask, responseRegionsTask);
 
         GetMemberProfileWithPreferencesQueryResult result = new();
 
@@ -29,20 +41,33 @@ public class GetMemberProfileWithPreferencesQueryHandler : IRequestHandler<GetMe
         if (outputMemberProfileWithPreferenceTask.Preferences.Any())
             result.Preferences = outputMemberProfileWithPreferenceTask.Preferences;
 
-        var outputMember = responseMember.Result;
+        var outputMember = responseMemberTask.Result;
 
         result.FullName = outputMember.FullName;
         result.Email = outputMember.Email;
         result.RegionId = outputMember.RegionId;
         result.UserType = Enum.Parse<MemberUserType>(outputMember.UserType);
 
-        ///if (outputMember.IsRegionalChair.HasValue)
         result.IsRegionalChair = outputMember.IsRegionalChair;
 
-        ///need to retrieve from respective API
-        result.RegionName = null!;
-        result.Apprenticeship = null!;
 
+        var outputRegions = responseRegionsTask.Result;
+        if (outputRegions.Regions.Any()) result.RegionName = outputRegions.Regions.Find(x => x.Id == result.RegionId)!.Area!;
+
+        result.Apprenticeship = new();
+
+        if (outputMember.ApprenticeId.HasValue)
+        {
+            GetMyApprenticeshipQueryHandler handler = new(_apprenticeAccountsApiClient, _coursesApiClient);
+            var responseMyApprenticeship = await handler.Handle(new GetMyApprenticeshipQuery { ApprenticeId = (Guid)outputMember.ApprenticeId }, cancellationToken);
+
+            if (responseMyApprenticeship != null)
+            {
+                result.Apprenticeship.Level = responseMyApprenticeship!.TrainingCourse!.Level.ToString();
+                result.Apprenticeship.Sector = responseMyApprenticeship.TrainingCourse!.Sector!;
+                result.Apprenticeship.Programmes = responseMyApprenticeship.TrainingCourse!.Name!;
+            }
+        }
         return result;
     }
 }
