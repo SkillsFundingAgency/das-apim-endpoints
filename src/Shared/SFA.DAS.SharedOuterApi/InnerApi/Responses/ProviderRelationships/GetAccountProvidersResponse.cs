422e80718d4f5eb84559706de1685aaf67f951b9
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2023-10-03 20:42:57 +0100

    new request and response and folder for ProviderRelationships logical grouping

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/ProviderRelationships/GetAccountProvidersResponse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Responses
+{
+    public abstract class GetAccountProvidersResponse
+    {
+        [JsonPropertyName("AccountId")]
+        public long AccountId { get; set; }
+        
+        [JsonPropertyName("AccountProviders")]
+        public List<AccountProviderResponse> AccountProviders { get; set; }
+    }
+
+    public class AccountProviderResponse
+    {
+        public long Id { get; set; }
+        public long ProviderUkprn { get; set; }
+        public string ProviderName { get; set; }
+    }
+}
