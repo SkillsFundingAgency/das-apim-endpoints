b8c5839aefe29fe14c86a725558afa884ad9e179
chrisfoster76 <chrisfoster186@googlemail.com>
2023-10-18 13:08:57 +0100

    Asserts success response code

--- src/Approvals/SFA.DAS.Approvals.UnitTests/Application/CommitmentPayment/Queries/WhenGettingDataLockEvents.cs
+++ src/Approvals/SFA.DAS.Approvals.UnitTests/Application/CommitmentPayment/Queries/WhenGettingDataLockEvents.cs
@@ -1,16 +1,16 @@
-﻿using System.Threading;
+﻿using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Approvals.Application.CommitmentPayment.Queries.GetDataLockEvents;
-using SFA.DAS.Approvals.InnerApi.Requests;
-using SFA.DAS.Approvals.InnerApi.Responses;
 using SFA.DAS.Approvals.InnerApi.Responses.ProviderEvent;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.Approvals.UnitTests.Application.CommitmentPayment.Queries
@@ -21,13 +21,16 @@ namespace SFA.DAS.Approvals.UnitTests.Application.CommitmentPayment.Queries
         public async Task Then_The_Api_Is_Called_With_The_Request_And_DataLockEvents_Returned(
             GetDataLockStatuesQuery query,
             GetDataLockStatusesQueryResult result,
-            PageOfResults<DataLockStatusEvent> apiResponse,
+            PageOfResults<DataLockStatusEvent> apiPagedResponse,
             [Frozen] Mock<IProviderPaymentEventsApiClient<ProviderEventsConfiguration>> apiClient,
             GetDataLockStatusesQueryHandler handler
         )
         {
+            var apiResponse =
+                new ApiResponse<PageOfResults<DataLockStatusEvent>>(apiPagedResponse, HttpStatusCode.OK, string.Empty);
+
             apiClient
-                .Setup(x => x.Get<PageOfResults<DataLockStatusEvent>>(It.IsAny<GetDataLockEventsRequest>()))
+                .Setup(x => x.GetWithResponseCode<PageOfResults<DataLockStatusEvent>>(It.IsAny<GetDataLockEventsRequest>()))
                 .ReturnsAsync(apiResponse);
 
             var actual = await handler.Handle(query, CancellationToken.None);
