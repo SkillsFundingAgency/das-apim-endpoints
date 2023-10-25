da465f1d42fe7ada3f83380cd8ae0e62b73bb861
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-10-25 13:38:28 +0100

    CSP-860 Added Employer call to Apprentice

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/MemberProfiles/GetMemberProfileWithPreferencesTests.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/MemberProfiles/GetMemberProfileWithPreferencesTests.cs
@@ -5,6 +5,8 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SFA.DAS.ApprenticeAan.Api.Controllers;
+using SFA.DAS.ApprenticeAan.Application.Common;
+using SFA.DAS.ApprenticeAan.Application.Employer.Queries.GetEmployerMemberSummary;
 using SFA.DAS.ApprenticeAan.Application.InnerApi.MyApprenticeships;
 using SFA.DAS.ApprenticeAan.Application.InnerApi.Standards.Requests;
 using SFA.DAS.ApprenticeAan.Application.MemberProfiles.Queries.GetMemberProfileWithPreferences;
@@ -18,24 +20,26 @@ using SFA.DAS.Testing.AutoFixture;
 namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers.MemberProfiles;
 public class GetMemberProfileWithPreferencesTests
 {
-    static readonly List<MemberPreference> memberPreferences = new()
-        {
-            new MemberPreference{ PreferenceId = 1, Value =  true },
-            new MemberPreference{ PreferenceId = 2, Value =  true },
-            new MemberPreference{ PreferenceId = 4, Value =  false },
-        };
+
 
     [Test]
-    [MoqInlineAutoData(true, true)]
-    [MoqInlineAutoData(true, false)]
-    [MoqInlineAutoData(false, true)]
-    [MoqInlineAutoData(false, false)]
+    [MoqInlineAutoData(true, true, MemberUserType.Apprentice)]
+    [MoqInlineAutoData(true, false, MemberUserType.Apprentice)]
+    [MoqInlineAutoData(false, true, MemberUserType.Apprentice)]
+    [MoqInlineAutoData(false, false, MemberUserType.Apprentice)]
+    [MoqInlineAutoData(true, true, MemberUserType.Employer)]
+    [MoqInlineAutoData(true, false, MemberUserType.Employer)]
+    [MoqInlineAutoData(false, true, MemberUserType.Employer)]
+    [MoqInlineAutoData(false, false, MemberUserType.Employer)]
     public async Task When_MediatorCommandSuccessful_Then_ReturnOk(
         bool isPublicView,
         bool isApprenticeshipSectionShow,
+        MemberUserType userType,
         MyApprenticeship? myApprenticeship,
+        Apprenticeship? apprenticeship,
         [Frozen] Mock<IMediator> mediatorMock,
         Guid appreticeId,
+        long accountId,
         Guid memberId,
         Guid requestedByMemberId,
         GetStandardResponse standardResponse,
@@ -44,24 +48,50 @@ public class GetMemberProfileWithPreferencesTests
     {
         GetMemberProfileWithPreferencesQueryResult memberProfileWithPreferencesQueryResult = new GetMemberProfileWithPreferencesQueryResult();
         memberProfileWithPreferencesQueryResult.ApprenticeId = appreticeId;
+        memberProfileWithPreferencesQueryResult.AccountId = accountId;
+        memberProfileWithPreferencesQueryResult.UserType = userType;
         GetMyApprenticeshipQuery myApprenticeshipQuery = new() { ApprenticeId = appreticeId };
-        List<MemberPreference> memberPreference = new List<MemberPreference>();
-        memberPreference.AddRange(memberPreferences);
-        if (isApprenticeshipSectionShow)
+        GetEmployerMemberSummaryQuery employerMemberSummaryQuery = new(accountId);
+        List<MemberPreference> memberPreferences = new()
         {
-            memberPreference.Add(new MemberPreference() { PreferenceId = 3, Value = true });
-        }
-        memberProfileWithPreferencesQueryResult.Preferences = memberPreference;
+            new MemberPreference{ PreferenceId = 1, Value =  true },
+            new MemberPreference{ PreferenceId = 2, Value =  true },
+            new MemberPreference{ PreferenceId = 3, Value =  isApprenticeshipSectionShow },
+            new MemberPreference{ PreferenceId = 4, Value =  false },
+        };
+        memberProfileWithPreferencesQueryResult.Preferences = memberPreferences;
         int apprenticeshipPreferenceId = 3;
         var isApprenticeSectionShareAllowed = (memberProfileWithPreferencesQueryResult.Preferences.Any(x => x.PreferenceId == apprenticeshipPreferenceId)) ? memberProfileWithPreferencesQueryResult.Preferences.FirstOrDefault(x => x.PreferenceId == apprenticeshipPreferenceId)!.Value : false;
+        GetEmployerMemberSummaryQueryResult? getEmployerMemberSummaryQueryResult = new GetEmployerMemberSummaryQueryResult();
         if (isPublicView && !isApprenticeSectionShareAllowed)
         {
             myApprenticeship = null;
+            apprenticeship = null;
+            getEmployerMemberSummaryQueryResult = null;
+        }
+        GetMemberProfileWithPreferencesModel response = null!;
+
+        if (isPublicView && !isApprenticeSectionShareAllowed)
+        {
+            response = new(memberProfileWithPreferencesQueryResult, null, null, isPublicView);
+        }
+        else if (memberProfileWithPreferencesQueryResult.UserType == MemberUserType.Apprentice && myApprenticeship != null)
+        {
+            response = new(memberProfileWithPreferencesQueryResult, myApprenticeship, null, isPublicView);
+        }
+        else
+        {
+            if (apprenticeship != null)
+            {
+                getEmployerMemberSummaryQueryResult!.ActiveCount = apprenticeship.ActiveApprenticesCount;
+                getEmployerMemberSummaryQueryResult!.Sectors = apprenticeship.Sectors;
+            }
+            response = new(memberProfileWithPreferencesQueryResult, null, apprenticeship, isPublicView);
+            mediatorMock.Setup(m => m.Send(It.Is<GetEmployerMemberSummaryQuery>(x => x.EmployerAccountId == accountId), cancellationToken)).ReturnsAsync(getEmployerMemberSummaryQueryResult!);
         }
 
-        GetMemberProfileWithPreferencesModel response = new(memberProfileWithPreferencesQueryResult, myApprenticeship, isPublicView);
-
         mediatorMock.Setup(m => m.Send(It.Is<GetMyApprenticeshipQuery>(x => x.ApprenticeId == appreticeId), cancellationToken)).ReturnsAsync(myApprenticeship);
+
         mediatorMock.Setup(m => m.Send(It.IsAny<GetMemberProfileWithPreferencesQuery>(), cancellationToken)).ReturnsAsync(memberProfileWithPreferencesQueryResult);
 
         Mock<IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration>> apprenticeAccountsApiClientMock = new();
