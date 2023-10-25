795ff87c551be8c979a87c7ce33099741cfb53a0
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-10-25 13:38:28 +0100

    CSP-860 Added Employer call to Apprentice

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/Infrastructure/ICommitmentsV2InnerApiClient.cs
@@ -0,0 +1,13 @@
+﻿using RestEase;
+using SFA.DAS.ApprenticeAan.Application.Employer.Queries.GetEmployerMemberSummary;
+
+namespace SFA.DAS.ApprenticeAan.Infrastructure;
+
+public interface ICommitmentsV2InnerApiClient
+{
+    [Get("/api/accounts/{employerAccountId}/summary")]
+    Task<AccountsSummary?> GetEmployerAccountSummary([Path] long employerAccountId, CancellationToken cancellationToken);
+
+    [Get("/api/apprenticeships/filters")]
+    Task<ApprenticeshipsFilterValues?> GetApprenticeshipsSummaryForEmployer([Query] long employerAccountId, CancellationToken cancellationToken);
+}
