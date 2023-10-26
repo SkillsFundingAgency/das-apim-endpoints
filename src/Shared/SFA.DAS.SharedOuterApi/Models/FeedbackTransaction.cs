310ad09210c0f26a5e02f384d20f5709f7ad3397
Chris Woodcock <chris@humesoftware.com>
2023-10-26 15:02:33 +0100

    Rename to match database field

--- src/Shared/SFA.DAS.SharedOuterApi/Models/FeedbackTransaction.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/Models/FeedbackTransaction.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.SharedOuterApi.Models
 {
     public class FeedbackTransaction
     {
-        public long ApprenticeFeedbackTransactionId { get; set; }
+        public long FeedbackTransactionId { get; set; }
         public Guid ApprenticeId { get; set; }
         public Guid ApprenticeFeedbackTargetId { get; set; }
     }
