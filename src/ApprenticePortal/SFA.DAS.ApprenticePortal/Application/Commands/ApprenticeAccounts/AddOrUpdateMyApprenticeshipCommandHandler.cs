662879826e3fd10f6c810d82d636bb664980e8b7
MarkFCain <mark.cain@digital.education.gov.uk>
2023-11-03 14:56:38 +0000

    CSP-1117 reversed the formatting applied by VS

--- src/ApprenticePortal/SFA.DAS.ApprenticePortal/Application/Commands/ApprenticeAccounts/AddOrUpdateMyApprenticeshipCommandHandler.cs
+++ src/ApprenticePortal/SFA.DAS.ApprenticePortal/Application/Commands/ApprenticeAccounts/AddOrUpdateMyApprenticeshipCommandHandler.cs
@@ -1,100 +1,100 @@
-﻿using MediatR;
-using Microsoft.Extensions.Logging;
-using SFA.DAS.ApprenticePortal.InnerApi.ApprenticeAccounts.Requests;
-using SFA.DAS.ApprenticePortal.InnerApi.CommitmentsV2.Requests;
-using SFA.DAS.ApprenticePortal.InnerApi.CommitmentsV2.Responses;
-using SFA.DAS.ApprenticePortal.Models;
+﻿﻿using System;
+using MediatR;
 using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.Infrastructure;
-using SFA.DAS.SharedOuterApi.InnerApi.Responses.TrainingProviderService;
 using SFA.DAS.SharedOuterApi.Interfaces;
-using SFA.DAS.SharedOuterApi.Services;
-using System;
-using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ApprenticePortal.InnerApi.ApprenticeAccounts.Requests;
+using SFA.DAS.ApprenticePortal.Models;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using System.Net;
+using SFA.DAS.ApprenticePortal.InnerApi.CommitmentsV2.Requests;
+using SFA.DAS.ApprenticePortal.InnerApi.CommitmentsV2.Responses;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.TrainingProviderService;
+using SFA.DAS.SharedOuterApi.Services;
 
-namespace SFA.DAS.ApprenticePortal.Application.Commands.ApprenticeAccounts;
-
-public class AddOrUpdateMyApprenticeshipCommandHandler : IRequestHandler<AddOrUpdateMyApprenticeshipCommand>
+namespace SFA.DAS.ApprenticePortal.Application.Commands.ApprenticeAccounts
 {
-    private readonly IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration> _accountsApiClient;
-    private readonly ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> _commitmentsApiClient;
-    private readonly TrainingProviderService _trainingProviderService;
-    private readonly ILogger<AddOrUpdateMyApprenticeshipCommandHandler> _logger;
-
-    public AddOrUpdateMyApprenticeshipCommandHandler(IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration> accountsApiClient,
-        ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> commitmentsApiClient,
-        TrainingProviderService trainingProviderService,
-        ILogger<AddOrUpdateMyApprenticeshipCommandHandler> logger)
+    public class AddOrUpdateMyApprenticeshipCommandHandler : IRequestHandler<AddOrUpdateMyApprenticeshipCommand>
     {
-        _accountsApiClient = accountsApiClient;
-        _commitmentsApiClient = commitmentsApiClient;
-        _trainingProviderService = trainingProviderService;
-        _logger = logger;
-    }
+        private readonly IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration> _accountsApiClient;
+        private readonly ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> _commitmentsApiClient;
+        private readonly TrainingProviderService _trainingProviderService;
+        private readonly ILogger<AddOrUpdateMyApprenticeshipCommandHandler> _logger;
 
-    public async Task<Unit> Handle(AddOrUpdateMyApprenticeshipCommand request, CancellationToken cancellationToken)
-    {
-        _logger.LogInformation(
-            "AddOrUpdateMyApprenticeshipCommand Apprentice:{ApprenticeId} CommitmentsApprenticeshipId : {CommitmentsApprenticeshipId}",
-            request.ApprenticeId, request.CommitmentsApprenticeshipId);
-
-        var commitmentsApprenticeship = await GetCommitmentsApprenticeshipDetails(request.CommitmentsApprenticeshipId);
-        var myNewApprenticeship = await BuildMyApprenticeship(commitmentsApprenticeship);
-
-        var response = await _accountsApiClient.GetResponseCode(new GetMyApprenticeshipRequest(request.ApprenticeId));
-
-        switch (response)
+        public AddOrUpdateMyApprenticeshipCommandHandler(IApprenticeAccountsApiClient<ApprenticeAccountsApiConfiguration> accountsApiClient,
+            ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> commitmentsApiClient,
+            TrainingProviderService trainingProviderService,
+            ILogger<AddOrUpdateMyApprenticeshipCommandHandler> logger)
         {
-            case HttpStatusCode.NotFound:
-                await _accountsApiClient.Post(new PostApprenticeshipRequest(request.ApprenticeId)
-                { Data = myNewApprenticeship });
-                break;
-            case HttpStatusCode.OK:
-                await _accountsApiClient.Put(new PutApprenticeshipRequest(request.ApprenticeId)
-                { Data = myNewApprenticeship });
-                break;
-            default:
-                _logger.LogError("Calling GetMyApprenticeship returned unexpected HTTP response code of {response}", response);
-                throw new ApplicationException(
-                    $"Call to GetMyApprenticeship, expected an OK or NotFound, but received {response}");
+            _accountsApiClient = accountsApiClient;
+            _commitmentsApiClient = commitmentsApiClient;
+            _trainingProviderService = trainingProviderService;
+            _logger = logger;
         }
 
-
-        return Unit.Value;
-    }
-
-    private async Task<MyApprenticeshipData> BuildMyApprenticeship(ApprenticeshipDetailsResponse commitmentsApprenticeship)
-    {
-        var provider = await _trainingProviderService.GetTrainingProviderDetails(commitmentsApprenticeship.ProviderId);
-
-        return new MyApprenticeshipData
+        public async Task<Unit> Handle(AddOrUpdateMyApprenticeshipCommand request, CancellationToken cancellationToken)
         {
-            ApprenticeshipId = commitmentsApprenticeship.Id,
-            Uln = !string.IsNullOrEmpty(commitmentsApprenticeship?.Uln) ? Convert.ToInt64(commitmentsApprenticeship.Uln) : null,
-            EmployerName = commitmentsApprenticeship.EmployerName,
-            StartDate = commitmentsApprenticeship.StartDate,
-            EndDate = commitmentsApprenticeship.EndDate,
-            TrainingCode = commitmentsApprenticeship.CourseCode,
-            StandardUId = commitmentsApprenticeship.StandardUId,
-            TrainingProviderId = commitmentsApprenticeship.ProviderId,
-            TrainingProviderName = ProviderName(provider)
-        };
-    }
+            _logger.LogInformation(
+                "AddOrUpdateMyApprenticeshipCommand Apprentice:{ApprenticeId} CommitmentsApprenticeshipId : {CommitmentsApprenticeshipId}",
+                request.ApprenticeId, request.CommitmentsApprenticeshipId);
 
-    private static string ProviderName(TrainingProviderResponse trainingProvider)
-        => string.IsNullOrWhiteSpace(trainingProvider.TradingName)
-            ? trainingProvider.LegalName
-            : trainingProvider.TradingName;
+            var commitmentsApprenticeship = await GetCommitmentsApprenticeshipDetails(request.CommitmentsApprenticeshipId);
+            var myNewApprenticeship = await BuildMyApprenticeship(commitmentsApprenticeship);
 
-    public async Task<ApprenticeshipDetailsResponse> GetCommitmentsApprenticeshipDetails(long apprenticeshipId)
-    {
-        var response = await _commitmentsApiClient.GetWithResponseCode<ApprenticeshipDetailsResponse>(new GetApprenticeshipDetailsRequest(apprenticeshipId));
+            var response = await _accountsApiClient.GetResponseCode(new GetMyApprenticeshipRequest(request.ApprenticeId));
 
-        if (response.StatusCode != HttpStatusCode.OK)
-            throw new HttpRequestContentException(response.ErrorContent, response.StatusCode);
+            switch (response)
+            {
+                case HttpStatusCode.NotFound:
+                    await _accountsApiClient.Post(new PostApprenticeshipRequest(request.ApprenticeId)
+                        { Data = myNewApprenticeship });
+                    break;
+                case HttpStatusCode.OK:
+                    await _accountsApiClient.Put(new PutApprenticeshipRequest(request.ApprenticeId)
+                        {Data = myNewApprenticeship});
+                    break;
+                default:
+                    _logger.LogError("Calling GetMyApprenticeship returned unexpected HTTP response code of {response}", response);
+                    throw new ApplicationException(
+                        $"Call to GetMyApprenticeship, expected an OK or NotFound, but received {response}");
+            }
 
-        return response.Body;
+
+            return Unit.Value;
+        }
+
+        private async Task<MyApprenticeshipData> BuildMyApprenticeship(ApprenticeshipDetailsResponse commitmentsApprenticeship)
+        {
+            var provider = await _trainingProviderService.GetTrainingProviderDetails(commitmentsApprenticeship.ProviderId);
+            return new MyApprenticeshipData
+            {
+                ApprenticeshipId = commitmentsApprenticeship.Id,
+                Uln = !string.IsNullOrEmpty(commitmentsApprenticeship?.Uln) ? Convert.ToInt64(commitmentsApprenticeship.Uln) : null,
+                EmployerName = commitmentsApprenticeship.EmployerName,
+                StartDate = commitmentsApprenticeship.StartDate,
+                EndDate = commitmentsApprenticeship.EndDate,
+                TrainingCode = commitmentsApprenticeship.CourseCode,
+                StandardUId = commitmentsApprenticeship.StandardUId,
+                TrainingProviderId = commitmentsApprenticeship.ProviderId,
+                TrainingProviderName = ProviderName(provider)
+            };
+        }
+
+        private static string ProviderName(TrainingProviderResponse trainingProvider)
+            => string.IsNullOrWhiteSpace(trainingProvider.TradingName)
+                ? trainingProvider.LegalName
+                : trainingProvider.TradingName;
+
+        public async Task<ApprenticeshipDetailsResponse> GetCommitmentsApprenticeshipDetails(long apprenticeshipId)
+        {
+            var response = await _commitmentsApiClient.GetWithResponseCode<ApprenticeshipDetailsResponse>(new GetApprenticeshipDetailsRequest(apprenticeshipId));
+
+            if (response.StatusCode != HttpStatusCode.OK)
+                throw new HttpRequestContentException(response.ErrorContent, response.StatusCode);
+
+            return response.Body;
+        }
     }
 }
--- src/ApprenticePortal/SFA.DAS.ApprenticePortal/Models/MyApprenticeshipData.cs
+++ src/ApprenticePortal/SFA.DAS.ApprenticePortal/Models/MyApprenticeshipData.cs
@@ -1,21 +1,22 @@
-﻿using System;
+﻿﻿using System;
 
-namespace SFA.DAS.ApprenticePortal.Models;
-
-public class MyApprenticeship : MyApprenticeshipData
+namespace SFA.DAS.ApprenticePortal.Models
 {
-    public string Title { get; set; }
-}
+    public class MyApprenticeship : MyApprenticeshipData
+    {
+        public string Title { get; set; }
+    }
 
-public class MyApprenticeshipData
-{
-    public long ApprenticeshipId { get; set; }
-    public long? Uln { get; set; }
-    public string EmployerName { get; set; }
-    public DateTime? StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
-    public long TrainingProviderId { get; set; }
-    public string TrainingProviderName { get; set; }
-    public string TrainingCode { get; set; }
-    public string StandardUId { get; set; }
+    public class MyApprenticeshipData
+    {
+        public long ApprenticeshipId { get; set; }
+        public long? Uln { get; set; }
+        public string EmployerName { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public long TrainingProviderId { get; set; }
+        public string TrainingProviderName { get; set; }
+        public string TrainingCode { get; set; }
+        public string StandardUId { get; set; }
+    }
 }
