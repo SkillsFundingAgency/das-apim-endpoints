ac6e3caad6b93987d579d7fa48fc632f6dd86c3a
ManishAgarwalDfE <139247114+ManishAgarwalDfE@users.noreply.github.com>
2023-11-06 11:15:53 +0000

    CSP-928 Fixed merge conflicts in IAanHubRestApiClient.cs

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/NotificationsControllerTests/NotificationsControllerTests.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/NotificationsControllerTests/NotificationsControllerTests.cs
@@ -1,16 +1,18 @@
 ﻿using System.Net;
 using AutoFixture.NUnit3;
-using FluentAssertions.Execution;
 using FluentAssertions;
+using FluentAssertions.Execution;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using RestEase;
 using SFA.DAS.ApprenticeAan.Api.Controllers;
+using SFA.DAS.ApprenticeAan.Api.Models.Notifications;
 using SFA.DAS.ApprenticeAan.Application.Infrastructure;
 using SFA.DAS.ApprenticeAan.Application.InnerApi.Notifications;
 using SFA.DAS.Testing.AutoFixture;
-using RestEase;
 
-namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers;
+namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers.NotificationsControllerTests;
 public class NotificationsControllerTests
 {
     [Test, MoqAutoData]
@@ -21,12 +23,15 @@ public class NotificationsControllerTests
     Guid notificationId,
     Guid requestedByMemberId)
     {
+        //Arrange
         notificationsResponse.MemberId = requestedByMemberId;
         apiClientMock.Setup(a => a.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>())).ReturnsAsync(new Response<GetNotificationResponse?>(string.Empty, new(HttpStatusCode.NotFound), () => null));
 
+        //Act
         var actual = await sut.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>());
         var actualObjectResult = actual as ObjectResult;
 
+        //Assert
         using (new AssertionScope())
         {
             apiClientMock.Verify(a => a.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>()));
@@ -44,12 +49,15 @@ public class NotificationsControllerTests
     Guid notificationId,
     Guid requestedByMemberId)
     {
+        //Arrange
         notificationsResponse.MemberId = requestedByMemberId;
         apiClientMock.Setup(a => a.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>())).ReturnsAsync(new Response<GetNotificationResponse?>(string.Empty, new(HttpStatusCode.OK), () => notificationsResponse));
 
+        //Act
         var actual = await sut.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>());
         var actualObjectResult = actual as ObjectResult;
 
+        //Assert
         using (new AssertionScope())
         {
             apiClientMock.Verify(a => a.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>()));
@@ -67,15 +75,78 @@ public class NotificationsControllerTests
     Guid notificationId,
     Guid requestedByMemberId)
     {
+        //Arrange
         notificationsResponse.MemberId = requestedByMemberId;
         apiClientMock.Setup(a => a.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>())).ReturnsAsync(new Response<GetNotificationResponse?>(string.Empty, new(HttpStatusCode.Unauthorized), () => null));
 
+        //Act
         Func<Task> actual = async () => await sut.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>());
 
+        //Assert
         using (new AssertionScope())
         {
             await actual.Should().ThrowAsync<InvalidOperationException>().WithMessage("Get notification didn't come back with a successful response");
             apiClientMock.Verify(a => a.GetNotification(notificationId, requestedByMemberId, It.IsAny<CancellationToken>()));
+        }
+    }
+
+    [Test, MoqAutoData]
+    public async Task CreateNotification_InvokesCommand(
+    [Frozen] Mock<IAanHubRestApiClient> apiClientMock,
+    [Greedy] NotificationsController sut,
+    CreateNotificationModel model,
+    Guid requestedByMemberId,
+    CancellationToken cancellationToken)
+    {
+        //Arrange
+        Response<GetNotificationResponse> notificationsResponse = new Response<GetNotificationResponse>(string.Empty, new(HttpStatusCode.Created), () => null!);
+        apiClientMock.Setup(a => a.PostNotification(requestedByMemberId, It.IsAny<PostNotificationRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(notificationsResponse);
+
+        //Act
+        var response = await sut.CreateNotification(requestedByMemberId, model, cancellationToken);
+
+        //Assert
+        apiClientMock.Verify(a => a.PostNotification(requestedByMemberId, It.IsAny<PostNotificationRequest>(), It.IsAny<CancellationToken>()));
+    }
+
+    [Test, MoqAutoData]
+    public void CreateNotification_InvalidCommand_ThrowsInvalidOperationException(
+    [Frozen] Mock<IMediator> mediatorMock,
+    [Greedy] NotificationsController sut,
+    CreateNotificationModel model,
+    Guid requestedByMemberId,
+    Response<GetNotificationResponse> expected,
+    CancellationToken cancellationToken)
+    {
+        //Arrange
+        expected.ResponseMessage.StatusCode = HttpStatusCode.NotFound;
+        mediatorMock.Setup(m => m.Send(It.IsAny<PostNotificationRequest>(), cancellationToken)).ReturnsAsync(expected.GetContent());
+
+        //Act
+        Assert.That(() => sut.CreateNotification(requestedByMemberId, model, cancellationToken), Throws.InvalidOperationException);
+    }
+
+    [Test, MoqAutoData]
+    public async Task CreateNotification_ValidCommand_ReturnsOk(
+    [Frozen] Mock<IAanHubRestApiClient> apiClientMock,
+    [Greedy] NotificationsController sut,
+    CreateNotificationModel model,
+    Guid requestedByMemberId,
+    CancellationToken cancellationToken)
+    {
+        //Arrange
+        Response<GetNotificationResponse> notificationsResponse = new Response<GetNotificationResponse>(string.Empty, new(HttpStatusCode.Created), () => null!);
+        apiClientMock.Setup(a => a.PostNotification(requestedByMemberId, It.IsAny<PostNotificationRequest>(), It.IsAny<CancellationToken>())).ReturnsAsync(notificationsResponse);
+
+        //Act
+        var response = await sut.CreateNotification(requestedByMemberId, model, cancellationToken);
+        var actualObjectResult = response as ObjectResult;
+
+        //Assert
+        using (new AssertionScope())
+        {
+            apiClientMock.Verify(a => a.PostNotification(requestedByMemberId, It.IsAny<PostNotificationRequest>(), It.IsAny<CancellationToken>()));
+            actualObjectResult.Should().BeOfType<OkObjectResult>();
         }
     }
 }
--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/NotificationsController.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/NotificationsController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Api.Models.Notifications;
 using SFA.DAS.ApprenticeAan.Application.Infrastructure;
 using SFA.DAS.ApprenticeAan.Application.Infrastructure.Configuration;
 using SFA.DAS.ApprenticeAan.Application.InnerApi.Notifications;
-using System.Net;
 
 namespace SFA.DAS.ApprenticeAan.Api.Controllers;
 
@@ -11,7 +12,6 @@ namespace SFA.DAS.ApprenticeAan.Api.Controllers;
 public class NotificationsController : Controller
 {
     private readonly IAanHubRestApiClient _outerApiClient;
-
     public NotificationsController(IAanHubRestApiClient outerApiClient)
     {
         _outerApiClient = outerApiClient;
@@ -27,6 +27,20 @@ public class NotificationsController : Controller
             HttpStatusCode.OK => response.GetContent(),
             HttpStatusCode.NotFound => null,
             _ => throw new InvalidOperationException("Get notification didn't come back with a successful response")
+        };
+        return Ok(result);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(GetNotificationResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> CreateNotification([FromHeader(Name = Constants.ApiHeaders.RequestedByMemberIdHeader)] Guid requestedByMemberId, [FromBody] CreateNotificationModel model, CancellationToken cancellationToken)
+    {
+        var response = await _outerApiClient.PostNotification(requestedByMemberId, new PostNotificationRequest() { RequestedByMemberId = requestedByMemberId, MemberId = model.MemberId, NotificationTemplateId = model.NotificationTemplateId }, cancellationToken);
+        var result = response.ResponseMessage.StatusCode switch
+        {
+            HttpStatusCode.Created => response.GetContent(),
+            HttpStatusCode.NotFound => null,
+            _ => throw new InvalidOperationException("Post notification didn't come back with a successful response")
         };
         return Ok(result);
     }
