dcb25cc95dea2f2f3df63997a02b8cf511de5c44
MarkFCain <mark.cain@digital.education.gov.uk>
2023-11-09 11:58:15 +0000

    CSP-1095 Get Calendar Event augmented with region name, school name and all fields

--- src/AdminAan/SFA.DAS.AdminAan/Application/CalendarEvents/Queries/GetCalendarEvent/GetCalendarEventQueryHandler.cs
+++ src/AdminAan/SFA.DAS.AdminAan/Application/CalendarEvents/Queries/GetCalendarEvent/GetCalendarEventQueryHandler.cs
@@ -5,15 +5,31 @@ namespace SFA.DAS.AdminAan.Application.CalendarEvents.Queries.GetCalendarEvent;
 public class GetCalendarEventQueryHandler : IRequestHandler<GetCalendarEventQuery, GetCalendarEventQueryResult?>
 {
     private readonly IAanHubRestApiClient _apiClient;
-
-    public GetCalendarEventQueryHandler(IAanHubRestApiClient apiClient)
+    private readonly IReferenceDataApiClient _apiReferenceDataApiClient;
+    public GetCalendarEventQueryHandler(IAanHubRestApiClient apiClient, IReferenceDataApiClient apiReferenceDataApiClient)
     {
         _apiClient = apiClient;
+        _apiReferenceDataApiClient = apiReferenceDataApiClient;
     }
 
     public async Task<GetCalendarEventQueryResult?> Handle(GetCalendarEventQuery request, CancellationToken cancellationToken)
     {
-        return await _apiClient.GetCalendarEvent(request.RequestedByMemberId, request.CalendarEventId, cancellationToken);
+        var result = await _apiClient.GetCalendarEvent(request.RequestedByMemberId, request.CalendarEventId, cancellationToken);
+
+        if (result?.RegionId != null)
+        {
+            var regionsResult = await _apiClient.GetRegions(cancellationToken);
+            result.RegionName = regionsResult.Regions.FirstOrDefault(x => x.Id == result.RegionId)?.Area;
+        }
+
+        if (!string.IsNullOrEmpty(result?.Urn.ToString()))
+        {
+            var organisationTypeEducational = 4;
+            var school = await _apiReferenceDataApiClient.GetSchoolFromUrn(result.Urn.ToString()!, organisationTypeEducational, cancellationToken);
+            result.SchoolName = school.GetContent()?.Name;
+        }
+
+        return result;
     }
 }
 
--- src/AdminAan/SFA.DAS.AdminAan/Infrastructure/IReferenceDataApiClient.cs
+++ src/AdminAan/SFA.DAS.AdminAan/Infrastructure/IReferenceDataApiClient.cs
@@ -8,4 +8,7 @@ public interface IReferenceDataApiClient
     [Get("api/organisations/educational")]
     [AllowAnyStatusCode]
     Task<Response<GetSchoolsQueryApiResult>> GetSchools(string searchTerm, int pageSize, int pageNumber, CancellationToken cancellationToken);
+
+    [Get("api/organisations/get")]
+    Task<Response<GetSchoolApiResult>> GetSchoolFromUrn(string identifier, int organisationType, CancellationToken cancellationToken);
 }
