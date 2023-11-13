448ddc21edc064945f3424e6f786a733bc755ffc
ratheeshri1984 <ratheeshri1984@gmail.com>
2023-11-13 08:29:21 +0000

    EC-218-Removed commented code

--- src/EarlyConnect/SFA.DAS.EarlyConnect/Application/Commands/CreateStudentData/CreateStudentDataCommandHandler.cs
+++ src/EarlyConnect/SFA.DAS.EarlyConnect/Application/Commands/CreateStudentData/CreateStudentDataCommandHandler.cs
@@ -25,37 +25,5 @@ namespace SFA.DAS.EarlyConnect.Application.Commands.CreateStudentData
 
             return Unit.Value;
         }
-
-        //public async Task<Unit> Handle(CreateStudentDataCommand request, CancellationToken cancellationToken)
-        //{
-
-        //    InnerApi.Requests.CreateLog createLog = new InnerApi.Requests.CreateLog
-        //    {
-        //        RequestType = request.RequestType,
-        //        RequestSource = request.RequestSource,
-        //        RequestIP = request.RequestIP,
-        //        Payload = request.Payload,
-        //        Status = CreateLogResponse.BulkUploadStatus.InProgress.ToString()
-        //    };
-
-        //    var createLogresponse = await _apiClient.PostWithResponseCode<InnerApi.Responses.CreateLogResponse>(new CreateLogRequest(createLog));
-
-        //    createLogresponse.EnsureSuccessStatusCode();
-
-        //    var response = await _apiClient.PostWithResponseCode<InnerApi.Requests.StudentData>(new CreateStudentDataRequest(request.StudentDataList), false);
-
-        //    InnerApi.Requests.UpdateLog updateLog = new InnerApi.Requests.UpdateLog
-        //    {
-        //        LogId = createLogresponse.Body.Id,
-        //        Status = response.StatusCode == HttpStatusCode.OK ? CreateLogResponse.BulkUploadStatus.Completed.ToString() : CreateLogResponse.BulkUploadStatus.Error.ToString(),
-        //        Error = response.ErrorContent
-        //    };
-
-        //    await _apiClient.PostWithResponseCode<InnerApi.Requests.UpdateLog>(new UpdateLogRequest(updateLog), false);
-
-        //    response.EnsureSuccessStatusCode();
-
-        //    return Unit.Value;
-        //}
     }
 }
