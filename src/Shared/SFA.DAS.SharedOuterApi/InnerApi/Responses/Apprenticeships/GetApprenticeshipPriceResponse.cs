2dc356a1fc106f9de81be548bc835fe4e1a838b9
Tom Gough <tom.gough@talentconsulting.uk>
2023-11-15 16:02:14 +0000

    FundingBandMaximum is now an int not a decimal (stored as an int so no value in passing it around as a decimal)

--- src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Apprenticeships/GetApprenticeshipPriceResponse.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Apprenticeships/GetApprenticeshipPriceResponse.cs
@@ -5,7 +5,7 @@ namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Apprenticeships
     public class GetApprenticeshipPriceResponse
     {
         public Guid ApprenticeshipKey { get; set; }
-        public decimal FundingBandMaximum { get; set; }
+        public int FundingBandMaximum { get; set; }
         public decimal TrainingPrice { get; set; }
         public decimal AssessmentPrice { get; set; }
         public DateTime EarliestEffectiveDate { get; set; }
