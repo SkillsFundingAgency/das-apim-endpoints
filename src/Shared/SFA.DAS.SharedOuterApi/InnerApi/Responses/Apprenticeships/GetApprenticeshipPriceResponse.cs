44f2f7a5385c5f664a2247a50b9b2c84eadf1573
Mike-V-Digital <122551010+Mike-V-Digital@users.noreply.github.com>
2023-11-15 09:56:08 +0000

    Update GetApprenticeshipPriceResponse.cs

--- src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Apprenticeships/GetApprenticeshipPriceResponse.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Apprenticeships/GetApprenticeshipPriceResponse.cs
@@ -1,7 +1,13 @@
-﻿namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Apprenticeships
+﻿using System;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Apprenticeships
 {
     public class GetApprenticeshipPriceResponse
     {
-        public ApprenticeshipPrice ApprenticeshipPrice { get; set; }
+        public Guid ApprenticeshipKey { get; set; }
+        public decimal FundingBandMaximum { get; set; }
+        public decimal TrainingPrice { get; set; }
+        public decimal AssessmentPrice { get; set; }
+        public DateTime EarliestEffectiveDate { get; set; }
     }
 }
