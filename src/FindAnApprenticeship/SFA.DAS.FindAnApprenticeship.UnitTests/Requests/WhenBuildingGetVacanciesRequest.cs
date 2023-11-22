8a1e10ce933bdffeef095bcb480e578ca2e35f5e
A-J-Wright <alissa.wright@education.gov.uk>
2023-11-22 10:36:04 +0000

    Revert "vacancy re-do"
    This reverts commit 842fe46c8ca3579c676545fa93db7b726c85ec4c.
    
    Add request test

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Requests/WhenBuildingGetVacanciesRequest.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindAnApprenticeship.InnerApi.Requests;
+
+namespace SFA.DAS.FindAnApprenticeship.UnitTests.Requests;
+
+public class WhenBuildingGetVacanciesRequest
+{
+    public class WhenBuildingGetApprenticeshipCountRequest
+    {
+        [Test, AutoData]
+        public void Then_The_Request_Url_Is_Correctly_Built(double lat, double lon, List<string> routes, int distance)
+        {
+            var actual = new GetApprenticeshipCountRequest(lat, lon, routes, distance);
+
+            actual.GetUrl.Should().Be($"/api/vacancies?lat={lat}&lon={lon}&routes={routes}&distance={distance}");
+        }
+    }
+}
