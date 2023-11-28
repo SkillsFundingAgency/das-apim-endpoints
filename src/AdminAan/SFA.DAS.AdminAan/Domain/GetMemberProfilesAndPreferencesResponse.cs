54cbaf638e413124cbd3c98009091b42e3b20a12
Deven Shah <devenshah1974@gmail.com>
2023-11-28 17:45:35 +0000

    CSP-1088 Added inner api method to retrieve profiles and preferences

---
+++ src/AdminAan/SFA.DAS.AdminAan/Domain/GetMemberProfilesAndPreferencesResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.AdminAan.Domain;
+
+public record GetMemberProfilesAndPreferencesResponse(IEnumerable<MemberProfileModel> Profiles, IEnumerable<MemberPreferenceModel> Preferences);
+
+public record MemberProfileModel(int ProfileId, string value, int? PreferenceId);
+
+public record MemberPreferenceModel(int PreferenceId, bool Value)
--- src/AdminAan/SFA.DAS.AdminAan/Infrastructure/IAanHubRestApiClient.cs
+++ src/AdminAan/SFA.DAS.AdminAan/Infrastructure/IAanHubRestApiClient.cs
@@ -54,8 +54,10 @@ public interface IAanHubRestApiClient
     [AllowAnyStatusCode]
     Task<Response<LookupAdminMemberResult>> GetMemberByEmail([Path] string email, CancellationToken cancellationToken);
 
+    [Get("/members/{memberId}/profile?public=false")]
+    Task<GetMemberProfilesAndPreferencesResponse> GetMemberProfileWithPreferences([Path] Guid memberId, [Header(Constants.ApiHeaders.RequestedByMemberIdHeader)] Guid requestedByMemberId, CancellationToken cancellationToken);
+
     [Post("/admins")]
     [AllowAnyStatusCode]
     Task<CreateAdminMemberCommandResult> CreateAdminMember([Body] CreateAdminMemberCommand command, CancellationToken cancellationToken);
 }
-
