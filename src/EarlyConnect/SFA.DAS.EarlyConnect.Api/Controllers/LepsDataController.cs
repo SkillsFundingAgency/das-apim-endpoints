d7ba48597ef86552824ef8f2e4a10d671bcaeb0b
ratheeshri1984 <ratheeshri1984@gmail.com>
2023-12-04 10:12:56 +0000

    EC-245-LEPS data with users

---
+++ src/EarlyConnect/SFA.DAS.EarlyConnect.Api/Controllers/LepsDataController.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using SFA.DAS.EarlyConnect.Api.Models;
+using SFA.DAS.EarlyConnect.Application.Queries.GetLEPSDataWithUsers;
+
+namespace SFA.DAS.EarlyConnect.Api.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("/api/leps-data/")]
+    public class LepsDataController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<LepsDataController> _logger;
+
+        public LepsDataController(IMediator mediator, ILogger<LepsDataController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        [Route("")]
+        public async Task<IActionResult> GetLepsDataWithUsers()
+        {
+            try
+            {
+                var result = await _mediator.Send(new GetLEPSDataWithUsersQuery { });
+
+                return Ok((GetLEPSDataListWithUsersResponse)result);
+            }
+            catch (Exception e)
+            {
+                var errorMessage = (e as SharedOuterApi.Exceptions.ApiResponseException)?.Error;
+
+                _logger.LogError(e, "Error getting leps data with users ");
+
+                return BadRequest($"Error getting leps data with users. {(errorMessage != null ? $"\nErrorInfo: {errorMessage}" : "")}");
+            }
+        }
+    }
+}
