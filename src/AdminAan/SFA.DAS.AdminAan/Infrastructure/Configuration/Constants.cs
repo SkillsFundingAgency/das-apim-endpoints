63b5e347d1ceb1e77be8f8fa88c90088bd7930df
Deven Shah <devenshah1974@gmail.com>
2023-12-04 16:34:26 +0000

    CSP-1088 Moved live status constant into AdminsController

--- src/AdminAan/SFA.DAS.AdminAan/Infrastructure/Configuration/Constants.cs
+++ src/AdminAan/SFA.DAS.AdminAan/Infrastructure/Configuration/Constants.cs
@@ -1,16 +1,8 @@
-﻿using System.ComponentModel;
-
-namespace SFA.DAS.AdminAan.Infrastructure.Configuration;
+﻿namespace SFA.DAS.AdminAan.Infrastructure.Configuration;
 public static class Constants
 {
     public static class ApiHeaders
     {
         public const string RequestedByMemberIdHeader = "X-RequestedByMemberId";
-    }
-
-    public enum Status
-    {
-        [Description("Live")]
-        Live
     }
 }
