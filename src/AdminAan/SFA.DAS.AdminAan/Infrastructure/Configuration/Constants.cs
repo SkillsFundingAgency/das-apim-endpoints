88b51719046d7b8196e027cb2062386fd29f45d8
Deven Shah <devenshah1974@gmail.com>
2023-12-04 16:29:53 +0000

    CSP-1088 Moved PageSize constant with in calendar events handler

--- src/AdminAan/SFA.DAS.AdminAan/Infrastructure/Configuration/Constants.cs
+++ src/AdminAan/SFA.DAS.AdminAan/Infrastructure/Configuration/Constants.cs
@@ -8,11 +8,6 @@ public static class Constants
         public const string RequestedByMemberIdHeader = "X-RequestedByMemberId";
     }
 
-    public static class AdminSettings
-    {
-        public const int PageSize = 10;
-    }
-
     public enum Status
     {
         [Description("Live")]
