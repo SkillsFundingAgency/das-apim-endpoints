3fe2de028f51efb3f1920d6301d9948c53bda4ae
A-J-Wright <alissa.wright@education.gov.uk>
2023-12-15 09:05:01 +0000

    test for version

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Requests/WhenBuildingGetVacanciesRequest.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Requests/WhenBuildingGetVacanciesRequest.cs
@@ -21,5 +21,7 @@ public class WhenBuildingGetVacanciesRequest
         var actual = new GetVacanciesRequest(lat, lon, routes, distance, sort, whatSearchTerm, pageNumber, pageSize);
 
         actual.GetUrl.Should().Be($"/api/vacancies?lat={lat}&lon={lon}&routes={string.Join("&routes=", routes)}&distanceInMiles={distance}&sort={sort}&searchTerm={whatSearchTerm}&pageNumber={pageNumber}&pageSize={pageSize}");
+
+        actual.Version.Should().Be("2.0");
     }
 }
