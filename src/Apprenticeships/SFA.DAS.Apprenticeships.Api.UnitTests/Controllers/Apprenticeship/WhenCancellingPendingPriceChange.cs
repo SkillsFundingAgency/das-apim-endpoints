6d2554bf02bfa4f8c260c6b9ebe3d25557bdaf03
Michael Young <mick.young1981@googlemail.com>
2023-12-21 13:40:34 +0000

    Cancel outer api endpoint

---
+++ src/Apprenticeships/SFA.DAS.Apprenticeships.Api.UnitTests/Controllers/Apprenticeship/WhenCancellingPendingPriceChange.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Apprenticeships.Api.Controllers;
+using SFA.DAS.Apprenticeships.Api.Models;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Apprenticeships;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Apprenticeships;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+using System.Threading.Tasks;
+using AutoFixture;
+
+namespace SFA.DAS.Apprenticeships.Api.UnitTests.Controllers.Apprenticeship
+{
+    public class WhenCancellingPendingPriceChange
+    {
+	    private Mock<IApprenticeshipsApiClient<ApprenticeshipsApiConfiguration>> _mockApprenticeshipsApiClient;
+	    private ApprenticeshipController _sut;
+        private Fixture _fixture;
+
+		[SetUp]
+	    public void SetUp()
+	    {
+            _fixture = new Fixture();
+
+		    _mockApprenticeshipsApiClient = new Mock<IApprenticeshipsApiClient<ApprenticeshipsApiConfiguration>>();
+            _sut = new ApprenticeshipController(_mockApprenticeshipsApiClient.Object, Mock.Of<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>>());
+	    }
+
+        [Test]
+        public async Task Then_Deletes_PendingPriceChangePrice_From_ApiClient()
+        {
+            //  Arrange
+            var apprenticeshipKey = _fixture.Create<Guid>();
+
+            //  Act
+            var result = await _sut.CancelPendingPriceChange(apprenticeshipKey);
+
+            //  Assert
+            result.ShouldBeOfType<OkResult>();
+            _mockApprenticeshipsApiClient.Verify(x =>
+                x.Delete(It.Is<CancelPendingPriceChangeRequest>(r => r.ApprenticeshipKey == apprenticeshipKey)), Times.Once);
+        }
+    }
+}
