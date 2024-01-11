337d9905cefb1b83edba7f978ba28e9d4f6f8089
chrisfoster76 <chrisfoster186@googlemail.com>
2024-01-11 14:10:53 +0000

    Adds controller and query for TaskList page WIP

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/Models/Applications/GetIndexApiResponse.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.Index;
+
+namespace SFA.DAS.FindAnApprenticeship.Api.Models.Applications
+{
+    public class GetIndexApiResponse
+    {
+        public string VacancyTitle { get; set; }
+
+        public static implicit operator GetIndexApiResponse(GetIndexQueryResult source)
+        {
+            return new GetIndexApiResponse
+            {
+                VacancyTitle = source.VacancyTitle
+            };
+        }
+    }
+}
---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Apply/Index/GetIndexQuery.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.Index
+{
+    public class GetIndexQuery : IRequest<GetIndexQueryResult>
+    {
+        public string ApplicantEmailAddress { get; set; }
+        public string VacancyReference { get; set; }
+    }
+
+    public class GetIndexQueryResult
+    {
+        public string VacancyTitle { get; set; }
+    }
+
+    public class GetIndexQueryHandler : IRequestHandler<GetIndexQuery,GetIndexQueryResult>
+    {
+        public GetIndexQueryHandler(IMediator mediator, ILogger<GetIndexQueryHandler> logger)
+        {
+            
+        }
+
+        public Task<GetIndexQueryResult> Handle(GetIndexQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new GetIndexQueryResult());
+        }
+    }
+
+}
