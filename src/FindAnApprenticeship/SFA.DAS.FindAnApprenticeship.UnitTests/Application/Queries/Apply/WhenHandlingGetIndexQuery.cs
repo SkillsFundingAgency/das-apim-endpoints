6fb7be7ec3566b8428b0b76292a70dcb3f8b1389
chrisfoster76 <chrisfoster186@googlemail.com>
2024-01-11 20:10:31 +0000

    WIP on tasklist api response

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Application/Queries/Apply/WhenHandlingGetIndexQuery.cs
@@ -0,0 +1,41 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.Index;
+using SFA.DAS.FindAnApprenticeship.InnerApi.Requests;
+using SFA.DAS.FindAnApprenticeship.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindAnApprenticeship.UnitTests.Application.Queries.Apply
+{
+    [TestFixture]
+    public class WhenHandlingGetIndexQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_VacancyDetails_Are_Returned_As_Expected(
+            GetIndexQuery query,
+            GetApprenticeshipVacancyItemResponse apiResponse,
+            [Frozen] Mock<IFindApprenticeshipApiClient<FindApprenticeshipApiConfiguration>> apiClient,
+            GetIndexQueryHandler handler)
+        {
+            var expectedRequest = new GetVacancyRequest(query.VacancyReference);
+
+            apiClient
+                .Setup(client => client.Get<GetApprenticeshipVacancyItemResponse>(It.Is<GetVacancyRequest>(r => r.GetUrl == expectedRequest.GetUrl)))
+                .ReturnsAsync(apiResponse);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Should().BeEquivalentTo(new
+            {
+                VacancyTitle = apiResponse.Title,
+                EmployerName = apiResponse.EmployerName,
+                ClosingDate = apiResponse.ClosingDate,
+                IsDisabilityConfident = apiResponse.IsDisabilityConfident
+            });
+        }
+    }
+}
--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Apply/Index/GetIndexQuery.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Apply/Index/GetIndexQuery.cs
@@ -1,7 +1,13 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.FindAnApprenticeship.InnerApi.Requests;
+using SFA.DAS.FindAnApprenticeship.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using static SFA.DAS.FindAnApprenticeship.Domain.Constants;
 
 namespace SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.Index
 {
@@ -14,19 +20,90 @@ namespace SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.Index
     public class GetIndexQueryResult
     {
         public string VacancyTitle { get; set; }
+        public string EmployerName { get; set; }
+        public DateTime ClosingDate { get; set; }
+        public bool IsDisabilityConfident { get; set; }
+        public EducationHistorySection EducationHistory { get; set; }
+        public WorkHistorySection WorkHistory { get; set; }
+        public ApplicationQuestionsSection ApplicationQuestions { get; set; }
+        public InterviewAdjustmentsSection InterviewAdjustments { get; set; }
+        public DisabilityConfidenceSection DisabilityConfidence { get; set; }
+
+        public class EducationHistorySection
+        {
+                public string Qualifications { get; set; }
+                public string TrainingCourses { get; set; }
+        }
+
+        public class WorkHistorySection
+        {
+            public string Jobs { get; set; }
+            public string VolunteeringAndWorkExperience { get; set; }
+        }
+
+        public class ApplicationQuestionsSection
+        {
+            public string SkillsAndStrengths { get; set; }
+            public string WhatInterestsYou { get; set; }
+            public string Travel { get; set; }
+        }
+
+        public class InterviewAdjustmentsSection
+        {
+            public string RequestAdjustments { get; set; }
+        }
+        public class DisabilityConfidenceSection
+        {
+            public string InterviewUnderDisabilityConfident { get; set; }
+        }
     }
 
     public class GetIndexQueryHandler : IRequestHandler<GetIndexQuery,GetIndexQueryResult>
     {
-        public GetIndexQueryHandler(IMediator mediator, ILogger<GetIndexQueryHandler> logger)
+        private readonly IFindApprenticeshipApiClient<FindApprenticeshipApiConfiguration> _findApprenticeshipApiClient;
+
+        public GetIndexQueryHandler(IFindApprenticeshipApiClient<FindApprenticeshipApiConfiguration> findApprenticeshipApiClient)
         {
-            
+            _findApprenticeshipApiClient = findApprenticeshipApiClient;
         }
 
-        public Task<GetIndexQueryResult> Handle(GetIndexQuery request, CancellationToken cancellationToken)
+        public async Task<GetIndexQueryResult> Handle(GetIndexQuery request, CancellationToken cancellationToken)
         {
-            return Task.FromResult(new GetIndexQueryResult());
+            var result = await _findApprenticeshipApiClient.Get<GetApprenticeshipVacancyItemResponse>(new GetVacancyRequest(request.VacancyReference));
+
+            if (result is null) return null;
+
+            return new GetIndexQueryResult
+            {
+                VacancyTitle = result.Title,
+                EmployerName = result.EmployerName,
+                ClosingDate = result.ClosingDate,
+                IsDisabilityConfident = result.IsDisabilityConfident,
+                EducationHistory = new GetIndexQueryResult.EducationHistorySection
+                {
+                    Qualifications = SectionStatus.NotYetStarted,
+                    TrainingCourses = SectionStatus.NotYetStarted
+                },
+                WorkHistory = new GetIndexQueryResult.WorkHistorySection
+                {
+                    Jobs = SectionStatus.NotYetStarted,
+                    VolunteeringAndWorkExperience = SectionStatus.NotYetStarted
+                },
+                ApplicationQuestions = new GetIndexQueryResult.ApplicationQuestionsSection
+                {
+                    SkillsAndStrengths = SectionStatus.NotYetStarted,
+                    WhatInterestsYou = SectionStatus.NotYetStarted,
+                    Travel = SectionStatus.NotYetStarted
+                },
+                InterviewAdjustments = new GetIndexQueryResult.InterviewAdjustmentsSection
+                {
+                    RequestAdjustments = SectionStatus.NotYetStarted
+                },
+                DisabilityConfidence = new GetIndexQueryResult.DisabilityConfidenceSection
+                {
+                    InterviewUnderDisabilityConfident = SectionStatus.NotYetStarted
+                }
+            };
         }
     }
-
 }
