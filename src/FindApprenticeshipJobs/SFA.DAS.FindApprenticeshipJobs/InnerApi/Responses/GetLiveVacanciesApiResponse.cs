08cdc4c264c3b7459f44b540a5f666ad42594790
chrisfoster76 <chrisfoster186@googlemail.com>
2024-01-15 20:20:18 +0000

    Mapping fix

--- src/FindApprenticeshipJobs/SFA.DAS.FindApprenticeshipJobs/InnerApi/Responses/GetLiveVacanciesApiResponse.cs
+++ src/FindApprenticeshipJobs/SFA.DAS.FindApprenticeshipJobs/InnerApi/Responses/GetLiveVacanciesApiResponse.cs
@@ -15,7 +15,9 @@ public class LiveVacancy
     public DateTime ClosingDate { get; set; }
     public string? Description { get; set; }
     public DisabilityConfident DisabilityConfident { get; set; }
-    public EmployerContact? EmployerContact { get; set; }
+    public string? EmployerContactEmail { get; set; }
+    public string? EmployerContactName { get; set; }
+    public string? EmployerContactPhone { get; set; }
     public string? EmployerDescription { get; set; }
     public Address? EmployerLocation { get; set; }
     public string? EmployerName { get; set; }
@@ -83,11 +85,4 @@ public class Qualification
     public string Subject { get; set; } = null!;
     public string Grade { get; set; } = null!;
     public string Weighting { get; set; } = null!;
-}
-
-public class EmployerContact 
-{
-    public string? EmployerContactEmail { get; set; }
-    public string? EmployerContactName { get; set; }
-    public string? EmployerContactPhone { get; set; }
 }
