40c73a229d61f3a9f27f589f4a206ad8518c89ac
A-J-Wright <alissa.wright@education.gov.uk>
2024-01-17 12:09:46 +0000

    update handler and request

--- src/Campaign/SFA.DAS.Campaign/Application/Queries/Adverts/GetAdvertsQueryHandler.cs
+++ src/Campaign/SFA.DAS.Campaign/Application/Queries/Adverts/GetAdvertsQueryHandler.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Campaign.Configuration;
+using SFA.DAS.Campaign.InnerApi.Requests;
 using SFA.DAS.Campaign.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.Campaign.Application.Queries.Adverts
@@ -35,11 +35,9 @@ namespace SFA.DAS.Campaign.Application.Queries.Adverts
         public async Task<GetAdvertsQueryResult> Handle(GetAdvertsQuery request, CancellationToken cancellationToken)
         {
             var routesTask = _courseService.GetRoutes();
-            var standardsTask =
-                _courseService.GetActiveStandards<GetStandardsListResponse>(nameof(GetStandardsListResponse));
             var locationTask = _locationLookupService.GetLocationInformation(request.Postcode, 0, 0);
 
-            await Task.WhenAll(routesTask, locationTask, standardsTask);
+            await Task.WhenAll(routesTask, locationTask);
 
             if (locationTask.Result == null)
             {
@@ -50,33 +48,17 @@ namespace SFA.DAS.Campaign.Application.Queries.Adverts
                 };
             }
 
+            var apprenticeshipVacancies = new List<GetVacanciesListItem>();
 
-            var standardLarsCode = standardsTask.Result.Standards.Where(c=>c.Route.Equals(request.Route, StringComparison.CurrentCultureIgnoreCase)).Select(c=>c.LarsCode).ToList();
+            var vacancyResult = await _findApprenticeshipApiClient.Get<GetVacanciesResponse>(
+                new GetVacanciesRequest(
+                    locationTask.Result.GeoPoint.FirstOrDefault(),
+                    locationTask.Result.GeoPoint.LastOrDefault(),
+                    request.Distance,
+                    0,
+                    20,
+                    request.Route));
 
-            var apprenticeshipVacancies = new List<GetVacanciesListItem>(); 
-               
-            var skip = 0;
-            var take = 15;
-            
-            while (true)
-            {
-                var standards = standardLarsCode.Skip(skip).Take(take).ToList();
-
-                if (standards.Count == 0)
-                {
-                    break;
-                }
-                
-                var advertRequest = new GetVacanciesRequest(0, 20, null, null, null, 
-                    standards, null,
-                    locationTask.Result.GeoPoint.FirstOrDefault(), locationTask.Result.GeoPoint.LastOrDefault(),
-                    request.Distance, null, null, "DistanceAsc");
-
-                var adverts = await _findApprenticeshipApiClient.Get<GetVacanciesResponse>(advertRequest);
-                
-                apprenticeshipVacancies.AddRange(adverts.ApprenticeshipVacancies.ToList());
-                skip += take;
-            }
 
             foreach (var advert in apprenticeshipVacancies)
             {
