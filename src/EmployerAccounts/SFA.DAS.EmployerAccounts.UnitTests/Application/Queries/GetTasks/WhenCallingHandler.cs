e9005ea0323a1fffe6bde541ec700c5753de1386
ogrwill <will.ogrady@digital.education.gov.uk>
2024-01-17 09:20:55 +0000

    APPMAN-698 unit test

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetTasks/WhenCallingHandler.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetTasks/WhenCallingHandler.cs
@@ -1,23 +1,56 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoFixture;
 using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerAccounts.Application.Queries.GetTasks;
-using SFA.DAS.Testing.AutoFixture;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Commitments;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Commitments;
+using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.GetTasks
 {
     [TestFixture]
     public class WhenCallingHandler
     {
-        [Test, MoqAutoData]
-        public async Task Then_Gets_Tasks_Returns_GetTasksQueryResult(
-            GetTasksQuery query,
-            GetTasksQueryHandler handler)
-        {
-            var result = await handler.Handle(query, CancellationToken.None);
+        private GetTasksQueryHandler _handler;
+        private GetTasksQuery _request;
+        private GetEmployerCohortsReadyForApprovalResponse _cohortsForReviewResponse;
+        private Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> _comtApiClient;
+        private Mock<ILogger<GetTasksQueryHandler>> _loggerMock;
 
-            result.Should().BeEquivalentTo(new GetTasksQueryResult());
+        [SetUp]
+        public void Setup()
+        {
+            _loggerMock = new Mock<ILogger<GetTasksQueryHandler>>();
+
+            var fixture = new Fixture();
+            _request = fixture.Create<GetTasksQuery>();
+
+            _cohortsForReviewResponse = fixture.Create<GetEmployerCohortsReadyForApprovalResponse>();
+
+            _comtApiClient = new Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>>();
+
+            _comtApiClient.Setup(x =>
+                    x.Get<GetEmployerCohortsReadyForApprovalResponse>(It.IsAny<GetEmployerCohortsReadyForApprovalRequest>()))
+                .ReturnsAsync(_cohortsForReviewResponse);
+
+            _handler = new GetTasksQueryHandler(_loggerMock.Object, _comtApiClient.Object);
+        }
+
+        [Test]
+        public async Task Then_Gets_Tasks_Returns_GetTasksQueryResult()
+        {
+            var result = await _handler.Handle(_request, CancellationToken.None);
+
+            result.Should().BeEquivalentTo(new GetTasksQueryResult
+            {
+                NumberOfCohortsForApproval = _cohortsForReviewResponse.EmployerCohortsReadyForApprovalResponse.Count()
+            });
         }
     }
 }
