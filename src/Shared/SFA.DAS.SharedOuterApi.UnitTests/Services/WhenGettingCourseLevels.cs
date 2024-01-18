25b1b4c4b8112042c56d3ea8e07bd3188ba98630
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2024-01-18 16:57:58 +0000

    Build failure fixes

--- src/Shared/SFA.DAS.SharedOuterApi.UnitTests/Services/WhenGettingCourseLevels.cs
+++ src/Shared/SFA.DAS.SharedOuterApi.UnitTests/Services/WhenGettingCourseLevels.cs
@@ -16,33 +16,33 @@ namespace SFA.DAS.SharedOuterApi.UnitTests.Services
     {
         [Test, MoqAutoData]
         public async Task Then_The_Api_Is_Called_With_The_Request_And_Levels_Returned_And_Added_To_Cache(
-            GetLevelsListResponse apiResponse,
+            GetCourseLevelsListResponse apiResponse,
             [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> apiClient,
             [Frozen] Mock<ICacheStorageService> cacheStorageService,
             CourseService service)
         {
             //Arrange
-            cacheStorageService.Setup(x => x.RetrieveFromCache<GetLevelsListResponse>(nameof(GetLevelsListResponse)))
-                .ReturnsAsync((GetLevelsListResponse)default);
-            apiClient.Setup(x => x.Get<GetLevelsListResponse>(It.IsAny<GetLevelsListRequest>())).ReturnsAsync(apiResponse);
+            cacheStorageService.Setup(x => x.RetrieveFromCache<GetCourseLevelsListResponse>(nameof(GetCourseLevelsListResponse)))
+                .ReturnsAsync((GetCourseLevelsListResponse)default);
+            apiClient.Setup(x => x.Get<GetCourseLevelsListResponse>(It.IsAny<GetCourseLevelsListRequest>())).ReturnsAsync(apiResponse);
 
             //Act
             var actual = await service.GetLevels();
 
             //Assert
             actual.Should().BeEquivalentTo(apiResponse);
-            cacheStorageService.Verify(x=>x.SaveToCache(nameof(GetLevelsListResponse), apiResponse, 23));
+            cacheStorageService.Verify(x=>x.SaveToCache(nameof(GetCourseLevelsListResponse), apiResponse, 23));
         }
 
         [Test, MoqAutoData]
         public async Task Then_If_The_Levels_Are_In_The_Cache_The_Api_Is_Not_Called(
-            GetLevelsListResponse apiResponse,
+            GetCourseLevelsListResponse apiResponse,
             [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> apiClient,
             [Frozen] Mock<ICacheStorageService> cacheStorageService,
             CourseService service)
         {
             //Arrange
-            cacheStorageService.Setup(x => x.RetrieveFromCache<GetLevelsListResponse>(nameof(GetLevelsListResponse)))
+            cacheStorageService.Setup(x => x.RetrieveFromCache<GetCourseLevelsListResponse>(nameof(GetCourseLevelsListResponse)))
                 .ReturnsAsync(apiResponse);
         
             //Act
@@ -50,7 +50,7 @@ namespace SFA.DAS.SharedOuterApi.UnitTests.Services
 
             //Assert
             actual.Should().BeEquivalentTo(apiResponse);
-            apiClient.Verify(x => x.Get<GetLevelsListResponse>(It.IsAny<GetLevelsListRequest>()), Times.Never);
+            apiClient.Verify(x => x.Get<GetCourseLevelsListResponse>(It.IsAny<GetCourseLevelsListRequest>()), Times.Never);
         }
     }
 }
