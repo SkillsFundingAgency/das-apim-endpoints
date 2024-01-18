10db7390f8fd50749e9f65c385ab9ae3b93b7f06
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2024-01-18 13:59:43 +0000

    ongoing with the changes

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/InnerApi/CandidateApi/Requests/UpdateSectionApiRequest.cs
@@ -0,0 +1,22 @@
+﻿using SFA.DAS.FindAnApprenticeship.Models;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests
+{
+    public class UpdateSectionApiRequest : IPutApiRequest
+    {
+        private readonly string _vacancyReference;
+
+        public UpdateSectionApiRequest(string vacancyReference, UpdateSectionRequest sectionRequest)
+        {
+            _vacancyReference = vacancyReference;
+            Data = new
+            {
+                sectionRequest = sectionRequest
+            };
+        }
+
+        public string PutUrl => $"/api/candidates/application/{_vacancyReference}/workHistory";
+        public object Data { get; set; }
+    }
+}
---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Models/UpdateSectionRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.FindAnApprenticeship.Models
+{
+    public class UpdateSectionRequest
+    {
+        public string Email { get; set; }
+        public SectionStatus Status { get; set; }
+    }
+}
