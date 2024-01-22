61493648ca85e1fc582226b8164f7d050c42effb
tom-gough <tom.gough.tgsd@outlook.com>
2024-01-22 15:31:03 +0000

    FLP-567 added passing through of auth header to inner api calls

--- src/Shared/SFA.DAS.SharedOuterApi/Infrastructure/InternalApiClient.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/Infrastructure/InternalApiClient.cs
@@ -1,7 +1,9 @@
-﻿using SFA.DAS.SharedOuterApi.Interfaces;
+﻿using System.Linq;
+using SFA.DAS.SharedOuterApi.Interfaces;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using SFA.DAS.Api.Common.Interfaces;
 
 namespace SFA.DAS.SharedOuterApi.Infrastructure
@@ -9,13 +11,16 @@ namespace SFA.DAS.SharedOuterApi.Infrastructure
     public class InternalApiClient<T> : ApiClient<T>, IInternalApiClient<T> where T : IInternalApiConfiguration
     {
         private readonly IAzureClientCredentialHelper _azureClientCredentialHelper;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public InternalApiClient(
             IHttpClientFactory httpClientFactory,
             T apiConfiguration,
-            IAzureClientCredentialHelper azureClientCredentialHelper) : base(httpClientFactory, apiConfiguration)
+            IAzureClientCredentialHelper azureClientCredentialHelper,
+            IHttpContextAccessor httpContextAccessor) : base(httpClientFactory, apiConfiguration)
         {
             _azureClientCredentialHelper = azureClientCredentialHelper;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         protected override async Task AddAuthenticationHeader(HttpRequestMessage httpRequestMessage)
@@ -24,6 +29,12 @@ namespace SFA.DAS.SharedOuterApi.Infrastructure
             {
                 var accessToken = await _azureClientCredentialHelper.GetAccessTokenAsync(Configuration.Identifier);
                 httpRequestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            }
+
+            var currentAuthHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"].FirstOrDefault();
+            if (currentAuthHeader != null)
+            {
+                httpRequestMessage.Headers.Add("Authorization", currentAuthHeader);
             }
         }
     }
