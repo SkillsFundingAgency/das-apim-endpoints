c71f8035dc67ce05075853bb2be2b08d63cf094a
ogrwill <will.ogrady@digital.education.gov.uk>
2024-01-24 12:15:35 +0000

    getting pending transfer requests to review

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts/Application/Queries/GetTasks/GetTasksQueryHandler.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts/Application/Queries/GetTasks/GetTasksQueryHandler.cs
@@ -2,15 +2,22 @@
 using System.Threading.Tasks;
 using System.Threading;
 using Microsoft.Extensions.Logging;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Commitments;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Commitments;
+using System.Linq;
 
 namespace SFA.DAS.EmployerAccounts.Application.Queries.GetTasks
 {
     public class GetTasksQueryHandler : IRequestHandler<GetTasksQuery, GetTasksQueryResult>
     {
         private readonly ILogger<GetTasksQueryHandler> _logger;
+        private readonly ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> _commitmentsV2ApiClient;
 
-        public GetTasksQueryHandler(ILogger<GetTasksQueryHandler> logger)
+        public GetTasksQueryHandler(ILogger<GetTasksQueryHandler> logger, ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> commitmentsV2ApiClient)
         {
+            _commitmentsV2ApiClient = commitmentsV2ApiClient;
             _logger = logger;
         }
 
@@ -18,8 +25,16 @@ namespace SFA.DAS.EmployerAccounts.Application.Queries.GetTasks
         {
             _logger.LogInformation($"Getting Tasks for account {request.AccountId}");
 
+            var transferRequestsTask = _commitmentsV2ApiClient.Get<GetTransferRequestSummaryResponse>(new GetTransferRequestsRequest(request.AccountId));
+
+            await Task.WhenAll(transferRequestsTask);
+            var transferRequests = await transferRequestsTask;
+
+            var pendingTransfertransferRequestsRequestsToReview = transferRequests?.TransferRequestSummaryResponse?.Where(x => x.Status == TransferApprovalStatus.Pending);
+
             return new GetTasksQueryResult()
             {
+                NumberOfTransferRequestToReview = pendingTransfertransferRequestsRequestsToReview?.Count() ?? 0
             };
         }
     }
---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Commitments/GetTransferRequestSummaryResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Commitments
+{
+    public class GetTransferRequestSummaryResponse
+    {
+        public IEnumerable<TransferRequestSummaryResponse> TransferRequestSummaryResponse { get; set; }
+    }
+
+    public class TransferRequestSummaryResponse
+    {
+        public string HashedTransferRequestId { get; set; }
+        public string HashedReceivingEmployerAccountId { get; set; }
+        public string CohortReference { get; set; }
+        public string HashedSendingEmployerAccountId { get; set; }
+        public long CommitmentId { get; set; }
+        public decimal TransferCost { get; set; }
+        public TransferApprovalStatus Status { get; set; }
+        public string ApprovedOrRejectedByUserName { get; set; }
+        public string ApprovedOrRejectedByUserEmail { get; set; }
+        public DateTime? ApprovedOrRejectedOn { get; set; }
+        public TransferType TransferType { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int FundingCap { get; set; }
+    }
+    public enum TransferApprovalStatus : byte
+    {
+        Pending = 0,
+        Approved = 1,
+        Rejected = 2
+    }
+
+    public enum TransferType
+    {
+        AsSender,
+        AsReceiver
+    }
+}
