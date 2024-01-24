7ee0795b0879f536ddacea5b6908588597de0683
ogrwill <will.ogrady@digital.education.gov.uk>
2024-01-24 13:57:24 +0000

    unit tests

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetTasks/WhenCallingHandler.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetTasks/WhenCallingHandler.cs
@@ -1,8 +1,15 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using AutoFixture.NUnit3;
 using FluentAssertions;
+using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerAccounts.Application.Queries.GetTasks;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Commitments;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Commitments;
+using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.GetTasks
@@ -12,12 +19,64 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.GetTasks
     {
         [Test, MoqAutoData]
         public async Task Then_Gets_Tasks_Returns_GetTasksQueryResult(
-            GetTasksQuery query,
-            GetTasksQueryHandler handler)
+          [Frozen] Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> mockCommitmentsApi,
+          GetTransferRequestSummaryResponse transferRequestResponse,
+          GetTasksQuery request,
+          GetTasksQueryHandler handler)
         {
-            var result = await handler.Handle(query, CancellationToken.None);
+            // Arrange
+            foreach (var tr in transferRequestResponse.TransferRequestSummaryResponse)
+            {
+                tr.Status = TransferApprovalStatus.Pending;
+            }
 
-            result.Should().BeEquivalentTo(new GetTasksQueryResult());
+            mockCommitmentsApi
+                .Setup(m => m.Get<GetTransferRequestSummaryResponse>(It.Is<GetTransferRequestsRequest>(r => r.AccountId == request.AccountId)))
+                .ReturnsAsync(transferRequestResponse);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            result.NumberOfTransferRequestToReview.Should().Be(3);
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_Only_Returns_Pending_TransferRequests(
+        [Frozen] Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> mockCommitmentsApi,
+        GetTransferRequestSummaryResponse transferRequestResponse,
+        GetTasksQuery request,
+        GetTasksQueryHandler handler)
+        {
+            transferRequestResponse.TransferRequestSummaryResponse.Select((cohort, index) =>
+            {
+                cohort.Status = index == 0 ? TransferApprovalStatus.Approved : TransferApprovalStatus.Pending;
+                return cohort;
+            }).ToArray();
+
+            mockCommitmentsApi
+                .Setup(m => m.Get<GetTransferRequestSummaryResponse>(It.Is<GetTransferRequestsRequest>(r => r.AccountId == request.AccountId)))
+                .ReturnsAsync(transferRequestResponse);
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+            result.NumberOfTransferRequestToReview.Should().Be(2);
+
+        }
+
+        [Test, MoqAutoData]
+        public async Task When_TransferRequests_Are_Null_Return_Zero(
+        [Frozen] Mock<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>> mockCommitmentsApi,
+        GetTasksQuery request,
+        GetTasksQueryHandler handler)
+        {
+            mockCommitmentsApi
+                .Setup(m => m.Get<GetTransferRequestSummaryResponse>(It.Is<GetTransferRequestsRequest>(r => r.AccountId == request.AccountId)))
+                .ReturnsAsync(new GetTransferRequestSummaryResponse());
+
+            // Act
+            var result = await handler.Handle(request, CancellationToken.None);
+
+            result.NumberOfTransferRequestToReview.Should().Be(0);
         }
     }
 }
---
+++ src/Shared/SFA.DAS.SharedOuterApi.UnitTests/InnerApi/Requests/WhenBuildingGetTransferRequestsRequest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Commitments;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.SharedOuterApi.UnitTests.InnerApi.Requests
+{
+    public class WhenBuildingGetTransferRequestsRequest
+    {
+        [Test, MoqAutoData]
+        public void Then_The_GetUrl_Is_Correctly_Built(
+          long accountId
+          )
+        {
+            var actual = new GetTransferRequestsRequest(accountId);
+            var expected = $"api/accounts/{accountId}/transfers";
+
+            actual.GetUrl.Should().Be(expected);
+        }
+    }
+}
