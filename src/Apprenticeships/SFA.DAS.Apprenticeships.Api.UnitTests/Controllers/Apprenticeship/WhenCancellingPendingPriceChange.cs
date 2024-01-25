0107d3ba0a7714acad1e318b8bd9f5fa4b818e34
Michael Young <mick.young1981@googlemail.com>
2024-01-25 19:07:27 +0000

    Merge fixes

--- src/Apprenticeships/SFA.DAS.Apprenticeships.Api.UnitTests/Controllers/Apprenticeship/WhenCancellingPendingPriceChange.cs
+++ src/Apprenticeships/SFA.DAS.Apprenticeships.Api.UnitTests/Controllers/Apprenticeship/WhenCancellingPendingPriceChange.cs
@@ -8,6 +8,8 @@ using SFA.DAS.SharedOuterApi.Interfaces;
 using System;
 using System.Threading.Tasks;
 using AutoFixture;
+using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace SFA.DAS.Apprenticeships.Api.UnitTests.Controllers.Apprenticeship
 {
@@ -23,7 +25,7 @@ namespace SFA.DAS.Apprenticeships.Api.UnitTests.Controllers.Apprenticeship
             _fixture = new Fixture();
 
 		    _mockApprenticeshipsApiClient = new Mock<IApprenticeshipsApiClient<ApprenticeshipsApiConfiguration>>();
-            _sut = new ApprenticeshipController(_mockApprenticeshipsApiClient.Object, Mock.Of<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>>());
+            _sut = new ApprenticeshipController(Mock.Of<ILogger<ApprenticeshipController>>(), _mockApprenticeshipsApiClient.Object, Mock.Of<IMediator>());
 	    }
 
         [Test]
