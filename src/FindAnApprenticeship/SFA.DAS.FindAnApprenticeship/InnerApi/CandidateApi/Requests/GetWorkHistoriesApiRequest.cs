a928d105eedd904b1fec356581155fe881133aa7
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2024-01-26 17:23:52 +0000

    APIM Get work Histories with unit testing ready

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/InnerApi/CandidateApi/Requests/GetWorkHistoriesApiRequest.cs
@@ -0,0 +1,20 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+
+namespace SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests
+{
+    public class GetWorkHistoriesApiRequest : IGetApiRequest
+    {
+        private readonly Guid _applicationId;
+        private readonly Guid _candidateId;
+
+        public GetWorkHistoriesApiRequest(Guid applicationId, Guid candidateId)
+        {
+            _applicationId = applicationId;
+            _candidateId = candidateId;
+        }
+
+        public string GetUrl => $"candidates/{_candidateId}/applications/{_applicationId}/work-history";
+        public object Data { get; set; }        
+    }
+}
