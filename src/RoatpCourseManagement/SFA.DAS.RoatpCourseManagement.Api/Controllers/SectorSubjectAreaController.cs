cec7074bff49826e1ac6c2cbd7dda3af35825e04
Deven Shah <devenshah1974@gmail.com>
2024-01-31 15:56:39 +0000

    CSP-1258 Added pass through endpoint for SSA1

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/SectorSubjectAreaController.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.RoatpCourseManagement.Application.SectorSubjectArea.Queries.GetAllSectorSubjectAreaTier1;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers;
+
+[ApiController]
+public class SectorSubjectAreaController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SectorSubjectAreaController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [Route("lookups/SectorSubjectAreaTier1")]
+    [HttpGet]
+    public async Task<IActionResult> GetAllSectorSubjectAreaTier1()
+    {
+        var result = await _mediator.Send(new GetAllSectorSubjectAreaTier1Query());
+
+        if (result.StatusCode != HttpStatusCode.OK)
+        {
+            return StatusCode((int)result.StatusCode, result.ErrorContent);
+        }
+
+        return Ok(result.Body);
+    }
+}
