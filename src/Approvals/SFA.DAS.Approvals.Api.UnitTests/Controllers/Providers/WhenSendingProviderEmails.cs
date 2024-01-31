fc30be770490187a7b84243990161b9c783d94e6
Paul Graham <paul.graham@coprime.co.uk>
2024-01-31 15:55:59 +0000

    added test for exception

--- src/Approvals/SFA.DAS.Approvals.Api.UnitTests/Controllers/Providers/WhenSendingProviderEmails.cs
+++ src/Approvals/SFA.DAS.Approvals.Api.UnitTests/Controllers/Providers/WhenSendingProviderEmails.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
@@ -32,8 +33,26 @@ namespace SFA.DAS.Approvals.Api.UnitTests.Controllers.Providers
 
             Assert.IsNotNull(controllerResult);
             controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
-
             mockMediator.Verify(x=>x.Send(It.Is<ProviderEmailCommand>(p=>p.ProviderId == ukprn), It.IsAny<CancellationToken>()));
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_Errors_When_Sending_Provider_Emails(
+            ProviderEmailRequest request,
+            long ukprn,
+            [Frozen] Mock<IMediator> mockMediator,
+            [Greedy] ProvidersController controller)
+        {
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.IsAny<ProviderEmailCommand>(),
+                    It.IsAny<CancellationToken>())).Throws<Exception>();
+
+            var controllerResult = await controller.EmailUsers(ukprn, request) as StatusCodeResult;
+
+            Assert.IsNotNull(controllerResult);
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.BadRequest);
+            mockMediator.Verify(x => x.Send(It.Is<ProviderEmailCommand>(p => p.ProviderId == ukprn), It.IsAny<CancellationToken>()));
         }
     }
 }
