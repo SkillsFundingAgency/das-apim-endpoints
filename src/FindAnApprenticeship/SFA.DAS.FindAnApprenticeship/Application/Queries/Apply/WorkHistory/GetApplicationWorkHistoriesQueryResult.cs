7bfcc81ab82bda5efeead3af9b7f9ef3b1eb676b
chrisfoster76 <chrisfoster186@googlemail.com>
2024-02-01 13:37:36 +0000

    Filter work history items to jobs only Move endpoint

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Apply/WorkHistory/GetApplicationWorkHistoriesQueryResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.WorkHistory
-{
-    public class GetApplicationWorkHistoriesQueryResult
-    {
-        public List<Models.WorkHistory> WorkHistories { get; set; } = [];        
-    }
-}
--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Apply/WorkHistory/GetJobsQuery.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Apply/WorkHistory/GetJobsQuery.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.WorkHistory
 {
-    public class GetApplicationWorkHistoriesQuery : IRequest<GetApplicationWorkHistoriesQueryResult>
+    public class GetJobsQuery : IRequest<GetJobsQueryResult>
     {
         public Guid CandidateId { get; set; }
         public Guid ApplicationId { get; set; }
