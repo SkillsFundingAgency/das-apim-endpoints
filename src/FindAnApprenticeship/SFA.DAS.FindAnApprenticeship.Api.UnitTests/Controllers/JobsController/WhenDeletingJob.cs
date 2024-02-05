4036dfc885ae70d813dd3477dd141663cc7fca5b
A-J-Wright <alissa.wright@education.gov.uk>
2024-02-05 13:14:40 +0000

    Tests

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api.UnitTests/Controllers/JobsController/WhenDeletingJob.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindAnApprenticeship.Application.Commands.Apply.DeleteJob;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindAnApprenticeship.Api.UnitTests.Controllers.JobsController
+{
+    [TestFixture]
+    public class WhenDeletingWorkHistory
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Command_Response_Is_Returned(
+            Guid candidateId,
+            Guid applicationId,
+            Guid workHistoryId,
+            [Frozen] Mock<IMediator> mediator,
+            [Greedy] Api.Controllers.JobsController controller)
+        {
+            mediator.Setup(x => x.Send(It.Is<DeleteJobCommand>(c =>
+                    c.CandidateId == candidateId
+                    && c.ApplicationId == applicationId
+                    && c.JobId == workHistoryId),
+                CancellationToken.None));
+
+            var actual = await controller.DeleteWorkHistory(candidateId, applicationId, workHistoryId);
+
+            actual.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Test, MoqAutoData]
+        public async Task Then_If_An_Exception_Is_Thrown_Then_Internal_Server_Error_Response_Returned(
+            Guid candidateId,
+            Guid applicationId,
+            Guid workHistoryId,
+            [Frozen] Mock<IMediator> mediator,
+            [Greedy] Api.Controllers.JobsController controller)
+        {
+            mediator.Setup(x => x.Send(It.IsAny<DeleteJobCommand>(),
+                    CancellationToken.None))
+                .ThrowsAsync(new Exception());
+
+            var actual = await controller.DeleteWorkHistory(candidateId, applicationId, workHistoryId) as StatusCodeResult;
+
+            Assert.IsNotNull(actual);
+            actual.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Application/Commands/Apply/WhenHandlingDeleteCommand.cs
@@ -0,0 +1,35 @@
+﻿using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindAnApprenticeship.Application.Commands.Apply.DeleteJob;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
+using SFA.DAS.SharedOuterApi.Services;
+
+namespace SFA.DAS.FindAnApprenticeship.UnitTests.Application.Commands.Apply
+{
+    public class WhenHandlingDeleteCommand
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_Job_Is_Deleted(
+            DeleteJobCommand command,
+            [Frozen] Mock<ICandidateApiClient<CandidateApiConfiguration>> candidateApiClient,
+            DeleteJobCommandHandler handler)
+        {
+            var expectedRequest = new DeleteJobRequest(command.ApplicationId, command.CandidateId, command.JobId);
+
+            candidateApiClient.Setup(client => client.Delete(It.Is<DeleteJobRequest>(r => r.DeleteUrl == expectedRequest.DeleteUrl)));
+
+            await handler.Handle(command, CancellationToken.None);
+
+            candidateApiClient.Verify(x => x.Delete(It.IsAny<DeleteJobRequest>()), Times.Once);
+        }
+    }
+}
