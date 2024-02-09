d5e04de98a41348a561d9ac70049863aef3918f7
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-02-09 10:00:12 +0000

    FAI-1092 add GetTrainingCourses endpoint

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/Controllers/TrainingCoursesController.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/Controllers/TrainingCoursesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.FindAnApprenticeship.Api.Models.Applications;
 using SFA.DAS.FindAnApprenticeship.Application.Commands.Apply.CreateTrainingCourse;
+using SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.TrainingCourses;
 
 namespace SFA.DAS.FindAnApprenticeship.Api.Controllers;
 
@@ -47,6 +48,25 @@ public class TrainingCoursesController : Controller
         catch (Exception ex)
         {
             _logger.LogError(ex, $"Error posting training course for application {applicationId}", applicationId);
+            return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+        }
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetTrainingCourses([FromRoute] Guid applicationId, [FromQuery] Guid candidateId)
+    {
+        try
+        {
+            var result = await _mediator.Send(new GetTrainingCoursesQuery
+            {
+                CandidateId = candidateId,
+                ApplicationId = applicationId
+            });
+            return Ok((GetTrainingCoursesApiResponse)result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Get Training Courses : An error occurred");
             return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
         }
     }
