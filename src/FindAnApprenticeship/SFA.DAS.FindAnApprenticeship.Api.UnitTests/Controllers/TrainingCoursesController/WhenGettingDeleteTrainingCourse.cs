95b8d39a49e3e0425efb765125c2c8edaf1159e3
A-J-Wright <alissa.wright@education.gov.uk>
2024-02-15 15:08:40 +0000

    tests

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api.UnitTests/Controllers/TrainingCoursesController/WhenGettingDeleteTrainingCourse.cs
@@ -0,0 +1,44 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions.Execution;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Testing.AutoFixture;
+using System.Threading.Tasks;
+using System;
+using SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.TrainingCourse;
+using System.Threading;
+using FluentAssertions;
+using SFA.DAS.FindAnApprenticeship.Api.Models.Applications;
+
+namespace SFA.DAS.FindAnApprenticeship.Api.UnitTests.Controllers.TrainingCoursesController;
+public class WhenGettingDeleteTrainingCourse
+{
+    [Test, MoqAutoData]
+    public async Task Then_The_Query_Response_Is_Returned(
+           Guid candidateId,
+           Guid applicationId,
+           Guid trainingCourseId,
+           GetDeleteTrainingCourseQueryResult queryResult,
+           [Frozen] Mock<IMediator> mediator,
+           [Greedy] Api.Controllers.TrainingCoursesController controller)
+    {
+        mediator.Setup(x => x.Send(It.Is<GetDeleteTrainingCourseQuery>(q =>
+                    q.CandidateId == candidateId
+                    && q.ApplicationId == applicationId
+                    && q.TrainingCourseId == trainingCourseId),
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(queryResult);
+
+        var actual = await controller.GetDeleteTrainingCourse(applicationId, trainingCourseId, candidateId);
+
+        using (new AssertionScope())
+        {
+            actual.Should().BeOfType<OkObjectResult>();
+            var actualObject = ((OkObjectResult)actual).Value as GetDeleteTrainingCourseApiResponse;
+            actualObject.Should().NotBeNull();
+            actualObject.Should().BeEquivalentTo((GetDeleteTrainingCourseApiResponse)queryResult);
+        }
+    }
+}
---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Application/Queries/Apply/WhenHandlingGetDeleteTrainingCourseQuery .cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.TrainingCourse;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindAnApprenticeship.UnitTests.Application.Queries.Apply;
+public class WhenHandlingGetDeleteTrainingCourseQuery
+{
+    [Test, MoqAutoData]
+    public async Task Then_The_QueryResult_Is_Returned_As_Expected(
+        GetDeleteTrainingCourseQuery query,
+        GetDeleteTrainingCourseResponse trainingCourseApiResponse,
+        [Frozen] Mock<ICandidateApiClient<CandidateApiConfiguration>> candidateApiClient,
+        GetDeleteTrainingCourseQueryHandler handler)
+    {
+        var expectedGetTrainingCourseRequest = new GetDeleteTrainingCourseRequest(query.ApplicationId, query.CandidateId, query.TrainingCourseId);
+        candidateApiClient
+            .Setup(client => client.Get<GetDeleteTrainingCourseResponse>(
+                It.Is<GetDeleteTrainingCourseRequest>(r => r.GetUrl == expectedGetTrainingCourseRequest.GetUrl)))
+            .ReturnsAsync(trainingCourseApiResponse);
+
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        result.Should().BeEquivalentTo((GetDeleteTrainingCourseQueryResult)trainingCourseApiResponse);
+    }
+}
