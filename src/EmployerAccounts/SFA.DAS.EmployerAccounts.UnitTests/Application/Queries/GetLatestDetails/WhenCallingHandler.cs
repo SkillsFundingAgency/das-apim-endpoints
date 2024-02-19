847627ffa631a3eb8ddc54c50d74498f2fdb4048
ogrwill <will.ogrady@digital.education.gov.uk>
2024-02-19 11:24:22 +0000

    unit tests

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetLatestDetails/WhenCallingHandler.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetLatestDetails/WhenCallingHandler.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Net;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerAccounts.Application.Queries.GetLatestDetails;
-using SFA.DAS.EmployerAccounts.Exceptions;
-using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.InnerApi.Requests.ReferenceData;
-using SFA.DAS.SharedOuterApi.InnerApi.Responses.ReferenceData;
-using SFA.DAS.SharedOuterApi.Interfaces;
-using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.EmployerAccounts.Strategies;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.GetLatestDetails
@@ -22,56 +15,19 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.GetLatestDetail
         [Test, MoqAutoData]
         public async Task Then_GetLatestDetails_from_Reference_Api(
           GetLatestDetailsQuery query,
-          ApiResponse<Organisation> apiResponse,
-          [Frozen] Mock<IReferenceDataApiClient<ReferenceDataApiConfiguration>> mockApiClient,
+          GetLatestDetailsResult expectedResult,
+          [Frozen] Mock<IOrganisationApiStrategyFactory> factory,
+          Mock<IOrganisationApiStrategy> strategy,
           GetLatestDetailsQueryHandler handler)
         {
-            mockApiClient
-                .Setup(client => client.GetWithResponseCode<Organisation>(It.IsAny<GetLatestDetailsRequest>()))
-                .ReturnsAsync(apiResponse);
+            factory.Setup(x => x.CreateStrategy(query.OrganisationType)).Returns(strategy.Object);
+
+            strategy.Setup(x => x.GetOrganisationDetails(query.Identifier, query.OrganisationType)).ReturnsAsync(expectedResult);
 
             var result = await handler.Handle(query, CancellationToken.None);
 
             result.Should().NotBeNull();
-            result.OrganisationDetail.Should().BeEquivalentTo(apiResponse.Body);
-        }
-
-        [Test, MoqAutoData]
-        public void Then_Throw_OrganisationNotFoundException_When_NotFound_Status_Code_Returned(
-        string errorMessage,
-        GetLatestDetailsQuery query,
-        [Frozen] Mock<IReferenceDataApiClient<ReferenceDataApiConfiguration>> mockApiClient,
-        GetLatestDetailsQueryHandler handler)
-        {
-            var notFoundResponse = new ApiResponse<Organisation>(null, HttpStatusCode.NotFound, errorMessage);
-
-            mockApiClient
-                .Setup(client => client.GetWithResponseCode<Organisation>(It.IsAny<GetLatestDetailsRequest>()))
-                .ReturnsAsync(notFoundResponse);
-
-            Func<Task> action = async () => await handler.Handle(query, CancellationToken.None);
-
-            action.Should().Throw<OrganisationNotFoundException>()
-                .WithMessage($"Did not find an organisation type Company with identifier {query.Identifier}");
-        }
-
-        [Test, MoqAutoData]
-        public void Then_ThrowInvalidGetOrganisationRequest_When_BadRequest_Status_Code_Returned(
-        string errorMessage,
-        GetLatestDetailsQuery query,
-        [Frozen] Mock<IReferenceDataApiClient<ReferenceDataApiConfiguration>> mockApiClient,
-        GetLatestDetailsQueryHandler handler)
-        {
-            var badRequestResponse = new ApiResponse<Organisation>(null, HttpStatusCode.BadRequest, errorMessage);
-
-            mockApiClient
-                .Setup(client => client.GetWithResponseCode<Organisation>(It.IsAny<GetLatestDetailsRequest>()))
-                .ReturnsAsync(badRequestResponse);
-
-            Func<Task> action = async () => await handler.Handle(query, CancellationToken.None);
-
-            action.Should().Throw<InvalidGetOrganisationException>()
-                .WithMessage(errorMessage);
+            result.Should().BeEquivalentTo(expectedResult);
         }
     }
 }
---
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Strategies/OrganisationApiStrategyFactoryTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.Strategies;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.ReferenceData;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerAccounts.UnitTests.Strategies
+{
+    public class OrganisationApiStrategyFactoryTests
+    {
+        [Test, MoqAutoData]
+        public void CreateStrategy_ShouldReturnReferenceDataApiStrategy_ForCompanyCharityPublicSector(
+        OrganisationApiStrategyFactory factory)
+        {
+            var organisationTypes = new[] { OrganisationType.Company, OrganisationType.Charity, OrganisationType.PublicSector };
+
+            foreach (var orgType in organisationTypes)
+            {
+                // Act
+                var strategy = factory.CreateStrategy(orgType);
+
+                // Assert
+                strategy.Should().BeOfType<ReferenceDataApiStrategy>();
+            }
+        }
+
+        [Test, MoqAutoData]
+        public void CreateStrategy_ShouldReturnEducationOrganisationApiStrategy_ForEducationOrganisation(
+        OrganisationApiStrategyFactory factory)
+        {
+            var orgType = OrganisationType.EducationOrganisation;
+
+            // Act
+            var strategy = factory.CreateStrategy(orgType);
+
+            // Assert
+            strategy.Should().BeOfType<EducationOrganisationApiStrategy>();
+
+        }
+
+        [Test, MoqAutoData]
+        public void CreateStrategy_ShouldThrowException_ForUnknownOrganisationType(
+            OrganisationApiStrategyFactory factory)
+        {
+            Action act = () => factory.CreateStrategy(0);
+
+            act.Should().Throw<InvalidOperationException>().WithMessage("No strategy found for OrganisationType: 0");
+        }
+    }
+}
