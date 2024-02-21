745cf72363f6139e982010505efecf24be56bb78
MarkFCain <mark.cain@digital.education.gov.uk>
2024-02-21 15:55:31 +0000

    CSP-1210 Employer Aan update to reduce shared apiclients

---
+++ src/EmployerAan/SFA.DAS.EmployerAan/Infrastructure/IApprenticeAccountsApiClient.cs
@@ -0,0 +1,10 @@
+﻿using RestEase;
+using SFA.DAS.EmployerAan.InnerApi.MyApprenticeships;
+
+namespace SFA.DAS.EmployerAan.Infrastructure;
+public interface IApprenticeAccountsApiClient : IHealthChecker
+{
+    [Get("apprentices/{apprenticeId}/MyApprenticeship")]
+    [AllowAnyStatusCode]
+    Task<Response<GetMyApprenticeshipResponse>> GetMyApprenticeship([Path] Guid apprenticeId, CancellationToken cancellationToken);
+}
