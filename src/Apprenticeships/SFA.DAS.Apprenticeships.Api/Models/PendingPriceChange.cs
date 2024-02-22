d78a731287634f179bc029bacc75e33c0509c78f
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-02-22 11:32:03 +0000

    FLP-618-Employer-View-PendingPriceChange

--- src/Apprenticeships/SFA.DAS.Apprenticeships.Api/Models/PendingPriceChange.cs
+++ src/Apprenticeships/SFA.DAS.Apprenticeships.Api/Models/PendingPriceChange.cs
@@ -11,5 +11,11 @@
 		public DateTime EffectiveFrom { get; set; }
 		public string Reason { get; set; }
         public long? Ukprn { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+		public Guid ApprenticeshipKey { get; set; }
+		public DateTime? ProviderApprovedDate { get; set; }
+		public DateTime? EmployerApprovedDate { get; set; }
+
 	}
 }
