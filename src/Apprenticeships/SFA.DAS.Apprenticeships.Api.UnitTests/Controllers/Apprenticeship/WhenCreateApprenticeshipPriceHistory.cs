d229dbaf2fd81f135c22f829b1a532b9122e9716
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2024-02-27 09:56:46 +0000

    Added unit tests for creating price history

--- src/Apprenticeships/SFA.DAS.Apprenticeships.Api.UnitTests/Controllers/Apprenticeship/WhenCreateApprenticeshipPriceHistory.cs
+++ src/Apprenticeships/SFA.DAS.Apprenticeships.Api.UnitTests/Controllers/Apprenticeship/WhenCreateApprenticeshipPriceHistory.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Net;
 using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
@@ -15,6 +18,13 @@ namespace SFA.DAS.Apprenticeships.Api.UnitTests.Controllers.Apprenticeship;
 
 public class WhenCreateApprenticeshipPriceHistory
 {
+    private readonly Fixture _fixture;
+
+    public WhenCreateApprenticeshipPriceHistory()
+    {
+		_fixture = new Fixture();
+	}
+
     [Test]
     public async Task ThenCreatesApprenticeshipPriceHistoryUsingApiClient()
     {
@@ -36,7 +46,7 @@ public class WhenCreateApprenticeshipPriceHistory
         apiClient.Setup(x => x.PostWithResponseCode<object>(It.IsAny<PostCreateApprenticeshipPriceChangeRequest>(), It.IsAny<bool>())).ReturnsAsync(new ApiResponse<object>("", HttpStatusCode.OK, ""));
 
         // Act
-        await sut.CreateApprenticeshipPriceChange(
+        var response = await sut.CreateApprenticeshipPriceChange(
             request.ApprenticeshipKey,
             new Models.CreateApprenticeshipPriceChangeRequest
             {
@@ -59,5 +69,24 @@ public class WhenCreateApprenticeshipPriceHistory
             ((CreateApprenticeshipPriceChangeRequest)r.Data).Reason == ((CreateApprenticeshipPriceChangeRequest)request.Data).Reason &&
             ((CreateApprenticeshipPriceChangeRequest)r.Data).EffectiveFromDate == ((CreateApprenticeshipPriceChangeRequest)request.Data).EffectiveFromDate &&
             r.ApprenticeshipKey == request.ApprenticeshipKey), It.IsAny<bool>()), Times.Once);
+        response.Should().BeOfType<OkResult>();
     }
+
+	[Test]
+	public async Task IfRequestFailsReturnsBadRequest()
+	{
+		var apiClient = new Mock<IApprenticeshipsApiClient<ApprenticeshipsApiConfiguration>>();
+		var sut = new ApprenticeshipController(Mock.Of<ILogger<ApprenticeshipController>>(), apiClient.Object, Mock.Of<ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration>>(), Mock.Of<IMediator>());
+
+		// Arrange
+		var apprenticeshipKey = Guid.NewGuid();
+		var request = _fixture.Create<Models.CreateApprenticeshipPriceChangeRequest>();
+		apiClient.Setup(x => x.PostWithResponseCode<object>(It.IsAny<PostCreateApprenticeshipPriceChangeRequest>(), It.IsAny<bool>())).ReturnsAsync(new ApiResponse<object>("", HttpStatusCode.NotFound, "Has Error"));
+
+		// Act
+		var response = await sut.CreateApprenticeshipPriceChange(apprenticeshipKey, request);
+
+		// Assert
+		response.Should().BeOfType<BadRequestResult>();
+	}
 }
