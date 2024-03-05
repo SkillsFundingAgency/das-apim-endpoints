5d2ca95722e2e1190930dc087fb8dd6bb563d7ea
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-05 15:30:48 +0000

    Implemented missing method body

--- src/Approvals/SFA.DAS.Approvals/Application/Authorization/Queries/GetCohortAccessQueryHandler.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/Authorization/Queries/GetCohortAccessQueryHandler.cs
@@ -1,13 +1,24 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Authorization;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Authorization;
+using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.Approvals.Application.Authorization.Queries;
 
-public class GetCohortAccessQueryHandler : IRequestHandler<GetCohortAccessQuery, bool>
+public class GetCohortAccessQueryHandler(ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> apiClient) : IRequestHandler<GetCohortAccessQuery, bool>
 {
-    public Task<bool> Handle(GetCohortAccessQuery request, CancellationToken cancellationToken)
+    public async Task<bool> Handle(GetCohortAccessQuery request, CancellationToken cancellationToken)
     {
-        throw new System.NotImplementedException();
+        var apiRequest = new GetCohortAccessRequest(request.Party, request.PartyId, request.CohortId);
+
+        var response = await apiClient.GetWithResponseCode<GetCohortAccessResponse>(apiRequest);
+
+        response.EnsureSuccessStatusCode();
+
+        return response.Body.HasCohortAccess;
     }
 }
