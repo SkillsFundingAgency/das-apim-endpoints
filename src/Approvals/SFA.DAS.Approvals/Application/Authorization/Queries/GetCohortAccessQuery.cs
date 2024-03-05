9c4dfe7555b32d87eab395fe0aa09d3970302c1d
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-05 15:31:33 +0000

    Tidy

--- src/Approvals/SFA.DAS.Approvals/Application/Authorization/Queries/GetCohortAccessQuery.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/Authorization/Queries/GetCohortAccessQuery.cs
@@ -3,7 +3,6 @@ using SFA.DAS.SharedOuterApi.Models;
 
 namespace SFA.DAS.Approvals.Application.Authorization.Queries;
 
-public record GetCohortAccessQuery(Party Party, long PartyId, long CohortId): IRequest<bool>
+public record GetCohortAccessQuery(Party Party, long PartyId, long CohortId) : IRequest<bool>
 {
-    
 }
