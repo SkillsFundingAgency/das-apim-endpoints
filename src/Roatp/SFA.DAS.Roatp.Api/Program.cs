891a6ffe4fdd8cbd962471588bbd8ab5a1fa0726
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-03-06 13:00:20 +0000

    Csp 1212 update roatp to net8 (#1362)
    * CSP-1212 update to Net8, remove shared apiclients, remove vulnerabilities
    
    * CSP-1212 updated readme

--- src/Roatp/SFA.DAS.Roatp.Api/Program.cs
+++ src/Roatp/SFA.DAS.Roatp.Api/Program.cs
@@ -2,23 +2,21 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using NLog.Web;
 
-namespace SFA.DAS.Roatp.Api
-{
-    public static class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace SFA.DAS.Roatp.Api;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder
-                        .UseStartup<Startup>()
-                        .UseNLog();
-                });
-            
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder
+                    .UseStartup<Startup>()
+                    .UseNLog();
+            });
 }
