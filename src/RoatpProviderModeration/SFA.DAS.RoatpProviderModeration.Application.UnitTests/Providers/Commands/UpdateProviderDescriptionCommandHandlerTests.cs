07fbe89cffd32dbf72393f34d7efc34fbe8a98ae
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-03-06 13:15:44 +0000

    CSP-1215 update roatp provider moderation to net 8 (#1365)

--- src/RoatpProviderModeration/SFA.DAS.RoatpProviderModeration.Application.UnitTests/Providers/Commands/UpdateProviderDescriptionCommandHandlerTests.cs
+++ src/RoatpProviderModeration/SFA.DAS.RoatpProviderModeration.Application.UnitTests/Providers/Commands/UpdateProviderDescriptionCommandHandlerTests.cs
@@ -1,48 +1,33 @@
-﻿using System.Net;
-using AutoFixture.NUnit3;
-using FluentAssertions;
+﻿using AutoFixture.NUnit3;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.RoatpProviderModeration.Application.Infrastructure;
 using SFA.DAS.RoatpProviderModeration.Application.InnerApi.Requests;
 using SFA.DAS.RoatpProviderModeration.Application.Provider.Commands.UpdateProviderDescription;
-using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.Infrastructure;
-using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
-using SFA.DAS.SharedOuterApi.Models;
 
-namespace SFA.DAS.RoatpProviderModeration.Application.UnitTests.Providers.Commands
+namespace SFA.DAS.RoatpProviderModeration.Application.UnitTests.Providers.Commands;
+
+[TestFixture]
+public class UpdateProviderDescriptionCommandHandlerTests
 {
-    [TestFixture]
-    public class UpdateProviderDescriptionCommandHandlerTests
+    [Test, MoqAutoData]
+    public async Task Handle_CallsApiClient(
+        [Frozen] Mock<IRoatpV2ApiClient> apiClientMock,
+        UpdateProviderDescriptionCommandHandler sut,
+        UpdateProviderDescriptionCommand command,
+        CancellationToken cancellationToken)
     {
-        [Test, MoqAutoData]
-        public async Task Handle_CallsApiClient(
-            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
-            UpdateProviderDescriptionCommandHandler sut,
-            UpdateProviderDescriptionCommand command,
-            CancellationToken cancellationToken)
-        {
-            apiClientMock.Setup(c => c.PatchWithResponseCode(It.IsAny<PatchProviderRequest>())).ReturnsAsync(new ApiResponse<string>("", HttpStatusCode.Accepted,null));
-            await sut.Handle(command, cancellationToken);
-            apiClientMock.Verify(a => a.PatchWithResponseCode(It.IsAny<PatchProviderRequest>()), Times.Once);
-        }
+        apiClientMock.Setup(c => c.UpdateProviderDescription(command.Ukprn, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<List<PatchOperation>>(), cancellationToken))
+           .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.Accepted, Version = new Version() });
 
-        [Test, MoqAutoData]
-        public void Handle_CallsApiClient_ReturnException(
-            [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
-            UpdateProviderDescriptionCommandHandler sut,
-            UpdateProviderDescriptionCommand command,
-            CancellationToken cancellationToken,
-            HttpRequestContentException expectedException)
-        {
-            apiClientMock.Setup(c => c.PatchWithResponseCode(It.IsAny<PatchProviderRequest>())).Throws(expectedException);
-
-            var actualException = Assert.ThrowsAsync<HttpRequestContentException>(() => sut.Handle(command, cancellationToken));
-
-            actualException.Should().Be(expectedException);
-        }
+        await sut.Handle(command, cancellationToken);
+        apiClientMock.Verify(a => a.UpdateProviderDescription(command.Ukprn, It.IsAny<string>(), It.IsAny<string>(), It.IsAny<List<PatchOperation>>(), cancellationToken), Times.Once);
     }
 }
