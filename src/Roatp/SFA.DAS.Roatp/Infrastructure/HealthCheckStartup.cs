fe049f91feb814fed06d3ad2d9b8a443a34d1332
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-03-08 13:11:33 +0000

    CSP-1292 Hotfix added back UseHealthChecks (#1372)
    * CSP-1292 Hotfix added back UseHealthChecks
    
    * CSP-1292 added namespace to new code
    
    * CSP-1292 fixed name of extension

---
+++ src/Roatp/SFA.DAS.Roatp/Infrastructure/HealthCheckStartup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+using SFA.DAS.Api.Common.Infrastructure;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.Roatp.Infrastructure;
+
+[ExcludeFromCodeCoverage]
+public static class UseHealthChecksExtension
+{
+    public static IApplicationBuilder UseHealthChecks(this IApplicationBuilder app)
+    {
+        app.UseHealthChecks("/health", new HealthCheckOptions
+        {
+            ResponseWriter = HealthCheckResponseWriter.WriteJsonResponse
+        });
+
+        app.UseHealthChecks("/ping", new HealthCheckOptions
+        {
+            Predicate = (_) => false,
+            ResponseWriter = (context, report) =>
+            {
+                context.Response.ContentType = "application/json";
+                return context.Response.WriteAsync("");
+            }
+        });
+
+        return app;
+    }
+}
