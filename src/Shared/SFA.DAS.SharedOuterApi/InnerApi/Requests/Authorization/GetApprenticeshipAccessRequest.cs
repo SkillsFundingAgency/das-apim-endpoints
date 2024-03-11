c9a9eaab9d8baae1f4e41c2a3f3474776a92e000
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-11 14:22:16 +0000

    Fixing GET requests URL

--- src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/Authorization/GetApprenticeshipAccessRequest.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/Authorization/GetApprenticeshipAccessRequest.cs
@@ -5,5 +5,5 @@ namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.Authorization;
 
 public record GetApprenticeshipAccessRequest(Party Party, long PartyId, long ApprenticeshipId) : IGetApiRequest
 {
-    public string GetUrl => "api/authorization/access-apprenticeship";
+    public string GetUrl => $"authorization/access-apprenticeship?Party={Party}&PartyId={PartyId}&ApprenticeshipId={ApprenticeshipId}";
 }
