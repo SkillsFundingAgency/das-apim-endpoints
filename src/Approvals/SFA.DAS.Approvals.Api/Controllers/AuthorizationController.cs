e95753e85b19183c68fefabbaf7f9aa3d47bf0f7
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-12 12:41:27 +0000

    Adding logging

--- src/Approvals/SFA.DAS.Approvals.Api/Controllers/AuthorizationController.cs
+++ src/Approvals/SFA.DAS.Approvals.Api/Controllers/AuthorizationController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.Approvals.Application.Authorization.Queries;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.Authorization;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.Commitments;
@@ -11,7 +12,7 @@ namespace SFA.DAS.Approvals.Api.Controllers;
 
 [ApiController]
 [Route("[controller]/")]
-public class AuthorizationController(ISender mediator) : Controller
+public class AuthorizationController(ISender mediator, ILogger<AuthorizationController> logger) : Controller
 {
     [HttpGet]
     [Route("{partyId}/can-access-cohort/{cohortId}")]
@@ -22,8 +23,9 @@ public class AuthorizationController(ISender mediator) : Controller
             var result = await mediator.Send(new GetCohortAccessQuery(party, partyId, cohortId));
             return Ok(new GetCohortAccessResponse { HasCohortAccess = result });
         }
-        catch (Exception)
+        catch (Exception exception)
         {
+            logger.LogError(exception, $"{nameof(CanAccessCohort)} threw an exception.");
             return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
         }
     }
@@ -37,8 +39,9 @@ public class AuthorizationController(ISender mediator) : Controller
             var result = await mediator.Send(new GetApprenticeshipAccessQuery(party, partyId, apprenticeshipId));
             return Ok(new GetApprenticeshipAccessResponse { HasApprenticeshipAccess = result });
         }
-        catch (Exception)
+        catch (Exception exception)
         {
+            logger.LogError(exception, $"{nameof(CanAccessApprenticeship)} threw an exception.");
             return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
         }
     }
