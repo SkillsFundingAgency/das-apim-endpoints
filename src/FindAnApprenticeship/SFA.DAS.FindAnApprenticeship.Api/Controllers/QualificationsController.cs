6dbd16f44e51fb140fbbcc165f9d2b5ce429d1cc
chrisfoster76 <chrisfoster186@googlemail.com>
2024-03-13 15:54:30 +0000

    Endpoint to set qualifications completion status, command, tests and all that

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/Controllers/QualificationsController.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/Controllers/QualificationsController.cs
@@ -5,6 +5,8 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.FindAnApprenticeship.Api.Models.Applications;
+using SFA.DAS.FindAnApprenticeship.Application.Commands.Apply.UpdateQualifications;
+using SFA.DAS.FindAnApprenticeship.Application.Commands.Apply.UpdateWhatInterestsYou;
 using SFA.DAS.FindAnApprenticeship.Application.Queries.Apply.Qualifications;
 
 namespace SFA.DAS.FindAnApprenticeship.Api.Controllers
@@ -34,5 +36,27 @@ namespace SFA.DAS.FindAnApprenticeship.Api.Controllers
                 return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
             }
         }
+
+        [HttpPost("")]
+        public async Task<IActionResult> PostQualifications([FromRoute] Guid applicationId, [FromBody] PostQualificationsApiRequest request)
+        {
+            try
+            {
+                await mediator.Send(new UpdateQualificationsCommand
+                {
+                    ApplicationId = applicationId,
+                    CandidateId = request.CandidateId,
+                    IsComplete = request.IsComplete
+                });
+
+                return Created();
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "PostQualifications : An error occurred");
+                return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
     }
 }
