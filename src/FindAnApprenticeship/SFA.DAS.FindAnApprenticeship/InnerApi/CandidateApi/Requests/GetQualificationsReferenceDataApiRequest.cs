c5912a7976393847cdcd18bad7e2a4f7d88beba6
chrisfoster76 <chrisfoster186@googlemail.com>
2024-03-18 09:51:25 +0000

    Adds qualifications request/response objects

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/InnerApi/CandidateApi/Requests/GetQualificationsReferenceDataApiRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests
+{
+    public class GetQualificationsReferenceDataApiRequest: IGetApiRequest
+    {
+        public string GetUrl => $"api/referencedata/qualifications";
+    }
+}
---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/InnerApi/CandidateApi/Responses/QualificationReferenceDataApiResponseItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses
+{
+    public class QualificationReferenceDataApiResponseItem
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; }
+        public short Order { get; set; }
+    }
+}
