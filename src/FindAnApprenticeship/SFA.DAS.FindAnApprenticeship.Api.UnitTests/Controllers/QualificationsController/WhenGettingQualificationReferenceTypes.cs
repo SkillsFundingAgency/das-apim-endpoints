08492d0e2417de3b24797d3d369568562d46de59
chrisfoster76 <chrisfoster186@googlemail.com>
2024-03-19 19:31:03 +0000

    Test fixes

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api.UnitTests/Controllers/QualificationsController/WhenGettingQualificationReferenceTypes.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api.UnitTests/Controllers/QualificationsController/WhenGettingQualificationReferenceTypes.cs
@@ -19,6 +19,7 @@ public class WhenGettingQualificationReferenceTypes
     [Test, MoqAutoData]
     public async Task Then_The_Request_Is_Handled_And_Data_Returned(
         Guid applicationId,
+        Guid candidateId,
         GetQualificationTypesQueryResult queryResult,
         [Frozen] Mock<IMediator> mediator,
         [Greedy]Api.Controllers.QualificationsController controller)
@@ -26,7 +27,7 @@ public class WhenGettingQualificationReferenceTypes
         mediator.Setup(x => x.Send(It.IsAny<GetQualificationTypesQuery>(), CancellationToken.None))
             .ReturnsAsync(queryResult);
         
-        var actual = await controller.GetAddSelectType(applicationId) as OkObjectResult;
+        var actual = await controller.GetAddSelectType(applicationId, candidateId) as OkObjectResult;
 
         actual.Should().NotBeNull();
         var actualValue = actual!.Value as GetQualificationReferenceTypesApiResponse;
@@ -36,14 +37,14 @@ public class WhenGettingQualificationReferenceTypes
     [Test, MoqAutoData]
     public async Task Then_If_There_Is_An_Exception_Then_Internal_Server_Error_Returned(
         Guid applicationId,
-        GetQualificationTypesQueryResult queryResult,
+        Guid candidateId,
         [Frozen] Mock<IMediator> mediator,
         [Greedy]Api.Controllers.QualificationsController controller)
     {
         mediator.Setup(x => x.Send(It.IsAny<GetQualificationTypesQuery>(), CancellationToken.None))
             .ThrowsAsync(new Exception());
         
-        var actual = await controller.GetAddSelectType(applicationId) as StatusCodeResult;
+        var actual = await controller.GetAddSelectType(applicationId, candidateId) as StatusCodeResult;
 
         actual.Should().NotBeNull();
         actual.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
