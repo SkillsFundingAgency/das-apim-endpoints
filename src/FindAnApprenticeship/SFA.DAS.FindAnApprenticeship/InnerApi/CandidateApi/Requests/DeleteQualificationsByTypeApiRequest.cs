ab5ca3435894bef8e40300639b1b7759c47b4fad
chrisfoster76 <chrisfoster186@googlemail.com>
2024-03-21 19:24:04 +0000

    Fixes url

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/InnerApi/CandidateApi/Requests/DeleteQualificationsByTypeApiRequest.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/InnerApi/CandidateApi/Requests/DeleteQualificationsByTypeApiRequest.cs
@@ -5,6 +5,6 @@ namespace SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests
 {
     public class DeleteQualificationsByTypeApiRequest(Guid applicationId, Guid candidateId, Guid qualificationReferenceId) : IDeleteApiRequest
     {
-        public string DeleteUrl => $"candidates/{candidateId}/applications/{applicationId}/qualifications?qualificationReferenceId={qualificationReferenceId}";
+        public string DeleteUrl => $"api/candidates/{candidateId}/applications/{applicationId}/qualifications?qualificationReferenceId={qualificationReferenceId}";
     }
 }
