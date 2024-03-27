99b196e2a3ce7eb9fe7022f1e83265d9e67d2b9e
Jon Harris <jon.harris@digital.education.gov.uk>
2024-03-27 11:31:28 +0000

    Adding ProviderRelationshipsApiConfiguration wireup

--- src/Reservations/SFA.DAS.Reservations.Api/AppStart/AddConfigurationOptionsExtension.cs
+++ src/Reservations/SFA.DAS.Reservations.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -4,25 +4,26 @@ using Microsoft.Extensions.Options;
 using SFA.DAS.Api.Common.Configuration;
 using SFA.DAS.SharedOuterApi.Configuration;
 
-namespace SFA.DAS.Reservations.Api.AppStart
+namespace SFA.DAS.Reservations.Api.AppStart;
+
+public static class AddConfigurationOptionsExtension
 {
-    public static class AddConfigurationOptionsExtension
+    public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
     {
-        public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
-        {
-            services.AddOptions();
-            services.Configure<CoursesApiConfiguration>(configuration.GetSection(nameof(CoursesApiConfiguration)));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<CoursesApiConfiguration>>().Value);
-            services.Configure<AccountsConfiguration>(configuration.GetSection("AccountsInnerApi"));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<AccountsConfiguration>>().Value);
-            services.Configure<AzureActiveDirectoryConfiguration>(configuration.GetSection("AzureAd"));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
-            services.Configure<EmployerProfilesApiConfiguration>(configuration.GetSection(nameof(EmployerProfilesApiConfiguration)));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<EmployerProfilesApiConfiguration>>().Value);
-            services.Configure<RoatpConfiguration>(configuration.GetSection(nameof(RoatpConfiguration)));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<RoatpConfiguration>>().Value);
-            services.Configure<RoatpV2ApiConfiguration>(configuration.GetSection("RoatpConfiguration"));
-            services.AddSingleton(cfg => cfg.GetService<IOptions<RoatpV2ApiConfiguration>>().Value);
-        }
+        services.AddOptions();
+        services.Configure<CoursesApiConfiguration>(configuration.GetSection(nameof(CoursesApiConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<CoursesApiConfiguration>>().Value);
+        services.Configure<AccountsConfiguration>(configuration.GetSection("AccountsInnerApi"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<AccountsConfiguration>>().Value);
+        services.Configure<AzureActiveDirectoryConfiguration>(configuration.GetSection("AzureAd"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
+        services.Configure<EmployerProfilesApiConfiguration>(configuration.GetSection(nameof(EmployerProfilesApiConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<EmployerProfilesApiConfiguration>>().Value);
+        services.Configure<RoatpConfiguration>(configuration.GetSection(nameof(RoatpConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<RoatpConfiguration>>().Value);
+        services.Configure<RoatpV2ApiConfiguration>(configuration.GetSection("RoatpConfiguration"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<RoatpV2ApiConfiguration>>().Value);
+        services.Configure<ProviderRelationshipsApiConfiguration>(configuration.GetSection(nameof(ProviderRelationshipsApiConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<ProviderRelationshipsApiConfiguration>>().Value);
     }
 }
