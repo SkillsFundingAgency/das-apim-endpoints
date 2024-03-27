5108c0a081d80092084f84e98eceaeee2f424fe7
Sarah Duncan <sarah.duncan@education.gov.uk>
2024-03-27 13:38:28 +0000

    FAI-1107 add phone number to putCandidate api response

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Candidate/PutCandidateCommandResult.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Candidate/PutCandidateCommandResult.cs
@@ -9,4 +9,5 @@ public class PutCandidateCommandResult
     public string Email { get; set; } = null!;
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
+    public string? PhoneNumber { get; set; }
 }
