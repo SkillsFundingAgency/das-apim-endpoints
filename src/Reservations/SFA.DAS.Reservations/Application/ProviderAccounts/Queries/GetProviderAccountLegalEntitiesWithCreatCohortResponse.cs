b9406195554efcd2505bdefc06d44e525a3e6eae
Jon Harris <jon.harris@digital.education.gov.uk>
2024-04-02 15:18:41 +0100

    Fixing response object

--- src/Reservations/SFA.DAS.Reservations/Application/ProviderAccounts/Queries/GetProviderAccountLegalEntitiesWithCreatCohortResponse.cs
+++ src/Reservations/SFA.DAS.Reservations/Application/ProviderAccounts/Queries/GetProviderAccountLegalEntitiesWithCreatCohortResponse.cs
@@ -1,9 +1,15 @@
 using System.Collections.Generic;
-using SFA.DAS.SharedOuterApi.InnerApi.Responses.ProviderRelationships;
+using System.Text.Json.Serialization;
 
 namespace SFA.DAS.Reservations.Application.ProviderAccounts.Queries;
 
 public record GetProviderAccountLegalEntitiesWithCreatCohortResponse
 {
     public List<GetProviderAccountLegalEntityWithCreatCohortItem> ProviderAccountLegalEntities { get ; set ; }
+}
+
+public class GetProviderAccountLegalEntityWithCreatCohortItem
+{
+    [JsonPropertyName("AccountId")]
+    public long AccountId { get; set; }
 }
