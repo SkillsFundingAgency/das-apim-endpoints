ae57ec4b9f0c6a878a1ab1d17ada9b1aa2b337dd
Chris Woodcock <chris@humesoftware.com>
2024-04-10 15:32:49 +0100

    Defer apprentice target update when data not available

---
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback/InnerApi/Requests/UpdateApprenticeFeedbackTargetDeferRequest.cs
@@ -0,0 +1,22 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+
+namespace SFA.DAS.ApprenticeFeedback.InnerApi.Requests
+{
+    public class UpdateApprenticeFeedbackTargetDeferRequest : IPostApiRequest<UpdateApprenticeFeedbackTargetDeferRequestData>
+    {
+        public string PostUrl => "api/apprenticefeedbacktarget/deferUpdate";
+
+        public UpdateApprenticeFeedbackTargetDeferRequestData Data { get; set; }
+
+        public UpdateApprenticeFeedbackTargetDeferRequest(UpdateApprenticeFeedbackTargetDeferRequestData data)
+        {
+            Data = data;
+        }
+    }
+
+    public class UpdateApprenticeFeedbackTargetDeferRequestData
+    {
+        public Guid ApprenticeFeedbackTargetId { get; internal set; }
+    }
+}
