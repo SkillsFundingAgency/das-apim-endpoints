6fd729f508f822476520cf7d4bf1f417f055c7b0
dinathain <55487881+dinathain@users.noreply.github.com>
2024-04-12 16:31:40 +0100

    Improve logging in ErrorController

--- src/Apprenticeships/SFA.DAS.Apprenticeships.Api/Controllers/ErrorController.cs
+++ src/Apprenticeships/SFA.DAS.Apprenticeships.Api/Controllers/ErrorController.cs
@@ -8,13 +8,25 @@ namespace SFA.DAS.Apprenticeships.Api.Controllers;
 [Route("[controller]")]
 public class ErrorController : ControllerBase
 {
+    private readonly ILogger<ErrorController> _logger;
+
+    public ErrorController(ILogger<ErrorController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet]
     [Route("")]
     public IActionResult Error()
     {
         if (HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error is ApiUnauthorizedException)
+        {
+            _logger.LogError("");
             return Unauthorized();
+        }
 
-        throw HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error ?? new Exception("An exception has occurred."); //this basic error message is very unlikely to be thrown as .Error should never be null
+        // This error is very unlikely to be thrown as .Error should never be null
+        _logger.LogError("An unexpected error has occurred. Error in the {p1} is null.", nameof(IExceptionHandlerFeature));
+        throw HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error ?? new Exception("An exception has occurred."); 
     }
 }
