f5793373e1c6db9a87e5ae522ecd0d94c8c51068
chrisfoster76 <chrisfoster186@googlemail.com>
2024-04-15 16:43:54 +0100

    Placeholder handler and tests

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Application/Queries/Applications/WhenHandlingGetIndexQuery.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.FindAnApprenticeship.Application.Queries.Applications.GetApplications;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.FindAnApprenticeship.UnitTests.Application.Queries.Applications
+{
+    [TestFixture]
+    public class WhenHandlingGetIndexQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_The_QueryResult_Is_Returned_As_Expected(
+            GetApplicationsQuery query,
+            GetApplicationsApiResponse applicationApiResponse,
+            [Frozen] Mock<IFindApprenticeshipApiClient<FindApprenticeshipApiConfiguration>> faaApiClient,
+            [Frozen] Mock<ICandidateApiClient<CandidateApiConfiguration>> candidateApiClient,
+            GetApplicationsQueryHandler handler)
+        {
+            var expectedGetApplicationRequest = new GetApplicationsApiRequest(query.CandidateId);
+            candidateApiClient
+                .Setup(client => client.Get<GetApplicationsApiResponse>(
+                    It.Is<GetApplicationsApiRequest>(r => r.GetUrl == expectedGetApplicationRequest.GetUrl)))
+                .ReturnsAsync(applicationApiResponse);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            using var scope = new AssertionScope();
+            result.Applications.Count.Should().Be(applicationApiResponse.Applications.Count);
+        }
+    }
+}
