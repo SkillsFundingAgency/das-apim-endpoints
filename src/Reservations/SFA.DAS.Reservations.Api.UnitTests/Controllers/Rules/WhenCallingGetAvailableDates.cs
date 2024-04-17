a040c94cc30d2760827aa062d80deca4ed38912f
ogrwill <will.ogrady@digital.education.gov.uk>
2024-04-17 11:02:23 +0100

    unit tests

---
+++ src/Reservations/SFA.DAS.Reservations.Api.UnitTests/Controllers/Rules/WhenCallingGetAvailableDates.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Api.Controllers;
+using SFA.DAS.Reservations.Api.Models;
+using SFA.DAS.Reservations.Application.Rules.Queries.GetAvailableDates;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Reservations.Api.UnitTests.Controllers.Rules
+{
+    public class WhenCallingGetAvailableDates
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_Available_Dates_From_Mediator(
+          int accountLegalEntityId,
+          GetAvailableDatesResult mediatorResult,
+          [Frozen] Mock<IMediator> mockMediator,
+          [Greedy] RulesController controller)
+        {
+            mockMediator
+                .Setup(mediator => mediator.Send(
+                    It.Is<GetAvailableDatesQuery>(query =>
+                        query.AccountLegalEntityId == accountLegalEntityId),
+                    It.IsAny<CancellationToken>()))
+                .ReturnsAsync(mediatorResult);
+
+            var controllerResult = await controller.GetAvailableDates(accountLegalEntityId) as ObjectResult;
+
+            Assert.That(controllerResult, Is.Not.Null);
+            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            var model = controllerResult.Value as GetAvailableDatesApiResponse;
+            Assert.That(model, Is.Not.Null);
+            model.Should().BeEquivalentTo(mediatorResult);
+        }
+    }
+}
---
+++ src/Reservations/SFA.DAS.Reservations.UnitTests/Application/Rules/WhenHandlingGetAvailableDatesQuery.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Reservations.Application.Rules.Queries.GetAvailableDates;
+using SFA.DAS.Reservations.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Reservations;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.Reservations.UnitTests.Application.Rules
+{
+    public class WhenHandlingGetAvailableDatesQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Then_Gets_Cohort_From_CommitmentsV2_Api(
+           GetAvailableDatesQuery query,
+           GetAvailableDatesResponse apiResponse,
+           [Frozen] Mock<IReservationApiClient<ReservationApiConfiguration>> mockApiClient,
+           GetAvailableDatesQueryHandler handler)
+        {
+            mockApiClient
+                .Setup(client => client.Get<GetAvailableDatesResponse>(It.Is<GetAvailableDatesRequest>(
+                    request => request.AccountLegalEntityId == query.AccountLegalEntityId
+                    )))
+                .ReturnsAsync(apiResponse);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Should().BeEquivalentTo(apiResponse);
+        }
+    }
+}
