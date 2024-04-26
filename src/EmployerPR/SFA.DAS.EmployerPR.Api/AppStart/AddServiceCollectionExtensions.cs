88809ac8bb92540f411a12d38a90133dce312d06
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-04-26 10:22:30 +0100

    CSP-1338 create employer PR outer api (#1421)
    * CSP-1338 create employer PR outer api
    
    * CSP-1338 added in wiring for govuk auth
    
    * CSP-1338 updated azure pipeline to include auth
    
    * CSP-1338 updates from TR comments and removed vulnerabilities

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.Api/AppStart/AddServiceCollectionExtensions.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Api.Common.AppStart;
+using SFA.DAS.Api.Common.Configuration;
+using SFA.DAS.Api.Common.Infrastructure;
+using SFA.DAS.Api.Common.Interfaces;
+using SFA.DAS.SharedOuterApi.AppStart;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Infrastructure.HealthCheck;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Services;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.EmployerPR.Api.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class AddServiceCollectionExtensions
+{
+    private static readonly string Ready = "ready";
+    public static IServiceCollection AddAuthentication(this IServiceCollection services, IConfigurationRoot configuration)
+    {
+        if (configuration.IsLocalAcceptanceTestsOrDev() || configuration.IsLocal()) return services;
+
+        var azureAdConfiguration = configuration
+            .GetSection("AzureAd")
+            .Get<AzureActiveDirectoryConfiguration>();
+        var policies = new Dictionary<string, string>
+        {
+            {"default", "APIM"}
+        };
+
+        services.AddAuthentication(azureAdConfiguration, policies);
+
+        return services;
+    }
+
+    public static IServiceCollection AddServiceRegistration(this IServiceCollection services, IConfigurationRoot configuration)
+    {
+        services.AddHttpClient();
+
+        services.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
+        services.AddTransient(typeof(IInternalApiClient<>), typeof(InternalApiClient<>));
+        services.AddTransient<IAccountsApiClient<AccountsConfiguration>, AccountsApiClient>();
+        services.AddTransient<IEmployerProfilesApiClient<EmployerProfilesApiConfiguration>, EmployerProfilesApiClient>();
+        services.AddTransient<IProviderRelationshipsApiClient<ProviderRelationshipsApiConfiguration>, ProviderRelationshipsApiClient>();
+
+        services.AddTransient<IEmployerAccountsService, EmployerAccountsService>();
+        return services;
+    }
+
+    public static IServiceCollection AddServiceHealthChecks(this IServiceCollection services)
+    {
+        services.AddHealthChecks()
+            .AddCheck<AccountsApiHealthCheck>(AccountsApiHealthCheck.HealthCheckResultDescription,
+                failureStatus: HealthStatus.Unhealthy,
+                tags: new[] { Ready })
+        .AddCheck<EmployerProfilesApiHealthCheck>(EmployerProfilesApiHealthCheck.HealthCheckResultDescription,
+                failureStatus: HealthStatus.Unhealthy,
+                tags: new[] { Ready });
+
+        return services;
+    }
+
+    public static IServiceCollection AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions();
+        services.Configure<EmployerProfilesApiConfiguration>(configuration.GetSection(nameof(EmployerProfilesApiConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<EmployerProfilesApiConfiguration>>()!.Value);
+        services.Configure<ProviderRelationshipsApiConfiguration>(configuration.GetSection(nameof(ProviderRelationshipsApiConfiguration)));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<ProviderRelationshipsApiConfiguration>>()!.Value);
+        services.Configure<AccountsConfiguration>(configuration.GetSection("AccountsInnerApi"));
+        services.AddSingleton(cfg => cfg.GetService<IOptions<AccountsConfiguration>>()!.Value);
+        return services;
+    }
+}
