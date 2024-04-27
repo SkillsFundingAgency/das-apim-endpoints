f498a6f4ecc49f2ef45c6c312c55eea2fb80a092
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-04-27 16:41:21 +0100

    FAI-1187 - Add handler for submitting application

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Apply/SubmitApplication/SubmitApplicationCommandHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.JsonPatch;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses;
+using SFA.DAS.FindAnApprenticeship.InnerApi.RecruitApi.Requests;
+using SFA.DAS.FindAnApprenticeship.Models;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindAnApprenticeship.Application.Commands.Apply.SubmitApplication;
+
+public class SubmitApplicationCommandHandler(IRecruitApiClient<RecruitApiConfiguration> recruitApiClient, ICandidateApiClient<CandidateApiConfiguration> candidateApiClient) : IRequestHandler<SubmitApplicationCommand, bool>
+{
+    public async Task<bool> Handle(SubmitApplicationCommand request, CancellationToken cancellationToken)
+    {
+        var application =
+            await candidateApiClient.Get<GetApplicationApiResponse>(
+                new GetApplicationApiRequest(request.CandidateId, request.ApplicationId, true));
+
+        if (application == null)
+        {
+            return false;
+        }
+
+        var response = await recruitApiClient.PostWithResponseCode<NullResponse>(
+            new PostSubmitApplicationRequest(request.CandidateId, application));
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            return false;
+        }
+        
+        var jsonPatchDocument = new JsonPatchDocument<Models.Application>();
+        
+        jsonPatchDocument.Replace(x => x.Status, ApplicationStatus.Submitted);
+        
+        var patchRequest = new PatchApplicationApiRequest(request.ApplicationId, request.CandidateId, jsonPatchDocument);
+        await candidateApiClient.PatchWithResponseCode(patchRequest);
+        
+        return true;
+    }
+}
