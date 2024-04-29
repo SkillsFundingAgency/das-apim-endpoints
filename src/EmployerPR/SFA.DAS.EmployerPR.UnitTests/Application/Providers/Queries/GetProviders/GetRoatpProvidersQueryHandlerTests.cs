4a6c38e0c10e1f628ae6e5f0bfecc10d4945f1c4
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-04-29 10:29:30 +0100

    CSP-1372 Get All Providers code and tests (#1445)
    * CSP-1372 Get All Providers code and tests
    
    * CSP-1372 coverlet added to test project config
    
    * CSP-1372 TR comments addressed
    
    * CSP-1372 fix to shared unittests proj

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.UnitTests/Application/Providers/Queries/GetProviders/GetRoatpProvidersQueryHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerPR.Application.Queries.GetRoatpProviders;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.RoatpV2;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerPR.UnitTests.Application.Providers.Queries.GetProviders;
+public class GetRoatpProvidersQueryHandlerTests
+{
+    [Test, MoqAutoData]
+    public async Task Handle_ReturnCalendarEvents(
+        [Frozen] Mock<IRoatpV2TrainingProviderService> trainingProviderService,
+        GetRoatpProvidersQueryHandler handler,
+        GetProvidersResponse expected,
+        CancellationToken cancellationToken)
+    {
+        var query = new GetRoatpProvidersQuery();
+        trainingProviderService.Setup(x => x.GetProviders(cancellationToken)).ReturnsAsync(expected);
+        var actual = await handler.Handle(query, cancellationToken);
+        actual.Should().Be(expected.RegisteredProviders.Select(provider => (RoatpProvider)provider));
+    }
+}
--- src/EmployerPR/SFA.DAS.EmployerPR/Application/Queries/GetEmployerAccountProviders/GetEmployerAccountProvidersQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SFA.DAS.EmployerPR.Application.Queries.GetEmployerAccountProviders;
-public class GetEmployerAccountProvidersQuery
-{
-    // this is for mediatr registration for the time being
-    public int AccountId { get; set; }
-}
