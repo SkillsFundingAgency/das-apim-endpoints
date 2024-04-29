dacd9a1514961dcb8e3f11b65656c7b2584530c9
Ali Reid <ali.reid@live.co.uk>
2024-04-29 10:06:14 +0100

    Controller tests

---
+++ src/ApprenticeApp/SFA.DAS.ApprenticeApp.UnitTests/Controllers/MyApprenticeDetailsControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeApp.Api.Controllers;
+using SFA.DAS.ApprenticeApp.Application.Queries.ApprenticeAccounts;
+using SFA.DAS.Testing.AutoFixture;
+using static SFA.DAS.ApprenticeApp.Api.Controllers.MyApprenticeshipController;
+
+namespace SFA.DAS.ApprenticeApp.UnitTests
+{
+    public class MyApprenticeDetailsControllerTests
+    {
+        [Test, MoqAutoData]
+        public async Task Get_Apprentice_Details_Test(
+            [Greedy] MyApprenticeshipController controller)
+        {
+            var httpContext = new DefaultHttpContext();
+            var apprenticeId = Guid.NewGuid();
+            var request = new MyApprenticeshipConfirmedRequest();
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = httpContext
+            };
+
+            var result = await controller.Post(apprenticeId, request);
+            result.Should().BeOfType(typeof(OkResult));
+        }
+     
+    }
+}
