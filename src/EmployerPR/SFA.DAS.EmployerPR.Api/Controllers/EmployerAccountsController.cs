2e7dc8a24bc7551712c0fc8a5f5bd5adb9c1fcd6
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-05-13 13:27:53 +0100

    Csp 1375 get legal entities for employer (#1449)
    * CSP-1375 Get Employer Account Legal Entities
    
    * CSP-1375 updating response and request to shared space
    
    * CSP-1375 fixed broken test for get roatp providers handler
    
    * CSP-1375 fixed broken test

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.Api/Controllers/EmployerAccountsController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerPR.Application.Queries.GetAccountLegalEntities;
+
+namespace SFA.DAS.EmployerPR.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class EmployerAccountsController(IMediator _mediator) : ControllerBase
+{
+    [HttpGet]
+    [Route("{hashedAccountId}/legalentities")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(GetAccountLegalEntitiesQueryResult), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAccountLegalEntities(string hashedAccountId, CancellationToken cancellationToken)
+    {
+        var queryResult = await _mediator.Send(new GetAccountLegalEntitiesQuery { HashedAccountId = hashedAccountId }, cancellationToken);
+        return Ok(queryResult);
+    }
+}
