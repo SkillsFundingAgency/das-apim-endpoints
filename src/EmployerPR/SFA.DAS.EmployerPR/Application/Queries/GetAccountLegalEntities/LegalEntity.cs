f58d465e55bdd8cb4a87005d5ca05cdec5b15ca8
Peter Williams <peter.david.williams.1991@gmail.com>
2024-05-21 16:14:41 +0100

    CSP-1375 - Refactored return object names to match desired response

--- src/EmployerPR/SFA.DAS.EmployerPR/Application/Queries/GetAccountLegalEntities/LegalEntity.cs
+++ src/EmployerPR/SFA.DAS.EmployerPR/Application/Queries/GetAccountLegalEntities/LegalEntity.cs
@@ -4,17 +4,17 @@ namespace SFA.DAS.EmployerPR.Application.Queries.GetAccountLegalEntities;
 
 public class LegalEntity
 {
-    public string? Name { get; set; }
-    public string? PublicHashedId { get; set; }
-    public long? LegalEntityId { get; set; }
+    public string? AccountLegalEntityName { get; set; }
+    public string? AccountLegalEntityPublicHashedId { get; set; }
+    public long? AccountLegalEntityId { get; set; }
 
     public static implicit operator LegalEntity(GetAccountLegalEntityResponse source)
     {
         return new LegalEntity
         {
-            Name = source.Name,
-            PublicHashedId = source.AccountLegalEntityPublicHashedId,
-            LegalEntityId = source.AccountLegalEntityId
+            AccountLegalEntityName = source.Name,
+            AccountLegalEntityPublicHashedId = source.AccountLegalEntityPublicHashedId,
+            AccountLegalEntityId = source.AccountLegalEntityId
         };
     }
 }
