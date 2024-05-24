eecb6a3c73e854551fef1198c0017923689e9fc6
Peter Williams <peter.david.williams.1991@gmail.com>
2024-05-24 10:02:34 +0100

    CSP-1390 - Fixed build failure caused by removing duplicate Operations definitions

--- src/EmployerPR/SFA.DAS.EmployerPR/Application/Queries/GetPermissions/GetPermissionsResponse.cs
+++ src/EmployerPR/SFA.DAS.EmployerPR/Application/Queries/GetPermissions/GetPermissionsResponse.cs
@@ -1,4 +1,4 @@
-﻿using SFA.DAS.SharedOuterApi.InnerApi.Responses.ProviderRelationships;
+﻿using SFA.DAS.SharedOuterApi.Models.ProviderRelationships;
 
 namespace SFA.DAS.EmployerPR.Application.Queries.GetPermissions;
 public class GetPermissionsResponse
