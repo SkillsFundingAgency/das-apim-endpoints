7f0221459412d7817b5397c6664503262c91f002
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-05-24 08:24:16 +0100

    FAI-1253 - Add sending of email on successful application submission

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Apply/SubmitApplication/SubmitApplicationCommandHandler.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Apply/SubmitApplication/SubmitApplicationCommandHandler.cs
@@ -4,17 +4,27 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.JsonPatch;
+using SFA.DAS.FindAnApprenticeship.Domain.EmailTemplates;
 using SFA.DAS.FindAnApprenticeship.Domain.Models;
 using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
 using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses;
 using SFA.DAS.FindAnApprenticeship.InnerApi.RecruitApi.Requests;
+using SFA.DAS.FindAnApprenticeship.InnerApi.Responses;
+using SFA.DAS.FindAnApprenticeship.Services;
+using SFA.DAS.Notifications.Messages.Commands;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Infrastructure;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.FindAnApprenticeship.Application.Commands.Apply.SubmitApplication;
 
-public class SubmitApplicationCommandHandler(IRecruitApiClient<RecruitApiConfiguration> recruitApiClient, ICandidateApiClient<CandidateApiConfiguration> candidateApiClient) : IRequestHandler<SubmitApplicationCommand, bool>
+public class SubmitApplicationCommandHandler(
+    IRecruitApiClient<RecruitApiConfiguration> recruitApiClient, 
+    ICandidateApiClient<CandidateApiConfiguration> candidateApiClient,
+    IVacancyService vacancyService,
+    INotificationService notificationService,
+    EmailEnvironmentHelper helper) 
+    : IRequestHandler<SubmitApplicationCommand, bool>
 {
     public async Task<bool> Handle(SubmitApplicationCommand request, CancellationToken cancellationToken)
     {
@@ -34,7 +44,17 @@ public class SubmitApplicationCommandHandler(IRecruitApiClient<RecruitApiConfigu
         {
             return false;
         }
-        
+
+        var vacancy = await vacancyService.GetVacancy(application.VacancyReference) as GetApprenticeshipVacancyItemResponse;
+        var email = new SubmitApplicationEmail(
+            helper.SubmitApplicationEmailTemplateId,
+            application.Candidate.Email,
+            application.Candidate.FirstName,
+            vacancy?.Title, vacancy?.EmployerName, 
+            vacancy?.Address.AddressLine4 ?? vacancy?.Address.AddressLine3 ?? vacancy?.Address.AddressLine2 ?? vacancy?.Address.AddressLine1, 
+            vacancy?.Address.Postcode, 
+            helper.CandidateApplicationUrl);
+        await notificationService.Send(new SendEmailCommand(email.TemplateId, email.RecipientAddress, email.Tokens));
         var jsonPatchDocument = new JsonPatchDocument<Domain.Models.Application>();
         
         jsonPatchDocument.Replace(x => x.Status, ApplicationStatus.Submitted);
