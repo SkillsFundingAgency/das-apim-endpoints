0853dff6121d43929f365476448512ce18141e92
Peter Williams <peter.david.williams.1991@gmail.com>
2024-05-24 09:29:05 +0100

    CSP-1390 - Minor refactor to resolve build errors

--- src/Approvals/SFA.DAS.Approvals.Api.UnitTests/Controllers/ProviderPermissions/WhenGettingHasPermission.cs
+++ src/Approvals/SFA.DAS.Approvals.Api.UnitTests/Controllers/ProviderPermissions/WhenGettingHasPermission.cs
@@ -13,7 +13,6 @@ using SFA.DAS.Approvals.Application.ProviderPermissions.Queries;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.ProviderRelationships;
 using SFA.DAS.SharedOuterApi.Models.ProviderRelationships;
 using SFA.DAS.Testing.AutoFixture;
-using Operation = SFA.DAS.SharedOuterApi.InnerApi.Requests.ProviderPermissions.Operation;
 
 namespace SFA.DAS.Approvals.Api.UnitTests.Controllers.ProviderPermissions;
 
