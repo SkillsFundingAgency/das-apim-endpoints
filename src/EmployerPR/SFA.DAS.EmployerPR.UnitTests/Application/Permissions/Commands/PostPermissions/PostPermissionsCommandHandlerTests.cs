55432677ab5c641a16766dc693bc5b23b516ab2d
shaun-downey-education <Shaun.Downey@education.gov.uk>
2024-06-06 11:12:44 +0100

    CSP-1411: Post Permissions Endpoint and associated unit tests.

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.UnitTests/Application/Permissions/Commands/PostPermissions/PostPermissionsCommandHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerPR.Application.Commands.PostPermissions;
+using SFA.DAS.EmployerPR.Infrastructure;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerPR.UnitTests.Application.Permissions.Commands.PostPermissions;
+
+public class PostPermissionsCommandHandlerTests
+{
+    [Test]
+    [MoqAutoData]
+    public async Task Handle_Returns_PostPermissionsCommandResult(
+        [Frozen] Mock<IProviderRelationshipsApiRestClient> providerRelationshipsApiRestClient,
+        PostPermissionsCommandHandler sut,
+        PostPermissionsCommand command,
+        PostPermissionsCommandResult result
+    )
+    {
+        providerRelationshipsApiRestClient.Setup(x =>
+            x.PostPermissions(
+                command,
+                CancellationToken.None
+            )
+        ).ReturnsAsync(result);
+
+        var actual = await sut.Handle(command, CancellationToken.None);
+        actual.Should().Be(result);
+    }
+}
