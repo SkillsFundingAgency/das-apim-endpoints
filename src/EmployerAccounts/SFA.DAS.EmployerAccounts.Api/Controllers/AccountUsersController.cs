ef5f594e89c1ef6978543a60aca8fa5cca5f3dbf
Jon Harris <jon.harris@digital.education.gov.uk>
2024-06-08 09:12:32 +0100

    Logging exceptions

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/Controllers/AccountUsersController.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.Api/Controllers/AccountUsersController.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Threading.Tasks;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.EmployerAccounts.Api.Models;
 using SFA.DAS.EmployerAccounts.Application.Queries.AccountUsers.Queries;
 
@@ -10,19 +11,15 @@ namespace SFA.DAS.EmployerAccounts.Api.Controllers
 {
     [ApiController]
     [Route("[controller]/")]
-    public class AccountUsersController : ControllerBase
+    public class AccountUsersController(IMediator mediator, ILogger<AccountUsersController> logger) : ControllerBase
     {
-        private readonly IMediator _mediator;
-
-        public AccountUsersController(IMediator mediator) => _mediator = mediator;
-
         [HttpGet]
         [Route("{userId}/accounts")]
         public async Task<IActionResult> GetUserAccounts(string userId, [FromQuery] string email)
         {
             try
             {
-                var result = await _mediator.Send(new GetAccountsQuery
+                var result = await mediator.Send(new GetAccountsQuery
                 {
                     UserId = userId,
                     Email = email
@@ -30,9 +27,9 @@ namespace SFA.DAS.EmployerAccounts.Api.Controllers
 
                 return Ok((UserAccountsApiResponse)result);
             }
-            catch (Exception e)
+            catch (Exception exception)
             {
-                Console.WriteLine(e);
+                logger.LogError(exception, "Exception caught in AccountUsersController.GetUserAccounts.");
                 return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
             }
         }
