5a27febd2a481f0779daedd2b4919d19ef542992
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2024-06-13 10:36:31 +0100

    CSP-1379: Employer Outer API - GET Relationships Endpoint (#1498) (#1507)
    * CSP-1379: Get employer relationships outer API endpoint.
    
    * CSP-1379: Upgrade transitive packages.
    
    * CSP-1379: Upgrade transitive packages.
    
    * CSP-1379: Unit test to cover construction of get employer relationships url.
    
    * CSP-1379: Rework management endpoints to work through a restease client within the EmployerPR API.
    
    * CSP-1379: Removed unused GetEmployerRelationshipsRequest object.
    
    * CSP-1379: Update operations namespace.
    
    * CSP-1379: Revert operations change.
    
    * CSP-1379: Update Operations reference.

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.Api/Controllers/EmployerRelationshipsController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.EmployerPR.Application.Queries.GetEmployerRelationships;
+
+namespace SFA.DAS.EmployerPR.Api.Controllers;
+
+[ApiController]
+[Route("relationships")]
+public class EmployerRelationshipsController(IMediator _mediator) : ControllerBase
+{
+    [HttpGet("employeraccount/{accountHashedId}")]
+    [Produces("application/json")]
+    [ProducesResponseType(typeof(GetEmployerRelationshipsQueryResult), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetEmployerRelationships(string accountHashedId, [FromQuery] long? ukprn, [FromQuery] string? accountlegalentityPublicHashedId, CancellationToken cancellationToken)
+    {
+        GetEmployerRelationshipsQuery query = new(accountHashedId, ukprn, accountlegalentityPublicHashedId);
+
+        var result = await _mediator.Send(query, cancellationToken);
+
+        return Ok(result);
+    }
+}
---
+++ src/EmployerPR/SFA.DAS.EmployerPR/Application/Queries/GetEmployerRelationships/GetEmployerRelationshipsQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace SFA.DAS.EmployerPR.Application.Queries.GetEmployerRelationships;
+
+public class GetEmployerRelationshipsQuery : IRequest<GetEmployerRelationshipsQueryResult>
+{
+    public string AccountHashedId { get; set; }
+
+    public long? Ukprn { get; set; }
+
+    public string? AccountlegalentityPublicHashedId { get; set; }
+
+    public GetEmployerRelationshipsQuery(string accountHashedId, long? ukprn = null, string? accountlegalentityPublicHashedId = null)
+    {
+        AccountHashedId = accountHashedId;
+        Ukprn = ukprn;
+        AccountlegalentityPublicHashedId = accountlegalentityPublicHashedId;
+    }
+}
