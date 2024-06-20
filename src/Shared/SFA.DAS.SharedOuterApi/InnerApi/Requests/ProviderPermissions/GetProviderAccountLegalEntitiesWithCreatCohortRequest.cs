e1ece2f26fb233e0390d9c439f17d648675d14cc
Peter Williams <peter.david.williams.1991@gmail.com>
2024-06-20 11:23:01 +0100

    CSP-1390 - Updated request url and to not hard code the operations list

--- src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/ProviderPermissions/GetProviderAccountLegalEntitiesWithCreatCohortRequest.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/ProviderPermissions/GetProviderAccountLegalEntitiesWithCreatCohortRequest.cs
@@ -1,15 +1,20 @@
+using System.Collections.Generic;
+using System.Linq;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using Operation = SFA.DAS.SharedOuterApi.Models.ProviderRelationships.Operation;
 
 namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.ProviderPermissions;
 
 public class GetProviderAccountLegalEntitiesWithCreatCohortRequest : IGetApiRequest
 {
     private readonly int? _ukprn;
+    private readonly string _operations;
 
-    public GetProviderAccountLegalEntitiesWithCreatCohortRequest(int? ukprn)
+    public GetProviderAccountLegalEntitiesWithCreatCohortRequest(int? ukprn, List<Operation> operations)
     {
         _ukprn = ukprn;
+        _operations = string.Join('&', operations.Select(o => $"operations={(int)o}"));
     }
 
-    public string GetUrl => $"accountproviderlegalentities?ukprn={_ukprn}&operations=0";
+    public string GetUrl => $"accountproviderlegalentities?ukprn={_ukprn}&{_operations}";
 }
