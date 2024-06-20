a1461a4533c1469557d73e9cc52fe05f00633657
MarkFCain <mark.cain@digital.education.gov.uk>
2024-06-20 12:14:18 +0100

    CSP-1521 Ping endpoint added

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.Api.UnitTests/Controllers/PingControllerTests/GetPingTests.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using SFA.DAS.EmployerPR.Api.Controllers;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerPR.Api.UnitTests.Controllers.PingControllerTests;
+public class GetPingTests
+{
+    [Test, MoqAutoData]
+    public async Task GetPing_ReturnsExpectedResponse(
+        [Greedy] PingController sut)
+    {
+        var result = sut.Get();
+
+        result.As<OkObjectResult>().Should().NotBeNull();
+        result.As<OkObjectResult>().Value.Should().Be("Pong");
+    }
+}
---
+++ src/EmployerPR/SFA.DAS.EmployerPR.Api/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.EmployerPR.Api.Controllers;
+
+[Route("ping")]
+[ApiController]
+[ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+public class PingController : ControllerBase
+{
+    [HttpGet, AllowAnonymous]
+    public IActionResult Get()
+    {
+        return Ok("Pong");
+    }
+}
+
