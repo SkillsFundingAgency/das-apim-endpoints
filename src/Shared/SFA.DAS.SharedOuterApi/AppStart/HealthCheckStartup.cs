5c59403474caa095c1be249286f547780cc90d58
MarkFCain <mark.cain@digital.education.gov.uk>
2024-06-21 12:56:55 +0100

    CSP-1521 reinstated ping healthcheck in outer

--- src/Shared/SFA.DAS.SharedOuterApi/AppStart/HealthCheckStartup.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/AppStart/HealthCheckStartup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
 using SFA.DAS.Api.Common.Infrastructure;
 
 namespace SFA.DAS.SharedOuterApi.AppStart
@@ -11,6 +12,16 @@ namespace SFA.DAS.SharedOuterApi.AppStart
             app.UseHealthChecks("/health", new HealthCheckOptions
             {
                 ResponseWriter = HealthCheckResponseWriter.WriteJsonResponse
+            });
+
+            app.UseHealthChecks("/ping", new HealthCheckOptions
+            {
+                Predicate = (_) => false,
+                ResponseWriter = (context, report) =>
+                {
+                    context.Response.ContentType = "application/json";
+                    return context.Response.WriteAsync("");
+                }
             });
 
             return app;
