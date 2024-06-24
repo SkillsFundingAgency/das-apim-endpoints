05c33b8a04a5215c917ed112a24ba9ade7fcddee
Chris Foster <chrisfoster186@googlemail.com>
2024-06-24 14:26:27 +0100

    Controller endpoint implementation WIP

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/Controllers/SavedVacanciesController.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using SFA.DAS.FindAnApprenticeship.Application.Queries.GetSavedVacancies;
+
+namespace SFA.DAS.FindAnApprenticeship.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SavedVacanciesController(IMediator mediator, ILogger<ApplicationsController> logger) : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] Guid candidateId)
+        {
+            try
+            {
+                var result = await mediator.Send(new GetSavedVacanciesQuery
+                {
+                    CandidateId = candidateId
+                });
+
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Get Saved Vacancies : An error occurred");
+                return new StatusCodeResult((int) HttpStatusCode.InternalServerError);
+            }
+        }
+    }
+}
---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/GetSavedVacancies/GetSavedVacanciesQuery.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SFA.DAS.FindAnApprenticeship.Application.Queries.GetSavedVacancies
+{
+    public class GetSavedVacanciesQuery : IRequest<GetSavedVacanciesQueryResult>
+    {
+        public Guid CandidateId { get; set; }
+    }
+
+    public class GetSavedVacanciesQueryResult
+    {
+
+    }
+
+    public class GetSavedVacanciesQueryHandler : IRequestHandler<GetSavedVacanciesQuery, GetSavedVacanciesQueryResult>
+    {
+        public async Task<GetSavedVacanciesQueryResult> Handle(GetSavedVacanciesQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
