30677676089a09584c7561ad5bbc6e5d3e4b8b28
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-06-24 13:37:56 +0100

    CSP-1521 Ping endpoint added (#1521)
    * CSP-1521 Ping endpoint added
    
    * CSP-1521 removed ping healthcheck
    
    * CSP-1521 reinstated ping healthcheck in outer
    
    * CSP-1521 updated from ping to serviceCheck
    
    * CSP-1521 rename test file
    
    * CSP-1521 reset healthcheckstartup

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.Api/Controllers/ServiceCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SFA.DAS.EmployerPR.Api.Controllers;
+
+[Route("serviceCheck")]
+[ApiController]
+[ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+public class ServiceCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
