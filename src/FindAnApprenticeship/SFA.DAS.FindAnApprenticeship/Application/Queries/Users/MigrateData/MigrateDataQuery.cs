0a81534f0c453dd39f23b19c8454750bd3c32c22
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-07-01 10:45:49 +0100

    Change queryhandler to get legacy and existing candidate data

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Users/MigrateData/MigrateDataQuery.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/Users/MigrateData/MigrateDataQuery.cs
@@ -1,9 +1,11 @@
-﻿using MediatR;
+﻿using System;
+using MediatR;
 
 namespace SFA.DAS.FindAnApprenticeship.Application.Queries.Users.MigrateData
 {
     public record MigrateDataQuery : IRequest<MigrateDataQueryResult>
     {
         public required string EmailAddress { get; set; }
+        public required Guid CandidateId { get; set; }
     }
 }
