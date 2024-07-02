c348519da9b05d1e252617c293957622f17602ae
Karen Hanna <karen@laveryhanna.com>
2024-07-02 16:33:12 +0100

    initial select request page added

---
+++ src/ProviderRequestApprenticeTraining/SFA.DAS.ProviderRequestApprenticeTraining/Application/Queries/GetSelectEmployerRequests/GetSelectEmployerRequestsQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.ProviderRequestApprenticeTraining.InnerApi.Requests;
+using SFA.DAS.ProviderRequestApprenticeTraining.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ProviderRequestApprenticeTraining.Application.Queries.GetSelectEmployerRequests
+{
+    public class GetSelectEmployerRequestsQueryHandler : IRequestHandler<GetSelectEmployerRequestsQuery, GetSelectEmployerRequestsResult>
+    {
+        private readonly IRequestApprenticeTrainingApiClient<RequestApprenticeTrainingApiConfiguration> _requestApprenticeshipTrainingApiClient;
+        private readonly ILogger<GetSelectEmployerRequestsQueryHandler> _logger;
+
+        public GetSelectEmployerRequestsQueryHandler(
+            IRequestApprenticeTrainingApiClient<RequestApprenticeTrainingApiConfiguration> requestApprenticeTrainingApiClient,
+            ILogger<GetSelectEmployerRequestsQueryHandler> logger)
+        {
+            _requestApprenticeshipTrainingApiClient = requestApprenticeTrainingApiClient;
+            _logger = logger;
+        }
+
+        public async Task<GetSelectEmployerRequestsResult> Handle(GetSelectEmployerRequestsQuery request, CancellationToken cancellationToken)
+        {
+            var selectEmployerRequests = await _requestApprenticeshipTrainingApiClient.
+                Get<List<GetSelectEmployerRequestsResponse>>(new GetSelectEmployerRequestsRequest(
+                    request.StandardReference,
+                    request.Ukprn));
+
+            return new GetSelectEmployerRequestsResult
+            {
+                SelectEmployerRequests = selectEmployerRequests.ToList()
+            };
+        }
+    }
+}
---
+++ src/ProviderRequestApprenticeTraining/SFA.DAS.ProviderRequestApprenticeTraining/InnerApi/Requests/GetSelectEmployerRequestsRequest.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+
+namespace SFA.DAS.ProviderRequestApprenticeTraining.InnerApi.Requests
+{
+    public class GetSelectEmployerRequestsRequest : IGetApiRequest
+    {
+        public string GetUrl => $"api/employerrequest/select-employer-requests?standardReference={StandardReference}&ukprn={Ukprn}";
+        public string StandardReference { get; set; }
+        public int Ukprn { get; set; }
+
+        public GetSelectEmployerRequestsRequest(string standardReference, int ukprn)
+        {
+            StandardReference = standardReference;
+            Ukprn = ukprn;
+        }
+    }
+}
