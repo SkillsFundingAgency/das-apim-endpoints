c5b923232ab16eb52ff84d7d4dd93a826ad218e9
Chris Woodcock <chris@humesoftware.com>
2024-07-04 17:37:18 +0100

    Added multiple locations page

---
+++ src/EmployerRequestApprenticeTraining/SFA.DAS.EmployerRequestApprenticeTraining.Api/Controllers/RegionsController.cs
@@ -0,0 +1,76 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.EmployerRequestApprenticeTraining.Application.Queries.GetClosestRegion;
+using SFA.DAS.EmployerRequestApprenticeTraining.Application.Queries.GetLocation;
+using SFA.DAS.EmployerRequestApprenticeTraining.Application.Queries.GetRegions;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerRequestApprenticeTraining.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class RegionsController : Controller
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<RegionsController> _logger;
+
+        public RegionsController(IMediator mediator, ILogger<RegionsController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await _mediator.Send(new GetRegionsQuery());
+
+                if (result.Regions != null)
+                {
+                    return Ok(result.Regions);
+                }
+
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error attempting to retrieve regions");
+                return new StatusCodeResult((int)HttpStatusCode.InternalServerError);
+            }
+        }
+
+        [HttpGet("closest")]
+        public async Task<IActionResult> GetClosestRegion([FromQuery] string locationName)
+        {
+            try
+            {
+                var matchingLocationResult = await _mediator.Send(new GetLocationQuery { ExactSearchTerm = locationName });
+                if(matchingLocationResult?.Location != null)
+                {
+                    var closestRegionResult = await _mediator.Send(new GetClosestRegionQuery 
+                    { 
+                        Latitude = matchingLocationResult.Location.Location.GeoPoint[0], 
+                        Longitude = matchingLocationResult.Location.Location.GeoPoint[1] 
+                    });
+
+                    if(closestRegionResult?.Region != null)
+                    {
+                        return Ok(closestRegionResult.Region);
+                    }
+                }
+
+                return NotFound();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Error attempting to retrieve closest region");
+                return BadRequest();
+            }
+        }
+    }
+}
