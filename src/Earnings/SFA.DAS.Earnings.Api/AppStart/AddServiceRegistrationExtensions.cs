89448a396f013f92b8a8bf62f1a81961f5aedfbe
tom-gough <tom.gough.tgsd@outlook.com>
2024-07-12 14:11:34 +0100

    FLP-851 WIP Initial commit for earnings outer api

---
+++ src/Earnings/SFA.DAS.Earnings.Api/AppStart/AddServiceRegistrationExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Api.Common.Configuration;
+using SFA.DAS.Api.Common.Infrastructure;
+using SFA.DAS.Api.Common.Interfaces;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Services;
+
+namespace SFA.DAS.Earnings.Api.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class AddServiceRegistrationExtensions
+{
+    public static void AddServiceRegistration(this IServiceCollection services, IConfiguration configuration)
+    {
+		//todo
+	  }
+}
+
+[ExcludeFromCodeCoverage]
+public static class AddConfigurationOptionsExtension
+{
+    public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddOptions();
+
+		//todo
+	}
+
+}
