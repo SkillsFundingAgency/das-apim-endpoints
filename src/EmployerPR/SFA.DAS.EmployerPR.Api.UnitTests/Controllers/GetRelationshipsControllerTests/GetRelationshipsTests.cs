0d0050e8d243e2adfc6d5b7aba1a809f487cf815
MarkFCain <mark.cain@digital.education.gov.uk>
2024-07-12 15:06:15 +0100

    CSP-1472 Get Relationships for Account Legal Entity

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.Api.UnitTests/Controllers/GetRelationshipsControllerTests/GetRelationshipsTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerPR.Api.Controllers;
+using SFA.DAS.EmployerPR.Application.Queries.GetRelationships;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerPR.Api.UnitTests.Controllers.GetRelationshipsControllerTests;
+
+public class GetRelationshipsTests
+{
+    [Test, MoqAutoData]
+    public async Task GetRelationships_InvokesQueryHandler(
+        [Frozen] Mock<IMediator> mediatorMock,
+        [Greedy] RelationshipsController sut,
+        GetRelationshipsQuery query,
+        CancellationToken cancellationToken)
+    {
+        await sut.GetRelationships(query, cancellationToken);
+
+        mediatorMock.Verify(
+            m => m.Send(It.IsAny<GetRelationshipsQuery>(),
+                It.IsAny<CancellationToken>()));
+    }
+
+    [Test, MoqAutoData]
+    public async Task GetRelationships_HandlerReturnsData_ReturnsExpectedResponse(
+        [Frozen] Mock<IMediator> mediatorMock,
+        [Greedy] RelationshipsController sut,
+        GetRelationshipsResponse queryResponse,
+        GetRelationshipsQuery query,
+        CancellationToken cancellationToken)
+    {
+        mediatorMock.Setup(m => m.Send(It.IsAny<GetRelationshipsQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(queryResponse);
+
+        var result = await sut.GetRelationships(query, cancellationToken);
+
+        result.As<OkObjectResult>().Should().NotBeNull();
+        result.As<OkObjectResult>().Value.Should().Be(queryResponse);
+    }
+}
--- src/EmployerPR/SFA.DAS.EmployerPR/Infrastructure/IProviderRelationshipsApiRestClient.cs
+++ src/EmployerPR/SFA.DAS.EmployerPR/Infrastructure/IProviderRelationshipsApiRestClient.cs
@@ -3,6 +3,7 @@ using SFA.DAS.EmployerPR.Application.Commands.PostNotifications;
 using SFA.DAS.EmployerPR.Application.Commands.PostPermissions;
 using SFA.DAS.EmployerPR.Application.Queries.GetEmployerRelationships;
 using SFA.DAS.EmployerPR.Application.Queries.GetPermissions;
+using SFA.DAS.EmployerPR.Application.Queries.GetRelationships;
 
 namespace SFA.DAS.EmployerPR.Infrastructure;
 
@@ -11,6 +12,9 @@ public interface IProviderRelationshipsApiRestClient
     [Get("permissions")]
     [AllowAnyStatusCode]
     Task<Response<GetPermissionsResponse>> GetPermissions([Query] long? ukprn, [Query] long? AccountLegalEntityId, CancellationToken cancellationToken);
+
+    [Get("relationships")]
+    Task<GetRelationshipsResponse> GetRelationships([Query] long? ukprn, [Query] int? accountLegalEntityId, CancellationToken cancellationToken);
 
     [Get("relationships/employeraccount/{AccountHashedId}")]
     Task<GetEmployerRelationshipsResponse> GetEmployerRelationships([Path] string AccountHashedId, [Query] long? Ukprn, [Query] string? AccountlegalentityPublicHashedId, CancellationToken cancellationToken);
