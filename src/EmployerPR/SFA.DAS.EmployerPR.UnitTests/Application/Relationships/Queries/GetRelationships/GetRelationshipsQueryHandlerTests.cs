6c785b7696acf590cc831e92ecdecea4f9a2a6bb
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-07-17 14:56:23 +0100

    CSP-1472 Get Relationships for Account Legal Entity (#1568)

---
+++ src/EmployerPR/SFA.DAS.EmployerPR.UnitTests/Application/Relationships/Queries/GetRelationships/GetRelationshipsQueryHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerPR.Application.Queries.GetRelationships;
+using SFA.DAS.EmployerPR.Infrastructure;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerPR.UnitTests.Application.Relationships.Queries.GetRelationships;
+public class GetRelationshipsQueryHandlerTests
+{
+    [Test, MoqAutoData]
+    public async Task Handle_ReturnRelationships(
+        [Frozen] Mock<IProviderRelationshipsApiRestClient> providerRelationshipsApiRestClient,
+        GetRelationshipsHandler handler,
+        GetRelationshipsQuery query,
+        GetRelationshipsResponse response
+    )
+    {
+        providerRelationshipsApiRestClient.Setup(x =>
+            x.GetRelationships(
+                query.Ukprn,
+                query.AccountLegalEntityId,
+                CancellationToken.None
+            )
+        ).ReturnsAsync(response);
+
+        var actual = await handler.Handle(query, CancellationToken.None);
+        actual.Should().BeEquivalentTo(response);
+    }
+}
