d574e5c42d39fcc1b5d3caae908effdda0e53a2e
Deven Shah <devenshah1974@gmail.com>
2024-07-18 11:50:21 +0100

    CSP-1432 Implements get relationships endpoint

---
+++ src/ProviderPR/SFA.DAS.ProviderPR/InnerApi/Requests/GetProviderRelationshipsRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace SFA.DAS.ProviderPR.InnerApi.Requests;
+
+public class GetProviderRelationshipsRequest
+{
+    public string? EmployerName { get; set; }
+    public bool HasPendingRequest { get; set; }
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+    public bool? HasCreateCohortPermission { get; set; }
+    public bool? HasRecruitWithReviewPermission { get; set; }
+    public bool? HasRecruitPermission { get; set; }
+    public bool? HasNoRecruitPermissions { get; set; }
+}
---
+++ src/ProviderPR/SFA.DAS.ProviderPR/InnerApi/Responses/GetProviderRelationshipsResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace SFA.DAS.ProviderPR.InnerApi.Responses;
+
+public class GetProviderRelationshipsResponse
+{
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+    public int TotalCount { get; set; }
+    public IEnumerable<ProviderRelationshipModel> Employers { get; set; } = [];
+}
+
+public class ProviderRelationshipModel
+{
+    public long Ukprn { get; set; }
+    public long? AccountLegalEntityId { get; set; }
+    public string? AgreementId { get; set; }
+    public Guid? RequestId { get; set; }
+    public string EmployerName { get; set; } = null!;
+    public bool? HasCreateCohortPermission { get; set; }
+    public bool? HasCreateAdvertPermission { get; set; }
+    public bool? HasCreateAdvertWithReviewPermission { get; set; }
+}
