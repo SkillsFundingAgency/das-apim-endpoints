f12f434ef17a33c85f40b58161e2f9f421c9446a
Chris Foster <chrisfoster186@googlemail.com>
2024-07-18 11:33:49 +0100

    Implements total positions available service

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Services/TotalPositionsAvailableService.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Services/TotalPositionsAvailableService.cs
@@ -2,21 +2,29 @@
 using SFA.DAS.SharedOuterApi.Interfaces;
 using System;
 using System.Threading.Tasks;
+using SFA.DAS.FindAnApprenticeship.InnerApi.RecruitApi.Requests;
 
 namespace SFA.DAS.FindAnApprenticeship.Services
 {
-    public interface ITotalPositionsAvailableService
-    {
-        Task<long> GetTotalPositionsAvailable();
-    }
-
     public class TotalPositionsAvailableService(
         IRecruitApiClient<RecruitApiConfiguration> recruitApiClient,
         ICacheStorageService cacheStorageService) : ITotalPositionsAvailableService
     {
         public async Task<long> GetTotalPositionsAvailable()
         {
-            throw new NotImplementedException();
+            var cachedValue = await cacheStorageService.RetrieveFromCache<long?>(nameof(GetTotalPositionsAvailableRequest));
+
+            if (cachedValue.HasValue)
+            {
+                return cachedValue.Value;
+            }
+
+            var totalPositionsAvailable = await recruitApiClient.Get<long>(new GetTotalPositionsAvailableRequest());
+
+            await cacheStorageService.SaveToCache(nameof(GetTotalPositionsAvailableRequest), totalPositionsAvailable,
+                    TimeSpan.FromHours(1));
+
+            return totalPositionsAvailable;
         }
     }
 }
