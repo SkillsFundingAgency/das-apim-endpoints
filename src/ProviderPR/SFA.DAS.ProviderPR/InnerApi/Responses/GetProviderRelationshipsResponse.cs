5571cd48228409b510662a771667622487775528
Deven Shah <devenshah1974@gmail.com>
2024-07-23 16:50:50 +0100

    CSP-1432 Fixed response to not have nullable bools

--- src/ProviderPR/SFA.DAS.ProviderPR/InnerApi/Responses/GetProviderRelationshipsResponse.cs
+++ src/ProviderPR/SFA.DAS.ProviderPR/InnerApi/Responses/GetProviderRelationshipsResponse.cs
@@ -14,7 +14,7 @@ public class ProviderRelationshipModel
     public string? AgreementId { get; set; }
     public Guid? RequestId { get; set; }
     public string EmployerName { get; set; } = null!;
-    public bool? HasCreateCohortPermission { get; set; }
-    public bool? HasRecruitmentPermission { get; set; }
-    public bool? HasRecruitmentWithReviewPermission { get; set; }
+    public bool HasCreateCohortPermission { get; set; }
+    public bool HasRecruitmentPermission { get; set; }
+    public bool HasRecruitmentWithReviewPermission { get; set; }
 }
