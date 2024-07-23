54210896661286d4805b0e4737c7b20b36b86a24
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2024-07-23 17:56:33 +0100

    Introduced the endpoint to update the candidate status from InProgress to Complete after Migration happy path

---
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Users/Status/UpdateCandidateStatusCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
+using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.FindAnApprenticeship.Application.Commands.Users.Status
+{
+    public record UpdateCandidateStatusCommandHandler(ICandidateApiClient<CandidateApiConfiguration> CandidateApiClient) : IRequestHandler<UpdateCandidateStatusCommand, Unit>
+    {
+        public async Task<Unit> Handle(UpdateCandidateStatusCommand request,
+            CancellationToken cancellationToken)
+        {
+            var existingUser =
+                await CandidateApiClient.GetWithResponseCode<GetCandidateApiResponse>(
+                    new GetCandidateApiRequest(request.GovUkIdentifier));
+
+            if (existingUser.StatusCode != HttpStatusCode.NotFound)
+            {
+                if (existingUser.Body.Email == request.Email)
+                {
+                    var updateEmailRequest = new PutCandidateApiRequest(existingUser.Body.Id, new PutCandidateApiRequestData
+                    {
+                        Email = request.Email,
+                        Status = request.Status
+                    });
+
+                    await CandidateApiClient.PutWithResponseCode<PutCandidateApiResponse>(updateEmailRequest);
+                }
+            }
+
+            return Unit.Value;
+        }
+    }
+}
