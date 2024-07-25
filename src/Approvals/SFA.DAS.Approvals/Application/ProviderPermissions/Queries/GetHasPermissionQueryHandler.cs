8ee30f229cab36ce441ea396996c1b22b88c2403
Peter Williams <peter.david.williams.1991@gmail.com>
2024-07-25 13:52:08 +0100

    CSP-1475 - Fixed namespace for GetHasPermissionRequest

--- src/Approvals/SFA.DAS.Approvals/Application/ProviderPermissions/Queries/GetHasPermissionQueryHandler.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/ProviderPermissions/Queries/GetHasPermissionQueryHandler.cs
@@ -2,7 +2,7 @@
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.InnerApi.Requests.ProviderPermissions;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.ProviderRelationships;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.Approvals.Application.ProviderPermissions.Queries;
@@ -12,7 +12,7 @@ public class GetHasPermissionQueryHandler(IProviderRelationshipsApiClient<Provid
     public async Task<bool> Handle(GetHasPermissionQuery request, CancellationToken cancellationToken)
     {
         var apiRequest = new GetHasPermissionRequest(request.Ukprn, request.AccountLegalEntityId, request.Operation);
-        
+
         return await apiClient.Get<bool>(apiRequest);
     }
 }
