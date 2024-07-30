92c808cb59ff7099d6eb90aa41aa6bd9206989c1
Deven Shah <devenshah1974@gmail.com>
2024-07-30 18:59:20 +0100

    CSP-1364 Refactor shared PR endpoints

--- src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetEmployerAccountTaskList/WhenHandlingGetEmployerAccountTaskListQuery.cs
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/Queries/GetEmployerAccountTaskList/WhenHandlingGetEmployerAccountTaskListQuery.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,9 +9,10 @@ using Moq;
 using NUnit.Framework;
 using SFA.DAS.EmployerAccounts.Application.Queries.GetEmployerAccountTaskList;
 using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.ProviderRelationships;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models.ProviderRelationships;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
@@ -22,9 +24,12 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
             GetEmployerAccountTaskListQuery query,
             GetProviderAccountLegalEntitiesResponse providerRelationshipResponse,
             GetAccountProvidersResponse accountProvidersResponse,
+            GetProviderAccountLegalEntityItem providerAccountLegalEntity,
             [Frozen] Mock<IProviderRelationshipsApiClient<ProviderRelationshipsApiConfiguration>> providerRelationshipApiClient,
             GetEmployerAccountTaskListQueryHandler handler)
         {
+            query.Operations = new List<Operation> { Operation.Recruitment, Operation.RecruitmentRequiresReview };
+
             providerRelationshipApiClient
                 .Setup(x =>
                     x.Get<GetAccountProvidersResponse>(It.Is<GetAccountProvidersRequest>(
@@ -32,20 +37,20 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
                             c.GetUrl.Equals(
                                 $"accounts/{query.AccountId}/providers"))))
                 .ReturnsAsync(accountProvidersResponse);
-            
+
             providerRelationshipApiClient
                 .Setup(x =>
-                    x.Get<GetProviderAccountLegalEntitiesResponse>(It.Is<GetEmployerAccountProviderPermissionsRequest>(
+                    x.Get<GetProviderAccountLegalEntitiesResponse>(It.Is<GetProviderAccountLegalEntitiesRequest>(
                         c =>
                             c.GetUrl.Equals(
-                                $"accountproviderlegalentities?accountHashedId={query.HashedAccountId}&operations=1&operations=2"))))
+                                $"accountproviderlegalentities?ukprn=&accounthashedid={query.HashedAccountId}&operations=1&operations=2"))))
                 .ReturnsAsync(providerRelationshipResponse);
 
             var actual = await handler.Handle(query, CancellationToken.None);
 
             actual.HasPermissions.Should().BeTrue();
         }
-        
+
         [Test, MoqAutoData]
         public async Task When_HasProviders_And_NoPermissions_ThenReturned(
             GetEmployerAccountTaskListQuery query,
@@ -54,8 +59,9 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
             [Frozen] Mock<IProviderRelationshipsApiClient<ProviderRelationshipsApiConfiguration>> providerRelationshipApiClient,
             GetEmployerAccountTaskListQueryHandler handler)
         {
+            query.Operations = new List<Operation> { Operation.Recruitment, Operation.RecruitmentRequiresReview };
             providerRelationshipResponse.AccountProviderLegalEntities = Array.Empty<GetProviderAccountLegalEntityItem>().ToList();
-            
+
             providerRelationshipApiClient
                 .Setup(x =>
                     x.Get<GetAccountProvidersResponse>(It.Is<GetAccountProvidersRequest>(
@@ -63,13 +69,13 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
                             c.GetUrl.Equals(
                                 $"accounts/{query.AccountId}/providers"))))
                 .ReturnsAsync(accountProvidersResponse);
-            
+
             providerRelationshipApiClient
                 .Setup(x =>
-                    x.Get<GetProviderAccountLegalEntitiesResponse>(It.Is<GetEmployerAccountProviderPermissionsRequest>(
+                    x.Get<GetProviderAccountLegalEntitiesResponse>(It.Is<GetProviderAccountLegalEntitiesRequest>(
                         c =>
                             c.GetUrl.Equals(
-                                $"accountproviderlegalentities?accountHashedId={query.HashedAccountId}&operations=1&operations=2"))))
+                                $"accountproviderlegalentities?ukprn=&accounthashedid={query.HashedAccountId}&operations=1&operations=2"))))
                 .ReturnsAsync(providerRelationshipResponse);
 
             var actual = await handler.Handle(query, CancellationToken.None);
@@ -77,7 +83,7 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
             actual.HasProviders.Should().BeTrue();
             actual.HasPermissions.Should().BeFalse();
         }
-        
+
         [Test, MoqAutoData]
         public async Task When_HasNoProviders_And_NoPermissions_ThenReturned(
             GetEmployerAccountTaskListQuery query,
@@ -86,9 +92,10 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
             [Frozen] Mock<IProviderRelationshipsApiClient<ProviderRelationshipsApiConfiguration>> providerRelationshipApiClient,
             GetEmployerAccountTaskListQueryHandler handler)
         {
+            query.Operations = new List<Operation> { Operation.Recruitment, Operation.RecruitmentRequiresReview };
             providerRelationshipResponse.AccountProviderLegalEntities = Array.Empty<GetProviderAccountLegalEntityItem>().ToList();
             accountProvidersResponse.AccountProviders = Array.Empty<AccountProviderResponse>().ToList();
-            
+
             providerRelationshipApiClient
                 .Setup(x =>
                     x.Get<GetAccountProvidersResponse>(It.Is<GetAccountProvidersRequest>(
@@ -96,17 +103,16 @@ namespace SFA.DAS.EmployerAccounts.UnitTests.Application.Queries.AccountUsers
                             c.GetUrl.Equals(
                                 $"accounts/{query.AccountId}/providers"))))
                 .ReturnsAsync(accountProvidersResponse);
-            
+
             providerRelationshipApiClient
                 .Setup(x =>
-                    x.Get<GetProviderAccountLegalEntitiesResponse>(It.Is<GetEmployerAccountProviderPermissionsRequest>(
+                    x.Get<GetProviderAccountLegalEntitiesResponse>(It.Is<GetProviderAccountLegalEntitiesRequest>(
                         c =>
                             c.GetUrl.Equals(
-                                $"accountproviderlegalentities?accountHashedId={query.HashedAccountId}&operations=1&operations=2"))))
+                                $"accountproviderlegalentities?ukprn=&accounthashedid={query.HashedAccountId}&operations=1&operations=2"))))
                 .ReturnsAsync(providerRelationshipResponse);
 
             var actual = await handler.Handle(query, CancellationToken.None);
-
             actual.HasProviders.Should().BeFalse();
             actual.HasPermissions.Should().BeFalse();
         }
