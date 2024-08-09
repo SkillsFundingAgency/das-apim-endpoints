192a8cbbb399c62605174f6a5eee31909cc47d96
CathyGroom <147840579+CathyGroom@users.noreply.github.com>
2024-08-09 13:59:34 +0100

    Add test

---
+++ src/ApprenticeApp/SFA.DAS.ApprenticeApp.UnitTests/Handlers/GetKsbsByApprenticeshipIdAndGuidListQueryHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.ApprenticeApp.Application.Queries.KsbProgress;
+using SFA.DAS.Testing.AutoFixture;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeApp.UnitTests.Handlers
+{
+    public class GetKsbsByApprenticeshipIdAndGuidListQueryHandlerTests
+    {
+        [Test, MoqAutoData]
+        public async Task GetKsbsByStandardId
+           (GetKsbsByApprenticeshipIdAndGuidListQueryHandler sut,
+           GetKsbsByApprenticeshipIdAndGuidListQuery query,
+           CancellationToken cancellationToken)
+        {
+            query.ApprenticeshipId = Guid.Empty;
+            query.Guids = new Guid[] { Guid.Empty };
+
+            await sut.Handle(query, cancellationToken);
+            sut.Should().NotBeNull();
+        }
+    }
+}
