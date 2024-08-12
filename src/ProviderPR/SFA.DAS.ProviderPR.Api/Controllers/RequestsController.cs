8e43123401c07890a9c50834cad0471202a6ed73
shaun-downey-education <shaun.downey@education.gov.uk>
2024-08-12 15:16:12 +0100

    CSP-1428: Update AddAccount method response types.

--- src/ProviderPR/SFA.DAS.ProviderPR.Api/Controllers/RequestsController.cs
+++ src/ProviderPR/SFA.DAS.ProviderPR.Api/Controllers/RequestsController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.ProviderPR.Application.Requests.Commands;
+using SFA.DAS.ProviderPR.Infrastructure;
 using SFA.DAS.ProviderPR.InnerApi.Responses;
 
 namespace SFA.DAS.ProviderPR.Api.Controllers;
@@ -10,7 +11,8 @@ namespace SFA.DAS.ProviderPR.Api.Controllers;
 public class RequestsController(IMediator _mediator) : ControllerBase
 {
     [HttpPost("addaccount")]
-    [ProducesResponseType(typeof(GetProviderRelationshipsResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(AddAccountRequestCommandResult), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> AddAccount([FromBody] AddAccountRequestCommand command, CancellationToken cancellationToken)
     {
         AddAccountRequestCommandResult result = await _mediator.Send(command, cancellationToken);
