993dae30c9bbad8e691cbf6725c7abd42be588d0
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-08-28 22:01:48 +0100

    FAI-1254 - Add withdraw application template id to email env helper

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Models/WhenCreatingEmailEnvironmentHelper.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.UnitTests/Models/WhenCreatingEmailEnvironmentHelper.cs
@@ -12,6 +12,7 @@ public class WhenCreatingEmailEnvironmentHelper
         var actual = new EmailEnvironmentHelper("PRd");
 
         actual.SubmitApplicationEmailTemplateId.Should().Be("ce8a2834-83ad-4b84-83a1-099f593cd313");
+        actual.WithdrawApplicationEmailTemplateId.Should().Be("d81b30cd-df12-46ed-9459-7e914f1f8a3b");
         actual.CandidateApplicationUrl.Should().Be("https://findapprenticeship.service.gov.uk/applications");
     }
     [Test]
@@ -20,6 +21,7 @@ public class WhenCreatingEmailEnvironmentHelper
         var actual = new EmailEnvironmentHelper("TEST");
 
         actual.SubmitApplicationEmailTemplateId.Should().Be("4b584d3c-7f56-4fd1-95fd-3099ddcb2ffa");
+        actual.WithdrawApplicationEmailTemplateId.Should().Be("983607b3-742f-4bec-b2cc-7846cbe4368d");
         actual.CandidateApplicationUrl.Should().Be("https://test-findapprenticeship.apprenticeships.education.gov.uk/applications");
     }
 }
--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Domain/Models/EmailEnvironmentHelper.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Domain/Models/EmailEnvironmentHelper.cs
@@ -9,11 +9,15 @@ public class EmailEnvironmentHelper
         SubmitApplicationEmailTemplateId =
             environmentName.Equals("PRD", StringComparison.CurrentCultureIgnoreCase) 
                 ? "ce8a2834-83ad-4b84-83a1-099f593cd313" : "4b584d3c-7f56-4fd1-95fd-3099ddcb2ffa";
-        
+        WithdrawApplicationEmailTemplateId=
+            environmentName.Equals("PRD", StringComparison.CurrentCultureIgnoreCase) 
+                ? "d81b30cd-df12-46ed-9459-7e914f1f8a3b" : "983607b3-742f-4bec-b2cc-7846cbe4368d";
+            
         CandidateApplicationUrl = environmentName.Equals("PRD", StringComparison.CurrentCultureIgnoreCase) 
             ? "https://findapprenticeship.service.gov.uk/applications" : $"https://{environmentName.ToLower()}-findapprenticeship.apprenticeships.education.gov.uk/applications";
     }
     
     public string SubmitApplicationEmailTemplateId { get; }
+    public string WithdrawApplicationEmailTemplateId { get; }
     public string CandidateApplicationUrl { get; }
 }
