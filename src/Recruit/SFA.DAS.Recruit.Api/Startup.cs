d0f850c81375d8144fe29f01b003d37edd6e5000
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-08-29 07:38:07 +0100

    FAI-1255 - Add nservice bus config and startup code to recruit

--- src/Recruit/SFA.DAS.Recruit.Api/Startup.cs
+++ src/Recruit/SFA.DAS.Recruit.Api/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Authorization;
