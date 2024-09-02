f0f0bd6f35acaddb4a9db3ba40a1dfc89d7a3cc9
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2024-09-02 15:48:07 +0100

    CSP-1468: Provider PR Outer API: Create Permission Request (#1615)
    * CSP-1428: Add Account Request - Updated RequestController to include AddAccountRequest method.
    
    * CSP-1468: Create Permission Request - Updated RequestController to include CreatePermissionRequest.

---
+++ src/ProviderPR/SFA.DAS.ProviderPR/InnerApi/Notifications/Commands/NotificationModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SFA.DAS.ProviderPR.InnerApi.Notifications.Commands;
+
+public class NotificationModel
+{
+    public required string TemplateName { get; set; }
+    public required string NotificationType { get; set; }
+    public long? Ukprn { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? Contact { get; set; }
+    public string? EmployerName { get; set; }
+    public Guid? RequestId { get; set; }
+    public long? AccountLegalEntityId { get; set; }
+    public int? PermitApprovals { get; set; }
+    public int? PermitRecruit { get; set; }
+    public required string CreatedBy { get; set; }
+}
