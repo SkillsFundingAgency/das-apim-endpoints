904b39898f52d1aa638f663723e8abeb18b4300e
tom-gough <tom.gough.tgsd@outlook.com>
2024-09-03 09:08:52 +0100

    FLP-860 merge fix

--- src/Earnings/SFA.DAS.Earnings.Api/Startup.cs
+++ src/Earnings/SFA.DAS.Earnings.Api/Startup.cs
@@ -93,7 +93,6 @@ public class Startup
             endpoints.MapControllerRoute(
                 name: "default",
                 pattern: "api/{controller=Earnings}/{action=index}/{id?}");
-                pattern: "api/{controller=Earnings}");
         });
 
         app.UseSwagger();
