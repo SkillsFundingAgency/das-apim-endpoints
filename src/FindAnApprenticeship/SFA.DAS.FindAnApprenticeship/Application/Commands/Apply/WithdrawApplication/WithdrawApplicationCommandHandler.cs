cf9874754d4da592f7bf9d8b6f01dde085a7307c
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-09-04 22:43:06 +0100

    FAI-1255 - make sure always address item for withdraw email

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Apply/WithdrawApplication/WithdrawApplicationCommandHandler.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Apply/WithdrawApplication/WithdrawApplicationCommandHandler.cs
@@ -55,7 +55,7 @@ public class WithdrawApplicationCommandHandler(
             application.Candidate.Email,
             application.Candidate.FirstName,
             vacancy?.Title, vacancy?.EmployerName,
-            vacancy?.Address.AddressLine4 ?? vacancy?.Address.AddressLine3 ?? vacancy?.Address.AddressLine2 ?? vacancy?.Address.AddressLine1,
+            vacancy?.Address.AddressLine4 ?? vacancy?.Address.AddressLine3 ?? vacancy?.Address.AddressLine2 ?? vacancy?.Address.AddressLine1 ?? "Unknown",
             vacancy?.Address.Postcode);
         
         await Task.WhenAll(
