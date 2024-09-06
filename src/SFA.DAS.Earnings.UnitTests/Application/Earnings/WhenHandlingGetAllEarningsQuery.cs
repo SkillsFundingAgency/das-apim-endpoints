46f0672376f55e8cdb2a6feef6c303699915d40b
tom-gough <tom.gough.tgsd@outlook.com>
2024-09-06 10:35:52 +0100

    FLP-862 Tidy up test file

--- src/SFA.DAS.Earnings.UnitTests/Application/Earnings/WhenHandlingGetAllEarningsQuery.cs
+++ src/SFA.DAS.Earnings.UnitTests/Application/Earnings/WhenHandlingGetAllEarningsQuery.cs
@@ -1,8 +1,6 @@
 using AutoFixture;
-using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
 using FluentAssertions;
 using Moq;
-using SFA.DAS.Apprenticeships.Types;
 using SFA.DAS.Earnings.Application.Earnings;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests.Apprenticeships;
@@ -473,51 +471,6 @@ namespace SFA.DAS.Earnings.UnitTests.Application.Earnings
             }
         }
 
-        //[Test]
-        //public async Task AndApprenticeshipStartedInPreviousAcademicYearThenReturnsPriceEpisodesForEachAcademicYear()
-        //{
-        //    //Arrange
-        //    _mockCollectionCalendarApiClient.Reset();
-        //    _mockCollectionCalendarApiClient
-        //        .Setup(x => x.Get<GetAcademicYearsResponse>(It.IsAny<GetAcademicYearsRequest>()))
-        //        .ReturnsAsync(BuildCollectionCalendarResponse(_apprenticeshipsResponse, false));
-
-        //    // Act
-        //    _result = await _handler.Handle(_query, CancellationToken.None);
-
-        //    // Assert
-        //    _result.Should().NotBeNull();
-
-        //    var apprenticeship = _apprenticeshipsResponse.Apprenticeships
-        //        .MinBy(x => x.Episodes
-        //            .Min(episode => episode.Prices
-        //                .Min(price => price.StartDate)));
-
-        //    var apprenticeshipStartDate = apprenticeship.Episodes
-        //            .Min(episode => episode.Prices
-        //                .Min(price => price.StartDate));
-
-        //    var fm36Learner = _result.FM36Learners
-        //        .SingleOrDefault(x => x.ULN == long.Parse(apprenticeship.Uln));
-
-        //    fm36Learner.PriceEpisodes.Count.Should().Be(apprenticeship.Episodes.SelectMany(e => e.Prices).Count() + 1);
-        //    fm36Learner.PriceEpisodes.Should().Contain(pe => pe.PriceEpisodeValues.EpisodeStartDate == apprenticeshipStartDate);
-        //    fm36Learner.PriceEpisodes.Should().Contain(pe => pe.PriceEpisodeValues.EpisodeStartDate == apprenticeshipStartDate.AddMonths(1));
-
-        //    //foreach (var apprenticeship in _apprenticeshipsResponse.Apprenticeships)
-        //    //{
-        //    //    var expectedEpisodeStartDate = apprenticeship.Episodes.Min(episode => episode.Prices.Min(price => price.StartDate));
-
-        //    //    var fm36Learner = _result.FM36Learners
-        //    //        .SingleOrDefault(x => x.ULN == long.Parse(apprenticeship.Uln));
-
-        //    //    fm36Learner.PriceEpisodes
-        //    //        .Select(episode => episode.PriceEpisodeValues.EpisodeStartDate)
-        //    //        .Should()
-        //    //        .AllBeEquivalentTo(expectedEpisodeStartDate);
-        //    //}
-        //}
-
         private GetApprenticeshipsResponse BuildApprenticeshipsResponse(long ukprn)
         {
             var response = new GetApprenticeshipsResponse
@@ -678,16 +631,6 @@ namespace SFA.DAS.Earnings.UnitTests.Application.Earnings
                 AcademicYear = "2021",
                 StartDate = new DateTime(2020, 8, 1),
                 EndDate = new DateTime(2021, 7, 31)
-            };
-
-            var earliestApprenticeshipStartDate = apprenticeshipsResponse.Apprenticeships
-                .Min(x => x.Episodes
-                    .Min(episode => episode.Prices
-                        .Min(price => price.StartDate)));
-
-            return new GetAcademicYearsResponse
-            {
-                StartDate = apprenticeshipStartedInCurrentAcademicYear ? earliestApprenticeshipStartDate.AddMonths(-1) : earliestApprenticeshipStartDate.AddMonths(1)
             };
         }
 
