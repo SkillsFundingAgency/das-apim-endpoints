437caad6b8b8c0d4add43294aa1e6ab9f00e335d
Chris Foster <chrisfoster186@googlemail.com>
2024-09-12 14:46:14 +0100

    Implementation of get notification settings

---
+++ src/AdminAan/SFA.DAS.AdminAan.Api/Controllers/NotificationSettingsController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AdminAan.Application.NotificationsSettings.Queries;
+
+namespace SFA.DAS.AdminAan.Api.Controllers
+{
+    [Route("notification-settings")]
+    [ApiController]
+    public class NotificationSettingsController( IMediator mediator) : ControllerBase
+    {
+        [Route("")]
+        [ProducesResponseType(typeof(GetNotificationsSettingsQueryResult), StatusCodes.Status200OK)]
+        [HttpGet]
+        public async Task<IActionResult> Index(Guid memberId)
+        {
+            var result = await mediator.Send(new GetNotificationsSettingsQuery
+            {
+                MemberId = memberId
+            });
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/AdminAan/SFA.DAS.AdminAan.UnitTests/Application/Notifications/Queries/GetNotificationSettingsQueryHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.AdminAan.Application.NotificationsSettings.Queries;
+using SFA.DAS.AdminAan.Domain;
+using SFA.DAS.AdminAan.Infrastructure;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.AdminAan.UnitTests.Application.Notifications.Queries
+{
+    public class GetNotificationSettingsQueryHandlerTests
+    {
+        [Test]
+        [MoqAutoData]
+        public async Task Handle_Return_NotificationPreferences(
+            GetNotificationsSettingsQuery query,
+            [Frozen] Mock<IAanHubRestApiClient> apiClient,
+            [Frozen] GetNotificationsSettingsQueryHandler handler,
+            GetMemberResponse apiResponse)
+        {
+            apiClient
+                .Setup(x => x.GetMember(query.MemberId, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(apiResponse);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.ReceiveNotifications.Should().Be(apiResponse.ReceiveNotifications);
+        }
+
+    }
+}
