77fa0be19f58c6c9e9fb5760505d139cf7500743
Deven Shah <devenshah1974@gmail.com>
2024-10-03 17:42:30 +0100

    CSP-1512 Added account history check

--- src/EmployerPR/SFA.DAS.EmployerPR.UnitTests/Application/Requests/Queries/ValidatePermissionsRequest/ValidatePermissionsRequestQueryHandlerTests.cs
+++ src/EmployerPR/SFA.DAS.EmployerPR.UnitTests/Application/Requests/Queries/ValidatePermissionsRequest/ValidatePermissionsRequestQueryHandlerTests.cs
@@ -1,4 +1,5 @@
-﻿using AutoFixture.NUnit3;
+﻿using System.Net;
+using AutoFixture.NUnit3;
 using FluentAssertions;
 using Moq;
 using NUnit.Framework;
@@ -7,6 +8,11 @@ using SFA.DAS.EmployerPR.Application.Requests.Queries.ValidateRequest;
 using SFA.DAS.EmployerPR.Common;
 using SFA.DAS.EmployerPR.Infrastructure;
 using SFA.DAS.EmployerPR.UnitTests.Common;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.EmployerAccounts;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.EmployerAccounts;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.EmployerPR.UnitTests.Application.Requests.Queries.ValidatePermissionsRequest;
@@ -46,20 +52,46 @@ public class ValidatePermissionsRequestQueryHandlerTests
         actual.IsRequestValid.Should().BeFalse();
     }
 
-    [Test, MoqInlineAutoData]
+    [Test, MoqAutoData]
     public async Task Handle_RequestTypeIsCreateAccount_SetsRequestStatus(
-        [Frozen] Mock<IProviderRelationshipsApiRestClient> clientMock,
+        [Frozen] Mock<IProviderRelationshipsApiRestClient> prApiClientMock,
+        [Frozen] Mock<IAccountsApiClient<AccountsConfiguration>> accountsApiClientMock,
         ValidatePermissionsRequestQueryHandler sut,
         ValidatePermissionsRequestQuery query,
         GetRequestQueryResult getRequestQueryResult,
         CancellationToken cancellationToken)
     {
+        accountsApiClientMock.Setup(a => a.GetWithResponseCode<GetAccountHistoriesByPayeResponse>(It.Is<GetAccountHistoriesByPayeRequest>(r => r.PayeRef == getRequestQueryResult.EmployerPAYE))).ReturnsAsync(new ApiResponse<GetAccountHistoriesByPayeResponse>(new(), HttpStatusCode.NotFound, null));
+
         getRequestQueryResult.RequestType = RequestType.CreateAccount;
-        clientMock.Setup(c => c.GetRequest(query.RequestId, cancellationToken)).ReturnsAsync(RestEaseResponseBuilder.GetOkResponse(getRequestQueryResult));
+        prApiClientMock.Setup(c => c.GetRequest(query.RequestId, cancellationToken)).ReturnsAsync(RestEaseResponseBuilder.GetOkResponse(getRequestQueryResult));
 
         var actual = await sut.Handle(query, cancellationToken);
 
         actual.IsRequestValid.Should().BeTrue();
         actual.Status.Should().Be(getRequestQueryResult.Status);
+    }
+
+    [Test]
+    [MoqInlineAutoData(HttpStatusCode.NotFound, false)]
+    [MoqInlineAutoData(HttpStatusCode.OK, true)]
+    public async Task Handle_RequestTypeIsCreateAccount_SetsRequestStatus(
+        HttpStatusCode expectedAccountHistoriesStatusCode,
+        bool expectedHasEmployerAccount,
+        [Frozen] Mock<IProviderRelationshipsApiRestClient> prApiClientMock,
+        [Frozen] Mock<IAccountsApiClient<AccountsConfiguration>> accountsApiClientMock,
+        ValidatePermissionsRequestQueryHandler sut,
+        ValidatePermissionsRequestQuery query,
+        GetRequestQueryResult getRequestQueryResult,
+        CancellationToken cancellationToken)
+    {
+        accountsApiClientMock.Setup(a => a.GetWithResponseCode<GetAccountHistoriesByPayeResponse>(It.Is<GetAccountHistoriesByPayeRequest>(r => r.PayeRef == getRequestQueryResult.EmployerPAYE))).ReturnsAsync(new ApiResponse<GetAccountHistoriesByPayeResponse>(new(), expectedAccountHistoriesStatusCode, null));
+
+        getRequestQueryResult.RequestType = RequestType.CreateAccount;
+        prApiClientMock.Setup(c => c.GetRequest(query.RequestId, cancellationToken)).ReturnsAsync(RestEaseResponseBuilder.GetOkResponse(getRequestQueryResult));
+
+        var actual = await sut.Handle(query, cancellationToken);
+
+        actual.HasEmployerAccount.Should().Be(expectedHasEmployerAccount);
     }
 }
--- src/EmployerPR/SFA.DAS.EmployerPR/Application/Requests/Queries/ValidateRequest/ValidatePermissionsRequestQueryHandler.cs
+++ src/EmployerPR/SFA.DAS.EmployerPR/Application/Requests/Queries/ValidateRequest/ValidatePermissionsRequestQueryHandler.cs
@@ -1,27 +1,33 @@
-﻿using MediatR;
+﻿using System.Net;
+using MediatR;
 using SFA.DAS.EmployerPR.Application.Requests.Queries.GetRequest;
 using SFA.DAS.EmployerPR.Common;
 using SFA.DAS.EmployerPR.Infrastructure;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.EmployerAccounts;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.EmployerAccounts;
+using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.EmployerPR.Application.Requests.Queries.ValidateRequest;
 
 public class ValidatePermissionsRequestQueryHandler(
-    IProviderRelationshipsApiRestClient _apiClient
+    IProviderRelationshipsApiRestClient _prApiClient,
+    IAccountsApiClient<AccountsConfiguration> _accountsApiClient
     ) : IRequestHandler<ValidatePermissionsRequestQuery, ValidatePermissionsRequestQueryResult>
 {
     public async Task<ValidatePermissionsRequestQueryResult> Handle(ValidatePermissionsRequestQuery query, CancellationToken cancellationToken)
     {
         ValidatePermissionsRequestQueryResult result = new();
 
-        var response = await _apiClient.GetRequest(query.RequestId, cancellationToken);
+        var permissionRequestResponse = await _prApiClient.GetRequest(query.RequestId, cancellationToken);
 
-        if (!response.ResponseMessage.IsSuccessStatusCode)
+        if (!permissionRequestResponse.ResponseMessage.IsSuccessStatusCode)
         {
             result.IsRequestValid = false;
             return result;
         }
 
-        GetRequestQueryResult permissionRequest = response.GetContent()!;
+        GetRequestQueryResult permissionRequest = permissionRequestResponse.GetContent()!;
 
         if (permissionRequest.RequestType != RequestType.CreateAccount)
         {
@@ -31,6 +37,11 @@ public class ValidatePermissionsRequestQueryHandler(
 
         result.IsRequestValid = true;
         result.Status = permissionRequest.Status;
+
+        var accountHistoriesResponse = await _accountsApiClient.GetWithResponseCode<GetAccountHistoriesByPayeResponse>(new GetAccountHistoriesByPayeRequest(permissionRequest.EmployerPAYE));
+
+        result.HasEmployerAccount = accountHistoriesResponse.StatusCode == HttpStatusCode.OK;
+
         return result;
     }
 }
