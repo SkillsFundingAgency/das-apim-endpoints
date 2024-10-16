df7417273907b17104950e22590ceaced5264e7d
Paul Graham <paul.graham@coprime.co.uk>
2024-10-16 16:05:11 +0100

    removed NLog replaced with AppInsights

--- src/Reservations/SFA.DAS.Reservations.Api/Startup.cs
+++ src/Reservations/SFA.DAS.Reservations.Api/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using SFA.DAS.Api.Common.AppStart;
@@ -33,6 +35,13 @@ namespace SFA.DAS.Reservations.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton(_env);
+
+            services.AddLogging(builder =>
+            {
+                builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
+                builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+                builder.SetMinimumLevel(LogLevel.Trace);
+            });
 
             services.AddConfigurationOptions(_configuration);
 
