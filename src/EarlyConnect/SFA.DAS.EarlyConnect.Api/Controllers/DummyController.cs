0d1edc2f03d39ed371bd871577a1ff0c883a3236
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-10-18 15:03:57 +0100

    dummy endpoint removed

--- src/EarlyConnect/SFA.DAS.EarlyConnect.Api/Controllers/DummyController.cs
+++ src/EarlyConnect/SFA.DAS.EarlyConnect.Api/Controllers/DummyController.cs
@@ -7,17 +7,17 @@ namespace SFA.DAS.EarlyConnect.Api.Controllers
     [ApiController]
     public class DummyController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
-        {
-            var response = new
-            {
-                Message = "This is a sample API response.",
-                Timestamp = DateTime.UtcNow
-            };
+        //[HttpGet]
+        //public IActionResult Get()
+        //{
+        //    var response = new
+        //    {
+        //        Message = "This is a sample API response.",
+        //        Timestamp = DateTime.UtcNow
+        //    };
 
-            return Ok(response);
-        }
+        //    return Ok(response);
+        //}
 
         [HttpGet("{id}")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
