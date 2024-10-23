9ec7992489b8776541cb493a9976db9b6044e3f0
Deven Shah <devenshah1974@gmail.com>
2024-10-23 14:08:53 +0100

    CSP-1513 Added new endpoint to the PR API Client

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/EmployerAccounts/PostCreateAccountResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.EmployerAccounts;
+public class PostCreateAccountResponse
+{
+    public required EmployerAccountSummary Account { get; set; }
+    public required AccountLegalEntitySummary AccountLegalEntity { get; set; }
+}
