c418d71be3d781e76fa8ded399dcbc23de9a3e2a
Karen Hanna <karen@laveryhanna.com>
2024-10-25 09:11:23 +0100

    bug fix - variant can be null when

--- src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback/Models/FeedbackTargetVariant.cs
+++ src/ApprenticeFeedback/SFA.DAS.ApprenticeFeedback/Models/FeedbackTargetVariant.cs
@@ -6,6 +6,6 @@ namespace SFA.DAS.ApprenticeFeedback.Models
     public class FeedbackTargetVariant
     {
         public long ApprenticeshipId { get; set; }
-        public string Variant { get; set; }
+        public string? Variant { get; set; }
     }
 }
