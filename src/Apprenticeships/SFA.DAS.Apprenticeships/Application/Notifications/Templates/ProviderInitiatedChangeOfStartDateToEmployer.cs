b3db70b3737ee9d8be7bce9d65ee76461cb35f87
tom-gough <tom.gough.tgsd@outlook.com>
2024-10-29 08:55:53 +0000

    FLP-490 namespace fix

--- src/Apprenticeships/SFA.DAS.Apprenticeships/Application/Notifications/Templates/ProviderInitiatedChangeOfStartDateToEmployer.cs
+++ src/Apprenticeships/SFA.DAS.Apprenticeships/Application/Notifications/Templates/ProviderInitiatedChangeOfStartDateToEmployer.cs
@@ -1,10 +1,11 @@
-﻿namespace SFA.DAS.Apprenticeships.Application.Notifications.Templates;
-
-internal static class ProviderInitiatedChangeOfStartDateToEmployer
+﻿namespace SFA.DAS.Apprenticeships.Application.Notifications.Templates
 {
-    public const string TemplateId = "ProviderInitiatedChangeOfStartDateToEmployer";
-    public const string TrainingProvider = "Training provider";
-    public const string Employer = "Employer";
-    public const string Apprentice = "apprentice";
-    public const string ReviewChangesUrl = "review changes URL";
+    internal static class ProviderInitiatedChangeOfStartDateToEmployer
+    {
+        public const string TemplateId = "ProviderInitiatedChangeOfStartDateToEmployer";
+        public const string TrainingProvider = "Training provider";
+        public const string Employer = "Employer";
+        public const string Apprentice = "apprentice";
+        public const string ReviewChangesUrl = "review changes URL";
+    }
 }
