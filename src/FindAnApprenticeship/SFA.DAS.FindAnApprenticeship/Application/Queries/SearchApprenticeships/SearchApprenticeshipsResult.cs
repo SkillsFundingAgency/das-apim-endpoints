b747fc69cfbca3e19a1944c9f2fa26738a4eeb00
killij <51908793+killij@users.noreply.github.com>
2024-10-30 11:04:45 +0000

    FAI-1954 - Add saved search count to the search results

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/SearchApprenticeships/SearchApprenticeshipsResult.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Queries/SearchApprenticeships/SearchApprenticeshipsResult.cs
@@ -19,5 +19,6 @@ namespace SFA.DAS.FindAnApprenticeship.Application.Queries.SearchApprenticeships
         public List<GetCourseLevelsListItem> Levels { get; set; }
         public long TotalFound { get; set; }
         public bool DisabilityConfident { get; set; }
+        public int SavedSearchesCount { get; init; }
     }
 }
