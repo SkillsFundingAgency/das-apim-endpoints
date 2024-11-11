1804ccdd499b46141522728909762ee23d34b186
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-11-11 16:40:06 +0000

    FAI-1946 - Add saved searches and routes to response if candidate id supplied

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Domain/Models/SavedSearch.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Domain/Models/SavedSearch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SFA.DAS.FindAnApprenticeship.InnerApi.FindApprenticeApi.Responses;
 
 namespace SFA.DAS.FindAnApprenticeship.Domain.Models;
 
@@ -10,3 +11,27 @@ public record SavedSearch(
     DateTime? EmailLastSendDate,
     SearchParameters SearchParameters
 );
+
+public static class SavedSearchExtensions
+{
+    public static SavedSearch MapSavedSearch(this GetCandidateSavedSearchesApiResponse.SavedSearchResponse source)
+    {
+        return new SavedSearch(
+            source.Id,
+            source.UserReference,
+            source.DateCreated,
+            source.LastRunDate,
+            source.EmailLastSendDate,
+            new SearchParameters(
+                source.SearchParameters.SearchTerm,
+                source.SearchParameters.SelectedRouteIds,
+                source.SearchParameters.Distance,
+                source.SearchParameters.DisabilityConfident,
+                source.SearchParameters.SelectedLevelIds,
+                source.SearchParameters.Location,
+                source.SearchParameters.Latitude,
+                source.SearchParameters.Longitude
+            )
+        );
+    }
+}
