8da9d792134f7769cff253ee4c9b9dd705eb7953
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-11-11 15:43:37 +0000

    CSP-1648 GET EmployerAgreementTemplates (#1746)

---
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts/Application/Queries/GetEmployerAgreementTemplates/GetEmployerAgreementTemplatesResponse.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.EmployerAccounts.Application.Models;
+
+namespace SFA.DAS.EmployerAccounts.Application.Queries.GetEmployerAgreementTemplates;
+public class GetEmployerAgreementTemplatesResponse
+{
+    public List<EmployerAgreementTemplate> EmployerAgreementTemplates { get; set; }
+}
