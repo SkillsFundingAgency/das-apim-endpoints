5fe47d48de816991101a89e19c48e08898dc5afc
PeterDavidWilliams <30286445+PeterDavidWilliams@users.noreply.github.com>
2024-11-21 14:30:58 +0000

    Csp 1699 add open telemetry registration to employer and provider pr (#1773)
    * CSP-1699 - Replaced ApplicationInsightTelemetry with OpenTelemetryRegistration in ProviderPR
    
    * CSP-1699 - Moved telemetry registration to its own file
    
    * CSP-1699 - Replaced ApplicationInsightTelemetry with OpenTelemetryRegistration in EmployerPR

---
+++ src/ProviderPR/SFA.DAS.ProviderPR.Api/AppStart/AddTelemetryRegistrationExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using SFA.DAS.SharedOuterApi.AppStart;
+
+namespace SFA.DAS.ProviderPR.Api.AppStart;
+
+[ExcludeFromCodeCoverage]
+public static class AddTelemetryRegistrationExtension
+{
+    public static IServiceCollection AddTelemetryRegistration(this IServiceCollection services, IConfigurationRoot configuration)
+    {
+        services.AddOpenTelemetryRegistration(configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]);
+
+        return services;
+    }
+}
