7f3701ac03151b24e75890ddcd07e2df8c606d37
Michael Young <mick.young1981@googlemail.com>
2024-11-27 15:05:47 +0000

    WiP

---
+++ src/Apprenticeships/SFA.DAS.Apprenticeships.Stubs/Program.cs
@@ -0,0 +1,187 @@
+﻿using System.Net;
+using System.Text.Json;
+using AutoFixture;
+using SFA.DAS.Apprenticeships.InnerApi;
+using SFA.DAS.Apprenticeships.Responses;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Apprenticeships;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.CollectionCalendar;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace SFA.DAS.Apprenticeships.Stubs
+{
+    public class Program
+    {
+        private static WireMockServer _fakeCoursesApi;
+        private static WireMockServer _fakeApprenticeshipsApi;
+        private static WireMockServer _fakeCommitmentsApi;
+        private static WireMockServer _fakeAccountsApi;
+        private static WireMockServer _fakeEmployerProfilesApi;
+        private static WireMockServer _fakeCollectionCalendarApi;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                _fakeCoursesApi = WireMockServer.Start(new WireMockServerSettings
+                {
+                    Urls = new[] { "http://*:6011" },
+                    StartAdminInterface = true,
+                });
+
+                _fakeApprenticeshipsApi = WireMockServer.Start(new WireMockServerSettings
+                {
+                    Urls = new[] { "http://*:6012" },
+                    StartAdminInterface = true,
+                });
+                SetUpApprenticeshipResponses();
+
+                _fakeCommitmentsApi = WireMockServer.Start(new WireMockServerSettings
+                {
+                    Urls = new[] { "http://*:6013" },
+                    StartAdminInterface = true,
+                });
+                SetUpCommitmentsResponses();
+
+                _fakeAccountsApi = WireMockServer.Start(new WireMockServerSettings
+                {
+                    Urls = new[] { "http://*:6014" },
+                    StartAdminInterface = true,
+                });
+                SetUpAccountsResponses();
+
+                _fakeEmployerProfilesApi = WireMockServer.Start(new WireMockServerSettings
+                {
+                    Urls = new[] { "http://*:6015" },
+                    StartAdminInterface = true,
+                });
+                SetUpEmployerProfilesResponses();
+
+                _fakeCollectionCalendarApi = WireMockServer.Start(new WireMockServerSettings
+                {
+                    Urls = new[] { "http://*:6016" },
+                    StartAdminInterface = true,
+                });
+                SetupCollectionCalendarResponses();
+
+                Console.WriteLine(("Please RETURN to stop server"));
+                Console.ReadLine();
+            }
+            finally
+            {
+                _fakeCoursesApi.Stop();
+                _fakeCoursesApi.Dispose();
+                _fakeApprenticeshipsApi.Stop();
+                _fakeApprenticeshipsApi.Dispose();
+                _fakeCommitmentsApi.Stop();
+                _fakeCommitmentsApi.Dispose();
+                _fakeAccountsApi.Stop();
+                _fakeAccountsApi.Dispose();
+                _fakeEmployerProfilesApi.Stop();
+                _fakeEmployerProfilesApi.Dispose();
+                _fakeCollectionCalendarApi.Stop();
+                _fakeCollectionCalendarApi.Dispose();
+            }
+        }
+
+        private static void SetupCollectionCalendarResponses()
+        {
+            _fakeCollectionCalendarApi.Given(
+                    Request.Create().WithPath($"/academicyears")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Create<GetAcademicYearsResponse>())));
+        }
+
+        private static void SetUpCommitmentsResponses()
+        {
+            _fakeCommitmentsApi.Given(
+                    Request.Create().WithPath($"/api/AccountLegalEntity/*")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Create<GetAccountLegalEntityResponse>())));
+
+            _fakeCommitmentsApi.Given(
+                    Request.Create().WithPath($"/api/providers/*")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Create<GetProviderResponse>())));
+
+            _fakeCommitmentsApi.Given(
+                    Request.Create().WithPath($"/api/trainingprogramme/*/versions")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Create<GetTrainingProgrammeVersionsResponse>())));
+        }
+
+        private static void SetUpApprenticeshipResponses()
+        {
+            _fakeApprenticeshipsApi.Given(
+                    Request.Create().WithPath($"/*/key")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Create<Guid>())));
+
+            _fakeApprenticeshipsApi.Given(
+                    Request.Create().WithPath($"/*/price")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Create<GetApprenticeshipPriceResponse>())));
+
+            _fakeApprenticeshipsApi.Given(
+                    Request.Create().WithPath($"/*/startDate")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Create<GetApprenticeshipStartDateResponse>())));
+        }
+
+        private static void SetUpAccountsResponses()
+        {
+            _fakeAccountsApi.Given(
+                    Request.Create().WithPath($"/api/user/*/accounts")
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().CreateMany<GetUserAccountsResponse>())));
+        }
+
+        private static void SetUpEmployerProfilesResponses()
+        {
+            _fakeEmployerProfilesApi.Given(
+                    Request.Create().WithPath($"/api/users/*")
+                        .UsingGet()
+                )
+                .RespondWith(
+                    Response.Create()
+                        .WithStatusCode((int)HttpStatusCode.OK)
+                        .WithHeader("Content-Type", "application/json")
+                        .WithBody(JsonSerializer.Serialize(new Fixture().Build<EmployerProfileUsersApiResponse>().With(x => x.Id, Guid.NewGuid().ToString).Create())));
+        }
+    }
+}
