9be337434688e6998e5cd63dcda31f330ac6bae8
Jon Harris <jon.harris@fuitsu.com>
2024-11-27 14:15:56 +0000

    Bit of refactoring/updating

--- src/Forecasting/SFA.DAS.Forecasting.Api/Models/GetPledgesResponse.cs
+++ src/Forecasting/SFA.DAS.Forecasting.Api/Models/GetPledgesResponse.cs
@@ -2,28 +2,27 @@
 using System.Linq;
 using SFA.DAS.Forecasting.Application.Pledges.Queries.GetPledges;
 
-namespace SFA.DAS.Forecasting.Api.Models
+namespace SFA.DAS.Forecasting.Api.Models;
+
+public class GetPledgesResponse
 {
-    public class GetPledgesResponse
+    public IEnumerable<Pledge> Pledges { get; set; }
+
+    public class Pledge
     {
-        public IEnumerable<Pledge> Pledges { get; set; }
+        public int Id { get; set; }
+        public long AccountId { get; set; }
+    }
 
-        public class Pledge
+    public static implicit operator GetPledgesResponse(GetPledgesQueryResult source)
+    {
+        return new GetPledgesResponse
         {
-            public int Id { get; set; }
-            public long AccountId { get; set; }
-        }
-
-        public static implicit operator GetPledgesResponse(GetPledgesQueryResult source)
-        {
-            return new GetPledgesResponse
+            Pledges = source.Pledges.Select(p => new Pledge
             {
-                Pledges = source.Pledges.Select(p => new Pledge
-                {
-                    Id = p.Id,
-                    AccountId = p.AccountId
-                })
-            };
-        }
+                Id = p.Id,
+                AccountId = p.AccountId
+            })
+        };
     }
 }
