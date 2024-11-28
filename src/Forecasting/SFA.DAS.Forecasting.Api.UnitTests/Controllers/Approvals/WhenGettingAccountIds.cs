40c742da246361c89bd9a4a06dc9a5331ad28201
Jon Harris <jon.harris@fuitsu.com>
2024-11-28 14:27:55 +0000

    Bit of refactoring/updating

--- src/Forecasting/SFA.DAS.Forecasting.Api.UnitTests/Controllers/Approvals/WhenGettingAccountIds.cs
+++ src/Forecasting/SFA.DAS.Forecasting.Api.UnitTests/Controllers/Approvals/WhenGettingAccountIds.cs
@@ -11,38 +11,37 @@ using SFA.DAS.Forecasting.Api.Controllers;
 using SFA.DAS.Forecasting.Api.Models;
 using SFA.DAS.Forecasting.Application.Approvals.Queries.GetAccountIds;
 
-namespace SFA.DAS.Forecasting.Api.UnitTests.Controllers.Approvals
+namespace SFA.DAS.Forecasting.Api.UnitTests.Controllers.Approvals;
+
+[TestFixture]
+public class WhenGettingAccountIds
 {
-    [TestFixture]
-    public class WhenGettingAccountIds
+    private ApprovalsController _controller;
+    private Mock<IMediator> _mediator;
+    private readonly Fixture _fixture = new();
+    private GetAccountIdsQueryResult _queryResult;
+
+    [SetUp]
+    public void Setup()
     {
-        private ApprovalsController _controller;
-        private Mock<IMediator> _mediator;
-        private readonly Fixture _fixture = new Fixture();
-        private GetAccountIdsQueryResult _queryResult;
+        _mediator = new Mock<IMediator>();
 
-        [SetUp]
-        public void Setup()
-        {
-            _mediator = new Mock<IMediator>();
+        _queryResult = _fixture.Create<GetAccountIdsQueryResult>();
+        _mediator.Setup(x => x.Send(It.IsAny<GetAccountIdsQuery>(), CancellationToken.None))
+            .ReturnsAsync(_queryResult);
 
-            _queryResult = _fixture.Create<GetAccountIdsQueryResult>();
-            _mediator.Setup(x => x.Send(It.IsAny<GetAccountIdsQuery>(), CancellationToken.None))
-                .ReturnsAsync(_queryResult);
+        _controller = new ApprovalsController(_mediator.Object, Mock.Of<ILogger<ApprovalsController>>());
+    }
 
-            _controller = new ApprovalsController(_mediator.Object, Mock.Of<ILogger<ApprovalsController>>());
-        }
+    [Test]
+    public async Task Then_Accounts_Are_Returned_Correctly()
+    {
+        var result = await _controller.GetAccountsWithCohorts() as ObjectResult;
 
-        [Test]
-        public async Task Then_Accounts_Are_Returned_Correctly()
-        {
-            var result = await _controller.GetAccountsWithCohorts() as ObjectResult;
+        Assert.That(result, Is.Not.Null);
+        var response = result.Value as GetAccountsWithCohortsResponse;
+        Assert.That(response, Is.Not.Null);
 
-            Assert.That(result, Is.Not.Null);
-            var response = result.Value as GetAccountsWithCohortsResponse;
-            Assert.That(response, Is.Not.Null);
-
-            response.AccountIds.Should().BeEquivalentTo(_queryResult.AccountIds);
-        }
+        response.AccountIds.Should().BeEquivalentTo(_queryResult.AccountIds);
     }
 }
