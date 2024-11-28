75a8121b4b2bd99e4d67a94811dd48730e94ea94
Chris Foster <chrisfoster186@googlemail.com>
2024-11-28 16:22:08 +0000

    Endpoint WIP

---
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/Onboarding/ConfirmDetails/Queries/GetOnboardingConfirmDetailsQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using SFA.DAS.EmployerAan.Infrastructure;
+
+namespace SFA.DAS.EmployerAan.Application.Onboarding.ConfirmDetails.Queries;
+
+public class GetOnboardingConfirmDetailsQueryHandler(ICommitmentsV2ApiClient commitmentsV2ApiClient)
+    : IRequestHandler<GetOnboardingConfirmDetailsQuery, GetOnboardingConfirmDetailsQueryResult>
+{
+    public async Task<GetOnboardingConfirmDetailsQueryResult> Handle(GetOnboardingConfirmDetailsQuery request, CancellationToken cancellationToken)
+    {
+        var responseEmployerAccountTask = commitmentsV2ApiClient.GetEmployerAccountSummary(request.EmployerAccountId, cancellationToken);
+        var responseEmployerSummaryTask = commitmentsV2ApiClient.GetApprenticeshipsSummaryForEmployer(request.EmployerAccountId, cancellationToken);
+
+        await Task.WhenAll(responseEmployerAccountTask, responseEmployerSummaryTask);
+
+        GetOnboardingConfirmDetailsQueryResult result = new();
+
+        var outputEmployerAccount = responseEmployerAccountTask.Result;
+
+        result.NumberOfActiveApprentices = outputEmployerAccount!.ApprenticeshipStatusSummaryResponse.Any() ?
+            outputEmployerAccount.ApprenticeshipStatusSummaryResponse.Sum(a => a.ActiveCount) : 0;
+
+        var outputEmployerSummary = responseEmployerSummaryTask.Result!;
+
+        result.Sectors = outputEmployerSummary.Sectors.ToList();
+
+        return result;
+    }
+}
