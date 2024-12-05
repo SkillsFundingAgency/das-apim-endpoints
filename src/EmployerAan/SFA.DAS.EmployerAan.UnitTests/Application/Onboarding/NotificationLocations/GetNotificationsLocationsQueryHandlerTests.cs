49a4b6185d82152a83ba1aad6cce769c3c88b2aa
Chris Foster <chrisfoster186@googlemail.com>
2024-12-05 11:34:30 +0000

    Adds new endpoint to support location search

---
+++ src/EmployerAan/SFA.DAS.EmployerAan.UnitTests/Application/Onboarding/NotificationLocations/GetNotificationsLocationsQueryHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.EmployerAan.Application.Onboarding.NotificationsLocations;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.EmployerAan.UnitTests.Application.Onboarding.NotificationLocations
+{
+    [TestFixture]
+    public class GetNotificationsLocationsQueryHandlerTests
+    {
+        [Test]
+        [MoqAutoData]
+        public async Task Handle_ReturnAddressesBasedOnQuery(GetNotificationsLocationsQuery query,
+           [Frozen] Mock<ILocationApiClient<LocationApiConfiguration>> apiClient,
+           [Frozen] GetNotificationsLocationsQueryHandler handler,
+           GetLocationsListResponse apiResponse)
+        {
+            var expectedApiRequest = new GetLocationsQueryRequest(query.SearchTerm);
+
+            apiClient
+                .Setup(x => x.Get<GetLocationsListResponse>(It.Is<GetLocationsQueryRequest>(r => r.GetUrl == expectedApiRequest.GetUrl)))
+                .ReturnsAsync(apiResponse);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Locations.Should().BeEquivalentTo(apiResponse.Locations.Select(x => new GetNotificationsLocationsQueryResult.Location
+            {
+                Name = x.DisplayName,
+                GeoPoint = x.Location.GeoPoint
+            }).Take(GetNotificationsLocationsQueryHandler.MaxResults));
+        }
+    }
+}
---
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/Onboarding/NotificationsLocations/GetNotificationsLocationsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.EmployerAan.Application.Onboarding.NotificationsLocations;
+
+public class GetNotificationsLocationsQueryHandler(ILocationApiClient<LocationApiConfiguration> apiClient) : IRequestHandler<GetNotificationsLocationsQuery,
+    GetNotificationsLocationsQueryResult>
+{
+    public const int MaxResults = 10;
+
+    public async Task<GetNotificationsLocationsQueryResult> Handle(GetNotificationsLocationsQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = await apiClient.Get<GetLocationsListResponse>(new GetLocationsQueryRequest(request.SearchTerm));
+
+        return new GetNotificationsLocationsQueryResult
+        {
+            Locations = result.Locations.Select(x => new GetNotificationsLocationsQueryResult.Location
+            {
+                Name = x.DisplayName,
+                GeoPoint = x.Location.GeoPoint
+            }).Take(MaxResults).ToList()
+        };
+    }
+}
