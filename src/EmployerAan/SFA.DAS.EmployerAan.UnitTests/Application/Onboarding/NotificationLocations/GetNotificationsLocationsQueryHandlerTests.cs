8c1f0fa3f3ac6d0d484efa85f3683da155f362ec
Chris Foster <chrisfoster186@googlemail.com>
2024-12-05 14:12:02 +0000

    WIP

--- src/EmployerAan/SFA.DAS.EmployerAan.UnitTests/Application/Onboarding/NotificationLocations/GetNotificationsLocationsQueryHandlerTests.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan.UnitTests/Application/Onboarding/NotificationLocations/GetNotificationsLocationsQueryHandlerTests.cs
@@ -6,6 +6,7 @@ using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.EmployerAan.UnitTests.Application.Onboarding.NotificationLocations
@@ -15,12 +16,18 @@ namespace SFA.DAS.EmployerAan.UnitTests.Application.Onboarding.NotificationLocat
     {
         [Test]
         [MoqAutoData]
-        public async Task Handle_ReturnAddressesBasedOnQuery(GetNotificationsLocationsQuery query,
-           [Frozen] Mock<ILocationApiClient<LocationApiConfiguration>> apiClient,
-           [Frozen] GetNotificationsLocationsQueryHandler handler,
-           GetLocationsListResponse apiResponse)
+        public async Task Handle_Return_Matches_From_Search_When_LocationLookupService_Returns_Nothing(
+            GetNotificationsLocationsQuery query,
+            [Frozen] Mock<ILocationLookupService> mockLocationLookupService,
+            [Frozen] Mock<ILocationApiClient<LocationApiConfiguration>> apiClient,
+            [Frozen] GetNotificationsLocationsQueryHandler handler,
+            GetLocationsListResponse apiResponse)
         {
             var expectedApiRequest = new GetLocationsQueryRequest(query.SearchTerm);
+
+            mockLocationLookupService
+                .Setup(x => x.GetLocationInformation(It.IsAny<string>(), It.IsAny<double>(), It.IsAny<double>(), It.IsAny<bool>()))
+                .ReturnsAsync(()=>null);
 
             apiClient
                 .Setup(x => x.Get<GetLocationsListResponse>(It.Is<GetLocationsQueryRequest>(r => r.GetUrl == expectedApiRequest.GetUrl)))
@@ -31,9 +38,32 @@ namespace SFA.DAS.EmployerAan.UnitTests.Application.Onboarding.NotificationLocat
             result.Locations.Should().BeEquivalentTo(apiResponse.Locations.Select(x => new GetNotificationsLocationsQueryResult.Location
             {
                 Name = x.DisplayName,
-                GeoPoint = x.Location.GeoPoint,
-                Source = "apiClient"
+                GeoPoint = x.Location.GeoPoint
             }).Take(GetNotificationsLocationsQueryHandler.MaxResults));
+        }
+
+        [Test]
+        [MoqAutoData]
+        public async Task Handle_Return_A_Single_Match_From_LocationLookupService(
+            GetNotificationsLocationsQuery query,
+            LocationItem locationData,
+            [Frozen] Mock<ILocationLookupService> mockLocationLookupService,
+            [Frozen] GetNotificationsLocationsQueryHandler handler)
+        {
+            mockLocationLookupService
+                .Setup(x => x.GetLocationInformation(query.SearchTerm, 0, 0, false))
+                .ReturnsAsync(locationData);
+
+            var result = await handler.Handle(query, CancellationToken.None);
+
+            result.Locations.Should().BeEquivalentTo(new List<GetNotificationsLocationsQueryResult.Location>
+            {
+                new()
+                {
+                    Name = locationData.Name,
+                    GeoPoint = locationData.GeoPoint
+                }
+            });
         }
     }
 }
