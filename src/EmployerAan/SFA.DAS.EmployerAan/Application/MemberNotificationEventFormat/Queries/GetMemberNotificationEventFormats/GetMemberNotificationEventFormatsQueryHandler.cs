74114015696ef6d8532642e03fdea10a43a5c00e
David Rimar <david.rimar@digital.education.gov.uk>
2024-12-13 09:58:22 +0000

    EC-814 adding event formats endpoint

---
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/MemberNotificationEventFormat/Queries/GetMemberNotificationEventFormats/GetMemberNotificationEventFormatsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using SFA.DAS.EmployerAan.Infrastructure;
+
+namespace SFA.DAS.EmployerAan.Application.MemberNotificationEventFormat.Queries.GetMemberNotificationEventFormats;
+
+public class GetMemberNotificationEventFormatsQueryHandler : IRequestHandler<GetMemberNotificationEventFormatsQuery, GetMemberNotificationEventFormatsQueryResult>
+{
+    private readonly IAanHubRestApiClient _apiClient;
+
+    public GetMemberNotificationEventFormatsQueryHandler(IAanHubRestApiClient apiClient)
+    {
+        _apiClient = apiClient;
+    }
+
+    public async Task<GetMemberNotificationEventFormatsQueryResult> Handle(GetMemberNotificationEventFormatsQuery request, CancellationToken cancellationToken)
+    {
+        var eventFormats = await _apiClient.GetMemberNotificationEventFormat(request.MemberId, cancellationToken);
+
+        return eventFormats;
+    }
+}
