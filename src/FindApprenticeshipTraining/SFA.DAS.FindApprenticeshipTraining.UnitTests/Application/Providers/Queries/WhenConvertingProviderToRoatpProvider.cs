0560567dec8df9977ab82277cd5e326a3f4c4124
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2024-12-16 14:18:06 +0000

    CSP-1825 Get Registered Providers (#1848)
    * CSP-1825 Get Registered Providers
    
    * CSP-1825 upeated call to use existing client:

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Providers/Queries/WhenConvertingProviderToRoatpProvider.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.Application.Providers.GetRoatpProviders;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.RoatpV2;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Application.Providers.Queries;
+public class WhenConvertingProviderToRoatpProvider
+{
+    [Test, AutoData]
+    public void Operator_ConvertsFrom_ProviderDetails(Provider source)
+    {
+        RoatpProvider sut = source;
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(sut.Ukprn, Is.EqualTo(source.Ukprn));
+            Assert.That(sut.Name, Is.EqualTo(source.Name));
+        });
+    }
+}
