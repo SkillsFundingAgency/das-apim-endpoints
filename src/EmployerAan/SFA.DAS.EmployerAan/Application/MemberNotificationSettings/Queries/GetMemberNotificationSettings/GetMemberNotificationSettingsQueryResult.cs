92a40160de39c0dbc628a1c49e85c7edd22f511a
David Rimar <david.rimar@digital.education.gov.uk>
2024-12-17 15:27:11 +0000

    make ReceiveNotifications nullable

--- src/EmployerAan/SFA.DAS.EmployerAan/Application/MemberNotificationSettings/Queries/GetMemberNotificationSettings/GetMemberNotificationSettingsQueryResult.cs
+++ src/EmployerAan/SFA.DAS.EmployerAan/Application/MemberNotificationSettings/Queries/GetMemberNotificationSettings/GetMemberNotificationSettingsQueryResult.cs
@@ -7,5 +7,5 @@ public class GetMemberNotificationSettingsQueryResult
 {
     public IEnumerable<MemberNotificationEventFormatModel> MemberNotificationEventFormats { get; set; } = Enumerable.Empty<MemberNotificationEventFormatModel>();
     public IEnumerable<MemberNotificationLocationsModel> MemberNotificationLocations { get; set; } = Enumerable.Empty<MemberNotificationLocationsModel>();
-    public bool ReceiveMonthlyNotifications { get; set; }
+    public bool? ReceiveMonthlyNotifications { get; set; }
 }
