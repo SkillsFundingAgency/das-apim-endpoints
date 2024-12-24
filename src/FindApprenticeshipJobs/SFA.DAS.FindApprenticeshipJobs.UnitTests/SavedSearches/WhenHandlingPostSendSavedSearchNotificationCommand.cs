fd4e6e7019b4d362b44572667cee6826cb1835bd
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2024-12-24 13:17:25 +0000

    FAI-2139 -Add showSearchLink email param
    Add param to determine whether the show search link should be visible on the email

--- src/FindApprenticeshipJobs/SFA.DAS.FindApprenticeshipJobs.UnitTests/SavedSearches/WhenHandlingPostSendSavedSearchNotificationCommand.cs
+++ src/FindApprenticeshipJobs/SFA.DAS.FindApprenticeshipJobs.UnitTests/SavedSearches/WhenHandlingPostSendSavedSearchNotificationCommand.cs
@@ -21,12 +21,14 @@ public class WhenHandlingPostSendSavedSearchNotificationCommand
     [Test]
     [MoqAutoData]
     public async Task Then_The_SavedSearch_Is_Patched_LastEmailSent_Updated_And_Email_Sent(
+        PostSendSavedSearchNotificationCommand.Vacancy vacancy,
         PostSendSavedSearchNotificationCommand command,
         EmailEnvironmentHelper emailEnvironmentHelper,
         [Frozen] Mock<IFindApprenticeshipApiClient<FindApprenticeshipApiConfiguration>> findApprenticeshipApiClient,
         [Frozen] Mock<INotificationService> notificationService,
         PostSendSavedSearchNotificationCommandHandler handler)
     {
+        command.Vacancies = [vacancy,vacancy,vacancy,vacancy,vacancy];
         var expectedPatchSavedSearchApiRequest =
             new PatchSavedSearchApiRequest(command.Id, new JsonPatchDocument<PatchSavedSearch>());
 
@@ -54,6 +56,53 @@ public class WhenHandlingPostSendSavedSearchNotificationCommand
                 && c.Tokens["firstName"] == command.User.FirstName 
                 && c.Tokens["newApprenticeships"] == $"{command.Vacancies.Count.ToString()} new {(command.Vacancies.Count == 1 ? "apprenticeship" : "apprenticeships")}"
                 && c.Tokens["searchAlertDescriptor"] == $"{command.SearchTerm} in {command.Location}"
+                && c.Tokens["showSearchLink"] == "no"
+                && !string.IsNullOrEmpty(c.Tokens["unsubscribeLink"])
+                && !string.IsNullOrEmpty(c.Tokens["searchUrl"])
+                && !string.IsNullOrEmpty(c.Tokens["searchParams"])
+            )
+        ), Times.Once);
+    }
+    
+    [Test]
+    [MoqAutoData]
+    public async Task Then_The_SavedSearch_Has_ShowSearchLink_When_More_Than_Five_Results(
+        PostSendSavedSearchNotificationCommand.Vacancy vacancy,
+        PostSendSavedSearchNotificationCommand command,
+        EmailEnvironmentHelper emailEnvironmentHelper,
+        [Frozen] Mock<IFindApprenticeshipApiClient<FindApprenticeshipApiConfiguration>> findApprenticeshipApiClient,
+        [Frozen] Mock<INotificationService> notificationService,
+        PostSendSavedSearchNotificationCommandHandler handler)
+    {
+        command.Vacancies = [vacancy,vacancy,vacancy,vacancy,vacancy,vacancy];
+        var expectedPatchSavedSearchApiRequest =
+            new PatchSavedSearchApiRequest(command.Id, new JsonPatchDocument<PatchSavedSearch>());
+
+        findApprenticeshipApiClient
+            .Setup(x => x.PatchWithResponseCode(
+                It.Is<PatchSavedSearchApiRequest>(c =>
+                    c.PatchUrl == expectedPatchSavedSearchApiRequest.PatchUrl
+                )))
+            .ReturnsAsync(new ApiResponse<string>(null!, HttpStatusCode.OK, string.Empty));
+
+
+        await handler.Handle(command, CancellationToken.None);
+
+        findApprenticeshipApiClient.Verify(x => x.PatchWithResponseCode(It.Is<PatchSavedSearchApiRequest>(c =>
+            c.PatchUrl.Contains(command.Id.ToString(), StringComparison.CurrentCultureIgnoreCase) 
+            && c.Data.Operations[1].path == "/LastRunDate" 
+            && DateTime.Parse(c.Data.Operations[1].value.ToString()).Date.Equals(DateTime.UtcNow.Date)
+            && c.Data.Operations[0].path == "/EmailLastSendDate" 
+            && DateTime.Parse(c.Data.Operations[0].value.ToString()).Date.Equals(DateTime.UtcNow.Date))), Times.Once);
+
+        notificationService.Verify(x => x.Send(
+            It.Is<SendEmailCommand>(c =>
+                c.RecipientsAddress == command.User.Email
+                && c.TemplateId == emailEnvironmentHelper.SavedSearchEmailNotificationTemplateId
+                && c.Tokens["firstName"] == command.User.FirstName 
+                && c.Tokens["newApprenticeships"] == $"{command.Vacancies.Count.ToString()} new {(command.Vacancies.Count == 1 ? "apprenticeship" : "apprenticeships")}"
+                && c.Tokens["searchAlertDescriptor"] == $"{command.SearchTerm} in {command.Location}"
+                && c.Tokens["showSearchLink"] == "yes"
                 && !string.IsNullOrEmpty(c.Tokens["unsubscribeLink"])
                 && !string.IsNullOrEmpty(c.Tokens["searchUrl"])
                 && !string.IsNullOrEmpty(c.Tokens["searchParams"])
