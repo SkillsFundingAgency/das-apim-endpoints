7aa3a9d5f618a02146aea9f45a08503723bfc40d
ogrwill <will.ogrady@digital.education.gov.uk>
2024-12-24 10:44:39 +0000

    correct namespaces

--- src/LevyTransferMatching/SFA.DAS.LevyTransferMatching.Api/Models/Functions/DeclineAcceptedFundingRequest.cs
+++ src/LevyTransferMatching/SFA.DAS.LevyTransferMatching.Api/Models/Functions/DeclineAcceptedFundingRequest.cs
@@ -1,6 +1,9 @@
-﻿namespace SFA.DAS.LevyTransferMatching.Api.Models.Functions;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.LevyTransferMatching.Api.Models.Functions;
 
 public class DeclineAcceptedFundingRequest
 {
+    [Required]
     public int ApplicationId { get; set; }
 }
--- src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/LevyTransferMatching/GetApplicationsToAutoDeclineRequest.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/LevyTransferMatching/GetApplicationsToAutoDeclineRequest.cs
@@ -1,5 +1,7 @@
 ﻿using SFA.DAS.SharedOuterApi.Interfaces;
 
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.LevyTransferMatching;
+
 public class GetApplicationsToAutoDeclineRequest() : IGetApiRequest
 {
     public string GetUrl => "/applications-auto-decline";
