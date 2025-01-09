ba1e28703524c8e0da351cba8b57426e3b57ec81
ogrwill <will.ogrady@digital.education.gov.uk>
2025-01-09 14:29:19 +0000

    setting template to _dev for testing

--- src/LevyTransferMatching/SFA.DAS.LevyTransferMatching/Application/Commands/SetApplicationOutcome/ReceiverApplicationApprovedEmail.cs
+++ src/LevyTransferMatching/SFA.DAS.LevyTransferMatching/Application/Commands/SetApplicationOutcome/ReceiverApplicationApprovedEmail.cs
@@ -1,29 +1,28 @@
 ﻿using System.Collections.Generic;
 using SFA.DAS.SharedOuterApi.Models.Messages;
 
-namespace SFA.DAS.LevyTransferMatching.Application.Commands.SetApplicationOutcome
+namespace SFA.DAS.LevyTransferMatching.Application.Commands.SetApplicationOutcome;
+
+public class ReceiverApplicationApprovedEmail : EmailTemplateArguments
 {
-    public class ReceiverApplicationApprovedEmail : EmailTemplateArguments
+    public ReceiverApplicationApprovedEmail(
+        string recipientEmail,
+        string employerName,
+        string reference,
+        string baseUrl,
+        string encodedAccountId,
+        string unsubscribeUrl
+        )
     {
-        public ReceiverApplicationApprovedEmail(
-             string recipientEmail,
-            string employerName,
-            string reference,
-            string baseUrl,
-            string encodedAccountId,
-            string unsubscribeUrl
-            )
+        TemplateId = "ReceiverApplicationApproved_dev";
+        RecipientAddress = recipientEmail;
+        Tokens = new Dictionary<string, string>
         {
-            TemplateId = "ReceiverApplicationApproved";
-            RecipientAddress = recipientEmail;
-            Tokens = new Dictionary<string, string>
-            {
-                {"employer", employerName },
-                {"reference", reference },
-                {"base_url", baseUrl },
-                {"encodedAccountId", encodedAccountId },
-                {"unsubscribe_url", unsubscribeUrl }
-            };
-        }
+            {"employer", employerName },
+            {"reference", reference },
+            {"base_url", baseUrl },
+            {"encodedAccountId", encodedAccountId },
+            {"unsubscribe_url", unsubscribeUrl }
+        };
     }
 }
