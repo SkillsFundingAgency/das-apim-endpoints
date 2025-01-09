8bab79735c2acda4376606f2ce9cb9377d406243
ogrwill <will.ogrady@digital.education.gov.uk>
2025-01-09 15:16:36 +0000

    primary constructor

--- src/LevyTransferMatching/SFA.DAS.LevyTransferMatching/Application/Commands/SetApplicationOutcome/ReceiverApplicationApprovedEmailCommandHandler.cs
+++ src/LevyTransferMatching/SFA.DAS.LevyTransferMatching/Application/Commands/SetApplicationOutcome/ReceiverApplicationApprovedEmailCommandHandler.cs
@@ -7,53 +7,44 @@ using SFA.DAS.LevyTransferMatching.Interfaces;
 using SFA.DAS.Notifications.Messages.Commands;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
-namespace SFA.DAS.LevyTransferMatching.Application.Commands.SetApplicationOutcome
+namespace SFA.DAS.LevyTransferMatching.Application.Commands.SetApplicationOutcome;
+
+public class ReceiverApplicationApprovedEmailCommandHandler(
+    IAccountsService accountsService,
+    ILogger<ReceiverApplicationApprovedEmailCommandHandler> logger,
+    INotificationService notificationService)
+    : IRequestHandler<ReceiverApplicationApprovedEmailCommand, Unit>
 {
-    public class ReceiverApplicationApprovedEmailCommandHandler : IRequestHandler<ReceiverApplicationApprovedEmailCommand, Unit>
+    public async Task<Unit> Handle(ReceiverApplicationApprovedEmailCommand request, CancellationToken cancellationToken)
     {
-        private readonly IAccountsService _accountsService;
-        private readonly ILogger<ReceiverApplicationApprovedEmailCommandHandler> _logger;
-        private readonly INotificationService _notificationService;
+        logger.LogInformation("Sending email for application {applicationId} approved to receiver {request.ReceiverId} for Pledge {pledgeId}",
+            request.ApplicationId, request.ReceiverId, request.PledgeId);
 
-        public ReceiverApplicationApprovedEmailCommandHandler(ILogger<ReceiverApplicationApprovedEmailCommandHandler> logger, 
-            INotificationService notificationService, IAccountsService accountsService)
+        var getAccountUsersTask = accountsService.GetAccountUsers(request.ReceiverId);
+        var getAccountTask = accountsService.GetAccount(request.EncodedAccountId);
+
+        await Task.WhenAll(getAccountUsersTask, getAccountTask);
+
+        var accountUsers = await getAccountUsersTask;
+        var users = accountUsers.Where(x => (x.Role == "Owner" || x.Role == "Transactor") && x.CanReceiveNotifications).ToList();
+
+        var account = await getAccountTask;
+
+        foreach (var user in users)
         {
-            _accountsService = accountsService;
-            _logger = logger;
-            _notificationService = notificationService;
+            var email = new ReceiverApplicationApprovedEmail(
+                user.Email,
+                account.DasAccountName,
+                request.EncodedApplicationId,
+                request.BaseUrl,
+                account.EncodedAccountId,
+                request.UnsubscribeNotificationsUrl);
+
+            var command = new SendEmailCommand(email.TemplateId, email.RecipientAddress, email.Tokens);
+
+            await notificationService.Send(command);
         }
 
-        public async Task<Unit> Handle(ReceiverApplicationApprovedEmailCommand request, CancellationToken cancellationToken)
-        {
-            _logger.LogInformation("Sending email for application {applicationId} approved to receiver {request.ReceiverId} for Pledge {pledgeId}", 
-                request.ApplicationId,request.ReceiverId, request.PledgeId);
-
-            var getAccountUsersTask = _accountsService.GetAccountUsers(request.ReceiverId);
-            var getAccountTask =  _accountsService.GetAccount(request.EncodedAccountId);
-
-            await Task.WhenAll(getAccountUsersTask, getAccountTask);
-
-            var accountUsers = await getAccountUsersTask;
-            var users = accountUsers.Where(x => (x.Role == "Owner" || x.Role == "Transactor") && x.CanReceiveNotifications).ToList();
-
-            var account = await getAccountTask;
-
-            foreach (var user in users)
-            {
-                var email = new ReceiverApplicationApprovedEmail(
-                    user.Email, 
-                    account.DasAccountName, 
-                    request.EncodedApplicationId,
-                    request.BaseUrl,
-                    account.EncodedAccountId,
-                    request.UnsubscribeNotificationsUrl);
-
-                var command = new SendEmailCommand(email.TemplateId, email.RecipientAddress, email.Tokens);
-
-                await _notificationService.Send(command);
-            }
-
-            return Unit.Value;
-        }
+        return Unit.Value;
     }
 }
