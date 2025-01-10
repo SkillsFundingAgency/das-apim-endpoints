c4038e28a6702cfae898bf7ae9b6a8068dfa7c73
ogrwill <will.ogrady@digital.education.gov.uk>
2025-01-10 10:24:26 +0000

    updating email tokens for  ApplicationCreatedEmailCommand

--- src/LevyTransferMatching/SFA.DAS.LevyTransferMatching/Application/Commands/CreateApplication/ApplicationCreatedEmail.cs
+++ src/LevyTransferMatching/SFA.DAS.LevyTransferMatching/Application/Commands/CreateApplication/ApplicationCreatedEmail.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections.Generic;
 using SFA.DAS.SharedOuterApi.Models.Messages;
 
-namespace SFA.DAS.LevyTransferMatching.Application.Commands.CreateApplication
+namespace SFA.DAS.LevyTransferMatching.Application.Commands.CreateApplication;
+
+public class ApplicationCreatedEmail : EmailTemplateArguments
 {
-    public class ApplicationCreatedEmail : EmailTemplateArguments
+    public ApplicationCreatedEmail(
+        string recipientEmail,
+        string employer,
+        string reference,
+        string unsubscribeUrl,
+        string templateId)
     {
-        public ApplicationCreatedEmail(
-            string recipientEmail, 
-            string userName,
-            string reference,
-            string templateId)
+        TemplateId = templateId;
+        RecipientAddress = recipientEmail;
+        Tokens = new Dictionary<string, string>
         {
-            TemplateId = templateId;
-            RecipientAddress = recipientEmail;
-            Tokens = new Dictionary<string, string>
-            {
-                {"user_name", userName },
-                {"reference", reference }                
-            };
-        }
+            {"employer", employer },
+            {"reference", reference },
+            {"unsubscribe_url", reference }
+        };
     }
 }
