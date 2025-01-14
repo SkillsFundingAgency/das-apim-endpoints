968b13839d697f07fd62e4d08cfb5b5fe79dfe96
CathyGroom <147840579+CathyGroom@users.noreply.github.com>
2025-01-14 15:41:31 +0000

    AAD-876 get course option
    Fix SC duplicate code issue.

--- src/ApprenticeApp/SFA.DAS.ApprenticeApp/Application/Queries/ApprenticeshipDetails/GetApprenticeshipQueryResult.cs
+++ src/ApprenticeApp/SFA.DAS.ApprenticeApp/Application/Queries/ApprenticeshipDetails/GetApprenticeshipQueryResult.cs
@@ -1,18 +1,9 @@
-﻿namespace SFA.DAS.ApprenticeApp.Application.Queries.ApprenticeshipDetails
+﻿using SFA.DAS.ApprenticeApp.InnerApi.CommitmentsV2.Responses;
+
+namespace SFA.DAS.ApprenticeApp.Application.Queries.ApprenticeshipDetails
 {
-    public class GetApprenticeshipQueryResult
+    public class GetApprenticeshipQueryResult : ApprenticeshipDetailsResponse
     {
-        public long Id { get; set; }
-        public long EmployerAccountId { get; set; }
-        public long AccountLegalEntityId { get; set; }
-        public string EmployerName { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Uln { get; set; }
-        public string StandardUId { get; set; }
-        public string CourseCode { get; set; }
-        public string CourseName { get; set; }
-        public string Option { get; set; }
+        
     }
 }
