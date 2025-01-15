0d9aca9e3f04af7114f1ba07d4e3e5f9e70971cd
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-01-15 11:23:54 +0000

    EC-791-Testcases

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api.UnitTests/Controllers/NotificationsLocations/NotificationsLocationsControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.ApprenticeAan.Api.Controllers;
+using SFA.DAS.ApprenticeAan.Application.NotificationsLocations.Queries;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.ApprenticeAan.Api.UnitTests.Controllers.NotificationsLocations;
+
+public class NotificationsLocationsControllerTests
+{
+    [Test, MoqAutoData]
+    public async Task Get_ReturnsCorrectViewModel(
+        GetNotificationsLocationsQueryResult response,
+        [Frozen] Mock<IMediator> mockMediator,
+        [Greedy] NotificationsLocationsController sut)
+    {
+        mockMediator
+            .Setup(m => m.Send(It.IsAny<GetNotificationsLocationsQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+
+        var result = await sut.Get("exampleSearchTerm");
+
+        result.As<OkObjectResult>().Should().NotBeNull();
+        result.As<OkObjectResult>().Value.Should().BeEquivalentTo(response);
+    }
+}
