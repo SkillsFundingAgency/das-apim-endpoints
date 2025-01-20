f52b77a777a3a692a61997f60487923bbec4dd2e
David Rimar <david.rimar@digital.education.gov.uk>
2025-01-20 10:31:22 +0000

    EC-803 member not settings wip

---
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Application/MemberNotificationSettings/Queries/GetMemberNotificationSettings/GetMemberNotificationSettingsQueryResult.cs
@@ -0,0 +1,16 @@
+﻿namespace SFA.DAS.ApprenticeAan.Application.MemberNotificationSettings.Queries.GetMemberNotificationSettings;
+
+public class GetMemberNotificationSettingsQueryResult
+{
+    public IEnumerable<MemberNotificationEventFormatModel> MemberNotificationEventFormats { get; set; } = Enumerable.Empty<MemberNotificationEventFormatModel>();
+    public IEnumerable<MemberNotificationLocationsModel> MemberNotificationLocations { get; set; } = Enumerable.Empty<MemberNotificationLocationsModel>();
+    public bool? ReceiveMonthlyNotifications { get; set; }
+}
+
+public class MemberNotificationEventFormatModel
+{
+}
+
+public class MemberNotificationLocationsModel
+{
+}
