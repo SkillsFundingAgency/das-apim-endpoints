6d209a48dce1c6cc545f3629b732d244fbec5fee
David Rimar <david.rimar@digital.education.gov.uk>
2025-01-23 10:11:14 +0000

    EC-803 update endpoint

--- src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/MemberNotificationSettingsController.cs
+++ src/ApprenticeAan/SFA.DAS.ApprenticeAan.Api/Controllers/MemberNotificationSettingsController.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeAan.Api.Models.MemberNotificationSettings;
+using SFA.DAS.ApprenticeAan.Application.MemberNotificationSettings.Commands.UpdateNotificationSettings;
 using SFA.DAS.ApprenticeAan.Application.MemberNotificationSettings.Queries.GetMemberNotificationSettings;
 
 namespace SFA.DAS.ApprenticeAan.Api.Controllers;
@@ -19,5 +21,29 @@ public class MemberNotificationSettingsController : ControllerBase
     {
         var memberNotificationSettings = await _mediator.Send(new GetMemberNotificationSettingsQuery(memberId), cancellationToken);
         return Ok(memberNotificationSettings);
+    }
+
+    [HttpPost("{memberId}")]
+    public async Task<IActionResult> Post([FromRoute] Guid memberId, [FromBody] NotificationsSettingsApiRequest request)
+    {
+        await _mediator.Send(new UpdateNotificationSettingsCommand
+        {
+            MemberId = memberId,
+            ReceiveNotifications = request.ReceiveNotifications,
+            EventTypes = request.EventTypes.Select(x => new UpdateNotificationSettingsCommand.NotificationEventType
+            {
+                EventType = x.EventType,
+                ReceiveNotifications = x.ReceiveNotifications
+            }).ToList(),
+            Locations = request.Locations.Select(x => new UpdateNotificationSettingsCommand.Location
+            {
+                Name = x.Name,
+                Radius = x.Radius,
+                Latitude = x.Latitude,
+                Longitude = x.Longitude
+            }).ToList()
+        });
+
+        return Ok();
     }
 }
