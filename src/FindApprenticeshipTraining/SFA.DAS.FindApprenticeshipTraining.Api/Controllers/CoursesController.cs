f9dd9b1eb0abd893db228d2443101b22ce2365a1
shaun-downey-education <shaun.downey@education.gov.uk>
2025-01-27 15:48:00 +0000

    CSP-1763: Levels controller method to return levels from courses api.

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/CoursesController.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/CoursesController.cs
@@ -1,8 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.FindApprenticeshipTraining.Application.Courses;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.FindApprenticeshipTraining.Api.Controllers;
 
-public sealed class CoursesController : ControllerBase
+[ApiController]
+[Route("[controller]/")]
+public sealed class CoursesController(IMediator _mediator) : ControllerBase
 {
-    public CoursesController : 
+    [HttpGet]
+    [Route("routes")]
+    public async Task<IActionResult> GetCourseRoutes()
+    {
+        var result = await _mediator.Send(new GetCourseRoutesQuery());
+        return Ok(result);
+    }
 }
---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Courses/GetCourseRoutesQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Application.Courses;
+
+public sealed class GetCourseRoutesQuery : IRequest<GetRoutesListResponse>;
