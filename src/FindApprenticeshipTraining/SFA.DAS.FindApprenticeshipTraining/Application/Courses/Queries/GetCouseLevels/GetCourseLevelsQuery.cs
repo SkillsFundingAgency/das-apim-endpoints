b3c4a3fe0147e87a07040693b07fe2e48f8372f8
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2025-01-28 14:15:41 +0000

    CSP-1763: FAT Outer API endpoint GET /Courses/routes (#1907)
    * added AgreementNotSigned
    
    * 1. In case of closed vacancy, the response object could not be cast into GetApprenticeshipVacancyItemResponse
    This results in a vacancy item response as null. If the Email tokens are empty, then the email is not sent.
    
    2.  Vacancy service => Separation of Concerns rule violation split this implementation as separate methods GetVacancy from FAA Api & Closed Vacancy from RAA Api
    
    * Included NHS Vacancies count in the search Results
    
    * Update SFA.DAS.FindAnApprenticeship.csproj
    
    * Updated SFA.DAS.VacancyServices.Wage to version 1.0.35
    
    * Saved Search Email alert Across England wording change
    
    * added failed requests alert and updated app connection string
    
    * WIP
    
    * CSP-1763: Levels controller method to return levels from courses api.
    
    ---------
    
    Co-authored-by: Paul Graham <paul.graham@coprime.co.uk>
    Co-authored-by: Balaji Jambulingam <balaji.j@hotmail.co.uk>
    Co-authored-by: Liam Crossley <liam.crossley@education.gov.uk>
    Co-authored-by: Siji Odunlami <sijiodunlami@gmail.com>
    Co-authored-by: Calum Rees <calumrees99@gmail.com>
    Co-authored-by: harsh-dfe <harsha.reddy@education.gov.uk>
    Co-authored-by: Calum Rees <81469431+calumrees99@users.noreply.github.com>
    Co-authored-by: Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Courses/Queries/GetCouseLevels/GetCourseLevelsQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-using SFA.DAS.SharedOuterApi.InnerApi.Responses;
-
-namespace SFA.DAS.FindApprenticeshipTraining.Application.Courses.Queries.GetCourseLevels;
-
-public sealed class GetCourseLevelsQuery : IRequest<GetCourseLevelsListResponse>;
