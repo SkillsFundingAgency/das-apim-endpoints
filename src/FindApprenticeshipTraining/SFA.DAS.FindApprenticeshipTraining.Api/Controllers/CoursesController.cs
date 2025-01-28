4d32665197ddf0728eef0309d8c140de63249684
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2025-01-28 14:05:02 +0000

    CSP-1762: FAT Outer API endpoint -  GET /Courses/levels (#1906)
    * added AgreementNotSigned
    
    * 1. In case of closed vacancy, the response object could not be cast into GetApprenticeshipVacancyItemResponse
    This results in a vacancy item response as null. If the Email tokens are empty, then the email is not sent.
    
    2.  Vacancy service => Separation of Concerns rule violation split this implementation as separate methods GetVacancy from FAA Api & Closed Vacancy from RAA Api
    
    * Included NHS Vacancies count in the search Results
    
    * Update SFA.DAS.FindAnApprenticeship.csproj
    
    * Updated SFA.DAS.VacancyServices.Wage to version 1.0.35
    
    * Saved Search Email alert Across England wording change
    
    * added failed requests alert and updated app connection string
    
    * CSP-1762: Get Course Levels endpoint and associated handler.
    
    * CSP-1762: Update unit test to use Assert.That
    
    * CSP-1762: Pushed unit test update to use fluent assertions to validate cover checker.
    
    ---------
    
    Co-authored-by: Paul Graham <paul.graham@coprime.co.uk>
    Co-authored-by: Balaji Jambulingam <balaji.j@hotmail.co.uk>
    Co-authored-by: Liam Crossley <liam.crossley@education.gov.uk>
    Co-authored-by: Siji Odunlami <sijiodunlami@gmail.com>
    Co-authored-by: Calum Rees <calumrees99@gmail.com>
    Co-authored-by: harsh-dfe <harsha.reddy@education.gov.uk>
    Co-authored-by: Calum Rees <81469431+calumrees99@users.noreply.github.com>
    Co-authored-by: Najam Muhammad <58732568+Najamuddin-Muhammad@users.noreply.github.com>

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/CoursesController.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.FindApprenticeshipTraining.Application.Courses.Queries.GetCourseLevels;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/")]
+public class CoursesController(IMediator _mediator) : ControllerBase
+{
+    [HttpGet]
+    [Route("levels")]
+    public async Task<IActionResult> GetCourseLevels()
+    {
+        var response = await _mediator.Send(new GetCourseLevelsQuery());
+        return Ok(response);
+    }
+}
