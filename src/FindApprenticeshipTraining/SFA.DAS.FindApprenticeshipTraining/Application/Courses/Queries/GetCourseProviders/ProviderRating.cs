7e56f2b8500e75353157c3c76ff5f6596898deb1
MarkFCain <mark.cain@digital.education.gov.uk>
2025-02-06 10:19:35 +0000

    CSP-1864 Get providers from larsCode

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Courses/Queries/GetCourseProviders/ProviderRating.cs
@@ -0,0 +1,10 @@
+﻿namespace SFA.DAS.FindApprenticeshipTraining.Application.Courses.Queries.GetCourseProviders;
+
+public enum ProviderRating
+{
+    NotYetReviewed,
+    Excellent,
+    Good,
+    Poor,
+    VeryPoor
+}
