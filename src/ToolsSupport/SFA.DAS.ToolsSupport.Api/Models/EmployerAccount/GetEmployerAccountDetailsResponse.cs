6adec01665b3bad01eac8858fe5f39af4b7cb38a
ogrwill <will.ogrady@digital.education.gov.uk>
2025-02-07 13:13:24 +0000

    update legalentity response from inner api

--- src/ToolsSupport/SFA.DAS.ToolsSupport.Api/Models/EmployerAccount/GetEmployerAccountDetailsResponse.cs
+++ src/ToolsSupport/SFA.DAS.ToolsSupport.Api/Models/EmployerAccount/GetEmployerAccountDetailsResponse.cs
@@ -1,29 +1,18 @@
 ﻿using SFA.DAS.ToolsSupport.Application.Queries.EmployerAccount.GetEmployerAccountDetails;
-using SFA.DAS.ToolsSupport.InnerApi.Responses;
 
 namespace SFA.DAS.ToolsSupport.Api.Models.EmployerAccount;
 
 public class GetEmployerAccountDetailsResponse
 {
-    public long AccountId { get; set; }
-    public string HashedAccountId { get; set; } = "";
-    public string DasAccountName { get; set; } = "";
-    public DateTime DateRegistered { get; set; }
-    public string OwnerEmail { get; set; } = "";
-    public IEnumerable<LegalEntity> LegalEntities { get; set; } = [];
+    public required GetEmployerAccountDetailsResult Account { get; set; }
 
     public static explicit operator GetEmployerAccountDetailsResponse(GetEmployerAccountDetailsResult source)
     {
-        if (source == null) return new GetEmployerAccountDetailsResponse();
+        if (source == null) return new GetEmployerAccountDetailsResponse { Account = new GetEmployerAccountDetailsResult() };
 
         return new GetEmployerAccountDetailsResponse
         {
-            AccountId = source.AccountId,
-            HashedAccountId = source.HashedAccountId,
-            DasAccountName = source.DasAccountName,
-            OwnerEmail = source.OwnerEmail,
-            DateRegistered = source.DateRegistered,
-            LegalEntities = source.LegalEntities
+            Account = source
         };
     }
 }
--- src/ToolsSupport/SFA.DAS.ToolsSupport/InnerApi/Responses/LegalEntity.cs
+++ src/ToolsSupport/SFA.DAS.ToolsSupport/InnerApi/Responses/LegalEntity.cs
@@ -16,4 +16,5 @@ public class LegalEntity
     public string Status { get; set; } = "";
     public long AccountLegalEntityId { get; set; }
     public string AccountLegalEntityPublicHashedId { get; set; } = "";
+    public string AgreementStatus { get; set; } = "";
 }
