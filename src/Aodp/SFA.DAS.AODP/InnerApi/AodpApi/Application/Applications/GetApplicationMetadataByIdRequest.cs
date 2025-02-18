7b4cb6d750055a273b3c813114186aa0e0685895
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-02-18 20:49:26 +0000

    remove Aodp

--- src/Aodp/SFA.DAS.AODP/InnerApi/AodpApi/Application/Applications/GetApplicationMetadataByIdRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.SharedOuterApi.Interfaces;
-
-public class GetApplicationMetadataByIdRequest : IGetApiRequest
-{
-    public Guid ApplicationId { get; set; }
-
-    public string GetUrl => $"/api/applications/{ApplicationId}/metadata";
-
-}
--- src/Aodp/SFA.DAS.Aodp/Application/Commands/FormBuilder/Questions/CreateQuestionCommandResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace SFA.DAS.Aodp.Application.Commands.FormBuilder.Questions;
-
-public class CreateQuestionCommandResponse 
-{
-    public Guid Id { get; set; }
-
-}
