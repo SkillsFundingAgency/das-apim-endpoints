c84887c341f2a86f11cf23884128fefe274ca630
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-02-19 16:06:49 +0000

    added delete application endpoint

--- src/Aodp/SFA.DAS.Aodp/Application/Queries/Application/Application/GetApplicationMetadataByIdQueryResponse.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/Application/Application/GetApplicationMetadataByIdQueryResponse.cs
@@ -4,5 +4,6 @@ public class GetApplicationMetadataByIdQueryResponse
     public Guid ApplicationId { get; set; }
     public Guid FormVersionId { get; set; }
     public Guid OrganisationId { get; set; }
+    public int Reference { get; set; }
 }
 
