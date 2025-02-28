a424c49c102159695c9825c91664bca641e1d645
Deven Shah <devenshah1974@gmail.com>
2025-02-28 14:14:26 +0000

    CSP-1892 Refactored POST Shortlist endpoint

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/InnerApi/Requests/PostShortlistForUserRequest.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/InnerApi/Requests/PostShortlistForUserRequest.cs
@@ -5,23 +5,22 @@ namespace SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests
 {
     public class PostShortlistForUserRequest : IPostApiRequest
     {
-        public string PostUrl => "api/shortlist";
+        public string PostUrl => "api/shortlists";
         public object Data { get; set; }
     }
 
     public class PostShortlistData
     {
-        public Guid ShortlistUserId { get ; set ; }
-        public float? Latitude { get ; set ; }
-        public float? Longitude { get ; set ; }
-        public int Larscode { get ; set ; }
-        public string LocationDescription { get ; set ; }
-        public int Ukprn { get ; set ; }
-        public string SectorSubjectArea { get ; set ; }
+        public Guid UserId { get; set; }
+        public int Ukprn { get; set; }
+        public int LarsCode { get; set; }
+        public string LocationDescription { get; set; }
+        public float? Latitude { get; set; }
+        public float? Longitude { get; set; }
     }
 
     public class PostShortListResponse
     {
-        public Guid Id { get ; set ; }
+        public Guid ShortlistId { get; set; }
     }
 }
