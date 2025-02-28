150315c149d37ee91c07e2ea1493a33ea69940c7
Deven Shah <devenshah1974@gmail.com>
2025-02-28 14:14:26 +0000

    CSP-1892 Refactored POST Shortlist endpoint

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Shortlist/Commands/WhenCreatingShortlistForUser.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Shortlist/Commands/WhenCreatingShortlistForUser.cs
@@ -1,45 +1,45 @@
 ﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using AutoFixture.NUnit3;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.CreateShortlistForUser;
 using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
-using SFA.DAS.Testing.AutoFixture;
-using System.Threading;
-using System.Threading.Tasks;
-using SFA.DAS.FindApprenticeshipTraining.Configuration;
-using SFA.DAS.FindApprenticeshipTraining.Services;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Application.Shortlist.Commands
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Application.Shortlist.Commands;
+
+public class WhenCreatingShortlistForUser
 {
-    public class WhenCreatingShortlistForUser
+    [Test, MoqAutoData]
+    public async Task Then_Creates_The_Shortlist_From_The_Request_Calling_Shortlist_Api(
+        PostShortListResponse expectedResult,
+        CreateShortlistForUserCommand command,
+        [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> roatpApiClient,
+        CreateShortlistForUserCommandHandler sut)
     {
-        [Test, MoqAutoData]
-        public async Task Then_Creates_The_Shortlist_From_The_Request_Calling_Shortlist_Api(
-            PostShortListResponse apiResponse,
-            CreateShortlistForUserCommand command,
-            [Frozen] Mock<IShortlistApiClient<ShortlistApiConfiguration>> shortlistApiClient,
-            CreateShortlistForUserCommandHandler handler)
-        {
-            //Arrange
-            shortlistApiClient.Setup(x => x
-                    .PostWithResponseCode<PostShortListResponse>(It.IsAny<PostShortlistForUserRequest>(),
-                        It.IsAny<bool>()))
-                .ReturnsAsync(new ApiResponse<PostShortListResponse>(null, HttpStatusCode.Accepted, ""));
-            
-            //Act
-            await handler.Handle(command, CancellationToken.None);
-        
-            //Assert
-            shortlistApiClient.Verify(x => x
-                .PostWithResponseCode<PostShortListResponse>(It.Is<PostShortlistForUserRequest>(c =>
-                    ((PostShortlistData)c.Data).Latitude.Equals(command.Lat)
-                    && ((PostShortlistData)c.Data).Longitude.Equals(command.Lon)
-                    && ((PostShortlistData)c.Data).Ukprn.Equals(command.Ukprn)
-                    && ((PostShortlistData)c.Data).LocationDescription.Equals(command.LocationDescription)
-                    && ((PostShortlistData)c.Data).Larscode.Equals(command.StandardId)
-                    && ((PostShortlistData)c.Data).ShortlistUserId.Equals(command.ShortlistUserId)), false));
-        }
+        //Arrange
+        roatpApiClient
+            .Setup(x => x.PostWithResponseCode<PostShortListResponse>(It.IsAny<PostShortlistForUserRequest>(), It.IsAny<bool>()))
+            .ReturnsAsync(new ApiResponse<PostShortListResponse>(expectedResult, HttpStatusCode.Created, ""));
+
+        //Act
+        PostShortListResponse actualResult = await sut.Handle(command, CancellationToken.None);
+
+        //Assert
+        roatpApiClient.Verify(x => x
+            .PostWithResponseCode<PostShortListResponse>(It.Is<PostShortlistForUserRequest>(c =>
+                ((PostShortlistData)c.Data).Latitude.Equals(command.Lat)
+                && ((PostShortlistData)c.Data).Longitude.Equals(command.Lon)
+                && ((PostShortlistData)c.Data).Ukprn.Equals(command.Ukprn)
+                && ((PostShortlistData)c.Data).LocationDescription.Equals(command.LocationDescription)
+                && ((PostShortlistData)c.Data).LarsCode.Equals(command.StandardId)
+                && ((PostShortlistData)c.Data).UserId.Equals(command.ShortlistUserId)), false));
+        actualResult.Should().BeEquivalentTo(expectedResult);
     }
 }
