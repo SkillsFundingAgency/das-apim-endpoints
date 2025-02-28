cdd1f9942fde24a861b4b2e06b1a336a1ee3e8c8
Deven Shah <devenshah1974@gmail.com>
2025-02-28 14:14:26 +0000

    CSP-1892 Refactored POST Shortlist endpoint

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Shortlist/Commands/CreateShortlistForUser/CreateShortlistForUserCommand.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Shortlist/Commands/CreateShortlistForUser/CreateShortlistForUserCommand.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using MediatR;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
 
 namespace SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.CreateShortlistForUser
 {
-    public class CreateShortlistForUserCommand : IRequest<Unit>
-    {    
-        public float? Lat { get ; set ; }
-        public float? Lon { get ; set ; }
-        public int Ukprn { get ; set ; }
-        public string LocationDescription { get ; set ; }
-        public int StandardId { get ; set ; }
-        public Guid ShortlistUserId { get ; set ; }
+    public class CreateShortlistForUserCommand : IRequest<PostShortListResponse>
+    {
+        public float? Lat { get; set; }
+        public float? Lon { get; set; }
+        public int Ukprn { get; set; }
+        public string LocationDescription { get; set; }
+        public int StandardId { get; set; }
+        public Guid ShortlistUserId { get; set; }
     }
 }
