f694a410b33a25aa2124747f8c1a86000ee2df45
Deven Shah <devenshah1974@gmail.com>
2025-02-28 14:39:02 +0000

    CSP-1896 Refactored Delete a shortlist item

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api.UnitTests/Controllers/Shortlist/WhenCallingDeleteShortlistUserItem.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api.UnitTests/Controllers/Shortlist/WhenCallingDeleteShortlistUserItem.cs
@@ -9,47 +9,22 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.FindApprenticeshipTraining.Api.Controllers;
-using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.DeleteShortlistItemForUser;
+using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.DeleteShortlistItem;
 using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.FindApprenticeshipTraining.Api.UnitTests.Controllers.Shortlist
+namespace SFA.DAS.FindApprenticeshipTraining.Api.UnitTests.Controllers.Shortlist;
+
+public class WhenCallingDeleteShortlistUserItem
 {
-    public class WhenCallingDeleteShortlistUserItem
+    [Test, MoqAutoData]
+    public async Task Then_Deletes_Shortlist_Item_For_User_From_Mediator(
+        Guid id,
+        [Frozen] Mock<IMediator> mockMediator,
+        [Greedy] ShortlistsController controller)
     {
-        [Test, MoqAutoData]
-        public async Task Then_Deletes_Shortlist_Item_For_User_From_Mediator(
-            Guid shortlistUserId,
-            Guid id,
-            [Frozen] Mock<IMediator> mockMediator,
-            [Greedy] ShortlistsController controller)
-        {
-            var controllerResult = await controller.DeleteShortlistItemForUser(id,shortlistUserId) as ObjectResult;
+        var controllerResult = await controller.DeleteShortlistItemForUser(id) as ObjectResult;
 
-            controllerResult!.StatusCode.Should().Be((int)HttpStatusCode.Accepted);
-            mockMediator
-                .Verify(mediator => mediator.Send(
-                    It.Is<DeleteShortlistItemForUserCommand>(command =>
-                        command.UserId == shortlistUserId 
-                        && command.Id == id),
-                    It.IsAny<CancellationToken>()), Times.Once);
-        }
-
-        [Test, MoqAutoData]
-        public async Task And_Exception_Then_Returns_Bad_Request(
-            Guid shortlistUserId,
-            Guid id,
-            [Frozen] Mock<IMediator> mockMediator,
-            [Greedy] ShortlistsController controller)
-        {
-            mockMediator
-                .Setup(mediator => mediator.Send(
-                    It.IsAny<DeleteShortlistItemForUserCommand>(),
-                    It.IsAny<CancellationToken>()))
-                .Throws<InvalidOperationException>();
-
-            var controllerResult = await controller.DeleteShortlistItemForUser(id,shortlistUserId) as StatusCodeResult;
-
-            controllerResult.StatusCode.Should().Be((int)HttpStatusCode.BadRequest);
-        }
+        controllerResult!.StatusCode.Should().Be((int)HttpStatusCode.Accepted);
+        mockMediator.Verify(mediator => mediator.Send(It.Is<DeleteShortlistItemCommand>(command => command.ShortlistId == id), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Shortlist/Commands/DeleteShortlistItem/DeleteShortlistItemCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.DeleteShortlistItem;
+
+public class DeleteShortlistItemCommandHandler(IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> _roatpCourseManagementApiClient) : IRequestHandler<DeleteShortlistItemCommand, Unit>
+{
+    public async Task<Unit> Handle(DeleteShortlistItemCommand request, CancellationToken cancellationToken)
+    {
+        await _roatpCourseManagementApiClient.Delete(new DeleteShortlistItemRequest(request.ShortlistId));
+
+        return Unit.Value;
+    }
+}
