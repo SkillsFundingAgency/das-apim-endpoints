359c566d2f09f4886ec499d585100de662f8695d
Deven Shah <devenshah1974@gmail.com>
2025-03-03 15:46:34 +0000

    CSP-1892 Renames StandardId to LarsCode in shortlist post request

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/ApiRequests/CreateShortListRequest.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/ApiRequests/CreateShortListRequest.cs
@@ -4,11 +4,11 @@ namespace SFA.DAS.FindApprenticeshipTraining.Api.ApiRequests
 {
     public class CreateShortListRequest
     {
-        public Guid ShortlistUserId { get ; set ; }
-        public float? Lat { get ; set ; }
-        public float? Lon { get ; set ; }
-        public int StandardId { get ; set ; }
-        public string LocationDescription { get ; set ; }
-        public int Ukprn { get ; set ; }
+        public Guid ShortlistUserId { get; set; }
+        public float? Lat { get; set; }
+        public float? Lon { get; set; }
+        public int LarsCode { get; set; }
+        public string LocationDescription { get; set; }
+        public int Ukprn { get; set; }
     }
 }
