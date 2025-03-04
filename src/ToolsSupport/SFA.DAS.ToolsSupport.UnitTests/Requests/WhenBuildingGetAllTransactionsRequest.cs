488418036f3dc63489fd7f6154bf5b4ea089af95
ogrwill <will.ogrady@digital.education.gov.uk>
2025-03-04 11:33:55 +0000

    adding request unit tests

---
+++ src/ToolsSupport/SFA.DAS.ToolsSupport.UnitTests/Requests/WhenBuildingGetAllTransactionsRequest.cs
@@ -0,0 +1,15 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using SFA.DAS.ToolsSupport.InnerApi.Requests;
+
+namespace SFA.DAS.ToolsSupport.UnitTests.Requests;
+public class WhenBuildingGetAllTransactionsRequest
+{
+    [Test, AutoData]
+    public void Then_The_Request_Is_Correctly_Build(string accountId, int year, int month)
+    {
+        var actual = new GetAllTransactionsRequest(accountId, year, month);
+
+        actual.GetUrl.Should().Be($"api/accounts/{accountId}/transactions/all-transactions/{year}/{month}");
+    }
+}
