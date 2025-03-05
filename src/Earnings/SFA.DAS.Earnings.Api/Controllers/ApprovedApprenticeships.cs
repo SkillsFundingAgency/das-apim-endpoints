c21faee5d12783d8e4996d81fba7337d4f391cea
Richard Parkins <rgparkins@hotmail.com>
2025-03-05 14:23:19 +0000

    Simple endpoint that returns OK for submission

---
+++ src/Earnings/SFA.DAS.Earnings.Api/Controllers/ApprovedApprenticeships.cs
@@ -0,0 +1,19 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Earnings.Api.LearnerData;
+using SFA.DAS.Earnings.Application.LearnerData.GetLearnerData;
+
+namespace SFA.DAS.Earnings.Api.Controllers;
+
+[ApiController]
+[Route("approvedapprenticeships")]
+public class LearnerDataController(
+    IMediator mediator) : Controller
+{
+    [HttpPost]
+    [Route("/providers/{ukprn}/academicyears/{academicyear}/apprenticeships")]
+    public async  Task<IActionResult> Search(long ukprn, int academicyear)
+    {
+        return Ok();
+    }   
+}
