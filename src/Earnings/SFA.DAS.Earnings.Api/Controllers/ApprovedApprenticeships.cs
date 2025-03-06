e82698771ceb00c2460ccdba2c0c73a8a9b86e23
Richard Parkins <rgparkins@hotmail.com>
2025-03-06 15:11:11 +0000

    PUT not POST and return a reference

--- src/Earnings/SFA.DAS.Earnings.Api/Controllers/ApprovedApprenticeships.cs
+++ src/Earnings/SFA.DAS.Earnings.Api/Controllers/ApprovedApprenticeships.cs
@@ -1,19 +1,16 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SFA.DAS.Earnings.Api.LearnerData;
-using SFA.DAS.Earnings.Application.LearnerData.GetLearnerData;
 
 namespace SFA.DAS.Earnings.Api.Controllers;
 
 [ApiController]
 [Route("approvedapprenticeships")]
-public class ApprovedApprenticeshipsController(
-    IMediator mediator) : Controller
+public class ApprovedApprenticeshipsController : Controller
 {
-    [HttpPost]
+    [HttpPut]
     [Route("/providers/{ukprn}/academicyears/{academicyear}/apprenticeships")]
-    public async  Task<IActionResult> Search(long ukprn, int academicyear)
+    public async Task<IActionResult> Search(long ukprn, int academicyear)
     {
-        return Accepted();
+        return Accepted( new { reference = 1231333 });
     }   
 }
