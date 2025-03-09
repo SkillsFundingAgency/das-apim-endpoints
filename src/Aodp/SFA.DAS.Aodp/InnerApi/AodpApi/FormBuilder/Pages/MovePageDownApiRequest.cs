20d0243be509dd2261bdaf7678edeaa3232e1cae
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-03-09 14:57:57 +0000

    wip

---
+++ src/Aodp/SFA.DAS.Aodp/InnerApi/AodpApi/FormBuilder/Pages/MovePageDownApiRequest.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Aodp.InnerApi.AodpApi.FormBuilder.Pages;
+
+public class MovePageDownApiRequest : IPutApiRequest
+{
+
+    public Guid PageId { get; set; }
+    public Guid FormVersionId { get; set; }
+    public Guid SectionId { get; set; }
+
+    public string PutUrl => $"/api/forms/{FormVersionId}/sections/{SectionId}/Pages/{PageId}/MoveDown";
+    public object Data { get; set; }
+}
