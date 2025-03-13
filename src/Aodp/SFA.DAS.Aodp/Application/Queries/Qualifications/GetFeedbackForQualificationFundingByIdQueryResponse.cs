684a84eeb0b324f4d6ec8f601e78354d0c4adc5f
Ravi Udari <ravi.udari@digital.education.gov.uk>
2025-03-13 15:53:03 +0000

    AWARD-438 Add query and handler for qualification funding feedback

---
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/Qualifications/GetFeedbackForQualificationFundingByIdQueryResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace SFA.DAS.Aodp.Application.Queries.Qualifications
+{
+    public class GetFeedbackForQualificationFundingByIdQueryResponse
+    {
+        public string Status { get; set; }
+        public string? Comments { get; set; }
+
+        public List<Funding> FundedOffers { get; set; } = new();
+
+        public class Funding
+        {
+            public Guid Id { get; set; }
+            public Guid FundingOfferId { get; set; }
+            public string FundedOfferName { get; set; }
+            public DateOnly? StartDate { get; set; }
+            public DateOnly? EndDate { get; set; }
+            public string? Comments { get; set; }
+        }
+
+    }
+}
+
