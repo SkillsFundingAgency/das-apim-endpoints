6c398679cf6931dd4de0c5ed530b30f6ea627bfe
David Rimar <david.rimar@digital.education.gov.uk>
2025-03-14 09:59:52 +0000

    AWARD-426 response amended

--- src/Aodp/SFA.DAS.Aodp/Application/Queries/Application/Application/GetApplicationDetailsByIdQueryResponse.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/Application/Application/GetApplicationDetailsByIdQueryResponse.cs
@@ -3,6 +3,35 @@
 public class GetApplicationDetailsByIdQueryResponse
 {
     public Guid ApplicationId { get; set; }
+    public List<Section> SectionsWithPagesAndQuestionsAndAnswers { get; set; } = new List<Section>();
 
-    // List of Questions with QuestionAnswers
+    public class Section
+    {
+        public Guid Id { get; set; }
+        public int Order { get; set; }
+        public string Title { get; set; }
+        public List<Page> Pages { get; set; } = new List<Page>();
+    }
+
+    public class Page
+    {
+        public Guid Id { get; set; }
+        public int Order { get; set; }
+        public string Title { get; set; }
+        public List<Question> Questions { get; set; } = new List<Question>();
+    }
+
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Type { get; set; }
+        public bool Required { get; set; }
+        public List<QuestionAnswer> QuestionAnswers { get; set; } = new List<QuestionAnswer>();
+    }
+
+    public class QuestionAnswer
+    {
+        public string AnswerText { get; set; }
+    }
 }
