edf1b047c6076714def6c8e68d50160f40398698
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2025-03-14 10:10:11 +0000

    CSP-1914: FAT Outer API: Extend Get /providers (#2029)
    * CSP-1914: Extended providers endpoint to allow querying of main and active provider information.
    
    * CSP-1914: Updated unit test to correctly compare collections.

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Providers/GetRoatpProviders/GetRoatpProvidersQuery.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Application/Providers/GetRoatpProviders/GetRoatpProvidersQuery.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 
 namespace SFA.DAS.FindApprenticeshipTraining.Application.Providers.GetRoatpProviders;
+
 public class GetRoatpProvidersQuery : IRequest<GetRoatpProvidersQueryResult>
 {
+    public bool Live { get; set; } = false;
 }
