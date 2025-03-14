ac15ba8ae9fd80c605e9f13be50aed9ee6748be0
Deven Shah <devenshah1974@gmail.com>
2025-03-14 16:04:41 +0000

    CSP-1927 Implements delete expired shortlist endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ShortlistsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.RoatpCourseManagement.Application.Shortlists.Commands.DeleteExpiredShortlists;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShortlistsController(IMediator _mediator) : ControllerBase
+{
+    [HttpDelete]
+    public async Task<IActionResult> DeleteExpiredShortlist()
+    {
+        await _mediator.Send(new DeleteExpiredShortlistsCommand());
+        return Accepted();
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Shortlists/Command/DeleteExpiredShortlistsCommandHandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.Shortlists.Commands.DeleteExpiredShortlists;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.Shortlists.Command;
+
+public class DeleteExpiredShortlistsCommandHandlerTests
+{
+    [Test]
+    public async Task Handle_WhenHandlingDeleteExpiredShortlistsCommand_ThenShouldCallCourseManagementApiClientDeleteMethod()
+    {
+        // Arrange
+        var courseManagementApiClient = new Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>>();
+        var sut = new DeleteExpiredShortlistsCommandHandler(courseManagementApiClient.Object);
+        var command = new DeleteExpiredShortlistsCommand();
+        // Act
+        await sut.Handle(command, CancellationToken.None);
+        // Assert
+        courseManagementApiClient.Verify(x => x.Delete(It.IsAny<DeleteExpiredShortlistsRequest>()), Times.Once);
+    }
+}
