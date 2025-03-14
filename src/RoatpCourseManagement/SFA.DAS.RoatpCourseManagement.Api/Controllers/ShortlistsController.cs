22bbc07d3df54c01388d44c0c2f34a4509293cd3
Deven Shah <devenshah1974@gmail.com>
2025-03-14 16:04:41 +0000

    CSP-1927 Implements delete expired shortlist endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ShortlistsController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.RoatpCourseManagement.Application.Shortlists.Commands.DeleteExpiredShortlists;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ShortlistsController(IMediator _mediator) : ControllerBase
+{
+    [HttpDelete]
+    [Route("expired")]
+    public async Task<IActionResult> DeleteExpiredShortlist()
+    {
+        await _mediator.Send(new DeleteExpiredShortlistsCommand());
+        return Accepted();
+    }
+}
