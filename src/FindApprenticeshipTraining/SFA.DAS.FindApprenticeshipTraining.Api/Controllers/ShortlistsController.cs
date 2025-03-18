e4cd240209082d17197070f4be60934e8f83be2b
Deven Shah <devenshah1974@gmail.com>
2025-03-18 15:37:42 +0000

    CSP-1927 Implements delete expired shortlist endpoint (#2063)
    * CSP-1927 Deleted get expired shortlist user ids endpoint
    
    * CSP-1927 Extends get shortlist for user response with expiry date
    
    * CSP-1927 Deleted DeleteShortlistForUser endpoint
    
    * CSP-1927 Marked shortlist api client as obsolete
    
    * CSP-1927 Implements delete expired shortlist endpoint
    
    * CSP-1927 Fixed azure client credential helper

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/ShortlistsController.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/ShortlistsController.cs
@@ -5,9 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.FindApprenticeshipTraining.Api.ApiRequests;
 using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.CreateShortlistForUser;
-using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.DeleteShortlistForUser;
 using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Commands.DeleteShortlistItem;
-using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Queries.GetExpiredShortlists;
 using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Queries.GetShortlistCountForUser;
 using SFA.DAS.FindApprenticeshipTraining.Application.Shortlist.Queries.GetShortlistsForUser;
 using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
@@ -63,41 +61,5 @@ public class ShortlistsController(IMediator _mediator, ILogger<ShortlistsControl
     {
         GetShortlistsForUserResponse result = await _mediator.Send(new GetShortlistsForUserQuery { UserId = userId });
         return Ok(result);
-    }
-
-    [HttpGet]
-    [Route("expired")]
-    public async Task<IActionResult> GetExpiredShortlistUserIds([FromQuery] uint expiryInDays)
-    {
-        try
-        {
-            var result = await _mediator.Send(new GetExpiredShortlistsQuery { ExpiryInDays = expiryInDays });
-
-            return Ok(new { result.UserIds });
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error getting list of expired shortlists");
-            return BadRequest();
-        }
-    }
-
-    [HttpDelete]
-    [Route("users/{userId}")]
-    public async Task<IActionResult> DeleteShortlistForUser(Guid userId)
-    {
-        try
-        {
-            await _mediator.Send(new DeleteShortlistForUserCommand
-            {
-                UserId = userId
-            });
-            return Accepted();
-        }
-        catch (Exception e)
-        {
-            _logger.LogError(e, "Error deleting shortlist");
-            return BadRequest();
-        }
     }
 }
