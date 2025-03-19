6fc0582528d29e081cd85522b61768949878d932
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-03-19 22:20:31 +0000

    DASD-12983 - Initial project creation for Recruit Jobs Outer

---
+++ src/RecruitJobs/SFA.DAS.Recruit.Jobs.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Recruit.Jobs.Configuration;
+
+namespace SFA.DAS.Recruit.Api.AppStart
+{
+    public static class AddConfigurationOptionsExtension
+    {
+        public static void AddConfigurationOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions();
+            services.Configure<RecruitJobsConfiguration>(configuration.GetSection(nameof(RecruitJobsConfiguration)));
+            services.AddSingleton(cfg => cfg.GetService<IOptions<RecruitJobsConfiguration>>().Value);
+            
+        }
+    }
+}
---
+++ src/RecruitJobs/SFA.DAS.Recruit.Jobs.Api/Controllers/AccountUsersController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Threading.Tasks;
+using System;
+
+namespace SFA.DAS.Recruit.Jobs.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/")]
+public class ApplicationReviewController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger<ApplicationReviewController> _logger;
+
+    public ApplicationReviewController(IMediator mediator, ILogger<ApplicationReviewController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    [Route("")]
+    public async Task<IActionResult> GetApplicationReview()
+    {
+        return Ok();
+    }
+}
