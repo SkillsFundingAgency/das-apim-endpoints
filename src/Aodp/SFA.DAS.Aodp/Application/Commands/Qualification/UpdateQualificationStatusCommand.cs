71c84588ef7dd0ff48820f02ef578f283f07aeff
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-03-19 09:44:38 +0000

    Made status change endpoint use process status change Id.

--- src/Aodp/SFA.DAS.Aodp/Application/Commands/Qualification/UpdateQualificationStatusCommand.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Commands/Qualification/UpdateQualificationStatusCommand.cs
@@ -5,5 +5,5 @@ namespace SFA.DAS.AODP.Application.Commands.Qualification;
 public class UpdateQualificationStatusCommand : IRequest<BaseMediatrResponse<EmptyResponse>>
 {
     public string QualificationReference { get; set; } = string.Empty;
-    public string Status { get; set; } = string.Empty;
+    public Guid ProcessStatusId { get; set; }
 }
