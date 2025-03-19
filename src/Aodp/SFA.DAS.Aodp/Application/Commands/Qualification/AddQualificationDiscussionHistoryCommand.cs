d752e4964d41db56dc68e5f572a37b7538b005d7
JSON Alexander <61407072+jasonalexander-ja@users.noreply.github.com>
2025-03-19 10:30:57 +0000

    Removed ActionTypeId from AddQualificationDiscussionHistoryCommand

--- src/Aodp/SFA.DAS.Aodp/Application/Commands/Qualification/AddQualificationDiscussionHistoryCommand.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Commands/Qualification/AddQualificationDiscussionHistoryCommand.cs
@@ -5,7 +5,6 @@ namespace SFA.DAS.Aodp.Application.Commands.Qualification;
 public class AddQualificationDiscussionHistoryCommand : IRequest<BaseMediatrResponse<EmptyResponse>>
 {
     public string QualificationReference { get; set; } = string.Empty;
-    public Guid ActionTypeId { get; set; }
     public string? UserDisplayName { get; set; }
     public string? Notes { get; set; }
 }
