384e40f932a99f134a2764b441211a72f6ebdec6
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-03-26 16:35:06 +0000

    add missing files (#2095)

---
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/FormBuilder/Forms/GetAllFormVersionsQueryResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace SFA.DAS.Aodp.Application.Queries.FormBuilder.Forms;
+
+public class GetAllFormVersionsQueryResponse
+{
+    public List<FormVersion> Data { get; set; }
+
+    public class FormVersion
+    {
+        public Guid Id { get; set; }
+        public Guid FormId { get; set; }
+        public string Title { get; set; }
+        public DateTime Version { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/FormBuilder/Sections/GetAllSectionsQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace SFA.DAS.Aodp.Application.Queries.FormBuilder.Sections;
+
+public class GetAllSectionsQuery : IRequest<BaseMediatrResponse<GetAllSectionsQueryResponse>>
+{
+    public readonly Guid FormVersionId;
+
+    public GetAllSectionsQuery(Guid formVersionId)
+    {
+        FormVersionId = formVersionId;
+    }
+}
