2a84d0a49866b5b6bd9de7538d1a6c29c21358eb
Karen Hanna <karen@laveryhanna.com>
2025-03-27 16:04:03 +0000

    register IAzureClientCredentialHelper as singleton

--- src/EpaoRegister/SFA.DAS.EpaoRegister.Api/AppStart/AddServiceRegistrationExtensions.cs
+++ src/EpaoRegister/SFA.DAS.EpaoRegister.Api/AppStart/AddServiceRegistrationExtensions.cs
@@ -14,7 +14,7 @@ namespace SFA.DAS.EpaoRegister.Api.AppStart
         public static void AddServiceRegistration(this IServiceCollection services)
         {
             services.AddHttpClient();
-            services.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
+            services.AddSingleton<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
 
             services.AddTransient(typeof(IInternalApiClient<>), typeof(InternalApiClient<>));
             services.AddTransient<ICoursesApiClient<CoursesApiConfiguration>, CourseApiClient>();
