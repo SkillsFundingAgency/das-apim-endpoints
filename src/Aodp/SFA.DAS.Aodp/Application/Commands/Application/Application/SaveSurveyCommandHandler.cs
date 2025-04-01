f612317c6ba210fc2731ec0654e4842a2e3a942c
Ravi Udari <ravi.udari@digital.education.gov.uk>
2025-04-01 12:13:45 +0100

    AWARD-669 survey save end point

---
+++ src/Aodp/SFA.DAS.Aodp/Application/Commands/Application/Application/SaveSurveyCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Aodp.Application.Commands.Application.Application
+{
+    public class SaveSurveyCommandHandler : IRequestHandler<SaveSurveyCommand, BaseMediatrResponse<EmptyResponse>>
+    {
+        private readonly IAodpApiClient<AodpApiConfiguration> _apiClient;
+
+
+        public SaveSurveyCommandHandler(IAodpApiClient<AodpApiConfiguration> apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<BaseMediatrResponse<EmptyResponse>> Handle(SaveSurveyCommand request, CancellationToken cancellationToken)
+        {
+            var response = new BaseMediatrResponse<EmptyResponse>()
+            {
+                Success = false
+            };
+
+            try
+            {
+                var result = await _apiClient.PostWithResponseCode<EmptyResponse>(new SaveSurveyApiRequest()
+                {
+                    Data = request
+                });
+                response.Success = true;
+            }
+            catch (Exception ex)
+            {
+                response.ErrorMessage = ex.Message;
+                response.Success = false;
+            }
+
+            return response;
+        }
+    }
+
+}
