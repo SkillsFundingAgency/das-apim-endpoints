dbd2a43d3868a11556737b65725168a021912a7d
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2025-04-04 17:34:30 +0100

    AWARD-652 qualificationId is missing fix (#2127)
    AWARD-652 qualificationId is missing fix (#2127)

--- src/Aodp/SFA.DAS.Aodp/Application/Commands/Application/Qualifications/CreateQualificationDiscussionHistoryCommand.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Commands/Application/Qualifications/CreateQualificationDiscussionHistoryCommand.cs
@@ -5,6 +5,7 @@ namespace SFA.DAS.Aodp.Application.Commands.Application.Qualifications
     public class CreateQualificationDiscussionHistoryCommand : IRequest<BaseMediatrResponse<EmptyResponse>>
     {
         public Guid QualificationVersionId { get; set; }
+        public Guid QualificationId { get; set; }
         public string? QualificationReference { get; set; }
         public Guid ActionTypeId { get; set; }
         public string? UserDisplayName { get; set; }
