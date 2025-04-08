7b7aa9029608e1e26d04a44178dd4ada9a3d4c54
FaisalAshraf-dfe <faisal.ashraf@education.gov.uk>
2025-04-08 15:50:44 +0100

    Changes for changed qualification details timeline. (#2122)
    Co-authored-by: Robert Rybnikar <robert.rybnikar@education.gov.uk>

---
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/Qualifications/GetQualificationDetailWithVersionsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using SFA.DAS.Aodp.Application.Queries.Qualifications;
+
+namespace SFA.DAS.AODP.Application.Queries.Qualifications
+{
+    public class GetQualificationDetailWithVersionsQuery : IRequest<BaseMediatrResponse<GetQualificationDetailsQueryResponse>>
+    {
+        public string QualificationReference { get; set; }
+    }
+}
