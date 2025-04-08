a30411b17a3e9cf4a1e8f359238b318a93f4a300
Pooja Gaurav <pooja.gaurav@fujitsuuki.onmicrosoft.com>
2025-04-08 11:04:57 +0100

    Removed the QualificationDiscussionHistory class as there is a separate class

--- src/Aodp/SFA.DAS.Aodp/Application/Queries/Qualifications/GetQualificationDetailsQueryResponse.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/Qualifications/GetQualificationDetailsQueryResponse.cs
@@ -93,24 +93,7 @@ public class GetQualificationDetailsQueryResponse
         public Guid Id { get; set; }
         public string Qan { get; set; } = null!;
         public string? QualificationName { get; set; }
-    }
-
-    public partial class QualificationDiscussionHistory
-    {
-        public Guid Id { get; set; }
-        public Guid QualificationId { get; set; }
-        public Guid ActionTypeId { get; set; }
-        public string? UserDisplayName { get; set; }
-        public string? Notes { get; set; }
-        public DateTime? Timestamp { get; set; }
-        public virtual ActionType ActionType { get; set; } = null!;
-    }
-
-    public class ActionType
-    {
-        public Guid Id { get; set; }
-        public string? Description { get; set; }
-    }
+    }  
 
     public partial class ProcessStatus
     {
