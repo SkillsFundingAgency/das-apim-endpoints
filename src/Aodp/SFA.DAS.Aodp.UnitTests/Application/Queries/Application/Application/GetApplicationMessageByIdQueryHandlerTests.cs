c66d939bdc72c4622e51c985ce3fb573107c8b63
HamzaIbraheem-DFE <hamza.ibraheem@education.gov.uk>
2025-04-10 16:24:27 +0100

    get msg by id endpoint (#2144)

--- src/Aodp/SFA.DAS.Aodp.UnitTests/Application/Queries/Application/Application/GetApplicationMessageByIdQueryHandlerTests.cs
+++ src/Aodp/SFA.DAS.Aodp.UnitTests/Application/Queries/Application/Application/GetApplicationMessageByIdQueryHandlerTests.cs
@@ -1,41 +1,42 @@
 ﻿using AutoFixture;
 using AutoFixture.AutoMoq;
 using Moq;
+using SFA.DAS.Aodp.Application.Queries.Application.Application;
+using SFA.DAS.Aodp.InnerApi.AodpApi.Application.Messages;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.Aodp.UnitTests.Application.Queries.Application.Application
 {
     [TestFixture]
-    public class GetApplicationsByOrganisationIdQueryHandlerTests
+    public class GetApplicationMessageByIdQueryHandlerTests
     {
         private IFixture _fixture;
         private Mock<IAodpApiClient<AodpApiConfiguration>> _apiClientMock;
-        private GetApplicationsByOrganisationIdQueryHandler _handler;
+        private GetApplicationMessageByIdQueryHandler _handler;
 
         [SetUp]
         public void SetUp()
         {
             _fixture = new Fixture().Customize(new AutoMoqCustomization());
             _apiClientMock = _fixture.Freeze<Mock<IAodpApiClient<AodpApiConfiguration>>>();
-            _handler = _fixture.Create<GetApplicationsByOrganisationIdQueryHandler>();
+            _handler = _fixture.Create<GetApplicationMessageByIdQueryHandler>();
         }
 
         [Test]
-        public async Task Then_The_Api_Is_Called_With_The_Request_And_OrganisationId_Is_Returned()
+        public async Task Then_The_Api_Is_Called_With_The_Request_And_Message_Is_Returned()
         {
             // Arrange
-            var query = _fixture.Create<GetApplicationsByOrganisationIdQuery>();
-            var response = _fixture.Create<GetApplicationsByOrganisationIdQueryResponse>();
+            var query = _fixture.Create<GetApplicationMessageByIdQuery>();
+            var response = _fixture.Create<GetApplicationMessageByIdQueryResponse>();
 
-            _apiClientMock.Setup(x => x.Get<GetApplicationsByOrganisationIdQueryResponse>(It.IsAny<GetApplicationsByOrganisationIdApiRequest>()))
+            _apiClientMock.Setup(x => x.Get<GetApplicationMessageByIdQueryResponse>(It.IsAny<GetApplicationMessageByIdApiRequest>()))
                           .ReturnsAsync(response);
-
             // Act
             var result = await _handler.Handle(query, CancellationToken.None);
 
             // Assert
-            _apiClientMock.Verify(x => x.Get<GetApplicationsByOrganisationIdQueryResponse>(It.IsAny<GetApplicationsByOrganisationIdApiRequest>()), Times.Once);
+            _apiClientMock.Verify(x => x.Get<GetApplicationMessageByIdQueryResponse>(It.IsAny<GetApplicationMessageByIdApiRequest>()), Times.Once);
 
             Assert.That(result.Success, Is.True);
             Assert.That(result.Value, Is.EqualTo(response));
@@ -45,10 +46,10 @@ namespace SFA.DAS.Aodp.UnitTests.Application.Queries.Application.Application
         public async Task Then_The_Api_Is_Called_With_The_Request_And_Failure_Is_Returned()
         {
             // Arrange
-            var query = _fixture.Create<GetApplicationsByOrganisationIdQuery>();
+            var query = _fixture.Create<GetApplicationMessageByIdQuery>();
             var exception = _fixture.Create<Exception>();
 
-            _apiClientMock.Setup(x => x.Get<GetApplicationsByOrganisationIdQueryResponse>(It.IsAny<GetApplicationsByOrganisationIdApiRequest>()))
+            _apiClientMock.Setup(x => x.Get<GetApplicationMessageByIdQueryResponse>(It.IsAny<GetApplicationMessageByIdApiRequest>()))
                           .ThrowsAsync(exception);
 
             // Act
@@ -60,3 +61,4 @@ namespace SFA.DAS.Aodp.UnitTests.Application.Queries.Application.Application
         }
     }
 }
+
