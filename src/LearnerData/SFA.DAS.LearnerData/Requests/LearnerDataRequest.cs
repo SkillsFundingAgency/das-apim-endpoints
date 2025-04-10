c0cead04c047d08114481b1f32f83c5f88949613
Paul Graham <paul.graham@coprime.co.uk>
2025-04-10 08:02:46 +0100

    made fields optional

--- src/LearnerData/SFA.DAS.LearnerData/Requests/LearnerDataRequest.cs
+++ src/LearnerData/SFA.DAS.LearnerData/Requests/LearnerDataRequest.cs
@@ -6,14 +6,14 @@ public class LearnerDataRequest
     public long UKPRN { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public string Email { get; set; }
+    public string? Email { get; set; }
     public DateTime DoB { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime PlannedEndDate { get; set; }
     public int PercentageLearningToBeDelivered { get; set; }
     public int EpaoPrice { get; set; }
     public int TrainingPrice { get; set; }
-    public string AgreementId { get; set; }
+    public string? AgreementId { get; set; }
     public bool IsFlexJob { get; set; }
     public int PlannedOTJTrainingHours { get; set; }
     public int StandardCode { get; set; }
