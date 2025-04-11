cd42bdd667234cf6f91049e3cd603fd719d60942
Paul Graham <paul.graham@coprime.co.uk>
2025-04-11 13:41:09 +0100

    WIP

---
+++ src/Approvals/SFA.DAS.Approvals/Application/Learners/Queries/GetLearnersForProviderQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using SFA.DAS.Approvals.InnerApi.CommitmentsV2Api.Requests;
+using SFA.DAS.Approvals.InnerApi.CommitmentsV2Api.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Approvals.Application.Apprentices.Queries.GetApprenticeshipsCSV
+{
+    public class GetLearnersForProviderQueryHandler(
+        ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> apiClient, IMapper mapper)
+        : IRequestHandler<GetApprenticeshipsCSVQuery, GetApprenticeshipsCSVQueryResult>
+    {
+        public async Task<GetApprenticeshipsCSVQueryResult> Handle(GetApprenticeshipsCSVQuery request, CancellationToken cancellationToken)
+        {
+            var apprenticeshipResponse = await apiClient.GetWithResponseCode<GetApprenticeshipsResponse>(
+                new GetApprenticeshipsCSVRequest(
+                    request.ProviderId,
+                    request.SearchTerm,
+                    request.EmployerName,
+                    request.CourseName,
+                    request.Status,
+                    request.StartDate,
+                    request.EndDate,
+                    request.Alert,
+                    request.ApprenticeConfirmationStatus,
+                    request.DeliveryModel
+                    ));
+
+            if (apprenticeshipResponse.StatusCode == HttpStatusCode.NotFound)
+            {
+                return null;
+            }
+
+            return mapper.Map<GetApprenticeshipsCSVQueryResult>(apprenticeshipResponse.Body);
+        }
+    }
+}
---
+++ src/Approvals/SFA.DAS.Approvals/Application/Learners/Queries/GetLearnersForProviderQueryResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SFA.DAS.Approvals.Application.Learners.Queries;
+
+public class GetLearnersForProviderQueryResult
+{
+    public DateTime LastSubmissionDate { get; set; }
+    public int Total { get; set; }
+    public int Page { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public long AccountLegalEntityId { get; set; }
+    public string EmployerName { get; set; }
+    public List<IlrLearnerSummary> Learners { get; set; }
+}
+
+public class IlrLearnerSummary
+{
+    public long Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public long Uln { get; set; }
+    public string Course { get; set; }
+}
