73147c654704ab27fba818c860c374e25f0a850c
shaun-downey-education <148352239+shaun-downey-education@users.noreply.github.com>
2025-04-14 10:41:49 +0100

    CSP-1909 Refactor course provider details endpoint (#2142)
    * CSP-1909: Added accessor service integration, Added GetCourseProviderDetails endpoint and removed legacy endpoint.
    
    * CSP-1909: Remove references to conflicting interface.
    
    * CSP-1909: Update unit tests with new interface reference.
    
    * CSP-1909: Clean up some sonar cloud complaints.
    
    * CSP-1909: Encode location url parameter.

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/InnerApi/Responses/ProviderCourseResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses;
+
+public sealed class ProviderCourseResponse
+{
+    public string CourseName { get; set; }
+    public int Level { get; set; }
+    public int LarsCode { get; set; }
+    public string IfateReferenceNumber { get; set; }
+}
