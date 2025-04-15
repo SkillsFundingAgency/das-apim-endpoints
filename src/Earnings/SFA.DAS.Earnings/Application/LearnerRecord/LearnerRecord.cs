33bacb3f0515f6b4bb7723d9dd9fba28f13e1aaf
Richard Parkins <rgparkins@hotmail.com>
2025-04-15 12:47:27 +0100

    update field names and nullable parameters

--- src/Earnings/SFA.DAS.Earnings/Application/LearnerRecord/LearnerRecord.cs
+++ src/Earnings/SFA.DAS.Earnings/Application/LearnerRecord/LearnerRecord.cs
@@ -8,7 +8,7 @@ public class LearnerRecord
     public DateTime DateOfBirth { get; set; }
     public string? LearnerEmail { get; set; }
     public long Ukprn { get; set; }
-    public int PriorLearningPercent { get; set; }
+    public int? PercentageLearningToBeDelivered { get; set; }
     public int StandardCode { get; set; }
     public int TrainingPrice { get; set; }
     public int EpaoPrice { get; set; }
@@ -16,6 +16,6 @@ public class LearnerRecord
     public DateTime PlannedEndDate { get; set; }
     public string? AgreementId { get; set; }
     public bool IsFlexiJob { get; set; }
-    public int PlannedOTJTrainingHours { get; set; }
+    public int? PlannedOTJTrainingHours { get; set; }
     public int ConsumerReference { get; set; }
 }
