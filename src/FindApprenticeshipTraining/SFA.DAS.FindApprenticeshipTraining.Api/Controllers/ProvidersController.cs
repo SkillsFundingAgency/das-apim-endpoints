b477901a8021364208b3b31d26f506a037496133
Deven Shah <devenshah1974@gmail.com>
2025-04-15 13:14:38 +0100

    CSP-2032 Fixes providers endpoint to get live providers only (#2151)

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/ProvidersController.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.Api/Controllers/ProvidersController.cs
@@ -1,10 +1,10 @@
-﻿using MediatR;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.FindApprenticeshipTraining.Application.Providers.GetProviderSummary;
 using SFA.DAS.FindApprenticeshipTraining.Application.Providers.GetRoatpProviders;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.FindApprenticeshipTraining.Api.Controllers;
 
@@ -15,9 +15,9 @@ public class ProvidersController(IMediator _mediator) : ControllerBase
     [HttpGet]
     [Produces("application/json")]
     [ProducesResponseType(typeof(GetRoatpProvidersQueryResult), StatusCodes.Status200OK)]
-    public async Task<IActionResult> GetProviders([FromQuery] bool? Live, CancellationToken cancellationToken)
+    public async Task<IActionResult> GetProviders(CancellationToken cancellationToken)
     {
-        return Ok(await _mediator.Send(new GetRoatpProvidersQuery() { Live = Live ?? false }, cancellationToken));
+        return Ok(await _mediator.Send(new GetRoatpProvidersQuery(), cancellationToken));
     }
 
     [HttpGet]
