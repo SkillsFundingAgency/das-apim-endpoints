5ff38d74bcb23e60db6a3995da9fd3ae43d96add
Paul Graham <paul.graham@coprime.co.uk>
2025-04-16 09:02:22 +0100

    Added tests to controller

--- src/Approvals/SFA.DAS.Approvals.Api.UnitTests/Controllers/SelectFundingOptions/WhenGettingSelectFundingOptions.cs
+++ src/Approvals/SFA.DAS.Approvals.Api.UnitTests/Controllers/SelectFundingOptions/WhenGettingSelectFundingOptions.cs
@@ -9,48 +9,67 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
 using SFA.DAS.Approvals.Api.Controllers;
-using SFA.DAS.Approvals.Application.SelectFunding.Queries;
-using SFA.DAS.Approvals.Application.SelectProvider.Queries;
+using SFA.DAS.Approvals.Application.Learners.Queries;
 using SFA.DAS.Testing.AutoFixture;
 
 namespace SFA.DAS.Approvals.Api.UnitTests.Controllers.SelectFundingOptions;
 
-public class WhenGettingSelectFundingOptions
+public class WhenGettingLearnersForProvider
 {
     [Test, MoqAutoData]
-    public async Task Then_Get_Returns_FundingOptionsForAccount_From_Mediator(
-                long accountId,
-                GetSelectFundingOptionsQueryResult mediatorResult,
-                [Frozen] Mock<IMediator> mockMediator,
-                [Greedy] SelectFundingOptionsController controller)
+    public async Task Then_Get_Returns_Learners_From_Mediator(
+        long providerId,
+        long accountLegalEntityId,
+        string searchTerm,
+        string sortColumn,
+        bool sortDescending,
+        int page,
+        GetLearnersForProviderQueryResult mediatorResult,
+        [Frozen] Mock<IMediator> mockMediator,
+        [Greedy] SelectIlrLearnersController controller)
     {
         mockMediator.Setup(mediator => mediator.Send(
-                It.Is<GetSelectFundingOptionsQuery>(x => x.AccountId == accountId),
+                It.IsAny<GetLearnersForProviderQuery>(),
                 It.IsAny<CancellationToken>()))
+            .Callback((object o, CancellationToken ct) =>
+            {
+                var q = (GetLearnersForProviderQuery) o;
+                q.ProviderId.Should().Be(providerId);
+                q.AccountLegalEntityId.Should().Be(accountLegalEntityId);
+                q.SearchTerm.Should().Be(searchTerm);
+                q.SortField.Should().Be(sortColumn);
+                q.SortDescending.Should().Be(sortDescending);
+                q.Page.Should().Be(page);
+            })
             .ReturnsAsync(mediatorResult);
 
-        var controllerResult = await controller.Get(accountId) as ObjectResult;
+        var controllerResult = await controller.Get(providerId, accountLegalEntityId, searchTerm, sortColumn, sortDescending, page) as ObjectResult;
 
         controllerResult.Should().NotBeNull();
         controllerResult.StatusCode.Should().Be((int)HttpStatusCode.OK);
-        var model = controllerResult.Value as GetSelectFundingOptionsQueryResult;
+        var model = controllerResult.Value as GetLearnersForProviderQueryResult;
         Assert.That(model, Is.Not.Null);
         model.Should().BeEquivalentTo(mediatorResult);
     }
 
     [Test, MoqAutoData]
-    public async Task And_Exception_Then_Returns_Bad_Request(
-        long accountId,
+    public async Task And_Exception_Then_Returns_InternalServerError(
+        long providerId,
+        long accountLegalEntityId,
+        string searchTerm,
+        string sortColumn,
+        bool sortDescending,
+        int page,
+        GetLearnersForProviderQueryResult mediatorResult,
         [Frozen] Mock<IMediator> mockMediator,
-        [Greedy] SelectFundingOptionsController controller)
+        [Greedy] SelectIlrLearnersController controller)
     {
-        mockMediator
-            .Setup(mediator => mediator.Send(
-                It.IsAny<GetSelectFundingOptionsQuery>(),
+        mockMediator.Setup(mediator => mediator.Send(
+                It.IsAny<GetLearnersForProviderQuery>(),
                 It.IsAny<CancellationToken>()))
             .Throws<InvalidOperationException>();
 
-        var controllerResult = await controller.Get(accountId) as StatusCodeResult;
+        var controllerResult = await controller.Get(providerId, accountLegalEntityId, searchTerm, sortColumn, sortDescending, page) as StatusCodeResult;
 
         controllerResult.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
     }
