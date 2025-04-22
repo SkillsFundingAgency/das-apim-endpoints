ac9a3fd654a8f7e7462b66ae7512c43ecd86c8d2
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-04-22 20:02:00 +0100

    FAI-2428 - Update config name

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/AppStart/AddConfigurationOptionsExtension.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship.Api/AppStart/AddConfigurationOptionsExtension.cs
@@ -26,7 +26,7 @@ namespace SFA.DAS.FindAnApprenticeship.Api.AppStart
             services.AddSingleton(cfg => cfg.GetService<IOptions<AzureActiveDirectoryConfiguration>>().Value);
             services.Configure<RecruitApiConfiguration>(configuration.GetSection(nameof(RecruitApiConfiguration)));
             services.AddSingleton(cfg => cfg.GetService<IOptions<RecruitApiConfiguration>>().Value);
-            services.Configure<RecruitApiV2Configuration>(configuration.GetSection(nameof(RecruitApiV2Configuration)));
+            services.Configure<RecruitApiV2Configuration>(configuration.GetSection("RecruitAltApiConfiguration"));
             services.AddSingleton(cfg => cfg.GetService<IOptions<RecruitApiV2Configuration>>().Value);
 
         }
