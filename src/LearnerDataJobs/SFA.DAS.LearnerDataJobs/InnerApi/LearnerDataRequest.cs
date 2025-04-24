b1af3a0cbfe6eb1f8872b64ff0b3458b5cef753c
Paul Graham <paul.graham@coprime.co.uk>
2025-04-24 11:17:50 +0100

    corrected field name

--- src/LearnerDataJobs/SFA.DAS.LearnerDataJobs/InnerApi/LearnerDataRequest.cs
+++ src/LearnerDataJobs/SFA.DAS.LearnerDataJobs/InnerApi/LearnerDataRequest.cs
@@ -10,7 +10,7 @@ public class LearnerDataRequest
     public DateTime DoB { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime PlannedEndDate { get; set; }
-    public int PriorLearningPercentage { get; set; }
+    public int? PercentageLearningToBeDelivered { get; set; }
     public int EpaoPrice { get; set; }
     public int TrainingPrice { get; set; }
     public string? AgreementId { get; set; }
