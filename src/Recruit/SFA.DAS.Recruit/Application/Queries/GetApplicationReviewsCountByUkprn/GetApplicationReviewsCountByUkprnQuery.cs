5b7be11eb9a9bcc9e7b45e60db89d67612f3e008
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-04-25 13:44:53 +0100

    ✨ Refactor application reviews count handling
    - Replace result classes with direct use of List<ApplicationReviewStats>.
    - Update tests to expect List<ApplicationReviewStats> instead of result classes.
    - Modify query classes to return List<ApplicationReviewStats>.
    - Update query handlers to return lists directly from API responses.
    - Enhance ApplicationReviewStats with additional properties for better tracking.
    
    Changes made by Balaji Jambulingam

--- src/Recruit/SFA.DAS.Recruit/Application/Queries/GetApplicationReviewsCountByUkprn/GetApplicationReviewsCountByUkprnQuery.cs
+++ src/Recruit/SFA.DAS.Recruit/Application/Queries/GetApplicationReviewsCountByUkprn/GetApplicationReviewsCountByUkprnQuery.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
 using MediatR;
+using SFA.DAS.Recruit.InnerApi.Responses;
 
 namespace SFA.DAS.Recruit.Application.Queries.GetApplicationReviewsCountByUkprn
 {
     public record GetApplicationReviewsCountByUkprnQuery(int Ukprn, List<long> VacancyReferences)
-        : IRequest<GetApplicationReviewsCountByUkprnQueryResult>;
+        : IRequest<List<ApplicationReviewStats>>;
 }
--- src/Recruit/SFA.DAS.Recruit/InnerApi/Responses/ApplicationReviewStats.cs
+++ src/Recruit/SFA.DAS.Recruit/InnerApi/Responses/ApplicationReviewStats.cs
@@ -3,9 +3,10 @@
     public record ApplicationReviewStats
     {
         public long VacancyReference { get; set; }
+        public int Applications { get; set; }
         public int NewApplications { get; set; }
+        public int SharedApplications { get; set; }
         public int SuccessfulApplications { get; set; }
         public int UnsuccessfulApplications { get; set; }
-        public int Applications { get; set; }
     }
 }
