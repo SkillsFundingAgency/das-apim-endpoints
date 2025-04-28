8ec6701abb4164f9881f14d9119505e6f7caa8ba
Paul Graham <paul.graham@coprime.co.uk>
2025-04-28 16:58:38 +0100

    APPMAN-1591 add new fields to Jobs outer API (#2146)
    * Added LearnerDataJobs Outer API folder and pipeline file
    
    * Changed platform building back to 2.2.19
    
    * check build after basic API
    
    * re-arranged outer api
    
    * add files to make website run swagger
    
    * update desc
    
    * added add learner endpoint
    
    * added unit tests
    
    * added tests
    
    * removed commented out code
    
    * fix route and add perorites
    
    * fix tests
    
    * fix inner API wireup auth corectly
    
    * keep agreementId as string
    
    * fix cong name of inner API
    
    * make some fields optional
    
    * corect call to inner API
    
    * added consumerReference to request
    
    * fix response
    
    * corrected field name
    
    * correct Recieved Date
    
    * corrected field name
    
    ---------
    
    Co-authored-by: debrr07 <rdebrayeng@hotmail.com>

---
+++ src/LearnerDataJobs/SFA.DAS.LearnerDataJobs/InnerApi/PutLearnerDataRequest.cs
@@ -0,0 +1,9 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.LearnerDataJobs.InnerApi;
+
+public class PutLearnerDataRequest(long providerId, long uln, int academicYear, int standardCode, LearnerDataRequest data) : IPutApiRequest
+{
+    public string PutUrl => $"providers/{providerId}/learners/{uln}/academicyears/{academicYear}/standardcodes/{standardCode}";
+    public object Data { get; set; } = data;
+}
