74b955bdd008e43154d2a56d594ec4576b158d17
Paul Graham <paul.graham@coprime.co.uk>
2025-04-29 19:31:10 +0100

    Revert "Merge branch 'APPMAN-1555-add-paging' into APPMAN-1567-non-levy-selection-and-populating-data"
    This reverts commit fe9580e5edeca88dd1804bdb61fae24317558b9f, reversing
    changes made to f69ea3672894c9676983dba4484552e53b84b2ca.

--- src/LearnerDataJobs/SFA.DAS.LearnerDataJobs/InnerApi/LearnerDataInnerApiConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.SharedOuterApi.Interfaces;
-
-namespace SFA.DAS.LearnerDataJobs.InnerApi;
-
-public class LearnerDataInnerApiConfiguration : IInternalApiConfiguration
-{
-    public string Url { get; set; }
-    public string Identifier { get; set; }
-}
