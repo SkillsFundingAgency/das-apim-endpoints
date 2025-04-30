a3f2e63f520551a32f67da196e9fe8987d5d28a3
Paul Graham <paul.graham@coprime.co.uk>
2025-04-30 09:18:18 +0100

    manually merged chnages into new branch

--- src/Approvals/SFA.DAS.Approvals.UnitTests/Services/WhenMappingLearnerRecords.cs
+++ src/Approvals/SFA.DAS.Approvals.UnitTests/Services/WhenMappingLearnerRecords.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoFixture.NUnit3;
 using FluentAssertions;
@@ -8,53 +7,43 @@ using NUnit.Framework;
 using SFA.DAS.Approvals.InnerApi.Responses;
 using SFA.DAS.Approvals.Services;
 using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Interfaces;
 using SFA.DAS.Testing.AutoFixture;
 
-namespace SFA.DAS.Approvals.UnitTests.Services
+namespace SFA.DAS.Approvals.UnitTests.Services;
+
+public class WhenMappingLearnerRecords
 {
-    public class WhenMappingLearnerRecords
+    [Test, MoqAutoData]
+    public async Task Then_It_Maps_The_Main_Fields_Correctly(
+        LearnerDataRecord inputDataRecord,
+        List<GetStandardsListItem> standards,
+        [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> coursesApi,
+        [Greedy] MapLearnerRecords sut
+    )
     {
-        [Test, MoqAutoData]
-        public async Task Then_It_Maps_The_Main_Fields_Correctly(
-            LearnerDataRecord inputDataRecord,
-            GetStandardsListResponse standardCourses,
-            [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> coursesApi,
-            [Greedy] MapLearnerRecords sut
-           )
-        {
+        var result = await sut.Map(new List<LearnerDataRecord> {inputDataRecord}, standards);
 
-            coursesApi.Setup(x => x.Get<GetStandardsListResponse>(It.IsAny<GetStandardsExportRequest>()))
-                .ReturnsAsync(standardCourses);
+        result[0].Id.Should().Be(inputDataRecord.Id);
+        result[0].FirstName.Should().Be(inputDataRecord.FirstName);
+        result[0].LastName.Should().Be(inputDataRecord.LastName);
+        result[0].Uln.Should().Be(inputDataRecord.Uln);
+    }
 
-            var result = await sut.Map(new List<LearnerDataRecord> { inputDataRecord });
+    [Test, MoqAutoData]
+    public async Task Then_It_Maps_The_Course_Field_Correctly(
+        LearnerDataRecord[] inputDataRecords,
+        List<GetStandardsListItem> courses,
+        [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> coursesApi,
+        [Greedy] MapLearnerRecords sut
+    )
+    {
+        courses[0].LarsCode = inputDataRecords[0].StandardCode;
+        courses[1].LarsCode = inputDataRecords[1].StandardCode;
 
-            result[0].Id.Should().Be(inputDataRecord.Id);
-            result[0].FirstName.Should().Be(inputDataRecord.FirstName);
-            result[0].LastName.Should().Be(inputDataRecord.LastName);
-            result[0].Uln.Should().Be(inputDataRecord.Uln);
-        }
+        var result = await sut.Map(inputDataRecords, courses);
 
-        [Test, MoqAutoData]
-        public async Task Then_It_Maps_The_Course_Field_Correctly(
-            LearnerDataRecord[] inputDataRecords,
-            GetStandardsListResponse standardCourses,
-            [Frozen] Mock<ICoursesApiClient<CoursesApiConfiguration>> coursesApi,
-            [Greedy] MapLearnerRecords sut
-        )
-        {
-            var courses = standardCourses.Standards.ToList();
-            courses[0].LarsCode = inputDataRecords[0].StandardCode;
-            courses[1].LarsCode = inputDataRecords[1].StandardCode;
-
-            coursesApi.Setup(x => x.Get<GetStandardsListResponse>(It.IsAny<GetStandardsExportRequest>()))
-                .ReturnsAsync(standardCourses);
-
-            var result = await sut.Map(inputDataRecords);
-
-            result[0].Course.Should().Be(courses[0].Title);
-            result[1].Course.Should().Be(courses[1].Title);
-        }
+        result[0].Course.Should().Be(courses[0].Title);
+        result[1].Course.Should().Be(courses[1].Title);
     }
 }
