b6c55667557e9fbb987cd50c281016730cb97d98
Paul Graham <paul.graham@coprime.co.uk>
2025-05-09 09:05:15 +0100

    added cohortId to query

--- src/Approvals/SFA.DAS.Approvals/Application/Learners/Queries/GetLearnersForProviderQuery.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/Learners/Queries/GetLearnersForProviderQuery.cs
@@ -5,7 +5,8 @@ namespace SFA.DAS.Approvals.Application.Learners.Queries
     public class GetLearnersForProviderQuery : IRequest<GetLearnersForProviderQueryResult>
     {
         public long ProviderId { get; set; }
-        public long AccountLegalEntityId { get; set; }
+        public long? AccountLegalEntityId { get; set; }
+        public long? CohortId { get; set; }
         public string SearchTerm { get; set; }
         public string SortField { get; set; }
         public bool SortDescending { get; set; }
