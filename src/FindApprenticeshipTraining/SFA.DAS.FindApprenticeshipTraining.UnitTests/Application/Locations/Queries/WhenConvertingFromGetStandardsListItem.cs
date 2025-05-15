af9d007a80b1d2ce2fa2f5be252b8d948976bb04
Deven Shah <devenshah1974@gmail.com>
2025-05-15 11:06:05 +0100

    CSP-2078 Implements get standards endpoint (#2198)

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Locations/Queries/WhenConvertingFromGetStandardsListItem.cs
@@ -0,0 +1,17 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.Application.Standards.Queries.GetStandards;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.Application.Locations.Queries;
+public class WhenConvertingFromGetStandardsListItem
+{
+    [Test, AutoData]
+    public void Then_Converts_StandardsListItem_To_Standard(GetStandardsListItem source)
+    {
+        var result = (Standard)source;
+        result.LarsCode.Should().Be(source.LarsCode);
+        result.Title.Should().Be(source.Title);
+    }
+}
