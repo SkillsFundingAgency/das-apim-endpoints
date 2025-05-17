0a570792308a6d220030aa6f3850736c3010ea5c
Paul Graham <paul.graham@coprime.co.uk>
2025-05-17 05:28:37 +0100

    APPMAN-1567-non-levy-selection-and-populating-data-third-avoiding some previous branches (#2202)
    * WIP
    
    * wip
    
    * added handler for getting learners and TESTs
    
    * Added tests to controller
    
    * correct Academic year
    
    * WIP
    
    * manually merged chnages into new branch
    
    * make percentage... nullable
    
    * make field nullable

---
+++ src/Approvals/SFA.DAS.Approvals/Services/MapLearnerRecords.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Approvals.Application.Learners.Queries;
+using SFA.DAS.Approvals.InnerApi.Responses;
+
+namespace SFA.DAS.Approvals.Services;
+
+public interface IMapLearnerRecords
+{
+    Task<List<LearnerSummary>> Map(IEnumerable<LearnerDataRecord> learner, List<GetStandardsListItem> list);
+}
+
+public class MapLearnerRecords(ILogger<IMapLearnerRecords> logger) : IMapLearnerRecords
+{
+    public async Task<List<LearnerSummary>> Map(IEnumerable<LearnerDataRecord> learners, List<GetStandardsListItem> list)
+    {
+        logger.LogInformation("Getting all Courses, to match with Learner Records");
+        var learnerSummaries = new List<LearnerSummary>();
+
+        logger.LogInformation("Mapping Learner record to summary");
+        foreach (var learner in learners)
+        {
+            learnerSummaries.Add(new LearnerSummary
+            {
+                Id = learner.Id,
+                FirstName = learner.FirstName,
+                LastName = learner.LastName,
+                Uln = learner.Uln,
+                Course = list.FirstOrDefault(x => x.LarsCode == learner.StandardCode)?.Title
+            });
+        }
+
+        return learnerSummaries;
+    }
+
+}
