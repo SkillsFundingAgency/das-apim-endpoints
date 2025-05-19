439455347f014d02bd7f652d71620e4731588ea9
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2025-05-19 13:05:12 +0100

    FLP-1148 Health Check

---
+++ src/Shared/SFA.DAS.SharedOuterApi/Infrastructure/HealthCheck/EarningsApiHealthCheck.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.SharedOuterApi.Infrastructure.HealthCheck;
+public class EarningsApiHealthCheck : ApiHealthCheck<EarningsApiConfiguration>, IHealthCheck
+{
+    public static readonly string HealthCheckDescription = "Earnings API";
+    public static string HealthCheckResultDescription => $"{HealthCheckDescription} check";
+
+    public EarningsApiHealthCheck(IEarningsApiClient<EarningsApiConfiguration> client, ILogger<EarningsApiHealthCheck> logger)
+        : base(HealthCheckDescription, HealthCheckResultDescription, client, logger)
+    {
+    }
+}
