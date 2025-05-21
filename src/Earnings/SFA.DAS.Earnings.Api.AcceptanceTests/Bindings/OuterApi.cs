98abdb4834d314fbe425aa8819610cb84d1c847c
Chris Foster <chrisfoster186@googlemail.com>
2025-05-21 16:14:12 +0100

    Fix namespaces; add stuff

---
+++ src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Bindings/OuterApi.cs
@@ -0,0 +1,46 @@
+﻿using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Earnings.Api.AcceptanceTests.Bindings
+{
+    [Binding]
+    [Scope(Tag = "outerApi")]
+    public class OuterApi
+    {
+        public static HttpClient Client { get; set; }
+        public static LocalWebApplicationFactory<Startup> Factory { get; set; }
+
+        private readonly TestContext _context;
+
+        public OuterApi(TestContext context)
+        {
+            _context = context;
+        }
+        
+        [BeforeScenario()]
+        public void Initialise()
+        {
+            if (Client == null)
+            {
+                var config = new Dictionary<string, string>
+                {
+                    {"Environment", "LOCAL_ACCEPTANCE_TESTS"},
+                    {"EmployerIncentivesInnerApi:url", _context?.InnerApi?.BaseAddress + "/"},
+                    {"CommitmentsV2InnerApi:url", _context?.CommitmentsV2InnerApi?.BaseAddress + "/"},
+                    {"CustomerEngagementFinanceApi:url", _context?.FinanceApi?.BaseAddress + "/"},
+                    {"CustomerEngagementFinanceApi:companyName", _context?.FinanceApi?.CompanyName},
+                    {"CustomerEngagementFinanceApi:apiVersion", _context?.FinanceApi?.ApiVersion},
+                    {"AccountsInnerApi:url", _context?.AccountsApi?.BaseAddress + "/"},
+                    {"EmploymentCheckInnerApi:url", _context?.EmploymentCheckApi?.BaseAddress + "/"},
+                    {"AzureAD:tenant", ""},
+                    {"AzureAD:identifier", ""}
+                };
+
+
+                Factory = new LocalWebApplicationFactory<Startup>(config);
+                Client = Factory.CreateClient();
+            }
+
+            _context.OuterApiClient = Client;
+        }
+    }
+}
---
+++ src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/TestContext.cs
@@ -0,0 +1,101 @@
+﻿using SFA.DAS.Earnings.Api.AcceptanceTests.Bindings;
+
+namespace SFA.DAS.Earnings.Api.AcceptanceTests
+{
+    public static class MockServers
+    {
+        public static MockApi InnerApi { get; set; }
+        public static MockApi AccountsApi { get; set; }
+        public static MockApi CommitmentsV2InnerApi { get; set; }
+        public static MockApi FinanceApi { get; set; }
+        public static MockApi EmploymentCheckApi { get; set; }
+    }
+
+
+    public class TestContext : IDisposable
+    {
+        public MockApi InnerApi
+        {
+            get => MockServers.InnerApi;
+            set
+            {
+                MockServers.InnerApi = value;
+                CleanUpOuterApi();
+            }
+        }
+
+        public MockApi CommitmentsV2InnerApi
+        {
+            get => MockServers.CommitmentsV2InnerApi;
+            set { 
+                MockServers.CommitmentsV2InnerApi = value;
+                CleanUpOuterApi();
+            }
+        }
+
+        public MockApi FinanceApi
+        {
+            get => MockServers.FinanceApi;
+            set
+            {
+                MockServers.FinanceApi = value;
+                CleanUpOuterApi();
+            }
+        }
+
+        public MockApi AccountsApi
+        {
+            get => MockServers.AccountsApi;
+            set
+            {
+                MockServers.AccountsApi = value;
+                CleanUpOuterApi();
+            }
+        }
+
+        public MockApi EmploymentCheckApi
+        {
+            get => MockServers.EmploymentCheckApi;
+            set
+            {
+                MockServers.EmploymentCheckApi = value;
+                CleanUpOuterApi();
+            }
+        }
+
+        public HttpClient OuterApiClient { get; set; }
+
+        private bool _isDisposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_isDisposed) return;
+
+            if (disposing)
+            {
+                InnerApi?.Reset();
+                CommitmentsV2InnerApi?.Reset();
+                FinanceApi?.Reset();
+                AccountsApi?.Reset();
+                EmploymentCheckApi?.Reset();
+            }
+
+            _isDisposed = true;
+        }
+
+        private void CleanUpOuterApi()
+        {
+            OuterApi.Factory?.Dispose();
+            OuterApi.Client?.Dispose();
+
+            OuterApi.Factory = null;
+            OuterApi.Client = null;
+        }
+    }
+}
