1a82e7e581d8028663802651ec004681760133d6
Chris Foster <chrisfoster186@googlemail.com>
2025-05-21 16:59:11 +0100

    Mini test

---
+++ src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Steps/TestSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+
+namespace SFA.DAS.Earnings.Api.AcceptanceTests.Steps
+{
+    [Binding]
+    public class TestSteps
+    {
+
+        [Given(@"That this is a test")]
+        public void GivenThatThisIsATest()
+        {
+            
+        }
+
+        [When(@"I do something")]
+        public void WhenIDoSomething()
+        {
+            
+        }
+
+        [Then(@"Something good happens")]
+        public void ThenSomethingGoodHappens()
+        {
+            
+        }
+
+    }
+}
