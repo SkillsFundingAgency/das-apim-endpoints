ff2deca3171cbf6016c9372ab16e1f074ec48394
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-05-22 18:50:36 +0100

    ✨ Enhance application submission and retrieval features
    - Added `Expired` status to `ApplicationStatus` enum.
    - Updated `SubmitApplicationCommandHandler` for better validation.
    - Introduced `ApplicationsController` to handle application requests.
    - Created `GetAllApplicationsByIdApiRequest` for fetching applications.
    - Added tests for `ApplicationsController` to ensure correct behavior.
    
    Changes made by Balaji Jambulingam

---
+++ src/Recruit/SFA.DAS.Recruit.UnitTests/Application/Candidates/Queries/WhenHandlingGetApplicationsByIdQuery.cs
@@ -0,0 +1,42 @@
+﻿using SFA.DAS.Recruit.Application.Candidates.Queries.GetApplicationsById;
+using SFA.DAS.Recruit.InnerApi.Requests;
+using SFA.DAS.Recruit.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using System.Net;
+using System.Threading;
+
+namespace SFA.DAS.Recruit.UnitTests.Application.Candidates.Queries
+{
+    [TestFixture]
+    public class WhenHandlingGetApplicationsByIdQuery
+    {
+        [Test, MoqAutoData]
+        public async Task Handle_ValidRequest_ReturnsExpectedResult(
+            GetApplicationsByIdQueryHandler handler,
+            GetApplicationsByIdQuery query,
+            GetAllApplicationsByIdApiResponse response,
+            [Frozen] Mock<ICandidateApiClient<CandidateApiConfiguration>> candidateApiClient)
+        {
+            // Arrange
+            var expectedPostUrl = new GetAllApplicationsByIdApiRequest(new GetAllApplicationsByIdApiRequestData
+            {
+                ApplicationIds = query.ApplicationIds,
+                IncludeDetails = query.IncludeDetails
+            });
+            candidateApiClient.Setup(x => x.PostWithResponseCode<GetAllApplicationsByIdApiResponse>(
+                    It.Is<GetAllApplicationsByIdApiRequest>(r =>
+                        r.PostUrl == expectedPostUrl.PostUrl && r.Payload == expectedPostUrl.Payload),
+                    true))
+
+                .ReturnsAsync(new ApiResponse<GetAllApplicationsByIdApiResponse>(response, HttpStatusCode.OK, string.Empty));
+            
+            // Act
+            var result = await handler.Handle(query, CancellationToken.None);
+            
+            // Assert
+            result.Should().BeEquivalentTo(response);
+        }
+    }
+}
---
+++ src/Recruit/SFA.DAS.Recruit/InnerApi/Responses/GetAllApplicationsByIdApiResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.Recruit.InnerApi.Responses
+{
+    public class GetAllApplicationsByIdApiResponse
+    {
+        public List<Domain.Application> Applications { get; set; }
+    }
+}
