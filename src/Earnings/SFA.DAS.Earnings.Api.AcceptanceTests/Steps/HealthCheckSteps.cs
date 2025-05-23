5000d5e9585e01342e0abd331a7e3d41049179d0
Chris Foster <chrisfoster186@googlemail.com>
2025-05-23 11:40:27 +0100

    Refactoring

--- src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Steps/HealthCheckSteps.cs
+++ src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Steps/HealthCheckSteps.cs
@@ -7,20 +7,14 @@ using WireMock.ResponseBuilders;
 namespace SFA.DAS.Earnings.Api.AcceptanceTests.Steps
 {
     [Binding]
-    public class TestSteps
+    public class HealthCheckSteps(TestContext context)
     {
-        private readonly TestContext _context;
         private HttpResponseMessage _response;
-
-        public TestSteps(TestContext context)
-        {
-            _context = context;
-        }
 
         [Given(@"the Earnings Inner Api status is (.*)")]
         public void GivenTheEmployerIncentivesInnerApiIs(HttpStatusCode status)
         {
-            _context.EarningsApi.MockServer
+            context.EarningsApi.MockServer
                 .Given(
                     Request.Create().WithPath($"/ping")
                         .UsingGet())
@@ -34,7 +28,7 @@ namespace SFA.DAS.Earnings.Api.AcceptanceTests.Steps
         [Given(@"the Apprenticeships Inner Api status is (.*)")]
         public void GivenTheApprenticeshipsInnerApiIs(HttpStatusCode status)
         {
-            _context.ApprenticeshipsApi.MockServer
+            context.ApprenticeshipsApi.MockServer
                 .Given(
                     Request.Create().WithPath($"/ping")
                         .UsingGet())
@@ -48,7 +42,7 @@ namespace SFA.DAS.Earnings.Api.AcceptanceTests.Steps
         [Given(@"the Collection Calendar Api status is (.*)")]
         public void GivenTheCollectionCalendarApiIs(HttpStatusCode status)
         {
-            _context.CollectionCalendarApi.MockServer
+            context.CollectionCalendarApi.MockServer
                 .Given(
                     Request.Create().WithPath($"/ping")
                         .UsingGet())
@@ -61,7 +55,7 @@ namespace SFA.DAS.Earnings.Api.AcceptanceTests.Steps
         [When(@"I request the service status")]
         public async Task WhenIRequestServiceStatus()
         {
-            _response = await _context.OuterApiClient.GetAsync($"/health");
+            _response = await context.OuterApiClient.GetAsync($"/health");
         }
 
         [Then(@"the result should be (.*)")]
