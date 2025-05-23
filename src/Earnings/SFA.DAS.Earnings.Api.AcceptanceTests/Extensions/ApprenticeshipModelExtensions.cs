4852f4d1467fd58836bf5ffbfe6dffdfc4267915
Chris Foster <chrisfoster186@googlemail.com>
2025-05-23 16:24:53 +0100

    Tidy up

--- src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Extensions/ApprenticeshipModelExtensions.cs
+++ src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Extensions/ApprenticeshipModelExtensions.cs
@@ -4,100 +4,99 @@ using SFA.DAS.SharedOuterApi.InnerApi.Responses.Earnings;
 using Apprenticeship = SFA.DAS.SharedOuterApi.InnerApi.Responses.Apprenticeships.Apprenticeship;
 using Episode = SFA.DAS.SharedOuterApi.InnerApi.Responses.Apprenticeships.Episode;
 
-namespace SFA.DAS.Earnings.Api.AcceptanceTests.Extensions
+namespace SFA.DAS.Earnings.Api.AcceptanceTests.Extensions;
+
+public static class ApprenticeshipModelExtensions
 {
-    public static class ApprenticeshipModelExtensions
+    public static InnerApiResponses GetInnerApiResponses(this ApprenticeshipModel apprenticeshipModel)
     {
-        public static InnerApiResponses GetInnerApiResponses(this ApprenticeshipModel apprenticeshipModel)
+        var apprenticeship = new Apprenticeship
         {
-            var apprenticeship = new Apprenticeship
-            {
-                Key = Guid.NewGuid(),
-                AgeAtStartOfApprenticeship = 18,
-                StartDate = apprenticeshipModel.PriceEpisodes.Min(x => x.StartDate),
-                PlannedEndDate = apprenticeshipModel.PriceEpisodes.Max(x => x.EndDate),
-                WithdrawnDate = null,
-                Uln = "123567",
-                Episodes =
-                [
-                    new Episode
-                    {
-                        Key = Guid.NewGuid(),
-                        TrainingCode = "test",
-                        Prices = apprenticeshipModel.PriceEpisodes.Select(x => new EpisodePrice
-                        {
-                            Key = x.Key,
-                            StartDate = x.StartDate,
-                            EndDate = x.EndDate,
-                            EndPointAssessmentPrice = 1000,
-                            FundingBandMaximum = 10000,
-                            TotalPrice = 11000,
-                            TrainingPrice = 10000
-                        }).ToList()
-                    }
-                ]
-            };
-
-            var earnings = new SharedOuterApi.InnerApi.Responses.Earnings.Apprenticeship
-            {
-                Key = apprenticeship.Key,
-                Episodes = new List<SharedOuterApi.InnerApi.Responses.Earnings.Episode>()
+            Key = Guid.NewGuid(),
+            AgeAtStartOfApprenticeship = 18,
+            StartDate = apprenticeshipModel.PriceEpisodes.Min(x => x.StartDate),
+            PlannedEndDate = apprenticeshipModel.PriceEpisodes.Max(x => x.EndDate),
+            WithdrawnDate = null,
+            Uln = "123567",
+            Episodes =
+            [
+                new Episode
                 {
-                    new SharedOuterApi.InnerApi.Responses.Earnings.Episode
+                    Key = Guid.NewGuid(),
+                    TrainingCode = "test",
+                    Prices = apprenticeshipModel.PriceEpisodes.Select(x => new EpisodePrice
                     {
-                        Key = Guid.NewGuid(),
-                        Instalments = apprenticeshipModel.Instalments.Select(x => new SharedOuterApi.InnerApi.Responses.Earnings.Instalment
+                        Key = x.Key,
+                        StartDate = x.StartDate,
+                        EndDate = x.EndDate,
+                        EndPointAssessmentPrice = 1000,
+                        FundingBandMaximum = 10000,
+                        TotalPrice = 11000,
+                        TrainingPrice = 10000
+                    }).ToList()
+                }
+            ]
+        };
+
+        var earnings = new SharedOuterApi.InnerApi.Responses.Earnings.Apprenticeship
+        {
+            Key = apprenticeship.Key,
+            Episodes = new List<SharedOuterApi.InnerApi.Responses.Earnings.Episode>()
+            {
+                new SharedOuterApi.InnerApi.Responses.Earnings.Episode
+                {
+                    Key = Guid.NewGuid(),
+                    Instalments = apprenticeshipModel.Instalments.Select(x => new SharedOuterApi.InnerApi.Responses.Earnings.Instalment
+                    {
+                        EpisodePriceKey = apprenticeshipModel.PriceEpisodes.Single(y => y.PriceEpisodeId == x.PriceEpisodeId).Key,
+                        AcademicYear = x.AcademicYear,
+                        DeliveryPeriod = x.DeliveryPeriod,
+                        Amount = x.Amount
+                    }).ToList(),
+                    AdditionalPayments = apprenticeshipModel.AdditionalPayments.Select(x =>
+                        new SFA.DAS.SharedOuterApi.InnerApi.Responses.Earnings.AdditionalPayment
                         {
-                            EpisodePriceKey = apprenticeshipModel.PriceEpisodes.Single(y => y.PriceEpisodeId == x.PriceEpisodeId).Key,
+                            AdditionalPaymentType = x.Type,
+                            Amount = x.Amount,
                             AcademicYear = x.AcademicYear,
-                            DeliveryPeriod = x.DeliveryPeriod,
-                            Amount = x.Amount
-                        }).ToList(),
-                        AdditionalPayments = apprenticeshipModel.AdditionalPayments.Select(x =>
-                            new SFA.DAS.SharedOuterApi.InnerApi.Responses.Earnings.AdditionalPayment
-                            {
-                                AdditionalPaymentType = x.Type,
-                                Amount = x.Amount,
-                                AcademicYear = x.AcademicYear,
-                                DueDate = x.DueDate,
-                                DeliveryPeriod = x.DeliveryPeriod
-                            }).ToList()
-                    }
-                },
-                FundingLineType = "test",
-                Ukprn = 10005077
-            };
+                            DueDate = x.DueDate,
+                            DeliveryPeriod = x.DeliveryPeriod
+                        }).ToList()
+                }
+            },
+            FundingLineType = "test",
+            Ukprn = 10005077
+        };
 
-            //For readability, tests need not describe instalments where they are not relevant to the test
-            //However, FM36 block generation requires each price episode to have at least one instalment in order to join episodes
-            //So, we can auto-generate if instalment information is missing
-            foreach (var episode in earnings.Episodes)
+        //For readability, tests need not describe instalments where they are not relevant to the test
+        //However, FM36 block generation requires each price episode to have at least one instalment in order to join episodes
+        //So, we can auto-generate if instalment information is missing
+        foreach (var episode in earnings.Episodes)
+        {
+            if (!episode.Instalments.Any())
             {
-                if (!episode.Instalments.Any())
+                foreach (var price in apprenticeship.Episodes.SelectMany(apprenticeshipEpisode => apprenticeshipEpisode.Prices))
                 {
-                    foreach (var price in apprenticeship.Episodes.SelectMany(apprenticeshipEpisode => apprenticeshipEpisode.Prices))
+                    episode.Instalments.Add(new Instalment
                     {
-                        episode.Instalments.Add(new Instalment
-                        {
-                            Amount = 0,
-                            AcademicYear = 0,
-                            DeliveryPeriod = 0,
-                            EpisodePriceKey = price.Key
-                        });
-                    }
+                        Amount = 0,
+                        AcademicYear = 0,
+                        DeliveryPeriod = 0,
+                        EpisodePriceKey = price.Key
+                    });
                 }
             }
-
-            return new InnerApiResponses
-            {
-                ApprenticeshipsInnerApiResponse = new GetApprenticeshipsResponse
-                {
-                    Ukprn = 10005077,
-                    Apprenticeships = [apprenticeship]
-                },
-                EarningsInnerApiResponse = [earnings]
-
-            };
         }
+
+        return new InnerApiResponses
+        {
+            ApprenticeshipsInnerApiResponse = new GetApprenticeshipsResponse
+            {
+                Ukprn = 10005077,
+                Apprenticeships = [apprenticeship]
+            },
+            EarningsInnerApiResponse = [earnings]
+        };
     }
 }
+
--- src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Extensions/PriceEpisodePeriodisedValuesExtensions.cs
+++ src/Earnings/SFA.DAS.Earnings.Api.AcceptanceTests/Extensions/PriceEpisodePeriodisedValuesExtensions.cs
@@ -1,51 +1,50 @@
 ﻿using ESFA.DC.ILR.FundingService.FM36.FundingOutput.Model.Output;
 
-namespace SFA.DAS.Earnings.Api.AcceptanceTests.Extensions
+namespace SFA.DAS.Earnings.Api.AcceptanceTests.Extensions;
+
+public static class PriceEpisodePeriodisedValuesExtensions
 {
-    public static class PriceEpisodePeriodisedValuesExtensions
+    public static decimal? GetPeriodValue(this PriceEpisodePeriodisedValues item, int period)
     {
-        public static decimal? GetPeriodValue(this PriceEpisodePeriodisedValues item, int period)
+        switch (period)
         {
-            switch (period)
-            {
-                case 1: return item.Period1;
-                case 2: return item.Period2;
-                case 3: return item.Period3;
-                case 4: return item.Period4;
-                case 5: return item.Period5;
-                case 6: return item.Period6;
-                case 7: return item.Period7;
-                case 8: return item.Period8;
-                case 9: return item.Period9;
-                case 10: return item.Period10;
-                case 11: return item.Period11;
-                case 12: return item.Period12;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(period), "Period must be between 1 and 12");
-            }
+            case 1: return item.Period1;
+            case 2: return item.Period2;
+            case 3: return item.Period3;
+            case 4: return item.Period4;
+            case 5: return item.Period5;
+            case 6: return item.Period6;
+            case 7: return item.Period7;
+            case 8: return item.Period8;
+            case 9: return item.Period9;
+            case 10: return item.Period10;
+            case 11: return item.Period11;
+            case 12: return item.Period12;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(period), "Period must be between 1 and 12");
         }
-
-        public static decimal? GetPeriodValue(this LearningDeliveryPeriodisedValues item, int period)
-        {
-            switch (period)
-            {
-                case 1: return item.Period1;
-                case 2: return item.Period2;
-                case 3: return item.Period3;
-                case 4: return item.Period4;
-                case 5: return item.Period5;
-                case 6: return item.Period6;
-                case 7: return item.Period7;
-                case 8: return item.Period8;
-                case 9: return item.Period9;
-                case 10: return item.Period10;
-                case 11: return item.Period11;
-                case 12: return item.Period12;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(period), "Period must be between 1 and 12");
-            }
-        }
-
-
     }
+
+    public static decimal? GetPeriodValue(this LearningDeliveryPeriodisedValues item, int period)
+    {
+        switch (period)
+        {
+            case 1: return item.Period1;
+            case 2: return item.Period2;
+            case 3: return item.Period3;
+            case 4: return item.Period4;
+            case 5: return item.Period5;
+            case 6: return item.Period6;
+            case 7: return item.Period7;
+            case 8: return item.Period8;
+            case 9: return item.Period9;
+            case 10: return item.Period10;
+            case 11: return item.Period11;
+            case 12: return item.Period12;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(period), "Period must be between 1 and 12");
+        }
+    }
+
+
 }
