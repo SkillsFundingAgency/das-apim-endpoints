a963ba803d1a747412818992f1f4c3f6d4db7d02
MarkFCain <mark.cain@digital.education.gov.uk>
2025-05-30 13:30:48 +0100

    CSP-2070 Get addresses search

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Locations/Queries/GetAddresses/GetAddressQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.Locations.Queries.GetAddresses;
+public class GetAddressesQuery : IGetApiRequest, IRequest<GetAddressesQueryResult>
+{
+    public GetAddressesQuery(string query) => Query = query;
+    public string Query { get; }
+    public double MinMatch { get; set; }
+
+    public string GetUrl => $"api/addresses?query={Query}&MinMatch={MinMatch}";
+}
