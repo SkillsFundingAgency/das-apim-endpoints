02a65ba1c6a1fc7c801459a44c3e32f89e1cfc2d
MarkFCain <mark.cain@digital.education.gov.uk>
2025-05-30 13:30:48 +0100

    CSP-2070 Get addresses search

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/Locations/Queries/GetAddresses/GetAddressesQueryResult.cs
@@ -0,0 +1,10 @@
+﻿using SFA.DAS.RoatpCourseManagement.Application.AddressLookup.Queries;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.Locations.Queries.GetAddresses;
+
+public class GetAddressesQueryResult
+{
+    public IEnumerable<AddressItem> Addresses { get; set; } = Enumerable.Empty<AddressItem>();
+}
