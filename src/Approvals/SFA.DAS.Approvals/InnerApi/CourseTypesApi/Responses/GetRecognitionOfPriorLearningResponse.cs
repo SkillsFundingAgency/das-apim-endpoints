cf0689f3f06bf458113628e844cd812f42f3dab6
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-06-17 16:28:47 +0100

    tidy

--- src/Approvals/SFA.DAS.Approvals/InnerApi/CourseTypesApi/Responses/GetRecognitionOfPriorLearningResponse.cs
+++ src/Approvals/SFA.DAS.Approvals/InnerApi/CourseTypesApi/Responses/GetRecognitionOfPriorLearningResponse.cs
@@ -3,6 +3,6 @@ namespace SFA.DAS.Approvals.InnerApi.CourseTypesApi.Responses
     public class GetRecognitionOfPriorLearningResponse
     {
         public bool IsRequired { get; set; }
-        public int? OffTheJobTrainingMinimumHours { get; set; }
+        public int OffTheJobTrainingMinimumHours { get; set; } = 278;
     }
 } 
