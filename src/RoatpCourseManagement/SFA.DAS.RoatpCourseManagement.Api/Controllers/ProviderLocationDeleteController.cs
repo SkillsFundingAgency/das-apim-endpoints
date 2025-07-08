024e05ad81067c5c9e0dff77664aca78db4c73bf
MarkFCain <mark.cain@digital.education.gov.uk>
2025-07-08 10:41:03 +0100

    CSP-2077 Delete Location Endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderLocationDeleteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.RoatpCourseManagement.Application.Locations.Commands.DeleteProviderLocation;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers;
+
+[ApiController]
+[Route("providers/{ukprn}/locations/{id}/delete")]
+public class ProviderLocationDeleteController(ILogger<ProviderLocationDeleteController> logger, IMediator mediator) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> DeleteProviderLocation([FromRoute] int ukprn, [FromRoute] Guid id, DeleteProviderLocationCommand command)
+    {
+        logger.LogInformation("Outer API: Request received to delete provider location for ukprn: {ukprn} id: {id} by UserId: {userId}", ukprn, id, command.UserId);
+        command.Ukprn = ukprn;
+        command.Id = id;
+        var response = await mediator.Send(command);
+
+        return response.StatusCode switch
+        {
+            HttpStatusCode.NotFound => NotFound(),
+            HttpStatusCode.NoContent => NoContent(),
+            _ => BadRequest()
+        };
+    }
+}
