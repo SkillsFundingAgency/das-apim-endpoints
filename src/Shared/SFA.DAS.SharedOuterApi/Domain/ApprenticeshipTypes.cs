ea515917e8bb14f59a667ac210a0b894d804cb14
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-07-09 23:16:39 +0100

    FAI-2510 - correct revert

--- src/Shared/SFA.DAS.SharedOuterApi/Domain/ApprenticeshipTypes.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/Domain/ApprenticeshipTypes.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace SFA.DAS.SharedOuterApi.Domain;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum ApprenticeshipTypes
 {
     Standard,
