dbc1c72ac2defd08e30a733d1cd1b5b4f3f0fd8d
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-07-22 22:38:32 +0100

    try something

--- src/Approvals/SFA.DAS.Approvals/Application/Apprentices/Commands/ConfirmEditApprenticeship/ConfirmEditApprenticeshipCommandHandler.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/Apprentices/Commands/ConfirmEditApprenticeship/ConfirmEditApprenticeshipCommandHandler.cs
@@ -32,8 +32,8 @@ public class ConfirmEditApprenticeshipCommandHandler(
         var editRequestData = new EditApprenticeshipApiRequestData
         {
             ApprenticeshipId = command.ApprenticeshipId,
-            ProviderId = command.ProviderId,
-            AccountId = command.AccountId,
+            ProviderId = command.ProviderId ?? apprenticeship.ProviderId,
+            AccountId = command.AccountId ?? apprenticeship.AccountId,
             FirstName = command.FirstName,
             LastName = command.LastName,
             Email = command.Email,
