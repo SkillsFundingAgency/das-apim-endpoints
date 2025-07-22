b1e2ec715244f7b51ecf74540567fda477983cc0
Ali Reid <ali.reid@live.co.uk>
2025-07-22 15:38:58 +0100

    Marshall in type

---
+++ src/ApprenticeApp/SFA.DAS.ApprenticeApp/Models/ApprenticeshipType.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace SFA.DAS.ApprenticeApp.Models
+{
+    public enum ApprenticeshipType
+    {
+        Apprenticeship = 0,
+        FoundationApprenticeship = 1
+    }    
+}
