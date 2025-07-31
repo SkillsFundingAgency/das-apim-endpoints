1dd3cf612c3575c723c7449f2d5cad56986e8641
Deven Shah <devenshah1974@gmail.com>
2025-07-31 13:54:57 +0100

    CSP-1754 Implements annual feedback endpoint

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/EmployerFeedback/EmployerFeedbackStarsSummary.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.EmployerFeedback;
+
+public class EmployerFeedbackStarsSummary
+{
+    public long Ukprn { get; set; }
+    public int Stars { get; set; }
+    public int ReviewCount { get; set; }
+}
