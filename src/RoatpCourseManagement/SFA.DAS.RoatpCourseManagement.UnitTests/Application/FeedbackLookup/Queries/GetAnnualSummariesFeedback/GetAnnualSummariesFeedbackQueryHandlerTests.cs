5f98c75d85e65cdd99255bc8ca2dfbda2b34fa8d
Deven Shah <devenshah1974@gmail.com>
2025-08-04 15:28:11 +0100

    CSP-1754 Implements annual feedback endpoint (#2311)

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/FeedbackLookup/Queries/GetAnnualSummariesFeedback/GetAnnualSummariesFeedbackQueryHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.FeedbackLookup.Queries.GetAnnualSummariesFeedback;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.ApprenticeFeedback;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.EmployerFeedback;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.ApprenticeFeedback;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.EmployerFeedback;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.FeedbackLookup.Queries.GetAnnualSummariesFeedback;
+
+public class GetAnnualSummariesFeedbackQueryHandlerTests
+{
+    [Test, MoqAutoData]
+    public async Task Handler_AddsEmployerFeedbackToResponse(
+        [Frozen] Mock<IEmployerFeedbackApiClient<EmployerFeedbackApiConfiguration>> employerFeedbackApiClientMock,
+        GetAnnualSummariesFeedbackQueryHandler sut,
+        EmployerFeedbackStarsSummary[] expected,
+        string timePeriod)
+    {
+        employerFeedbackApiClientMock
+            .Setup(a => a.Get<IEnumerable<EmployerFeedbackStarsSummary>>(It.IsAny<GetEmployerFeedbackAnnualReviewsRequest>()))
+            .ReturnsAsync(expected);
+        var actual = await sut.Handle(new GetAnnualSummariesFeedbackQuery(timePeriod), default);
+        employerFeedbackApiClientMock
+            .Verify(a => a.Get<IEnumerable<EmployerFeedbackStarsSummary>>(It.IsAny<GetEmployerFeedbackAnnualReviewsRequest>()), Times.Once);
+        Assert.That(actual.EmployersFeedback, Is.EqualTo(expected));
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handler_AddsApprenticesFeedbackToResponse(
+        [Frozen] Mock<IApprenticeFeedbackApiClient<ApprenticeFeedbackApiConfiguration>> apprenticeFeedbackApiClientMock,
+        GetAnnualSummariesFeedbackQueryHandler sut,
+        ApprenticeFeedbackStarsSummary[] expected,
+        string timePeriod)
+    {
+        apprenticeFeedbackApiClientMock
+            .Setup(a => a.Get<IEnumerable<ApprenticeFeedbackStarsSummary>>(It.IsAny<GetApprenticeFeedbackAnnualReviewsRequest>()))
+            .ReturnsAsync(expected);
+        var actual = await sut.Handle(new GetAnnualSummariesFeedbackQuery(timePeriod), default);
+        Assert.That(actual.ApprenticesFeedback, Is.EqualTo(expected));
+    }
+}
