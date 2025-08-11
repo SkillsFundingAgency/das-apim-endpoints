3ed7bec5eb5c6204f21332a0217a2e0e00da98d6
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2025-08-11 15:24:13 +0100

    Unit tests moved and working

---
+++ src/LearnerData/SFA.DAS.LearnerData.UnitTests/Application/Fm36/TestHelpers/InstalmentHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace SFA.DAS.LearnerData.UnitTests.Application.Fm36.TestHelpers;
+
+internal static class InstalmentHelper
+{
+    internal static int GetNumberOfInstalmentsBetweenDates(DateTime startDate, DateTime endDate)
+    {
+        var totalDays = 1 + (endDate - startDate).Days;
+        var includedCensusDateCounter = 0;
+        for (var i = 0; i < totalDays; i++)
+        {
+            var dateToCheck = startDate.AddDays(i);
+            var daysInMonth = DateTime.DaysInMonth(dateToCheck.Year, dateToCheck.Month);
+
+            if (dateToCheck.Day == daysInMonth)
+                includedCensusDateCounter++;
+        }
+        return includedCensusDateCounter;
+
+    }
+
+}
