5ee8b89b3c14d1dd2bb4f5223e48b219c62b6291
Liam Crossley <liam.crossley@education.gov.uk>
2025-08-12 17:50:18 +0100

    FAI-2668 RAA Added calls to provider's Manage Standards list

--- src/Recruit/SFA.DAS.Recruit/Application/Queries/GetTrainingProgrammes/GetTrainingProgrammesQueryHandler.cs
+++ src/Recruit/SFA.DAS.Recruit/Application/Queries/GetTrainingProgrammes/GetTrainingProgrammesQueryHandler.cs
@@ -1,31 +1,67 @@
-﻿using System;
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Recruit.Domain;
+using SFA.DAS.Recruit.InnerApi.Requests;
+using SFA.DAS.Recruit.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.ProviderCoursesService;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using MediatR;
-using SFA.DAS.Recruit.Domain;
-using SFA.DAS.Recruit.InnerApi.Responses;
-using SFA.DAS.SharedOuterApi.Configuration;
-using SFA.DAS.SharedOuterApi.InnerApi.Requests;
-using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.Recruit.Application.Queries.GetTrainingProgrammes;
 
-public class GetTrainingProgrammesQueryHandler(ICoursesApiClient<CoursesApiConfiguration> coursesApiClient)
+public class GetTrainingProgrammesQueryHandler(
+    ICoursesApiClient<CoursesApiConfiguration> coursesApiClient,
+    IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> roatpApiClient,
+    ILogger<GetTrainingProgrammesQueryHandler> _logger)
     : IRequestHandler<GetTrainingProgrammesQuery, GetTrainingProgrammesQueryResult>
 {
     public async Task<GetTrainingProgrammesQueryResult> Handle(GetTrainingProgrammesQuery request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Fetching active standards");
+
         var standards = await coursesApiClient.Get<GetStandardsListResponse>(new GetActiveStandardsListRequest());
-        var trainingProgrammes = new List<TrainingProgramme>();
-        trainingProgrammes.AddRange(standards.Standards?
-            .Where(c => request.IncludeFoundationApprenticeships || c.ApprenticeshipType.Equals("Apprenticeship", StringComparison.CurrentCultureIgnoreCase))
+        var allTrainingProgrammes = new List<TrainingProgramme>();
+        allTrainingProgrammes.AddRange(standards.Standards?
+            .Where(c => request.IncludeFoundationApprenticeships ||
+                    c.ApprenticeshipType.Equals("Apprenticeship", StringComparison.CurrentCultureIgnoreCase))
             .Select(item => (TrainingProgramme)item) ?? []);
-            
+
+        List<TrainingProgramme> filteredCourses = allTrainingProgrammes;
+
+        if (request.Ukprn.HasValue)
+        {
+            _logger.LogInformation("Filtering training programmes for UKPRN {Ukprn}", request.Ukprn.Value);
+
+            var providerCourses = await roatpApiClient.Get<List<ProviderCourse>>(new GetAllProviderCoursesRequest(request.Ukprn.Value ));
+
+            if (providerCourses == null || !providerCourses.Any())
+            {
+                _logger.LogInformation("No provider courses found for UKPRN {ukprn}", request.Ukprn.Value);
+                return new GetTrainingProgrammesQueryResult
+                {
+                    TrainingProgrammes = allTrainingProgrammes
+                };
+            }
+
+            var providerLarsCodes = providerCourses?.Select(c => c.LarsCode.ToString()).ToHashSet();
+
+            filteredCourses = allTrainingProgrammes
+                .Where(p => providerLarsCodes.Contains(p.Id))
+                .ToList();
+        }
+
+        _logger.LogInformation("Returning {Count} training programmes", filteredCourses.Count);
+
         return new GetTrainingProgrammesQueryResult
         {
-            TrainingProgrammes = trainingProgrammes
+            TrainingProgrammes = filteredCourses
         };
     }
 }
