8a59ad8281f3aee997ce0f7195459e6925e853b5
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-08-13 23:11:22 +0100

    FAI-2672 - Add email template for vacancy review response

---
+++ src/Recruit/SFA.DAS.Recruit.UnitTests/Domain/EmailTemplates/WhenBuildingVacancyReviewResponseEmailTemplate.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using SFA.DAS.Recruit.Domain.EmailTemplates;
+
+namespace SFA.DAS.Recruit.UnitTests.Domain.EmailTemplates;
+
+public class WhenBuildingVacancyReviewResponseEmailTemplate
+{
+    [Test, AutoData]
+    public void Then_The_Values_Are_Set_Correctly(string templateId,string recipientEmail, string advertTitle, string firstName, string vacancyReference, string employerName, string findAnApprenticeshipAdvertUrl, string notificationSettingsUrl)
+    {
+        var expectedTokens = new Dictionary<string, string>
+        {
+            {"advertTitle", advertTitle },
+            {"firstName", firstName },
+            {"employerName", employerName },
+            {"FindAnApprenticeshipAdvertURL", findAnApprenticeshipAdvertUrl},
+            {"notificationSettingsURL", notificationSettingsUrl},
+            {"VACcode", vacancyReference}
+        };
+        
+        var actual = new VacancyReviewResponseEmailTemplate(templateId, recipientEmail, advertTitle, firstName, employerName,findAnApprenticeshipAdvertUrl, notificationSettingsUrl, vacancyReference);
+        
+        actual.TemplateId.Should().Be(templateId);
+        actual.Tokens.Should().BeEquivalentTo(expectedTokens);
+        actual.RecipientAddress.Should().Be(recipientEmail);
+    }
+}
