77ca687fc4cb89d1211c743e16744190383e48f3
Phil Head <152269503+philhead1@users.noreply.github.com>
2025-08-15 12:45:55 +0100

    Added Unit Tests and removed redundant code

---
+++ src/EmployerFeedback/SFA.DAS.EmployerFeedback.UnitTests/Application/Queries/GetProvider/GetProviderQueryTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.EmployerFeedback.Application.Queries.GetProvider
+{
+    [TestFixture]
+    public class GetProviderQueryTest
+    {
+        [Test]
+        public void GetProviderQuery_ShouldHaveProviderIdProperty()
+        {
+            // Arrange
+            var query = new GetProviderQuery();
+            // Act
+            query.ProviderId = 123;
+            // Assert
+            Assert.That(123, Is.EqualTo(query.ProviderId));
+        }
+    }
+}
