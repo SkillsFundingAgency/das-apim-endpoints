adf5d2005cd330f96171e41e77a1fb0e50aa4d18
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-08-18 23:11:33 +0100

    FAI-2672 - Send employer notification on employer review

--- src/Recruit/SFA.DAS.Recruit.UnitTests/Application/VacancyReview/Commands/WhenHandlingUpsertVacancyReviewCommand.cs
+++ src/Recruit/SFA.DAS.Recruit.UnitTests/Application/VacancyReview/Commands/WhenHandlingUpsertVacancyReviewCommand.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Threading;
+using SFA.DAS.Notifications.Messages.Commands;
 using SFA.DAS.Recruit.Application.VacancyReview.Commands.UpsertVacancyReview;
+using SFA.DAS.Recruit.Domain;
+using SFA.DAS.Recruit.InnerApi.Recruit.Requests;
+using SFA.DAS.Recruit.InnerApi.Recruit.Responses;
 using SFA.DAS.Recruit.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Infrastructure;
@@ -29,4 +34,67 @@ public class WhenHandlingUpsertVacancyReviewCommand
             x => x.PutWithResponseCode<NullResponse>(
                 It.Is<PutCreateVacancyReviewRequest>(c => c.PutUrl == expectedPutRequest.PutUrl)), Times.Once);
     }
+    
+    [Test, MoqAutoData]
+    public async Task Then_If_The_Command_Is_Approving_An_Employer_Created_Vacancy_Review_Then_Notifications_Sent_For_Employers_With_Immediate_Notification(
+        UpsertVacancyReviewCommand command,
+        RecruitUserApiResponse userApiResponse1,
+        RecruitUserApiResponse userApiResponse2,
+        RecruitUserApiResponse userApiResponse3,
+        [Frozen] EmailEnvironmentHelper emailEnvironmentHelper,
+        [Frozen] Mock<IRecruitApiClient<RecruitApiConfiguration>> recruitApiClient,
+        [Frozen] Mock<INotificationService> notificationService,
+        UpsertVacancyReviewCommandHandler handler)
+    {
+        userApiResponse1.NotificationPreferences.EventPreferences =
+        [
+            new EventPreference
+            {
+                Event = "VacancyApprovedOrRejectedByDfE",
+                Frequency = "Default",
+                Method = "Email",
+                Scope = "OrganisationVacancies"
+            }
+        ];
+        userApiResponse3.NotificationPreferences.EventPreferences =
+        [
+            new EventPreference
+            {
+                Event = "VacancyApprovedOrRejectedByDfE",
+                Frequency = "Daily",
+                Method = "Email",
+                Scope = "OrganisationVacancies"
+            }
+        ];
+        command.VacancyReview.ManualOutcome = "Approved";
+        var expectedPutRequest = new PutCreateVacancyReviewRequest(command.Id, command.VacancyReview);
+        recruitApiClient.Setup(
+                x => x.PutWithResponseCode<NullResponse>(
+                    It.Is<PutCreateVacancyReviewRequest>(c => c.PutUrl == expectedPutRequest.PutUrl)))
+            .ReturnsAsync(new ApiResponse<NullResponse>(null, HttpStatusCode.Created, ""));
+        recruitApiClient
+            .Setup(x => x.GetAll<RecruitUserApiResponse>(
+                It.Is<GetEmployerRecruitUserNotificationPreferencesApiRequest>(c =>
+                    c.GetAllUrl.Contains(command.VacancyReview.AccountId.ToString())))).ReturnsAsync([userApiResponse1, userApiResponse2, userApiResponse3]);
+
+        await handler.Handle(command, CancellationToken.None);
+
+        recruitApiClient.Verify(
+            x => x.PutWithResponseCode<NullResponse>(
+                It.Is<PutCreateVacancyReviewRequest>(c => c.PutUrl == expectedPutRequest.PutUrl)), Times.Once);
+        notificationService.Verify(x=>x.Send(
+            It.Is<SendEmailCommand>(c=>
+                c.RecipientsAddress == userApiResponse1.Email
+                && c.TemplateId == emailEnvironmentHelper.VacancyReviewApprovedTemplateId
+                && c.Tokens["advertTitle"] == command.VacancyReview.VacancyTitle
+                && c.Tokens["firstName"] == userApiResponse1.Name
+                && c.Tokens["employerName"] == command.VacancyReview.EmployerName
+                && c.Tokens["FindAnApprenticeshipAdvertURL"] == string.Format(emailEnvironmentHelper.LiveVacancyUrl,command.VacancyReview.VacancyReference.ToString())
+                && c.Tokens["notificationSettingsURL"] == string.Format(emailEnvironmentHelper.NotificationsSettingsEmployerUrl, command.VacancyReview.HashedAccountId)
+                && c.Tokens["VACcode"] == command.VacancyReview.VacancyReference.ToString()
+            )
+        ), Times.Once);
+        notificationService.Verify(x => x.Send(It.IsAny<SendEmailCommand>()), Times.Once);
+    }
+    
 }
--- src/Recruit/SFA.DAS.Recruit/Application/VacancyReview/Commands/UpsertVacancyReview/UpsertVacancyReviewCommandHandler.cs
+++ src/Recruit/SFA.DAS.Recruit/Application/VacancyReview/Commands/UpsertVacancyReview/UpsertVacancyReviewCommandHandler.cs
@@ -1,18 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
-using SFA.DAS.Recruit.InnerApi.Requests;
+using SFA.DAS.Notifications.Messages.Commands;
+using SFA.DAS.Recruit.Domain;
+using SFA.DAS.Recruit.Domain.EmailTemplates;
+using SFA.DAS.Recruit.InnerApi.Recruit.Requests;
+using SFA.DAS.Recruit.InnerApi.Recruit.Responses;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Infrastructure;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.Recruit.Application.VacancyReview.Commands.UpsertVacancyReview;
 
-public class UpsertVacancyReviewCommandHandler(IRecruitApiClient<RecruitApiConfiguration> apiClient) : IRequestHandler<UpsertVacancyReviewCommand>
+public class UpsertVacancyReviewCommandHandler(IRecruitApiClient<RecruitApiConfiguration> apiClient, INotificationService notificationService, EmailEnvironmentHelper helper) : IRequestHandler<UpsertVacancyReviewCommand>
 {
     public async Task Handle(UpsertVacancyReviewCommand request, CancellationToken cancellationToken)
     {
         await apiClient.PutWithResponseCode<NullResponse>(
             new PutCreateVacancyReviewRequest(request.Id, request.VacancyReview));
+        
+        if (!string.IsNullOrEmpty(request.VacancyReview.ManualOutcome) 
+            && request.VacancyReview.ManualOutcome.Equals("Approved", StringComparison.CurrentCultureIgnoreCase))
+        {
+            var users = await apiClient.GetAll<RecruitUserApiResponse>(
+                new GetEmployerRecruitUserNotificationPreferencesApiRequest(request.VacancyReview.AccountId));
+
+            var usersToNotify = users.Where(user => user.NotificationPreferences.EventPreferences.Any(c =>
+                c.Event.Equals("VacancyApprovedOrRejectedByDfE", StringComparison.CurrentCultureIgnoreCase) &&
+                c.Frequency.Equals("Default", StringComparison.CurrentCultureIgnoreCase))).ToList();
+
+            var emailTasks = usersToNotify
+                .Select(apiResponse => new VacancyReviewResponseEmailTemplate(
+                    helper.VacancyReviewApprovedTemplateId,
+                    apiResponse.Email, 
+                    request.VacancyReview.VacancyTitle, 
+                    apiResponse.Name,
+                    request.VacancyReview.EmployerName,
+                    string.Format(helper.LiveVacancyUrl, request.VacancyReview.VacancyReference.ToString()),
+                    string.Format(helper.NotificationsSettingsEmployerUrl, request.VacancyReview.HashedAccountId),
+                    request.VacancyReview.VacancyReference.ToString()))
+                .Select(email => new SendEmailCommand(email.TemplateId, email.RecipientAddress, email.Tokens))
+                .Select(notificationService.Send).ToList();
+            
+            await Task.WhenAll(emailTasks);
+        }
     }
 }
