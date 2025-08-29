95bf21c158ec8a59edfb1f0f6b465b8fb58357c8
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-08-29 16:58:18 +0100

    FAI-2749
    Initial commit with new project structure added and endpoint to fetch the Qa Dashboard

---
+++ src/RecruitQa/SFA.DAS.RecruitQa/InnerApi/Responses/GetQaDashboardApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.RecruitQa.InnerApi.Responses;
+public record GetQaDashboardApiResponse
+{
+    public int TotalVacanciesForReview { get; set; } = 0;
+    public int TotalVacanciesBrokenSla { get; set; } = 0;
+    public int TotalVacanciesResubmitted { get; set; } = 0;
+    public int TotalVacanciesSubmittedTwelveTwentyFourHours { get; set; } = 0;
+}
