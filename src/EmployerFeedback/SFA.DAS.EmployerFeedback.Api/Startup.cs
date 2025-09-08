013f7b1cc61728903c1770e6c2ba92b84379f00f
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-09-08 15:58:27 +0100

    P2-2807-SubmitFeedback

--- src/EmployerFeedback/SFA.DAS.EmployerFeedback.Api/Startup.cs
+++ src/EmployerFeedback/SFA.DAS.EmployerFeedback.Api/Startup.cs
@@ -21,93 +21,100 @@ namespace SFA.DAS.EmployerFeedback.Api;
 
 [ExcludeFromCodeCoverage]
 public class Startup
+{
+    private readonly IConfiguration _configuration;
+    private readonly IWebHostEnvironment _env;
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment env)
     {
-        private readonly IConfiguration _configuration;
-        private readonly IWebHostEnvironment _env;
+        _env = env;
+        _configuration = configuration.BuildSharedConfiguration();
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSingleton(_env);
 
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        services.AddConfigurationOptions(_configuration);
+
+        if (!_configuration.IsLocalOrDev())
         {
-            _env = env;
-            _configuration = configuration.BuildSharedConfiguration();
-        }
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton(_env);
-
-            services.AddConfigurationOptions(_configuration);
-
-            if (!_configuration.IsLocalOrDev())
-            {
-                var azureAdConfiguration = _configuration
-                    .GetSection("AzureAd")
-                    .Get<AzureActiveDirectoryConfiguration>();
-                var policies = new Dictionary<string, string>
+            var azureAdConfiguration = _configuration
+                .GetSection("AzureAd")
+                .Get<AzureActiveDirectoryConfiguration>();
+            var policies = new Dictionary<string, string>
                 {
                     {"default", "APIM"}
                 };
 
-                services.AddAuthentication(azureAdConfiguration, policies);
-            }
-
-            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(GetAccountsQuery).Assembly));
-            services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(GetAttributesQuery).Assembly));
-            services.AddServiceRegistration();
-            
-            services.Configure<RouteOptions>(options =>
-                {
-                    options.LowercaseUrls = true;
-                    options.LowercaseQueryStrings = true;
-                }).AddMvc(o =>
-                {
-                    if (!_configuration.IsLocalOrDev())
-                    {
-                        o.Filters.Add(new AuthorizeFilter("default"));
-                    }
-                })
-                .AddJsonOptions(options => options.JsonSerializerOptions.IgnoreNullValues = true);
-
-            if (_configuration["Environment"] != "DEV")
-            {
-                services.AddHealthChecks()
-                     .AddCheck<AccountsApiHealthCheck>(AccountsApiHealthCheck.HealthCheckResultDescription);
-            }
-            
-            services.AddOpenTelemetryRegistration(_configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]!);
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmployerFeedbackOuterApi", Version = "v1" });
-            });
+            services.AddAuthentication(azureAdConfiguration, policies);
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(GetAccountsQuery).Assembly));
+        services.AddMediatR(configuration => configuration.RegisterServicesFromAssembly(typeof(GetAttributesQuery).Assembly));
+        services.AddServiceRegistration();
+
+        services.Configure<RouteOptions>(options =>
+            {
+                options.LowercaseUrls = true;
+                options.LowercaseQueryStrings = true;
+            }).AddMvc(o =>
+            {
+                if (!_configuration.IsLocalOrDev())
+                {
+                    o.Filters.Add(new AuthorizeFilter("default"));
+                }
+            })
+            .AddJsonOptions(options => options.JsonSerializerOptions.IgnoreNullValues = true);
+
+        if (_configuration["Environment"] != "DEV")
         {
-            
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseAuthentication();
-
-            if (!_configuration["Environment"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
-            {
-                app.UseHealthChecks();
-            }
-            
-            app.UseRouting();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "api/{controller=Account}/{action=index}/{id?}");
-            });
-        
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployerFeedbackOuterApi");
-                c.RoutePrefix = string.Empty;
-            });
+            services.AddHealthChecks()
+                 .AddCheck<AccountsApiHealthCheck>(AccountsApiHealthCheck.HealthCheckResultDescription);
         }
+        services
+            .AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(
+                    new System.Text.Json.Serialization.JsonStringEnumConverter());
+            });
+
+        services.AddOpenTelemetryRegistration(_configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]!);
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "EmployerFeedbackOuterApi", Version = "v1" });
+        });
     }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseAuthentication();
+
+        if (!_configuration["Environment"].Equals("DEV", StringComparison.CurrentCultureIgnoreCase))
+        {
+            app.UseHealthChecks();
+        }
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "api/{controller=Account}/{action=index}/{id?}");
+        });
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployerFeedbackOuterApi");
+            c.RoutePrefix = string.Empty;
+        });
+    }
+}
