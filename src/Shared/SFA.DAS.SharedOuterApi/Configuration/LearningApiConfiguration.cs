e7772bdeef9151241a79c5eb4ce930d870462f66
Chris Foster <chrisfoster186@googlemail.com>
2025-09-09 11:55:45 +0100

    Correct config error

--- src/Shared/SFA.DAS.SharedOuterApi/Configuration/LearningApiConfiguration.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/Configuration/LearningApiConfiguration.cs
@@ -1,9 +1,10 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.SharedOuterApi.Configuration
 {
     [ExcludeFromCodeCoverage]
-    public class LearningApiConfiguration
+    public class LearningApiConfiguration : IInternalApiConfiguration
     {
         public string Url { get; set; }
         public string Identifier { get; set; }
