958ab41d61b972306f7d081a30885162938ea345
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-09-11 11:08:42 +0100

    get all learners

--- src/LearnerDataJobs/SFA.DAS.LearnerDataJobs/Responses/GetAllLearnersResponse.cs
+++ src/LearnerDataJobs/SFA.DAS.LearnerDataJobs/Responses/GetAllLearnersResponse.cs
@@ -5,7 +5,7 @@ public class GetAllLearnersResponse
     public IEnumerable<LearnerDataApiResponse> Data { get; set; } = new List<LearnerDataApiResponse>();
     public int Page { get; set; }
     public int PageSize { get; set; }
-    public int TotalCount { get; set; }
+    public int TotalItems { get; set; }
     public int TotalPages { get; set; }
 }
 
@@ -16,17 +16,19 @@ public class LearnerDataApiResponse
     public long Ukprn { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
+    public string? Email { get; set; }
     public DateTime Dob { get; set; }
+    public int AcademicYear { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime PlannedEndDate { get; set; }
-    public decimal PercentageLearningToBeDelivered { get; set; }
-    public decimal EpaoPrice { get; set; }
-    public decimal TrainingPrice { get; set; }
-    public long AgreementId { get; set; }
+    public int? PercentageLearningToBeDelivered { get; set; }
+    public int EpaoPrice { get; set; }
+    public int TrainingPrice { get; set; }
+    public string? AgreementId { get; set; }
+    public string ConsumerReference { get; set; } = string.Empty;
+    public Guid CorrelationId { get; set; }
+    public DateTime ReceivedDate { get; set; }
+    public int StandardCode { get; set; }
     public bool IsFlexiJob { get; set; }
     public int PlannedOTJTrainingHours { get; set; }
-    public int StandardCode { get; set; }
-    public string ConsumerReference { get; set; } = string.Empty;
-    public string AcademicYear { get; set; } = string.Empty;
 }
