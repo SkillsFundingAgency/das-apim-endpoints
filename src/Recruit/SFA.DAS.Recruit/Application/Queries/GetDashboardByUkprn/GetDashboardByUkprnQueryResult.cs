7e5ebb68e4d9badd335c0559d85080085a13df1b
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2025-09-18 16:09:58 +0100

    ✨ Refactor dashboard query result classes
    - Inherit `GetDashboardByAccountIdQueryResult` and
      `GetDashboardByUkprnQueryResult` from `DashboardBaseModel`.
    - Consolidate common properties for applications and vacancies.
    - Update implicit operator methods to use a new `Map` method.
    - Expand `GetDashboardApiResponse` with additional vacancy properties.
    - Promote code reuse and maintainability through shared logic.
    
    Changes made by Balaji Jambulingam

--- src/Recruit/SFA.DAS.Recruit/Application/Queries/GetDashboardByUkprn/GetDashboardByUkprnQueryResult.cs
+++ src/Recruit/SFA.DAS.Recruit/Application/Queries/GetDashboardByUkprn/GetDashboardByUkprnQueryResult.cs
@@ -1,27 +1,9 @@
-﻿namespace SFA.DAS.Recruit.Application.Queries.GetDashboardByUkprn
-{
-    public record GetDashboardByUkprnQueryResult
-    {
-        public int NewApplicationsCount { get; set; } = 0;
-        public int EmployerReviewedApplicationsCount { get; set; } = 0;
-        public int SharedApplicationsCount { get; set; } = 0;
-        public int AllSharedApplicationsCount { get; set; } = 0;
-        public int SuccessfulApplicationsCount { get; set; } = 0;
-        public int UnsuccessfulApplicationsCount { get; set; } = 0;
-        public bool HasNoApplications { get; set; } = false;
+﻿using SFA.DAS.Recruit.Application.Queries.Base;
 
-        public static implicit operator GetDashboardByUkprnQueryResult(InnerApi.Responses.GetDashboardApiResponse response)
-        {
-            return new GetDashboardByUkprnQueryResult
-            {
-                NewApplicationsCount = response.NewApplicationsCount,
-                EmployerReviewedApplicationsCount = response.EmployerReviewedApplicationsCount,
-                SharedApplicationsCount = response.SharedApplicationsCount,
-                AllSharedApplicationsCount = response.AllSharedApplicationsCount,
-                SuccessfulApplicationsCount = response.SuccessfulApplicationsCount,
-                UnsuccessfulApplicationsCount = response.UnsuccessfulApplicationsCount,
-                HasNoApplications = response.HasNoApplications
-            };
-        }
-    }
+namespace SFA.DAS.Recruit.Application.Queries.GetDashboardByUkprn;
+
+public record GetDashboardByUkprnQueryResult : DashboardBaseModel
+{
+    public static implicit operator GetDashboardByUkprnQueryResult(InnerApi.Responses.GetDashboardApiResponse response) =>
+        Map(response, () => new GetDashboardByUkprnQueryResult());
 }
--- src/Recruit/SFA.DAS.Recruit/InnerApi/Responses/GetDashboardApiResponse.cs
+++ src/Recruit/SFA.DAS.Recruit/InnerApi/Responses/GetDashboardApiResponse.cs
@@ -1,13 +1,20 @@
-﻿namespace SFA.DAS.Recruit.InnerApi.Responses
+﻿namespace SFA.DAS.Recruit.InnerApi.Responses;
+
+public record GetDashboardApiResponse
 {
-    public record GetDashboardApiResponse
-    {
-        public int NewApplicationsCount { get; set; } = 0;
-        public int EmployerReviewedApplicationsCount { get; set; } = 0;
-        public int SharedApplicationsCount { get; set; } = 0;
-        public int AllSharedApplicationsCount { get; set; } = 0;
-        public int SuccessfulApplicationsCount { get; set; } = 0;
-        public int UnsuccessfulApplicationsCount { get; set; } = 0;
-        public bool HasNoApplications { get; set; } = false;
-    }
+    public int NewApplicationsCount { get; set; } = 0;
+    public int EmployerReviewedApplicationsCount { get; set; } = 0;
+    public int SharedApplicationsCount { get; set; } = 0;
+    public int AllSharedApplicationsCount { get; set; } = 0;
+    public int SuccessfulApplicationsCount { get; set; } = 0;
+    public int UnsuccessfulApplicationsCount { get; set; } = 0;
+    public bool HasNoApplications { get; set; } = false;
+    public int ClosedVacanciesCount { get; set; } = 0;
+    public int DraftVacanciesCount { get; set; } = 0;
+    public int ReviewVacanciesCount { get; set; } = 0;
+    public int ReferredVacanciesCount { get; set; } = 0;
+    public int LiveVacanciesCount { get; set; } = 0;
+    public int SubmittedVacanciesCount { get; set; } = 0;
+    public int ClosingSoonVacanciesCount { get; set; } = 0;
+    public int ClosingSoonWithNoApplications { get; set; } = 0;
 }
