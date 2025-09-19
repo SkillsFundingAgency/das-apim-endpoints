27db803acd476c396fb548bc33d731c1046738d8
Afzal Hussain <afi259@hotmail.co.uk>
2025-09-19 15:36:22 +0100

    CSP-2231 Impliments Get Organisations End Point

--- src/AdminRoatp/SFA.DAS.AdminRoatp.Api/Controllers/OrganisationsController.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api/Controllers/OrganisationsController.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using SFA.DAS.AdminRoatp.Application.Organisation.Queries;
+using SFA.DAS.AdminRoatp.Application.Queries.GetOrganisation;
+using System.Net;
 
 namespace SFA.DAS.AdminRoatp.Api.Controllers;
 
@@ -19,9 +21,11 @@ public class OrganisationsController(IMediator _mediator, ILogger<OrganisationsC
     }
 
     [HttpGet]
-    public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(List<GetOrganisationsQueryResponse>))]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(IDictionary<string, string>))]
+    public async Task<IActionResult> GetOrganisations([FromQuery] string searchTerm, CancellationToken cancellationToken)
     {
-        _logger.LogInformation("Received request to get all organisations");
-        return Ok(new { Message = "This endpoint is not implemented yet." });
+        GetOrganisationsQueryResponse response = await _mediator.Send(new GetOrganisationsQuery(searchTerm), cancellationToken);
+        return Ok(response);
     }
 }
---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/Application/Queries/GetOrganisation/GetOrganisationsQueryHandlerTests.cs
@@ -0,0 +1,49 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.AdminRoatp.Application.Queries.GetOrganisation;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Exceptions;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.Testing.AutoFixture;
+using System.Net;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.Application.Queries.GetOrganisation;
+public class GetOrganisationsQueryHandlerTests
+{
+    [Test, MoqAutoData]
+    public async Task Handle_SuccessfulResponse_ReturnsData(
+        [Frozen] Mock<IRoatpServiceApiClient<RoatpConfiguration>> apiClientMock,
+        GetOrganisationsQuery query,
+        SearchOrganisationResponse response,
+        GetOrganisationsQueryHandler sut
+        )
+    {
+        var apiResponse = new ApiResponse<SearchOrganisationResponse>(response, HttpStatusCode.OK, null, new Dictionary<string, IEnumerable<string>>());
+        var expectedResponse = new GetOrganisationsQueryResponse { Organisations = response.SearchResults.Select(c => (AdminRoatp.Application.Queries.GetOrganisation.Organisation)c) };
+        apiClientMock.Setup(x => x.GetWithResponseCode<SearchOrganisationResponse>(It.IsAny<SearchOrganisationRequest>())).ReturnsAsync(apiResponse);
+
+        var result = await sut.Handle(query, CancellationToken.None);
+
+        apiClientMock.Verify(a => a.GetWithResponseCode<SearchOrganisationResponse>(It.Is<SearchOrganisationRequest>(r => r.SearchTerm == query.SearchTerm)), Times.Once);
+        result.Organisations.Should().BeEquivalentTo(expectedResponse.Organisations);
+        result.Should().NotBeNull();
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_UnsuccessfulResponse_ThrowsException(
+        [Frozen] Mock<IRoatpServiceApiClient<RoatpConfiguration>> apiClientMock,
+        GetOrganisationsQuery query,
+        GetOrganisationsQueryHandler sut)
+    {
+        var apiResponse = new ApiResponse<SearchOrganisationResponse>(It.IsAny<SearchOrganisationResponse>(), HttpStatusCode.InternalServerError, "");
+        apiClientMock.Setup(a => a.GetWithResponseCode<SearchOrganisationResponse>(It.IsAny<SearchOrganisationRequest>())).ReturnsAsync(apiResponse);
+
+        Func<Task> result = () => sut.Handle(query, It.IsAny<CancellationToken>());
+
+        await result.Should().ThrowAsync<ApiResponseException>();
+    }
+}
