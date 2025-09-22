ae38d3a7c62f9376fc610ff7c593919926635f6c
Afzal Hussain <afi259@hotmail.co.uk>
2025-09-22 10:11:00 +0100

    CSP-2231 Removed old get organisation controller and handler

--- src/AdminRoatp/SFA.DAS.AdminRoatp.Api/Controllers/OrganisationsController.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api/Controllers/OrganisationsController.cs
@@ -1,6 +1,5 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using SFA.DAS.AdminRoatp.Application.Organisation.Queries;
 using SFA.DAS.AdminRoatp.Application.Queries.GetOrganisation;
 using System.Net;
 
@@ -10,16 +9,6 @@ namespace SFA.DAS.AdminRoatp.Api.Controllers;
 [ApiController]
 public class OrganisationsController(IMediator _mediator, ILogger<OrganisationsController> _logger) : ControllerBase
 {
-    [HttpGet]
-    [Route("{ukprn}")]
-    [ProducesResponseType(typeof(GetOrganisationQueryResponse), StatusCodes.Status200OK)]
-    public async Task<IActionResult> Get([FromRoute] int ukprn, CancellationToken cancellationToken)
-    {
-        _logger.LogInformation("Received request to get organisation with UKPRN: {Ukprn}", ukprn);
-        GetOrganisationQueryResponse response = await _mediator.Send(new GetOrganisationQuery(ukprn), cancellationToken);
-        return Ok(response);
-    }
-
     [HttpGet]
     [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(List<GetOrganisationsQueryResponse>))]
     [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(IDictionary<string, string>))]
