1c9462f0e2a5524ef192296f094b568b7090deb2
Afzal Hussain <afi259@hotmail.co.uk>
2025-09-30 17:39:33 +0100

    CSP-2229 Implements search organisation endpoint
    CSP-2229 Implements search organisation endpoint
    
    CSP-2229 Separated search organisation response class to different class files
    
    CSP-2229 Removed unnecessary code block and added loging
    
    CSP-2229 Added code to handle not found response
    
    CSP-2229 Added swagger doc ref for not found
    
    CSP-2229 Amended code to resolve review comments
    
    CSP-2229 Reverted changes to controller name and route
    
    CSP-2229 Amendments made to address further review comments
    
    CSP-2229 Amended route to fix build error

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/Application/Queries/GetOrganisation/GetOrganisationQueryHandlerTests.cs
@@ -0,0 +1,59 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.AdminRoatp.Application.Queries.GetOrganisation;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Exceptions;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.Testing.AutoFixture;
+using System.Net;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.Application.Queries.GetOrganisation;
+public class GetOrganisationQueryHandlerTests
+{
+    [Test, MoqAutoData]
+
+    public async Task Handle_SuccessfulResponse_ReturnsData(
+        [Frozen] Mock<IRoatpServiceApiClient<RoatpConfiguration>> apiClient,
+        GetOrganisationQueryHandler sut,
+        GetOrganisationQuery request,
+        GetOrganisationResponse apiResponse)
+    {
+        apiClient.Setup(a => a.GetWithResponseCode<GetOrganisationResponse>(It.Is<GetOrganisationRequest>(r => r.GetUrl.Equals(new GetOrganisationRequest(request.ukprn).GetUrl)))).ReturnsAsync(new ApiResponse<GetOrganisationResponse>(apiResponse, HttpStatusCode.OK, ""));
+
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        result.Should().BeEquivalentTo(apiResponse);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_Unsuccessful404Response_ReturnNullResponse(
+    [Frozen] Mock<IRoatpServiceApiClient<RoatpConfiguration>> apiClientMock,
+    GetOrganisationQuery query,
+    GetOrganisationQueryHandler sut)
+    {
+        var apiResponse = new ApiResponse<GetOrganisationResponse>(It.IsAny<GetOrganisationResponse>(), HttpStatusCode.NotFound, "");
+        apiClientMock.Setup(a => a.GetWithResponseCode<GetOrganisationResponse>(It.IsAny<GetOrganisationRequest>())).ReturnsAsync(apiResponse);
+
+        var result = await sut.Handle(query, It.IsAny<CancellationToken>());
+
+        result.Should().BeNull();
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_UnsuccessfulResponse_ThrowsException(
+        [Frozen] Mock<IRoatpServiceApiClient<RoatpConfiguration>> apiClientMock,
+        GetOrganisationQuery query,
+        GetOrganisationQueryHandler sut)
+    {
+        var apiResponse = new ApiResponse<GetOrganisationResponse>(It.IsAny<GetOrganisationResponse>(), HttpStatusCode.InternalServerError, "");
+        apiClientMock.Setup(a => a.GetWithResponseCode<GetOrganisationResponse>(It.IsAny<GetOrganisationRequest>())).ReturnsAsync(apiResponse);
+
+        Func<Task> result = () => sut.Handle(query, It.IsAny<CancellationToken>());
+
+        await result.Should().ThrowAsync<ApiResponseException>();
+    }
+}
