5af09ad73831c0f9d9bb3cc5cad35ba5abf1267c
Afzal Hussain <afi259@hotmail.co.uk>
2025-09-30 17:49:34 +0100

    CSP-2229 Separated search organisation response class to different class files

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Roatp/OrganisationType.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+public class OrganisationType : BaseEntity
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+}
---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Roatp/ProviderType.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+public class ProviderType : BaseEntity
+{
+    public int Id { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+}
