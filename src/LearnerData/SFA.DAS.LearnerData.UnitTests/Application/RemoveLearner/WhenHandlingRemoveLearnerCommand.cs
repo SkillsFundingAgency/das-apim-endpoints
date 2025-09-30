1bbe9cb3bc75ebe5fe68c7321881c319904e1f92
tom-gough <tom.gough.tgsd@outlook.com>
2025-09-30 09:05:22 +0100

    FLP-1271 remove learner command

---
+++ src/LearnerData/SFA.DAS.LearnerData.UnitTests/Application/RemoveLearner/WhenHandlingRemoveLearnerCommand.cs
@@ -0,0 +1,136 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.LearnerData.Application.RemoveLearner;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Earnings;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Learning;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Learning;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using System.Net;
+
+namespace SFA.DAS.LearnerData.UnitTests.Application.RemoveLearner;
+
+public class WhenHandlingRemoveLearnerCommand
+{
+    private Fixture _fixture;
+
+#pragma warning disable CS8618
+    private Mock<ILearningApiClient<LearningApiConfiguration>> _learningApiClient;
+    private Mock<IEarningsApiClient<EarningsApiConfiguration>> _earningsApiClient;
+    private Mock<ILogger<RemoveLearnerCommandHandler>> _logger;
+    private RemoveLearnerCommandHandler _sut;
+#pragma warning restore CS8618
+
+    public WhenHandlingRemoveLearnerCommand()
+    {
+        _fixture = new Fixture();
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        _learningApiClient = new Mock<ILearningApiClient<LearningApiConfiguration>>();
+        _earningsApiClient = new Mock<IEarningsApiClient<EarningsApiConfiguration>>();
+        _logger = new Mock<ILogger<RemoveLearnerCommandHandler>>();
+        _sut = new RemoveLearnerCommandHandler(
+            _logger.Object,
+            _learningApiClient.Object,
+            _earningsApiClient.Object);
+    }
+
+    [Test]
+    public async Task Then_Learner_Is_Removed_Successfully()
+    {
+        // Arrange
+        var command = _fixture.Create<RemoveLearnerCommand>();
+        var startDate = DateTime.UtcNow;
+
+        _learningApiClient.Setup(x => x.Get<GetLearningStartDateResponse>(
+                It.Is<GetLearningStartDateRequest>(r => r.LearningKey == command.LearningKey)))
+            .ReturnsAsync(new GetLearningStartDateResponse
+            {
+                LearningKey = command.LearningKey,
+                ActualStartDate = startDate
+            });
+
+        _learningApiClient.Setup(x => x.DeleteWithResponseCode<string>(
+                It.Is<RemoveLearnerApiDeleteRequest>(r => r.LearningKey == command.LearningKey), It.IsAny<bool>()))
+            .ReturnsAsync(new ApiResponse<string>("", HttpStatusCode.NoContent, ""));
+
+        _earningsApiClient.Setup(x => x.PatchWithResponseCode<WithdrawLearnerRequest>(
+                It.IsAny<WithdrawLearnerPatchRequest>()))
+            .ReturnsAsync(new ApiResponse<string>("", HttpStatusCode.NoContent, ""));
+
+        // Act
+        await _sut.Handle(command, CancellationToken.None);
+
+        // Assert
+        _learningApiClient.Verify(x => x.Get<GetLearningStartDateResponse>(
+            It.Is<GetLearningStartDateRequest>(r => r.LearningKey == command.LearningKey)), Times.Once);
+
+        _learningApiClient.Verify(x => x.DeleteWithResponseCode<string>(
+            It.Is<RemoveLearnerApiDeleteRequest>(r => r.LearningKey == command.LearningKey && r.Ukprn == command.Ukprn), It.IsAny<bool>()), Times.Once);
+
+        _earningsApiClient.Verify(x => x.PatchWithResponseCode<WithdrawLearnerRequest>(
+            It.Is<WithdrawLearnerPatchRequest>(r => r.LearningKey == command.LearningKey && r.Data.WithdrawalDate == startDate)), Times.Once);
+    }
+
+    [Test]
+    public void Then_Throws_If_No_StartDate_Found()
+    {
+        // Arrange
+        var command = _fixture.Create<RemoveLearnerCommand>();
+
+        _learningApiClient.Setup(x => x.Get<GetLearningStartDateResponse>(
+                It.IsAny<GetLearningStartDateRequest>()))
+            .ReturnsAsync((GetLearningStartDateResponse)null);
+
+        // Act & Assert
+        Assert.ThrowsAsync<Exception>(async () => await _sut.Handle(command, CancellationToken.None));
+    }
+
+    [Test]
+    public void Then_Throws_If_Delete_Fails()
+    {
+        // Arrange
+        var command = _fixture.Create<RemoveLearnerCommand>();
+        var startDate = DateTime.UtcNow;
+
+        _learningApiClient.Setup(x => x.Get<GetLearningStartDateResponse>(
+                It.IsAny<GetLearningStartDateRequest>()))
+            .ReturnsAsync(new GetLearningStartDateResponse { LearningKey = command.LearningKey, ActualStartDate = startDate });
+
+        _learningApiClient.Setup(x => x.DeleteWithResponseCode<string>(
+                It.IsAny<RemoveLearnerApiDeleteRequest>(), It.IsAny<bool>()))
+            .ReturnsAsync(new ApiResponse<string>("", HttpStatusCode.InternalServerError, ""));
+
+        // Act & Assert
+        Assert.ThrowsAsync<Exception>(async () => await _sut.Handle(command, CancellationToken.None));
+    }
+
+    [Test]
+    public void Then_Throws_If_Earnings_Patch_Fails()
+    {
+        // Arrange
+        var command = _fixture.Create<RemoveLearnerCommand>();
+        var startDate = DateTime.UtcNow;
+
+        _learningApiClient.Setup(x => x.Get<GetLearningStartDateResponse>(
+                It.IsAny<GetLearningStartDateRequest>()))
+            .ReturnsAsync(new GetLearningStartDateResponse { LearningKey = command.LearningKey, ActualStartDate = startDate });
+
+        _learningApiClient.Setup(x => x.DeleteWithResponseCode<string>(
+                It.IsAny<RemoveLearnerApiDeleteRequest>(), It.IsAny<bool>()))
+            .ReturnsAsync(new ApiResponse<string>("", HttpStatusCode.NoContent, ""));
+
+        _earningsApiClient.Setup(x => x.PatchWithResponseCode<WithdrawLearnerRequest>(
+                It.IsAny<WithdrawLearnerPatchRequest>()))
+            .ReturnsAsync(new ApiResponse<string>("", HttpStatusCode.InternalServerError, ""));
+
+        // Act & Assert
+        Assert.ThrowsAsync<Exception>(async () => await _sut.Handle(command, CancellationToken.None));
+    }
+}
---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/Earnings/WithdrawLearnerPatchRequest.cs
@@ -0,0 +1,25 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.Earnings;
+
+public class WithdrawLearnerPatchRequest : IPatchApiRequest<WithdrawLearnerRequest>
+{
+    public WithdrawLearnerPatchRequest(Guid learningKey, DateTime withdrawalDate)
+    {
+        LearningKey = learningKey;
+        Data = new WithdrawLearnerRequest
+        {
+            WithdrawalDate = withdrawalDate
+        };
+    }
+
+    public Guid LearningKey { get; set; }
+    public string PatchUrl => $"{LearningKey}/withdraw";
+    public WithdrawLearnerRequest Data { get; set; }
+}
+
+public class WithdrawLearnerRequest
+{
+    public DateTime WithdrawalDate { get; set; }
+}
