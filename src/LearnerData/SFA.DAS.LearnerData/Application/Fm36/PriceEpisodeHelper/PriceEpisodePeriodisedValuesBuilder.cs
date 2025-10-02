c9d91041828eb1c258a56e97d8e7201e69781a42
tom-gough <tom.gough.tgsd@outlook.com>
2025-10-02 10:09:22 +0100

    FLP-1353 sonarcloud fix

--- src/LearnerData/SFA.DAS.LearnerData/Application/Fm36/PriceEpisodeHelper/PriceEpisodePeriodisedValuesBuilder.cs
+++ src/LearnerData/SFA.DAS.LearnerData/Application/Fm36/PriceEpisodeHelper/PriceEpisodePeriodisedValuesBuilder.cs
@@ -18,18 +18,7 @@ public static class PriceEpisodePeriodisedValuesBuilder
         return new PriceEpisodePeriodisedValues
         {
             AttributeName = attributeName,
-            Period1 = value,
-            Period2 = value,
-            Period3 = value,
-            Period4 = value,
-            Period5 = value,
-            Period6 = value,
-            Period7 = value,
-            Period8 = value,
-            Period9 = value,
-            Period10 = value,
-            Period11 = value,
-            Period12 = value
+            Period1 = value, Period2 = value, Period3 = value, Period4 = value, Period5 = value, Period6 = value, Period7 = value, Period8 = value, Period9 = value, Period10 = value, Period11 = value, Period12 = value
         };
     }
 
