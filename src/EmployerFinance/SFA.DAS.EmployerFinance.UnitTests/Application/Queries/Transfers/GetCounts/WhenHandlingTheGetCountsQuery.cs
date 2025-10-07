b472793d6492edc0611a4d52e2e769a359bf23f0
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-10-07 14:06:24 +0100

    update unit tests

--- src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/Queries/Transfers/GetCounts/WhenHandlingTheGetCountsQuery.cs
+++ src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/Queries/Transfers/GetCounts/WhenHandlingTheGetCountsQuery.cs
@@ -1,22 +1,16 @@
-﻿using AutoFixture.NUnit3;
-using Moq;
-using NUnit.Framework;
-using SFA.DAS.EmployerFinance.Application.Queries.Transfers.GetCounts;
+﻿using SFA.DAS.EmployerFinance.Application.Queries.Transfers.GetCounts;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests.LevyTransferMatching;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.LevyTransferMatching;
 using SFA.DAS.SharedOuterApi.Interfaces;
-using SFA.DAS.Testing.AutoFixture;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace SFA.DAS.EmployerFinance.UnitTests.Application.Queries.Transfers.GetCounts
+namespace SFA.DAS.EmployerFinance.UnitTests.Application.Queries.Transfers.GetCounts;
+
+public class WhenHandlingTheGetCountsQuery
 {
-    public class WhenHandlingTheGetCountsQuery
-    {
         [Test, MoqAutoData]
         public async Task And_AccountId_Specified_Then_Counts_Returned(
             long accountId,
@@ -46,12 +40,11 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Application.Queries.Transfers.GetCou
 
             var results = await getCountsQueryHandler.Handle(getCountsQuery, CancellationToken.None);
 
-            Assert.That(getPledgesResponse.TotalPledges, Is.EqualTo(results.PledgesCount));
-            Assert.That(getApplicationsResponse.Applications.Count(), Is.EqualTo(results.ApplicationsCount));
-            Assert.That((getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.ApprovedPledgeApplications) +
+            results.PledgesCount.Should().Be(getPledgesResponse.TotalPledges);
+            results.ApplicationsCount.Should().Be(getApplicationsResponse.Applications.Count());
+            results.CurrentYearEstimatedCommittedSpend.Should().Be(getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.ApprovedPledgeApplications) +
                              getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.AcceptedPledgeApplications)
                              + getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.PledgeOriginatedCommitments)
-                             + getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.TransferConnections)), Is.EqualTo(results.CurrentYearEstimatedCommittedSpend));
+                             + getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.TransferConnections));
         }
-    }
 }
