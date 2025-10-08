eda926bd5cf548fcbf658ca24040e566bb5fecb4
killij <51908793+killij@users.noreply.github.com>
2025-10-08 14:59:34 +0100

    FAI-2786 Get profanities from recruit inner

---
+++ src/Recruit/SFA.DAS.Recruit.Api/Controllers/ProhibitedContentController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Recruit.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.Recruit.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/")]
+public class ProhibitedContentController(ILogger<ProhibitedContentController> logger): ControllerBase
+{
+    [HttpGet]
+    [Route("profanities")]
+    public async Task<IActionResult> GetProfanities([FromServices] IRecruitApiClient<RecruitApiConfiguration> recruitInnerClient)
+    {
+        var response = await recruitInnerClient.GetWithResponseCode<List<string>>(new GetProfanitiesRequest());
+        if (response.StatusCode.IsSuccessStatusCode())
+        {
+            return Ok(response.Body);
+        }
+
+        logger.LogError("Failed to retrieve profanities list, response had status '{Status}' with content '{ErrorContent}'", response.StatusCode, response.ErrorContent);
+        return Ok(new List<string>());
+    }
+}
