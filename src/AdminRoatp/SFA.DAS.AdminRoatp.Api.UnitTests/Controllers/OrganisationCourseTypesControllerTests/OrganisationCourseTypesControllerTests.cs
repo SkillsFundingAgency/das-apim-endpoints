580f7f0b0d125d994ccf23f19296bf808272c6e1
afzal-hussain259 <afi259@hotmail.co.uk>
2025-10-09 15:59:04 +0100

    CSP-2251 Implements Update Allowed course type endpoint (#2427)
    * CSP-2251 Implements Update Allowed course type endpoint
    
    * CSP-2251 Amended return to no content
    
    * CSP-2251 Fixed sonar issues with unit tests
    
    * CSP-2251 Updated unit test

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api.UnitTests/Controllers/OrganisationCourseTypesControllerTests/OrganisationCourseTypesControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.AdminRoatp.Api.Controllers;
+using SFA.DAS.AdminRoatp.Application.Commands.UpdateOrganisationCourseTypes;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.AdminRoatp.Api.UnitTests.Controllers.OrganisationCourseTypesControllerTests;
+public class OrganisationCourseTypesControllerTests
+{
+    [Test, MoqAutoData]
+
+    public async Task UpdateCourseTypes_ValidRequest_CallsMediatorAndReturnsNoContent(
+        [Frozen] Mock<IMediator> mediatorMock,
+        [Greedy] OrganisationCourseTypesController sut,
+        UpdateOrganisationCourseTypesCommand command,
+        UpdateCourseTypesModel model)
+    {
+        mediatorMock.Setup(m => m.Send(It.IsAny<UpdateOrganisationCourseTypesCommand>(), It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
+
+        var result = await sut.UpdateCourseTypes(command.ukprn, model, It.IsAny<CancellationToken>());
+
+        result.Should().BeOfType<NoContentResult>();
+        mediatorMock.Verify(m => m.Send(It.Is<UpdateOrganisationCourseTypesCommand>(r => r.ukprn == command.ukprn && r.CourseTypeIds == model.CourseTypeIds && r.UserId == model.UserId), It.IsAny<CancellationToken>()), Times.Once());
+
+    }
+}
---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/Application/Commands/UpdateOrganisationCourseTypes/UpdateOrganisationCourseTypesCommandHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using Moq;
+using SFA.DAS.AdminRoatp.Application.Commands.UpdateOrganisationCourseTypes;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Exceptions;
+using SFA.DAS.SharedOuterApi.Infrastructure;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using SFA.DAS.Testing.AutoFixture;
+using System.Net;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.Application.Commands.UpdateOrganisationCourseTypes;
+public class UpdateOrganisationCourseTypesCommandHandlerTests
+{
+    [Test, MoqAutoData]
+
+    public async Task Handle_CallsApiClient_EnsuresSuccessResponse(
+        [Frozen] Mock<IRoatpServiceApiClient<RoatpConfiguration>> apiClientMock,
+        UpdateOrganisationCourseTypesCommandHandler sut,
+        UpdateOrganisationCourseTypesCommand command,
+        NullResponse apiResponse)
+    {
+        var updateCourseTypes = new UpdateCourseTypesModel(command.CourseTypeIds, command.UserId);
+        var apiRequest = new UpdateCourseTypesRequest(command.ukprn, updateCourseTypes);
+        apiClientMock.Setup(a => a.PutWithResponseCode<NullResponse>(It.Is<UpdateCourseTypesRequest>(r => r.PutUrl.Equals(apiRequest.PutUrl)))).ReturnsAsync(new ApiResponse<NullResponse>(apiResponse, HttpStatusCode.OK, ""));
+
+        Func<Task> result = () => sut.Handle(command, CancellationToken.None);
+
+        await result.Should().NotThrowAsync();
+        apiClientMock.Verify(x => x.PutWithResponseCode<NullResponse>(It.Is<UpdateCourseTypesRequest>(r => r.PutUrl.Equals(apiRequest.PutUrl))), Times.Once);
+    }
+
+    [Test, MoqAutoData]
+    public async Task Handle_CallsApiClient_ThrowsException(
+        [Frozen] Mock<IRoatpServiceApiClient<RoatpConfiguration>> apiClientMock,
+        UpdateOrganisationCourseTypesCommandHandler sut,
+        UpdateOrganisationCourseTypesCommand command)
+    {
+        var apiResponse = new ApiResponse<NullResponse>(It.IsAny<NullResponse>(), HttpStatusCode.InternalServerError, "");
+        apiClientMock.Setup(a => a.PutWithResponseCode<NullResponse>(It.IsAny<UpdateCourseTypesRequest>())).ReturnsAsync(apiResponse);
+
+        Func<Task> result = () => sut.Handle(command, It.IsAny<CancellationToken>());
+
+        await result.Should().ThrowAsync<ApiResponseException>();
+    }
+}
