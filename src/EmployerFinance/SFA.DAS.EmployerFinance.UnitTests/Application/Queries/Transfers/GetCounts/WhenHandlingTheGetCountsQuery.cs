17a384498b74efda1a866c49c7cb0c4caf9b9bad
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2025-10-10 00:11:42 +0100

    remove forecasting altogether

--- src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/Queries/Transfers/GetCounts/WhenHandlingTheGetCountsQuery.cs
+++ src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/Queries/Transfers/GetCounts/WhenHandlingTheGetCountsQuery.cs
@@ -1,8 +1,8 @@
 ﻿using SFA.DAS.EmployerFinance.Application.Queries.Transfers.GetCounts;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests;
-using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests.LevyTransferMatching;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.LevyTransferMatching;
 using SFA.DAS.SharedOuterApi.Interfaces;
 using System.Linq;
@@ -15,10 +15,8 @@ public class WhenHandlingTheGetCountsQuery
         public async Task And_AccountId_Specified_Then_Counts_Returned(
             long accountId,
             GetPledgesResponse getPledgesResponse,
-            GetTransferFinancialBreakdownResponse getTransferFinancialBreakdownResponse,
             GetApplicationsResponse getApplicationsResponse,
             [Frozen] Mock<ILevyTransferMatchingApiClient<LevyTransferMatchingApiConfiguration>> levyTransferMatchingClient,
-            [Frozen] Mock<IForecastingApiClient<ForecastingApiConfiguration>> forecastingApiConfiguration,
             GetCountsQueryHandler getCountsQueryHandler)
         {
             GetCountsQuery getCountsQuery = new GetCountsQuery()
@@ -30,10 +28,6 @@ public class WhenHandlingTheGetCountsQuery
                 .Setup(x => x.Get<GetPledgesResponse>(It.IsAny<GetPledgesRequest>()))
                 .ReturnsAsync(getPledgesResponse);
 
-            forecastingApiConfiguration
-                .Setup(x => x.Get<GetTransferFinancialBreakdownResponse>(It.IsAny<GetTransferFinancialBreakdownRequest>()))
-                .ReturnsAsync(getTransferFinancialBreakdownResponse);
-
             levyTransferMatchingClient
                 .Setup(x => x.Get<GetApplicationsResponse>(It.IsAny<GetApplicationsRequest>()))
                 .ReturnsAsync(getApplicationsResponse);
@@ -42,9 +36,6 @@ public class WhenHandlingTheGetCountsQuery
 
             results.PledgesCount.Should().Be(getPledgesResponse.TotalPledges);
             results.ApplicationsCount.Should().Be(getApplicationsResponse.Applications.Count());
-            results.CurrentYearEstimatedCommittedSpend.Should().Be(getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.ApprovedPledgeApplications) +
-                             getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.AcceptedPledgeApplications)
-                             + getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.PledgeOriginatedCommitments)
-                             + getTransferFinancialBreakdownResponse.Breakdown.Sum(x => x.FundsOut.TransferConnections));
+            results.CurrentYearEstimatedCommittedSpend.Should().Be(0); // No longer available since forecasting service is decommissioned
         }
 }
