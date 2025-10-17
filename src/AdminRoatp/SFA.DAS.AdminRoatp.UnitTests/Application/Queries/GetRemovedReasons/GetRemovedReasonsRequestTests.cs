e3b6091796e839d1732ddf49af90246d88a78745
Afzal Hussain <afi259@hotmail.co.uk>
2025-10-17 11:54:38 +0100

    CSP-2248 Implemented controller

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/Application/Queries/GetRemovedReasons/GetRemovedReasonsRequestTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.Application.Queries.GetRemovedReasons;
+public class GetRemovedReasonsRequestTests
+{
+    [Test]
+
+    public void GetRemovedReasonsRequest_GetUrl_ReturnsCorrectGetUrl()
+    {
+        // Arrange
+        GetRemovedReasonsRequest sut = new();
+        var exapectedUrl = "removed-reasons";
+
+        // Act
+        var url = sut.GetUrl;
+
+        // Assert
+        url.Should().Be(exapectedUrl);
+    }
+}
--- src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Queries/GetRemovedReasons/GetRemovedReasonsQueryHandler.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Queries/GetRemovedReasons/GetRemovedReasonsQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Extensions;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
@@ -6,10 +7,12 @@ using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.AdminRoatp.Application.Queries.GetRemovedReasons;
-public class GetRemovedReasonsQueryHandler(IRoatpServiceApiClient<RoatpConfiguration> _apiClient) : IRequestHandler<GetRemovedReasonsQuery, GetRemovedReasonsResponse>
+public class GetRemovedReasonsQueryHandler(IRoatpServiceApiClient<RoatpConfiguration> _apiClient, ILogger<GetRemovedReasonsQueryHandler> _logger) : IRequestHandler<GetRemovedReasonsQuery, GetRemovedReasonsResponse>
 {
     public async Task<GetRemovedReasonsResponse> Handle(GetRemovedReasonsQuery request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Handle get all removed reasons request");
+
         var response = await _apiClient.GetWithResponseCode<GetRemovedReasonsResponse>(new GetRemovedReasonsRequest());
 
         response.EnsureSuccessStatusCode();
