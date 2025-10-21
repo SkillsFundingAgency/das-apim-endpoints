5d7204f328c97d1cea26f55104a167a846a10762
Afzal Hussain <afi259@hotmail.co.uk>
2025-10-21 16:15:43 +0100

    CSP-2249 Created controller

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api/Controllers/OrganisationTypesController.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AdminRoatp.Application.Queries.GetOrganisationTypes;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+using System.Net;
+
+namespace SFA.DAS.AdminRoatp.Api.Controllers;
+[Route("organisation-types")]
+[ApiController]
+public class OrganisationTypesController(IMediator _mediator, ILogger<OrganisationTypesController> _logger) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(GetOrganisationTypesResponse))]
+    public async Task<IActionResult> GetOrganisationTypes(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Received request to GetOrganisationTypes.");
+        GetOrganisationTypesResponse result = await _mediator.Send(new GetOrganisationTypesQuery(), cancellationToken);
+        return Ok(result);
+    }
+}
