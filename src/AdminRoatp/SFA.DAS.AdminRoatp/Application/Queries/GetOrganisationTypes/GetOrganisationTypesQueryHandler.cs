786d623a63e41ad24b0abc2bd854144a91849181
Afzal Hussain <afi259@hotmail.co.uk>
2025-10-21 14:37:02 +0100

    CSP-2249 Created Handler

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Queries/GetOrganisationTypes/GetOrganisationTypesQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.AdminRoatp.Application.Queries.GetOrganisationTypes;
+public class GetOrganisationTypesQueryHandler(IRoatpServiceApiClient<RoatpConfiguration> _apiClient) : IRequestHandler<GetOrganisationTypesQuery, GetOrganisationTypesResponse>
+{
+    public async Task<GetOrganisationTypesResponse> Handle(GetOrganisationTypesQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _apiClient.GetWithResponseCode<GetOrganisationTypesResponse>(new GetOrganisationTypesRequest());
+
+        result.EnsureSuccessStatusCode();
+
+        return new() { OrganisationTypes = result.Body.OrganisationTypes };
+    }
+}
