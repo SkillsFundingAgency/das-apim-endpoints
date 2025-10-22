16ed2da10f2e5ce578d85653936486e2d0ce92e4
Deven Shah <devenshah1974@gmail.com>
2025-10-22 16:00:59 +0100

    CSP-2267 Fixed missing test and sonar issue

--- src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/Application/Queries/GetOrganisation/GetOrganisationQueryResultTests.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/Application/Queries/GetOrganisation/GetOrganisationQueryResultTests.cs
@@ -1,4 +1,5 @@
 ﻿using AutoFixture.NUnit3;
+using FluentAssertions;
 using SFA.DAS.AdminRoatp.Application.Queries.GetOrganisation;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
 
@@ -11,21 +12,6 @@ public class GetOrganisationQueryResultTests
     {
         GetOrganisationQueryResult result = source;
         // Assert
-        Assert.That(source.OrganisationId, Is.EqualTo(result.OrganisationId));
-        Assert.That(source.Ukprn, Is.EqualTo(result.Ukprn));
-        Assert.That(source.LegalName, Is.EqualTo(result.LegalName));
-        Assert.That(source.TradingName, Is.EqualTo(result.TradingName));
-        Assert.That(source.CompanyNumber, Is.EqualTo(result.CompanyNumber));
-        Assert.That(source.CharityNumber, Is.EqualTo(result.CharityNumber));
-        Assert.That(source.ProviderType, Is.EqualTo(result.ProviderType));
-        Assert.That(source.OrganisationTypeId, Is.EqualTo(result.OrganisationTypeId));
-        Assert.That(source.OrganisationType, Is.EqualTo(result.OrganisationType));
-        Assert.That(source.Status, Is.EqualTo(result.Status));
-        Assert.That(source.ApplicationDeterminedDate, Is.EqualTo(result.ApplicationDeterminedDate));
-        Assert.That(source.RemovedReasonId, Is.EqualTo(result.RemovedReasonId));
-        Assert.That(source.RemovedReason, Is.EqualTo(result.RemovedReason));
-        Assert.That(source.StartDate, Is.EqualTo(result.StartDate));
-        Assert.That(source.LastUpdatedDate, Is.EqualTo(result.LastUpdatedDate));
-        Assert.That(source.AllowedCourseTypes, Is.EqualTo(result.AllowedCourseTypes));
+        source.Should().BeEquivalentTo(result, options => options.ExcludingMissingMembers());
     }
 }
