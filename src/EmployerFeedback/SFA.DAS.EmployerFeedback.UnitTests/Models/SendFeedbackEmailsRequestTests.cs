a549e2272c586792285d0c6c477edfc37ddb2ede
ratheeshri1984 <ratheeshri1984@gmail.com>
2025-11-03 17:21:44 +0000

    P2-2869-Tests move to a seperate file

--- src/EmployerFeedback/SFA.DAS.EmployerFeedback.UnitTests/Models/SendFeedbackEmailsRequestTests.cs
+++ src/EmployerFeedback/SFA.DAS.EmployerFeedback.UnitTests/Models/SendFeedbackEmailsRequestTests.cs
@@ -36,24 +36,4 @@ namespace SFA.DAS.EmployerFeedback.UnitTests.Models
             Assert.That(request.EmployerAccountsBaseUrl, Is.EqualTo("https://test.com"));
         }
     }
-
-    [TestFixture]
-    public class NotificationTemplateRequestTests
-    {
-        [Test]
-        public void Then_SetsPropertiesCorrectly()
-        {
-            var templateName = "TestTemplate";
-            var templateId = Guid.NewGuid();
-
-            var template = new NotificationTemplateRequest
-            {
-                TemplateName = templateName,
-                TemplateId = templateId
-            };
-
-            Assert.That(template.TemplateName, Is.EqualTo(templateName));
-            Assert.That(template.TemplateId, Is.EqualTo(templateId));
-        }
-    }
 }
