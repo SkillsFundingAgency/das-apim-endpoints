9ae0bdda7a78e928eb3c4fd6fd1647a178636509
Chris Woodcock <chris@humesoftware.com>
2025-11-12 14:38:55 +0000

    Added get certificates endpoint

---
+++ src/DigitalCertificates/SFA.DAS.DigitalCertificates/Application/Queries/GetCertificates/GetCertificatesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace SFA.DAS.DigitalCertificates.Application.Queries.GetCertificates
+{
+    public class GetCertificatesQuery : IRequest<GetCertificatesResult>
+    {
+        public Guid UserId { get; set; }
+    }
+}
---
+++ src/DigitalCertificates/SFA.DAS.DigitalCertificates/Application/Queries/GetCertificates/GetCertificatesQueryHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.DigitalCertificates.InnerApi.Requests;
+using SFA.DAS.DigitalCertificates.InnerApi.Requests.Assessor;
+using SFA.DAS.DigitalCertificates.InnerApi.Responses;
+using SFA.DAS.DigitalCertificates.Models;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
+using SFA.DAS.SharedOuterApi.Interfaces;
+using SFA.DAS.SharedOuterApi.Models;
+using User = SFA.DAS.DigitalCertificates.InnerApi.Responses.User;
+
+namespace SFA.DAS.DigitalCertificates.Application.Queries.GetCertificates
+{
+    public class GetCertificatesQueryHandler : IRequestHandler<GetCertificatesQuery, GetCertificatesResult>
+    {
+        private readonly IDigitalCertificatesApiClient<DigitalCertificatesApiConfiguration> _digitalCertificatesApiClient;
+        private readonly IAssessorsApiClient<AssessorsApiConfiguration> _assessorsApiClient;
+
+        public GetCertificatesQueryHandler(IDigitalCertificatesApiClient<DigitalCertificatesApiConfiguration> digitalCertificatesApiClient, IAssessorsApiClient<AssessorsApiConfiguration> assessorsApiClient)
+        {
+            _digitalCertificatesApiClient = digitalCertificatesApiClient;
+            _assessorsApiClient = assessorsApiClient;
+        }
+
+        public async Task<GetCertificatesResult> Handle(GetCertificatesQuery request, CancellationToken cancellationToken)
+        {
+            var result = new GetCertificatesResult();
+
+            ApiResponse<GetAuthorisationResponse> authorisationResponse = await _digitalCertificatesApiClient.
+                GetWithResponseCode<GetAuthorisationResponse>(new GetAuthorisationRequest(request.UserId));
+
+            if (authorisationResponse?.StatusCode != System.Net.HttpStatusCode.NotFound)
+            {
+                authorisationResponse.EnsureSuccessStatusCode();
+                var ulnAuthorisation = authorisationResponse.Body.Authorisation;
+
+                ApiResponse<GetCertificatesResponse> certificatesResponse= await _assessorsApiClient.
+                    GetWithResponseCode<GetCertificatesResponse>(new GetCertificatesRequest(ulnAuthorisation.Uln));
+
+                if (certificatesResponse?.StatusCode != System.Net.HttpStatusCode.NotFound)
+                {
+                    certificatesResponse.EnsureSuccessStatusCode();
+
+                    result.Authorisation = ulnAuthorisation;
+                    result.Certificates = certificatesResponse.Body.Certificates;
+                }
+            }
+
+            return result;
+        }
+    }
+}
