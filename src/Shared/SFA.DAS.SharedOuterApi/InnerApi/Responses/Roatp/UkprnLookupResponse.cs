54d3afcc0000e4c40a81965cc8796a05b6b119c4
Afzal Hussain <afi259@hotmail.co.uk>
2025-11-12 18:25:10 +0000

    CSP-2280 Implemented handler

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Roatp/UkprnLookupResponse.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+public class UkprnLookupResponse
+{
+    public bool Success { get; set; }
+    public List<ProviderDetails> Results { get; set; }
+}
