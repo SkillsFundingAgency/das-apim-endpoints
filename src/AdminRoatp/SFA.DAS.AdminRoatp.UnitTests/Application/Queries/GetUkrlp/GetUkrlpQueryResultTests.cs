a6a1e7335ff41d2272b1a58120ab308e6e883bc7
Afzal Hussain <afi259@hotmail.co.uk>
2025-11-13 16:07:04 +0000

    CSP-2280 Controller added

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/Application/Queries/GetUkrlp/GetUkrlpQueryResultTests.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using SFA.DAS.AdminRoatp.Application.Queries.GetUkrlp;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.Application.Queries.GetUkrlp;
+public class GetUkrlpQueryResultTests
+{
+    [Test]
+    public void ImplicitConversion_AllDataPresent_MapsAllDataCorrectly()
+    {
+        // Arrange
+        var providerDetails = new ProviderDetails
+        {
+            ProviderName = "TestName1",
+            ProviderAliases = new List<ProviderAlias>
+            {
+                new ProviderAlias
+                {
+                    Alias = "TestAlias1"
+                },
+            },
+            VerificationDetails = new List<VerificationDetails>
+            {
+                new VerificationDetails
+                {
+                    VerificationAuthority = "charity commission",
+                    VerificationId = "12345"
+                },
+                new VerificationDetails
+                {
+                    VerificationAuthority = "companies house",
+                    VerificationId = "67890"
+                }
+            }
+        };
+
+        // Act
+        GetUkrlpQueryResult sut = providerDetails;
+
+        // Assert
+        sut.LegalName.Should().Be("TestName1");
+        sut.TradingName.Should().Be("TestAlias1");
+        sut.CharityNumber.Should().Be("12345");
+        sut.CompanyNumber.Should().Be("67890");
+    }
+
+    [Test]
+    public void ImplicitConversion_MissingVerificationDetails_MapsCharityAndCompanyNumberAsNull()
+    {
+        // Arrange
+        var providerDetails = new ProviderDetails
+        {
+            ProviderName = "TestName1",
+            ProviderAliases = new List<ProviderAlias>
+            {
+                new ProviderAlias
+                {
+                    Alias = "TestAlias1"
+                },
+            },
+            VerificationDetails = new List<VerificationDetails>()
+        };
+
+        // Act
+        GetUkrlpQueryResult sut = providerDetails;
+
+        // Assert
+        sut.LegalName.Should().Be("TestName1");
+        sut.TradingName.Should().Be("TestAlias1");
+        sut.CharityNumber.Should().BeNull();
+        sut.CompanyNumber.Should().BeNull();
+    }
+
+    [TestCase("charity commission", "12345", "12345", null)]
+    [TestCase("companies house", "67890", null, "67890")]
+    [TestCase("test authority", "54321", null, null)]
+    public void ImplicitConversion_VariationOfVerificationDetails_MapsCharityAndCompanyNumberCorrectly(string verificationAuthority, string verificationId, string? expectedCharityNumber, string? expectedCompanyNumber)
+    {
+        // Arrange
+        var providerDetails = new ProviderDetails
+        {
+            ProviderName = "TestName1",
+            ProviderAliases = new List<ProviderAlias>
+            {
+                new ProviderAlias
+                {
+                    Alias = "TestAlias1"
+                },
+            },
+            VerificationDetails = new List<VerificationDetails>
+            {
+                new VerificationDetails
+                {
+                    VerificationAuthority = verificationAuthority,
+                    VerificationId = verificationId
+                }
+            }
+        };
+
+        // Act
+        GetUkrlpQueryResult sut = providerDetails;
+
+        // Assert
+        sut.LegalName.Should().Be("TestName1");
+        sut.TradingName.Should().Be("TestAlias1");
+        sut.CharityNumber.Should().Be(expectedCharityNumber);
+        sut.CompanyNumber.Should().Be(expectedCompanyNumber);
+    }
+}
