2f215d27f12c1178097683daed40c52afb14dc0d
afzal-hussain259 <afi259@hotmail.co.uk>
2025-11-18 14:05:22 +0000

    CSP 2280 Implement query ukrlp endpoint (#2504)
    * CSP-2280 Implemented handler
    
    * CSP-2280 Controller added
    
    * CSP-2280 unit tests updated
    
    * CSP-2280 Unit test changes
    
    * CSP-2280 Further updates to unit test for code coverage
    
    * CSP-2280 Comments addressed

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/Roatp/GetUkrlpRequest.cs
@@ -0,0 +1,13 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+public class GetUkrlpRequest : IGetApiRequest
+{
+    int Ukprn { get; set; }
+
+    public GetUkrlpRequest(int ukprn)
+    {
+        Ukprn = ukprn;
+    }
+    public string GetUrl => $"/organisations/{Ukprn}/ukrlp-data";
+}
