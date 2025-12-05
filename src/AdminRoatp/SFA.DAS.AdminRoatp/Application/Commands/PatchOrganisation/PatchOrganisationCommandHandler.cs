78120bcbb0e567da5e57be0c29a56b36ef8eeb21
MarkFCain <mark.cain@digital.education.gov.uk>
2025-12-05 15:04:59 +0000

    CSP-2346 patch organisation type changed to main

--- src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Commands/PatchOrganisation/PatchOrganisationCommandHandler.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Commands/PatchOrganisation/PatchOrganisationCommandHandler.cs
@@ -1,14 +1,44 @@
 ﻿using System.Net;
 using MediatR;
+using SFA.DAS.AdminRoatp.Application.Commands.CreateProvider;
+using SFA.DAS.AdminRoatp.Application.Queries.GetOrganisation;
 using SFA.DAS.AdminRoatp.Infrastructure;
+using SFA.DAS.AdminRoatp.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Roatp;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp.Common;
+using SFA.DAS.SharedOuterApi.Interfaces;
 
 namespace SFA.DAS.AdminRoatp.Application.Commands.PatchOrganisation;
 
-public class PatchOrganisationCommandHandler(IRoatpServiceRestApiClient _roatpServiceApiClient) : IRequestHandler<PatchOrganisationCommand, HttpStatusCode>
+public class PatchOrganisationCommandHandler(IRoatpServiceRestApiClient _roatpServiceApiClient, IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> _roatpV2ApiClient, IRoatpServiceApiClient<RoatpConfiguration> _roatpApiClient) : IRequestHandler<PatchOrganisationCommand, HttpStatusCode>
 {
     public async Task<HttpStatusCode> Handle(PatchOrganisationCommand request, CancellationToken cancellationToken)
     {
         HttpResponseMessage response = await _roatpServiceApiClient.PatchOrganisation(request.Ukprn, request.UserId, request.PatchDoc, cancellationToken);
+
+        var operations = request.PatchDoc.Operations;
+
+        var isChangeOfTypeToMain = operations.Any(x => x.path == "/ProviderType" && x.value.ToString() == ProviderType.Main.ToString());
+
+        if (response.IsSuccessStatusCode && isChangeOfTypeToMain)
+        {
+            var organisationResponse = await _roatpApiClient.GetWithResponseCode<OrganisationResponse>(new GetOrganisationRequest(request.Ukprn));
+            GetOrganisationQueryResult organisationDetails = organisationResponse.Body;
+            var createProviderModel = new CreateProviderModel
+            {
+                Ukprn = request.Ukprn,
+                UserId = request.UserId,
+                UserDisplayName = request.UserName,
+                LegalName = organisationDetails.LegalName,
+                TradingName = organisationDetails.TradingName
+            };
+
+            var postProviderRequest = new PostProviderRequest(createProviderModel);
+            await _roatpV2ApiClient.PostWithResponseCode<int>(postProviderRequest);
+        }
+
         return response.StatusCode;
     }
 }
