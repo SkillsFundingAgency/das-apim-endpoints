e0b385ea8a6849c04ebf0733f952f5962ecf5440
Chris Foster <chrisfoster186@googlemail.com>
2025-12-10 10:36:29 +0000

    WIP - new tests

---
+++ src/LearnerData/SFA.DAS.LearnerData.UnitTests/Extensions/LearningUpdateChangesExtensionsTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.LearnerData;
+using SFA.DAS.LearnerData.Extensions;
+
+namespace SFA.DAS.LearnerData.UnitTests.Extensions
+{
+    [TestFixture]
+    public class LearningUpdateChangesExtensionsTests
+    {
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.ExpectedEndDate)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.CompletionDate)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.Withdrawal)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.ReverseWithdrawal)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.BreakInLearningStarted)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.BreakInLearningRemoved)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.BreaksInLearningUpdated)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.Prices)]
+        public void HasOnProgrammeUpdate_ReturnsTrue_ForOnProgrammeChanges(UpdateLearnerApiPutResponse.LearningUpdateChanges change)
+        {
+            var changes = new List<UpdateLearnerApiPutResponse.LearningUpdateChanges> { change };
+
+            changes.HasOnProgrammeUpdate().Should().BeTrue();
+        }
+
+        [Test]
+        public void HasOnProgrammeUpdate_ReturnsFalse_WhenNoRelevantChanges()
+        {
+            var changes = new List<UpdateLearnerApiPutResponse.LearningUpdateChanges>
+            {
+                UpdateLearnerApiPutResponse.LearningUpdateChanges.MathsAndEnglish
+            };
+
+            changes.HasOnProgrammeUpdate().Should().BeFalse();
+        }
+
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.MathsAndEnglish)]
+        [TestCase(UpdateLearnerApiPutResponse.LearningUpdateChanges.MathsAndEnglishWithdrawal)]
+        public void HasEnglishAndMathsUpdate_ReturnsTrue_ForEnglishAndMathsChanges(UpdateLearnerApiPutResponse.LearningUpdateChanges change)
+        {
+            var changes = new List<UpdateLearnerApiPutResponse.LearningUpdateChanges> { change };
+
+            changes.HasEnglishAndMathsUpdate().Should().BeTrue();
+        }
+
+        [Test]
+        public void HasEnglishAndMathsUpdate_ReturnsFalse_WhenNoRelevantChanges()
+        {
+            var changes = new List<UpdateLearnerApiPutResponse.LearningUpdateChanges>
+            {
+                UpdateLearnerApiPutResponse.LearningUpdateChanges.ExpectedEndDate
+            };
+
+            changes.HasEnglishAndMathsUpdate().Should().BeFalse();
+        }
+
+        [Test]
+        public void HasLearningSupportUpdate_ReturnsTrue_WhenLearningSupportChangePresent()
+        {
+            var changes = new List<UpdateLearnerApiPutResponse.LearningUpdateChanges>
+            {
+                UpdateLearnerApiPutResponse.LearningUpdateChanges.LearningSupport
+            };
+
+            changes.HasLearningSupportUpdate().Should().BeTrue();
+        }
+
+        [Test]
+        public void HasLearningSupportUpdate_ReturnsFalse_WhenNoRelevantChanges()
+        {
+            var changes = new List<UpdateLearnerApiPutResponse.LearningUpdateChanges>
+            {
+                UpdateLearnerApiPutResponse.LearningUpdateChanges.ExpectedEndDate
+            };
+
+            changes.HasLearningSupportUpdate().Should().BeFalse();
+        }
+    }
+}
