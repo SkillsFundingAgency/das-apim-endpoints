61d6c595104aad0252c0d9b23299503223173d4a
killij <51908793+killij@users.noreply.github.com>
2025-12-19 15:25:59 +0000

    FAI-2934 Add RecruitArtificialIntelligenceService

---
+++ src/Recruit/SFA.DAS.Recruit/Global.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+
+namespace SFA.DAS.Recruit;
+
+public class Global
+{
+    public static JsonSerializerOptions JsonSerializerOptions => new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+}
