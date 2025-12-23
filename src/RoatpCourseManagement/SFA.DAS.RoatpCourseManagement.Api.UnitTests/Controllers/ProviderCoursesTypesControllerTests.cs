bb06ba112ceb8b4ea1b849f202716fbc0caa8426
Mark Cain <31033361+MarkFCain@users.noreply.github.com>
2025-12-23 11:15:57 +0000

    CSP-2342 Get CourseTypes by provider (#2577)

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api.UnitTests/Controllers/ProviderCoursesTypesControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Api.Controllers;
+using SFA.DAS.RoatpCourseManagement.Application.ProviderCourseTypes.Queries;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.UnitTests.Controllers
+{
+    [TestFixture]
+    public class ProviderCoursesTypesControllerTests
+    {
+        const int ValidUkprn = 101;
+        [TestCase(0, 404)]
+        [TestCase(ValidUkprn, 200)]
+        public async Task GetProviderCourseTypes_ReturnsAppropriateResponse(int ukprn, int expectedStatusCode)
+        {
+            var mediatorMock = new Mock<IMediator>();
+            mediatorMock.Setup(m => m.Send(It.Is<GetProviderCourseTypesQuery>(q => q.Ukprn == ValidUkprn), It.IsAny<CancellationToken>())).ReturnsAsync(new List<ProviderCourseTypeResult>());
+            var subject = new ProviderCourseTypesController(mediatorMock.Object, Mock.Of<ILogger<ProviderCourseTypesController>>());
+
+            var response = await subject.GetProviderCourseTypes(ukprn);
+
+            var statusCodeResult = response as IStatusCodeActionResult;
+
+            Assert.That(expectedStatusCode, Is.EqualTo(statusCodeResult!.StatusCode.GetValueOrDefault()));
+        }
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/ProviderCourseTypes/Queries/GetProviderCourseTypesQuery.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MediatR;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.ProviderCourseTypes.Queries;
+public class GetProviderCourseTypesQuery : IRequest<List<ProviderCourseTypeResult>>
+{
+    public int Ukprn { get; }
+    public GetProviderCourseTypesQuery(int ukprn)
+    {
+        Ukprn = ukprn;
+    }
+}
