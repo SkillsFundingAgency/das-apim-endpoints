1fe9f3c2b3960f1d54ec7af1e13486c3539dfe9e
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2025-12-23 13:09:38 +0000

    FAI-2949 - Add new request and response for recruit call

---
+++ src/EmployerAccounts/SFA.DAS.EmployerAccounts.UnitTests/Application/InnerApi/Requests/WhenBuildingGetVacanciesByAccountIdApiRequest.cs
@@ -0,0 +1,17 @@
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerAccounts.InnerApi.Requests;
+
+namespace SFA.DAS.EmployerAccounts.UnitTests.Application.InnerApi.Requests;
+
+public class WhenBuildingGetVacanciesByAccountIdApiRequest
+{
+    [Test, AutoData]
+    public void Then_The_Url_Is_Correctly_Build(long accountId)
+    {
+        var actual = new GetVacanciesByAccountIdApiRequest(accountId);
+        
+        actual.GetUrl.Should().Be($"api/accounts/{accountId}/vacancies?page=1&pageSize=1&sortColumn=&sortOrder=Desc&filterBy=All&searchTerm=");
+    }
+}
