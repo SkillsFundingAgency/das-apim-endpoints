7ba36b744b4ed44ee80e51aaff261e39cc0e6192
Deven Shah <devenshah1974@gmail.com>
2026-01-04 21:59:36 +0000

    CSP-2366 Organised endpoints in swagger using tags

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderCourseLocationDeleteController.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderCourseLocationDeleteController.cs
@@ -1,45 +1,47 @@
 ﻿using System;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.BulkDeleteProviderCourse;
 using SFA.DAS.RoatpCourseManagement.Application.Standards.Commands.DeleteProviderCourseLocation;
 
-namespace SFA.DAS.RoatpCourseManagement.Api.Controllers
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers;
+
+[ApiController]
+[Tags("Provider Course Location")]
+[Route("")]
+public class ProviderCourseLocationDeleteController : ControllerBase
 {
-    [ApiController]
-    public class ProviderCourseLocationDeleteController : ControllerBase
+    private readonly IMediator _mediator;
+    private readonly ILogger<ProviderCourseLocationDeleteController> _logger;
+
+    public ProviderCourseLocationDeleteController(ILogger<ProviderCourseLocationDeleteController> logger, IMediator mediator)
     {
-        private readonly IMediator _mediator;
-        private readonly ILogger<ProviderCourseLocationDeleteController> _logger;
+        _logger = logger;
+        _mediator = mediator;
+    }
 
-        public ProviderCourseLocationDeleteController(ILogger<ProviderCourseLocationDeleteController> logger, IMediator mediator)
-        {
-            _logger = logger;
-            _mediator = mediator;
-        }
+    [HttpPost]
+    [Route("providers/{ukprn}/courses/{larsCode}/bulk-delete-course-locations")]
+    public async Task<IActionResult> BulkDeleteProviderCourseLocations(int ukprn, string larsCode, BulkDeleteProviderCourseLocationsCommand command)
+    {
+        _logger.LogInformation("Outer API: Request received to bulk delete provider course locations for ukprn: {Ukprn} larscode: {Larscode} option: {DeleteOption}", ukprn, larsCode, command.DeleteProviderCourseLocationOption);
+        command.Ukprn = ukprn;
+        command.LarsCode = larsCode;
+        await _mediator.Send(command);
+        return NoContent();
+    }
 
-        [HttpPost]
-        [Route("providers/{ukprn}/courses/{larsCode}/bulk-delete-course-locations")]
-        public async Task<IActionResult> BulkDeleteProviderCourseLocations(int ukprn, string larsCode, BulkDeleteProviderCourseLocationsCommand command)
-        {
-            _logger.LogInformation("Outer API: Request received to bulk delete provider course locations for ukprn: {ukprn} larscode: {larscode} option: {deleteOption}", ukprn, larsCode, command.DeleteProviderCourseLocationOption);
-            command.Ukprn = ukprn;
-            command.LarsCode = larsCode;
-            await _mediator.Send(command);
-            return NoContent();
-        }
-
-        [HttpPost]
-        [Route("providers/{ukprn}/courses/{larsCode}/location/{id}/delete")]
-        public async Task<IActionResult> DeleteProviderCourseLocation(int ukprn, string larsCode, Guid id, DeleteProviderCourseLocationCommand command)
-        {
-            _logger.LogInformation("Outer API: Request received to delete provider course location for ukprn: {ukprn} larscode: {larscode} providerCourseLocationId: {id}", ukprn, larsCode, id);
-            command.Ukprn = ukprn;
-            command.LarsCode = larsCode;
-            await _mediator.Send(command);
-            return NoContent();
-        }
+    [HttpPost]
+    [Route("providers/{ukprn}/courses/{larsCode}/location/{id}/delete")]
+    public async Task<IActionResult> DeleteProviderCourseLocation(int ukprn, string larsCode, Guid id, DeleteProviderCourseLocationCommand command)
+    {
+        _logger.LogInformation("Outer API: Request received to delete provider course location for ukprn: {Ukprn} larscode: {Larscode} providerCourseLocationId: {LocationId}", ukprn, larsCode, id);
+        command.Ukprn = ukprn;
+        command.LarsCode = larsCode;
+        await _mediator.Send(command);
+        return NoContent();
     }
 }
