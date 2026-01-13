20e7e52de68c5d2fe27802d3e6967e946e3c8d38
Afzal Hussain <afi259@hotmail.co.uk>
2026-01-13 18:21:14 +0000

    CSP-2409 Fixed get provider course type to remove null response

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/ProviderCourseTypes/Queries/GetProviderCourseTypesQueryHandler.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/ProviderCourseTypes/Queries/GetProviderCourseTypesQueryHandler.cs
@@ -1,31 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
-using MediatR;
+﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.RoatpCourseManagement.Application.ProviderCourseTypes.Queries;
 public class GetProviderCourseTypesQueryHandler(IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> _courseManagementApiClient, ILogger<GetProviderCourseTypesQueryHandler> _logger) : IRequestHandler<GetProviderCourseTypesQuery, List<ProviderCourseTypeResult>>
 {
     public async Task<List<ProviderCourseTypeResult>> Handle(GetProviderCourseTypesQuery query, CancellationToken cancellationToken)
     {
-        try
-        {
-            var courses = await _courseManagementApiClient.Get<List<ProviderCourseTypeResult>>(new GetProviderCourseTypesRequest(query.Ukprn));
-            if (courses != null) return courses;
+        var courses = await _courseManagementApiClient.GetWithResponseCode<List<ProviderCourseTypeResult>>(new GetProviderCourseTypesRequest(query.Ukprn));
 
-            _logger.LogInformation("Courses data not found for {Ukprn}", query.Ukprn);
-            return null;
+        _logger.LogInformation("Hnadling GetProviderCourseTypes request for {Ukprn}", query.Ukprn);
 
-        }
-        catch (Exception ex)
+        if (courses.StatusCode != HttpStatusCode.OK)
         {
-            _logger.LogError(ex, "Error occurred trying to retrieve Course types for Ukprn {Ukprn}", query.Ukprn);
-            throw;
+            _logger.LogError("Error occurred trying to retrieve Course types for Ukprn {Ukprn}", query.Ukprn);
+            throw new InvalidOperationException();
         }
+
+        return courses.Body;
     }
 }
