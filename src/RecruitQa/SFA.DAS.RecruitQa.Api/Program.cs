c9ec0ba20cf3b9fa174094114f7d6382ea230162
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-01-20 14:41:56 +0000

    FAI-2846 - Add nservicebus and other configuration

--- src/RecruitQa/SFA.DAS.RecruitQa.Api/Program.cs
+++ src/RecruitQa/SFA.DAS.RecruitQa.Api/Program.cs
@@ -1,8 +1,13 @@
+using NServiceBus.ObjectBuilder.MSDependencyInjection;
 using SFA.DAS.RecruitQa.Api;
 using SFA.DAS.SharedOuterApi.AppStart;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration.BuildSharedConfiguration();
+builder.Host.ConfigureContainer<UpdateableServiceProvider>(containerBuilder =>
+{
+    Task.FromResult(containerBuilder.StartNServiceBus(configuration, "SFA.DAS.APIMRecruitQA"));
+});
 Startup.ConfigureServices(builder.Services, builder.Environment, configuration);
 var app = builder.Build();
 Startup.ConfigureApp(app, configuration);
