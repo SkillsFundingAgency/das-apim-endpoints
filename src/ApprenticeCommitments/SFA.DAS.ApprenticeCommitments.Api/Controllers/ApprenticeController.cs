2ae095e39fe7729a175a7bad46e7b588180a3a9b
NWake-CGI <nick.wake@cgi.com>
2026-01-26 17:15:26 +0000

    Find By Personal Details & Find By Uln Requests added to commitments api

--- src/ApprenticeCommitments/SFA.DAS.ApprenticeCommitments.Api/Controllers/ApprenticeController.cs
+++ src/ApprenticeCommitments/SFA.DAS.ApprenticeCommitments.Api/Controllers/ApprenticeController.cs
@@ -1,9 +1,12 @@
-using Microsoft.AspNetCore.Mvc;
-using SFA.DAS.ApprenticeCommitments.Application.Services;
-using System;
-using System.Threading.Tasks;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.ApprenticeCommitments.Application.Queries.Apprentices;
+using SFA.DAS.ApprenticeCommitments.Application.Queries.Apprenticeships;
+using SFA.DAS.ApprenticeCommitments.Application.Services;
 using SFA.DAS.SharedOuterApi.Apprentice.GovUK.Auth.Controllers;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeCommitments.Api.Controllers
 {
@@ -19,11 +22,33 @@ namespace SFA.DAS.ApprenticeCommitments.Api.Controllers
     public class ApprenticeController : ApprenticeControllerBase
     {
         private readonly ResponseReturningApiClient _client;
+        private readonly IMediator _mediator;
 
-        public ApprenticeController(TemporaryAccountsResponseReturningApiClient client, IMediator mediator) : base(mediator) => _client = client;
+        public ApprenticeController(TemporaryAccountsResponseReturningApiClient client, IMediator mediator) : base(mediator)
+        {
+            _client = client;
+            _mediator = mediator;
+        }
 
         [HttpGet("/apprentices/{id}")]
         public Task<IActionResult> GetApprentice(Guid id)
             => _client.Get($"apprentices/{id}");
+
+        [HttpGet("/apprentices")]
+        public async Task<IActionResult> GetApprenticeAccountByName([FromQuery] GetApprenticeByPersonalDetailQuery request)
+        {
+            var result = await _mediator.Send(request);
+
+            return Ok(result);
+        }
+
+        [HttpGet("/apprentice/{uln}")]
+        public async Task<IActionResult> GetApprenticeshipByUln([FromRoute] int uln)
+        {
+            var request = new GetApprenticeshipByUlnQuery { Uln = uln };
+            var result = await _mediator.Send(request);
+            
+            return Ok(result.MyApprenticeship);
+        }
     }
 }
---
+++ src/ApprenticeCommitments/SFA.DAS.ApprenticeCommitments/Apis/ApprenticeAccountsApi/GetApprenticeshipByUlnRequest.cs
@@ -0,0 +1,21 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ApprenticeCommitments.Apis.ApprenticeAccountsApi
+{
+    public class GetApprenticeshipByUlnRequest : IGetApiRequest
+    {
+        public int Uln { get; set; }
+
+        public GetApprenticeshipByUlnRequest(int uln)
+        {
+            Uln = uln;
+        }
+
+        public string GetUrl => $"apprentice/{Uln}";
+    }
+}
