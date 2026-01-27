d78d69a027ba34b86c5570da5ea3e7b928ff9549
karen-hanna <50406092+karen-hanna@users.noreply.github.com>
2026-01-27 22:39:26 +0000

    Qfast release2 (#2618)
    * added withdraw application
    
    * updated Microsoft,identity.client version
    
    * fixed sonar cloud issues
    
    * modified emailservice and create Message handler to return a bool indicating if one or more emails were sent
    
    ---------
    
    Co-authored-by: Karen Hanna <karen@alveryhanna.com>

--- src/Aodp/SFA.DAS.Aodp/Application/Commands/Application/Application/WithdrawApplicationCommandResponse.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Commands/Application/Application/WithdrawApplicationCommandResponse.cs
@@ -2,7 +2,7 @@
 
 namespace SFA.DAS.Aodp.Application.Commands.Application.Application;
 
-public class SubmitApplicationCommandResponse
+public class WithdrawApplicationCommandResponse
 {
     public IReadOnlyCollection<NotificationDefinition> Notifications { get; init; }
             = Array.Empty<NotificationDefinition>();
