b7c3272387186fff38a9276f6084a3a0f2b1151d
Harsh Paladugu <35364461+phkumar13@users.noreply.github.com>
2026-02-02 11:50:43 +0000

    Updated to include Person Origin and added additional logging

--- src/Campaign/SFA.DAS.Campaign.Api/Controllers/CampaignEnquiryController.cs
+++ src/Campaign/SFA.DAS.Campaign.Api/Controllers/CampaignEnquiryController.cs
@@ -27,20 +27,37 @@ public class CampaignEnquiryController(ILogger<CampaignEnquiryController> logger
     {
         try
         {
+            logger.LogInformation("Register Campaign Interest Outer API: Received request to add user details to campaign");
+
             var request = new PostRegisterInterestApiRequest(userData);
             var response = await apiClient.PostWithResponseCode<EnquiryUserDataModel>(request);
 
-            return response.StatusCode switch
+            switch (response.StatusCode)
             {
-                HttpStatusCode.Created => CreatedAtAction(nameof(RegisterInterest), response),
-                HttpStatusCode.BadRequest => BadRequest(),
-                HttpStatusCode.InternalServerError => StatusCode((int)HttpStatusCode.InternalServerError),
-                _ => throw new InvalidOperationException("Campain Interest didn't come back with a successful response")
-            };
+                case HttpStatusCode.Created:
+                    {
+                        logger.LogInformation("Register Campaign Interest Outer API: Successfully added user details to campaign");
+                        return CreatedAtAction(nameof(RegisterInterest), response);
+                    }
+                case HttpStatusCode.BadRequest:
+                    {
+                        logger.LogWarning("Register Campaign Interest Outer API received Bad request from Inner API");
+                        return BadRequest();
+                    }
+                case HttpStatusCode.InternalServerError:
+                    {
+                        logger.LogError("Register Campaign Interest Outer API received Internal server error from Inner API");
+                        return StatusCode((int)HttpStatusCode.InternalServerError);
+                    }
+                default:
+                    {
+                        throw new InvalidOperationException("Campaign Interest didn't receive a successful response from Inner API");
+                    }
+            }
         }
         catch (Exception e)
         {
-            logger.LogError(e, "Error saving Campain Interest");
+            logger.LogError(e, "Error attempting to register Campaign Interest");
             return StatusCode((int)HttpStatusCode.InternalServerError);
         }
     }
--- src/Campaign/SFA.DAS.Campaign/Models/EnquiryUserDataModel.cs
+++ src/Campaign/SFA.DAS.Campaign/Models/EnquiryUserDataModel.cs
@@ -5,18 +5,12 @@ namespace SFA.DAS.Campaign.Models;
 public class EnquiryUserDataModel
 {
     public string FirstName { get; set; }
-
     public string LastName { get; set; }
-
     public string Email { get; set; }
-
     public string UkEmployerSize { get; set; }
-
     public string PrimaryIndustry { get; set; }
-
     public string PrimaryLocation { get; set; }
-
     public DateTime AppsgovSignUpDate { get; set; }
-
+    public string PersonOrigin { get; set; }
     public bool IncludeInUR { get; set; }
 }
