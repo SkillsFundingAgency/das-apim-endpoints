fdbb34641c269c791292fcaf2741d12bdfba136a
Chris Foster <chrisfoster186@googlemail.com>
2026-02-03 11:58:39 +0000

    Initial boilerplate for new shortCourse POST endpoint

---
+++ src/LearnerData/SFA.DAS.LearnerData.Api/Controllers/ShortCoursesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.LearnerData.Requests;
+
+namespace SFA.DAS.LearnerData.Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShortCoursesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/providers/{ukprn}/shortCourses")]
+        public IActionResult CreateShortCourse(ShortCourseRequest request)
+        {
+            //echo the input
+            return Ok(request);
+        }
+    }
+}
---
+++ src/LearnerData/SFA.DAS.LearnerData/Requests/ShortCourseRequest.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SFA.DAS.LearnerData.Requests
+{
+    public class ShortCourseRequest
+    {
+        [Required]
+        public LearnerRequestDetails Learner { get; set; }
+
+        [Required]
+        public ShortCourseDelivery Delivery { get; set; }
+    }
+
+    public class ShortCourseDelivery
+    {
+        [Required]
+        public List<ShortCourseOnProgramme> OnProgramme { get; set; }
+    }
+
+    public class ShortCourseOnProgramme
+    {
+        [Required]
+        public string? CourseCode { get; set; }
+        public string? AgreementId { get; set; }
+        [Required]
+        public DateTime? StartDate { get; set; }
+        [Required]
+        public DateTime? ExpectedEndDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public DateTime? WithdrawalDate { get; set; }
+        [Required]
+        public List<LearningSupportRequestDetails> LearningSupport { get; set; }
+        [Required]
+        public DateTime? PauseDate { get; set; }
+        [Required]
+        public int? AimSequenceNumber { get; set; }
+        public DateTime? ActualEndDate { get; set; }
+        public Milestone[] Milestones { get; set; }
+    }
+
+    public enum Milestone
+    {
+        ThirtyPercentLearningComplete,
+        LearningComplete
+    }
+}
