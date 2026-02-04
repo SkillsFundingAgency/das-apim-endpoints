22f46030b127c691d49edd3e4f712a7105b9d520
Karen Hanna <karen@alveryhanna.com>
2026-02-04 10:42:01 +0000

    allow nulls for SaveReviewerCommand

--- src/Aodp/SFA.DAS.Aodp/Application/Commands/Application/Review/SaveReviewerCommand.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Commands/Application/Review/SaveReviewerCommand.cs
@@ -5,10 +5,10 @@ namespace SFA.DAS.Aodp.Application.Commands.Application.Review
     public class SaveReviewerCommand : IRequest<BaseMediatrResponse<SaveReviewerCommandResponse>>
     {
         public Guid ApplicationId { get; set; }
-        public string ReviewerFieldName { get; set; }
+        public required string ReviewerFieldName { get; set; }
         public string? ReviewerValue { get; set; }
-        public string UserType { get; set; }
-        public string SentByName { get; set; }
-        public string SentByEmail { get; set; }
+        public required string UserType { get; set; }
+        public required string SentByName { get; set; }
+        public required string SentByEmail { get; set; }
     }
 }
