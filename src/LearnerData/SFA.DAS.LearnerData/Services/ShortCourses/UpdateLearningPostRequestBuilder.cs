78046be658802560ccbce3d9c9bbf3750f0a560e
Chris Foster <chrisfoster186@googlemail.com>
2026-02-05 10:19:01 +0000

    New classes 'n' stuff

---
+++ src/LearnerData/SFA.DAS.LearnerData/Services/ShortCourses/UpdateLearningPostRequestBuilder.cs
@@ -0,0 +1,18 @@
+﻿using SFA.DAS.LearnerData.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.LearnerData.ShortCourses;
+
+namespace SFA.DAS.LearnerData.Services.ShortCourses
+{
+    public interface IUpdateLearningPostRequestBuilder
+    {
+        CreateDraftShortCourseApiPostRequest Build(ShortCourseRequest request);
+    }
+
+    public class UpdateLearningPostRequestBuilder : IUpdateLearningPostRequestBuilder
+    {
+        public CreateDraftShortCourseApiPostRequest Build(ShortCourseRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/LearnerData/ShortCourses/CreateDraftShortCourseApiPostRequest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.LearnerData.ShortCourses
+{
+    /// <summary>
+    /// Request to create a draft short course learner record
+    /// </summary>
+    public class CreateDraftShortCourseApiPostRequest
+    {
+        /// <summary>
+        /// Learner details to be updated
+        /// </summary>
+        public LearningUpdateDetails LearnerUpdateDetails { get; set; }
+
+        /// <summary>
+        /// Learning support details
+        /// </summary>
+        public List<LearningSupportUpdatedDetails> LearningSupport { get; set; } = new();
+
+        /// <summary>
+        /// On programme details
+        /// </summary>
+        public OnProgramme OnProgramme { get; set; }
+    }
+
+    /// <summary>
+    /// On programme details
+    /// </summary>
+    public class OnProgramme
+    {
+        /// <summary>
+        /// Course code
+        /// </summary>
+        public string CourseCode { get; set; } = null!;
+
+        /// <summary>
+        /// Employer identifier
+        /// </summary>
+        public long EmployerId { get; set; }
+
+        /// <summary>
+        /// Provider UKPRN
+        /// </summary>
+        public long Ukprn { get; set; }
+
+        /// <summary>
+        /// Start date of the short course
+        /// </summary>
+        public DateTime StartDate { get; set; }
+
+        /// <summary>
+        /// Withdrawal date of the short course
+        /// </summary>
+        public DateTime? WithdrawalDate { get; set; }
+
+        /// <summary>
+        /// Completion date of the short course
+        /// </summary>
+        public DateTime? CompletionDate { get; set; }
+
+        /// <summary>
+        /// Expected end date of the short course
+        /// </summary>
+        public DateTime ExpectedEndDate { get; set; }
+
+        /// <summary>
+        /// Milestones for the short course
+        /// </summary>
+        public List<Milestone> Milestones { get; set; } = new();
+
+        /// <summary>
+        /// Price of the short course
+        /// </summary>
+        public decimal Price { get; set; }
+    }
+
+    public enum Milestone
+    {
+        ThirtyPercentLearningComplete,
+        LearningComplete
+    }
+}
