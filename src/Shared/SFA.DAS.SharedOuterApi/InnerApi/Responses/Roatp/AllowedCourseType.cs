2f2eb436075df8553c77daf5c73676a1ae9d108d
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2026-02-05 15:13:22 +0000

    CSP-2447 Align Course Types - rename ApprenticeshipUnit to ShortCourse (#2635)
    CSP-2447 Align Course Types - rename ApprenticeshipUnit to ShortCourse (#2635)

--- src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Roatp/AllowedCourseType.cs
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Roatp/AllowedCourseType.cs
@@ -1,3 +1,3 @@
 ﻿namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
 
-public record AllowedCourseType(int CourseTypeId, string CourseTypeName);
+public record AllowedCourseType(int CourseTypeId, CourseType CourseTypeName);
