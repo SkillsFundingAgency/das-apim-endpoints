f0bed0ecd3a2c2301f80a7fba91a082d34dc5336
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-02-06 13:00:24 +0000

    CSP-2453 Added caching for standard details by LARS code

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Services/CachedStandardDetailsService.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Courses;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.FindApprenticeshipTraining.Services;
+
+public class CachedStandardDetailsService : ICachedStandardDetailsService
+{
+    private const int StandardDetailsCacheDurationInHours = 24;
+
+    ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
+    private readonly ICacheStorageService _cacheStorageService;
+
+    public CachedStandardDetailsService(
+        ICoursesApiClient<CoursesApiConfiguration> coursesApiClient,
+        ICacheStorageService cacheStorageService)
+    {
+        _coursesApiClient = coursesApiClient;
+        _cacheStorageService = cacheStorageService;
+    }
+
+    public async Task<StandardDetailResponse> GetStandardDetails(string larsCode)
+    {
+        var cachedStandardDetails = await _cacheStorageService
+            .RetrieveFromCache<StandardDetailResponse>(nameof(StandardDetailResponse));
+
+        if (cachedStandardDetails != null)
+            return cachedStandardDetails;
+
+        var apiResponse = await _coursesApiClient.GetWithResponseCode<StandardDetailResponse>(new GetStandardDetailsByIdRequest(larsCode));
+        cachedStandardDetails = apiResponse.Body;
+        await _cacheStorageService.SaveToCache(nameof(StandardDetailResponse), cachedStandardDetails, StandardDetailsCacheDurationInHours);
+
+        return cachedStandardDetails;
+    }
+}
