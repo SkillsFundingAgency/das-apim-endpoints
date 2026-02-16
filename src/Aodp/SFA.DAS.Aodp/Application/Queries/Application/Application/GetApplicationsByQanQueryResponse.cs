fe2de56b15cce7b6105fe2968ee241b56ef1fdb6
DAR, Adeel <Adeel.DAR@EDUCATION.GOV.UK>
2026-02-16 16:54:58 +0000

    Added the Status and ReferenceId fields of the applications

--- src/Aodp/SFA.DAS.Aodp/Application/Queries/Application/Application/GetApplicationsByQanQueryResponse.cs
+++ src/Aodp/SFA.DAS.Aodp/Application/Queries/Application/Application/GetApplicationsByQanQueryResponse.cs
@@ -10,5 +10,7 @@ public class GetApplicationsByQanQueryResponse
         public string? Name { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? SubmittedDate { get; set; }
+        public string? Status { get; set; }
+        public int ReferenceId { get; set; }
     }
 }
