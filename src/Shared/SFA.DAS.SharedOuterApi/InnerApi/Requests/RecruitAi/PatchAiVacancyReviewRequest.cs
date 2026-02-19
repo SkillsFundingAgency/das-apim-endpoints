5813a94fcfe7397e596b7592b634d2d7d60984b2
killij <51908793+killij@users.noreply.github.com>
2026-02-19 09:24:35 +0000

    FAI-3002 Add request/responses

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/RecruitAi/PatchAiVacancyReviewRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.JsonPatch;
+using SFA.DAS.SharedOuterApi.Domain.Recruit.Ai;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.RecruitAi;
+
+public struct PatchAiVacancyReviewRequest(Guid vacancyReviewId, JsonPatchDocument<PatchableAiVacancyReviewDto> patchDocument) : IPatchApiRequest<JsonPatchDocument<PatchableAiVacancyReviewDto>>
+{
+    public string PatchUrl => $"api/ai-vacancy-reviews/{vacancyReviewId}";
+    public JsonPatchDocument<PatchableAiVacancyReviewDto> Data { get; set; } = patchDocument;
+}
+
+public class PatchableAiVacancyReviewDto
+{
+    public bool ManualReviewRequired { get; set; }
+    public string? Output { get; set; }
+    public AiReviewStatus Status { get; set; }
+}
---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Recruit/GetVacancyResponse.cs
@@ -0,0 +1,5 @@
+using SFA.DAS.SharedOuterApi.Domain.Recruit;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Recruit;
+
+public class GetVacancyResponse : Vacancy;
