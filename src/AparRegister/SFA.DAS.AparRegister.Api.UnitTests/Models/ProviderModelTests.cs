c25f7ecc013ddd0868658bfa26e7075bcd5ef852
Deven Shah <devenshah1974@gmail.com>
2026-02-23 15:57:12 +0000

    CSP-2441 Implements new status events endpoint for FCS (#2662)
    * CSP-2441 Refactors provider register endpoint to use roatp v1
    
    * CSP-2441 Impements get provider status events endpoint
    
    * CSP-2441 Added missing test

---
+++ src/AparRegister/SFA.DAS.AparRegister.Api.UnitTests/Models/ProviderModelTests.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+using SFA.DAS.AparRegister.Api.Models;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Roatp.Common;
+
+namespace SFA.DAS.AparRegister.Api.UnitTests.Models;
+
+public class ProviderModelTests
+{
+    [Test]
+    public void Operator_ConvertsFromOrganisationResponse()
+    {
+        var source = new OrganisationResponse
+        {
+            Ukprn = 12345678,
+            LegalName = "Test Provider",
+            TradingName = "Test Trading Name",
+            Status = OrganisationStatus.Active,
+            ProviderType = ProviderType.Main
+        };
+        ProviderModel result = source;
+
+        using (new AssertionScope())
+        {
+            result.Ukprn.Should().Be(source.Ukprn);
+            result.Name.Should().Be(source.LegalName);
+            result.TradingName.Should().Be(source.TradingName);
+            result.StatusId.Should().Be((int)source.Status);
+            result.ProviderTypeId.Should().Be((int)source.ProviderType);
+        }
+    }
+
+    [TestCase(ProviderType.Main, true)]
+    [TestCase(ProviderType.Employer, true)]
+    [TestCase(ProviderType.Supporting, false)]
+    public void CanAccessApprenticeshipService_ReturnsExpectedResult(ProviderType providerType, bool expected)
+    {
+        var model = new ProviderModel
+        {
+            ProviderTypeId = (int)providerType,
+            StatusId = (int)OrganisationStatus.Active
+        };
+        model.CanAccessApprenticeshipService.Should().Be(expected);
+    }
+}
