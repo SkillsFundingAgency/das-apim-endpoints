13382724044b51f6da8395705ab967da1ebcb37b
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2026-02-25 13:26:40 +0000

    FLP-1525 Get GSO Learners

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Requests/Learning/GetAllShortCourseLearningsRequest.cs
@@ -0,0 +1,8 @@
+﻿using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Requests.Learning;
+
+public class GetAllShortCourseLearningsRequest(string ukprn, int academicYear, int page, int? pageSize) : IGetApiRequest
+{
+    public string GetUrl => $"/{ukprn}/academicyears/{academicYear}/shortcourses?page={page}&pageSize={pageSize}";
+}
