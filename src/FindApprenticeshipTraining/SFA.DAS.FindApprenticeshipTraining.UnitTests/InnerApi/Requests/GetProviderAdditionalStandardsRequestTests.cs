c0f758257ba87400f473fd995b567c55cb1962b3
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-02-25 16:29:15 +0000

    CSP-2467 Added unit tests for GetProviderAdditionalStandardsRequest

---
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/InnerApi/Requests/GetProviderAdditionalStandardsRequestTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.FindApprenticeshipTraining.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Common;
+
+namespace SFA.DAS.FindApprenticeshipTraining.UnitTests.InnerApi.Requests
+{
+    public class GetProviderAdditionalStandardsRequestTests
+    {
+        [Test, AutoData]
+        public void Version_WhenRequested_ReturnsApiVersionNumberTwo(int providerId)
+        {
+            var req = new GetProviderAdditionalStandardsRequest(providerId);
+            req.Version.Should().Be(ApiVersionNumber.Two);
+        }
+
+        [Test, AutoData]
+        public void GetUrl_GivenProviderId_ReturnsExpectedPath(int providerId)
+        {
+            var req = new GetProviderAdditionalStandardsRequest(providerId);
+            req.GetUrl.Should().Be($"api/providers/{providerId}/courses");
+        }
+    }
+}
