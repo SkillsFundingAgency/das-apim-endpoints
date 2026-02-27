b1b4b55764e86418640bc2f50b337bd1e1d2a018
Corey Faulconbridge <corey.faulconbridge@digital.education.gov.uk>
2026-02-27 13:53:51 +0000

    remove unnecessary prop

--- src/Approvals/SFA.DAS.Approvals/InnerApi/EmploymentCheckApi/Responses/EvsCheckResponse.cs
+++ src/Approvals/SFA.DAS.Approvals/InnerApi/EmploymentCheckApi/Responses/EvsCheckResponse.cs
@@ -6,7 +6,6 @@ public class EvsCheckResponse
 {
     public long EmployerId { get; set; }
     public long ApprenticeshipId { get; set; }
-    public string Ukprn { get; set; }
     public string Uln { get; set; }
     public DateTime? RequestDate { get; set; }
     public DateTime? DateOfCheck { get; set; }
