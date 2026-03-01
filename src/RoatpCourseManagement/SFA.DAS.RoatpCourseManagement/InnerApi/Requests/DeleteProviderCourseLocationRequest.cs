442c21c871389e14541c479115cd1ae573149e68
Deven Shah <devenshah1974@gmail.com>
2026-03-01 22:55:50 +0000

    CSP-2472 Fixed incorrect delete provider course location request

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/DeleteProviderCourseLocationRequest.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/DeleteProviderCourseLocationRequest.cs
@@ -2,15 +2,14 @@
 using System.Web;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
-namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests
+namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+
+public class DeleteProviderCourseLocationRequest : IDeleteApiRequest
 {
-    public class DeleteProviderCourseLocationRequest : IDeleteApiRequest
-    {
-        public int Ukprn { get; set; }
-        public string LarsCode { get; set; }
-        public Guid Id { get; set; }
-        public string UserId { get; set; }
-        public string UserDisplayName { get; set; }
-        public string DeleteUrl => $"/providers/{Ukprn}/courses/{LarsCode}/location/{Id}?userId={HttpUtility.UrlEncode(UserId)}&userDisplayName={HttpUtility.UrlEncode(UserDisplayName)}";
-    }
+    public int Ukprn { get; set; }
+    public string LarsCode { get; set; }
+    public Guid ProviderCourseLocationId { get; set; }
+    public string UserId { get; set; }
+    public string UserDisplayName { get; set; }
+    public string DeleteUrl => $"/providers/{Ukprn}/courses/{LarsCode}/locations/{ProviderCourseLocationId}?userId={HttpUtility.UrlEncode(UserId)}&userDisplayName={HttpUtility.UrlEncode(UserDisplayName)}";
 }
