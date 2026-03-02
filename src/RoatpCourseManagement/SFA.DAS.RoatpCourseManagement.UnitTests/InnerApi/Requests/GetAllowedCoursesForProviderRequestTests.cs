5a6e21571ada0ed40b5b8cabe566f6d6fef5af3e
Deven Shah <devenshah1974@gmail.com>
2026-03-02 16:11:43 +0000

    CSP-2472 Available courses now filters based on allowed courses

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/InnerApi/Requests/GetAllowedCoursesForProviderRequestTests.cs
@@ -0,0 +1,20 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.InnerApi;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.InnerApi.Requests;
+
+public class GetAllowedCoursesForProviderRequestTests
+{
+    [Test, AutoData]
+    public void GetUrl_ReturnsExpectedValue()
+    {
+        int ukprn = 10012002;
+        CourseType courseType = CourseType.ShortCourse;
+        GetAllowedCoursesForProviderRequest sut = new(ukprn, courseType);
+
+        sut.GetUrl.Should().Be("providers/10012002/allowed-courses?courseType=ShortCourse");
+    }
+}
