7abc3d3dc4fa1c24ee87d3f7435c61da6b63e32a
Deven Shah <devenshah1974@gmail.com>
2026-03-06 16:10:49 +0000

    CSP-2512 Implements get provider course forecasts endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.Api/Controllers/ProviderCourseForecastsController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.RoatpCourseManagement.Application.ProviderCourseForecasts.Queries.GetProviderCourseForecasts;
+
+namespace SFA.DAS.RoatpCourseManagement.Api.Controllers;
+
+[ApiController]
+[Tags("Provider Courses")]
+[Route("providers/{ukprn}/courses/{larsCode}/forecasts")]
+public class ProviderCourseForecastsController(IMediator _mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetProviderCourseForecasts([FromRoute] int ukprn, [FromRoute] string larsCode, CancellationToken cancellationToken)
+    {
+        GetProviderCourseForecastsQueryResult result = await _mediator.Send(new GetProviderCourseForecastsQuery(ukprn, larsCode), cancellationToken);
+        return Ok(result);
+    }
+}
