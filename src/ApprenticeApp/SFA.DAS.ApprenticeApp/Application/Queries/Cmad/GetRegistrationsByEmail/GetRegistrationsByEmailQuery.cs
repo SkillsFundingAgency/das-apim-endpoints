8f1eb25d0dadcf23d9b05ac5427f9fbbb1809aed
NWake-CGI <nick.wake@cgi.com>
2026-03-09 14:47:34 +0000

    Unit tests

--- src/ApprenticeApp/SFA.DAS.ApprenticeApp/Application/Queries/Cmad/GetRegistrationsByEmail/GetRegistrationsByEmailQuery.cs
+++ src/ApprenticeApp/SFA.DAS.ApprenticeApp/Application/Queries/Cmad/GetRegistrationsByEmail/GetRegistrationsByEmailQuery.cs
@@ -1,12 +1,7 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SFA.DAS.ApprenticeApp.Application.Queries.Cmad.GetRegistrationsByEmail
-{
+{    
     public class GetRegistrationsByEmailQuery : IRequest<GetRegistrationsByEmailQueryResult>
     {
         public string Email { get; set; }
