2daea1fee8bd8c474e6645b40331485fb7ebc8de
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2026-03-10 12:23:49 +0000

    Earnings returned

--- src/LearnerData/SFA.DAS.LearnerData/Application/GetShortCourseEarnings/GetShortCourseEarningsQueryHandler.cs
+++ src/LearnerData/SFA.DAS.LearnerData/Application/GetShortCourseEarnings/GetShortCourseEarningsQueryHandler.cs
@@ -1,7 +1,10 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
+using SFA.DAS.SharedOuterApi.InnerApi.Requests.Earnings;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests.Learning;
+using SFA.DAS.SharedOuterApi.InnerApi.Responses.Earnings;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.Learning.GetShortCourseLearnersForEarningsResponse;
 using SFA.DAS.SharedOuterApi.Interfaces;
 
@@ -11,13 +14,16 @@ public class GetShortCourseEarningsQueryHandler : IRequestHandler<GetShortCourse
 {
     private readonly ILogger<GetShortCourseEarningsQueryHandler> _logger;
     private readonly ILearningApiClient<LearningApiConfiguration> _learningApiClient;
+    private readonly IEarningsApiClient<EarningsApiConfiguration> _earningsApiClient;
 
     public GetShortCourseEarningsQueryHandler(
         ILogger<GetShortCourseEarningsQueryHandler> logger, 
-        ILearningApiClient<LearningApiConfiguration> learningApiClient)
+        ILearningApiClient<LearningApiConfiguration> learningApiClient,
+        IEarningsApiClient<EarningsApiConfiguration> earningsApiClient)
     {
         _logger = logger;
         _learningApiClient = learningApiClient;
+        _earningsApiClient = earningsApiClient;
     }
 
     public async Task<GetShortCourseEarningsResult> Handle(GetShortCourseEarningsQuery request, CancellationToken cancellationToken)
@@ -26,7 +32,9 @@ public class GetShortCourseEarningsQueryHandler : IRequestHandler<GetShortCourse
 
         var (learnings, totalLearners) = await GetLearnings(request);
 
-        var shortCourseEarnings = CombineLearningsAndEarningsData(learnings);
+        var earnings = await GetRelatedEarnings(request, learnings);
+
+        var shortCourseEarnings = CombineLearningsAndEarningsData(learnings, earnings);
 
         return BuildResult(request, shortCourseEarnings, totalLearners);
     }
@@ -60,21 +68,68 @@ public class GetShortCourseEarningsQueryHandler : IRequestHandler<GetShortCourse
 
         return (learners!, totalItems);
     }
-    
-    private List<ShortCourseEarnings> CombineLearningsAndEarningsData(List<Learning> learnings)
+
+    private async Task<List<EarningsByLearningKey>> GetRelatedEarnings(GetShortCourseEarningsQuery request, List<Learning> learnings)
     {
-        return learnings.Select(learning => new ShortCourseEarnings
+
+        var tasks = learnings.Select(learning => GetShortCourseEarning(request.Ukprn, learning)).ToList();
+
+        var results = await Task.WhenAll(tasks);
+
+        if (results.All(x=>x.EarningsData != null))
         {
-            LearningKey = learning.LearningKey.ToString(),
-            //LearnerRef = no mapping,
-            Courses = learning.Episodes.Select(episode => new Course
-            {
-                Approved = episode.IsApproved
-            }).ToList()
-        }).ToList();
+            _logger.LogInformation("All earnings data successfully retrieved for {ukprn} from Earnings API", request.Ukprn);
+            return results.ToList();
+        }
+
+        throw new ApplicationException($"Failed to retrieve all earnings data for {request.Ukprn} from Earnings API.");
     }
 
-    private GetShortCourseEarningsResult BuildResult(GetShortCourseEarningsQuery query, List<ShortCourseEarnings> shortCourseEarnings, int totalItems)
+    private async Task<EarningsByLearningKey> GetShortCourseEarning(long ukprn, Learning learning)
+    {
+        var request = new GetShortCourseDataRequest(ukprn, learning.LearningKey);
+        var response = await _earningsApiClient.GetWithResponseCode<GetShortCourseDataResponse>(request);
+
+        if (response.StatusCode.IsSuccessStatusCode())
+        {
+            _logger.LogInformation("Successfully retrieved earnings data for {learningKy} from Earnings API", learning.LearningKey);
+            return new EarningsByLearningKey(learning.LearningKey, response.Body);
+        }
+
+        _logger.LogError("Failed to retrieve earnings data for {learningKey} from Earnings API. Status code: {statusCode}", learning.LearningKey, response.StatusCode);
+        return new EarningsByLearningKey(learning.LearningKey, null);
+    }
+
+    private List<ShortCourseActiveEarnings> CombineLearningsAndEarningsData(List<Learning> learnings, List<EarningsByLearningKey> earnings)
+    {
+        var combined = new List<ShortCourseActiveEarnings>();
+
+        foreach (var learning in learnings)
+        {
+            var earning = earnings.Single(e => e.LearningKey == learning.LearningKey);
+
+            combined.Add(new ShortCourseActiveEarnings
+            {
+                LearningKey = learning.LearningKey.ToString(),
+                Courses = learning.Episodes.Select(episode => new Course
+                {
+                    Approved = episode.IsApproved,
+                    CoursePrice = episode.Price,
+                    Earnings = earning.EarningsData!.Earnings.Select(e=> new Earning
+                    {
+                        Amount = e.Amount,
+                        CollectionPeriod = e.CollectionPeriod,
+                        CollectionYear = e.CollectionYear,
+                        Milestone = e.Type
+                    }).ToList()
+                }).ToList()
+            });
+        }
+
+        return combined;
+    }
+
+    private GetShortCourseEarningsResult BuildResult(GetShortCourseEarningsQuery query, List<ShortCourseActiveEarnings> shortCourseEarnings, int totalItems)
     {
         var result = new GetShortCourseEarningsResult { Items = shortCourseEarnings };
 
@@ -85,4 +140,16 @@ public class GetShortCourseEarningsQueryHandler : IRequestHandler<GetShortCourse
         return result;
     }
      
+}
+
+public class EarningsByLearningKey
+{
+    public Guid LearningKey { get; set; }
+    public GetShortCourseDataResponse? EarningsData { get; set; }
+
+    public EarningsByLearningKey(Guid learningKey, GetShortCourseDataResponse? earningsData)
+    {
+        LearningKey = learningKey;
+        EarningsData = earningsData;
+    }
 }
--- src/LearnerData/SFA.DAS.LearnerData/Application/GetShortCourseEarnings/GetShortCourseEarningsResult.cs
+++ src/LearnerData/SFA.DAS.LearnerData/Application/GetShortCourseEarnings/GetShortCourseEarningsResult.cs
@@ -3,7 +3,7 @@ using SFA.DAS.SharedOuterApi.InnerApi.Responses.Learning.GetShortCourseLearnersF
 
 namespace SFA.DAS.LearnerData.Application.GetShortCourseEarnings;
 
-public class GetShortCourseEarningsResult : PagedQueryResult<ShortCourseEarnings>
+public class GetShortCourseEarningsResult : PagedQueryResult<ShortCourseActiveEarnings>
 {
 
 }
@@ -14,10 +14,13 @@ public class GetPagedLearnersFromLearningInner : PagedQueryResult<Learning>
 }
 
 #pragma warning disable CS8618
-public class ShortCourseEarnings
+/// <summary>
+/// This is combined data from the Learning and Earnings API for approved short courses
+/// that are active during the period specified in the query
+/// </summary>
+public class ShortCourseActiveEarnings
 {
     public string LearningKey { get; set; }
-    public string LearnerRef { get; set; }
     public List<Course> Courses { get; set; }
 }
 
