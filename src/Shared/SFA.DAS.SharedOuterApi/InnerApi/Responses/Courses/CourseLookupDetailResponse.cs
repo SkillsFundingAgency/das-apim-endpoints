e08c670d19db1c73767360c6b88fb905d09805d0
Paul Graham <paul.graham@coprime.co.uk>
2026-03-11 16:37:33 +0000

    now calls Courses API to get course details

---
+++ src/Shared/SFA.DAS.SharedOuterApi/InnerApi/Responses/Courses/CourseLookupDetailResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SFA.DAS.SharedOuterApi.InnerApi.Responses.Courses
+{
+    [ExcludeFromCodeCoverage]
+    public class CourseLookupDetailResponse 
+    {
+        public int LarsCode { get; set; }
+        public string Title { get; set; }
+        public string LearningType { get; set; }
+    }
+}
