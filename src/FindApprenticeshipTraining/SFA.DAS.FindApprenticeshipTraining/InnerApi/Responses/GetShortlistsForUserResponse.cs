8bc453aebc8b403d63ef1b012e03c0ee9e485d43
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-03-11 13:15:57 +0000

    CSP-2470 Added CourseType and ApprenticeshipType properties to the ShortlistCourseModel

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/InnerApi/Responses/GetShortlistsForUserResponse.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/InnerApi/Responses/GetShortlistsForUserResponse.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using SFA.DAS.SharedOuterApi.Common;
+using SFA.DAS.SharedOuterApi.InnerApi;
 
 namespace SFA.DAS.FindApprenticeshipTraining.InnerApi.Responses;
 
@@ -20,6 +22,8 @@ public class ShortlistCourseModel
     public int Ordering { get; set; }
     public string LarsCode { get; set; }
     public string StandardName { get; set; }
+    public CourseType CourseType { get; set; }
+    public ApprenticeshipType ApprenticeshipType { get; set; }
     public List<ShortlistLocationModel> Locations { get; set; } = [];
 }
 
