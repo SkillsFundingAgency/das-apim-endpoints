5ffde4e67d9e3a1595f097f2d85cbe62fd012fb8
Deven Shah <devenshah1974@gmail.com>
2026-03-11 16:21:05 +0000

    CSP-2514 Implements post provider course forecast endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/ProviderCourseForecasts/Commands/UpsertProviderCourseForecasts/UpsertProviderCourseForecastsCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.ProviderCourseForecasts.Commands.UpsertProviderCourseForecasts;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.ProviderCourseForecasts.Commands.UpsertProviderCourseForecasts;
+
+public class UpsertProviderCourseForecastsCommandHandlerTests
+{
+    [Test, AutoData]
+    public async Task Handle_WhenCalled_ThenShouldPostToApi(
+        int ukprn,
+        string larsCode,
+        IEnumerable<UpsertProviderCourseForecastModel> forecasts,
+        CancellationToken cancellationToken)
+    {
+        // Arrange
+        var apiClientMock = new Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>>();
+        var handler = new UpsertProviderCourseForecastsCommandHandler(apiClientMock.Object);
+        var command = new UpsertProviderCourseForecastsCommand(ukprn, larsCode, forecasts);
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+        // Assert
+        apiClientMock.Verify(client => client.PostWithResponseCode<object>(
+            It.Is<UpsertProviderCourseForecastsRequest>(req =>
+                req.Ukprn == ukprn &&
+                req.LarsCode == larsCode &&
+                req.Data == forecasts), false), Times.Once);
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/ProviderCourseForecasts/Commands/UpsertProviderCourseForecasts/UpsertProviderCourseForecastsCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Interfaces;
+
+namespace SFA.DAS.RoatpCourseManagement.Application.ProviderCourseForecasts.Commands.UpsertProviderCourseForecasts;
+
+public class UpsertProviderCourseForecastsCommandHandler(IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> _courseManagementApiClient) : IRequestHandler<UpsertProviderCourseForecastsCommand>
+{
+    public async Task Handle(UpsertProviderCourseForecastsCommand request, CancellationToken cancellationToken)
+    {
+        await _courseManagementApiClient.PostWithResponseCode<object>(new UpsertProviderCourseForecastsRequest(request.Ukprn, request.LarsCode, request.Forecasts), false);
+    }
+}
