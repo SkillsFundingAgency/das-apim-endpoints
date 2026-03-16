61415b38052a895ba5db1cdc205e2a4782821ced
Chris Foster <chrisfoster186@googlemail.com>
2026-03-16 13:59:23 +0000

    Fixes issue where UKPRN and Price were set to default values on update

--- src/LearnerData/SFA.DAS.LearnerData/Application/UpdateShortCourse/UpdateShortCourseLearningCommandHandler.cs
+++ src/LearnerData/SFA.DAS.LearnerData/Application/UpdateShortCourse/UpdateShortCourseLearningCommandHandler.cs
@@ -81,6 +81,8 @@ public class UpdateShortCourseLearningCommandHandler : IRequestHandler<UpdateSho
             },
             OnProgramme = new ShortCourseOnProgrammeUpdateDetails
             {
+                Ukprn = command.Ukprn,
+                Price = 1000,
                 StartDate = currentOnProgramme.StartDate,
                 ExpectedEndDate = currentOnProgramme.ExpectedEndDate,
                 CompletionDate = currentOnProgramme.CompletionDate,
