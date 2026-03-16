ae102147aa3152d448e9d31a825e430fa0ecb4f7
ratheeshri1984 <ratheeshri1984@gmail.com>
2026-03-16 12:36:01 +0000

    2665-Testcase updated

--- src/DigitalCertificates/SFA.DAS.DigitalCertificates.UnitTests/Application/Commands/CreateCertificatePrintRequest/WhenCreatingCertificatePrintRequestCommand.cs
+++ src/DigitalCertificates/SFA.DAS.DigitalCertificates.UnitTests/Application/Commands/CreateCertificatePrintRequest/WhenCreatingCertificatePrintRequestCommand.cs
@@ -3,61 +3,48 @@ using AutoFixture.NUnit3;
 using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.DigitalCertificates.Application.Commands.CreateCertificatePrintRequest;
-using SFA.DAS.DigitalCertificates.Constants;
-using SFA.DAS.DigitalCertificates.InnerApi.Requests.Assessor;
 
 namespace SFA.DAS.DigitalCertificates.UnitTests.Application.Commands.CreateCertificatePrintRequest
 {
     public class WhenCreatingCertificatePrintRequestCommand
     {
         [Test, AutoData]
-        public void Then_CertificateId_Is_Set(Guid certificateId)
+        public void Then_Command_Properties_Are_Set(
+            Guid certificateId,
+            Address address,
+            EmailData email)
         {
-            var command = new CreateCertificatePrintRequestCommand { CertificateId = certificateId };
+            var command = new CreateCertificatePrintRequestCommand
+            {
+                CertificateId = certificateId,
+                Address = address,
+                Email = email
+            };
 
             command.CertificateId.Should().Be(certificateId);
+            command.Address.Should().Be(address);
+            command.Email.Should().Be(email);
         }
 
         [Test, AutoData]
-        public void Then_Address_Properties_Are_Mapped_To_RequestData(CreateCertificatePrintRequestCommand command)
+        public void Then_Address_Properties_Are_Set(Address address)
         {
-            // Act
-            PutCertificatePrintRequestData requestData = command;
-
-            // Assert
-            requestData.Address.ContactName.Should().Be(command.Address.ContactName);
-            requestData.Address.ContactOrganisation.Should().Be(command.Address.ContactOrganisation);
-            requestData.Address.ContactAddLine1.Should().Be(command.Address.ContactAddLine1);
-            requestData.Address.ContactAddLine2.Should().Be(command.Address.ContactAddLine2);
-            requestData.Address.ContactAddLine3.Should().Be(command.Address.ContactAddLine3);
-            requestData.Address.ContactAddLine4.Should().Be(command.Address.ContactAddLine4);
-            requestData.Address.ContactPostCode.Should().Be(command.Address.ContactPostCode);
-        }
-
-        [Test]
-        public void Then_Null_Address_Maps_To_Null_RequestData_Address()
-        {
-            var command = new CreateCertificatePrintRequestCommand { Address = null };
-
-            PutCertificatePrintRequestData requestData = command;
-
-            requestData.Address.Should().BeNull();
+            address.ContactName.Should().NotBeNullOrEmpty();
+            address.ContactOrganisation.Should().NotBeNullOrEmpty();
+            address.ContactAddLine1.Should().NotBeNullOrEmpty();
+            address.ContactAddLine2.Should().NotBeNullOrEmpty();
+            address.ContactAddLine3.Should().NotBeNullOrEmpty();
+            address.ContactAddLine4.Should().NotBeNullOrEmpty();
+            address.ContactPostCode.Should().NotBeNullOrEmpty();
         }
 
         [Test, AutoData]
-        public void Then_PrintRequestedBy_Is_Set_To_Constant(CreateCertificatePrintRequestCommand command)
+        public void Then_EmailData_Properties_Are_Set(EmailData email)
         {
-            PutCertificatePrintRequestData requestData = command;
-
-            requestData.PrintRequestedBy.Should().Be(CertificateConstants.PrintRequestedBy);
-        }
-
-        [Test, AutoData]
-        public void Then_PrintRequestedAt_Is_Set_To_UtcNow(CreateCertificatePrintRequestCommand command)
-        {
-            PutCertificatePrintRequestData requestData = command;
-
-            requestData.PrintRequestedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+            email.EmailAddress.Should().NotBeNullOrEmpty();
+            email.UserName.Should().NotBeNullOrEmpty();
+            email.LinkDomain.Should().NotBeNullOrEmpty();
+            email.TemplateId.Should().NotBeNullOrEmpty();
         }
     }
 }
