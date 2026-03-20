fcc4eb94d3a3fed89b9f8d88dcbfe002a1fa3542
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-03-20 18:02:19 +0000

    CSP-2423 renamed the test

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Courses/Queries/GetCourseByLarsCode/WhenCreatingGetCourseByLarsCodeQueryResult.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining.UnitTests/Application/Courses/Queries/GetCourseByLarsCode/WhenCreatingGetCourseByLarsCodeQueryResult.cs
@@ -9,7 +9,7 @@ public sealed class WhenCreatingGetCourseByLarsCodeQueryResult
 {
     [Test]
     [MoqAutoData]
-    public void Then_StandardDetailResponse_Should_Convert_Correctly(StandardDetailsLookupResponse response)
+    public void ImplicitConversion_StandardDetailsLookupResponse_MapsAllProperties(StandardDetailsLookupResponse response)
     {
         GetCourseByLarsCodeQueryResult sut = response;
         Assert.Multiple(() =>
