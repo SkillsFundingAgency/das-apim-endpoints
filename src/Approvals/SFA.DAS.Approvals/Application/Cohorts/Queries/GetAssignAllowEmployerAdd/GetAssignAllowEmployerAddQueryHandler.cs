6c1e8d33c1a3998ac3d835dc7dba6de608b5d442
skumardfe <Santhosh.KUMAR@education.gov.uk>
2026-03-24 14:39:59 +0000

    add learning Type reference

--- src/Approvals/SFA.DAS.Approvals/Application/Cohorts/Queries/GetAssignAllowEmployerAdd/GetAssignAllowEmployerAddQueryHandler.cs
+++ src/Approvals/SFA.DAS.Approvals/Application/Cohorts/Queries/GetAssignAllowEmployerAdd/GetAssignAllowEmployerAddQueryHandler.cs
@@ -5,7 +5,7 @@ using SFA.DAS.Approvals.InnerApi.CourseTypesApi.Requests;
 using SFA.DAS.Approvals.InnerApi.CourseTypesApi.Responses;
 using SFA.DAS.Approvals.InnerApi.Responses;
 using SFA.DAS.Approvals.Services;
-using SFA.DAS.Approvals.Types;
+using SFA.DAS.SharedOuterApi.Common;
 using SFA.DAS.SharedOuterApi.Configuration;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests.Reservations;
 using SFA.DAS.SharedOuterApi.Interfaces;
