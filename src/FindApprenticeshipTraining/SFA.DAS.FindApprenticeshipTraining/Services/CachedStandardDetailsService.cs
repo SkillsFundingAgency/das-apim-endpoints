e834d8687b5a495c635c8e718b6fe6ec5d7d1c6e
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2026-03-27 13:12:54 +0000

    CSP-2464 Refactored courses integration V2 endpoint (#2733)
    CSP-2464 Refactored courses integration V2 endpoint (#2733)

--- src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Services/CachedStandardDetailsService.cs
+++ src/FindApprenticeshipTraining/SFA.DAS.FindApprenticeshipTraining/Services/CachedStandardDetailsService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using SFA.DAS.SharedOuterApi.Configuration;
+using SFA.DAS.SharedOuterApi.Extensions;
 using SFA.DAS.SharedOuterApi.InnerApi.Requests;
 using SFA.DAS.SharedOuterApi.InnerApi.Responses.Courses;
 using SFA.DAS.SharedOuterApi.Interfaces;
@@ -21,16 +22,31 @@ public class CachedStandardDetailsService : ICachedStandardDetailsService
         _cacheStorageService = cacheStorageService;
     }
 
+    public async Task<GetKsbsForCourseOptionResponse> GetKsbsForCourseOption(string larsCode)
+    {
+        var ksbsCacheKey = $"{nameof(GetKsbsForCourseOptionResponse)}-{larsCode}";
+        GetKsbsForCourseOptionResponse cachedKsbsForCourseOption = await _cacheStorageService.RetrieveFromCache<GetKsbsForCourseOptionResponse>(ksbsCacheKey);
+
+        if (cachedKsbsForCourseOption != null)
+            return cachedKsbsForCourseOption;
+
+        var apiResponse = await _coursesApiClient.GetWithResponseCode<GetKsbsForCourseOptionResponse>(new GetKsbsForCourseOptionRequest(larsCode));
+        apiResponse.EnsureSuccessStatusCode();
+        cachedKsbsForCourseOption = apiResponse.Body;
+        await _cacheStorageService.SaveToCache(ksbsCacheKey, cachedKsbsForCourseOption, StandardDetailsCacheDurationInHours);
+        return cachedKsbsForCourseOption;
+    }
+
     public async Task<StandardDetailsLookupResponse> GetStandardDetails(string larsCode)
     {
         var standardDetailsCacheKey = $"{nameof(StandardDetailsLookupResponse)}-{larsCode}";
-        var cachedStandardDetails = await _cacheStorageService
-            .RetrieveFromCache<StandardDetailsLookupResponse>(standardDetailsCacheKey);
+        StandardDetailsLookupResponse cachedStandardDetails = await _cacheStorageService.RetrieveFromCache<StandardDetailsLookupResponse>(standardDetailsCacheKey);
 
         if (cachedStandardDetails != null)
             return cachedStandardDetails;
 
         var apiResponse = await _coursesApiClient.GetWithResponseCode<StandardDetailsLookupResponse>(new GetStandardDetailsLookupRequest(larsCode));
+        apiResponse.EnsureSuccessStatusCode();
         cachedStandardDetails = apiResponse.Body;
         await _cacheStorageService.SaveToCache(standardDetailsCacheKey, cachedStandardDetails, StandardDetailsCacheDurationInHours);
 
