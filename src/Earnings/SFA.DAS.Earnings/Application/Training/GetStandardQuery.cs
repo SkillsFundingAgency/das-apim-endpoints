aa7a34d4b053f1abafecf9365301ad1320b8bbcd
Chris Foster <chrisfoster186@googlemail.com>
2026-04-21 13:36:00 +0100

    Cleanup

--- src/Earnings/SFA.DAS.Earnings/Application/Training/GetStandardQuery.cs
+++ src/Earnings/SFA.DAS.Earnings/Application/Training/GetStandardQuery.cs
@@ -1,35 +1,9 @@
 ﻿using MediatR;
 using SFA.DAS.Earnings.InnerApi;
-using SFA.DAS.SharedOuterApi.Types.Configuration;
-using SFA.DAS.SharedOuterApi.Types.InnerApi.Requests.Courses;
-using SFA.DAS.SharedOuterApi.Types.Interfaces;
 
 namespace SFA.DAS.Earnings.Application.Training;
 
-public class GetStandardQuery : IRequest<GetStandardsListItem>
+public class GetStandardQuery(string courseCode) : IRequest<GetStandardsListItem>
 {
-    public string CourseCode { get; set; }
-
-    public GetStandardQuery(string courseCode)
-    {
-        CourseCode = courseCode;
-    }
-}
-
-public class GetStandardQueryHandler : IRequestHandler<GetStandardQuery, GetStandardsListItem>
-{
-    private readonly ICoursesApiClient<CoursesApiConfiguration> _coursesApiClient;
-
-    public GetStandardQueryHandler(ICoursesApiClient<CoursesApiConfiguration> coursesApiClient)
-    {
-        _coursesApiClient = coursesApiClient;
-    }
-
-    public async Task<GetStandardsListItem> Handle(GetStandardQuery request, CancellationToken cancellationToken)
-    {
-        var course = await _coursesApiClient.Get<GetStandardsListItem>(
-            new GetStandardDetailsByIdRequest(request.CourseCode));
-
-        return course;
-    }
+    public string CourseCode { get; set; } = courseCode;
 }
