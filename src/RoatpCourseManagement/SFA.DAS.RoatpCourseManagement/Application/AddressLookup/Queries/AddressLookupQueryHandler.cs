eb57ae4838b96f74c1c81a11ecfcbfca0469fe93
Afzal Hussain <afi259@hotmail.co.uk>
2026-04-23 16:55:05 +0100

    CSP-2246 Remove logging with count

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/Queries/AddressLookupQueryHandler.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/Queries/AddressLookupQueryHandler.cs
@@ -5,31 +5,29 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using SFA.DAS.SharedOuterApi.Types.Interfaces;
 
-namespace SFA.DAS.RoatpCourseManagement.Application.AddressLookup.Queries
+namespace SFA.DAS.RoatpCourseManagement.Application.AddressLookup.Queries;
+
+public class AddressLookupQueryHandler : IRequestHandler<AddresssLookupQuery, AddresssLookupQueryResult>
 {
-    public class AddressLookupQueryHandler : IRequestHandler<AddresssLookupQuery, AddresssLookupQueryResult>
+    private readonly ILocationLookupService _locationLookupService;
+    private readonly ILogger<AddressLookupQueryHandler> _logger;
+
+    public AddressLookupQueryHandler(ILocationLookupService locationLookupService, ILogger<AddressLookupQueryHandler> logger)
     {
-        private readonly ILocationLookupService _locationLookupService;
-        private readonly ILogger<AddressLookupQueryHandler> _logger;
+        _locationLookupService = locationLookupService;
+        _logger = logger;
+    }
 
-        public AddressLookupQueryHandler(ILocationLookupService locationLookupService, ILogger<AddressLookupQueryHandler> logger)
+    public async Task<AddresssLookupQueryResult> Handle(AddresssLookupQuery request, CancellationToken cancellationToken)
+    {
+        var result = await _locationLookupService.GetExactMatchAddresses(request.Postcode);
+        if (result == null)
         {
-            _locationLookupService = locationLookupService;
-            _logger = logger;
+            _logger.LogWarning($"Invalid postcode: {request.Postcode}.");
+            return null;
         }
-
-        public async Task<AddresssLookupQueryResult> Handle(AddresssLookupQuery request, CancellationToken cancellationToken)
-        {
-            var result = await _locationLookupService.GetExactMatchAddresses(request.Postcode);
-            if (result == null)
-            {
-                _logger.LogWarning($"Invalid postcode: {request.Postcode}.");
-                return null;
-            }
-            _logger.LogInformation($"Found {result.Addresses.Count()} addresses for postcode: {request.Postcode}");
-            var response = new AddresssLookupQueryResult();
-            response.Addresses = result.Addresses.Select(a => (AddressItem)a).ToList();
-            return response;
-        }
+        var response = new AddresssLookupQueryResult();
+        response.Addresses = result.Addresses.Select(a => (AddressItem)a).ToList();
+        return response;
     }
 }
