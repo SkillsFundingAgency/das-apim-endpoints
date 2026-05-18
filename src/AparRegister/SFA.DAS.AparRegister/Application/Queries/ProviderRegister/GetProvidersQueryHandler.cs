db4a4d468982c04e2c66f111e6463752ba70e8ef
Afzal Hussain <afi259@hotmail.co.uk>
2026-05-18 15:34:15 +0100

    CSP-2575 Updated solution and added shared projects

--- src/AparRegister/SFA.DAS.AparRegister/Application/Queries/ProviderRegister/GetProvidersQueryHandler.cs
+++ src/AparRegister/SFA.DAS.AparRegister/Application/Queries/ProviderRegister/GetProvidersQueryHandler.cs
@@ -10,7 +10,6 @@ using SFA.DAS.SharedOuterApi.Types.InnerApi.Requests.Roatp;
 using SFA.DAS.SharedOuterApi.Types.InnerApi.Responses.Roatp;
 using SFA.DAS.SharedOuterApi.Types.InnerApi.Responses.Roatp.Common;
 using SFA.DAS.SharedOuterApi.Types.Interfaces;
-using SFA.DAS.Apim.Shared.Interfaces;
 
 namespace SFA.DAS.AparRegister.Application.Queries.ProviderRegister;
 
