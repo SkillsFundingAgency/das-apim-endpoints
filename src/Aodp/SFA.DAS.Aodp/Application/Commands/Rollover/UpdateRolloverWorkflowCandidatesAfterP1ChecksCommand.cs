4ea040281319a89ee565d23fd74070530546f76d
khanna <karen@laveryhanna.com>
2026-05-22 13:00:53 +0100

    added command for p1 checks

---
+++ src/Aodp/SFA.DAS.Aodp/Application/Commands/Rollover/UpdateRolloverWorkflowCandidatesAfterP1ChecksCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+namespace SFA.DAS.Aodp.Application.Commands.Rollover;
+
+public record UpdateRolloverWorkflowCandidatesAfterP1ChecksCommand : IRequest<BaseMediatrResponse<EmptyResponse>>
+{
+}
---
+++ src/Aodp/SFA.DAS.Aodp/InnerApi/AodpApi/Rollover/UpdateRolloverWorkflowCandidatesAfterP1ChecksApiRequest.cs
@@ -0,0 +1,11 @@
+﻿using SFA.DAS.Apim.Shared.Interfaces;
+
+namespace SFA.DAS.Aodp.InnerApi.AodpApi.Rollover
+{
+    public class UpdateRolloverWorkflowCandidatesAfterP1ChecksApiRequest : IPostApiRequest
+    {
+        public string PostUrl => $"api/rollover/p1checks";
+
+        public object Data { get; set; } = new { };
+    }
+}
