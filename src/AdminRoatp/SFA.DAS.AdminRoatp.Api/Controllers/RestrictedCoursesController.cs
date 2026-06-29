7f5e4746a1b30457393f40d715c127e15ac0a0f3
afzal-hussain259 <afi259@hotmail.co.uk>
2026-06-29 14:31:47 +0100

    CSP-2624 Implemented get restricted courses in admin outer (#2974)
    * CSP-2624 Implemented get restricted courses in admin outer
    
    * CSP-2624 updated test to fix sonar issues

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api/Controllers/RestrictedCoursesController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AdminRoatp.Application.Queries.GetAllRestrictedCourses;
+using SFA.DAS.AdminRoatp.InnerApi.Responses;
+
+namespace SFA.DAS.AdminRoatp.Api.Controllers;
+
+[ApiController]
+[Route("/restricted-courses")]
+public class RestrictedCoursesController(IMediator _mediator, ILogger<RestrictedCoursesController> _logger) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(GetAllRestrictedCoursesResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetRestrictedCourses([FromQuery] bool restricted)
+    {
+        _logger.LogInformation("Request received to get restricted courses");
+
+        GetAllRestrictedCoursesQuery query = new(restricted);
+        GetAllRestrictedCoursesResponse result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
