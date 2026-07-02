bf0aaddd78ae2a6370f03096de782a6b14f6ab67
Afzal Hussain <afi259@hotmail.co.uk>
2026-07-02 11:16:04 +0100

    CSP-2627 Implemented outer api for get allowed providers by course

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api/Controllers/CourseController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.AdminRoatp.Application.Queries.GetAllowedProviders;
+using SFA.DAS.AdminRoatp.InnerApi.Responses;
+
+namespace SFA.DAS.AdminRoatp.Api.Controllers;
+
+[ApiController]
+[Route("/courses")]
+public class CourseController(IMediator _mediator, ILogger<RestrictedCoursesController> _logger) : ControllerBase
+{
+    [HttpGet("{larsCode}/providers/allowed")]
+    [ProducesResponseType(typeof(GetAllowedProvidersResponse), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetAllowedProvidersByCourse([FromRoute] string larsCode)
+    {
+        _logger.LogInformation("Request received to get allowed providers for a course");
+
+        GetAllowedProvidersQuery query = new(larsCode);
+        GetAllowedProvidersResponse result = await _mediator.Send(query);
+        return Ok(result);
+    }
+}
---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/InnerApi/Responses/GetAllowedProvidersResponse.cs
@@ -0,0 +1,19 @@
+﻿using SFA.DAS.AdminRoatp.InnerApi.Models;
+using SFA.DAS.SharedOuterApi.Types.Constants;
+using SFA.DAS.SharedOuterApi.Types.InnerApi;
+
+namespace SFA.DAS.AdminRoatp.InnerApi.Responses;
+
+public class GetAllowedProvidersResponse
+{
+    public string LarsCode { get; set; } = string.Empty;
+    public string IfateReferenceNumber { get; set; } = string.Empty;
+    public string CourseName { get; set; } = string.Empty;
+    public string Route { get; set; } = string.Empty;
+    public LearningType LearningType { get; set; }
+    public CourseType CourseType { get; set; }
+    public bool IsActiveAvailable { get; set; }
+    public DateTime? DateLastStarts { get; set; }
+    public bool IsCourseRestricted { get; set; }
+    public List<ProviderModel> Providers { get; set; } = [];
+}
