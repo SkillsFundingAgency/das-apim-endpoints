52203e030a8b80edab9ed012ee51a9115d65a9da
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-07-03 10:00:56 +0100

    CSP-2688 Remove changes to populate coordinates

--- src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/Queries/AddressItem.cs
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/Application/AddressLookup/Queries/AddressItem.cs
@@ -10,8 +10,6 @@ namespace SFA.DAS.RoatpCourseManagement.Application.AddressLookup.Queries
         public string Town { get; set; }
         public string County { get; set; }
         public string Postcode { get; set; }
-        public double? Latitude { get; set; }
-        public double? Longitude { get; set; }
 
         public static implicit operator AddressItem(GetAddressesListItem source) =>
             new AddressItem
@@ -22,8 +20,6 @@ namespace SFA.DAS.RoatpCourseManagement.Application.AddressLookup.Queries
                 Town = source.PostTown,
                 County = source.County,
                 Postcode = source.Postcode,
-                Latitude = source.Latitude,
-                Longitude = source.Longitude,
             };
     }
 }
