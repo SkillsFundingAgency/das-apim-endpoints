d039cc954f5d848d17f66e775a439e86a0f5eb5c
afzal-hussain259 <afi259@hotmail.co.uk>
2026-07-06 10:12:08 +0100

    CSP-2627 Implemented outer api for get allowed providers by course (#2985)
    * CSP-2627 Implemented outer api for get allowed providers by course
    
    * CSP-2627 fixed sonar issue

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api.UnitTests/Controllers/CourseControllerTests/CourseControllerGetTests.cs
@@ -0,0 +1,32 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SFA.DAS.AdminRoatp.Api.Controllers;
+using SFA.DAS.AdminRoatp.Application.Queries.GetAllowedProviders;
+using SFA.DAS.AdminRoatp.InnerApi.Responses;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.AdminRoatp.Api.UnitTests.Controllers.CourseControllerTests;
+
+public class CourseControllerGetTests
+{
+    [Test, MoqAutoData]
+    public async Task WhenGetAllowedProvidersByCourseIsInvoked_ThenReturnsOkResult(
+        [Frozen] Mock<IMediator> mediatorMock,
+        [Greedy] CourseController sut,
+        GetAllowedProvidersResponse expected,
+        GetAllowedProvidersQuery query)
+    {
+        // Arrange
+        mediatorMock.Setup(m => m.Send(It.IsAny<GetAllowedProvidersQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(expected);
+
+        // Act
+        var result = await sut.GetAllowedProvidersByCourse(query.larsCode);
+
+        // Assert
+        result.As<OkObjectResult>().Value.Should().Be(expected);
+    }
+}
