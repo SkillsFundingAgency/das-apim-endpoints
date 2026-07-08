e4cce789ae7dd33132feeb30e6315880aca82fd5
ratheeshri1984 <ratheeshri1984@gmail.com>
2026-07-08 14:37:29 +0100

    P2-3001-StddCode in GetUserAction

--- src/Admin/SFA.DAS.Admin/Application/Queries/GetUserActionByCode/GetUserActionByCodeQueryHandler.cs
+++ src/Admin/SFA.DAS.Admin/Application/Queries/GetUserActionByCode/GetUserActionByCodeQueryHandler.cs
@@ -1,27 +1,34 @@
 ﻿using MediatR;
 using SFA.DAS.DigitalCertificates.Contracts.ApiRequests;
-using SFA.DAS.Apim.Shared.Extensions;
-using SFA.DAS.DigitalCertificates.Contracts.Client;
+using SFA.DAS.SharedOuterApi.Types.Configuration;
+using SFA.DAS.SharedOuterApi.Types.Interfaces;
+using SFA.DAS.Admin.InnerApi.Requests.Assessor;
+using DigitalCertificatesApiClient = SFA.DAS.DigitalCertificates.Contracts.Client.IDigitalCertificatesApiClient<SFA.DAS.DigitalCertificates.Contracts.Client.DigitalCertificatesApiConfiguration>;
+using SFA.DAS.Admin.Enums;
+using SFA.DAS.Admin.InnerApi.Responses.Assessor;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
-
 using GetUserActionByCodeResponse = SFA.DAS.DigitalCertificates.Contracts.ApiResponses.GetUserActionByCodeQueryResult;
+using SFA.DAS.Apim.Shared.Extensions;
 
 namespace SFA.DAS.Admin.Application.Queries.GetUserActionByCode
 {
     public class GetUserActionByCodeQueryHandler : IRequestHandler<GetUserActionByCodeQuery, GetUserActionByCodeQueryResult>
     {
-        private readonly IDigitalCertificatesApiClient<DigitalCertificatesApiConfiguration> _digitalCertificatesApiClient;
+        private readonly DigitalCertificatesApiClient _digitalCertificatesApiClient;
+        private readonly IAssessorsApiClient<AssessorsApiConfiguration> _assessorsApiClient;
 
-        public GetUserActionByCodeQueryHandler(IDigitalCertificatesApiClient<DigitalCertificatesApiConfiguration> digitalCertificatesApiClient)
+        public GetUserActionByCodeQueryHandler(DigitalCertificatesApiClient digitalCertificatesApiClient, IAssessorsApiClient<AssessorsApiConfiguration> assessorsApiClient)
         {
             _digitalCertificatesApiClient = digitalCertificatesApiClient;
+            _assessorsApiClient = assessorsApiClient;
         }
 
         public async Task<GetUserActionByCodeQueryResult> Handle(GetUserActionByCodeQuery request, CancellationToken cancellationToken)
         {
-            var apiResponse = await _digitalCertificatesApiClient.GetWithResponseCode<GetUserActionByCodeResponse>(new GetUsersUseractionsByCodeApiRequest(request.Code));
+            var apiResponse = await _digitalCertificatesApiClient.GetWithResponseCode<GetUserActionByCodeResponse>(
+                new GetUsersUseractionsByCodeApiRequest(request.Code));
 
             if (apiResponse?.StatusCode == HttpStatusCode.NotFound)
             {
@@ -30,11 +37,30 @@ namespace SFA.DAS.Admin.Application.Queries.GetUserActionByCode
 
             apiResponse?.EnsureSuccessStatusCode();
 
-            var body = apiResponse?.Body;
+            if (apiResponse?.Body == null)
+            {
+                return null;
+            }
 
-            if (body == null) return null;
+            var result = (GetUserActionByCodeQueryResult)apiResponse.Body;
 
-            return (GetUserActionByCodeQueryResult)body;
+            if (result.CertificateType == CertificateType.Standard.ToString() && result.CertificateId.HasValue)
+            {
+                var certResponse = await _assessorsApiClient
+                    .GetWithResponseCode<GetStandardCertificateResponse>(
+                        new GetStandardCertificateRequest(result.CertificateId.Value, false));
+
+                if (certResponse?.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return result;
+                }
+
+                certResponse?.EnsureSuccessStatusCode();
+
+                result.StandardCode = certResponse?.Body?.StandardCode;
+            }
+
+            return result;
         }
     }
 }
