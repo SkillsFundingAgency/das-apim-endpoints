6bace7c022989ff0f76877408ce3abc28d7106cd
Chris Woodcock <chris@humesoftware.com>
2026-07-09 16:19:15 +0100

    Remove the employer demand outer api which has been decommissioned

--- src/EmployerDemand/SFA.DAS.EmployerDemand/Application/Demand/Queries/GetUnmetDemandsWithStoppedCourse/GetUnmetDemandsWithStoppedCourseQuery.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.EmployerDemand.Application.Demand.Queries.GetUnmetDemandsWithStoppedCourse
-{
-    public class GetUnmetDemandsWithStoppedCourseQuery : IRequest<GetUnmetDemandsWithStoppedCourseResult>
-    {
-    }
-}
--- src/EmployerDemand/SFA.DAS.EmployerDemand/InnerApi/Requests/GetEmployerDemandsOlderThan3YearsRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using SFA.DAS.Apim.Shared.Interfaces;
-
-namespace SFA.DAS.EmployerDemand.InnerApi.Requests
-{
-    public class GetEmployerDemandsOlderThan3YearsRequest : IGetApiRequest
-    {
-        public string GetUrl => "api/demand/older-than-3-years";
-    }
-}
