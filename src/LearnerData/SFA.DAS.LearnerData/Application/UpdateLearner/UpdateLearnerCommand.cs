4f85713fdc13ccee1dccc41768675589e51e9941
MikeV-TalentConsulting <122601927+MikeV-TalentConsulting@users.noreply.github.com>
2026-07-10 09:06:17 +0100

    Update UpdateLearnerCommand.cs

--- src/LearnerData/SFA.DAS.LearnerData/Application/UpdateLearner/UpdateLearnerCommand.cs
+++ src/LearnerData/SFA.DAS.LearnerData/Application/UpdateLearner/UpdateLearnerCommand.cs
@@ -7,7 +7,7 @@ namespace SFA.DAS.LearnerData.Application.UpdateLearner;
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 public class UpdateLearnerCommand : IRequest
 {
-    public Guid LearningKey { get; set; }
+    public Guid LearnerKey { get; set; }
     public long Ukprn { get; set; }
     public UpdateLearnerRequest UpdateLearnerRequest { get; set; }
 }
