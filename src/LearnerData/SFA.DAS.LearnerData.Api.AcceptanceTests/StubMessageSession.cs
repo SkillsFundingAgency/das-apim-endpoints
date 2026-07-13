9a1de31e4644e69abf2b746ef9b4723fc9781c23
tom-gough <tom.gough@talentconsulting.uk>
2026-07-13 14:42:47 +0100

    FLP-1901 Added acceptance test to cover approvals being informed of multiple on programme learnings on creation

--- src/LearnerData/SFA.DAS.LearnerData.Api.AcceptanceTests/StubMessageSession.cs
+++ src/LearnerData/SFA.DAS.LearnerData.Api.AcceptanceTests/StubMessageSession.cs
@@ -5,6 +5,7 @@ namespace SFA.DAS.LearnerData.Api.AcceptanceTests;
 internal class StubMessageSession : IMessageSession
 {
     public readonly static List<object> SentMessages = new List<object>();
+    public readonly static List<object> PublishedMessages = new List<object>();
 
     public Task Send(object message, SendOptions options)
     {
@@ -12,7 +13,11 @@ internal class StubMessageSession : IMessageSession
         return Task.CompletedTask;
     }
     public Task Send<T>(Action<T> messageConstructor, SendOptions options) => Task.CompletedTask;
-    public Task Publish(object message, PublishOptions options) => Task.CompletedTask;
+    public Task Publish(object message, PublishOptions options)
+    {
+        PublishedMessages.Add(message);
+        return Task.CompletedTask;
+    }
     public Task Publish<T>(Action<T> messageConstructor, PublishOptions options) => Task.CompletedTask;
     public Task Subscribe(Type eventType, SubscribeOptions options) => Task.CompletedTask;
     public Task Unsubscribe(Type eventType, UnsubscribeOptions options) => Task.CompletedTask;
