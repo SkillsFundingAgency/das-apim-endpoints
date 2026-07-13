65d41f20c80602a98c1e2ca59b962c9592df727a
Ravi UDARI <Ravi.UDARI@EDUCATION.GOV.UK>
2026-07-13 16:38:22 +0100

    CSP-2002 Removed all code for the GetUserAccounts , including its MediatR handler, query, result, domain model, and associated unit tests.

--- src/EmployerAan/SFA.DAS.EmployerAan/Application/User/GetUserAccounts/GetUserAccountsQuery.cs
+++
@@ -1,5 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.EmployerAan.Application.User.GetUserAccounts;
-
-public record GetUserAccountsQuery(string UserId, string Email) : IRequest<GetUserAccountsQueryResult>;
