b72be6dda638ee5b5498cba8aa9fbe56bba2d461
Paul Graham <paul.graham@coprime.co.uk>
2026-07-23 16:45:14 +0100

    added main controller and handler

---
+++ src/Approvals/SFA.DAS.Approvals.Api/Controllers/ApprenticeshipApprovalsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.Approvals.Application.ApprenticeshipApprovals.Query;
+namespace SFA.DAS.Approvals.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/")]
+public class ApprenticeshipApprovalsController(
+    ILogger<ApprenticeshipApprovalsController> logger,
+    IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    [Route("/employer/{accountId:long}/apprentices/{apprenticeshipId:long}/approvals/{approvalRequestId:guid}")]
+    public async Task<IActionResult> GetApprenticeshipApproval(long accountId, long apprenticeshipId, Guid approvalRequestId)
+    {
+        try
+        {
+            var result = await mediator.Send(new GetApprenticeshipApprovalQuery { EmployerAccountId = accountId, ApprovalRequestId = approvalRequestId, ApprenticeshipId = apprenticeshipId });
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+        catch (UnauthorizedAccessException e)
+        {
+            logger.LogError(e, "Access denied in GetApprenticeshipApproval {apprenticeshipId} for account {accountId}", apprenticeshipId, accountId);
+            return Forbid();
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error in GetApprenticeshipApproval {apprenticeshipId}", apprenticeshipId);
+            return BadRequest();
+        }
+    }
+}
---
+++ src/Approvals/SFA.DAS.Approvals/Application/ApprenticeshipApprovals/Query/GetApprenticeshipApprovalQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Approvals.InnerApi.CommitmentsV2Api.Requests;
+using SFA.DAS.SharedOuterApi.Types.Configuration;
+using SFA.DAS.SharedOuterApi.Types.Interfaces;
+
+namespace SFA.DAS.Approvals.Application.ApprenticeshipApprovals.Query;
+
+public class GetApprenticeshipApprovalQueryHandler(
+    ICommitmentsV2ApiClient<CommitmentsV2ApiConfiguration> apiClient) : IRequestHandler<GetApprenticeshipApprovalQuery, GetApprenticeshipApprovalResponse?>
+{
+    public async Task<GetApprenticeshipApprovalResponse?> Handle(GetApprenticeshipApprovalQuery request, CancellationToken cancellationToken)
+    {
+        var result = await apiClient.Get<GetApprenticeshipApprovalResponse>(new GetApprenticeshipApprovalRequest(request.ApprenticeshipId, request.ApprovalRequestId));
+
+        if (result == null) 
+            return null;
+
+        if(result.AccountId != request.EmployerAccountId)
+            throw new UnauthorizedAccessException("This Employer does not have access to this apprenticeship approval.");
+
+        return result;
+    }
+}
