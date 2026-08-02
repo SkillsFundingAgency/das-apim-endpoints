da3095a2e57468a80bbfdb0ac29a19eda359b899
Hamzah Shakeel <hamzah.shakeel@education,gov.uk>
2026-08-02 19:16:41 +0100

    Remove obsolete rollover form value limit

--- src/Aodp/SFA.DAS.Aodp.Api/Program.cs
+++ src/Aodp/SFA.DAS.Aodp.Api/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using NServiceBus.ObjectBuilder.MSDependencyInjection;
@@ -53,12 +52,6 @@ builder.Services
     });
 
 builder.Services.AddApplicationInsightsTelemetry(configuration);
-
-builder.Services.Configure<FormOptions>(options =>
-{
-    // A rollover batch can contain 10,000 candidates with several fields per candidate.
-    options.ValueCountLimit = 100_000;
-});
 
 builder.Services.AddAuthentication(configuration);
 builder.Services.AddConfigurationOptions(configuration);
