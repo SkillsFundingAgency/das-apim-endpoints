88d162b6605ecb5241729e2ff26cdd1079f6bcf6
Witek <witek.poreda@education.gov.uk>
2026-08-14 14:47:37 +0100

    APPMAN-2722 multi select validation wip

---
+++ src/Approvals/SFA.DAS.Approvals/Application/SelectMultiple/Commands/ValidateSelectMultipleLearnerRecordsCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using SFA.DAS.Approvals.InnerApi.Requests;
+
+namespace SFA.DAS.Approvals.Application.SelectMultiple.Commands
+{
+    public class ValidateSelectMultipleLearnerRecordsCommand : IRequest
+    {
+        public long ProviderId { get; set; }
+        public long? FileUploadLogId { get; set; }
+        public List<BulkUploadAddDraftApprenticeshipRequest> CsvRecords { get; set; }
+        public UserInfo UserInfo { get; set; }
+    }
+}
