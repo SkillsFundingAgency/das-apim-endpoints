301a86b44231d2db100ab90c92f00b06fe5be7be
Afzal Hussain <afi259@hotmail.co.uk>
2026-08-17 17:37:14 +0100

    CSP-2753 Implement get provider allowed courses endpoint

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/InnerApi/Requests/GetProviderAllowedCoursesRequestTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using SFA.DAS.AdminRoatp.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Types.InnerApi;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.InnerApi.Requests;
+
+public class GetProviderAllowedCoursesRequestTests
+{
+    [Test, MoqAutoData]
+    public void WhenRequestIsCreated_ThenGetUrlIsCorrect(
+        int ukprn,
+        CourseType courseType)
+    {
+        // Arrange
+        var sut = new GetProviderAllowedCoursesRequest(ukprn, courseType);
+
+        // Act
+        var result = sut.GetUrl;
+
+        // Assert
+        result.Should().Be($"providers/{ukprn}/allowed-courses?courseType={courseType}");
+    }
+}
