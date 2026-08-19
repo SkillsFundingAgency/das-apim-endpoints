e64a20657ab2d3f80bf0ecf8269bbce3351ede85
Afzal Hussain <afi259@hotmail.co.uk>
2026-08-19 13:24:33 +0100

    CSP-2754 Implement get provider availabel courses endpoint

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Queries/GetProviderAvailableCourses/GetProviderAvailableCoursesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using SFA.DAS.SharedOuterApi.Types.InnerApi;
+
+namespace SFA.DAS.AdminRoatp.Application.Queries.GetProviderAvailableCourses;
+
+public class GetProviderAvailableCoursesQuery : IRequest<GetProviderAvailableCoursesQueryResult>
+{
+    public int Ukprn { get; set; }
+    public CourseType? CourseType { get; set; }
+}
