dca63f062a7439c8ac9aadee350425e3c4d184d5
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-08-21 11:14:57 +0100

    Add saved vacancy metric and refactor SaveVacancy handler
    Added IncreaseVacancySaved metric to IMetrics and implementation, with OpenTelemetry counter registration. Updated SaveVacancyCommandHandler to increment metric on successful save and refactored vacancy reference extraction. Enhanced unit tests to verify metric behavior. Declared VacancySavedCounterName constant and refactored some fields to const in Constants.cs.

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Vacancies/SaveVacancy/SaveVacancyCommandHandler.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Vacancies/SaveVacancy/SaveVacancyCommandHandler.cs
@@ -1,38 +1,45 @@
 ﻿using MediatR;
+using SFA.DAS.Apim.Shared.Extensions;
 using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
 using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses;
 using SFA.DAS.SharedOuterApi.Types.Configuration;
-
-using SFA.DAS.Apim.Shared.Extensions;
 using SFA.DAS.SharedOuterApi.Types.Interfaces;
-using SFA.DAS.Apim.Shared.Interfaces;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using SFA.DAS.FindAnApprenticeship.Services;
 
 namespace SFA.DAS.FindAnApprenticeship.Application.Commands.Vacancies.SaveVacancy
 {
-    public record SaveVacancyCommandHandler(ICandidateApiClient<CandidateApiConfiguration> CandidateApiClient) : IRequestHandler<SaveVacancyCommand, SaveVacancyCommandResult>
+    public class SaveVacancyCommandHandler(
+        ICandidateApiClient<CandidateApiConfiguration> candidateApiClient,
+        IMetrics metrics) : IRequestHandler<SaveVacancyCommand, SaveVacancyCommandResult>
     {
         public async Task<SaveVacancyCommandResult> Handle(SaveVacancyCommand request, CancellationToken cancellationToken)
         {
+            // Avoid string array allocation from Split
+            var vacancyId = request.VacancyId;
+            var separatorIndex = vacancyId.IndexOf('-');
+            var vacancyReference = separatorIndex >= 0
+                ? vacancyId[..separatorIndex]
+                : vacancyId;
+
+            var createdOn = DateTime.UtcNow;
+
             var postData = new PostSavedVacancyApiRequestData
             {
-                VacancyId = request.VacancyId,
-                VacancyReference = request.VacancyId.Split('-')[0],
-                CreatedOn = DateTime.UtcNow
+                VacancyId = vacancyId,
+                VacancyReference = vacancyReference,
+                CreatedOn = createdOn
             };
 
             var postRequest = new PutSavedVacancyApiRequest(request.CandidateId, postData);
 
-            var response = await CandidateApiClient.PutWithResponseCode<PutSavedVacancyApiResponse>(postRequest);
+            var response = await candidateApiClient.PutWithResponseCode<PutSavedVacancyApiResponse>(postRequest);
 
             response.EnsureSuccessStatusCode();
 
-            if ((int)response.StatusCode > 300)
-            {
-                throw new InvalidOperationException();
-            }
+            metrics.IncreaseVacancySaved(vacancyReference);
 
             return response.Body;
         }
