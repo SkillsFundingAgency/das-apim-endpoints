f487976bb80f35d5a599803dfb83e88ad2512b6d
afzal-hussain259 <afi259@hotmail.co.uk>
2026-08-24 15:00:58 +0100

    CSP-2753 Implement get provider allowed courses endpoint (#3059)
    * CSP-2753 Implement get provider allowed courses endpoint
    
    * CSP-2753 comments addressed
    
    * CSP-2754 get available provider courses (#3064)
    
    * CSP-2754 Implement get provider availabel courses endpoint
    
    * CSP-2754 Added test coverage
    
    * CSP-2754 added exception is course type is null

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/InnerApi/Requests/GetProviderAllowedCoursesRequestTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using SFA.DAS.AdminRoatp.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Types.InnerApi;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.InnerApi.Requests;
+
+public class GetProviderAllowedCoursesRequestTests
+{
+    [Test]
+    [MoqInlineAutoData(CourseType.Apprenticeship)]
+    [MoqInlineAutoData(CourseType.ShortCourse)]
+    [MoqInlineAutoData(null)]
+    public void WhenRequestIsCreated_ThenGetUrlIsCorrect(
+        int ukprn,
+        CourseType? courseType)
+    {
+        // Arrange
+        var sut = new GetProviderAllowedCoursesRequest(ukprn, courseType);
+
+        // Act
+        var result = sut.GetUrl;
+
+        // Assert
+        result.Should().Be($"providers/{ukprn}/allowed-courses?courseType={courseType}");
+    }
+}
