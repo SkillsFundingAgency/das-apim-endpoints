11390e708a514028798c29b4056d4b46d9b801cb
Deven Shah <devenshah1974@gmail.com>
2026-08-25 15:08:25 +0100

    CSP-2767 Refarored available courses endpoint

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement.UnitTests/Application/Standards/Queries/GetAvailableCoursesForProvider/GetAvailableCoursesForProviderQueryHandlerTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.RoatpCourseManagement.Application.Standards.Queries.GetAvailableCoursesForProvider;
+using SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+using SFA.DAS.SharedOuterApi.Types.Configuration;
+using SFA.DAS.SharedOuterApi.Types.InnerApi;
+using SFA.DAS.SharedOuterApi.Types.Interfaces;
+using SFA.DAS.Testing.AutoFixture;
+
+namespace SFA.DAS.RoatpCourseManagement.UnitTests.Application.Standards.Queries.GetAvailableCoursesForProvider;
+
+[TestFixture]
+public class GetAvailableCoursesForProviderQueryHandlerTests
+{
+    [Test, MoqAutoData]
+    public async Task Handle_EmptyProviderCourses_ReturnsAllStandards(
+        [Frozen] Mock<IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration>> apiClientMock,
+        GetAvailableCoursesForProviderQueryHandler sut,
+        GetAvailableCoursesForProviderQueryResult expected,
+        int ukprn)
+    {
+        GetAvailableCoursesForProviderQuery request = new(ukprn, CourseType.Apprenticeship);
+        apiClientMock.Setup(a => a.Get<GetAvailableCoursesForProviderQueryResult>(It.IsAny<GetAvailableCoursesForProviderRequest>())).ReturnsAsync(expected);
+
+        var result = await sut.Handle(request, new CancellationToken());
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/GetAvailableCoursesForProviderRequest.cs
@@ -0,0 +1,16 @@
+﻿using SFA.DAS.Apim.Shared.Interfaces;
+using SFA.DAS.SharedOuterApi.Types.InnerApi;
+
+namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+
+public class GetAvailableCoursesForProviderRequest : IGetApiRequest
+{
+    private int _ukprn;
+    private CourseType _courseType;
+    public GetAvailableCoursesForProviderRequest(int ukprn, CourseType courseType)
+    {
+        _ukprn = ukprn;
+        _courseType = courseType;
+    }
+    public string GetUrl => $"providers/{_ukprn}/course-types/{_courseType}/available-courses";
+}
