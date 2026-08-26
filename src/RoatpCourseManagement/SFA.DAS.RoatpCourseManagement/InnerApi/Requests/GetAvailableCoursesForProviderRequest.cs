d3bab0fefd6b9db5860ac82fbd3a965aa892b505
Deven Shah <devenshah1974@gmail.com>
2026-08-26 15:38:08 +0100

    CSP-2767 Refactored available courses endpoint (#3078)
    * CSP-2767 Refarored available courses endpoint
    
    * CSP-2767 Added missing coverage

---
+++ src/RoatpCourseManagement/SFA.DAS.RoatpCourseManagement/InnerApi/Requests/GetAvailableCoursesForProviderRequest.cs
@@ -0,0 +1,16 @@
+﻿using SFA.DAS.Apim.Shared.Interfaces;
+using SFA.DAS.SharedOuterApi.Types.InnerApi;
+
+namespace SFA.DAS.RoatpCourseManagement.InnerApi.Requests;
+
+public class GetAvailableCoursesForProviderRequest : IGetApiRequest
+{
+    private readonly int _ukprn;
+    private readonly CourseType _courseType;
+    public GetAvailableCoursesForProviderRequest(int ukprn, CourseType courseType)
+    {
+        _ukprn = ukprn;
+        _courseType = courseType;
+    }
+    public string GetUrl => $"providers/{_ukprn}/course-types/{_courseType}/available-courses";
+}
