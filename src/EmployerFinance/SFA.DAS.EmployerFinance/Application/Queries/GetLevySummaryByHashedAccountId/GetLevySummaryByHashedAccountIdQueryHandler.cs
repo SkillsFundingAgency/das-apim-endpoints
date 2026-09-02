dc4a086c42266ee8fb9b4c7a7795e02ff326d135
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-09-02 13:59:25 +0100

    Add API for levy summary by hashed account ID
    Introduced FinanceLevyController with GetLevySummary action using MediatR to retrieve levy summary by hashed account ID. Added query, handler, result/response/request records, and unit tests for controller, handler, and API URL. Updated solution to include new shared projects and ensured .NET conventions.

---
+++ src/EmployerFinance/SFA.DAS.EmployerFinance/Application/Queries/GetLevySummaryByHashedAccountId/GetLevySummaryByHashedAccountIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using SFA.DAS.EmployerFinance.InnerApi.Responses;
+using SFA.DAS.SharedOuterApi.Types.Configuration;
+using SFA.DAS.SharedOuterApi.Types.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+using SFA.DAS.EmployerFinance.InnerApi.Requests;
+
+namespace SFA.DAS.EmployerFinance.Application.Queries.GetLevySummaryByHashedAccountId;
+
+public class GetLevySummaryByHashedAccountIdQueryHandler(IFinanceApiClient<FinanceApiConfiguration> financeApiClient)
+    : IRequestHandler<GetLevySummaryByHashedAccountIdQuery, GetLevySummaryByHashedAccountIdQueryResult>
+{
+    public async Task<GetLevySummaryByHashedAccountIdQueryResult> Handle(GetLevySummaryByHashedAccountIdQuery request, CancellationToken cancellationToken)
+    {
+        var levySummary = await financeApiClient.Get<GetLevySummaryByHashedAccountIdResponse>(new GetLevySummaryByHashedAccountIdRequest(request.HashedAccountId));
+        return new GetLevySummaryByHashedAccountIdQueryResult
+        {
+            CurrentLevyFunds = levySummary.CurrentLevyFunds
+        };
+    }
+}
---
+++ src/EmployerFinance/SFA.DAS.EmployerFinance/Application/Queries/GetLevySummaryByHashedAccountId/GetLevySummaryByHashedAccountIdQueryResult.cs
@@ -0,0 +1,6 @@
+﻿namespace SFA.DAS.EmployerFinance.Application.Queries.GetLevySummaryByHashedAccountId;
+
+public sealed record GetLevySummaryByHashedAccountIdQueryResult
+{
+    public decimal CurrentLevyFunds { get; init; }
+}
