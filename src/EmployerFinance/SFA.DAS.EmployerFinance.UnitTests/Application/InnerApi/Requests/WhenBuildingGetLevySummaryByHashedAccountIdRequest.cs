de5c0dd42b7afb008b5a0ed6ec5bcfb9ef973164
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-09-02 16:12:04 +0100

    Minor corrections

--- src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/InnerApi/Requests/WhenBuildingGetLevySummaryByHashedAccountIdRequest.cs
+++ src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/InnerApi/Requests/WhenBuildingGetLevySummaryByHashedAccountIdRequest.cs
@@ -5,10 +5,9 @@ namespace SFA.DAS.EmployerFinance.UnitTests.Application.InnerApi.Requests;
 [TestFixture]
 internal class WhenBuildingGetLevySummaryByHashedAccountIdRequest
 {
-    [Test]
-    public void Then_The_Request_Url_Is_Correctly_Formed()
+    [Test, MoqAutoData]
+    public void Then_The_Request_Url_Is_Correctly_Formed(string hashedAccountId)
     {
-        var hashedAccountId = "ABC123";
         var request = new GetLevySummaryByHashedAccountIdRequest(hashedAccountId);
 
         request.GetUrl.Should().Be($"api/accounts/{hashedAccountId}/levy/summary");
