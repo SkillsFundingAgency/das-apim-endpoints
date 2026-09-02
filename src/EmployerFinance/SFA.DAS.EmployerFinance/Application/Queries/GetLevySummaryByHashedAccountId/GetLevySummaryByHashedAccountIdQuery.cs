e4447104ce17a77d44350a2180f16a58a029e521
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-09-02 13:59:25 +0100

    Add API for levy summary by hashed account ID
    Introduced FinanceLevyController with GetLevySummary action using MediatR to retrieve levy summary by hashed account ID. Added query, handler, result/response/request records, and unit tests for controller, handler, and API URL. Updated solution to include new shared projects and ensured .NET conventions.

---
+++ src/EmployerFinance/SFA.DAS.EmployerFinance/Application/Queries/GetLevySummaryByHashedAccountId/GetLevySummaryByHashedAccountIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace SFA.DAS.EmployerFinance.Application.Queries.GetLevySummaryByHashedAccountId;
+
+public sealed record GetLevySummaryByHashedAccountIdQuery(string HashedAccountId)
+    : IRequest<GetLevySummaryByHashedAccountIdQueryResult>;
---
+++ src/EmployerFinance/SFA.DAS.EmployerFinance/InnerApi/Responses/GetLevySummaryByHashedAccountIdResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace SFA.DAS.EmployerFinance.InnerApi.Responses;
+
+public record GetLevySummaryByHashedAccountIdResponse
+{
+    public decimal CurrentLevyFunds { get; set; }
+}
