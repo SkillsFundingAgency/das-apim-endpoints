f6d57585985f9af679619b6d6bae1f3fc8556376
killij <51908793+killij@users.noreply.github.com>
2026-09-09 15:20:23 +0100

    FAI-3663 Fix query string formatting

--- src/RecruitQa/SFA.DAS.RecruitQa/InnerApi/Requests/GetVacancyReviewsByUserRequest.cs
+++ src/RecruitQa/SFA.DAS.RecruitQa/InnerApi/Requests/GetVacancyReviewsByUserRequest.cs
@@ -1,10 +1,15 @@
 using System.Web;
-using SFA.DAS.SharedOuterApi.Types.Interfaces;
+using Microsoft.AspNetCore.WebUtilities;
 using SFA.DAS.Apim.Shared.Interfaces;
 
 namespace SFA.DAS.RecruitQa.InnerApi.Requests;
 
 public class GetVacancyReviewsByUserRequest(string userId, DateTime? assignationExpiry, string? reviewStatus) : IGetApiRequest
 {
-    public string GetUrl => $"api/users/vacancyreviews?assignationExpiry={assignationExpiry:yyyy-MMM-dd HH:mm:ss}&status={reviewStatus}&userId={HttpUtility.UrlEncode(userId)}";
+    public string GetUrl => QueryHelpers.AddQueryString("api/users/vacancyreviews", new Dictionary<string, string?>
+    {
+        ["assignationExpiry"] = assignationExpiry is null ? null : $"{assignationExpiry:O}",
+        ["status"] = reviewStatus,
+        ["userId"] = userId,
+    });
 }
