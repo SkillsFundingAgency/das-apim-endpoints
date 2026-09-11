d7faa9a861bc4b3c9c5c808b75e82a9d64f95287
Afzal Hussain <afi259@hotmail.co.uk>
2026-09-11 10:56:26 +0100

    CSP-2756 Fixed sonar issues

--- src/AdminRoatp/SFA.DAS.AdminRoatp/InnerApi/Models/AddCourseTypesModel.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/InnerApi/Models/AddCourseTypesModel.cs
@@ -4,7 +4,7 @@ namespace SFA.DAS.AdminRoatp.InnerApi.Models;
 
 public class AddCourseTypesModel
 {
-    public IEnumerable<CourseType> CourseTypes { get; set; }
-    public string UserId { get; set; }
-    public string UserDisplayName { get; set; }
+    public IEnumerable<CourseType> CourseTypes { get; set; } = [];
+    public string UserId { get; set; } = string.Empty;
+    public string UserDisplayName { get; set; } = string.Empty;
 }
