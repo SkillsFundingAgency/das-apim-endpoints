1b11eb59e5b2f2571b9dcd226e0af06167341c0b
afzal-hussain259 <afi259@hotmail.co.uk>
2026-09-11 10:05:32 +0100

    CSP-2744 Implements changes to provider allowed course (#3084)
    * CSP-2744 Implement changes to provider allowed course
    
    * CSP-2743 Fixed CourseType int values to match backend
    
    * CSP-2744 amdned to use enum value
    
    * CSP-2744 refactored to change from upsert to add
    
    * CSP-2744 updated test name
    
    * CSP-2744 further changes to test names
    
    * CSP-2744 Amended v1 sync call to send Apprenticeship course type and userdisplayname as userid.
    
    ---------
    
    Co-authored-by: Deven Shah <devenshah1974@gmail.com>

--- src/AdminRoatp/SFA.DAS.AdminRoatp/InnerApi/Requests/AddProviderAllowedCourseRequest.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/InnerApi/Requests/AddProviderAllowedCourseRequest.cs
@@ -3,13 +3,13 @@ using SFA.DAS.Apim.Shared.Interfaces;
 
 namespace SFA.DAS.AdminRoatp.InnerApi.Requests;
 
-public class UpsertProviderAllowedCourseRequest : IPostApiRequest
+public class AddProviderAllowedCourseRequest : IPostApiRequest
 {
     public int Ukprn { get; set; }
     public string LarsCode { get; set; }
     public object Data { get; set; }
     public string PostUrl => $"providers/{Ukprn}/allowed-courses/{LarsCode}";
-    public UpsertProviderAllowedCourseRequest(int ukprn, string larsCode, UpsertProviderAllowedCourseModel data)
+    public AddProviderAllowedCourseRequest(int ukprn, string larsCode, AddProviderAllowedCourseModel data)
     {
         Ukprn = ukprn;
         LarsCode = larsCode;
