e713630b0dfcc44c6e0f0e8056024cc272f98bdf
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-09-15 16:56:25 +0100

    ✨ feat: add employer funding projection API endpoint
    1. Added FundingProjectionController with GET endpoint for employer funding projection by account ID, including error handling and logging
    2. Implemented GetEmployerFundingProjectionByAccountIdRequest, Response, Query, QueryHandler, and QueryResult classes
    3. Registered MediatR and new query handler in service registration extension
    4. Added unit tests for controller, query handler, and request URL construction
    5. Updated AddConfigurationOptionsExtension for correct options retrieval
    6. Updated solution and project files to include new unit test project and references
    
    Changes made by Balaji Jambulingam

---
+++ src/EmployerFinanceJobs/SFA.DAS.EmployerFinanceJobs.Api.UnitTests/InnerApi/WhenBuildingGetEmployerFundingProjectionByAccountIdRequest.cs
@@ -0,0 +1,22 @@
+﻿using SFA.DAS.EmployerFinanceJobs.InnerApi.Requests;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SFA.DAS.EmployerFinanceJobs.UnitTests.InnerApi;
+
+[TestFixture]
+internal class WhenBuildingGetEmployerFundingProjectionByAccountIdRequest
+{
+    [Test]
+    public void Then_The_Url_Is_Correctly_Built()
+    {
+        // Arrange
+        var accountId = 123456;
+        var request = new GetEmployerFundingProjectionByAccountIdRequest(accountId);
+        // Act
+        var url = request.GetUrl;
+        // Assert
+        Assert.That(url, Is.EqualTo($"api/employer/{accountId}/funding-projection"));
+    }
+}
---
+++ src/EmployerFinanceJobs/SFA.DAS.EmployerFinanceJobs/Queries/GetEmployerFundingProjectionByAccountId/GetEmployerFundingProjectionByAccountIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace SFA.DAS.EmployerFinanceJobs.Queries.GetEmployerFundingProjectionByAccountId;
+
+public sealed record GetEmployerFundingProjectionByAccountIdQuery(long AccountId)
+    : IRequest<GetEmployerFundingProjectionByAccountIdQueryResult>;
