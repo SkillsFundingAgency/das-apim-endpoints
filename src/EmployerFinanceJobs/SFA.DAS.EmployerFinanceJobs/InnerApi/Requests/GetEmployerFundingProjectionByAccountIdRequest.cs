1ca2e936ca4eda38bc0b56599687685a528085fc
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-09-15 17:38:07 +0100

    ⏪ revert: remove EmployerFundingProjection API & tests
    1. Removed FundingProjectionController and related API/query records
    2. Deleted MediatR query, handler, and result for funding projection
    3. Removed all unit tests for controller and query handler
    4. Cleaned up MediatR service registration in AddServiceRegistrationExtension
    5. Updated csproj files to add folder structure for removed components
    6. Cleaned up using statements in AddServiceRegistrationExtension
    
    Changes made by Balaji Jambulingam

--- src/EmployerFinanceJobs/SFA.DAS.EmployerFinanceJobs/InnerApi/Requests/GetEmployerFundingProjectionByAccountIdRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using SFA.DAS.Apim.Shared.Interfaces;
-
-namespace SFA.DAS.EmployerFinanceJobs.InnerApi.Requests;
-
-public sealed record GetEmployerFundingProjectionByAccountIdRequest(long AccountId) : IGetApiRequest
-{
-    public string GetUrl => $"api/employer/{AccountId}/funding-projection";
-}
--- src/EmployerFinanceJobs/SFA.DAS.EmployerFinanceJobs/Queries/GetEmployerFundingProjectionByAccountId/GetEmployerFundingProjectionByAccountIdQuery.cs
+++
@@ -1,6 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.EmployerFinanceJobs.Queries.GetEmployerFundingProjectionByAccountId;
-
-public sealed record GetEmployerFundingProjectionByAccountIdQuery(long AccountId)
-    : IRequest<GetEmployerFundingProjectionByAccountIdQueryResult>;
