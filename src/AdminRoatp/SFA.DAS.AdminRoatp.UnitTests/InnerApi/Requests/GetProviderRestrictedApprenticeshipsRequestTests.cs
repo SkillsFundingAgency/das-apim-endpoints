cfb8588b6a54f707994dcac2adaec5df58993a2c
afzal-hussain259 <afi259@hotmail.co.uk>
2026-09-16 10:22:36 +0100

    CSP-2769 Implement get provider resticted apprenticeships endpoint (#3127)

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.UnitTests/InnerApi/Requests/GetProviderRestrictedApprenticeshipsRequestTests.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture.NUnit3;
+using FluentAssertions;
+using SFA.DAS.AdminRoatp.InnerApi.Requests;
+
+namespace SFA.DAS.AdminRoatp.UnitTests.InnerApi.Requests;
+
+public class GetProviderRestrictedApprenticeshipsRequestTests
+{
+    [Test, AutoData]
+    public void WhenCreatingRequest_ThenSetsCorrectUrl(int ukprn)
+    {
+        // Arrange
+        var sut = new GetProviderRestrictedApprenticeshipsRequest(ukprn);
+
+        // Assert
+        sut.GetUrl.Should().Be($"providers/{ukprn}/restricted-apprenticeships");
+    }
+}
