621c3d0d83ad6cc37252ae3d6266e318f381fa1a
Ravi Udari <95303213+raviudari01@users.noreply.github.com>
2026-09-16 10:10:10 +0100

    CSP-2637 ReApplying renamed CourseTypeName to CourseType and remove CourseTypeId change (#3126)
    CSP-2637 ReApplying renamed CourseTypeName to CourseType and remove CourseTypeId change (#3126)

--- src/AdminRoatp/SFA.DAS.AdminRoatp/InnerApi/Models/ProviderCourseTypeModel.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/InnerApi/Models/ProviderCourseTypeModel.cs
@@ -4,7 +4,6 @@ namespace SFA.DAS.AdminRoatp.InnerApi.Models;
 
 public class ProviderCourseTypeModel
 {
-    public int CourseTypeId { get; set; }
     public CourseType CourseType { get; set; }
     public bool IsRestricted { get; set; }
     public int? RestrictedCount { get; set; }
