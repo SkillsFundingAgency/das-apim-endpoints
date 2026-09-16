e82991c30616bfb1b13266bb467ee661a4761d45
killij <51908793+killij@users.noreply.github.com>
2026-09-16 09:03:44 +0100

    FAI-3625 Small refactor to try and satisfy Sonar

--- src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Candidate/CreateCandidateCommandHandler.cs
+++ src/FindAnApprenticeship/SFA.DAS.FindAnApprenticeship/Application/Commands/Candidate/CreateCandidateCommandHandler.cs
@@ -9,6 +9,7 @@ using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Requests;
 using SFA.DAS.FindAnApprenticeship.InnerApi.CandidateApi.Responses;
 using SFA.DAS.SharedOuterApi.Types.Configuration;
 using SFA.DAS.Apim.Shared.Extensions;
+using SFA.DAS.Apim.Shared.Models;
 using SFA.DAS.SharedOuterApi.Types.Interfaces;
 
 namespace SFA.DAS.FindAnApprenticeship.Application.Commands.Candidate;
@@ -19,37 +20,7 @@ public class CreateCandidateCommandHandler(
 {
     public async Task<CreateCandidateCommandResult> Handle(CreateCandidateCommand request, CancellationToken cancellationToken)
     {
-        var existingUser = await candidateApiClient.GetWithResponseCode<GetCandidateApiResponse>(new GetCandidateApiRequest(request.GovUkIdentifier));
-
-        if (existingUser.StatusCode == HttpStatusCode.NotFound)
-        {
-            existingUser = await candidateApiClient.GetWithResponseCode<GetCandidateApiResponse>(new GetCandidateByEmailAddressApiRequest(request.Email));
-            switch (existingUser.StatusCode)
-            {
-                case HttpStatusCode.NotFound:
-                    break;
-                case HttpStatusCode.OK:
-                    if (existingUser.Body.GovUkIdentifier is not null && existingUser.Body.GovUkIdentifier != request.GovUkIdentifier)
-                    {
-                        throw new NotSupportedException("GovUkIdentifier is already set. If it requires updating, support need to set the existing value to null.");
-                    }
-                    
-                    if (existingUser.Body.GovUkIdentifier is null)
-                    {
-                        // patch the user
-                        var patchDocument = new JsonPatchDocument<PatchableCandidate>();
-                        patchDocument.Replace(x => x.GovUkIdentifier, request.GovUkIdentifier);
-                        patchDocument.Replace(x => x.UpdatedOn, DateTime.UtcNow);
-                        var patchUserRequest = new PatchCandidateApiRequest(existingUser.Body.Id, patchDocument);
-                        var patchResponse = await candidateApiClient.PatchWithResponseCode(patchUserRequest);
-                        patchResponse.EnsureSuccessStatusCode();
-                        existingUser.Body.GovUkIdentifier = request.GovUkIdentifier;
-                    }
-                    break;
-                default:
-                    throw new NotSupportedException("There was a problem retrieving the candidate by their email address. This could possibly be due to there being multiple accounts with the same email address.");
-            }
-        }
+        var existingUser = await LookupCandidateAsync(request.GovUkIdentifier, request.Email);
 
         if (existingUser.StatusCode != HttpStatusCode.NotFound)
         {
@@ -105,5 +76,48 @@ public class CreateCandidateCommandHandler(
             PhoneNumber = candidateResult.Body.PhoneNumber,
             Status = UserStatus.Incomplete
         };
+    }
+
+    private async Task<ApiResponse<GetCandidateApiResponse>> LookupCandidateAsync(string govUkIdentifier, string emailAddress)
+    {
+        var existingUser = await candidateApiClient.GetWithResponseCode<GetCandidateApiResponse>(new GetCandidateApiRequest(govUkIdentifier));
+        if (existingUser.StatusCode == HttpStatusCode.NotFound)
+        {
+            existingUser = await LookupCandidateByEmailAddressAsync(emailAddress, govUkIdentifier);
+        }
+        
+        return existingUser;
+    }
+
+    private async Task<ApiResponse<GetCandidateApiResponse>> LookupCandidateByEmailAddressAsync(string emailAddress, string govUkIdentifier)
+    {
+        var existingUser = await candidateApiClient.GetWithResponseCode<GetCandidateApiResponse>(new GetCandidateByEmailAddressApiRequest(emailAddress));
+        switch (existingUser.StatusCode)
+        {
+            case HttpStatusCode.NotFound:
+                break;
+            case HttpStatusCode.OK:
+                if (existingUser.Body.GovUkIdentifier is not null && existingUser.Body.GovUkIdentifier != govUkIdentifier)
+                {
+                    throw new NotSupportedException("GovUkIdentifier is already set. If it requires updating, support need to set the existing value to null.");
+                }
+                    
+                if (existingUser.Body.GovUkIdentifier is null)
+                {
+                    // patch the user
+                    var patchDocument = new JsonPatchDocument<PatchableCandidate>();
+                    patchDocument.Replace(x => x.GovUkIdentifier, govUkIdentifier);
+                    patchDocument.Replace(x => x.UpdatedOn, DateTime.UtcNow);
+                    var patchUserRequest = new PatchCandidateApiRequest(existingUser.Body.Id, patchDocument);
+                    var patchResponse = await candidateApiClient.PatchWithResponseCode(patchUserRequest);
+                    patchResponse.EnsureSuccessStatusCode();
+                    existingUser.Body.GovUkIdentifier = govUkIdentifier;
+                }
+                break;
+            default:
+                throw new NotSupportedException("There was a problem retrieving the candidate by their email address. This could possibly be due to there being multiple accounts with the same email address.");
+        }
+
+        return existingUser;
     }
 }
