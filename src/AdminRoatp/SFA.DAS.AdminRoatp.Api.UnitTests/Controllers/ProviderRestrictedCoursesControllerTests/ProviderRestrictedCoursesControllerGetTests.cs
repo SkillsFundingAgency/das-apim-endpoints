c18353e45db2080cab6f81b27a6bed9a6f590b47
Afzal Hussain <afi259@hotmail.co.uk>
2026-09-18 10:12:50 +0100

    CSP-2771 Implement get privider not restricted appreticeships endpoint

--- src/AdminRoatp/SFA.DAS.AdminRoatp.Api.UnitTests/Controllers/ProviderRestrictedCoursesControllerTests/ProviderRestrictedCoursesControllerGetTests.cs
+++ src/AdminRoatp/SFA.DAS.AdminRoatp.Api.UnitTests/Controllers/ProviderRestrictedCoursesControllerTests/ProviderRestrictedCoursesControllerGetTests.cs
@@ -4,6 +4,7 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using SFA.DAS.AdminRoatp.Api.Controllers;
+using SFA.DAS.AdminRoatp.Application.Queries.GetProviderNotRestrictedApprenticeships;
 using SFA.DAS.AdminRoatp.Application.Queries.GetProviderRestrictedApprenticeships;
 using SFA.DAS.AdminRoatp.InnerApi.Responses;
 using SFA.DAS.Testing.AutoFixture;
@@ -52,6 +53,49 @@ public class ProviderRestrictedCoursesControllerGetTests
         // Assert
         mediatorMock.Verify(x => x.Send(
             It.Is<GetProviderRestrictedApprenticeshipsQuery>(q => q.Ukprn == ukprn), It.IsAny<CancellationToken>()),
+            Times.Once);
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenGetProviderNotRestrictedApprenticeshipsIsInvoked_ThenReturnsOkResult(
+        [Frozen] Mock<IMediator> mediatorMock,
+        [Greedy] ProviderRestrictedCoursesController sut,
+        GetProviderNotRestrictedApprenticeshipsResponse response,
+        int ukprn)
+    {
+        // Arrange
+        mediatorMock
+            .Setup(x => x.Send(It.IsAny<GetProviderNotRestrictedApprenticeshipsQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+
+        // Act
+        var result = await sut.GetProviderNotRestrictedApprenticeships(ukprn, CancellationToken.None);
+
+        // Assert
+        result.Should().BeOfType<OkObjectResult>();
+
+        var okResult = result as OkObjectResult;
+        okResult!.Value.Should().Be(response);
+    }
+
+    [Test, MoqAutoData]
+    public async Task WhenGetProviderNotRestrictedApprenticeshipsIsInvoked_ThenMediatorIsCalled(
+        [Frozen] Mock<IMediator> mediatorMock,
+        [Greedy] ProviderRestrictedCoursesController sut,
+        GetProviderNotRestrictedApprenticeshipsResponse response,
+        int ukprn)
+    {
+        // Arrange
+        mediatorMock
+            .Setup(x => x.Send(It.Is<GetProviderNotRestrictedApprenticeshipsQuery>(q => q.Ukprn == ukprn), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(response);
+
+        // Act
+        await sut.GetProviderNotRestrictedApprenticeships(ukprn, CancellationToken.None);
+
+        // Assert
+        mediatorMock.Verify(x => x.Send(
+            It.Is<GetProviderNotRestrictedApprenticeshipsQuery>(q => q.Ukprn == ukprn), It.IsAny<CancellationToken>()),
             Times.Once);
     }
 }
