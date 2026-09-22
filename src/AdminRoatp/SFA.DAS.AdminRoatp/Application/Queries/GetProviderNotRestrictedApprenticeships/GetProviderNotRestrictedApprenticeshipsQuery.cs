fb56da9edb4997176c07ac665abeaae4f4d80956
afzal-hussain259 <afi259@hotmail.co.uk>
2026-09-22 17:29:15 +0100

    CSP-2771 Implement get provider not restricted apprenticeships endpoint (#3130)
    * CSP-2771 Implement get privider not restricted appreticeships endpoint
    
    * CSP-2771 fixed sonar issue

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Queries/GetProviderNotRestrictedApprenticeships/GetProviderNotRestrictedApprenticeshipsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using SFA.DAS.AdminRoatp.InnerApi.Responses;
+
+namespace SFA.DAS.AdminRoatp.Application.Queries.GetProviderNotRestrictedApprenticeships;
+
+public class GetProviderNotRestrictedApprenticeshipsQuery : IRequest<GetProviderNotRestrictedApprenticeshipsResponse>
+{
+    public int Ukprn { get; set; }
+}
---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Queries/GetProviderNotRestrictedApprenticeships/GetProviderNotRestrictedApprenticeshipsQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.AdminRoatp.InnerApi.Requests;
+using SFA.DAS.AdminRoatp.InnerApi.Responses;
+using SFA.DAS.Apim.Shared.Extensions;
+using SFA.DAS.SharedOuterApi.Types.Configuration;
+using SFA.DAS.SharedOuterApi.Types.Interfaces;
+
+namespace SFA.DAS.AdminRoatp.Application.Queries.GetProviderNotRestrictedApprenticeships;
+
+public class GetProviderNotRestrictedApprenticeshipsQueryHandler(IRoatpCourseManagementApiClient<RoatpV2ApiConfiguration> _courseManagementApiClient, ILogger<GetProviderNotRestrictedApprenticeshipsQueryHandler> _logger) : IRequestHandler<GetProviderNotRestrictedApprenticeshipsQuery, GetProviderNotRestrictedApprenticeshipsResponse>
+{
+    public async Task<GetProviderNotRestrictedApprenticeshipsResponse> Handle(GetProviderNotRestrictedApprenticeshipsQuery request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Handle GetProviderNotRestrictedApprenticeships request for Ukprn {Ukprn}", request.Ukprn);
+
+        var response = await _courseManagementApiClient.GetWithResponseCode<GetProviderNotRestrictedApprenticeshipsResponse>(new GetProviderNotRestrictedApprenticeshipsRequest(request.Ukprn));
+
+        response.EnsureSuccessStatusCode();
+
+        return response.Body;
+    }
+}
