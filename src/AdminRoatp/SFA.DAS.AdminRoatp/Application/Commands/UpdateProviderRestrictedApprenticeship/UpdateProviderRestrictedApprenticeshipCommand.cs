008e6c0e51e3003b8f7afad2b14c6b0fb1cc22c6
afzal-hussain259 <afi259@hotmail.co.uk>
2026-09-25 10:26:36 +0100

    CSP-2802 Implement update provider restricted appreticeship endpoint (#3137)
    Co-authored-by: Sudhakar Chinoor <45876897+sudhakarchinoor@users.noreply.github.com>

---
+++ src/AdminRoatp/SFA.DAS.AdminRoatp/Application/Commands/UpdateProviderRestrictedApprenticeship/UpdateProviderRestrictedApprenticeshipCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace SFA.DAS.AdminRoatp.Application.Commands.UpdateProviderRestrictedApprenticeship;
+
+public class UpdateProviderRestrictedApprenticeshipCommand : IRequest
+{
+    public int Ukprn { get; set; }
+    public string LarsCode { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public string UserDisplayName { get; set; } = string.Empty;
+    public DateTime? LastDateStarts { get; set; }
+}
