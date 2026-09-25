1875227683ec9f6f2db4899c5352130872cf06a7
Balaji Jambulingam <balaji.j@hotmail.co.uk>
2026-09-25 13:56:43 +0100

    Minor unit test correction

--- src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/InnerApi/Requests/WhenBuildingGetCommittedLearnersCostByAccountIdRequest.cs
+++ src/EmployerFinance/SFA.DAS.EmployerFinance.UnitTests/Application/InnerApi/Requests/WhenBuildingGetCommittedLearnersCostByAccountIdRequest.cs
@@ -9,7 +9,7 @@ internal class WhenBuildingGetCommittedLearnersCostByAccountIdRequest
     public void Then_The_Request_Url_Is_Correctly_Formed(long accountId, int pageNumber, int pageItemCount, int? transferSenderId)
     {
         var request = new GetCommittedLearnersCostByAccountIdRequest(accountId, pageNumber, pageItemCount, transferSenderId);
-        request.GetUrl.Should().Be($"api/apprenticeships?accountId={accountId}&pageNumber={pageNumber}&pageItemCount={pageItemCount}&transferSenderId={transferSenderId}");
+        request.GetUrl.Should().Be($"api/apprenticeships?accountId={accountId}&transferSenderId={transferSenderId}&pageNumber={pageNumber}&pageItemCount={pageItemCount}");
     }
 
     [Test, MoqAutoData]
